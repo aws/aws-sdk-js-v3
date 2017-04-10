3cc21c692cf1d40ab95c110df1314182514e15d1
Jonathan Eskew <jonathan@jeskew.net>
2017-04-10 08:43:16 -0700

    Clean up master branch

--- packages/cancellation-token/index.ts
+++
@@ -1,9 +0,0 @@
-import Token from "./lib/Token";
-import TokenSource from "./lib/TokenSource";
-
-const toExport = {
-    Token: Token,
-    TokenSource: TokenSource,
-};
-
-export default toExport;
--- packages/sdk-crypto/lib/Ie11CryptoProvider/Key.ts
+++
@@ -1,8 +0,0 @@
-interface Key {
-    readonly algorithm: string;
-    readonly extractable: boolean;
-    readonly keyUsage: Array<string>;
-    readonly type: string;
-}
-
-export default Key;
