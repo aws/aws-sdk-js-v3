025ff34d01a6f9e709f756d8627925d11c895124
Jonathan Eskew <eskewj@amazon.com>
2017-05-14 14:06:29 -0700

    Add SJCL hmac module

---
+++ packages/crypto-sjcl-hmac/index.d.ts
@@ -0,0 +1,33 @@
+import {BitArray} from '@aws/crypto-sjcl-bitArray';
+
+/**
+ * HMAC with the specified hash function.
+ */
+declare class hmac {
+
+    /**
+     * @param key the key for HMAC.
+     * @param Hash The hash function to use.
+     */
+    constructor(key: BitArray, Hash?: any);
+
+    /**
+     * HMAC with the specified hash function. Alias of this::mac
+     * @param data The data to mac.
+     */
+    encrypt(data: BitArray|string): BitArray;
+
+    /**
+     * HMAC with the specified hash function.
+     * @param data The data to mac.
+     */
+    mac(data: BitArray|string): BitArray;
+
+    reset(data: BitArray): void;
+
+    update(data: BitArray): void;
+
+    digest(data: BitArray): BitArray;
+}
+
+export = hmac;
