172ec134b7b016de2d11c783550c1864d11179d9
Jonathan Eskew <eskewj@amazon.com>
2017-06-08 13:02:44 -0700

    Add Google Closure library attribution

--- packages/util-utf8-browser/lib/pureJs.ts
+++ packages/util-utf8-browser/lib/pureJs.ts
@@ -1,3 +1,10 @@
+/**
+ * Converts a JS string from its native UCS-2/UTF-16 representation into a
+ * Uint8Array of the bytes used to represent the equivalent characters in UTF-8.
+ *
+ * Cribbed from the `goog.crypt.stringToUtf8ByteArray` function in the Google
+ * Closure library, though updated to use typed arrays.
+ */
 export function fromUtf8(input: string): Uint8Array {
     const bytes: Array<number> = [];
     for (let i = 0, len = input.length; i < len; i++) {
@@ -35,6 +42,14 @@ export function fromUtf8(input: string): Uint8Array {
     return Uint8Array.from(bytes);
 }
 
+/**
+ * Converts a typed array of bytes containing UTF-8 data into a native JS
+ * string.
+ *
+ * Partly cribbed from the `goog.crypt.utf8ByteArrayToString` function in the
+ * Google Closure library, though updated to use typed arrays and to better
+ * handle astral plane code points.
+ */
 export function toUtf8(input: Uint8Array): string {
     let decoded = '';
     for (let i = 0, len = input.length; i < len; i++) {
