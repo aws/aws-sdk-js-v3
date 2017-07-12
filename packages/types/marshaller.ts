a4cda14769a4a48170fac5aad16ff623e1c52303
Jonathan Eskew <eskewj@amazon.com>
2017-07-12 09:03:57 -0700

    Add types for marshaller/unmarshaller

---
+++ packages/types/marshaller.ts
@@ -0,0 +1,28 @@
+import {Member, OperationModel} from "./protocol";
+import {HttpRequest} from "./http";
+
+export interface BodySerializer<SerializedType = string> {
+    /**
+     * Converts the provided `input` into the serialized format described in the
+     * provided `shape`.
+     *
+     * @param shape The serialization model shape to which the input should be
+     *              converted
+     * @param input The value to convert
+     *
+     * @throws if a node in the input cannot be converted into the type
+     *          specified by the serialization model
+     */
+    build(shape: Member, input: any): SerializedType;
+}
+
+export interface RequestSerializer<StreamType = Uint8Array> {
+    /**
+     * Converts the provided `input` into an HTTP request
+     *
+     * @param operation The operation to be executed via the returned HTTP
+     *                  request.
+     * @param input     The user input to serialize.
+     */
+    serialize(operation: OperationModel, input: any): HttpRequest<StreamType>;
+}
