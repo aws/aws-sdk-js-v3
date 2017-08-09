98ceacd1cb28caac753cf9dca903bc150f1a01b0
zheallan <zyp19930608@gmail.com>
2017-08-08 22:40:00 -0700

    update .gitignore

--- packages/is-iterable/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isIterable(arg) {
-    return Boolean(arg)
-        && typeof Symbol !== 'undefined'
-        && typeof arg[Symbol.iterator] === 'function';
-}
-exports.isIterable = isIterable;
-//# sourceMappingURL=index.js.map
