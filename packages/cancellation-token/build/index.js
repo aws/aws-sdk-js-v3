604a0736c5f506e15961218989d924009143ccab
zheallan <zyp19930608@gmail.com>
2017-10-24 13:43:10 -0700

    clean work space

--- packages/cancellation-token/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./Token"));
-__export(require("./TokenSource"));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSw2QkFBd0I7QUFDeEIsbUNBQThCIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0ICogZnJvbSBcIi4vVG9rZW5cIjtcbmV4cG9ydCAqIGZyb20gXCIuL1Rva2VuU291cmNlXCI7XG4iXX0=
