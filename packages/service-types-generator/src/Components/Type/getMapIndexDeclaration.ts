4e68eff3e5bca007e831339d512a3b427f6ec1f1
Jonathan Eskew <eskewj@amazon.com>
2017-10-30 18:59:19 -0700

    Only used mapped index types when the key is an enumeration

---
+++ packages/service-types-generator/src/Components/Type/getMapIndexDeclaration.ts
@@ -0,0 +1,17 @@
+import {getStringDeclaration} from "./getStringDeclaration";
+import {TreeModelMap, TreeModelShape} from "@aws/service-model";
+
+export function getMapIndexDeclaration(map: TreeModelMap): string {
+    if (map.key.shape.type !== 'string') {
+        throw new Error(
+            `Invalid type: map keys must be strings, but ${map.name} is indexed with a ${map.key.shape.type}`
+        );
+    }
+
+    const keyType = getStringDeclaration(map.key.shape);
+    if (keyType === 'string') {
+        return '[key: string]';
+    }
+
+    return `[key in ${keyType}]`;
+}
