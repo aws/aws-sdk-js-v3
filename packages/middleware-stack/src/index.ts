3ff8f060cf4f2a7446d74f1479a35fe97098ab95
Jonathan Eskew <eskewj@amazon.com>
2017-11-02 21:17:25 -0700

    Move middlewarestack interface to @aws/types

--- packages/middleware-stack/src/index.ts
+++ packages/middleware-stack/src/index.ts
@@ -1,6 +1,8 @@
 import {
     Handler,
+    HandlerOptions,
     Middleware,
+    MiddlewareStack as IMiddlewareStack,
     HandlerExecutionContext,
 } from '@aws/types';
 
@@ -15,69 +17,12 @@ interface HandlerListEntry<
     tags?: Set<string>;
 }
 
-export interface HandlerOptions {
-    /**
-     * Handlers are ordered using a "step" that describes the stage of command
-     * execution at which the handler will be executed. The available steps are:
-     *
-     * - initialize: The input is being prepared and validated. Examples of
-     *      typical initialization tasks include injecting default options and
-     *      parameter validation.
-     * - build: The input is being serialized into an HTTP request. The input
-     *      should not be altered in middleware occupying this step, as it may
-     *      have already been serialized into a request. Examples of typical
-     *      build tasks include request construction and injecting HTTP headers.
-     * - finalize: The request is being prepared to be sent over the wire. The
-     *      request in this stage should already be semantically complete and
-     *      should therefore only be altered as match the recipient's
-     *      expectations. Examples of typical finalization tasks include request
-     *      signing and injecting hop-by-hop headers.
-     *
-     *      Unlike initialization and build handlers, which are executed once
-     *      per operation execution, finalization handlers will be executed for
-     *      each HTTP request sent.
-     *
-     * @default 'build'
-     */
-    step?: Step;
-
-    /**
-     * A number that specifies how early in a given step of the middleware stack
-     * a handler should be executed. Higher numeric priorities will be executed
-     * earlier.
-     *
-     * @default 0
-     */
-    priority?: number;
-
-    /**
-     * A set of strings that identify the general purpose or important
-     * characteristics of a given handler.
-     */
-    tags?: Set<string>;
-}
-
-/**
- * Builds a single handler function from zero or more middleware classes and a
- * core handler. The core handler is meant to send command objects to AWS
- * services and return promises that will resolve with the operation result or
- * be rejected with an error.
- *
- * When a composed handler is invoked, the arguments will pass through all
- * middleware in a defined order, and the return from the innermost handler will
- * pass through all middleware in the reverse of that order.
- */
-export class MiddlewareStack<T extends Handler<any, any>> {
+export class MiddlewareStack<T extends Handler<any, any, any>> implements
+    IMiddlewareStack<T>
+{
     private readonly entries: Array<HandlerListEntry<T>> = [];
     private sorted: boolean = false;
 
-    /**
-     * Add middleware to the list, optionally specifying a step, priority, and
-     * tags.
-     *
-     * Middleware registered at the same step and with the same priority may be
-     * executed in any order.
-     */
     add(
         middleware: Middleware<T>,
         {
@@ -95,21 +40,12 @@ export class MiddlewareStack<T extends Handler<any, any>> {
         });
     }
 
-    /**
-     * Create a shallow clone of this list. Step bindings and handler priorities
-     * and tags are preserved in the copy.
-     */
     clone(): MiddlewareStack<T> {
         const clone = new MiddlewareStack<T>();
         clone.entries.push(...this.entries);
         return clone;
     }
 
-    /**
-     * Create a list containing the middlewares in this list as well as the
-     * middlewares in the `from` list. Neither source is modified, and step
-     * bindings and handler priorities and tags are preserved in the copy.
-     */
     concat(
         from: MiddlewareStack<T>
     ): MiddlewareStack<T> {
@@ -118,14 +54,6 @@ export class MiddlewareStack<T extends Handler<any, any>> {
         return clone;
     }
 
-    /**
-     * Removes middleware from the stack.
-     *
-     * If a string is provided, any entry in the stack whose tags contain that
-     * string will be removed from the stack.
-     *
-     * If a middleware class is provided, all usages thereof will be removed.
-     */
     remove(toRemove: Middleware<T>|string): boolean {
         const {length} = this.entries;
         if (typeof toRemove === 'string') {
@@ -137,16 +65,6 @@ export class MiddlewareStack<T extends Handler<any, any>> {
         return this.entries.length < length;
     }
 
-    /**
-     * Builds a single handler function from zero or more middleware classes and
-     * a core handler. The core handler is meant to send command objects to AWS
-     * services and return promises that will resolve with the operation result
-     * or be rejected with an error.
-     *
-     * When a composed handler is invoked, the arguments will pass through all
-     * middleware in a defined order, and the return from the innermost handler
-     * will pass through all middleware in the reverse of that order.
-     */
     resolve(handler: T, context: HandlerExecutionContext): T {
         if (!this.sorted) {
             this.sort();
