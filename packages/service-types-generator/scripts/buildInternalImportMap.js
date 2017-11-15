49c645d918c33ac451d98c5c7999dc67b864785c
Jonathan Eskew <eskewj@amazon.com>
2017-11-15 11:38:47 -0800

    Ensure generator can be run even if installed somewhere

---
+++ packages/service-types-generator/scripts/buildInternalImportMap.js
@@ -0,0 +1,35 @@
+const {readdirSync, readFileSync, statSync, writeFileSync} = require('fs');
+const {dirname, join} = require('path');
+
+let content = `import {Import} from '@aws/build-types';
+
+/**
+ * @internal
+ */
+export const IMPORTS: {[key: string]: Import} = {`;
+
+const packageRoot = dirname(__dirname);
+const packagesRoot = dirname(packageRoot);
+
+for (const packageName of readdirSync(packagesRoot)) {
+    const packageDir = join(packagesRoot, packageName);
+    if (statSync(packageDir).isDirectory()) {
+        const packageJson = JSON.parse(
+            readFileSync(join(packageDir, 'package.json'), 'utf8')
+        );
+        content += `
+    '${packageName}': {
+        package: '@aws/${packageName}',
+        version: '^${packageJson.version}',
+    },`;
+    }
+}
+
+content += `
+};
+`;
+
+writeFileSync(
+    join(packageRoot, 'src', 'Components', 'Client', 'internalImports.ts'),
+    content
+);
