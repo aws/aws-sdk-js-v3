974d547a6dec99a324d3a9da5a309c0d85606ed0
Jonathan Eskew <eskewj@amazon.com>
2017-11-19 10:41:56 -0800

    Use crypto feature detection rather than generic runtime detection

---
+++ packages/crypto-random-source-universal/src/browserUsage.spec.ts
@@ -0,0 +1,31 @@
+import {randomValues} from './';
+
+jest.mock('crypto', () => {
+    throw new Error('Crypto module is not defined');
+});
+
+jest.mock('@aws/crypto-random-source-browser', () => {
+    return { randomValues: jest.fn() };
+});
+import {randomValues as browserRandomValues} from '@aws/crypto-random-source-browser';
+jest.mock('@aws/crypto-random-source-node', () => {
+    return { randomValues: jest.fn() };
+});
+import {randomValues as nodeRandomValues} from '@aws/crypto-random-source-node';
+
+beforeEach(() => {
+    (browserRandomValues as any).mockReset();
+    (nodeRandomValues as any).mockReset();
+});
+
+describe('implementation selection', () => {
+    it(
+        'should use the browser implementation when the crypto module is not defined',
+        async () => {
+            await randomValues(1);
+
+            expect((nodeRandomValues as any).mock.calls.length).toBe(0);
+            expect((browserRandomValues as any).mock.calls.length).toBe(1);
+        }
+    );
+});
