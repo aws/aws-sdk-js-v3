2a8479c0357ee00607fce2bc913491fa32c698b0
Christopher Radek <cjradek@gmail.com>
2017-11-21 08:36:31 -0800

    Code Commit browser (#76)
    * Only add port in fetch handler if it is defined
    
    * Adds codecommit browser package

---
+++ packages/sdk-codecommit-browser/types/GetRepositoryTriggersInput.ts
@@ -0,0 +1,9 @@
+/**
+ * <p>Represents the input of a get repository triggers operation.</p>
+ */
+export interface GetRepositoryTriggersInput {
+    /**
+     * <p>The name of the repository for which the trigger is configured.</p>
+     */
+    repositoryName: string;
+}
