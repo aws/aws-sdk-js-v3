1f20aff47cd6a3f9197f0f84cd305d4127288466
Jonathan Eskew <eskewj@amazon.com>
2017-11-28 17:17:05 -0800

    Ensure undefined environment variables are not set to "undefined" at the end of tests

--- packages/region-provider/src/fromSharedConfigFiles.spec.ts
+++ packages/region-provider/src/fromSharedConfigFiles.spec.ts
@@ -1,8 +1,18 @@
 import {fromSharedConfigFiles} from './fromSharedConfigFiles';
 import {ProviderError} from '@aws/property-provider';
-import {writeFileSync} from 'fs';
-import {tmpdir} from 'os';
-import {join} from 'path';
+
+const mockRegion = 'mars-west-1';
+
+jest.mock('@aws/shared-ini-file-loader', () => {
+    const module = jest.genMockFromModule('@aws/shared-ini-file-loader') as any;
+    module.loadSharedConfigFiles = jest.fn(() => Promise.resolve({
+        credentialsFile: {},
+        configFile: {
+            default: {region: mockRegion}
+        },
+    }));
+    return module;
+});
 
 describe('fromSharedConfigFiles', () => {
     it('should throw a ProviderError if no config files were loaded', () => {
@@ -87,29 +97,10 @@ describe('fromSharedConfigFiles', () => {
         expect(await fromSharedConfigFiles({loadedConfig})()).toBe('us-west-1');
     });
 
-    describe('config file loading', () => {
-        const expectedRegion = 'us-west-1';
-        const configFile =
-`[default]
-region: ${expectedRegion}`;
-
-        const configPathEnv = process.env.AWS_CONFIG_FILE;
-
-        beforeAll(() => {
-            const configPath = join(tmpdir(), `config-test-${Date.now()}`);
-            writeFileSync(configPath, configFile);
-            process.env.AWS_CONFIG_FILE = configPath;
-        });
-
-        afterAll(() => {
-            process.env.AWS_CONFIG_FILE = configPathEnv;
-        });
-
-        it(
-            'should use the shared ini file loader if no pre-loaded config is supplied',
-            async () => {
-                expect(await fromSharedConfigFiles()()).toBe(expectedRegion);
-            }
-        );
-    });
+    it(
+        'should use the shared ini file loader if no pre-loaded config is supplied',
+        async () => {
+            expect(await fromSharedConfigFiles()()).toBe(mockRegion);
+        }
+    );
 });
