9f7aa769ca3dd8685acf998a298ed221d279cfc1
zheallan <zyp19930608@gmail.com>
2017-12-13 15:37:17 -0800

    squash! clean workspace

--- packages/protocol-rest/src/RestParser.ts
+++ packages/protocol-rest/src/RestParser.ts
@@ -32,7 +32,6 @@ export class RestParser<StreamType> implements ResponseParser<StreamType> {
         input: HttpResponse<StreamType>
     ): Promise<OutputType> {
         const output: Partial<OutputType> = {};
-
         const {
             headers: responseHeaders
         } = input;
