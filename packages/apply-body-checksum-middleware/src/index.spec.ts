f6aad5dfe4175dfa15751eb936bd298e101d6868
Jonathan Eskew <eskewj@amazon.com>
2018-01-17 19:06:58 -0800

    Add body-checksum-applying middleware

---
+++ packages/apply-body-checksum-middleware/src/index.spec.ts
@@ -0,0 +1,171 @@
+import { applyBodyChecksumMiddleware } from './';
+import { BuildHandler, HashConstructor, HttpRequest } from '@aws/types';
+
+describe('applyChecksumMiddleware', () => {
+    const mockEncoder = jest.fn(() => 'encoded');
+    const mockHashUpdate = jest.fn(() => {});
+    const mockHashDigest = jest.fn(() => new Uint8Array(0));
+    const MockHash: HashConstructor = class {} as any;
+    MockHash.prototype.update = mockHashUpdate
+    MockHash.prototype.digest = mockHashDigest;
+
+    const next = jest.fn(() => Promise.resolve({}));
+
+    class ExoticStream {}
+
+    const request: HttpRequest<ExoticStream> = {
+        method: 'REPORT',
+        headers: {
+            'Content-Length': '10',
+            Range: 'bytes=0-1000',
+        },
+        protocol: 'https:',
+        hostname: 'example.com',
+        path: '/'
+    }
+
+    beforeEach(() => {
+        mockEncoder.mockClear();
+        mockHashUpdate.mockClear();
+        mockHashDigest.mockClear();
+        next.mockClear();
+    });
+
+    for (const body of [
+        'body',
+        new ArrayBuffer(10),
+        new Uint8Array(10),
+        void 0
+    ]) {
+        it(
+            'should calculate the body hash using the supplied hash constructor, encode the result using the supplied encoder, and set the encoded hash to the provided header',
+            async () => {
+                await applyBodyChecksumMiddleware(
+                    'checksumHeader',
+                    MockHash,
+                    mockEncoder,
+                )(next, {} as any)({
+                    request: {...request, body},
+                    input: {}
+                });
+
+                expect(next.mock.calls).toEqual([[{
+                    input: {},
+                    request: {
+                        ...request,
+                        body,
+                        headers: {
+                            ...request.headers,
+                            checksumHeader: 'encoded',
+                        }
+                    }
+                }]]);
+                expect(mockHashUpdate.mock.calls).toEqual([[body || '']]);
+            }
+        );
+
+        it(
+            'should do nothing if a case-insenitive match for the desired header has already been set',
+            async () => {
+                await applyBodyChecksumMiddleware(
+                    'checksumHeader',
+                    MockHash,
+                    mockEncoder
+                )(next, {} as any)({
+                    request: {
+                        ...request,
+                        body,
+                        headers: {
+                            ...request.headers,
+                            cHeCkSuMhEaDeR: 'foo'
+                        }
+                    },
+                    input: {}
+                });
+
+                expect(next.mock.calls).toEqual([[{
+                    input: {},
+                    request: {
+                        ...request,
+                        body,
+                        headers: {
+                            ...request.headers,
+                            cHeCkSuMhEaDeR: 'foo',
+                        }
+                    }
+                }]]);
+
+                expect(mockHashUpdate.mock.calls.length).toBe(0);
+                expect(mockHashDigest.mock.calls.length).toBe(0);
+                expect(mockEncoder.mock.calls.length).toBe(0);
+            }
+        )
+    }
+
+    it('should throw if no request has been defined', async () => {
+        await expect(
+            applyBodyChecksumMiddleware(
+                'checksumHeader',
+                MockHash,
+                mockEncoder
+            )(next, {} as any)({ input: {} })
+        ).rejects.toBeInstanceOf(Error);
+    });
+
+    it(
+        'should throw if a streaming body is encounterd and no stream collector was provided',
+        async () => {
+            const handler = applyBodyChecksumMiddleware<ExoticStream>(
+                'checksumHeader',
+                MockHash,
+                mockEncoder
+            )(next, {} as any);
+
+            await expect(handler(({
+                request: {
+                    ...request,
+                    body: new ExoticStream
+                },
+                input: {}
+            }))).rejects.toBeInstanceOf(Error);
+
+            expect(mockHashUpdate.mock.calls.length).toBe(0);
+            expect(mockHashDigest.mock.calls.length).toBe(0);
+            expect(mockEncoder.mock.calls.length).toBe(0);
+        }
+    );
+
+    it(
+        'should set the header if the body is unhashable and a static valueIfStreaming has been supplied',
+        async () => {
+            await applyBodyChecksumMiddleware(
+                'checksumHeader',
+                MockHash,
+                mockEncoder,
+                async (stream: ExoticStream) => new Uint8Array(0)
+            )(next, {} as any)({
+                request: {
+                    ...request,
+                    body: new ExoticStream
+                },
+                input: {}
+            });
+
+            expect(next.mock.calls).toEqual([[{
+                input: {},
+                request: {
+                    ...request,
+                    body: new Uint8Array(0),
+                    headers: {
+                        ...request.headers,
+                        checksumHeader: 'encoded',
+                    }
+                },
+            }]]);
+
+            expect(mockHashUpdate.mock.calls).toEqual([[new Uint8Array(0)]]);
+            expect(mockHashDigest.mock.calls.length).toBe(1);
+            expect(mockEncoder.mock.calls.length).toBe(1);
+        }
+    );
+});
---
+++ packages/apply-body-checksum-middleware/src/index.ts
@@ -0,0 +1,76 @@
+import { isArrayBuffer } from '@aws/is-array-buffer';
+import {
+    BuildHandler,
+    BuildHandlerArguments,
+    BuildMiddleware,
+    Encoder,
+    Hash,
+    HeaderBag,
+    StreamCollector,
+} from '@aws/types';
+
+export function applyBodyChecksumMiddleware<StreamType = Uint8Array>(
+    headerName: string,
+    hashCtor: {new (): Hash},
+    encoder: Encoder,
+    streamCollector: StreamCollector<StreamType> = throwOnStream
+): BuildMiddleware<any, any, StreamType> {
+    return <Output extends object>(
+        next: BuildHandler<any, Output, any>
+    ): BuildHandler<any, Output, any> => async (
+        args: BuildHandlerArguments<any, any>
+    ): Promise<Output> => {
+        const {request} = args;
+        if (!request) {
+            throw new Error('Unable to determine request checksum due to missing request.');
+        }
+
+        let {body, headers} = request;
+        if (!hasHeader(headerName, headers)) {
+            const hash = new hashCtor();
+
+            if (
+                body &&
+                typeof body !== 'string' &&
+                !ArrayBuffer.isView(body) &&
+                !isArrayBuffer(body)
+            ) {
+                body = await streamCollector(body);
+            }
+
+            hash.update(body || '');
+            headers = {
+                ...headers,
+                [headerName]: encoder(await hash.digest())
+            }
+        }
+
+        return next({
+            ...args,
+            request: {
+                ...request,
+                headers,
+                body,
+            }
+        });
+    };
+}
+
+function hasHeader(soughtHeader: string, headers: HeaderBag): boolean {
+    soughtHeader = soughtHeader.toLowerCase();
+    for (const headerName of Object.keys(headers)) {
+        if (soughtHeader === headerName.toLowerCase()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function throwOnStream(stream: any): never {
+    throw new Error(
+        `applyBodyChecksumMiddleware encountered a request with a streaming body of type ${
+            Object.prototype.toString.call(stream)
+        }, but no stream collector function was provided`
+    );
+}
