2ea83ce8ef96cacd2ad3704c0796925fbfc49f10
Jonathan Eskew <eskewj@amazon.com>
2018-02-20 12:42:26 -0800

    Validate message length before attempting to validate checksums

--- packages/eventstream-marshaller/src/splitMessage.ts
+++ packages/eventstream-marshaller/src/splitMessage.ts
@@ -5,32 +5,31 @@ export interface MessageParts {
     body: Uint8Array;
 }
 
-export function splitMessage(message: ArrayBufferView): MessageParts {
-    if (message.byteLength < 16) {
+export function splitMessage(
+    { byteLength, byteOffset, buffer }: ArrayBufferView
+): MessageParts {
+    if (byteLength < 16) {
         throw new Error(
             'Provided message too short to accommodate event stream message overhead'
         );
     }
 
-    const view = new DataView(
-        message.buffer,
-        message.byteOffset,
-        message.byteLength
-    );
+    const view = new DataView(buffer, byteOffset, byteLength);
 
     const messageLength = view.getUint32(0, false);
-    const headerLength = view.getUint32(4, false);
-    const expectedPreludeChecksum = view.getUint32(8, false);
-    const expectedMessagehecksum = view.getUint32(view.byteLength - 4, false);
 
-    if (view.byteLength !== messageLength) {
+    if (byteLength !== messageLength) {
         throw new Error(
             'Reported message length does not match received message length'
         );
     }
 
+    const headerLength = view.getUint32(4, false);
+    const expectedPreludeChecksum = view.getUint32(8, false);
+    const expectedMessageChecksum = view.getUint32(byteLength - 4, false);
+
     const checksummer = (new Crc32).update(
-        new Uint8Array(view.buffer, view.byteOffset, 8)
+        new Uint8Array(buffer, byteOffset, 8)
     );
     if (
         expectedPreludeChecksum !== checksummer.digest()
@@ -45,27 +44,23 @@ export function splitMessage(message: ArrayBufferView): MessageParts {
     }
 
     checksummer.update(new Uint8Array(
-        view.buffer,
-        view.byteOffset + 8,
-        view.byteLength - 12
+        buffer,
+        byteOffset + 8,
+        byteLength - 12
     ));
-    if (expectedMessagehecksum !== checksummer.digest()) {
+    if (expectedMessageChecksum !== checksummer.digest()) {
         throw new Error(
             `The message checksum (${
                 checksummer.digest()
-            }) did not match the expected value of ${expectedMessagehecksum}`
+            }) did not match the expected value of ${expectedMessageChecksum}`
         )
     }
 
     return {
-        headers: new DataView(
-            message.buffer,
-            message.byteOffset + 12,
-            headerLength
-        ),
+        headers: new DataView(buffer, byteOffset + 12, headerLength),
         body: new Uint8Array(
-            message.buffer,
-            message.byteOffset + 12 + headerLength,
+            buffer,
+            byteOffset + 12 + headerLength,
             messageLength - headerLength - 16
         )
     };
