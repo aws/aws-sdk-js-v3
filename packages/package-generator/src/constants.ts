05dfd2141f4ada3d515f5732346ed7656fc085d7
Allan Zheng <zheallan@amazon.com>
2019-01-23 14:51:40 -0800

    update License year to current year

--- packages/package-generator/src/constants.ts
+++ packages/package-generator/src/constants.ts
@@ -247,7 +247,7 @@ export const APACHE_2_LICENSE =
       same "printed page" as the copyright notice for easier
       identification within third-party archives.
 
-   Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   Copyright ${(new Date()).getFullYear()} Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
