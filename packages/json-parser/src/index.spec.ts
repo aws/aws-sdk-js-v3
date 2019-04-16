db82a18483a6ed5c84d4fade9a84d4dedd4b3c14
Chase Coalwell <chasecoa@amazon.com>
2019-04-16 07:47:47 -0700

    fix json parsing date tests

--- packages/json-parser/src/index.spec.ts
+++ packages/json-parser/src/index.spec.ts
@@ -127,7 +127,7 @@ describe('JsonParser', () => {
     describe('timestamps', () => {
         const timestampShape: Member = {shape: {type: "timestamp"}};
         const unixTimestamp = 1495481594;
-        const rfc822Timestamp = 'Mon, May 22, 2017 7:33:14 GMT'
+        const rfc822Timestamp = 'Mon, May 22, 2017 19:33:14 GMT'
         const isoTimestamp = '2017-05-22T19:33:14.000Z';
         const date = new Date(isoTimestamp);
         const jsonBody = new JsonParser(jest.fn());
@@ -137,15 +137,15 @@ describe('JsonParser', () => {
                 .toEqual(date);
         });
 
-        // it('should convert rfc822 timeStamps to date objects', () => {
-        //     expect(jsonBody.parse(timestampShape, rfc822Timestamp))
-        //         .toEqual(date);
-        // });
+        it('should convert rfc822 timeStamps to date objects', () => {
+            expect(jsonBody.parse(timestampShape, JSON.stringify(rfc822Timestamp)))
+                .toEqual(date);
+        });
 
-        // it('should convert iso8601 timeStamps to date objects', () => {
-        //     expect(jsonBody.parse(timestampShape, isoTimestamp))
-        //         .toEqual(date);
-        // });
+        it('should convert iso8601 timeStamps to date objects', () => {
+            expect(jsonBody.parse(timestampShape, JSON.stringify(isoTimestamp)))
+                .toEqual(date);
+        });
 
         it('should return undefined if the input is not a timestamp', () => {
             expect(jsonBody.parse(timestampShape, JSON.stringify('foo')))
