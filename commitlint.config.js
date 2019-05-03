b820f9f2bf16e374361f1e58149e4345ab756b9c
Allan Zheng <zheallan@amazon.com>
2019-05-03 10:22:21 -0700

    chore: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    "type-enum": [2, "always", [
+      'feat',
+      'fix',
+      'docs',
+      'test',
+      'chore'
+    ]]
+  }
+}
