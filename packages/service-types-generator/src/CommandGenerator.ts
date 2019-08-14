d11041ffc42d48c0e5e91dbc47759c345c9fe789
Allan Zheng <zheallan@amazon.com>
2019-08-14 11:09:35 -0700

    fix(service-types-generator): remove versions from cloudfront commands

--- packages/service-types-generator/src/CommandGenerator.ts
+++ packages/service-types-generator/src/CommandGenerator.ts
@@ -56,7 +56,9 @@ export class CommandGenerator {
 
     for (let operationName of Object.keys(operations)) {
       const operation = operations[operationName];
-
+      //always refer operation.name to operationName.
+      //fix: https://github.com/aws/aws-models/blob/b0fc604f00ce8a3fe50ae39445afabffa0c2982a/cloudfront/2019-03-26/service-2.json#L34
+      operation.name = operationName;
       const command = new Command(
         operation,
         this.target,
