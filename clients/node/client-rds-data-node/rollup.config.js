13696c116c9a40330fb5867bdf61765ba6f6369e
Allan Zheng <zheallan@amazon.com>
2019-09-16 10:23:27 -0700

    chore: rollup to browser SDK

--- clients/node/client-rds-data-node/rollup.config.js
+++ clients/node/client-rds-data-node/rollup.config.js
@@ -5,6 +5,8 @@ import sourcemaps from "rollup-plugin-sourcemaps";
 // import shim from "rollup-plugin-shim";
 // import visualizer from "rollup-plugin-visualizer";
 import replace from "rollup-plugin-replace";
+import resolve from "rollup-plugin-node-resolve";
+import importJson from "rollup-plugin-json";
 
 const pkg = require("./package.json");
 const version = pkg.version;
@@ -22,49 +24,51 @@ export function browserConfig(test = false, production = false) {
     preserveSymlinks: false,
     external: [...depNames],
     plugins: [
-      sourcemaps(),
       replace({
-        include: "*Configuration.js",
+        include: ["dist/es/*Configuration.js"],
         delimiters: ["", ""],
         values: {
           'from "@aws-sdk/node-http-handler"':
-            'from "@aws-sdk/fetch-http-handler"'
+            "from '@aws-sdk/fetch-http-handler'",
+          "__aws_sdk_http_handler.NodeHttpHandler(configuration)":
+            "__aws_sdk_http_handler.FetchHttpHandler(configuration)"
         }
-      })
+      }),
+      replace({
+        include: ["dist/es/*Configuration.js"],
+        delimiters: ["", ""],
+        values: {
+          'import * as __aws_sdk_credential_provider_node from "@aws-sdk/credential-provider-node"':
+            'import * as __aws_sdk_invalid_dependency from "@aws-sdk/invalid-dependency"',
+          "__aws_sdk_credential_provider_node.defaultProvider":
+            "__aws_sdk_invalid_dependency.invalidFunction('Credentials is required')"
+        }
+      }),
+      replace({
+        include: ["dist/es/*Configuration.js"],
+        delimiters: ["", ""],
+        values: {
+          'import * as __aws_sdk_credential_provider_node from "@aws-sdk/credential-provider-node"':
+            'import * as __aws_sdk_invalid_dependency from "@aws-sdk/invalid-dependency"',
+          "__aws_sdk_credential_provider_node.defaultProvider":
+            "__aws_sdk_invalid_dependency.invalidFunction('Credentials is required')"
+        }
+      }),
+      replace({
+        include: ["dist/es/*Configuration.js"],
+        delimiters: ["", ""],
+        values: {
+          'import * as __aws_sdk_util_body_length_node from "@aws-sdk/util-body-length-node"':
+            'import * as __aws_sdk_util_body_length_browser from "@aws-sdk/util-body-length-browser"',
+          " defaultValue: __aws_sdk_util_body_length_node":
+            " defaultValue: __aws_sdk_util_body_length_browser"
+        }
+      }),
+      importJson(),
+      resolve(),
+      sourcemaps()
     ]
   };
-
-  if (test) {
-    baseConfig.input = [
-      "dist-esm/test/*.spec.js",
-      "dist-esm/test/browser/*.spec.js"
-    ];
-    baseConfig.plugins.unshift(multiEntry({ exports: false }));
-    baseConfig.output.file = "dist-test/index.browser.js";
-    // mark fs-extra as external
-    baseConfig.external = ["fs-extra"];
-
-    baseConfig.context = "null";
-
-    // Disable tree-shaking of test code.  In rollup-plugin-node-resolve@5.0.0, rollup started respecting
-    // the "sideEffects" field in package.json.  Since our package.json sets "sideEffects=false", this also
-    // applies to test code, which causes all tests to be removed by tree-shaking.
-    baseConfig.treeshake = false;
-  } else if (production) {
-    baseConfig.output.file = "browser/azure-storage-blob.min.js";
-    baseConfig.plugins.push(
-      terser({
-        output: {
-          preamble: banner
-        }
-      })
-      // Comment visualizer because it only works on Node.js 8+; Uncomment it to get bundle analysis report
-      // visualizer({
-      //   filename: "./statistics.html",
-      //   sourcemap: true
-      // })
-    );
-  }
 
   return baseConfig;
 }
