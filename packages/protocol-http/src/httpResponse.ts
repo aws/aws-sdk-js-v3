213708fad00657dace39935669754dcb9c249224
AllanFly120 <zheallan@amazon.com>
2019-10-02 11:29:05 -0700

    chore: add updated RDS DATA smithy model (#370)
    * chore: add http request type guard for middlewares
    
    header default middleware
    expect continue middleare
    
    * chore: add http request type guard for apply body checksum middlewares
    
    * chore: only build and test demo smithy client
    
    * feat: add isInstance to httpResponse
    
    * chore: update rds data model
    
    * chore: update to ts3.7; update rds model#3
    
    * chore: update tsconfig to genenerte types only once
    
    * chore: update gitignore
    
    * chore: parse body with streamCollector
    
    * chore: add error deserialization
    
    * chore: stub Field union
    
    * fix: fix middleware-header-default test
    
    * fix: fix retry-middleware unit test

--- packages/protocol-http/src/httpResponse.ts
+++ packages/protocol-http/src/httpResponse.ts
@@ -13,9 +13,17 @@ export class HttpResponse {
   public statusCode: number;
   public headers: HeaderBag;
   public body?: any;
+  private readonly isHttpResponse = true;
+
   constructor(options: HttpResponseOptions) {
     this.statusCode = options.statusCode;
     this.headers = options.headers || {};
     this.body = options.body;
+  }
+
+  static isInstance(request: unknown): request is HttpResponse {
+    return (
+      request !== undefined && (request as HttpResponse).isHttpResponse === true
+    );
   }
 }
