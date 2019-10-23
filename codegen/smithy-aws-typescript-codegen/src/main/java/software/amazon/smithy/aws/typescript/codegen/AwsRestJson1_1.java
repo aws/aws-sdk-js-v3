bb81842126372e31ef836018e4c763cd872d51b2
Michael Dowling <mtdowling@gmail.com>
2019-10-22 17:38:24 -0700

    feat: add start of HTTP protocol serde

--- codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AwsRestJson1_1.java
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AwsRestJson1_1.java
@@ -15,27 +15,51 @@
 
 package software.amazon.smithy.aws.typescript.codegen;
 
-import software.amazon.smithy.typescript.codegen.ApplicationProtocol;
-import software.amazon.smithy.typescript.codegen.integration.ProtocolGenerator;
+import java.util.List;
+import software.amazon.smithy.codegen.core.SymbolProvider;
+import software.amazon.smithy.model.knowledge.HttpBinding;
+import software.amazon.smithy.model.shapes.OperationShape;
+import software.amazon.smithy.model.traits.JsonNameTrait;
+import software.amazon.smithy.typescript.codegen.TypeScriptWriter;
+import software.amazon.smithy.typescript.codegen.integration.HttpBindingProtocolGenerator;
 
-public class AwsRestJson1_1 implements ProtocolGenerator {
+public class AwsRestJson1_1 extends HttpBindingProtocolGenerator {
+
     @Override
     public String getName() {
         return "aws.rest-json-1.1";
     }
 
     @Override
-    public ApplicationProtocol getApplicationProtocol() {
-        return ApplicationProtocol.createDefaultHttpApplicationProtocol();
+    protected String getDocumentContentType() {
+        return "application/x-amz-json-1.1";
     }
 
     @Override
-    public void generateRequestSerializers(GenerationContext context) {
-        context.getWriter().write("// TODO: write request deserializers");
-    }
+    protected void serializeDocument(
+            GenerationContext context,
+            OperationShape operation,
+            List<HttpBinding> documentBindings
+    ) {
+        SymbolProvider symbolProvider = context.getSymbolProvider();
+        TypeScriptWriter writer = context.getWriter();
 
-    @Override
-    public void generateResponseDeserializers(GenerationContext context) {
-        context.getWriter().write("// TODO: write response deserializers");
+        writer.write("let bodyParams: any = {};");
+        for (HttpBinding binding : documentBindings) {
+            // The name of the member to get from the input shape.
+            String memberName = symbolProvider.toMemberName(binding.getMember());
+            // Use the jsonName trait value if present, otherwise use the member name.
+            String locationName = binding.getMember().getTrait(JsonNameTrait.class)
+                    .map(JsonNameTrait::getValue)
+                    .orElseGet(binding::getLocationName);
+            writer.openBlock("if (input.$L !== undefined) {", "}", memberName, () -> {
+                // TODO: walk the input to serialize appropriately.
+                // TODO: we need to serialize Date values as epoch-seconds.
+                // TODO: we need to serialize blob values as base64 encoded strings.
+                writer.write("bodyParams['$L'] = input.$L;", locationName, memberName);
+            });
+        }
+
+        writer.write("body = JSON.stringify(bodyParams);");
     }
 }
