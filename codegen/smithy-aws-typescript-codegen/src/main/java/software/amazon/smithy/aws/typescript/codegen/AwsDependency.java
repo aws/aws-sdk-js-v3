48cd3a99a397c4700560d4d3ea18ae52e205f5a7
Michael Dowling <mtdowling@gmail.com>
2019-11-13 14:58:30 -0800

    chore: fix AWS integrations and dependencies (#447)
    This commit adds AWS runtime config integrations and fixes dependency
    management to match smithy-typescript. It also fixes an issue where the
    SymbolProvider decoration was not happening.

---
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AwsDependency.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.aws.typescript.codegen;
+
+import static software.amazon.smithy.typescript.codegen.TypeScriptDependency.NORMAL_DEPENDENCY;
+
+import java.util.Collections;
+import java.util.List;
+import software.amazon.smithy.codegen.core.SymbolDependency;
+import software.amazon.smithy.codegen.core.SymbolDependencyContainer;
+
+/**
+ * This enum should define all TypeScript dependencies that are introduced by
+ * this package.
+ */
+public enum AwsDependency implements SymbolDependencyContainer {
+
+    MIDDLEWARE_SIGNING(NORMAL_DEPENDENCY, "@aws-sdk/middleware-signing", "^0.1.0-preview.7"),
+    CREDENTIAL_PROVIDER_NODE(NORMAL_DEPENDENCY, "@aws-sdk/credential-provider-node", "^0.1.0-preview.7"),
+    REGION_PROVIDER(NORMAL_DEPENDENCY, "@aws-sdk/region-provider", "^0.1.0-preview.5");
+
+    public final String packageName;
+    public final String version;
+    public final SymbolDependency dependency;
+
+    AwsDependency(String type, String name, String version) {
+        this.dependency = SymbolDependency.builder().dependencyType(type).packageName(name).version(version).build();
+        this.packageName = name;
+        this.version = version;
+    }
+
+    @Override
+    public List<SymbolDependency> getDependencies() {
+        return Collections.singletonList(dependency);
+    }
+}
