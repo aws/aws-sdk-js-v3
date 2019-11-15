ffdd7c46b5c74185e7a14c8b1e4071a30884518d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-11-14 17:05:17 -0800

    chore: remove unused client.send with callbacks (#450)
    Fixes: https://github.com/aws/aws-sdk-js-v3/issues/449

--- packages/types/src/client.ts
+++ packages/types/src/client.ts
@@ -20,28 +20,6 @@ interface InvokeFunction<
     >,
     options?: any
   ): Promise<OutputType>;
-  <InputType extends InputTypes, OutputType extends OutputTypes>(
-    command: Command<
-      InputTypes,
-      InputType,
-      OutputTypes,
-      OutputType,
-      ResolvedClientConfiguration
-    >,
-    options: any,
-    cb: (err: any, data?: OutputType) => void
-  ): void;
-  <InputType extends InputTypes, OutputType extends OutputTypes>(
-    command: Command<
-      InputTypes,
-      InputType,
-      OutputTypes,
-      OutputType,
-      ResolvedClientConfiguration
-    >,
-    options?: any,
-    cb?: (err: any, data?: OutputType) => void
-  ): Promise<OutputType> | void;
 }
 
 /**
