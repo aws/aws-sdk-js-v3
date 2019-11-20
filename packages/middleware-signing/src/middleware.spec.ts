1455f3d30ec266b6cc52fe72c5c06a1c9dc564ec
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-11-19 17:20:07 -0800

    test: should add systemClockOffset while signing the request

--- packages/middleware-signing/src/middleware.spec.ts
+++ packages/middleware-signing/src/middleware.spec.ts
@@ -4,12 +4,13 @@ import { HttpRequest } from "@aws-sdk/protocol-http";
 
 describe("SigningHandler", () => {
   const noOpSigner: RequestSigner = {
-    sign: (request: HttpRequest) =>
+    sign: (request: HttpRequest, options: { signingDate: Date }) =>
       Promise.resolve({
         ...request,
         headers: {
           ...request.headers,
-          signed: "true"
+          signed: "true",
+          signingDateTime: options.signingDate.getTime()
         }
       })
   } as any;
@@ -34,5 +35,28 @@ describe("SigningHandler", () => {
     const { calls } = (noOpNext as any).mock;
     expect(calls.length).toBe(1);
     expect(calls[0][0].request.headers.signed).toBe("true");
+  });
+
+  it("should add systemClockOffset while signing the request", async () => {
+    const systemClockOffset = 100000;
+    const now = Date.now();
+    const signingHandler = awsAuthMiddleware({
+      signer: noOpSigner,
+      systemClockOffset
+    } as any)(noOpNext, {} as any);
+    await signingHandler({
+      input: {},
+      request: new HttpRequest({
+        headers: {}
+      })
+    });
+
+    const { calls } = (noOpNext as any).mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0].request.headers.signed).toBe("true");
+    // Using greater than to ensure there are no timing issues
+    expect(calls[0][0].request.headers.signingDateTime).toBeGreaterThan(
+      now + systemClockOffset - 1
+    );
   });
 });
