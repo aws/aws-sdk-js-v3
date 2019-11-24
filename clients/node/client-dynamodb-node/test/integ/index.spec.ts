96569081a57e1861f640d151092a39c68e4bb2cd
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-11-24 14:07:57 -0800

    WIP: experimental commit

---
+++ clients/node/client-dynamodb-node/test/integ/index.spec.ts
@@ -0,0 +1,49 @@
+import { DynamoDB } from "../../DynamoDB";
+
+describe("DynamoDB integration tests", () => {
+  const client = new DynamoDB({});
+  const tableName = `aws-js-integration-${Math.random()
+    .toString(36)
+    .substring(2)}`;
+
+  describe("Table CRUD operations", () => {
+    beforeAll(async () => {
+      const params = {
+        TableName: tableName,
+        AttributeDefinitions: [
+          {
+            AttributeName: "Artist",
+            AttributeType: "S"
+          },
+          {
+            AttributeName: "SongTitle",
+            AttributeType: "S"
+          }
+        ],
+        KeySchema: [
+          {
+            AttributeName: "Artist",
+            KeyType: "HASH"
+          },
+          {
+            AttributeName: "SongTitle",
+            KeyType: "RANGE"
+          }
+        ],
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 5,
+          WriteCapacityUnits: 5
+        }
+      };
+      const response = await client.createTable(params);
+      console.log(response);
+    });
+
+    afterAll(async () => {
+      const response = await client.deleteTable({
+        TableName: tableName
+      });
+      console.log(response);
+    });
+  });
+});
