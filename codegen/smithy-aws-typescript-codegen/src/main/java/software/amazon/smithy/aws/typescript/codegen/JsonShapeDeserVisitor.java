f758bae6c8d624393657ef88c196d3a795d8bb7c
Kevin Stich <stickevi@amazon.com>
2019-11-25 13:02:30 -0800

    Update HTTP binding protocol generation (#465)
    This commit updates the support for aws.rest-json-1.1 to build atop
    the refactor of protocol generation done in smithy-typescript. It
    also introduces support for the aws.rest-json-1.0 protocol.
    
    In doing so, general components were built atop the abstractions
    provided. The aws.rest-json-1.* set of protocols serialize BigInteger
    and BigDecimal shapes as JSON numbers on the wire, so an implementation
    of the DocumentMemberSerVisitor was created to fail generation.
    Implementations of the [DocumentShape[Deser|Ser]Visitor classes are
    also included to support serde of shapes in request and response
    bodies.
    
    A rest-json implementation of the HttpBindingProtocolGenerator has been
    created to combine the above aspects and handling of document bodies.

---
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/JsonShapeDeserVisitor.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.aws.typescript.codegen;
+
+import java.util.Map;
+import java.util.TreeMap;
+import software.amazon.smithy.model.shapes.CollectionShape;
+import software.amazon.smithy.model.shapes.DocumentShape;
+import software.amazon.smithy.model.shapes.MapShape;
+import software.amazon.smithy.model.shapes.MemberShape;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeIndex;
+import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.shapes.UnionShape;
+import software.amazon.smithy.model.traits.ErrorTrait;
+import software.amazon.smithy.model.traits.JsonNameTrait;
+import software.amazon.smithy.model.traits.TimestampFormatTrait.Format;
+import software.amazon.smithy.typescript.codegen.TypeScriptWriter;
+import software.amazon.smithy.typescript.codegen.integration.DocumentMemberDeserVisitor;
+import software.amazon.smithy.typescript.codegen.integration.DocumentShapeDeserVisitor;
+import software.amazon.smithy.typescript.codegen.integration.ProtocolGenerator.GenerationContext;
+
+/**
+ * Visitor to generate deserialization functions for shapes in AWS JSON protocol
+ * document bodies.
+ *
+ * No standard visitation methods are overridden; function body generation for all
+ * expected deserializers is handled by this class.
+ *
+ * Timestamps are deserialized from {@link Format}.EPOCH_SECONDS by default.
+ */
+final class JsonShapeDeserVisitor extends DocumentShapeDeserVisitor {
+
+    JsonShapeDeserVisitor(GenerationContext context) {
+        super(context);
+    }
+
+    private DocumentMemberDeserVisitor getMemberVisitor(String dataSource) {
+        return new JsonMemberDeserVisitor(getContext(), dataSource, Format.EPOCH_SECONDS);
+    }
+
+    @Override
+    protected void deserializeCollection(GenerationContext context, CollectionShape shape) {
+        TypeScriptWriter writer = context.getWriter();
+        Shape target = context.getModel().getShapeIndex().getShape(shape.getMember().getTarget()).get();
+
+        // Dispatch to the output value provider for any additional handling.
+        writer.openBlock("return (output || []).map((entry: any) =>", ");", () -> {
+            writer.write(target.accept(getMemberVisitor("entry")));
+        });
+    }
+
+    @Override
+    protected void deserializeDocument(GenerationContext context, DocumentShape shape) {
+        TypeScriptWriter writer = context.getWriter();
+        // Documents are JSON content, so don't modify.
+        writer.write("return output;");
+    }
+
+    @Override
+    protected void deserializeMap(GenerationContext context, MapShape shape) {
+        TypeScriptWriter writer = context.getWriter();
+        Shape target = context.getModel().getShapeIndex().getShape(shape.getValue().getTarget()).get();
+
+        // Get the right serialization for each entry in the map. Undefined
+        // outputs won't have this deserializer invoked.
+        writer.write("let mapParams: any = {};");
+        writer.openBlock("Object.keys(output).forEach(key => {", "});", () -> {
+            // Dispatch to the output value provider for any additional handling.
+            writer.write("mapParams[key] = $L;", target.accept(getMemberVisitor("output[key]")));
+        });
+        writer.write("return mapParams;");
+    }
+
+    @Override
+    protected void deserializeStructure(GenerationContext context, StructureShape shape) {
+        TypeScriptWriter writer = context.getWriter();
+
+        // Prepare the document contents structure.
+        // Use a TreeMap to sort the members.
+        Map<String, MemberShape> members = new TreeMap<>(shape.getAllMembers());
+        writer.openBlock("let contents: any = {", "};", () -> {
+            writer.write("__type: $S,", shape.getId().getName());
+            if (shape.hasTrait(ErrorTrait.class)) {
+                writer.write("$$fault: $S,", shape.getTrait(ErrorTrait.class).get().getValue());
+            }
+            // Set all the members to undefined to meet type constraints.
+            members.forEach((memberName, memberShape) -> writer.write("$L: undefined,", memberName));
+        });
+        members.forEach((memberName, memberShape) -> {
+            // Use the jsonName trait value if present, otherwise use the member name.
+            String locationName = memberShape.getTrait(JsonNameTrait.class)
+                    .map(JsonNameTrait::getValue)
+                    .orElse(memberName);
+            Shape target = context.getModel().getShapeIndex().getShape(memberShape.getTarget()).get();
+
+            // Generate an if statement to set the bodyParam if the member is set.
+            writer.openBlock("if (output.$L !== undefined) {", "}", locationName, () -> {
+                writer.write("contents.$L = $L;", memberName,
+                        // Dispatch to the output value provider for any additional handling.
+                        target.accept(getMemberVisitor("output." + locationName)));
+            });
+        });
+
+        writer.write("return contents;");
+    }
+
+    @Override
+    protected void deserializeUnion(GenerationContext context, UnionShape shape) {
+        TypeScriptWriter writer = context.getWriter();
+        ShapeIndex index = context.getModel().getShapeIndex();
+
+        // Check for any known union members and return when we find one.
+        Map<String, MemberShape> members = new TreeMap<>(shape.getAllMembers());
+        members.forEach((memberName, memberShape) -> {
+            Shape target = index.getShape(memberShape.getTarget()).get();
+            // Use the jsonName trait value if present, otherwise use the member name.
+            String locationName = memberShape.getTrait(JsonNameTrait.class)
+                    .map(JsonNameTrait::getValue)
+                    .orElse(memberName);
+            writer.openBlock("if (output.$L !== undefined) {", "}", locationName, () -> {
+                writer.openBlock("return {", "};", () -> {
+                    // Dispatch to the output value provider for any additional handling.
+                    writer.write("$L: $L", memberName, target.accept(getMemberVisitor("output." + locationName)));
+                });
+            });
+        });
+        // Or write to the unknown member the element in the output.
+        writer.write("return { $$unknown: output[Object.keys(output)[0]] };");
+    }
+}
