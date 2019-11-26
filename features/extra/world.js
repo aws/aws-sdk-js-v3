00a1de0abc3722cf09963df289d7d1a36731624d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-11-26 13:50:02 -0800

    chore: remove global AWS object

--- features/extra/world.js
+++ features/extra/world.js
@@ -1,11 +1,10 @@
 var world = require('./helpers');
 var path = require('path');
 
-world.AWS = require('../../lib/aws');
 try {
-  world.AWS.config.loadFromPath(path.resolve('./configuration'));
+  world.config.loadFromPath(path.resolve('./configuration'));
 } catch (e) {} finally {
-  process.env['CONFIGURED_REGION'] = world.AWS.config.region || new world.AWS.Config().region;
+  process.env['CONFIGURED_REGION'] = world.config.region || 'us-west-2';
 }
 
 var WorldConstructor = function WorldConstructor(callback) {
