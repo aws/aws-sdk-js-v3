3cdbdef7b475197d0d4c06cb0bfb6cf7c81dc812
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-11-26 11:50:46 -0800

    feat: enable clockSkew correction by default (#459)

--- packages/middleware-signing/src/middleware.ts
+++ packages/middleware-signing/src/middleware.ts
@@ -10,6 +10,13 @@ import {
 import { AwsAuthResolvedConfig } from "./configurations";
 import { HttpRequest } from "@aws-sdk/protocol-http";
 
+const isClockSkewed = (newServerTime: number, systemClockOffset: number) =>
+  Math.abs(getSkewCorrectedDate(systemClockOffset).getTime() - newServerTime) >=
+  300000;
+
+const getSkewCorrectedDate = (systemClockOffset: number) =>
+  new Date(Date.now() + systemClockOffset);
+
 export function awsAuthMiddleware<Input extends object, Output extends object>(
   options: AwsAuthResolvedConfig
 ): FinalizeRequestMiddleware<Input, Output> {
@@ -20,10 +27,23 @@ export function awsAuthMiddleware<Input extends object, Output extends object>(
       args: FinalizeHandlerArguments<Input>
     ): Promise<FinalizeHandlerOutput<Output>> {
       if (!HttpRequest.isInstance(args.request)) return next(args);
-      return next({
+      const output = await next({
         ...args,
-        request: await options.signer.sign(args.request)
+        request: await options.signer.sign(args.request, {
+          signingDate: new Date(Date.now() + options.systemClockOffset)
+        })
       });
+
+      const { headers } = output.response as any;
+      const dateHeader = headers && (headers.date || headers.Date);
+      if (dateHeader) {
+        const serverTime = Date.parse(dateHeader);
+        if (isClockSkewed(serverTime, options.systemClockOffset)) {
+          options.systemClockOffset = serverTime - Date.now();
+        }
+      }
+
+      return output;
     };
 }
 
