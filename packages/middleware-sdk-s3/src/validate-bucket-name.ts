1e3bbb8ea701681392af623034f09565678b90a5
Chase Coalwell <chasecoa@amazon.com>
2019-11-29 16:24:14 -0800

    feat: migrate s3 middleware

--- packages/middleware-sdk-s3/src/validate-bucket-name.ts
+++ packages/middleware-sdk-s3/src/validate-bucket-name.ts
@@ -1,15 +1,42 @@
-import { Handler, SerializeHandlerArguments } from "@aws-sdk/types";
+import {
+  InitializeHandler,
+  InitializeHandlerArguments,
+  InitializeHandlerOptions,
+  InitializeHandlerOutput,
+  InitializeMiddleware,
+  MetadataBearer,
+  Pluggable
+} from "@aws-sdk/types";
 
-export const validateBucketName = (
-  next: Handler<any, any>
-): Handler<any, any> => async (args: SerializeHandlerArguments<any, any>) => {
-  const { input } = args;
-  if (typeof input.Bucket === "string" && input.Bucket.indexOf("/") >= 0) {
-    const err = new Error(
-      `Bucket name shouldn't contain '/', received '${input.Bucket}'`
-    );
-    err.name = "InvalidBucketName";
-    throw err;
-  }
-  return next({ ...args });
+export function validateBucketNameMiddleware(): InitializeMiddleware<any, any> {
+  return <Output extends MetadataBearer>(
+    next: InitializeHandler<any, Output>
+  ): InitializeHandler<any, Output> => async (
+    args: InitializeHandlerArguments<any>
+  ): Promise<InitializeHandlerOutput<Output>> => {
+    const { input } = args;
+    if (typeof input.Bucket === "string" && input.Bucket.indexOf("/") >= 0) {
+      const err = new Error(
+        `Bucket name shouldn't contain '/', received '${input.Bucket}'`
+      );
+      err.name = "InvalidBucketName";
+      throw err;
+    }
+    return next({ ...args });
+  };
+}
+
+export const validateBucketNameMiddlewareOptions: InitializeHandlerOptions = {
+  step: "initialize",
+  tags: ["SET_EXPECT_HEADER", "EXPECT_HEADER"],
+  name: "addExpectContinueMiddleware"
 };
+
+export const getValidateBucketNamePlugin = (): Pluggable<any, any> => ({
+  applyToStack: clientStack => {
+    clientStack.add(
+      validateBucketNameMiddleware(),
+      validateBucketNameMiddlewareOptions
+    );
+  }
+});
