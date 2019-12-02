98b6ad8c1438e842573b60d5958d24dae93d57e9
Allan Zheng <zheallan@amazon.com>
2019-12-02 15:52:53 -0800

    fix: fix spaces addBuiltinPlugins

--- codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AddBuiltinPlugins.java
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AddBuiltinPlugins.java
@@ -56,17 +56,17 @@ public class AddBuiltinPlugins implements TypeScriptIntegration {
                 RuntimeClientPlugin.builder()
                         .withConventions("@aws-sdk/middleware-sdk-api-gateway", "^0.1.0-preview.5",
                                         "AcceptsHeader", HAS_MIDDLEWARE)
-                        .servicePredicate((m,s) -> s.getId().getName().equals("BackplaneControlService"))
+                        .servicePredicate((m, s) -> s.getId().getName().equals("BackplaneControlService"))
                         .build(),
                 RuntimeClientPlugin.builder()
                         .withConventions("@aws-sdk/middleware-sdk-s3", "^0.1.0-preview.2",
                                         "validateBucketNameMiddleware", HAS_MIDDLEWARE)
-                        .servicePredicate((m,s) -> s.getId().getName().equals("AmazonS3"))
+                        .servicePredicate((m, s) -> s.getId().getName().equals("AmazonS3"))
                         .build(),
                 RuntimeClientPlugin.builder()
                         .withConventions("@aws-sdk/middleware-expect-continue", "^0.1.0-preview.5",
                                         "AddExpectContinue", HAS_MIDDLEWARE)
-                        .servicePredicate((m,s) -> s.getId().getName().equals("AmazonS3"))
+                        .servicePredicate((m, s) -> s.getId().getName().equals("AmazonS3"))
                         .build()
         );
     }
