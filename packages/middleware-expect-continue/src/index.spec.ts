882636c01bf905ee18c17cdf82a85c98855b1601
Chase Coalwell <chasecoa@amazon.com>
2019-12-02 10:30:42 -0800

    Feat: update and apply expect continue middleware (#477)
    * feat: migrate expect-continue middleware
    
    * feat: apply expect-continue plugin

--- packages/middleware-expect-continue/src/index.spec.ts
+++ packages/middleware-expect-continue/src/index.spec.ts
@@ -1,56 +1,40 @@
-import { BuildHandlerArguments } from "@aws-sdk/types";
-import { addExpectContinue } from "./index";
-import { PutObject } from "./operation.mock";
+import { addExpectContinueMiddleware } from "./index";
+import { HttpRequest } from "@aws-sdk/protocol-http";
 
-describe("addExpectContinue", () => {
+describe("addExpectContinueMiddleware", () => {
   const mockNextHandler = jest.fn();
-  const mockExecutionContext = {
-    model: PutObject,
-    logger: {} as any
-  };
-  const mockHandlerArgs: Partial<BuildHandlerArguments<any>> = {
-    request: {
-      headers: {},
-      method: "PUT",
-      path: "/",
-      protocol: "https:",
-      hostname: "foo.amazonaws.com"
-    }
-  };
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it("sets the Expect header to 100-continue if there is a request body", async () => {
-    const handler = addExpectContinue(mockNextHandler, mockExecutionContext);
-    const handlerArgs = {
-      request: {
-        ...mockHandlerArgs.request,
-        body: "test"
-      }
-    };
+    const handler = addExpectContinueMiddleware()(mockNextHandler, {} as any);
+    await handler({
+      input: {},
+      request: new HttpRequest({
+        body: "foo",
+        headers: {}
+      })
+    });
 
-    await handler(handlerArgs as any);
-
-    // ensure the next handler was called
-    expect(mockNextHandler.mock.calls.length).toBe(1);
+    const { calls } = (mockNextHandler as any).mock;
+    expect(calls.length).toBe(1);
     const { request } = mockNextHandler.mock.calls[0][0];
     expect(request.headers["Expect"]).toBe("100-continue");
   });
 
   it("does not set the Expect header to 100-continue if there is no request body", async () => {
-    const handler = addExpectContinue(mockNextHandler, mockExecutionContext);
-    const handlerArgs = {
-      request: {
-        ...mockHandlerArgs.request
-      }
-    };
+    const handler = addExpectContinueMiddleware()(mockNextHandler, {} as any);
+    await handler({
+      input: {},
+      request: new HttpRequest({
+        headers: {}
+      })
+    });
 
-    await handler(handlerArgs as any);
-
-    // ensure the next handler was called
-    expect(mockNextHandler.mock.calls.length).toBe(1);
+    const { calls } = (mockNextHandler as any).mock;
+    expect(calls.length).toBe(1);
     const { request } = mockNextHandler.mock.calls[0][0];
     expect(request.headers["Expect"]).toBeUndefined();
   });
