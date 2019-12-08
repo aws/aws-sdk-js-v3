06b29ac369e2f7fef6705f76ab368345dc06b682
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-08 15:21:15 -0800

    feat: integ tests for codepipeline from v2 (#524)

--- features/codepipeline/step_definitions/codepipeline.js
+++ features/codepipeline/step_definitions/codepipeline.js
@@ -1,6 +1,8 @@
+var { CodePipeline } = require('../../../clients/node/client-codepipeline-node');
+
 module.exports = function() {
   this.Before('@codepipeline', function (callback) {
-    this.service = new this.AWS.CodePipeline({region: 'us-east-1'});
+    this.service = new CodePipeline({region: 'us-east-1'});
     callback();
   });
 
