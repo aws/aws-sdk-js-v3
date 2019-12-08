dc43928abd0472c46fda4422ce1a9ae78d363f6b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-08 15:23:51 -0800

    feat: integ tests for emr from v2 (#533)

--- features/emr/step_definitions/emr.js
+++ features/emr/step_definitions/emr.js
@@ -1,11 +1,13 @@
+var { EMR } = require('../../../clients/node/client-emr-node');
+
 module.exports = function() {
   this.Before("@emr", function (callback) {
-    this.service = new this.AWS.EMR();
+    this.service = new EMR({});
     callback();
   });
 
   this.Given(/^I run an EMR job flow with invalid parameters$/, function(callback) {
-    this.service = new this.AWS.EMR();
+    this.service = new EMR({});
     var params = {Name: '', Instances: {MasterInstanceType: 'invalid'}};
     this.request(null, 'runJobFlow', params, callback, false);
   });
