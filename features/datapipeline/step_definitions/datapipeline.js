042c7f0783705c4a695e1d350dec7fa7b163e194
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-07 22:32:03 -0800

    feat: integ tests for datapipeline from minimal changes from v2 (#500)

--- features/datapipeline/step_definitions/datapipeline.js
+++ features/datapipeline/step_definitions/datapipeline.js
@@ -1,16 +1,10 @@
+var { DataPipeline } = require('../../../clients/node/client-data-pipeline-node');
+
 module.exports = function() {
   this.Before("@datapipeline", function (callback) {
-    this.service = new this.AWS.DataPipeline();
+    this.service = new DataPipeline({});
     callback();
   });
-
-  var sortObjects = function sortObjects(objects) {
-    this.AWS.util.arrayEach(objects, function(item) {
-      item.fields.sort(function (field1, field2) {
-        return field1.key < field2.key ? -1 : 1;
-      });
-    });
-  };
 
   this.Given(/^I create a Data Pipeline with name prefix "([^"]*)"$/, function(prefix, callback) {
     var params = {
