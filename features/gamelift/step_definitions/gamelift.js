2455ba618519c0466e096cd4c85c2d3806ae0b98
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-08 15:24:12 -0800

    feat: integ tests for gamelift from v2 (#534)

--- features/gamelift/step_definitions/gamelift.js
+++ features/gamelift/step_definitions/gamelift.js
@@ -1,6 +1,8 @@
+var { GameLift } = require('../../../clients/node/client-gamelift-node');
+
 module.exports = function() {
   this.Before("@gamelift", function (callback) {
-    this.service = new this.AWS.GameLift();
+    this.service = new GameLift({});
     callback();
   });
 
