e02f7c93cfcc092a03927267e04f6536013a1a3a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-08 14:55:15 -0800

    feat: integ tests for swf from v2

--- features/swf/step_definitions/swf.js
+++ features/swf/step_definitions/swf.js
@@ -1,6 +1,8 @@
+var { SWF } = require('../../../clients/node/client-swf-node');
+
 module.exports = function() {
   this.Before('@swf', function (callback) {
-    this.service = new this.AWS.SWF();
+    this.service = new SWF({});
     callback();
   });
 };
