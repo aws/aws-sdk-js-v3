aee6cfdafed6bc6b7a3cab6710af1834001c9aa0
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-08 15:25:24 -0800

    feat: integ tests for kms from v2 (#537)

--- features/kms/step_definitions/kms.js
+++ features/kms/step_definitions/kms.js
@@ -1,6 +1,8 @@
+var { KMS } = require('../../../clients/node/client-kms-node');
+
 module.exports = function() {
   this.Before("@kms", function (callback) {
-    this.service = new this.AWS.KMS();
+    this.service = new KMS({});
     callback();
   });
 
