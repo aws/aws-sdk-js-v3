43a864d61465ffb560959bda2e249fd57bf06572
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-07 23:16:37 -0800

    feat: integ tests for elastic-load-balancing from v2 (#512)

--- features/elbv2/step_definitions/elbv2.js
+++ features/elbv2/step_definitions/elbv2.js
@@ -1,6 +1,8 @@
+var { ElasticLoadBalancingv2 } = require('../../../clients/node/client-elastic-load-balancing-v2-node');
+
 module.exports = function() {
   this.Before("@elbv2", function (callback) {
-    this.service = new this.AWS.ELBv2();
+    this.service = new ElasticLoadBalancingv2({});
     callback();
   });
 
