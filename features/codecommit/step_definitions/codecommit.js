01e19c1f195c0e98a5431a7001a9d2ba435f776a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-08 13:11:49 -0800

    feat: integ tests for codecommit from v2

--- features/codecommit/step_definitions/codecommit.js
+++ features/codecommit/step_definitions/codecommit.js
@@ -1,6 +1,8 @@
+var { CodeCommit } = require('../../../clients/node/client-codecommit-node');
+
 module.exports = function() {
   this.Before('@codecommit', function (callback) {
-    this.service = new this.AWS.CodeCommit({region: 'us-east-1'});
+    this.service = new CodeCommit({region: 'us-east-1'});
     callback();
   });
 
