0b650e20aa77c7894f36c47af8991ea65a743bc4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-08 13:45:00 -0800

    feat: integ tests for ecr from v2

--- features/ecr/step_definitions/ecr.js
+++ features/ecr/step_definitions/ecr.js
@@ -1,6 +1,8 @@
+var { ECR } = require('../../../clients/node/client-ecr-node');
+
 module.exports = function() {
   this.Before("@ecr", function (callback) {
-    this.service = new this.AWS.ECR();
+    this.service = new ECR({});
     callback();
   });
 
