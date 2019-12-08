c1f9ef504a396784c073af83c86e7afa8a18f62a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-07 22:31:50 -0800

    feat: integ tests for efs from minimal changes from v2 (#502)

--- features/efs/step_definitions/efs.js
+++ features/efs/step_definitions/efs.js
@@ -1,6 +1,8 @@
+var { EFS } = require('../../../clients/node/client-efs-node');
+
 module.exports = function() {
   this.Before('@efs', function (callback) {
-    this.service = new this.AWS.EFS({region: 'us-west-2'});
+    this.service = new EFS({region: 'us-west-2'});
     callback();
   });
 };
