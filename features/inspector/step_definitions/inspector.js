6299dd4c7c84331190be934db937e84a5f707ac6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-08 15:24:23 -0800

    feat: integ tests for inspector from v2 (#535)

--- features/inspector/step_definitions/inspector.js
+++ features/inspector/step_definitions/inspector.js
@@ -1,6 +1,8 @@
+var { Inspector } = require('../../../clients/node/client-inspector-node');
+
 module.exports = function() {
   this.Before("@inspector", function (callback) {
-    this.service = new this.AWS.Inspector({region: 'us-west-2'});
+    this.service = new Inspector({region: 'us-west-2'});
     callback();
   });
 
