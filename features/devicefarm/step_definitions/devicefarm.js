3c19540de92d56fa37d1375914c7c1db0db4c90a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-12-08 13:31:33 -0800

    feat: integ tests for device-farm from v2

--- features/devicefarm/step_definitions/devicefarm.js
+++ features/devicefarm/step_definitions/devicefarm.js
@@ -1,6 +1,8 @@
+var { DeviceFarm } = require('../../../clients/node/client-device-farm-node');
+
 module.exports = function() {
   this.Before('@devicefarm', function (callback) {
-    this.service = new this.AWS.DeviceFarm({region: 'us-west-2'});
+    this.service = new DeviceFarm({region: 'us-west-2'});
     callback();
   });
 
