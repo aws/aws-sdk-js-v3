941c8aa0a0ecd1f6c5378ad24a74dbed7a9d11e5
Chase Coalwell <chasecoa@amazon.com>
2019-12-10 11:50:36 -0800

    ssecMiddleware migration (#544)
    * feat: rename package
    
    * feat: migrate middleware-ssec
    
    * feat: apply ssec middleware plugin

---
+++ packages/middleware-ssec/src/index.spec.ts
@@ -0,0 +1,52 @@
+import { ssecMiddleware } from "./";
+import { HashConstructor } from "@aws-sdk/types";
+
+describe("ssecMiddleware", () => {
+  const next = jest.fn();
+  const decoder = jest.fn().mockResolvedValue(new Uint8Array(0));
+  const encoder = jest.fn().mockReturnValue("base64");
+  const mockHashUpdate = jest.fn();
+  const mockHashDigest = jest.fn().mockReturnValue(new Uint8Array(0));
+  const MockHash: HashConstructor = class {} as any;
+  MockHash.prototype.update = mockHashUpdate;
+  MockHash.prototype.digest = mockHashDigest;
+
+  beforeEach(() => {
+    next.mockClear();
+    decoder.mockClear();
+    encoder.mockClear();
+    mockHashUpdate.mockClear();
+    mockHashDigest.mockClear();
+  });
+
+  it("should base64 encode input keys and set respective MD5 inputs", async () => {
+    const args = {
+      input: {
+        SSECustomerKey: "foo",
+        CopySourceSSECustomerKey: "bar"
+      }
+    };
+
+    const handler = ssecMiddleware({
+      base64Encoder: encoder,
+      utf8Decoder: decoder,
+      md5: MockHash
+    })(next, {} as any);
+
+    await handler(args);
+
+    expect(next.mock.calls.length).toBe(1);
+    expect(next).toHaveBeenCalledWith({
+      input: {
+        SSECustomerKey: "base64",
+        SSECustomerKeyMD5: "base64",
+        CopySourceSSECustomerKey: "base64",
+        CopySourceSSECustomerKeyMD5: "base64"
+      }
+    });
+    expect(decoder.mock.calls.length).toBe(2);
+    expect(encoder.mock.calls.length).toBe(4);
+    expect(mockHashUpdate.mock.calls.length).toBe(2);
+    expect(mockHashDigest.mock.calls.length).toBe(2);
+  });
+});
--- packages/ssec-middleware/README.md
+++
@@ -1,4 +0,0 @@
-# @aws-sdk/ssec-middleware
-
-[![NPM version](https://img.shields.io/npm/v/@aws-sdk/ssec-middleware/preview.svg)](https://www.npmjs.com/package/@aws-sdk/ssec-middleware)
-[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/ssec-middleware.svg)](https://www.npmjs.com/package/@aws-sdk/ssec-middleware)
