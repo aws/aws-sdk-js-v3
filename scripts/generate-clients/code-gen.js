6cf55c6d687c043f6b85f4079fdd9eda451b1f86
AllanFly120 <zheallan@amazon.com>
2019-12-10 17:04:37 -0800

    chore: add yarn codegen script calling Gradle script (#545)
    * chore: add generate-clients script generating clients from models path
    
    * feat: clean the codegen input and output folder after generating
    
    * fix: update lerna commands
    
    use use '--include-dependencies' in replace of '--include-filtered-dependencies'

---
+++ scripts/generate-clients/code-gen.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const { copyFileSync, emptyDirSync } = require("fs-extra");
+const { readdirSync, lstatSync } = require("fs");
+const { spawn } = require("child_process");
+
+const CODE_GEN_INPUT_DIR = path.normalize(
+  path.join(__dirname, "..", "..", "codegen", "sdk-codegen", "aws-models")
+);
+const CODE_GEN_ROOT = path.normalize(
+  path.join(__dirname, "..", "..", "codegen")
+);
+
+async function generateClients(models) {
+  console.info("models directory: ", models);
+  if (models === CODE_GEN_INPUT_DIR) {
+    // console.log("skipping copying models to codegen directory");
+    throw new Error(
+      `models directory cannot be the same as ${CODE_GEN_INPUT_DIR}`
+    );
+  } else {
+    console.log(`clearing code gen input folder...`);
+    emptyDirSync(CODE_GEN_INPUT_DIR);
+    console.log(`copying models from ${models} to ${CODE_GEN_INPUT_DIR}...`);
+    // copySync(models, CODE_GEN_INPUT_DIR, {
+    //   overwrite: true
+    // });
+    for (const modelFileName of readdirSync(models)) {
+      const modelPath = path.join(models, modelFileName);
+      if (!lstatSync(modelPath).isFile()) continue;
+      console.log(`copying model ${modelFileName}...`);
+      copyFileSync(modelPath, path.join(CODE_GEN_INPUT_DIR, modelFileName), {
+        overwrite: true
+      });
+    }
+  }
+  await spawnProcess(
+    "./gradlew",
+    [":sdk-codegen:clean", ":sdk-codegen:build"],
+    {
+      cwd: CODE_GEN_ROOT
+    }
+  );
+}
+
+const spawnProcess = (command, args = [], options = {}) =>
+  new Promise((resolve, reject) => {
+    try {
+      const ls = spawn(command, args, options);
+      ls.stdout.on("data", data => {
+        console.log(data.toString());
+      });
+      ls.stderr.on("data", data => {
+        console.error(`stderr: ${data.toString()}`);
+      });
+
+      ls.on("close", code => {
+        console.log(`child process exited with code ${code}`);
+        resolve();
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+
+module.exports = { generateClients, CODE_GEN_INPUT_DIR };
