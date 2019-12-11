bd8d9700ffdc629aa998879d7ae3104b7bcc5e9d
Chase Coalwell <chasecoa@amazon.com>
2019-12-11 14:37:34 -0800

    feat: shared Glacier plugin with accountIdDefaultMiddleware (#548)
    * feat: add and apply accountIdDefaultMiddleware
    
    * feat: remove middleware-input-default
    
    * feat: shared plugin
    
    * fix: remove second glacier plugin

---
+++ packages/middleware-sdk-glacier/src/account-id-default.ts
@@ -0,0 +1,29 @@
+import {
+  InitializeHandler,
+  InitializeHandlerArguments,
+  InitializeHandlerOptions,
+  InitializeHandlerOutput,
+  InitializeMiddleware,
+  MetadataBearer,
+  Pluggable
+} from "@aws-sdk/types";
+
+export function accountIdDefaultMiddleware(): InitializeMiddleware<any, any> {
+  return <Output extends MetadataBearer>(
+    next: InitializeHandler<any, Output>
+  ): InitializeHandler<any, Output> => async (
+    args: InitializeHandlerArguments<any>
+  ): Promise<InitializeHandlerOutput<Output>> => {
+    const { input } = args;
+    if (input.accountId === undefined) {
+      input.accountId = "-";
+    }
+    return next({ ...args, input });
+  };
+}
+
+export const accountIdDefaultMiddlewareOptions: InitializeHandlerOptions = {
+  step: "initialize",
+  tags: ["ACCOUNT_ID_DEFAULT"],
+  name: "accountIdDefaultMiddleware"
+};
--- packages/middleware-sdk-glacier/src/index.spec.ts
+++ packages/middleware-sdk-glacier/src/index.spec.ts
@@ -1,7 +1,24 @@
-import { addGlacierApiVersionMiddleware } from "./index";
+import {
+  accountIdDefaultMiddleware,
+  addGlacierApiVersionMiddleware,
+  getGlacierPlugin,
+  resolveGlacierMiddlewareConfig
+} from "./index";
 
 describe("middleware-sdk-glacier package exports", () => {
   it("addGlacierApiVersionMiddleware", () => {
     expect(typeof addGlacierApiVersionMiddleware).toBe("function");
+  });
+
+  it("accountIdDefaultMiddleware", () => {
+    expect(typeof accountIdDefaultMiddleware).toBe("function");
+  });
+
+  it("getGlacierPlugin", () => {
+    expect(typeof getGlacierPlugin).toBe("function");
+  });
+
+  it("resolveGlacierMiddlewareConfig", () => {
+    expect(typeof resolveGlacierMiddlewareConfig).toBe("function");
   });
 });
