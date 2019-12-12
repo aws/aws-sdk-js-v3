3260bd7a3eb5fc090af0d4868a859e1e3501efcf
Kevin Stich <stickevi@amazon.com>
2019-12-12 11:45:18 -0800

    feat: update JSON protocol codegen for cleanliness (#547)
    * Update JSON protocol codegen for cleanliness
    
    This commit includes assorted updates to the protocol generators
    to improve reusability, set proper visibility, use an import alias
    for SerdeContext, and other minor updates.
    
    It also includes a fix for handling the unknown case when
    deserializing a union.
    
    * Use Model instead of deprecated ShapeIndex
    
    * Use better name for document body shape serde gen

--- codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AwsJsonRpc1_1.java
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AwsJsonRpc1_1.java
@@ -22,7 +22,7 @@ package software.amazon.smithy.aws.typescript.codegen;
  *
  * @see JsonRpcProtocolGenerator
  */
-public class AwsJsonRpc1_1 extends JsonRpcProtocolGenerator {
+final class AwsJsonRpc1_1 extends JsonRpcProtocolGenerator {
 
     @Override
     protected String getDocumentContentType() {
