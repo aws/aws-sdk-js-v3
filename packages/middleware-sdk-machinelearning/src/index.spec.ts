74e5802078fba5f0b6c17a7ec8662cb76d4a8397
Chase Coalwell <chasecoa@amazon.com>
2019-12-12 15:43:23 -0800

    feat: add predictEndpointMiddleware for Machinelearning (#553)
    * feat: add predictEndpointMiddleware
    
    * feat: remove modeled-endpoint-middleware
    
    * feat: apply PredictEndpoint plugin

---
+++ packages/middleware-sdk-machinelearning/src/index.spec.ts
@@ -0,0 +1,11 @@
+import { predictEndpointMiddleware, getPredictEndpointPlugin } from "./index";
+
+describe("middleware-sdk-machinelearning package exports", () => {
+  it("predictEndpointMiddleware", () => {
+    expect(typeof predictEndpointMiddleware).toBe("function");
+  });
+
+  it("getPredictEndpointPlugin", () => {
+    expect(typeof getPredictEndpointPlugin).toBe("function");
+  });
+});
