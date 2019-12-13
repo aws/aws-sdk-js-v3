f6659e78a77ca1b781844692d53b2b8671809ba1
Allan Zheng <zheallan@amazon.com>
2019-12-13 15:05:01 -0800

    chore: update workspace and clients npm scripts
    Now in each client you can run `yarn build` or `yarn pretest`. They are
    the same that build js files to `dist` folder. In the workspace root,
    you can run `yarn: build:all` to compile the whole workspace from .ts to
    .js. You can also `yarn: test:all` to run all the test we have. Furthermore,
    you can run `yarn test:functional` to run the hostname integration test.
    This script can only be called after the repo is compiled.

--- clients/client-rds-data/endpoints.ts
+++ clients/client-rds-data/endpoints.ts
@@ -26,47 +26,59 @@ const AWS_REGIONS = new Set([
   "us-east-1",
   "us-east-2",
   "us-west-1",
-  "us-west-2"
+  "us-west-2",
 ]);
-const AWS_CN_REGIONS = new Set(["cn-north-1", "cn-northwest-1"]);
-const AWS_ISO_REGIONS = new Set(["us-iso-east-1"]);
-const AWS_ISO_B_REGIONS = new Set(["us-isob-east-1"]);
-const AWS_US_GOV_REGIONS = new Set(["us-gov-west-1", "us-gov-east-1"]);
+const AWS_CN_REGIONS = new Set([
+  "cn-north-1",
+  "cn-northwest-1",
+]);
+const AWS_ISO_REGIONS = new Set([
+  "us-iso-east-1",
+]);
+const AWS_ISO_B_REGIONS = new Set([
+  "us-isob-east-1",
+]);
+const AWS_US_GOV_REGIONS = new Set([
+  "us-gov-west-1",
+  "us-gov-east-1",
+]);
 
-export let defaultRegionInfoProvider: RegionInfoProvider;
-defaultRegionInfoProvider = function(region: string, options?: any) {
+export const defaultRegionInfoProvider: RegionInfoProvider = (
+  region: string,
+  options?: any
+) => {
   switch (region) {
     // First, try to match exact region names.
     // Next, try to match partition endpoints.
     default:
-      if (region in AWS_REGIONS) {
+      if (AWS_REGIONS.has(region)) {
         return {
-          hostname: AWS_TEMPLATE.replace("{region}", region)
+          hostname: AWS_TEMPLATE.replace("{region}", region),
         };
       }
-      if (region in AWS_CN_REGIONS) {
+      if (AWS_CN_REGIONS.has(region)) {
         return {
-          hostname: AWS_CN_TEMPLATE.replace("{region}", region)
+          hostname: AWS_CN_TEMPLATE.replace("{region}", region),
         };
       }
-      if (region in AWS_ISO_REGIONS) {
+      if (AWS_ISO_REGIONS.has(region)) {
         return {
-          hostname: AWS_ISO_TEMPLATE.replace("{region}", region)
+          hostname: AWS_ISO_TEMPLATE.replace("{region}", region),
         };
       }
-      if (region in AWS_ISO_B_REGIONS) {
+      if (AWS_ISO_B_REGIONS.has(region)) {
         return {
-          hostname: AWS_ISO_B_TEMPLATE.replace("{region}", region)
+          hostname: AWS_ISO_B_TEMPLATE.replace("{region}", region),
         };
       }
-      if (region in AWS_US_GOV_REGIONS) {
+      if (AWS_US_GOV_REGIONS.has(region)) {
         return {
-          hostname: AWS_US_GOV_TEMPLATE.replace("{region}", region)
+          hostname: AWS_US_GOV_TEMPLATE.replace("{region}", region),
         };
       }
       // Finally, assume it's an AWS partition endpoint.
       return {
-        hostname: AWS_TEMPLATE.replace("{region}", region)
+        hostname: AWS_TEMPLATE.replace("{region}", region),
       };
   }
 };
