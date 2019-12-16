5ee70f4e9d94b8df94b417a85d57ef720c0769b1
Allan Zheng <zheallan@amazon.com>
2019-12-16 13:50:27 -0800

    fix: move static files generation to codegen

---
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AwsPackageFixturesGeneratorIntegration.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.aws.typescript.codegen;
+
+import java.util.Calendar;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import software.amazon.smithy.codegen.core.SymbolProvider;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.typescript.codegen.TypeScriptSettings;
+import software.amazon.smithy.typescript.codegen.TypeScriptWriter;
+import software.amazon.smithy.typescript.codegen.integration.TypeScriptIntegration;
+import software.amazon.smithy.utils.IoUtils;
+
+
+public class AwsPackageFixturesGeneratorIntegration implements TypeScriptIntegration {
+    @Override
+    public void writeAdditionalFiles(
+            TypeScriptSettings settings,
+            Model model,
+            SymbolProvider symbolProvider,
+            BiConsumer<String, Consumer<TypeScriptWriter>> writerFactory
+    ) {
+        writerFactory.accept(".gitignore", writer -> {
+            String resource =  IoUtils.readUtf8Resource(
+                    getClass().getClassLoader(), "software/amazon/smithy/aws/typescript/codegen/gitignore");
+            writer.write(resource);
+        });
+        writerFactory.accept(".npmignore", writer -> {
+            String resource =  IoUtils.readUtf8Resource(
+                    getClass().getClassLoader(), "software/amazon/smithy/aws/typescript/codegen/npmignore");
+            writer.write(resource);
+        });
+        writerFactory.accept("LICENCE", writer -> {
+            String resource =  IoUtils.readUtf8Resource(
+                    getClass().getClassLoader(), "software/amazon/smithy/aws/typescript/codegen/LICENCE.template");
+            resource = resource.replace("${year}", Integer.toString(Calendar.getInstance().get(Calendar.YEAR)));
+            writer.write(resource);
+        });
+        writerFactory.accept("README.md", writer -> {
+            String resource =  IoUtils.readUtf8Resource(
+                    getClass().getClassLoader(), "software/amazon/smithy/aws/typescript/codegen/README.md.template");
+            resource = resource.replaceAll("\\$\\{packageName\\}", settings.getPackageName());
+            writer.write(resource);
+        });
+    }
+}
--- scripts/generate-clients/copy-to-clients.js
+++ scripts/generate-clients/copy-to-clients.js
@@ -1,13 +1,6 @@
 const { join, normalize } = require("path");
 const { copySync, ensureDirSync } = require("fs-extra");
-const handlebars = require("handlebars");
-const {
-  readdirSync,
-  lstatSync,
-  readFileSync,
-  existsSync,
-  writeFileSync
-} = require("fs");
+const { readdirSync, lstatSync, readFileSync, existsSync } = require("fs");
 
 const CODE_GEN_OUTPUT_DIR = normalize(
   join(
@@ -22,18 +15,27 @@ const CODE_GEN_OUTPUT_DIR = normalize(
   )
 );
 
-/**
- * templates are a black list of files we don't want codegen artifact
- * to override in the clients folder
- */
-const templates = readdirSync(join(__dirname, "templates"))
-  .filter(name => /.sample$/.test(name))
-  .reduce((accumulator, curr) => {
-    const templatePath = join(__dirname, "templates", curr);
-    const template = readFileSync(templatePath).toString();
-    accumulator[curr.replace(/.sample$/, "")] = template;
-    return accumulator;
-  }, {});
+const getOverwritablePredicate = packageName => pathName => {
+  const overwritablePathnames = [
+    "commands",
+    "lib",
+    "models",
+    "protocols",
+    "LICENCE",
+    "runtimeConfig.ts",
+    "runtimeConfig.browser.ts",
+    "runtimeConfig.shared.ts",
+    "index.ts"
+  ];
+  return (
+    pathName.toLowerCase().indexOf(
+      packageName
+        .toLowerCase()
+        .replace("@aws-sdk/client-", "")
+        .replace("-", "")
+    ) >= 0 || overwritablePathnames.indexOf(pathName) >= 0
+  );
+};
 
 async function copyToClients(clientsDir) {
   for (const modelName of readdirSync(CODE_GEN_OUTPUT_DIR)) {
@@ -55,68 +57,17 @@ async function copyToClients(clientsDir) {
     const packageName = packageManifest.name;
     console.log(`copying ${packageName} from ${artifactPath} to ${clientsDir}`);
     const destPath = join(clientsDir, packageName.replace("@aws-sdk/", ""));
-
-    //Data used to generate files from template
-    const templateData = {
-      year: new Date().getFullYear(),
-      packageName
-    };
-
-    for (const packageSub of [
-      ...readdirSync(artifactPath),
-      ...Object.keys(templates)
-    ]) {
+    const overwritablePredicate = getOverwritablePredicate(packageName);
+    for (const packageSub of readdirSync(artifactPath)) {
       const packageSubPath = join(artifactPath, packageSub);
       const destSubPath = join(destPath, packageSub);
-
-      if (Object.keys(templates).indexOf(packageSub) >= 0) {
-        if (packageSub === "package.json") {
-          /**
-           * Copy package.json content in detail.
-           * Basically merge the generated package.json and dest package.json
-           * but prefer the values from dest when they contain the same key
-           * */
-          const destManifest = JSON.parse(
-            existsSync(destSubPath)
-              ? readFileSync(destSubPath).toString()
-              : handlebars.compile(templates[packageSub])(templateData)
-          );
-          const updatedManifest = mergeManifest(packageManifest, destManifest);
-          writeFileSync(destSubPath, JSON.stringify(updatedManifest, null, 2));
-        } else if (!existsSync(destSubPath)) {
-          //for files not yet exists and we have a template for it; generate from template
-          const file = handlebars.compile(templates[packageSub])(templateData);
-          writeFileSync(destSubPath, file);
-        } else {
-          //for files we have template but we already have a new version in clients folder, always prefer current one
-          //PASS
-        }
-      } else {
-        //For things not in codegen artifact black list, overwrite the existing ones.
+      if (overwritablePredicate(packageSub) || !existsSync(destSubPath)) {
+        //Overwrite the directories and files that are overwritable, or not yet exists
         if (lstatSync(packageSubPath).isDirectory()) ensureDirSync(destSubPath);
         copySync(packageSubPath, destSubPath, { overwrite: true });
       }
     }
   }
 }
-
-const mergeManifest = (source, dest) => {
-  return {
-    ...source,
-    ...dest,
-    scripts: {
-      ...source.scripts,
-      ...dest.scripts
-    },
-    dependencies: {
-      ...source.dependencies,
-      ...dest.dependencies
-    },
-    devDependencies: {
-      ...source.devDependencies,
-      ...dest.devDependencies
-    }
-  };
-};
 
 module.exports = { copyToClients, CODE_GEN_OUTPUT_DIR };
