fb2f6bcacd3b72a800c5de4245c530658ff65c1e
Michael Dowling <mtdowling@gmail.com>
2019-12-17 14:50:55 -0800

    feat: start endpoint resolver generation (#472)

--- clients/client-rds-data/runtimeConfig.shared.ts
+++ clients/client-rds-data/runtimeConfig.shared.ts
@@ -1,4 +1,6 @@
+import { defaultRegionInfoProvider } from "./endpoints";
 export const ClientSharedValues = {
   apiVersion: "2018-08-01",
   signingName: "rds-data",
+  regionInfoProvider: defaultRegionInfoProvider
 };
---
+++ tests/functional/endpoints/fixtures.js
@@ -0,0 +1,25 @@
+//reference: https://github.com/boto/botocore/blob/develop/tests/functional/test_regions.py
+module.exports.KNOWN_REGIONS = {
+  "ap-northeast-1": {
+    "rds-data": "rds-data.ap-northeast-1.amazonaws.com"
+  },
+  "ap-southeast-1": {
+    "rds-data": "rds-data.ap-southeast-1.amazonaws.com"
+  },
+  "ap-southeast-2": {
+    "rds-data": "rds-data.ap-southeast-2.amazonaws.com"
+  },
+  "aws-us-gov-global": {},
+  "cn-north-1": {
+    "rds-data": "rds-data.cn-north-1.amazonaws.com.cn"
+  },
+  "eu-central-1": {},
+  "eu-west-1": {},
+  "fips-us-gov-west-1": {},
+  "s3-external-1": {},
+  "sa-east-1": {},
+  "us-east-1": {},
+  "us-gov-west-1": {},
+  "us-west-1": {},
+  "us-west-2": {}
+};
