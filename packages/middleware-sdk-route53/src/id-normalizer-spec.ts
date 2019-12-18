e28382758efb7e5964c939806b7fcb2fad2c3649
Chase Coalwell <chasecoa@amazon.com>
2019-12-18 10:26:10 -0800

    feat: migrate route-53 middleware and apply plugin (#550)

---
+++ packages/middleware-sdk-route53/src/id-normalizer-spec.ts
@@ -0,0 +1,24 @@
+import { idNormalizerMiddleware } from "./id-normalizer";
+
+const prefixedProps = ["/hostedzone/ID", "/change/ID", "/delegationset/ID"];
+const idParams = ["DelegationSetId", "HostedZoneId", "Id"];
+
+describe("idNormalizerMiddleware", () => {
+  for (const paramName of idParams) {
+    for (const prefixed of prefixedProps) {
+      it(`should strip the prefix from the ${paramName} parameter`, async () => {
+        const next = jest.fn();
+        const input = { [paramName]: prefixed };
+
+        const handler = idNormalizerMiddleware()(next, {} as any);
+
+        await handler({ input });
+
+        expect(next.mock.calls.length).toBe(1);
+        expect(next.mock.calls[0][0]).toEqual({
+          input: { [paramName]: "ID" }
+        });
+      });
+    }
+  }
+});
