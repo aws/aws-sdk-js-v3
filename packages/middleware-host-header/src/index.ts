2664c8599b7e4cade092a5548b630eb1ab63bad6
Allan Zheng <zheallan@amazon.com>
2019-12-18 23:58:26 -0800

    feat: add a middleware inserting right host header

---
+++ packages/middleware-host-header/src/index.ts
@@ -0,0 +1,61 @@
+import { HttpRequest } from "@aws-sdk/protocol-http";
+import {
+  RequestHandler,
+  BuildMiddleware,
+  Provider,
+  Endpoint,
+  BuildHandlerOptions,
+  AbsoluteLocation,
+  Pluggable
+} from "@aws-sdk/types";
+
+export interface HostHeaderInputConfig {}
+interface PreviouslyResolved {
+  requestHandler: RequestHandler<any, any>;
+  endpoint: Provider<Endpoint>;
+}
+export interface HostHeaderResolvedConfig {
+  requestHandler: RequestHandler<any, any>;
+  endpoint: Provider<Endpoint>;
+}
+export function resolveHostHeaderConfig<T>(
+  input: T & PreviouslyResolved & HostHeaderInputConfig
+): T & HostHeaderResolvedConfig {
+  return input;
+}
+
+export const hostHeaderMiddleware = <
+  Input extends object,
+  Output extends object
+>(
+  options: HostHeaderResolvedConfig
+): BuildMiddleware<Input, Output> => next => async args => {
+  if (!HttpRequest.isInstance(args.request)) return next(args);
+  const { request } = args;
+  const { metadata = [] } = options.requestHandler;
+  //For H2 request, remove 'host' header and use ':authority' header instead
+  //reference: https://nodejs.org/dist/latest-v13.x/docs/api/errors.html#ERR_HTTP2_INVALID_CONNECTION_HEADERS
+  if (metadata.includes("h2") && !request.headers[":authority"]) {
+    delete request.headers["host"];
+    request.headers[":authority"] = "";
+    //non-H2 request and 'host' header is not set, set the 'host' header to request's hostname.
+  } else if (!request.headers["host"]) {
+    request.headers["host"] = (await options.endpoint()).hostname;
+  }
+  return next(args);
+};
+
+export const hostHeaderMiddlewareOptions: BuildHandlerOptions &
+  AbsoluteLocation = {
+  name: "hostHeaderMiddleware",
+  step: "build",
+  tags: ["HOST"]
+};
+
+export const getHostHeaderPlugin = (
+  options: HostHeaderResolvedConfig
+): Pluggable<any, any> => ({
+  applyToStack: clientStack => {
+    clientStack.add(hostHeaderMiddleware(options), hostHeaderMiddlewareOptions);
+  }
+});
