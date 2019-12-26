867c0549f5292f76a70e8722c1f3d4e694870473
Allan Zheng <zheallan@amazon.com>
2019-12-26 12:51:04 -0800

    fix: use Response constructor to collect readableStream

--- packages/stream-collector-browser/src/index.ts
+++ packages/stream-collector-browser/src/index.ts
@@ -3,32 +3,7 @@ import { StreamCollector } from "@aws-sdk/types";
 export const streamCollector: StreamCollector = (
   stream: ReadableStream
 ): Promise<Uint8Array> => {
-  // new Promise<Uint8Array>((resolve, reject) => {
-  // const reader = new FileReader();
-  // reader.onload = () => resolve(new Uint8Array(reader.result as ArrayBuffer));
-  // reader.onabort = () => reject(new Error("Read aborted"));
-  // reader.onerror = () => reject(reader.error);
-  // reader.readAsArrayBuffer(stream);
-  // });
-  const reader = stream.getReader();
-  const collectingStream = new ReadableStream({
-    start(controller) {
-      const pump = (): Promise<any> => {
-        return reader.read().then(({ done, value }) => {
-          // When no more data needs to be consumed, close the stream
-          if (done) {
-            controller.close();
-            return;
-          }
-          // Enqueue the next data chunk into our target stream
-          controller.enqueue(value);
-          return pump();
-        });
-      };
-      return pump();
-    }
-  });
-  return new Response(collectingStream).blob().then(blob => {
+  return new Response(stream).blob().then(blob => {
     const reader = new FileReader();
     reader.readAsArrayBuffer(blob);
     return new Uint8Array(reader.result as ArrayBuffer);
