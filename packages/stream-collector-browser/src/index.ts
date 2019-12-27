2ea91060245d1db44fcf9b73c417a0f8ed4aeb73
Allan Zheng <zheallan@amazon.com>
2019-12-26 17:13:41 -0800

    fix: use Response.arrayBuffer() in replace of FileReader

--- packages/stream-collector-browser/src/index.ts
+++ packages/stream-collector-browser/src/index.ts
@@ -3,14 +3,7 @@ import { StreamCollector } from "@aws-sdk/types";
 export const streamCollector: StreamCollector = (
   stream: ReadableStream
 ): Promise<Uint8Array> => {
-  const blobToArrayBuffer = (stream: Blob) =>
-    new Promise<Uint8Array>((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = () =>
-        resolve(new Uint8Array(reader.result as ArrayBuffer));
-      reader.onabort = () => reject(new Error("Read aborted"));
-      reader.onerror = () => reject(reader.error);
-      reader.readAsArrayBuffer(stream);
-    });
-  return new Response(stream).blob().then(blobToArrayBuffer);
+  return new Response(stream)
+    .arrayBuffer()
+    .then(arrayBuffer => new Uint8Array(arrayBuffer));
 };
