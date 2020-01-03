c43de93826707c41f212076ce4ec4e7526ae5ad9
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-03 09:49:32 -0800

    feat: generate client-api-gateway (#615)

---
+++ clients/client-api-gateway/commands/CreateDomainNameCommand.ts
@@ -0,0 +1,78 @@
+import {
+  APIGatewayClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes,
+} from "../APIGatewayClient";
+import {
+  CreateDomainNameRequest,
+  DomainName,
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1CreateDomainNameCommand,
+  serializeAws_restJson1_1CreateDomainNameCommand,
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse,
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+} from "@aws-sdk/types";
+
+export type CreateDomainNameCommandInput = CreateDomainNameRequest;
+export type CreateDomainNameCommandOutput = DomainName;
+
+export class CreateDomainNameCommand extends $Command<CreateDomainNameCommandInput, CreateDomainNameCommandOutput, APIGatewayClientResolvedConfig> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateDomainNameCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: APIGatewayClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateDomainNameCommandInput, CreateDomainNameCommandOutput> {
+    this.middlewareStack.use(getSerdePlugin(configuration, this.serialize, this.deserialize));
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any,
+    }
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateDomainNameCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1CreateDomainNameCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateDomainNameCommandOutput> {
+    return deserializeAws_restJson1_1CreateDomainNameCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
