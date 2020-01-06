a73a7b8d3025b61507c639b2ef4eb2006c84e845
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:07:35 -0800

    feat: add client-signer (#643)

---
+++ clients/client-signer/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-signer
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-signer/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-signer)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-signer.svg)](https://www.npmjs.com/package/@aws-sdk/client-signer)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-signer/commands/GetSigningProfileCommand.ts
@@ -0,0 +1,84 @@
+import {
+  GetSigningProfileRequest,
+  GetSigningProfileResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1GetSigningProfileCommand,
+  serializeAws_restJson1_1GetSigningProfileCommand
+} from "../protocols/Aws_restJson1_1";
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  signerClientResolvedConfig
+} from "../signerClient";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetSigningProfileCommandInput = GetSigningProfileRequest;
+export type GetSigningProfileCommandOutput = GetSigningProfileResponse;
+
+export class GetSigningProfileCommand extends $Command<
+  GetSigningProfileCommandInput,
+  GetSigningProfileCommandOutput,
+  signerClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetSigningProfileCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: signerClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetSigningProfileCommandInput, GetSigningProfileCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetSigningProfileCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1GetSigningProfileCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetSigningProfileCommandOutput> {
+    return deserializeAws_restJson1_1GetSigningProfileCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
