77b16617ada78041ea17fd432ee0bd5f1d2ca24f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:43 -0800

    feat: add client-codestar-notifications (#643)

---
+++ clients/client-codestar-notifications/commands/UpdateNotificationRuleCommand.ts
@@ -0,0 +1,93 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  codestarnotificationsClientResolvedConfig
+} from "../codestarnotificationsClient";
+import {
+  UpdateNotificationRuleRequest,
+  UpdateNotificationRuleResult
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1UpdateNotificationRuleCommand,
+  serializeAws_restJson1_1UpdateNotificationRuleCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateNotificationRuleCommandInput = UpdateNotificationRuleRequest;
+export type UpdateNotificationRuleCommandOutput = UpdateNotificationRuleResult;
+
+export class UpdateNotificationRuleCommand extends $Command<
+  UpdateNotificationRuleCommandInput,
+  UpdateNotificationRuleCommandOutput,
+  codestarnotificationsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateNotificationRuleCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: codestarnotificationsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateNotificationRuleCommandInput,
+    UpdateNotificationRuleCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateNotificationRuleCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1UpdateNotificationRuleCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateNotificationRuleCommandOutput> {
+    return deserializeAws_restJson1_1UpdateNotificationRuleCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
