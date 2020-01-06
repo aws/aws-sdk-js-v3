970e40737cd231bd28b46db69464b4b24c601923
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 10:49:57 -0800

    feat: add client-apigatewayv2 (#642)

---
+++ clients/client-apigatewayv2/commands/GetModelsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ApiGatewayV2ClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ApiGatewayV2Client";
+import { GetModelsRequest, GetModelsResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1GetModelsCommand,
+  serializeAws_restJson1_1GetModelsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetModelsCommandInput = GetModelsRequest;
+export type GetModelsCommandOutput = GetModelsResponse;
+
+export class GetModelsCommand extends $Command<
+  GetModelsCommandInput,
+  GetModelsCommandOutput,
+  ApiGatewayV2ClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetModelsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ApiGatewayV2ClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetModelsCommandInput, GetModelsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetModelsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1GetModelsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetModelsCommandOutput> {
+    return deserializeAws_restJson1_1GetModelsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
