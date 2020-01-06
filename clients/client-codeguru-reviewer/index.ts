47e0868c9bd10825648d573da8231fcf099d55e1
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:41 -0800

    feat: add client-codeguru-reviewer (#643)

---
+++ clients/client-codeguru-reviewer/index.ts
@@ -0,0 +1,6 @@
+export * from "./CodeGuruReviewerClient";
+export * from "./CodeGuruReviewer";
+export * from "./commands/AssociateRepositoryCommand";
+export * from "./commands/ListRepositoryAssociationsCommand";
+export * from "./commands/DisassociateRepositoryCommand";
+export * from "./commands/DescribeRepositoryAssociationCommand";
