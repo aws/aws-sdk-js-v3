cd5a004032d181f24273ae74f953873015dd6a0a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:04:56 -0800

    feat: add client-iot-1click-projects (#643)

---
+++ clients/client-iot-1click-projects/commands/DisassociateDeviceFromPlacementCommand.ts
@@ -0,0 +1,93 @@
+import {
+  IoT1ClickProjectsClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../IoT1ClickProjectsClient";
+import {
+  DisassociateDeviceFromPlacementRequest,
+  DisassociateDeviceFromPlacementResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1DisassociateDeviceFromPlacementCommand,
+  serializeAws_restJson1_1DisassociateDeviceFromPlacementCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DisassociateDeviceFromPlacementCommandInput = DisassociateDeviceFromPlacementRequest;
+export type DisassociateDeviceFromPlacementCommandOutput = DisassociateDeviceFromPlacementResponse;
+
+export class DisassociateDeviceFromPlacementCommand extends $Command<
+  DisassociateDeviceFromPlacementCommandInput,
+  DisassociateDeviceFromPlacementCommandOutput,
+  IoT1ClickProjectsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DisassociateDeviceFromPlacementCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: IoT1ClickProjectsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DisassociateDeviceFromPlacementCommandInput,
+    DisassociateDeviceFromPlacementCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DisassociateDeviceFromPlacementCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DisassociateDeviceFromPlacementCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DisassociateDeviceFromPlacementCommandOutput> {
+    return deserializeAws_restJson1_1DisassociateDeviceFromPlacementCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
