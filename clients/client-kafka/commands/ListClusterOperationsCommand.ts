75ed041f91f9b6254a31bf989671d250c495fa1f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:05:14 -0800

    feat: add client-kafka (#643)

---
+++ clients/client-kafka/commands/ListClusterOperationsCommand.ts
@@ -0,0 +1,90 @@
+import {
+  KafkaClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../KafkaClient";
+import {
+  ListClusterOperationsRequest,
+  ListClusterOperationsResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1ListClusterOperationsCommand,
+  serializeAws_restJson1_1ListClusterOperationsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListClusterOperationsCommandInput = ListClusterOperationsRequest;
+export type ListClusterOperationsCommandOutput = ListClusterOperationsResponse;
+
+export class ListClusterOperationsCommand extends $Command<
+  ListClusterOperationsCommandInput,
+  ListClusterOperationsCommandOutput,
+  KafkaClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListClusterOperationsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: KafkaClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListClusterOperationsCommandInput,
+    ListClusterOperationsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListClusterOperationsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1ListClusterOperationsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListClusterOperationsCommandOutput> {
+    return deserializeAws_restJson1_1ListClusterOperationsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
