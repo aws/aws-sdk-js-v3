f099dc3995fb87ffa126bafa3128f526bf88f3be
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 13:28:14 -0800

    chore: remove service-types-generator

--- packages/service-types-generator/src/Components/Model/helpers.ts
+++
@@ -1,5 +0,0 @@
-import { SerializationModel } from "@aws-sdk/types";
-
-export function requiresImport(shape: SerializationModel): boolean {
-  return ["list", "map", "structure"].indexOf(shape.type) > -1;
-}
