bb9205303f444fc6283965be233df81f8ad3cc46
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:06:23 -0800

    feat: add client-personalize-runtime (#643)

---
+++ clients/client-personalize-runtime/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-personalize-runtime
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-personalize-runtime/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-personalize-runtime)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-personalize-runtime.svg)](https://www.npmjs.com/package/@aws-sdk/client-personalize-runtime)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-personalize-runtime/commands/GetPersonalizedRankingCommand.ts
@@ -0,0 +1,93 @@
+import {
+  PersonalizeRuntimeClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../PersonalizeRuntimeClient";
+import {
+  GetPersonalizedRankingRequest,
+  GetPersonalizedRankingResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1GetPersonalizedRankingCommand,
+  serializeAws_restJson1_1GetPersonalizedRankingCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetPersonalizedRankingCommandInput = GetPersonalizedRankingRequest;
+export type GetPersonalizedRankingCommandOutput = GetPersonalizedRankingResponse;
+
+export class GetPersonalizedRankingCommand extends $Command<
+  GetPersonalizedRankingCommandInput,
+  GetPersonalizedRankingCommandOutput,
+  PersonalizeRuntimeClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetPersonalizedRankingCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: PersonalizeRuntimeClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    GetPersonalizedRankingCommandInput,
+    GetPersonalizedRankingCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetPersonalizedRankingCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1GetPersonalizedRankingCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetPersonalizedRankingCommandOutput> {
+    return deserializeAws_restJson1_1GetPersonalizedRankingCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
