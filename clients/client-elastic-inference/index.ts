d8888d1647f2b01265ae69b55a40f092377bc66d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:04:18 -0800

    feat: add client-elastic-inference (#643)

---
+++ clients/client-elastic-inference/index.ts
@@ -0,0 +1,5 @@
+export * from "./ElasticInferenceClient";
+export * from "./ElasticInference";
+export * from "./commands/TagResourceCommand";
+export * from "./commands/ListTagsForResourceCommand";
+export * from "./commands/UntagResourceCommand";
