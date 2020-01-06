717efdb64968b5731ca57f5d24a651359b510d8a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:07:29 -0800

    feat: add client-sesv2 (#643)

---
+++ clients/client-sesv2/commands/ListDedicatedIpPoolsCommand.ts
@@ -0,0 +1,90 @@
+import {
+  SESv2ClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SESv2Client";
+import {
+  ListDedicatedIpPoolsRequest,
+  ListDedicatedIpPoolsResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1ListDedicatedIpPoolsCommand,
+  serializeAws_restJson1_1ListDedicatedIpPoolsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListDedicatedIpPoolsCommandInput = ListDedicatedIpPoolsRequest;
+export type ListDedicatedIpPoolsCommandOutput = ListDedicatedIpPoolsResponse;
+
+export class ListDedicatedIpPoolsCommand extends $Command<
+  ListDedicatedIpPoolsCommandInput,
+  ListDedicatedIpPoolsCommandOutput,
+  SESv2ClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListDedicatedIpPoolsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SESv2ClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListDedicatedIpPoolsCommandInput,
+    ListDedicatedIpPoolsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListDedicatedIpPoolsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1ListDedicatedIpPoolsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListDedicatedIpPoolsCommandOutput> {
+    return deserializeAws_restJson1_1ListDedicatedIpPoolsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
