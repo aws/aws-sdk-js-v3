c2ef372566b77ca6c3c676679c2a17006d541c2e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:04:25 -0800

    feat: add client-elasticsearch-service (#643)

---
+++ clients/client-elasticsearch-service/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-elasticsearch-service
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-elasticsearch-service/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-elasticsearch-service)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-elasticsearch-service.svg)](https://www.npmjs.com/package/@aws-sdk/client-elasticsearch-service)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-elasticsearch-service/commands/DescribeReservedElasticsearchInstanceOfferingsCommand.ts
@@ -0,0 +1,95 @@
+import {
+  ElasticsearchServiceClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ElasticsearchServiceClient";
+import {
+  DescribeReservedElasticsearchInstanceOfferingsRequest,
+  DescribeReservedElasticsearchInstanceOfferingsResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1DescribeReservedElasticsearchInstanceOfferingsCommand,
+  serializeAws_restJson1_1DescribeReservedElasticsearchInstanceOfferingsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeReservedElasticsearchInstanceOfferingsCommandInput = DescribeReservedElasticsearchInstanceOfferingsRequest;
+export type DescribeReservedElasticsearchInstanceOfferingsCommandOutput = DescribeReservedElasticsearchInstanceOfferingsResponse;
+
+export class DescribeReservedElasticsearchInstanceOfferingsCommand extends $Command<
+  DescribeReservedElasticsearchInstanceOfferingsCommandInput,
+  DescribeReservedElasticsearchInstanceOfferingsCommandOutput,
+  ElasticsearchServiceClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(
+    readonly input: DescribeReservedElasticsearchInstanceOfferingsCommandInput
+  ) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ElasticsearchServiceClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeReservedElasticsearchInstanceOfferingsCommandInput,
+    DescribeReservedElasticsearchInstanceOfferingsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeReservedElasticsearchInstanceOfferingsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DescribeReservedElasticsearchInstanceOfferingsCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeReservedElasticsearchInstanceOfferingsCommandOutput> {
+    return deserializeAws_restJson1_1DescribeReservedElasticsearchInstanceOfferingsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
