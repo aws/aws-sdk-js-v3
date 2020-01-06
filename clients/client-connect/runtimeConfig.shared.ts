ba9f8bcd054ad164c3bf9559896df1ab7de73fad
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:50 -0800

    feat: add client-connect (#643)

---
+++ clients/client-connect/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2017-08-08",
+  signingName: "connect",
+  regionInfoProvider: defaultRegionInfoProvider
+};
