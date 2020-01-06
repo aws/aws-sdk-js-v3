e56ebed15398d03eddb2114f47fbb6db92c0d430
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:46 -0800

    feat: add client-cognito-sync (#643)

---
+++ clients/client-cognito-sync/commands/UpdateRecordsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  CognitoSyncClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CognitoSyncClient";
+import { UpdateRecordsRequest, UpdateRecordsResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1UpdateRecordsCommand,
+  serializeAws_restJson1_1UpdateRecordsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateRecordsCommandInput = UpdateRecordsRequest;
+export type UpdateRecordsCommandOutput = UpdateRecordsResponse;
+
+export class UpdateRecordsCommand extends $Command<
+  UpdateRecordsCommandInput,
+  UpdateRecordsCommandOutput,
+  CognitoSyncClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateRecordsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CognitoSyncClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<UpdateRecordsCommandInput, UpdateRecordsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateRecordsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1UpdateRecordsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateRecordsCommandOutput> {
+    return deserializeAws_restJson1_1UpdateRecordsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
