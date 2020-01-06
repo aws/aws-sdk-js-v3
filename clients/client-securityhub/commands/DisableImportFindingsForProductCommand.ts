6c57800e979e954193f60d1073f4d974d0bf8df3
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:07:21 -0800

    feat: add client-securityhub (#643)

---
+++ clients/client-securityhub/commands/DisableImportFindingsForProductCommand.ts
@@ -0,0 +1,93 @@
+import {
+  SecurityHubClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SecurityHubClient";
+import {
+  DisableImportFindingsForProductRequest,
+  DisableImportFindingsForProductResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1DisableImportFindingsForProductCommand,
+  serializeAws_restJson1_1DisableImportFindingsForProductCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DisableImportFindingsForProductCommandInput = DisableImportFindingsForProductRequest;
+export type DisableImportFindingsForProductCommandOutput = DisableImportFindingsForProductResponse;
+
+export class DisableImportFindingsForProductCommand extends $Command<
+  DisableImportFindingsForProductCommandInput,
+  DisableImportFindingsForProductCommandOutput,
+  SecurityHubClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DisableImportFindingsForProductCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SecurityHubClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DisableImportFindingsForProductCommandInput,
+    DisableImportFindingsForProductCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DisableImportFindingsForProductCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DisableImportFindingsForProductCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DisableImportFindingsForProductCommandOutput> {
+    return deserializeAws_restJson1_1DisableImportFindingsForProductCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
