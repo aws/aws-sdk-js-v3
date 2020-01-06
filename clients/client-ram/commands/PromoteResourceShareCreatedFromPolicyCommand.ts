0234a0ef113d6776495a9c7a87a0ab447d67d851
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:06:57 -0800

    feat: add client-ram (#643)

---
+++ clients/client-ram/commands/PromoteResourceShareCreatedFromPolicyCommand.ts
@@ -0,0 +1,95 @@
+import {
+  RAMClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../RAMClient";
+import {
+  PromoteResourceShareCreatedFromPolicyRequest,
+  PromoteResourceShareCreatedFromPolicyResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1PromoteResourceShareCreatedFromPolicyCommand,
+  serializeAws_restJson1_1PromoteResourceShareCreatedFromPolicyCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type PromoteResourceShareCreatedFromPolicyCommandInput = PromoteResourceShareCreatedFromPolicyRequest;
+export type PromoteResourceShareCreatedFromPolicyCommandOutput = PromoteResourceShareCreatedFromPolicyResponse;
+
+export class PromoteResourceShareCreatedFromPolicyCommand extends $Command<
+  PromoteResourceShareCreatedFromPolicyCommandInput,
+  PromoteResourceShareCreatedFromPolicyCommandOutput,
+  RAMClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(
+    readonly input: PromoteResourceShareCreatedFromPolicyCommandInput
+  ) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: RAMClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    PromoteResourceShareCreatedFromPolicyCommandInput,
+    PromoteResourceShareCreatedFromPolicyCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: PromoteResourceShareCreatedFromPolicyCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1PromoteResourceShareCreatedFromPolicyCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<PromoteResourceShareCreatedFromPolicyCommandOutput> {
+    return deserializeAws_restJson1_1PromoteResourceShareCreatedFromPolicyCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
