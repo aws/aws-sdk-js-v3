82258e4796310cc91bf6d7d835ba3e53c052a091
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:20 -0800

    feat: add client-batch (#643)

---
+++ clients/client-batch/commands/DescribeComputeEnvironmentsCommand.ts
@@ -0,0 +1,93 @@
+import {
+  BatchClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../BatchClient";
+import {
+  DescribeComputeEnvironmentsRequest,
+  DescribeComputeEnvironmentsResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1DescribeComputeEnvironmentsCommand,
+  serializeAws_restJson1_1DescribeComputeEnvironmentsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeComputeEnvironmentsCommandInput = DescribeComputeEnvironmentsRequest;
+export type DescribeComputeEnvironmentsCommandOutput = DescribeComputeEnvironmentsResponse;
+
+export class DescribeComputeEnvironmentsCommand extends $Command<
+  DescribeComputeEnvironmentsCommandInput,
+  DescribeComputeEnvironmentsCommandOutput,
+  BatchClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeComputeEnvironmentsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: BatchClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeComputeEnvironmentsCommandInput,
+    DescribeComputeEnvironmentsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeComputeEnvironmentsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DescribeComputeEnvironmentsCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeComputeEnvironmentsCommandOutput> {
+    return deserializeAws_restJson1_1DescribeComputeEnvironmentsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
