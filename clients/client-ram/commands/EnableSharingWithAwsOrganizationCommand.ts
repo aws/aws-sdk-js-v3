aa4c024bf9fc27ea2ee926019a57d0755286b28b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:06:57 -0800

    feat: add client-ram (#643)

---
+++ clients/client-ram/commands/EnableSharingWithAwsOrganizationCommand.ts
@@ -0,0 +1,93 @@
+import {
+  RAMClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../RAMClient";
+import {
+  EnableSharingWithAwsOrganizationRequest,
+  EnableSharingWithAwsOrganizationResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1EnableSharingWithAwsOrganizationCommand,
+  serializeAws_restJson1_1EnableSharingWithAwsOrganizationCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type EnableSharingWithAwsOrganizationCommandInput = EnableSharingWithAwsOrganizationRequest;
+export type EnableSharingWithAwsOrganizationCommandOutput = EnableSharingWithAwsOrganizationResponse;
+
+export class EnableSharingWithAwsOrganizationCommand extends $Command<
+  EnableSharingWithAwsOrganizationCommandInput,
+  EnableSharingWithAwsOrganizationCommandOutput,
+  RAMClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: EnableSharingWithAwsOrganizationCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: RAMClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    EnableSharingWithAwsOrganizationCommandInput,
+    EnableSharingWithAwsOrganizationCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: EnableSharingWithAwsOrganizationCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1EnableSharingWithAwsOrganizationCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<EnableSharingWithAwsOrganizationCommandOutput> {
+    return deserializeAws_restJson1_1EnableSharingWithAwsOrganizationCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-ram/commands/ListPrincipalsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  RAMClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../RAMClient";
+import { ListPrincipalsRequest, ListPrincipalsResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1ListPrincipalsCommand,
+  serializeAws_restJson1_1ListPrincipalsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListPrincipalsCommandInput = ListPrincipalsRequest;
+export type ListPrincipalsCommandOutput = ListPrincipalsResponse;
+
+export class ListPrincipalsCommand extends $Command<
+  ListPrincipalsCommandInput,
+  ListPrincipalsCommandOutput,
+  RAMClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListPrincipalsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: RAMClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListPrincipalsCommandInput, ListPrincipalsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListPrincipalsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1ListPrincipalsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListPrincipalsCommandOutput> {
+    return deserializeAws_restJson1_1ListPrincipalsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
