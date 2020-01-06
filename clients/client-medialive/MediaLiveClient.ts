a5d826d290d8819bac3de56f3f96084b03bf7a35
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:05:49 -0800

    feat: add client-medialive (#643)

---
+++ clients/client-medialive/MediaLiveClient.ts
@@ -0,0 +1,362 @@
+import {
+  BatchUpdateScheduleRequest,
+  BatchUpdateScheduleResponse,
+  CreateChannelRequest,
+  CreateChannelResponse,
+  CreateInputRequest,
+  CreateInputResponse,
+  CreateInputSecurityGroupRequest,
+  CreateInputSecurityGroupResponse,
+  CreateMultiplexProgramRequest,
+  CreateMultiplexProgramResponse,
+  CreateMultiplexRequest,
+  CreateMultiplexResponse,
+  CreateTagsRequest,
+  DeleteChannelRequest,
+  DeleteChannelResponse,
+  DeleteInputRequest,
+  DeleteInputResponse,
+  DeleteInputSecurityGroupRequest,
+  DeleteInputSecurityGroupResponse,
+  DeleteMultiplexProgramRequest,
+  DeleteMultiplexProgramResponse,
+  DeleteMultiplexRequest,
+  DeleteMultiplexResponse,
+  DeleteReservationRequest,
+  DeleteReservationResponse,
+  DeleteScheduleRequest,
+  DeleteScheduleResponse,
+  DeleteTagsRequest,
+  DescribeChannelRequest,
+  DescribeChannelResponse,
+  DescribeInputRequest,
+  DescribeInputResponse,
+  DescribeInputSecurityGroupRequest,
+  DescribeInputSecurityGroupResponse,
+  DescribeMultiplexProgramRequest,
+  DescribeMultiplexProgramResponse,
+  DescribeMultiplexRequest,
+  DescribeMultiplexResponse,
+  DescribeOfferingRequest,
+  DescribeOfferingResponse,
+  DescribeReservationRequest,
+  DescribeReservationResponse,
+  DescribeScheduleRequest,
+  DescribeScheduleResponse,
+  ListChannelsRequest,
+  ListChannelsResponse,
+  ListInputSecurityGroupsRequest,
+  ListInputSecurityGroupsResponse,
+  ListInputsRequest,
+  ListInputsResponse,
+  ListMultiplexProgramsRequest,
+  ListMultiplexProgramsResponse,
+  ListMultiplexesRequest,
+  ListMultiplexesResponse,
+  ListOfferingsRequest,
+  ListOfferingsResponse,
+  ListReservationsRequest,
+  ListReservationsResponse,
+  ListTagsForResourceRequest,
+  ListTagsForResourceResponse,
+  PurchaseOfferingRequest,
+  PurchaseOfferingResponse,
+  StartChannelRequest,
+  StartChannelResponse,
+  StartMultiplexRequest,
+  StartMultiplexResponse,
+  StopChannelRequest,
+  StopChannelResponse,
+  StopMultiplexRequest,
+  StopMultiplexResponse,
+  UpdateChannelClassRequest,
+  UpdateChannelClassResponse,
+  UpdateChannelRequest,
+  UpdateChannelResponse,
+  UpdateInputRequest,
+  UpdateInputResponse,
+  UpdateInputSecurityGroupRequest,
+  UpdateInputSecurityGroupResponse,
+  UpdateMultiplexProgramRequest,
+  UpdateMultiplexProgramResponse,
+  UpdateMultiplexRequest,
+  UpdateMultiplexResponse,
+  UpdateReservationRequest,
+  UpdateReservationResponse
+} from "./models/index";
+import { ClientDefaultValues as __ClientDefaultValues } from "./runtimeConfig";
+import {
+  EndpointsInputConfig,
+  EndpointsResolvedConfig,
+  RegionInputConfig,
+  RegionResolvedConfig,
+  resolveEndpointsConfig,
+  resolveRegionConfig
+} from "@aws-sdk/config-resolver";
+import { getContentLengthPlugin } from "@aws-sdk/middleware-content-length";
+import {
+  HostHeaderInputConfig,
+  HostHeaderResolvedConfig,
+  getHostHeaderPlugin,
+  resolveHostHeaderConfig
+} from "@aws-sdk/middleware-host-header";
+import {
+  RetryInputConfig,
+  RetryResolvedConfig,
+  getRetryPlugin,
+  resolveRetryConfig
+} from "@aws-sdk/middleware-retry";
+import {
+  AwsAuthInputConfig,
+  AwsAuthResolvedConfig,
+  getAwsAuthPlugin,
+  resolveAwsAuthConfig
+} from "@aws-sdk/middleware-signing";
+import {
+  UserAgentInputConfig,
+  UserAgentResolvedConfig,
+  getUserAgentPlugin,
+  resolveUserAgentConfig
+} from "@aws-sdk/middleware-user-agent";
+import { HttpHandler as __HttpHandler } from "@aws-sdk/protocol-http";
+import {
+  Client as __Client,
+  SmithyConfiguration as __SmithyConfiguration,
+  SmithyResolvedConfiguration as __SmithyResolvedConfiguration
+} from "@aws-sdk/smithy-client";
+import {
+  RegionInfoProvider,
+  Credentials as __Credentials,
+  Decoder as __Decoder,
+  Encoder as __Encoder,
+  HashConstructor as __HashConstructor,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer,
+  Provider as __Provider,
+  StreamCollector as __StreamCollector,
+  UrlParser as __UrlParser
+} from "@aws-sdk/types";
+
+export type ServiceInputTypes =
+  | BatchUpdateScheduleRequest
+  | CreateChannelRequest
+  | CreateInputRequest
+  | CreateInputSecurityGroupRequest
+  | CreateMultiplexProgramRequest
+  | CreateMultiplexRequest
+  | CreateTagsRequest
+  | DeleteChannelRequest
+  | DeleteInputRequest
+  | DeleteInputSecurityGroupRequest
+  | DeleteMultiplexProgramRequest
+  | DeleteMultiplexRequest
+  | DeleteReservationRequest
+  | DeleteScheduleRequest
+  | DeleteTagsRequest
+  | DescribeChannelRequest
+  | DescribeInputRequest
+  | DescribeInputSecurityGroupRequest
+  | DescribeMultiplexProgramRequest
+  | DescribeMultiplexRequest
+  | DescribeOfferingRequest
+  | DescribeReservationRequest
+  | DescribeScheduleRequest
+  | ListChannelsRequest
+  | ListInputSecurityGroupsRequest
+  | ListInputsRequest
+  | ListMultiplexProgramsRequest
+  | ListMultiplexesRequest
+  | ListOfferingsRequest
+  | ListReservationsRequest
+  | ListTagsForResourceRequest
+  | PurchaseOfferingRequest
+  | StartChannelRequest
+  | StartMultiplexRequest
+  | StopChannelRequest
+  | StopMultiplexRequest
+  | UpdateChannelClassRequest
+  | UpdateChannelRequest
+  | UpdateInputRequest
+  | UpdateInputSecurityGroupRequest
+  | UpdateMultiplexProgramRequest
+  | UpdateMultiplexRequest
+  | UpdateReservationRequest;
+
+export type ServiceOutputTypes =
+  | __MetadataBearer
+  | BatchUpdateScheduleResponse
+  | CreateChannelResponse
+  | CreateInputResponse
+  | CreateInputSecurityGroupResponse
+  | CreateMultiplexProgramResponse
+  | CreateMultiplexResponse
+  | DeleteChannelResponse
+  | DeleteInputResponse
+  | DeleteInputSecurityGroupResponse
+  | DeleteMultiplexProgramResponse
+  | DeleteMultiplexResponse
+  | DeleteReservationResponse
+  | DeleteScheduleResponse
+  | DescribeChannelResponse
+  | DescribeInputResponse
+  | DescribeInputSecurityGroupResponse
+  | DescribeMultiplexProgramResponse
+  | DescribeMultiplexResponse
+  | DescribeOfferingResponse
+  | DescribeReservationResponse
+  | DescribeScheduleResponse
+  | ListChannelsResponse
+  | ListInputSecurityGroupsResponse
+  | ListInputsResponse
+  | ListMultiplexProgramsResponse
+  | ListMultiplexesResponse
+  | ListOfferingsResponse
+  | ListReservationsResponse
+  | ListTagsForResourceResponse
+  | PurchaseOfferingResponse
+  | StartChannelResponse
+  | StartMultiplexResponse
+  | StopChannelResponse
+  | StopMultiplexResponse
+  | UpdateChannelClassResponse
+  | UpdateChannelResponse
+  | UpdateInputResponse
+  | UpdateInputSecurityGroupResponse
+  | UpdateMultiplexProgramResponse
+  | UpdateMultiplexResponse
+  | UpdateReservationResponse;
+
+export interface ClientDefaults
+  extends Partial<__SmithyResolvedConfiguration<__HttpHandlerOptions>> {
+  /**
+   * The HTTP handler to use. Fetch in browser and Https in Nodejs.
+   */
+  requestHandler?: __HttpHandler;
+
+  /**
+   * A constructor for a class implementing the @aws-sdk/types.Hash interface
+   * that computes the SHA-256 HMAC or checksum of a string or binary buffer.
+   */
+  sha256?: __HashConstructor;
+
+  /**
+   * The function that will be used to convert strings into HTTP endpoints.
+   */
+  urlParser?: __UrlParser;
+
+  /**
+   * A function that can calculate the length of a request body.
+   */
+  bodyLengthChecker?: (body: any) => number | undefined;
+
+  /**
+   * A function that converts a stream into an array of bytes.
+   */
+  streamCollector?: __StreamCollector;
+
+  /**
+   * The function that will be used to convert a base64-encoded string to a byte array
+   */
+  base64Decoder?: __Decoder;
+
+  /**
+   * The function that will be used to convert binary data to a base64-encoded string
+   */
+  base64Encoder?: __Encoder;
+
+  /**
+   * The function that will be used to convert a UTF8-encoded string to a byte array
+   */
+  utf8Decoder?: __Decoder;
+
+  /**
+   * The function that will be used to convert binary data to a UTF-8 encoded string
+   */
+  utf8Encoder?: __Encoder;
+
+  /**
+   * The string that will be used to populate default value in 'User-Agent' header
+   */
+  defaultUserAgent?: string;
+
+  /**
+   * The runtime environment
+   */
+  runtime?: string;
+
+  /**
+   * The service name with which to sign requests.
+   */
+  signingName?: string;
+
+  /**
+   * Default credentials provider; Not available in browser runtime
+   */
+  credentialDefaultProvider?: (input: any) => __Provider<__Credentials>;
+
+  /**
+   * Provider function that return promise of a region string
+   */
+  regionDefaultProvider?: (input: any) => __Provider<string>;
+
+  /**
+   * Fetch related hostname, signing name or signing region with given region.
+   */
+  regionInfoProvider?: RegionInfoProvider;
+}
+
+export type MediaLiveClientConfig = Partial<
+  __SmithyConfiguration<__HttpHandlerOptions>
+> &
+  ClientDefaults &
+  RegionInputConfig &
+  EndpointsInputConfig &
+  AwsAuthInputConfig &
+  RetryInputConfig &
+  UserAgentInputConfig &
+  HostHeaderInputConfig;
+
+export type MediaLiveClientResolvedConfig = __SmithyResolvedConfiguration<
+  __HttpHandlerOptions
+> &
+  Required<ClientDefaults> &
+  RegionResolvedConfig &
+  EndpointsResolvedConfig &
+  AwsAuthResolvedConfig &
+  RetryResolvedConfig &
+  UserAgentResolvedConfig &
+  HostHeaderResolvedConfig;
+
+/**
+ * API for AWS Elemental MediaLive
+ */
+export class MediaLiveClient extends __Client<
+  __HttpHandlerOptions,
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  MediaLiveClientResolvedConfig
+> {
+  readonly config: MediaLiveClientResolvedConfig;
+
+  constructor(configuration: MediaLiveClientConfig) {
+    let _config_0 = {
+      ...__ClientDefaultValues,
+      ...configuration
+    };
+    let _config_1 = resolveRegionConfig(_config_0);
+    let _config_2 = resolveEndpointsConfig(_config_1);
+    let _config_3 = resolveAwsAuthConfig(_config_2);
+    let _config_4 = resolveRetryConfig(_config_3);
+    let _config_5 = resolveUserAgentConfig(_config_4);
+    let _config_6 = resolveHostHeaderConfig(_config_5);
+    super(_config_6);
+    this.config = _config_6;
+    this.middlewareStack.use(getAwsAuthPlugin(this.config));
+    this.middlewareStack.use(getRetryPlugin(this.config));
+    this.middlewareStack.use(getUserAgentPlugin(this.config));
+    this.middlewareStack.use(getContentLengthPlugin(this.config));
+    this.middlewareStack.use(getHostHeaderPlugin(this.config));
+  }
+
+  destroy(): void {}
+}
---
+++ clients/client-medialive/commands/UpdateChannelCommand.ts
@@ -0,0 +1,81 @@
+import {
+  MediaLiveClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MediaLiveClient";
+import { UpdateChannelRequest, UpdateChannelResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1UpdateChannelCommand,
+  serializeAws_restJson1_1UpdateChannelCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateChannelCommandInput = UpdateChannelRequest;
+export type UpdateChannelCommandOutput = UpdateChannelResponse;
+
+export class UpdateChannelCommand extends $Command<
+  UpdateChannelCommandInput,
+  UpdateChannelCommandOutput,
+  MediaLiveClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateChannelCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MediaLiveClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<UpdateChannelCommandInput, UpdateChannelCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateChannelCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1UpdateChannelCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateChannelCommandOutput> {
+    return deserializeAws_restJson1_1UpdateChannelCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
