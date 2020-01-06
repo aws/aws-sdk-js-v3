95bb468fc2c3d255e6d1c55e72831472c0995648
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:07:39 -0800

    feat: add client-sso (#643)

---
+++ clients/client-sso/index.ts
@@ -0,0 +1,6 @@
+export * from "./SSOClient";
+export * from "./SSO";
+export * from "./commands/GetRoleCredentialsCommand";
+export * from "./commands/LogoutCommand";
+export * from "./commands/ListAccountRolesCommand";
+export * from "./commands/ListAccountsCommand";
