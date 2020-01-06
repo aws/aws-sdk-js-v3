611720cbea1c3075fbcb2ebd0aaf27ce56d181e7
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:07 -0800

    feat: add client-appconfig (#643)

---
+++ clients/client-appconfig/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-appconfig
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-appconfig/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-appconfig)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-appconfig.svg)](https://www.npmjs.com/package/@aws-sdk/client-appconfig)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-appconfig/commands/GetDeploymentStrategyCommand.ts
@@ -0,0 +1,90 @@
+import {
+  AppConfigClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../AppConfigClient";
+import {
+  DeploymentStrategy,
+  GetDeploymentStrategyRequest
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1GetDeploymentStrategyCommand,
+  serializeAws_restJson1_1GetDeploymentStrategyCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetDeploymentStrategyCommandInput = GetDeploymentStrategyRequest;
+export type GetDeploymentStrategyCommandOutput = DeploymentStrategy;
+
+export class GetDeploymentStrategyCommand extends $Command<
+  GetDeploymentStrategyCommandInput,
+  GetDeploymentStrategyCommandOutput,
+  AppConfigClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetDeploymentStrategyCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: AppConfigClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    GetDeploymentStrategyCommandInput,
+    GetDeploymentStrategyCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetDeploymentStrategyCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1GetDeploymentStrategyCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetDeploymentStrategyCommandOutput> {
+    return deserializeAws_restJson1_1GetDeploymentStrategyCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
