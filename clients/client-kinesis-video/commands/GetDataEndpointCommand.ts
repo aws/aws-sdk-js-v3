0244db048e07d4d9ebd6279d848808b18c8fa294
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:05:17 -0800

    feat: add client-kinesis-video (#643)

---
+++ clients/client-kinesis-video/commands/GetDataEndpointCommand.ts
@@ -0,0 +1,81 @@
+import {
+  KinesisVideoClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../KinesisVideoClient";
+import { GetDataEndpointInput, GetDataEndpointOutput } from "../models/index";
+import {
+  deserializeAws_restJson1_1GetDataEndpointCommand,
+  serializeAws_restJson1_1GetDataEndpointCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetDataEndpointCommandInput = GetDataEndpointInput;
+export type GetDataEndpointCommandOutput = GetDataEndpointOutput;
+
+export class GetDataEndpointCommand extends $Command<
+  GetDataEndpointCommandInput,
+  GetDataEndpointCommandOutput,
+  KinesisVideoClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetDataEndpointCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: KinesisVideoClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetDataEndpointCommandInput, GetDataEndpointCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetDataEndpointCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1GetDataEndpointCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetDataEndpointCommandOutput> {
+    return deserializeAws_restJson1_1GetDataEndpointCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
