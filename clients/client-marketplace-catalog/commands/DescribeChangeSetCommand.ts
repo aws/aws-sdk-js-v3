daee2b8b21e81cd2834d4572322dec31da7ce203
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:05:41 -0800

    feat: add client-marketplace-catalog (#643)

---
+++ clients/client-marketplace-catalog/commands/DescribeChangeSetCommand.ts
@@ -0,0 +1,84 @@
+import {
+  MarketplaceCatalogClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MarketplaceCatalogClient";
+import {
+  DescribeChangeSetRequest,
+  DescribeChangeSetResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1DescribeChangeSetCommand,
+  serializeAws_restJson1_1DescribeChangeSetCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeChangeSetCommandInput = DescribeChangeSetRequest;
+export type DescribeChangeSetCommandOutput = DescribeChangeSetResponse;
+
+export class DescribeChangeSetCommand extends $Command<
+  DescribeChangeSetCommandInput,
+  DescribeChangeSetCommandOutput,
+  MarketplaceCatalogClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeChangeSetCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MarketplaceCatalogClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeChangeSetCommandInput, DescribeChangeSetCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeChangeSetCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DescribeChangeSetCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeChangeSetCommandOutput> {
+    return deserializeAws_restJson1_1DescribeChangeSetCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
