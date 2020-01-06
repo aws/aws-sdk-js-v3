e2b2b5518e34e07023ca2ac3b3310d00d5a19fac
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:41 -0800

    feat: add client-codeguru-reviewer (#643)

---
+++ clients/client-codeguru-reviewer/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2019-09-19",
+  signingName: "codeguru-reviewer",
+  regionInfoProvider: defaultRegionInfoProvider
+};
