f635cf9ae76a8952a6c512d3b31141c926b551ed
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:04:14 -0800

    feat: add client-eks (#643)

---
+++ clients/client-eks/commands/DescribeNodegroupCommand.ts
@@ -0,0 +1,84 @@
+import {
+  EKSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../EKSClient";
+import {
+  DescribeNodegroupRequest,
+  DescribeNodegroupResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1DescribeNodegroupCommand,
+  serializeAws_restJson1_1DescribeNodegroupCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeNodegroupCommandInput = DescribeNodegroupRequest;
+export type DescribeNodegroupCommandOutput = DescribeNodegroupResponse;
+
+export class DescribeNodegroupCommand extends $Command<
+  DescribeNodegroupCommandInput,
+  DescribeNodegroupCommandOutput,
+  EKSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeNodegroupCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: EKSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeNodegroupCommandInput, DescribeNodegroupCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeNodegroupCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DescribeNodegroupCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeNodegroupCommandOutput> {
+    return deserializeAws_restJson1_1DescribeNodegroupCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-eks/commands/UpdateClusterConfigCommand.ts
@@ -0,0 +1,90 @@
+import {
+  EKSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../EKSClient";
+import {
+  UpdateClusterConfigRequest,
+  UpdateClusterConfigResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1UpdateClusterConfigCommand,
+  serializeAws_restJson1_1UpdateClusterConfigCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateClusterConfigCommandInput = UpdateClusterConfigRequest;
+export type UpdateClusterConfigCommandOutput = UpdateClusterConfigResponse;
+
+export class UpdateClusterConfigCommand extends $Command<
+  UpdateClusterConfigCommandInput,
+  UpdateClusterConfigCommandOutput,
+  EKSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateClusterConfigCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: EKSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateClusterConfigCommandInput,
+    UpdateClusterConfigCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateClusterConfigCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1UpdateClusterConfigCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateClusterConfigCommandOutput> {
+    return deserializeAws_restJson1_1UpdateClusterConfigCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
