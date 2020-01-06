a89e8fae043827bc439be2bf57b996ee1040ed14
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:20 -0800

    feat: add client-batch (#643)

---
+++ clients/client-batch/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-batch
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-batch/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-batch)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-batch.svg)](https://www.npmjs.com/package/@aws-sdk/client-batch)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-batch/commands/UpdateComputeEnvironmentCommand.ts
@@ -0,0 +1,93 @@
+import {
+  BatchClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../BatchClient";
+import {
+  UpdateComputeEnvironmentRequest,
+  UpdateComputeEnvironmentResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1UpdateComputeEnvironmentCommand,
+  serializeAws_restJson1_1UpdateComputeEnvironmentCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateComputeEnvironmentCommandInput = UpdateComputeEnvironmentRequest;
+export type UpdateComputeEnvironmentCommandOutput = UpdateComputeEnvironmentResponse;
+
+export class UpdateComputeEnvironmentCommand extends $Command<
+  UpdateComputeEnvironmentCommandInput,
+  UpdateComputeEnvironmentCommandOutput,
+  BatchClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateComputeEnvironmentCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: BatchClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateComputeEnvironmentCommandInput,
+    UpdateComputeEnvironmentCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateComputeEnvironmentCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1UpdateComputeEnvironmentCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateComputeEnvironmentCommandOutput> {
+    return deserializeAws_restJson1_1UpdateComputeEnvironmentCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
