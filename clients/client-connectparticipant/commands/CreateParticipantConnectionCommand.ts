9f37b164c31a7d870e01b2795f83d0336df4f304
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:53 -0800

    feat: add client-connectparticipant (#643)

---
+++ clients/client-connectparticipant/commands/CreateParticipantConnectionCommand.ts
@@ -0,0 +1,93 @@
+import {
+  ConnectParticipantClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ConnectParticipantClient";
+import {
+  CreateParticipantConnectionRequest,
+  CreateParticipantConnectionResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1CreateParticipantConnectionCommand,
+  serializeAws_restJson1_1CreateParticipantConnectionCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateParticipantConnectionCommandInput = CreateParticipantConnectionRequest;
+export type CreateParticipantConnectionCommandOutput = CreateParticipantConnectionResponse;
+
+export class CreateParticipantConnectionCommand extends $Command<
+  CreateParticipantConnectionCommandInput,
+  CreateParticipantConnectionCommandOutput,
+  ConnectParticipantClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateParticipantConnectionCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ConnectParticipantClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    CreateParticipantConnectionCommandInput,
+    CreateParticipantConnectionCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateParticipantConnectionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1CreateParticipantConnectionCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateParticipantConnectionCommandOutput> {
+    return deserializeAws_restJson1_1CreateParticipantConnectionCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
