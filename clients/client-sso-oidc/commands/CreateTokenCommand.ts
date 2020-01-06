785d4b3ed38dbc1313adaac948d4ad25d0e4251a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:07:42 -0800

    feat: add client-sso-oidc (#643)

---
+++ clients/client-sso-oidc/commands/CreateTokenCommand.ts
@@ -0,0 +1,81 @@
+import {
+  SSOOIDCClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SSOOIDCClient";
+import { CreateTokenRequest, CreateTokenResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1CreateTokenCommand,
+  serializeAws_restJson1_1CreateTokenCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateTokenCommandInput = CreateTokenRequest;
+export type CreateTokenCommandOutput = CreateTokenResponse;
+
+export class CreateTokenCommand extends $Command<
+  CreateTokenCommandInput,
+  CreateTokenCommandOutput,
+  SSOOIDCClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateTokenCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SSOOIDCClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateTokenCommandInput, CreateTokenCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateTokenCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1CreateTokenCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateTokenCommandOutput> {
+    return deserializeAws_restJson1_1CreateTokenCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-sso-oidc/commands/RegisterClientCommand.ts
@@ -0,0 +1,81 @@
+import {
+  SSOOIDCClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SSOOIDCClient";
+import { RegisterClientRequest, RegisterClientResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1RegisterClientCommand,
+  serializeAws_restJson1_1RegisterClientCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type RegisterClientCommandInput = RegisterClientRequest;
+export type RegisterClientCommandOutput = RegisterClientResponse;
+
+export class RegisterClientCommand extends $Command<
+  RegisterClientCommandInput,
+  RegisterClientCommandOutput,
+  SSOOIDCClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: RegisterClientCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SSOOIDCClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<RegisterClientCommandInput, RegisterClientCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: RegisterClientCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1RegisterClientCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<RegisterClientCommandOutput> {
+    return deserializeAws_restJson1_1RegisterClientCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
