6a0dbf3ff8d8b70e3d303a13ecb80bf8949b1d8e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:04:45 -0800

    feat: add client-iot (#643)

---
+++ clients/client-iot/commands/ListTopicRuleDestinationsCommand.ts
@@ -0,0 +1,93 @@
+import {
+  IoTClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../IoTClient";
+import {
+  ListTopicRuleDestinationsRequest,
+  ListTopicRuleDestinationsResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1ListTopicRuleDestinationsCommand,
+  serializeAws_restJson1_1ListTopicRuleDestinationsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListTopicRuleDestinationsCommandInput = ListTopicRuleDestinationsRequest;
+export type ListTopicRuleDestinationsCommandOutput = ListTopicRuleDestinationsResponse;
+
+export class ListTopicRuleDestinationsCommand extends $Command<
+  ListTopicRuleDestinationsCommandInput,
+  ListTopicRuleDestinationsCommandOutput,
+  IoTClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListTopicRuleDestinationsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: IoTClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListTopicRuleDestinationsCommandInput,
+    ListTopicRuleDestinationsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListTopicRuleDestinationsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1ListTopicRuleDestinationsCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListTopicRuleDestinationsCommandOutput> {
+    return deserializeAws_restJson1_1ListTopicRuleDestinationsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-iot/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2015-05-28",
+  signingName: "execute-api",
+  regionInfoProvider: defaultRegionInfoProvider
+};
