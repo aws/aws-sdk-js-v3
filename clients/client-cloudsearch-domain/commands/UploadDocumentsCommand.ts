ac943ce2aa66dedc6de1168464a1ea2ef2de76bf
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:38 -0800

    feat: add client-cloudsearch-domain (#643)

---
+++ clients/client-cloudsearch-domain/commands/UploadDocumentsCommand.ts
@@ -0,0 +1,84 @@
+import {
+  CloudSearchDomainClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudSearchDomainClient";
+import {
+  UploadDocumentsRequest,
+  UploadDocumentsResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1UploadDocumentsCommand,
+  serializeAws_restJson1_1UploadDocumentsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UploadDocumentsCommandInput = UploadDocumentsRequest;
+export type UploadDocumentsCommandOutput = UploadDocumentsResponse;
+
+export class UploadDocumentsCommand extends $Command<
+  UploadDocumentsCommandInput,
+  UploadDocumentsCommandOutput,
+  CloudSearchDomainClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UploadDocumentsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudSearchDomainClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<UploadDocumentsCommandInput, UploadDocumentsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UploadDocumentsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1UploadDocumentsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UploadDocumentsCommandOutput> {
+    return deserializeAws_restJson1_1UploadDocumentsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
