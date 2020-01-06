99f83e2414376b4fbc9a09bdd5cba9225ec5eb20
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:07:17 -0800

    feat: add client-schemas (#643)

---
+++ clients/client-schemas/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-schemas
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-schemas/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-schemas)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-schemas.svg)](https://www.npmjs.com/package/@aws-sdk/client-schemas)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-schemas/commands/GetCodeBindingSourceCommand.ts
@@ -0,0 +1,90 @@
+import {
+  GetCodeBindingSourceRequest,
+  GetCodeBindingSourceResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1GetCodeBindingSourceCommand,
+  serializeAws_restJson1_1GetCodeBindingSourceCommand
+} from "../protocols/Aws_restJson1_1";
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  schemasClientResolvedConfig
+} from "../schemasClient";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetCodeBindingSourceCommandInput = GetCodeBindingSourceRequest;
+export type GetCodeBindingSourceCommandOutput = GetCodeBindingSourceResponse;
+
+export class GetCodeBindingSourceCommand extends $Command<
+  GetCodeBindingSourceCommandInput,
+  GetCodeBindingSourceCommandOutput,
+  schemasClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetCodeBindingSourceCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: schemasClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    GetCodeBindingSourceCommandInput,
+    GetCodeBindingSourceCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetCodeBindingSourceCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1GetCodeBindingSourceCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetCodeBindingSourceCommandOutput> {
+    return deserializeAws_restJson1_1GetCodeBindingSourceCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
