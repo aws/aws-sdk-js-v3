f353e3ba4fdff174e1372965d72e75d62a7c363d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:06:02 -0800

    feat: add client-mediastore-data (#643)

---
+++ clients/client-mediastore-data/commands/DescribeObjectCommand.ts
@@ -0,0 +1,81 @@
+import {
+  MediaStoreDataClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MediaStoreDataClient";
+import { DescribeObjectRequest, DescribeObjectResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1DescribeObjectCommand,
+  serializeAws_restJson1_1DescribeObjectCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeObjectCommandInput = DescribeObjectRequest;
+export type DescribeObjectCommandOutput = DescribeObjectResponse;
+
+export class DescribeObjectCommand extends $Command<
+  DescribeObjectCommandInput,
+  DescribeObjectCommandOutput,
+  MediaStoreDataClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeObjectCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MediaStoreDataClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeObjectCommandInput, DescribeObjectCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeObjectCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DescribeObjectCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeObjectCommandOutput> {
+    return deserializeAws_restJson1_1DescribeObjectCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
