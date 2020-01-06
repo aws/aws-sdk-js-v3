b31d587ea8d3a3d21f0408b9c43508df33e99b36
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:06:41 -0800

    feat: add client-pinpoint-sms-voice (#643)

---
+++ clients/client-pinpoint-sms-voice/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2018-09-05",
+  signingName: "sms-voice",
+  regionInfoProvider: defaultRegionInfoProvider
+};
