442613f675308def6b7533533a6186f7b9cf9299
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:06:23 -0800

    feat: add client-personalize-runtime (#643)

---
+++ clients/client-personalize-runtime/index.ts
@@ -0,0 +1,4 @@
+export * from "./PersonalizeRuntimeClient";
+export * from "./PersonalizeRuntime";
+export * from "./commands/GetPersonalizedRankingCommand";
+export * from "./commands/GetRecommendationsCommand";
