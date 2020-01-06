46784d140bd810568d5e4af5400a0c0584e867d6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:43 -0800

    feat: add client-codestar-notifications (#643)

---
+++ clients/client-codestar-notifications/commands/SubscribeCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  codestarnotificationsClientResolvedConfig
+} from "../codestarnotificationsClient";
+import { SubscribeRequest, SubscribeResult } from "../models/index";
+import {
+  deserializeAws_restJson1_1SubscribeCommand,
+  serializeAws_restJson1_1SubscribeCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type SubscribeCommandInput = SubscribeRequest;
+export type SubscribeCommandOutput = SubscribeResult;
+
+export class SubscribeCommand extends $Command<
+  SubscribeCommandInput,
+  SubscribeCommandOutput,
+  codestarnotificationsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: SubscribeCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: codestarnotificationsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<SubscribeCommandInput, SubscribeCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: SubscribeCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1SubscribeCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<SubscribeCommandOutput> {
+    return deserializeAws_restJson1_1SubscribeCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
