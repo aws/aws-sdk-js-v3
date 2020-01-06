8b2cea1589a2d08177a4b42f9763d4d8dc436e18
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:03:56 -0800

    feat: add client-dataexchange (#643)

---
+++ clients/client-dataexchange/commands/StartJobCommand.ts
@@ -0,0 +1,81 @@
+import {
+  DataExchangeClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DataExchangeClient";
+import { StartJobRequest, StartJobResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1StartJobCommand,
+  serializeAws_restJson1_1StartJobCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type StartJobCommandInput = StartJobRequest;
+export type StartJobCommandOutput = StartJobResponse;
+
+export class StartJobCommand extends $Command<
+  StartJobCommandInput,
+  StartJobCommandOutput,
+  DataExchangeClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: StartJobCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DataExchangeClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<StartJobCommandInput, StartJobCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: StartJobCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1StartJobCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<StartJobCommandOutput> {
+    return deserializeAws_restJson1_1StartJobCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-dataexchange/commands/UpdateRevisionCommand.ts
@@ -0,0 +1,81 @@
+import {
+  DataExchangeClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DataExchangeClient";
+import { UpdateRevisionRequest, UpdateRevisionResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1UpdateRevisionCommand,
+  serializeAws_restJson1_1UpdateRevisionCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateRevisionCommandInput = UpdateRevisionRequest;
+export type UpdateRevisionCommandOutput = UpdateRevisionResponse;
+
+export class UpdateRevisionCommand extends $Command<
+  UpdateRevisionCommandInput,
+  UpdateRevisionCommandOutput,
+  DataExchangeClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateRevisionCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DataExchangeClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<UpdateRevisionCommandInput, UpdateRevisionCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateRevisionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1UpdateRevisionCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateRevisionCommandOutput> {
+    return deserializeAws_restJson1_1UpdateRevisionCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
