09bfbc2cd6b0102a4f646428f2ab4134221f065f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:06:44 -0800

    feat: add client-polly (#643)

---
+++ clients/client-polly/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-polly
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-polly/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-polly)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-polly.svg)](https://www.npmjs.com/package/@aws-sdk/client-polly)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-polly/commands/DescribeVoicesCommand.ts
@@ -0,0 +1,81 @@
+import {
+  PollyClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../PollyClient";
+import { DescribeVoicesInput, DescribeVoicesOutput } from "../models/index";
+import {
+  deserializeAws_restJson1_1DescribeVoicesCommand,
+  serializeAws_restJson1_1DescribeVoicesCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeVoicesCommandInput = DescribeVoicesInput;
+export type DescribeVoicesCommandOutput = DescribeVoicesOutput;
+
+export class DescribeVoicesCommand extends $Command<
+  DescribeVoicesCommandInput,
+  DescribeVoicesCommandOutput,
+  PollyClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeVoicesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: PollyClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeVoicesCommandInput, DescribeVoicesCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeVoicesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DescribeVoicesCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeVoicesCommandOutput> {
+    return deserializeAws_restJson1_1DescribeVoicesCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
