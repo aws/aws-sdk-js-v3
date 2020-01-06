c5ba48884ec49b97f1f1987f46c9367721d784e1
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:07:59 -0800

    feat: add client-xray (#643)

---
+++ clients/client-xray/commands/CreateSamplingRuleCommand.ts
@@ -0,0 +1,84 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  XRayClientResolvedConfig
+} from "../XRayClient";
+import {
+  CreateSamplingRuleRequest,
+  CreateSamplingRuleResult
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1CreateSamplingRuleCommand,
+  serializeAws_restJson1_1CreateSamplingRuleCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateSamplingRuleCommandInput = CreateSamplingRuleRequest;
+export type CreateSamplingRuleCommandOutput = CreateSamplingRuleResult;
+
+export class CreateSamplingRuleCommand extends $Command<
+  CreateSamplingRuleCommandInput,
+  CreateSamplingRuleCommandOutput,
+  XRayClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateSamplingRuleCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: XRayClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateSamplingRuleCommandInput, CreateSamplingRuleCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateSamplingRuleCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1CreateSamplingRuleCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateSamplingRuleCommandOutput> {
+    return deserializeAws_restJson1_1CreateSamplingRuleCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-xray/commands/GetSamplingRulesCommand.ts
@@ -0,0 +1,84 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  XRayClientResolvedConfig
+} from "../XRayClient";
+import {
+  GetSamplingRulesRequest,
+  GetSamplingRulesResult
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1GetSamplingRulesCommand,
+  serializeAws_restJson1_1GetSamplingRulesCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetSamplingRulesCommandInput = GetSamplingRulesRequest;
+export type GetSamplingRulesCommandOutput = GetSamplingRulesResult;
+
+export class GetSamplingRulesCommand extends $Command<
+  GetSamplingRulesCommandInput,
+  GetSamplingRulesCommandOutput,
+  XRayClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetSamplingRulesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: XRayClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetSamplingRulesCommandInput, GetSamplingRulesCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetSamplingRulesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1GetSamplingRulesCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetSamplingRulesCommandOutput> {
+    return deserializeAws_restJson1_1GetSamplingRulesCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
