ccaf964449c80f78b83bac2221e0c7f64ce102bf
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:04:59 -0800

    feat: add client-iot-events (#643)

---
+++ clients/client-iot-events/commands/DescribeDetectorModelCommand.ts
@@ -0,0 +1,90 @@
+import {
+  IoTEventsClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../IoTEventsClient";
+import {
+  DescribeDetectorModelRequest,
+  DescribeDetectorModelResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1DescribeDetectorModelCommand,
+  serializeAws_restJson1_1DescribeDetectorModelCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeDetectorModelCommandInput = DescribeDetectorModelRequest;
+export type DescribeDetectorModelCommandOutput = DescribeDetectorModelResponse;
+
+export class DescribeDetectorModelCommand extends $Command<
+  DescribeDetectorModelCommandInput,
+  DescribeDetectorModelCommandOutput,
+  IoTEventsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeDetectorModelCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: IoTEventsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeDetectorModelCommandInput,
+    DescribeDetectorModelCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeDetectorModelCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DescribeDetectorModelCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeDetectorModelCommandOutput> {
+    return deserializeAws_restJson1_1DescribeDetectorModelCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
