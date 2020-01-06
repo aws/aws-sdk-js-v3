dfe66f0958a6d1392b3d83a4058b606eaebdc6cb
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:06:21 -0800

    feat: add client-personalize-events (#643)

---
+++ clients/client-personalize-events/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2018-03-22",
+  signingName: "personalize",
+  regionInfoProvider: defaultRegionInfoProvider
+};
