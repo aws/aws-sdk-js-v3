972a44f98634e285f952e2a6593415a9f9708b57
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:07:17 -0800

    feat: add client-schemas (#643)

---
+++ clients/client-schemas/commands/DeleteSchemaVersionCommand.ts
@@ -0,0 +1,88 @@
+import { DeleteSchemaVersionRequest } from "../models/index";
+import {
+  deserializeAws_restJson1_1DeleteSchemaVersionCommand,
+  serializeAws_restJson1_1DeleteSchemaVersionCommand
+} from "../protocols/Aws_restJson1_1";
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  schemasClientResolvedConfig
+} from "../schemasClient";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type DeleteSchemaVersionCommandInput = DeleteSchemaVersionRequest;
+export type DeleteSchemaVersionCommandOutput = __MetadataBearer;
+
+export class DeleteSchemaVersionCommand extends $Command<
+  DeleteSchemaVersionCommandInput,
+  DeleteSchemaVersionCommandOutput,
+  schemasClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteSchemaVersionCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: schemasClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DeleteSchemaVersionCommandInput,
+    DeleteSchemaVersionCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteSchemaVersionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1DeleteSchemaVersionCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteSchemaVersionCommandOutput> {
+    return deserializeAws_restJson1_1DeleteSchemaVersionCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
