99dd5db0c73b1cb1a4597afcdb79add3a7004983
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:06:35 -0800

    feat: add client-pinpoint-email (#643)

---
+++ clients/client-pinpoint-email/commands/GetAccountCommand.ts
@@ -0,0 +1,81 @@
+import {
+  PinpointEmailClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../PinpointEmailClient";
+import { GetAccountRequest, GetAccountResponse } from "../models/index";
+import {
+  deserializeAws_restJson1_1GetAccountCommand,
+  serializeAws_restJson1_1GetAccountCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetAccountCommandInput = GetAccountRequest;
+export type GetAccountCommandOutput = GetAccountResponse;
+
+export class GetAccountCommand extends $Command<
+  GetAccountCommandInput,
+  GetAccountCommandOutput,
+  PinpointEmailClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetAccountCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: PinpointEmailClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetAccountCommandInput, GetAccountCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetAccountCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1GetAccountCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetAccountCommandOutput> {
+    return deserializeAws_restJson1_1GetAccountCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
