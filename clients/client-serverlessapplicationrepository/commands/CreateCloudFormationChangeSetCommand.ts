4f953c97017d98cc2e601a364b7377d5b31b35d1
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:07:26 -0800

    feat: add client-serverlessapplicationrepository (#643)

---
+++ clients/client-serverlessapplicationrepository/commands/CreateCloudFormationChangeSetCommand.ts
@@ -0,0 +1,93 @@
+import {
+  ServerlessApplicationRepositoryClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ServerlessApplicationRepositoryClient";
+import {
+  CreateCloudFormationChangeSetRequest,
+  CreateCloudFormationChangeSetResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1CreateCloudFormationChangeSetCommand,
+  serializeAws_restJson1_1CreateCloudFormationChangeSetCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateCloudFormationChangeSetCommandInput = CreateCloudFormationChangeSetRequest;
+export type CreateCloudFormationChangeSetCommandOutput = CreateCloudFormationChangeSetResponse;
+
+export class CreateCloudFormationChangeSetCommand extends $Command<
+  CreateCloudFormationChangeSetCommandInput,
+  CreateCloudFormationChangeSetCommandOutput,
+  ServerlessApplicationRepositoryClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateCloudFormationChangeSetCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ServerlessApplicationRepositoryClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    CreateCloudFormationChangeSetCommandInput,
+    CreateCloudFormationChangeSetCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateCloudFormationChangeSetCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1CreateCloudFormationChangeSetCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateCloudFormationChangeSetCommandOutput> {
+    return deserializeAws_restJson1_1CreateCloudFormationChangeSetCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-serverlessapplicationrepository/commands/ListApplicationVersionsCommand.ts
@@ -0,0 +1,93 @@
+import {
+  ServerlessApplicationRepositoryClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ServerlessApplicationRepositoryClient";
+import {
+  ListApplicationVersionsRequest,
+  ListApplicationVersionsResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1ListApplicationVersionsCommand,
+  serializeAws_restJson1_1ListApplicationVersionsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListApplicationVersionsCommandInput = ListApplicationVersionsRequest;
+export type ListApplicationVersionsCommandOutput = ListApplicationVersionsResponse;
+
+export class ListApplicationVersionsCommand extends $Command<
+  ListApplicationVersionsCommandInput,
+  ListApplicationVersionsCommandOutput,
+  ServerlessApplicationRepositoryClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListApplicationVersionsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ServerlessApplicationRepositoryClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListApplicationVersionsCommandInput,
+    ListApplicationVersionsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListApplicationVersionsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1ListApplicationVersionsCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListApplicationVersionsCommandOutput> {
+    return deserializeAws_restJson1_1ListApplicationVersionsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
