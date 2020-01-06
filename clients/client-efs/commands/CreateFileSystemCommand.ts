c28d5283b6b9654745078cc4ebdff329754157da
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:04:10 -0800

    feat: add client-efs (#643)

---
+++ clients/client-efs/commands/CreateFileSystemCommand.ts
@@ -0,0 +1,84 @@
+import {
+  EFSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../EFSClient";
+import {
+  CreateFileSystemRequest,
+  FileSystemDescription
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1CreateFileSystemCommand,
+  serializeAws_restJson1_1CreateFileSystemCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateFileSystemCommandInput = CreateFileSystemRequest;
+export type CreateFileSystemCommandOutput = FileSystemDescription;
+
+export class CreateFileSystemCommand extends $Command<
+  CreateFileSystemCommandInput,
+  CreateFileSystemCommandOutput,
+  EFSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateFileSystemCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: EFSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateFileSystemCommandInput, CreateFileSystemCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateFileSystemCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1CreateFileSystemCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateFileSystemCommandOutput> {
+    return deserializeAws_restJson1_1CreateFileSystemCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
