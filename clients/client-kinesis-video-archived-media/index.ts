f3995bbeecb5bd42dbd1873c5ef195a426ab2990
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 11:05:20 -0800

    feat: add client-kinesis-video-archived-media (#643)

---
+++ clients/client-kinesis-video-archived-media/index.ts
@@ -0,0 +1,6 @@
+export * from "./KinesisVideoArchivedMediaClient";
+export * from "./KinesisVideoArchivedMedia";
+export * from "./commands/ListFragmentsCommand";
+export * from "./commands/GetHLSStreamingSessionURLCommand";
+export * from "./commands/GetMediaForFragmentListCommand";
+export * from "./commands/GetDASHStreamingSessionURLCommand";
