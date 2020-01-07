fc0292b01767858bbd0b8729052f4424704d8c7e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:54:42 -0800

    feat: add client-pi (#656)

---
+++ clients/client-pi/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2018-02-27",
+  signingName: "pi",
+  regionInfoProvider: defaultRegionInfoProvider
+};
