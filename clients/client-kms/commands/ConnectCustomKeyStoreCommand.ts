b03ecf5e6a335009dc942e49cf4b1845cec92487
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:08 -0800

    feat: add client-kms (#656)

---
+++ clients/client-kms/commands/ConnectCustomKeyStoreCommand.ts
@@ -0,0 +1,87 @@
+import {
+  KMSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../KMSClient";
+import {
+  ConnectCustomKeyStoreRequest,
+  ConnectCustomKeyStoreResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1ConnectCustomKeyStoreCommand,
+  serializeAws_json1_1ConnectCustomKeyStoreCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ConnectCustomKeyStoreCommandInput = ConnectCustomKeyStoreRequest;
+export type ConnectCustomKeyStoreCommandOutput = ConnectCustomKeyStoreResponse;
+
+export class ConnectCustomKeyStoreCommand extends $Command<
+  ConnectCustomKeyStoreCommandInput,
+  ConnectCustomKeyStoreCommandOutput,
+  KMSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ConnectCustomKeyStoreCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: KMSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ConnectCustomKeyStoreCommandInput,
+    ConnectCustomKeyStoreCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ConnectCustomKeyStoreCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ConnectCustomKeyStoreCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ConnectCustomKeyStoreCommandOutput> {
+    return deserializeAws_json1_1ConnectCustomKeyStoreCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-kms/commands/EnableKeyCommand.ts
@@ -0,0 +1,82 @@
+import {
+  KMSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../KMSClient";
+import { EnableKeyRequest } from "../models/index";
+import {
+  deserializeAws_json1_1EnableKeyCommand,
+  serializeAws_json1_1EnableKeyCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type EnableKeyCommandInput = EnableKeyRequest;
+export type EnableKeyCommandOutput = __MetadataBearer;
+
+export class EnableKeyCommand extends $Command<
+  EnableKeyCommandInput,
+  EnableKeyCommandOutput,
+  KMSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: EnableKeyCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: KMSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<EnableKeyCommandInput, EnableKeyCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: EnableKeyCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1EnableKeyCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<EnableKeyCommandOutput> {
+    return deserializeAws_json1_1EnableKeyCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
