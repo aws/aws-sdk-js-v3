e4474b9f9a66064735a50e93c1d9cb94782faf77
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:51 -0800

    feat: add client-mediastore (#656)

---
+++ clients/client-mediastore/commands/StartAccessLoggingCommand.ts
@@ -0,0 +1,84 @@
+import {
+  MediaStoreClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MediaStoreClient";
+import {
+  StartAccessLoggingInput,
+  StartAccessLoggingOutput
+} from "../models/index";
+import {
+  deserializeAws_json1_1StartAccessLoggingCommand,
+  serializeAws_json1_1StartAccessLoggingCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type StartAccessLoggingCommandInput = StartAccessLoggingInput;
+export type StartAccessLoggingCommandOutput = StartAccessLoggingOutput;
+
+export class StartAccessLoggingCommand extends $Command<
+  StartAccessLoggingCommandInput,
+  StartAccessLoggingCommandOutput,
+  MediaStoreClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: StartAccessLoggingCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MediaStoreClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<StartAccessLoggingCommandInput, StartAccessLoggingCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: StartAccessLoggingCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1StartAccessLoggingCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<StartAccessLoggingCommandOutput> {
+    return deserializeAws_json1_1StartAccessLoggingCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-mediastore/commands/StopAccessLoggingCommand.ts
@@ -0,0 +1,84 @@
+import {
+  MediaStoreClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MediaStoreClient";
+import {
+  StopAccessLoggingInput,
+  StopAccessLoggingOutput
+} from "../models/index";
+import {
+  deserializeAws_json1_1StopAccessLoggingCommand,
+  serializeAws_json1_1StopAccessLoggingCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type StopAccessLoggingCommandInput = StopAccessLoggingInput;
+export type StopAccessLoggingCommandOutput = StopAccessLoggingOutput;
+
+export class StopAccessLoggingCommand extends $Command<
+  StopAccessLoggingCommandInput,
+  StopAccessLoggingCommandOutput,
+  MediaStoreClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: StopAccessLoggingCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MediaStoreClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<StopAccessLoggingCommandInput, StopAccessLoggingCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: StopAccessLoggingCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1StopAccessLoggingCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<StopAccessLoggingCommandOutput> {
+    return deserializeAws_json1_1StopAccessLoggingCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
