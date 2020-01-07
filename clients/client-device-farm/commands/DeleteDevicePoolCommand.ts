20646ae60b06eef7e3cec919056d5c35ad5d2c02
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:31 -0800

    feat: add client-device-farm (#656)

---
+++ clients/client-device-farm/commands/DeleteDevicePoolCommand.ts
@@ -0,0 +1,84 @@
+import {
+  DeviceFarmClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DeviceFarmClient";
+import {
+  DeleteDevicePoolRequest,
+  DeleteDevicePoolResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1DeleteDevicePoolCommand,
+  serializeAws_json1_1DeleteDevicePoolCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DeleteDevicePoolCommandInput = DeleteDevicePoolRequest;
+export type DeleteDevicePoolCommandOutput = DeleteDevicePoolResult;
+
+export class DeleteDevicePoolCommand extends $Command<
+  DeleteDevicePoolCommandInput,
+  DeleteDevicePoolCommandOutput,
+  DeviceFarmClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteDevicePoolCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DeviceFarmClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DeleteDevicePoolCommandInput, DeleteDevicePoolCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteDevicePoolCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteDevicePoolCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteDevicePoolCommandOutput> {
+    return deserializeAws_json1_1DeleteDevicePoolCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-device-farm/commands/ListOfferingPromotionsCommand.ts
@@ -0,0 +1,87 @@
+import {
+  DeviceFarmClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DeviceFarmClient";
+import {
+  ListOfferingPromotionsRequest,
+  ListOfferingPromotionsResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1ListOfferingPromotionsCommand,
+  serializeAws_json1_1ListOfferingPromotionsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListOfferingPromotionsCommandInput = ListOfferingPromotionsRequest;
+export type ListOfferingPromotionsCommandOutput = ListOfferingPromotionsResult;
+
+export class ListOfferingPromotionsCommand extends $Command<
+  ListOfferingPromotionsCommandInput,
+  ListOfferingPromotionsCommandOutput,
+  DeviceFarmClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListOfferingPromotionsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DeviceFarmClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListOfferingPromotionsCommandInput,
+    ListOfferingPromotionsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListOfferingPromotionsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListOfferingPromotionsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListOfferingPromotionsCommandOutput> {
+    return deserializeAws_json1_1ListOfferingPromotionsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
