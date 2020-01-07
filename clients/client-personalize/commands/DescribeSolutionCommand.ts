b65bf4669945bd1f99eb76fd39875d01a29f4e8b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:54:34 -0800

    feat: add client-personalize (#656)

---
+++ clients/client-personalize/commands/DescribeSolutionCommand.ts
@@ -0,0 +1,84 @@
+import {
+  PersonalizeClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../PersonalizeClient";
+import {
+  DescribeSolutionRequest,
+  DescribeSolutionResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1DescribeSolutionCommand,
+  serializeAws_json1_1DescribeSolutionCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeSolutionCommandInput = DescribeSolutionRequest;
+export type DescribeSolutionCommandOutput = DescribeSolutionResponse;
+
+export class DescribeSolutionCommand extends $Command<
+  DescribeSolutionCommandInput,
+  DescribeSolutionCommandOutput,
+  PersonalizeClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeSolutionCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: PersonalizeClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeSolutionCommandInput, DescribeSolutionCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeSolutionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeSolutionCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeSolutionCommandOutput> {
+    return deserializeAws_json1_1DescribeSolutionCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-personalize/commands/ListBatchInferenceJobsCommand.ts
@@ -0,0 +1,87 @@
+import {
+  PersonalizeClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../PersonalizeClient";
+import {
+  ListBatchInferenceJobsRequest,
+  ListBatchInferenceJobsResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1ListBatchInferenceJobsCommand,
+  serializeAws_json1_1ListBatchInferenceJobsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListBatchInferenceJobsCommandInput = ListBatchInferenceJobsRequest;
+export type ListBatchInferenceJobsCommandOutput = ListBatchInferenceJobsResponse;
+
+export class ListBatchInferenceJobsCommand extends $Command<
+  ListBatchInferenceJobsCommandInput,
+  ListBatchInferenceJobsCommandOutput,
+  PersonalizeClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListBatchInferenceJobsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: PersonalizeClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListBatchInferenceJobsCommandInput,
+    ListBatchInferenceJobsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListBatchInferenceJobsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListBatchInferenceJobsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListBatchInferenceJobsCommandOutput> {
+    return deserializeAws_json1_1ListBatchInferenceJobsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
