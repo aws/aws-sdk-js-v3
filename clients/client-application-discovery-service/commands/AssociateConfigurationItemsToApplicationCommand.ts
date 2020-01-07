d722c0c5b1d155f725174d2148c7620ceb4414d4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:47:56 -0800

    feat: add client-application-discovery-service (#656)

---
+++ clients/client-application-discovery-service/commands/AssociateConfigurationItemsToApplicationCommand.ts
@@ -0,0 +1,95 @@
+import {
+  ApplicationDiscoveryServiceClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ApplicationDiscoveryServiceClient";
+import {
+  AssociateConfigurationItemsToApplicationRequest,
+  AssociateConfigurationItemsToApplicationResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1AssociateConfigurationItemsToApplicationCommand,
+  serializeAws_json1_1AssociateConfigurationItemsToApplicationCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type AssociateConfigurationItemsToApplicationCommandInput = AssociateConfigurationItemsToApplicationRequest;
+export type AssociateConfigurationItemsToApplicationCommandOutput = AssociateConfigurationItemsToApplicationResponse;
+
+export class AssociateConfigurationItemsToApplicationCommand extends $Command<
+  AssociateConfigurationItemsToApplicationCommandInput,
+  AssociateConfigurationItemsToApplicationCommandOutput,
+  ApplicationDiscoveryServiceClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(
+    readonly input: AssociateConfigurationItemsToApplicationCommandInput
+  ) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ApplicationDiscoveryServiceClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    AssociateConfigurationItemsToApplicationCommandInput,
+    AssociateConfigurationItemsToApplicationCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AssociateConfigurationItemsToApplicationCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AssociateConfigurationItemsToApplicationCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AssociateConfigurationItemsToApplicationCommandOutput> {
+    return deserializeAws_json1_1AssociateConfigurationItemsToApplicationCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
