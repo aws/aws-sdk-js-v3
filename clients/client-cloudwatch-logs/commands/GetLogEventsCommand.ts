e4bf7143487e84dacb7b493a6d53a5fd1c2c842c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:42 -0800

    feat: add client-cloudwatch-logs (#656)

---
+++ clients/client-cloudwatch-logs/commands/GetLogEventsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  CloudWatchLogsClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudWatchLogsClient";
+import { GetLogEventsRequest, GetLogEventsResponse } from "../models/index";
+import {
+  deserializeAws_json1_1GetLogEventsCommand,
+  serializeAws_json1_1GetLogEventsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetLogEventsCommandInput = GetLogEventsRequest;
+export type GetLogEventsCommandOutput = GetLogEventsResponse;
+
+export class GetLogEventsCommand extends $Command<
+  GetLogEventsCommandInput,
+  GetLogEventsCommandOutput,
+  CloudWatchLogsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetLogEventsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudWatchLogsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetLogEventsCommandInput, GetLogEventsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetLogEventsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1GetLogEventsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetLogEventsCommandOutput> {
+    return deserializeAws_json1_1GetLogEventsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
