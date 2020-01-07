6507b0e044ed7d37976a6c564d7684d703f68d7a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:08 -0800

    feat: add client-kms (#656)

---
+++ clients/client-kms/commands/EncryptCommand.ts
@@ -0,0 +1,81 @@
+import {
+  KMSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../KMSClient";
+import { EncryptRequest, EncryptResponse } from "../models/index";
+import {
+  deserializeAws_json1_1EncryptCommand,
+  serializeAws_json1_1EncryptCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type EncryptCommandInput = EncryptRequest;
+export type EncryptCommandOutput = EncryptResponse;
+
+export class EncryptCommand extends $Command<
+  EncryptCommandInput,
+  EncryptCommandOutput,
+  KMSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: EncryptCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: KMSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<EncryptCommandInput, EncryptCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: EncryptCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1EncryptCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<EncryptCommandOutput> {
+    return deserializeAws_json1_1EncryptCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
