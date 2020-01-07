0f94d4f05a36158682e88824ae6816b8a46764b6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:49:53 -0800

    feat: add client-cost-and-usage-report-service (#656)

---
+++ clients/client-cost-and-usage-report-service/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2017-01-06",
+  signingName: "cur",
+  regionInfoProvider: defaultRegionInfoProvider
+};
