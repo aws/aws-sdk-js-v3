551657fd85b36f40e108071501fff479b9a28b44
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:49:13 -0800

    feat: add client-codestar (#656)

---
+++ clients/client-codestar/commands/DisassociateTeamMemberCommand.ts
@@ -0,0 +1,87 @@
+import {
+  CodeStarClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CodeStarClient";
+import {
+  DisassociateTeamMemberRequest,
+  DisassociateTeamMemberResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1DisassociateTeamMemberCommand,
+  serializeAws_json1_1DisassociateTeamMemberCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DisassociateTeamMemberCommandInput = DisassociateTeamMemberRequest;
+export type DisassociateTeamMemberCommandOutput = DisassociateTeamMemberResult;
+
+export class DisassociateTeamMemberCommand extends $Command<
+  DisassociateTeamMemberCommandInput,
+  DisassociateTeamMemberCommandOutput,
+  CodeStarClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DisassociateTeamMemberCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CodeStarClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DisassociateTeamMemberCommandInput,
+    DisassociateTeamMemberCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DisassociateTeamMemberCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DisassociateTeamMemberCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DisassociateTeamMemberCommandOutput> {
+    return deserializeAws_json1_1DisassociateTeamMemberCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
