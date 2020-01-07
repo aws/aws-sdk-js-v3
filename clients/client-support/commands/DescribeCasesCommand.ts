690fb2111acee5d881c4de9a9631ac15c603d27a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:56:17 -0800

    feat: add client-support (#656)

---
+++ clients/client-support/commands/DescribeCasesCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  SupportClientResolvedConfig
+} from "../SupportClient";
+import { DescribeCasesRequest, DescribeCasesResponse } from "../models/index";
+import {
+  deserializeAws_json1_1DescribeCasesCommand,
+  serializeAws_json1_1DescribeCasesCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeCasesCommandInput = DescribeCasesRequest;
+export type DescribeCasesCommandOutput = DescribeCasesResponse;
+
+export class DescribeCasesCommand extends $Command<
+  DescribeCasesCommandInput,
+  DescribeCasesCommandOutput,
+  SupportClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeCasesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SupportClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeCasesCommandInput, DescribeCasesCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeCasesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeCasesCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeCasesCommandOutput> {
+    return deserializeAws_json1_1DescribeCasesCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-support/commands/DescribeTrustedAdvisorCheckSummariesCommand.ts
@@ -0,0 +1,95 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  SupportClientResolvedConfig
+} from "../SupportClient";
+import {
+  DescribeTrustedAdvisorCheckSummariesRequest,
+  DescribeTrustedAdvisorCheckSummariesResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1DescribeTrustedAdvisorCheckSummariesCommand,
+  serializeAws_json1_1DescribeTrustedAdvisorCheckSummariesCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeTrustedAdvisorCheckSummariesCommandInput = DescribeTrustedAdvisorCheckSummariesRequest;
+export type DescribeTrustedAdvisorCheckSummariesCommandOutput = DescribeTrustedAdvisorCheckSummariesResponse;
+
+export class DescribeTrustedAdvisorCheckSummariesCommand extends $Command<
+  DescribeTrustedAdvisorCheckSummariesCommandInput,
+  DescribeTrustedAdvisorCheckSummariesCommandOutput,
+  SupportClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(
+    readonly input: DescribeTrustedAdvisorCheckSummariesCommandInput
+  ) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SupportClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeTrustedAdvisorCheckSummariesCommandInput,
+    DescribeTrustedAdvisorCheckSummariesCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeTrustedAdvisorCheckSummariesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeTrustedAdvisorCheckSummariesCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeTrustedAdvisorCheckSummariesCommandOutput> {
+    return deserializeAws_json1_1DescribeTrustedAdvisorCheckSummariesCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
