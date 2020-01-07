21a226e66b7d9d8396f034fbb58a5bf1d5dc7e28
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:47:39 -0800

    feat: add client-acm-pca (#656)

---
+++ clients/client-acm-pca/commands/TagCertificateAuthorityCommand.ts
@@ -0,0 +1,88 @@
+import {
+  ACMPCAClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ACMPCAClient";
+import { TagCertificateAuthorityRequest } from "../models/index";
+import {
+  deserializeAws_json1_1TagCertificateAuthorityCommand,
+  serializeAws_json1_1TagCertificateAuthorityCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type TagCertificateAuthorityCommandInput = TagCertificateAuthorityRequest;
+export type TagCertificateAuthorityCommandOutput = __MetadataBearer;
+
+export class TagCertificateAuthorityCommand extends $Command<
+  TagCertificateAuthorityCommandInput,
+  TagCertificateAuthorityCommandOutput,
+  ACMPCAClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: TagCertificateAuthorityCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ACMPCAClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    TagCertificateAuthorityCommandInput,
+    TagCertificateAuthorityCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: TagCertificateAuthorityCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1TagCertificateAuthorityCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<TagCertificateAuthorityCommandOutput> {
+    return deserializeAws_json1_1TagCertificateAuthorityCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-acm-pca/commands/UpdateCertificateAuthorityCommand.ts
@@ -0,0 +1,91 @@
+import {
+  ACMPCAClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ACMPCAClient";
+import { UpdateCertificateAuthorityRequest } from "../models/index";
+import {
+  deserializeAws_json1_1UpdateCertificateAuthorityCommand,
+  serializeAws_json1_1UpdateCertificateAuthorityCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type UpdateCertificateAuthorityCommandInput = UpdateCertificateAuthorityRequest;
+export type UpdateCertificateAuthorityCommandOutput = __MetadataBearer;
+
+export class UpdateCertificateAuthorityCommand extends $Command<
+  UpdateCertificateAuthorityCommandInput,
+  UpdateCertificateAuthorityCommandOutput,
+  ACMPCAClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateCertificateAuthorityCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ACMPCAClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateCertificateAuthorityCommandInput,
+    UpdateCertificateAuthorityCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateCertificateAuthorityCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1UpdateCertificateAuthorityCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateCertificateAuthorityCommandOutput> {
+    return deserializeAws_json1_1UpdateCertificateAuthorityCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
