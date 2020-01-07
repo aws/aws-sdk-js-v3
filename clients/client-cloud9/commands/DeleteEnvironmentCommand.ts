337ca824381b10b666a47c8fc3c5c50011a61caa
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:22 -0800

    feat: add client-cloud9 (#656)

---
+++ clients/client-cloud9/commands/DeleteEnvironmentCommand.ts
@@ -0,0 +1,84 @@
+import {
+  Cloud9ClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../Cloud9Client";
+import {
+  DeleteEnvironmentRequest,
+  DeleteEnvironmentResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1DeleteEnvironmentCommand,
+  serializeAws_json1_1DeleteEnvironmentCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DeleteEnvironmentCommandInput = DeleteEnvironmentRequest;
+export type DeleteEnvironmentCommandOutput = DeleteEnvironmentResult;
+
+export class DeleteEnvironmentCommand extends $Command<
+  DeleteEnvironmentCommandInput,
+  DeleteEnvironmentCommandOutput,
+  Cloud9ClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteEnvironmentCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: Cloud9ClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DeleteEnvironmentCommandInput, DeleteEnvironmentCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteEnvironmentCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteEnvironmentCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteEnvironmentCommandOutput> {
+    return deserializeAws_json1_1DeleteEnvironmentCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
