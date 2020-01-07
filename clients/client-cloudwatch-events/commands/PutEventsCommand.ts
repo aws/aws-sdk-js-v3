bf11104d35cf7995d349314061d2d4311a3ed4d3
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:37 -0800

    feat: add client-cloudwatch-events (#656)

---
+++ clients/client-cloudwatch-events/commands/PutEventsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  CloudWatchEventsClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudWatchEventsClient";
+import { PutEventsRequest, PutEventsResponse } from "../models/index";
+import {
+  deserializeAws_json1_1PutEventsCommand,
+  serializeAws_json1_1PutEventsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type PutEventsCommandInput = PutEventsRequest;
+export type PutEventsCommandOutput = PutEventsResponse;
+
+export class PutEventsCommand extends $Command<
+  PutEventsCommandInput,
+  PutEventsCommandOutput,
+  CloudWatchEventsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: PutEventsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudWatchEventsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<PutEventsCommandInput, PutEventsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: PutEventsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1PutEventsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<PutEventsCommandOutput> {
+    return deserializeAws_json1_1PutEventsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
