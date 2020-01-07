6b1f34e3b093bc9c7ec505cffcda01d753f067b9
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:06 -0800

    feat: add client-data-pipeline (#656)

---
+++ clients/client-data-pipeline/commands/DescribeObjectsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  DataPipelineClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DataPipelineClient";
+import { DescribeObjectsInput, DescribeObjectsOutput } from "../models/index";
+import {
+  deserializeAws_json1_1DescribeObjectsCommand,
+  serializeAws_json1_1DescribeObjectsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeObjectsCommandInput = DescribeObjectsInput;
+export type DescribeObjectsCommandOutput = DescribeObjectsOutput;
+
+export class DescribeObjectsCommand extends $Command<
+  DescribeObjectsCommandInput,
+  DescribeObjectsCommandOutput,
+  DataPipelineClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeObjectsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DataPipelineClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeObjectsCommandInput, DescribeObjectsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeObjectsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeObjectsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeObjectsCommandOutput> {
+    return deserializeAws_json1_1DescribeObjectsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
