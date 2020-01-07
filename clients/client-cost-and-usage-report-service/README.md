a8ee5786dd33d706f967807c4ba4999bc0f86298
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:49:53 -0800

    feat: add client-cost-and-usage-report-service (#656)

---
+++ clients/client-cost-and-usage-report-service/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-cost-and-usage-report-service
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-cost-and-usage-report-service/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-cost-and-usage-report-service)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-cost-and-usage-report-service.svg)](https://www.npmjs.com/package/@aws-sdk/client-cost-and-usage-report-service)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-cost-and-usage-report-service/commands/ModifyReportDefinitionCommand.ts
@@ -0,0 +1,87 @@
+import {
+  CostandUsageReportServiceClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CostandUsageReportServiceClient";
+import {
+  ModifyReportDefinitionRequest,
+  ModifyReportDefinitionResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1ModifyReportDefinitionCommand,
+  serializeAws_json1_1ModifyReportDefinitionCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ModifyReportDefinitionCommandInput = ModifyReportDefinitionRequest;
+export type ModifyReportDefinitionCommandOutput = ModifyReportDefinitionResponse;
+
+export class ModifyReportDefinitionCommand extends $Command<
+  ModifyReportDefinitionCommandInput,
+  ModifyReportDefinitionCommandOutput,
+  CostandUsageReportServiceClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ModifyReportDefinitionCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CostandUsageReportServiceClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ModifyReportDefinitionCommandInput,
+    ModifyReportDefinitionCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ModifyReportDefinitionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ModifyReportDefinitionCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ModifyReportDefinitionCommandOutput> {
+    return deserializeAws_json1_1ModifyReportDefinitionCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
