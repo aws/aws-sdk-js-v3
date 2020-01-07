720adbea12e7b1cc9e151379e8f09c9fdfee8d79
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:56:34 -0800

    feat: add client-waf (#656)

---
+++ clients/client-waf/commands/GetChangeTokenCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  WAFClientResolvedConfig
+} from "../WAFClient";
+import { GetChangeTokenRequest, GetChangeTokenResponse } from "../models/index";
+import {
+  deserializeAws_json1_1GetChangeTokenCommand,
+  serializeAws_json1_1GetChangeTokenCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetChangeTokenCommandInput = GetChangeTokenRequest;
+export type GetChangeTokenCommandOutput = GetChangeTokenResponse;
+
+export class GetChangeTokenCommand extends $Command<
+  GetChangeTokenCommandInput,
+  GetChangeTokenCommandOutput,
+  WAFClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetChangeTokenCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: WAFClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetChangeTokenCommandInput, GetChangeTokenCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetChangeTokenCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1GetChangeTokenCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetChangeTokenCommandOutput> {
+    return deserializeAws_json1_1GetChangeTokenCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-waf/commands/UpdateGeoMatchSetCommand.ts
@@ -0,0 +1,84 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  WAFClientResolvedConfig
+} from "../WAFClient";
+import {
+  UpdateGeoMatchSetRequest,
+  UpdateGeoMatchSetResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1UpdateGeoMatchSetCommand,
+  serializeAws_json1_1UpdateGeoMatchSetCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateGeoMatchSetCommandInput = UpdateGeoMatchSetRequest;
+export type UpdateGeoMatchSetCommandOutput = UpdateGeoMatchSetResponse;
+
+export class UpdateGeoMatchSetCommand extends $Command<
+  UpdateGeoMatchSetCommandInput,
+  UpdateGeoMatchSetCommandOutput,
+  WAFClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateGeoMatchSetCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: WAFClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<UpdateGeoMatchSetCommandInput, UpdateGeoMatchSetCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateGeoMatchSetCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1UpdateGeoMatchSetCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateGeoMatchSetCommandOutput> {
+    return deserializeAws_json1_1UpdateGeoMatchSetCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
