e23c9202eec11e47864987a72054e3d32097ec27
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:54:25 -0800

    feat: add client-organizations (#656)

---
+++ clients/client-organizations/commands/CreateOrganizationalUnitCommand.ts
@@ -0,0 +1,90 @@
+import {
+  OrganizationsClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../OrganizationsClient";
+import {
+  CreateOrganizationalUnitRequest,
+  CreateOrganizationalUnitResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1CreateOrganizationalUnitCommand,
+  serializeAws_json1_1CreateOrganizationalUnitCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateOrganizationalUnitCommandInput = CreateOrganizationalUnitRequest;
+export type CreateOrganizationalUnitCommandOutput = CreateOrganizationalUnitResponse;
+
+export class CreateOrganizationalUnitCommand extends $Command<
+  CreateOrganizationalUnitCommandInput,
+  CreateOrganizationalUnitCommandOutput,
+  OrganizationsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateOrganizationalUnitCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: OrganizationsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    CreateOrganizationalUnitCommandInput,
+    CreateOrganizationalUnitCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateOrganizationalUnitCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateOrganizationalUnitCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateOrganizationalUnitCommandOutput> {
+    return deserializeAws_json1_1CreateOrganizationalUnitCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-organizations/commands/ListHandshakesForAccountCommand.ts
@@ -0,0 +1,90 @@
+import {
+  OrganizationsClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../OrganizationsClient";
+import {
+  ListHandshakesForAccountRequest,
+  ListHandshakesForAccountResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1ListHandshakesForAccountCommand,
+  serializeAws_json1_1ListHandshakesForAccountCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListHandshakesForAccountCommandInput = ListHandshakesForAccountRequest;
+export type ListHandshakesForAccountCommandOutput = ListHandshakesForAccountResponse;
+
+export class ListHandshakesForAccountCommand extends $Command<
+  ListHandshakesForAccountCommandInput,
+  ListHandshakesForAccountCommandOutput,
+  OrganizationsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListHandshakesForAccountCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: OrganizationsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListHandshakesForAccountCommandInput,
+    ListHandshakesForAccountCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListHandshakesForAccountCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListHandshakesForAccountCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListHandshakesForAccountCommandOutput> {
+    return deserializeAws_json1_1ListHandshakesForAccountCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
