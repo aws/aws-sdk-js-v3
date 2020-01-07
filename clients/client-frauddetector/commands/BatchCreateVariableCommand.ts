47cbaf1fff33f7ff10e10c03fc0c599c55d90084
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:51:51 -0800

    feat: add client-frauddetector (#656)

---
+++ clients/client-frauddetector/commands/BatchCreateVariableCommand.ts
@@ -0,0 +1,87 @@
+import {
+  FraudDetectorClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../FraudDetectorClient";
+import {
+  BatchCreateVariableRequest,
+  BatchCreateVariableResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1BatchCreateVariableCommand,
+  serializeAws_json1_1BatchCreateVariableCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type BatchCreateVariableCommandInput = BatchCreateVariableRequest;
+export type BatchCreateVariableCommandOutput = BatchCreateVariableResult;
+
+export class BatchCreateVariableCommand extends $Command<
+  BatchCreateVariableCommandInput,
+  BatchCreateVariableCommandOutput,
+  FraudDetectorClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: BatchCreateVariableCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: FraudDetectorClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    BatchCreateVariableCommandInput,
+    BatchCreateVariableCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: BatchCreateVariableCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1BatchCreateVariableCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<BatchCreateVariableCommandOutput> {
+    return deserializeAws_json1_1BatchCreateVariableCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
