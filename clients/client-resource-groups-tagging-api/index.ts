2c5965a1ca9e6094a554c11c8644768d272dea04
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:55:00 -0800

    feat: add client-resource-groups-tagging-api (#656)

---
+++ clients/client-resource-groups-tagging-api/index.ts
@@ -0,0 +1,7 @@
+export * from "./ResourceGroupsTaggingAPIClient";
+export * from "./ResourceGroupsTaggingAPI";
+export * from "./commands/GetResourcesCommand";
+export * from "./commands/GetTagKeysCommand";
+export * from "./commands/GetTagValuesCommand";
+export * from "./commands/TagResourcesCommand";
+export * from "./commands/UntagResourcesCommand";
