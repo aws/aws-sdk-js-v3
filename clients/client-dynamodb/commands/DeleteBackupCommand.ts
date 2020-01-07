db0c22c3f170a4b7a9fe7d651cda9520554ea900
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 17:13:43 -0800

    feat: add client-dynamodb (#658)

---
+++ clients/client-dynamodb/commands/DeleteBackupCommand.ts
@@ -0,0 +1,81 @@
+import {
+  DynamoDBClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DynamoDBClient";
+import { DeleteBackupInput, DeleteBackupOutput } from "../models/index";
+import {
+  deserializeAws_json1_0DeleteBackupCommand,
+  serializeAws_json1_0DeleteBackupCommand
+} from "../protocols/Aws_json1_0";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DeleteBackupCommandInput = DeleteBackupInput;
+export type DeleteBackupCommandOutput = DeleteBackupOutput;
+
+export class DeleteBackupCommand extends $Command<
+  DeleteBackupCommandInput,
+  DeleteBackupCommandOutput,
+  DynamoDBClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteBackupCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DynamoDBClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DeleteBackupCommandInput, DeleteBackupCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteBackupCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_0DeleteBackupCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteBackupCommandOutput> {
+    return deserializeAws_json1_0DeleteBackupCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-dynamodb/commands/TransactWriteItemsCommand.ts
@@ -0,0 +1,84 @@
+import {
+  DynamoDBClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DynamoDBClient";
+import {
+  TransactWriteItemsInput,
+  TransactWriteItemsOutput
+} from "../models/index";
+import {
+  deserializeAws_json1_0TransactWriteItemsCommand,
+  serializeAws_json1_0TransactWriteItemsCommand
+} from "../protocols/Aws_json1_0";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type TransactWriteItemsCommandInput = TransactWriteItemsInput;
+export type TransactWriteItemsCommandOutput = TransactWriteItemsOutput;
+
+export class TransactWriteItemsCommand extends $Command<
+  TransactWriteItemsCommandInput,
+  TransactWriteItemsCommandOutput,
+  DynamoDBClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: TransactWriteItemsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DynamoDBClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<TransactWriteItemsCommandInput, TransactWriteItemsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: TransactWriteItemsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_0TransactWriteItemsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<TransactWriteItemsCommandOutput> {
+    return deserializeAws_json1_0TransactWriteItemsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
