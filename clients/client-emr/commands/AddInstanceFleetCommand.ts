1515efd98027b791fd351c10939ca4e1b1068fce
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:51:11 -0800

    feat: add client-emr (#656)

---
+++ clients/client-emr/commands/AddInstanceFleetCommand.ts
@@ -0,0 +1,81 @@
+import {
+  EMRClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../EMRClient";
+import { AddInstanceFleetInput, AddInstanceFleetOutput } from "../models/index";
+import {
+  deserializeAws_json1_1AddInstanceFleetCommand,
+  serializeAws_json1_1AddInstanceFleetCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type AddInstanceFleetCommandInput = AddInstanceFleetInput;
+export type AddInstanceFleetCommandOutput = AddInstanceFleetOutput;
+
+export class AddInstanceFleetCommand extends $Command<
+  AddInstanceFleetCommandInput,
+  AddInstanceFleetCommandOutput,
+  EMRClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: AddInstanceFleetCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: EMRClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<AddInstanceFleetCommandInput, AddInstanceFleetCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AddInstanceFleetCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AddInstanceFleetCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AddInstanceFleetCommandOutput> {
+    return deserializeAws_json1_1AddInstanceFleetCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-emr/commands/SetVisibleToAllUsersCommand.ts
@@ -0,0 +1,85 @@
+import {
+  EMRClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../EMRClient";
+import { SetVisibleToAllUsersInput } from "../models/index";
+import {
+  deserializeAws_json1_1SetVisibleToAllUsersCommand,
+  serializeAws_json1_1SetVisibleToAllUsersCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type SetVisibleToAllUsersCommandInput = SetVisibleToAllUsersInput;
+export type SetVisibleToAllUsersCommandOutput = __MetadataBearer;
+
+export class SetVisibleToAllUsersCommand extends $Command<
+  SetVisibleToAllUsersCommandInput,
+  SetVisibleToAllUsersCommandOutput,
+  EMRClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: SetVisibleToAllUsersCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: EMRClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    SetVisibleToAllUsersCommandInput,
+    SetVisibleToAllUsersCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: SetVisibleToAllUsersCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1SetVisibleToAllUsersCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<SetVisibleToAllUsersCommandOutput> {
+    return deserializeAws_json1_1SetVisibleToAllUsersCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
