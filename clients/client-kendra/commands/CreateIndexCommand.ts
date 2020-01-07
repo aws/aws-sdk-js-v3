fefab6a224a1323344f228bd2235fdaca04e0513
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:52:48 -0800

    feat: add client-kendra (#656)

---
+++ clients/client-kendra/commands/CreateIndexCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  kendraClientResolvedConfig
+} from "../kendraClient";
+import { CreateIndexRequest, CreateIndexResponse } from "../models/index";
+import {
+  deserializeAws_json1_1CreateIndexCommand,
+  serializeAws_json1_1CreateIndexCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateIndexCommandInput = CreateIndexRequest;
+export type CreateIndexCommandOutput = CreateIndexResponse;
+
+export class CreateIndexCommand extends $Command<
+  CreateIndexCommandInput,
+  CreateIndexCommandOutput,
+  kendraClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateIndexCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: kendraClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateIndexCommandInput, CreateIndexCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateIndexCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateIndexCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateIndexCommandOutput> {
+    return deserializeAws_json1_1CreateIndexCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-kendra/commands/DescribeIndexCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  kendraClientResolvedConfig
+} from "../kendraClient";
+import { DescribeIndexRequest, DescribeIndexResponse } from "../models/index";
+import {
+  deserializeAws_json1_1DescribeIndexCommand,
+  serializeAws_json1_1DescribeIndexCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeIndexCommandInput = DescribeIndexRequest;
+export type DescribeIndexCommandOutput = DescribeIndexResponse;
+
+export class DescribeIndexCommand extends $Command<
+  DescribeIndexCommandInput,
+  DescribeIndexCommandOutput,
+  kendraClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeIndexCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: kendraClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeIndexCommandInput, DescribeIndexCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeIndexCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeIndexCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeIndexCommandOutput> {
+    return deserializeAws_json1_1DescribeIndexCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
