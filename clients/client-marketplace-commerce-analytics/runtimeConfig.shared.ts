24f44d581c77db40a0134dc65285f17d4ab00416
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:42 -0800

    feat: add client-marketplace-commerce-analytics (#656)

---
+++ clients/client-marketplace-commerce-analytics/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2015-07-01",
+  signingName: "marketplacecommerceanalytics",
+  regionInfoProvider: defaultRegionInfoProvider
+};
