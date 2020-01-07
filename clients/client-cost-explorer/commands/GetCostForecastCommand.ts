5a11e4c39b021de1f6d48a4cca9ce6195ac25e0d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:49:58 -0800

    feat: add client-cost-explorer (#656)

---
+++ clients/client-cost-explorer/commands/GetCostForecastCommand.ts
@@ -0,0 +1,84 @@
+import {
+  CostExplorerClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CostExplorerClient";
+import {
+  GetCostForecastRequest,
+  GetCostForecastResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1GetCostForecastCommand,
+  serializeAws_json1_1GetCostForecastCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetCostForecastCommandInput = GetCostForecastRequest;
+export type GetCostForecastCommandOutput = GetCostForecastResponse;
+
+export class GetCostForecastCommand extends $Command<
+  GetCostForecastCommandInput,
+  GetCostForecastCommandOutput,
+  CostExplorerClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetCostForecastCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CostExplorerClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetCostForecastCommandInput, GetCostForecastCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetCostForecastCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1GetCostForecastCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetCostForecastCommandOutput> {
+    return deserializeAws_json1_1GetCostForecastCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
