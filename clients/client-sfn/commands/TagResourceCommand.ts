2bd8fcec1223c8b36e8a6a338859453ffedb39d2
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 17:13:55 -0800

    feat: add client-sfn (#658)

---
+++ clients/client-sfn/commands/TagResourceCommand.ts
@@ -0,0 +1,81 @@
+import {
+  SFNClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SFNClient";
+import { TagResourceInput, TagResourceOutput } from "../models/index";
+import {
+  deserializeAws_json1_0TagResourceCommand,
+  serializeAws_json1_0TagResourceCommand
+} from "../protocols/Aws_json1_0";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type TagResourceCommandInput = TagResourceInput;
+export type TagResourceCommandOutput = TagResourceOutput;
+
+export class TagResourceCommand extends $Command<
+  TagResourceCommandInput,
+  TagResourceCommandOutput,
+  SFNClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: TagResourceCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SFNClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<TagResourceCommandInput, TagResourceCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: TagResourceCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_0TagResourceCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<TagResourceCommandOutput> {
+    return deserializeAws_json1_0TagResourceCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
