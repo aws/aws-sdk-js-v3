e25ff28c2b6826cc844c4ca386330b11269251b6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:56:17 -0800

    feat: add client-support (#656)

---
+++ clients/client-support/commands/AddCommunicationToCaseCommand.ts
@@ -0,0 +1,87 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  SupportClientResolvedConfig
+} from "../SupportClient";
+import {
+  AddCommunicationToCaseRequest,
+  AddCommunicationToCaseResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1AddCommunicationToCaseCommand,
+  serializeAws_json1_1AddCommunicationToCaseCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type AddCommunicationToCaseCommandInput = AddCommunicationToCaseRequest;
+export type AddCommunicationToCaseCommandOutput = AddCommunicationToCaseResponse;
+
+export class AddCommunicationToCaseCommand extends $Command<
+  AddCommunicationToCaseCommandInput,
+  AddCommunicationToCaseCommandOutput,
+  SupportClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: AddCommunicationToCaseCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SupportClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    AddCommunicationToCaseCommandInput,
+    AddCommunicationToCaseCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AddCommunicationToCaseCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AddCommunicationToCaseCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AddCommunicationToCaseCommandOutput> {
+    return deserializeAws_json1_1AddCommunicationToCaseCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-support/commands/ResolveCaseCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  SupportClientResolvedConfig
+} from "../SupportClient";
+import { ResolveCaseRequest, ResolveCaseResponse } from "../models/index";
+import {
+  deserializeAws_json1_1ResolveCaseCommand,
+  serializeAws_json1_1ResolveCaseCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ResolveCaseCommandInput = ResolveCaseRequest;
+export type ResolveCaseCommandOutput = ResolveCaseResponse;
+
+export class ResolveCaseCommand extends $Command<
+  ResolveCaseCommandInput,
+  ResolveCaseCommandOutput,
+  SupportClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ResolveCaseCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SupportClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ResolveCaseCommandInput, ResolveCaseCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ResolveCaseCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ResolveCaseCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ResolveCaseCommandOutput> {
+    return deserializeAws_json1_1ResolveCaseCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
