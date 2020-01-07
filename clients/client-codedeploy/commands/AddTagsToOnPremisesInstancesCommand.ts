ff0a782a56761d8b1c52650b96ffcf26270f91f7
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:59 -0800

    feat: add client-codedeploy (#656)

---
+++ clients/client-codedeploy/commands/AddTagsToOnPremisesInstancesCommand.ts
@@ -0,0 +1,91 @@
+import {
+  CodeDeployClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CodeDeployClient";
+import { AddTagsToOnPremisesInstancesInput } from "../models/index";
+import {
+  deserializeAws_json1_1AddTagsToOnPremisesInstancesCommand,
+  serializeAws_json1_1AddTagsToOnPremisesInstancesCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type AddTagsToOnPremisesInstancesCommandInput = AddTagsToOnPremisesInstancesInput;
+export type AddTagsToOnPremisesInstancesCommandOutput = __MetadataBearer;
+
+export class AddTagsToOnPremisesInstancesCommand extends $Command<
+  AddTagsToOnPremisesInstancesCommandInput,
+  AddTagsToOnPremisesInstancesCommandOutput,
+  CodeDeployClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: AddTagsToOnPremisesInstancesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CodeDeployClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    AddTagsToOnPremisesInstancesCommandInput,
+    AddTagsToOnPremisesInstancesCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AddTagsToOnPremisesInstancesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AddTagsToOnPremisesInstancesCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AddTagsToOnPremisesInstancesCommandOutput> {
+    return deserializeAws_json1_1AddTagsToOnPremisesInstancesCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
