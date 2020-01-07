cc8b716eee0da5596f50cc0afd66bbc35abda75e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:25 -0800

    feat: add client-dax (#656)

---
+++ clients/client-dax/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2017-04-19",
+  signingName: "dax",
+  regionInfoProvider: defaultRegionInfoProvider
+};
