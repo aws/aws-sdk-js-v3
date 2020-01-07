c1e03be40f6829f98463030b70d8861c7063e831
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:57 -0800

    feat: add client-ecr (#656)

---
+++ clients/client-ecr/commands/GetDownloadUrlForLayerCommand.ts
@@ -0,0 +1,87 @@
+import {
+  ECRClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ECRClient";
+import {
+  GetDownloadUrlForLayerRequest,
+  GetDownloadUrlForLayerResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1GetDownloadUrlForLayerCommand,
+  serializeAws_json1_1GetDownloadUrlForLayerCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetDownloadUrlForLayerCommandInput = GetDownloadUrlForLayerRequest;
+export type GetDownloadUrlForLayerCommandOutput = GetDownloadUrlForLayerResponse;
+
+export class GetDownloadUrlForLayerCommand extends $Command<
+  GetDownloadUrlForLayerCommandInput,
+  GetDownloadUrlForLayerCommandOutput,
+  ECRClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetDownloadUrlForLayerCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ECRClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    GetDownloadUrlForLayerCommandInput,
+    GetDownloadUrlForLayerCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetDownloadUrlForLayerCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1GetDownloadUrlForLayerCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetDownloadUrlForLayerCommandOutput> {
+    return deserializeAws_json1_1GetDownloadUrlForLayerCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-ecr/commands/InitiateLayerUploadCommand.ts
@@ -0,0 +1,87 @@
+import {
+  ECRClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ECRClient";
+import {
+  InitiateLayerUploadRequest,
+  InitiateLayerUploadResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1InitiateLayerUploadCommand,
+  serializeAws_json1_1InitiateLayerUploadCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type InitiateLayerUploadCommandInput = InitiateLayerUploadRequest;
+export type InitiateLayerUploadCommandOutput = InitiateLayerUploadResponse;
+
+export class InitiateLayerUploadCommand extends $Command<
+  InitiateLayerUploadCommandInput,
+  InitiateLayerUploadCommandOutput,
+  ECRClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: InitiateLayerUploadCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ECRClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    InitiateLayerUploadCommandInput,
+    InitiateLayerUploadCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: InitiateLayerUploadCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1InitiateLayerUploadCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<InitiateLayerUploadCommandOutput> {
+    return deserializeAws_json1_1InitiateLayerUploadCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
