d81e5c7a88122b7bea6265cc4859deee56319dad
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:56:27 -0800

    feat: add client-transfer (#656)

---
+++ clients/client-transfer/commands/DeleteUserCommand.ts
@@ -0,0 +1,82 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  TransferClientResolvedConfig
+} from "../TransferClient";
+import { DeleteUserRequest } from "../models/index";
+import {
+  deserializeAws_json1_1DeleteUserCommand,
+  serializeAws_json1_1DeleteUserCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type DeleteUserCommandInput = DeleteUserRequest;
+export type DeleteUserCommandOutput = __MetadataBearer;
+
+export class DeleteUserCommand extends $Command<
+  DeleteUserCommandInput,
+  DeleteUserCommandOutput,
+  TransferClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteUserCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: TransferClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DeleteUserCommandInput, DeleteUserCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteUserCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteUserCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteUserCommandOutput> {
+    return deserializeAws_json1_1DeleteUserCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-transfer/commands/DescribeServerCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  TransferClientResolvedConfig
+} from "../TransferClient";
+import { DescribeServerRequest, DescribeServerResponse } from "../models/index";
+import {
+  deserializeAws_json1_1DescribeServerCommand,
+  serializeAws_json1_1DescribeServerCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeServerCommandInput = DescribeServerRequest;
+export type DescribeServerCommandOutput = DescribeServerResponse;
+
+export class DescribeServerCommand extends $Command<
+  DescribeServerCommandInput,
+  DescribeServerCommandOutput,
+  TransferClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeServerCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: TransferClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeServerCommandInput, DescribeServerCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeServerCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeServerCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeServerCommandOutput> {
+    return deserializeAws_json1_1DescribeServerCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
