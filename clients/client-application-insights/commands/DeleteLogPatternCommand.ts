414df92e47712fdd3d7d0e0161d7e87fcbb9c27c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:01 -0800

    feat: add client-application-insights (#656)

---
+++ clients/client-application-insights/commands/DeleteLogPatternCommand.ts
@@ -0,0 +1,84 @@
+import {
+  ApplicationInsightsClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ApplicationInsightsClient";
+import {
+  DeleteLogPatternRequest,
+  DeleteLogPatternResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1DeleteLogPatternCommand,
+  serializeAws_json1_1DeleteLogPatternCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DeleteLogPatternCommandInput = DeleteLogPatternRequest;
+export type DeleteLogPatternCommandOutput = DeleteLogPatternResponse;
+
+export class DeleteLogPatternCommand extends $Command<
+  DeleteLogPatternCommandInput,
+  DeleteLogPatternCommandOutput,
+  ApplicationInsightsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteLogPatternCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ApplicationInsightsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DeleteLogPatternCommandInput, DeleteLogPatternCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteLogPatternCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteLogPatternCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteLogPatternCommandOutput> {
+    return deserializeAws_json1_1DeleteLogPatternCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
