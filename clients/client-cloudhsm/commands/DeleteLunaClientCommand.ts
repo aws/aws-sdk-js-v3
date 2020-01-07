98f1266026a45d2ef64b2ac79fd949f07216f15c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:25 -0800

    feat: add client-cloudhsm (#656)

---
+++ clients/client-cloudhsm/commands/DeleteLunaClientCommand.ts
@@ -0,0 +1,84 @@
+import {
+  CloudHSMClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudHSMClient";
+import {
+  DeleteLunaClientRequest,
+  DeleteLunaClientResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1DeleteLunaClientCommand,
+  serializeAws_json1_1DeleteLunaClientCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DeleteLunaClientCommandInput = DeleteLunaClientRequest;
+export type DeleteLunaClientCommandOutput = DeleteLunaClientResponse;
+
+export class DeleteLunaClientCommand extends $Command<
+  DeleteLunaClientCommandInput,
+  DeleteLunaClientCommandOutput,
+  CloudHSMClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteLunaClientCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudHSMClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DeleteLunaClientCommandInput, DeleteLunaClientCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteLunaClientCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteLunaClientCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteLunaClientCommandOutput> {
+    return deserializeAws_json1_1DeleteLunaClientCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
