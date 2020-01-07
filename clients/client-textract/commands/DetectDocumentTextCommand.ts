0f7873e15fe129d146805d231fbfff20dab70d00
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:56:21 -0800

    feat: add client-textract (#656)

---
+++ clients/client-textract/commands/DetectDocumentTextCommand.ts
@@ -0,0 +1,84 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  TextractClientResolvedConfig
+} from "../TextractClient";
+import {
+  DetectDocumentTextRequest,
+  DetectDocumentTextResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1DetectDocumentTextCommand,
+  serializeAws_json1_1DetectDocumentTextCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DetectDocumentTextCommandInput = DetectDocumentTextRequest;
+export type DetectDocumentTextCommandOutput = DetectDocumentTextResponse;
+
+export class DetectDocumentTextCommand extends $Command<
+  DetectDocumentTextCommandInput,
+  DetectDocumentTextCommandOutput,
+  TextractClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DetectDocumentTextCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: TextractClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DetectDocumentTextCommandInput, DetectDocumentTextCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DetectDocumentTextCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DetectDocumentTextCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DetectDocumentTextCommandOutput> {
+    return deserializeAws_json1_1DetectDocumentTextCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-textract/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2018-06-27",
+  signingName: "textract",
+  regionInfoProvider: defaultRegionInfoProvider
+};
