ac5873764a86d0332bb46074e1367167797b355d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:29 -0800

    feat: add client-cloudhsm-v2 (#656)

---
+++ clients/client-cloudhsm-v2/commands/CopyBackupToRegionCommand.ts
@@ -0,0 +1,84 @@
+import {
+  CloudHSMV2ClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudHSMV2Client";
+import {
+  CopyBackupToRegionRequest,
+  CopyBackupToRegionResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1CopyBackupToRegionCommand,
+  serializeAws_json1_1CopyBackupToRegionCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CopyBackupToRegionCommandInput = CopyBackupToRegionRequest;
+export type CopyBackupToRegionCommandOutput = CopyBackupToRegionResponse;
+
+export class CopyBackupToRegionCommand extends $Command<
+  CopyBackupToRegionCommandInput,
+  CopyBackupToRegionCommandOutput,
+  CloudHSMV2ClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CopyBackupToRegionCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudHSMV2ClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CopyBackupToRegionCommandInput, CopyBackupToRegionCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CopyBackupToRegionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CopyBackupToRegionCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CopyBackupToRegionCommandOutput> {
+    return deserializeAws_json1_1CopyBackupToRegionCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
