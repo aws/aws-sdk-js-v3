2b53024fb230543bec8cdb476347428cc3ecc2fa
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:31 -0800

    feat: add client-machine-learning (#656)

---
+++ clients/client-machine-learning/commands/DeleteBatchPredictionCommand.ts
@@ -0,0 +1,87 @@
+import {
+  MachineLearningClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MachineLearningClient";
+import {
+  DeleteBatchPredictionInput,
+  DeleteBatchPredictionOutput
+} from "../models/index";
+import {
+  deserializeAws_json1_1DeleteBatchPredictionCommand,
+  serializeAws_json1_1DeleteBatchPredictionCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DeleteBatchPredictionCommandInput = DeleteBatchPredictionInput;
+export type DeleteBatchPredictionCommandOutput = DeleteBatchPredictionOutput;
+
+export class DeleteBatchPredictionCommand extends $Command<
+  DeleteBatchPredictionCommandInput,
+  DeleteBatchPredictionCommandOutput,
+  MachineLearningClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteBatchPredictionCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MachineLearningClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DeleteBatchPredictionCommandInput,
+    DeleteBatchPredictionCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteBatchPredictionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteBatchPredictionCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteBatchPredictionCommandOutput> {
+    return deserializeAws_json1_1DeleteBatchPredictionCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-machine-learning/commands/DescribeMLModelsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  MachineLearningClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MachineLearningClient";
+import { DescribeMLModelsInput, DescribeMLModelsOutput } from "../models/index";
+import {
+  deserializeAws_json1_1DescribeMLModelsCommand,
+  serializeAws_json1_1DescribeMLModelsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeMLModelsCommandInput = DescribeMLModelsInput;
+export type DescribeMLModelsCommandOutput = DescribeMLModelsOutput;
+
+export class DescribeMLModelsCommand extends $Command<
+  DescribeMLModelsCommandInput,
+  DescribeMLModelsCommandOutput,
+  MachineLearningClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeMLModelsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MachineLearningClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeMLModelsCommandInput, DescribeMLModelsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeMLModelsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeMLModelsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeMLModelsCommandOutput> {
+    return deserializeAws_json1_1DescribeMLModelsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
