4b43c1d72f417a64945f557495160afa0870e77c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:57 -0800

    feat: add client-ecr (#656)

---
+++ clients/client-ecr/commands/BatchGetImageCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ECRClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ECRClient";
+import { BatchGetImageRequest, BatchGetImageResponse } from "../models/index";
+import {
+  deserializeAws_json1_1BatchGetImageCommand,
+  serializeAws_json1_1BatchGetImageCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type BatchGetImageCommandInput = BatchGetImageRequest;
+export type BatchGetImageCommandOutput = BatchGetImageResponse;
+
+export class BatchGetImageCommand extends $Command<
+  BatchGetImageCommandInput,
+  BatchGetImageCommandOutput,
+  ECRClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: BatchGetImageCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ECRClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<BatchGetImageCommandInput, BatchGetImageCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: BatchGetImageCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1BatchGetImageCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<BatchGetImageCommandOutput> {
+    return deserializeAws_json1_1BatchGetImageCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-ecr/commands/StartLifecyclePolicyPreviewCommand.ts
@@ -0,0 +1,93 @@
+import {
+  ECRClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ECRClient";
+import {
+  StartLifecyclePolicyPreviewRequest,
+  StartLifecyclePolicyPreviewResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1StartLifecyclePolicyPreviewCommand,
+  serializeAws_json1_1StartLifecyclePolicyPreviewCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type StartLifecyclePolicyPreviewCommandInput = StartLifecyclePolicyPreviewRequest;
+export type StartLifecyclePolicyPreviewCommandOutput = StartLifecyclePolicyPreviewResponse;
+
+export class StartLifecyclePolicyPreviewCommand extends $Command<
+  StartLifecyclePolicyPreviewCommandInput,
+  StartLifecyclePolicyPreviewCommandOutput,
+  ECRClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: StartLifecyclePolicyPreviewCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ECRClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    StartLifecyclePolicyPreviewCommandInput,
+    StartLifecyclePolicyPreviewCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: StartLifecyclePolicyPreviewCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1StartLifecyclePolicyPreviewCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<StartLifecyclePolicyPreviewCommandOutput> {
+    return deserializeAws_json1_1StartLifecyclePolicyPreviewCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
