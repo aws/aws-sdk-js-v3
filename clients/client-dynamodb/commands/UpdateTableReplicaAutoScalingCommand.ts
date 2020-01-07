623993f4a3dce577b21e895d08ad1361a893c1a5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 17:13:43 -0800

    feat: add client-dynamodb (#658)

---
+++ clients/client-dynamodb/commands/UpdateTableReplicaAutoScalingCommand.ts
@@ -0,0 +1,93 @@
+import {
+  DynamoDBClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DynamoDBClient";
+import {
+  UpdateTableReplicaAutoScalingInput,
+  UpdateTableReplicaAutoScalingOutput
+} from "../models/index";
+import {
+  deserializeAws_json1_0UpdateTableReplicaAutoScalingCommand,
+  serializeAws_json1_0UpdateTableReplicaAutoScalingCommand
+} from "../protocols/Aws_json1_0";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateTableReplicaAutoScalingCommandInput = UpdateTableReplicaAutoScalingInput;
+export type UpdateTableReplicaAutoScalingCommandOutput = UpdateTableReplicaAutoScalingOutput;
+
+export class UpdateTableReplicaAutoScalingCommand extends $Command<
+  UpdateTableReplicaAutoScalingCommandInput,
+  UpdateTableReplicaAutoScalingCommandOutput,
+  DynamoDBClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateTableReplicaAutoScalingCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DynamoDBClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateTableReplicaAutoScalingCommandInput,
+    UpdateTableReplicaAutoScalingCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateTableReplicaAutoScalingCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_0UpdateTableReplicaAutoScalingCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateTableReplicaAutoScalingCommandOutput> {
+    return deserializeAws_json1_0UpdateTableReplicaAutoScalingCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
