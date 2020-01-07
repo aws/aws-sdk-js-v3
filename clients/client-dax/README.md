644b94ac87c70de988fb3bc02f8c93866b66e96e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:25 -0800

    feat: add client-dax (#656)

---
+++ clients/client-dax/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-dax
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-dax/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-dax)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-dax.svg)](https://www.npmjs.com/package/@aws-sdk/client-dax)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-dax/commands/UpdateParameterGroupCommand.ts
@@ -0,0 +1,87 @@
+import {
+  DAXClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DAXClient";
+import {
+  UpdateParameterGroupRequest,
+  UpdateParameterGroupResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1UpdateParameterGroupCommand,
+  serializeAws_json1_1UpdateParameterGroupCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateParameterGroupCommandInput = UpdateParameterGroupRequest;
+export type UpdateParameterGroupCommandOutput = UpdateParameterGroupResponse;
+
+export class UpdateParameterGroupCommand extends $Command<
+  UpdateParameterGroupCommandInput,
+  UpdateParameterGroupCommandOutput,
+  DAXClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateParameterGroupCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DAXClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateParameterGroupCommandInput,
+    UpdateParameterGroupCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateParameterGroupCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1UpdateParameterGroupCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateParameterGroupCommandOutput> {
+    return deserializeAws_json1_1UpdateParameterGroupCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
