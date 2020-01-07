5d57a19c535e001882200f22bae8e78f0b5846c7
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:51:39 -0800

    feat: add client-forecast (#656)

---
+++ clients/client-forecast/commands/DescribeDatasetGroupCommand.ts
@@ -0,0 +1,87 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  forecastClientResolvedConfig
+} from "../forecastClient";
+import {
+  DescribeDatasetGroupRequest,
+  DescribeDatasetGroupResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1DescribeDatasetGroupCommand,
+  serializeAws_json1_1DescribeDatasetGroupCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeDatasetGroupCommandInput = DescribeDatasetGroupRequest;
+export type DescribeDatasetGroupCommandOutput = DescribeDatasetGroupResponse;
+
+export class DescribeDatasetGroupCommand extends $Command<
+  DescribeDatasetGroupCommandInput,
+  DescribeDatasetGroupCommandOutput,
+  forecastClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeDatasetGroupCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: forecastClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeDatasetGroupCommandInput,
+    DescribeDatasetGroupCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeDatasetGroupCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeDatasetGroupCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeDatasetGroupCommandOutput> {
+    return deserializeAws_json1_1DescribeDatasetGroupCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
