e5ce3d5d3c939300f5db312e78b262a0e27ba121
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:56:51 -0800

    feat: add client-wafv2 (#656)

---
+++ clients/client-wafv2/commands/ListWebACLsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  WAFV2ClientResolvedConfig
+} from "../WAFV2Client";
+import { ListWebACLsRequest, ListWebACLsResponse } from "../models/index";
+import {
+  deserializeAws_json1_1ListWebACLsCommand,
+  serializeAws_json1_1ListWebACLsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListWebACLsCommandInput = ListWebACLsRequest;
+export type ListWebACLsCommandOutput = ListWebACLsResponse;
+
+export class ListWebACLsCommand extends $Command<
+  ListWebACLsCommandInput,
+  ListWebACLsCommandOutput,
+  WAFV2ClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListWebACLsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: WAFV2ClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListWebACLsCommandInput, ListWebACLsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListWebACLsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListWebACLsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListWebACLsCommandOutput> {
+    return deserializeAws_json1_1ListWebACLsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
