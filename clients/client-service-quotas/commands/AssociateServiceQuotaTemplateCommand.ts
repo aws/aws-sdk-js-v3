f936458a80e9b635b89da79340dad8d0cd95bb27
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:55:42 -0800

    feat: add client-service-quotas (#656)

---
+++ clients/client-service-quotas/commands/AssociateServiceQuotaTemplateCommand.ts
@@ -0,0 +1,93 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  ServiceQuotasClientResolvedConfig
+} from "../ServiceQuotasClient";
+import {
+  AssociateServiceQuotaTemplateRequest,
+  AssociateServiceQuotaTemplateResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1AssociateServiceQuotaTemplateCommand,
+  serializeAws_json1_1AssociateServiceQuotaTemplateCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type AssociateServiceQuotaTemplateCommandInput = AssociateServiceQuotaTemplateRequest;
+export type AssociateServiceQuotaTemplateCommandOutput = AssociateServiceQuotaTemplateResponse;
+
+export class AssociateServiceQuotaTemplateCommand extends $Command<
+  AssociateServiceQuotaTemplateCommandInput,
+  AssociateServiceQuotaTemplateCommandOutput,
+  ServiceQuotasClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: AssociateServiceQuotaTemplateCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ServiceQuotasClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    AssociateServiceQuotaTemplateCommandInput,
+    AssociateServiceQuotaTemplateCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AssociateServiceQuotaTemplateCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AssociateServiceQuotaTemplateCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AssociateServiceQuotaTemplateCommandOutput> {
+    return deserializeAws_json1_1AssociateServiceQuotaTemplateCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
