365fa24b443cc11658379bea2e560641085da24e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-07 10:39:38 -0800

    chore: remove clients blocking yarn test:all (#667)

--- clients/client-savingsplans/runtimeConfig.shared.ts
+++
@@ -1,7 +0,0 @@
-import { defaultRegionInfoProvider } from "./endpoints";
-
-export const ClientSharedValues = {
-  apiVersion: "2019-06-28",
-  signingName: "savingsplans",
-  regionInfoProvider: defaultRegionInfoProvider
-};
