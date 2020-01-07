c7fb5c47c4a02434724a875909e0542ca07f927e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:51:39 -0800

    feat: add client-forecast (#656)

---
+++ clients/client-forecast/commands/DeleteForecastExportJobCommand.ts
@@ -0,0 +1,88 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  forecastClientResolvedConfig
+} from "../forecastClient";
+import { DeleteForecastExportJobRequest } from "../models/index";
+import {
+  deserializeAws_json1_1DeleteForecastExportJobCommand,
+  serializeAws_json1_1DeleteForecastExportJobCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type DeleteForecastExportJobCommandInput = DeleteForecastExportJobRequest;
+export type DeleteForecastExportJobCommandOutput = __MetadataBearer;
+
+export class DeleteForecastExportJobCommand extends $Command<
+  DeleteForecastExportJobCommandInput,
+  DeleteForecastExportJobCommandOutput,
+  forecastClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteForecastExportJobCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: forecastClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DeleteForecastExportJobCommandInput,
+    DeleteForecastExportJobCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteForecastExportJobCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteForecastExportJobCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteForecastExportJobCommandOutput> {
+    return deserializeAws_json1_1DeleteForecastExportJobCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-forecast/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2018-06-26",
+  signingName: "forecast",
+  regionInfoProvider: defaultRegionInfoProvider
+};
