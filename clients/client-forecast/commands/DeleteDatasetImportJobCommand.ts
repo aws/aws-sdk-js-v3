38b17fcd41915a7d2c7224f301d1dd831be30ca5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:51:39 -0800

    feat: add client-forecast (#656)

---
+++ clients/client-forecast/commands/DeleteDatasetImportJobCommand.ts
@@ -0,0 +1,85 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  forecastClientResolvedConfig
+} from "../forecastClient";
+import { DeleteDatasetImportJobRequest } from "../models/index";
+import {
+  deserializeAws_json1_1DeleteDatasetImportJobCommand,
+  serializeAws_json1_1DeleteDatasetImportJobCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type DeleteDatasetImportJobCommandInput = DeleteDatasetImportJobRequest;
+export type DeleteDatasetImportJobCommandOutput = __MetadataBearer;
+
+export class DeleteDatasetImportJobCommand extends $Command<
+  DeleteDatasetImportJobCommandInput,
+  DeleteDatasetImportJobCommandOutput,
+  forecastClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteDatasetImportJobCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: forecastClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DeleteDatasetImportJobCommandInput,
+    DeleteDatasetImportJobCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteDatasetImportJobCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteDatasetImportJobCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteDatasetImportJobCommandOutput> {
+    return deserializeAws_json1_1DeleteDatasetImportJobCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-forecast/commands/DeletePredictorCommand.ts
@@ -0,0 +1,82 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  forecastClientResolvedConfig
+} from "../forecastClient";
+import { DeletePredictorRequest } from "../models/index";
+import {
+  deserializeAws_json1_1DeletePredictorCommand,
+  serializeAws_json1_1DeletePredictorCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type DeletePredictorCommandInput = DeletePredictorRequest;
+export type DeletePredictorCommandOutput = __MetadataBearer;
+
+export class DeletePredictorCommand extends $Command<
+  DeletePredictorCommandInput,
+  DeletePredictorCommandOutput,
+  forecastClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeletePredictorCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: forecastClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DeletePredictorCommandInput, DeletePredictorCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeletePredictorCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeletePredictorCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeletePredictorCommandOutput> {
+    return deserializeAws_json1_1DeletePredictorCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
