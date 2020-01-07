3e6a42b4dff3c861f98d17efea1c437eeef63788
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:31 -0800

    feat: add client-machine-learning (#656)

---
+++ clients/client-machine-learning/commands/DeleteRealtimeEndpointCommand.ts
@@ -0,0 +1,87 @@
+import {
+  MachineLearningClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MachineLearningClient";
+import {
+  DeleteRealtimeEndpointInput,
+  DeleteRealtimeEndpointOutput
+} from "../models/index";
+import {
+  deserializeAws_json1_1DeleteRealtimeEndpointCommand,
+  serializeAws_json1_1DeleteRealtimeEndpointCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DeleteRealtimeEndpointCommandInput = DeleteRealtimeEndpointInput;
+export type DeleteRealtimeEndpointCommandOutput = DeleteRealtimeEndpointOutput;
+
+export class DeleteRealtimeEndpointCommand extends $Command<
+  DeleteRealtimeEndpointCommandInput,
+  DeleteRealtimeEndpointCommandOutput,
+  MachineLearningClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteRealtimeEndpointCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MachineLearningClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DeleteRealtimeEndpointCommandInput,
+    DeleteRealtimeEndpointCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteRealtimeEndpointCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteRealtimeEndpointCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteRealtimeEndpointCommandOutput> {
+    return deserializeAws_json1_1DeleteRealtimeEndpointCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
