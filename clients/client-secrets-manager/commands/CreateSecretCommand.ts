71df2b8e764edf476783046dd11cb60bd2cdd074
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:55:27 -0800

    feat: add client-secrets-manager (#656)

---
+++ clients/client-secrets-manager/commands/CreateSecretCommand.ts
@@ -0,0 +1,81 @@
+import {
+  SecretsManagerClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SecretsManagerClient";
+import { CreateSecretRequest, CreateSecretResponse } from "../models/index";
+import {
+  deserializeAws_json1_1CreateSecretCommand,
+  serializeAws_json1_1CreateSecretCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateSecretCommandInput = CreateSecretRequest;
+export type CreateSecretCommandOutput = CreateSecretResponse;
+
+export class CreateSecretCommand extends $Command<
+  CreateSecretCommandInput,
+  CreateSecretCommandOutput,
+  SecretsManagerClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateSecretCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SecretsManagerClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateSecretCommandInput, CreateSecretCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateSecretCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateSecretCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateSecretCommandOutput> {
+    return deserializeAws_json1_1CreateSecretCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-secrets-manager/commands/GetRandomPasswordCommand.ts
@@ -0,0 +1,84 @@
+import {
+  SecretsManagerClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SecretsManagerClient";
+import {
+  GetRandomPasswordRequest,
+  GetRandomPasswordResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1GetRandomPasswordCommand,
+  serializeAws_json1_1GetRandomPasswordCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetRandomPasswordCommandInput = GetRandomPasswordRequest;
+export type GetRandomPasswordCommandOutput = GetRandomPasswordResponse;
+
+export class GetRandomPasswordCommand extends $Command<
+  GetRandomPasswordCommandInput,
+  GetRandomPasswordCommandOutput,
+  SecretsManagerClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetRandomPasswordCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SecretsManagerClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetRandomPasswordCommandInput, GetRandomPasswordCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetRandomPasswordCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1GetRandomPasswordCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetRandomPasswordCommandOutput> {
+    return deserializeAws_json1_1GetRandomPasswordCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
