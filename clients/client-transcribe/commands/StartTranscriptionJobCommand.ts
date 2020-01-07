ecd85ae3787bac6c843ba1fa1eac9ac5126e3173
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:56:24 -0800

    feat: add client-transcribe (#656)

---
+++ clients/client-transcribe/commands/StartTranscriptionJobCommand.ts
@@ -0,0 +1,87 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  TranscribeClientResolvedConfig
+} from "../TranscribeClient";
+import {
+  StartTranscriptionJobRequest,
+  StartTranscriptionJobResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1StartTranscriptionJobCommand,
+  serializeAws_json1_1StartTranscriptionJobCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type StartTranscriptionJobCommandInput = StartTranscriptionJobRequest;
+export type StartTranscriptionJobCommandOutput = StartTranscriptionJobResponse;
+
+export class StartTranscriptionJobCommand extends $Command<
+  StartTranscriptionJobCommandInput,
+  StartTranscriptionJobCommandOutput,
+  TranscribeClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: StartTranscriptionJobCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: TranscribeClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    StartTranscriptionJobCommandInput,
+    StartTranscriptionJobCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: StartTranscriptionJobCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1StartTranscriptionJobCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<StartTranscriptionJobCommandOutput> {
+    return deserializeAws_json1_1StartTranscriptionJobCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
