908879ab51fb399c1e65328bbc8d40c3de475d8c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:47 -0800

    feat: add client-codebuild (#656)

---
+++ clients/client-codebuild/commands/ListCuratedEnvironmentImagesCommand.ts
@@ -0,0 +1,93 @@
+import {
+  CodeBuildClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CodeBuildClient";
+import {
+  ListCuratedEnvironmentImagesInput,
+  ListCuratedEnvironmentImagesOutput
+} from "../models/index";
+import {
+  deserializeAws_json1_1ListCuratedEnvironmentImagesCommand,
+  serializeAws_json1_1ListCuratedEnvironmentImagesCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListCuratedEnvironmentImagesCommandInput = ListCuratedEnvironmentImagesInput;
+export type ListCuratedEnvironmentImagesCommandOutput = ListCuratedEnvironmentImagesOutput;
+
+export class ListCuratedEnvironmentImagesCommand extends $Command<
+  ListCuratedEnvironmentImagesCommandInput,
+  ListCuratedEnvironmentImagesCommandOutput,
+  CodeBuildClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListCuratedEnvironmentImagesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CodeBuildClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListCuratedEnvironmentImagesCommandInput,
+    ListCuratedEnvironmentImagesCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListCuratedEnvironmentImagesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListCuratedEnvironmentImagesCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListCuratedEnvironmentImagesCommandOutput> {
+    return deserializeAws_json1_1ListCuratedEnvironmentImagesCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
