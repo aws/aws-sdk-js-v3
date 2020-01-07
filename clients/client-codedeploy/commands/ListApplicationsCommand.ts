a7693440c14f64f56f693e7bfee78835a34e967d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:59 -0800

    feat: add client-codedeploy (#656)

---
+++ clients/client-codedeploy/commands/ListApplicationsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  CodeDeployClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CodeDeployClient";
+import { ListApplicationsInput, ListApplicationsOutput } from "../models/index";
+import {
+  deserializeAws_json1_1ListApplicationsCommand,
+  serializeAws_json1_1ListApplicationsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListApplicationsCommandInput = ListApplicationsInput;
+export type ListApplicationsCommandOutput = ListApplicationsOutput;
+
+export class ListApplicationsCommand extends $Command<
+  ListApplicationsCommandInput,
+  ListApplicationsCommandOutput,
+  CodeDeployClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListApplicationsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CodeDeployClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListApplicationsCommandInput, ListApplicationsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListApplicationsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListApplicationsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListApplicationsCommandOutput> {
+    return deserializeAws_json1_1ListApplicationsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
