e21af70870d82ce5c74cc4aad57511f273a0c828
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:55:00 -0800

    feat: add client-resource-groups-tagging-api (#656)

---
+++ clients/client-resource-groups-tagging-api/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2017-01-26",
+  signingName: "tagging",
+  regionInfoProvider: defaultRegionInfoProvider
+};
