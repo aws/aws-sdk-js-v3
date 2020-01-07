a492bfc266745522b5b631018bd127b2751300f2
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:38 -0800

    feat: add client-macie (#656)

---
+++ clients/client-macie/commands/AssociateS3ResourcesCommand.ts
@@ -0,0 +1,87 @@
+import {
+  MacieClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MacieClient";
+import {
+  AssociateS3ResourcesRequest,
+  AssociateS3ResourcesResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1AssociateS3ResourcesCommand,
+  serializeAws_json1_1AssociateS3ResourcesCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type AssociateS3ResourcesCommandInput = AssociateS3ResourcesRequest;
+export type AssociateS3ResourcesCommandOutput = AssociateS3ResourcesResult;
+
+export class AssociateS3ResourcesCommand extends $Command<
+  AssociateS3ResourcesCommandInput,
+  AssociateS3ResourcesCommandOutput,
+  MacieClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: AssociateS3ResourcesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MacieClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    AssociateS3ResourcesCommandInput,
+    AssociateS3ResourcesCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AssociateS3ResourcesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AssociateS3ResourcesCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AssociateS3ResourcesCommandOutput> {
+    return deserializeAws_json1_1AssociateS3ResourcesCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
