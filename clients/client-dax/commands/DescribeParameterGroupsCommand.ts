3b6e8476197f97bc0786ff9726c69dde9eedef3d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:25 -0800

    feat: add client-dax (#656)

---
+++ clients/client-dax/commands/DescribeParameterGroupsCommand.ts
@@ -0,0 +1,90 @@
+import {
+  DAXClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DAXClient";
+import {
+  DescribeParameterGroupsRequest,
+  DescribeParameterGroupsResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1DescribeParameterGroupsCommand,
+  serializeAws_json1_1DescribeParameterGroupsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeParameterGroupsCommandInput = DescribeParameterGroupsRequest;
+export type DescribeParameterGroupsCommandOutput = DescribeParameterGroupsResponse;
+
+export class DescribeParameterGroupsCommand extends $Command<
+  DescribeParameterGroupsCommandInput,
+  DescribeParameterGroupsCommandOutput,
+  DAXClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeParameterGroupsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DAXClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeParameterGroupsCommandInput,
+    DescribeParameterGroupsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeParameterGroupsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeParameterGroupsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeParameterGroupsCommandOutput> {
+    return deserializeAws_json1_1DescribeParameterGroupsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
