29b15af5177fb53d783e044dda7e8b68d935641c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:47:44 -0800

    feat: add client-alexa-for-business (#656)

---
+++ clients/client-alexa-for-business/commands/UpdateNetworkProfileCommand.ts
@@ -0,0 +1,87 @@
+import {
+  AlexaForBusinessClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../AlexaForBusinessClient";
+import {
+  UpdateNetworkProfileRequest,
+  UpdateNetworkProfileResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1UpdateNetworkProfileCommand,
+  serializeAws_json1_1UpdateNetworkProfileCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateNetworkProfileCommandInput = UpdateNetworkProfileRequest;
+export type UpdateNetworkProfileCommandOutput = UpdateNetworkProfileResponse;
+
+export class UpdateNetworkProfileCommand extends $Command<
+  UpdateNetworkProfileCommandInput,
+  UpdateNetworkProfileCommandOutput,
+  AlexaForBusinessClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateNetworkProfileCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: AlexaForBusinessClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateNetworkProfileCommandInput,
+    UpdateNetworkProfileCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateNetworkProfileCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1UpdateNetworkProfileCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateNetworkProfileCommandOutput> {
+    return deserializeAws_json1_1UpdateNetworkProfileCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
