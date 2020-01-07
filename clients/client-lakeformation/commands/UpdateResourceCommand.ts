4d64334b6221a82ed0f510a6d184165ddbf47d14
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:14 -0800

    feat: add client-lakeformation (#656)

---
+++ clients/client-lakeformation/commands/UpdateResourceCommand.ts
@@ -0,0 +1,81 @@
+import {
+  LakeFormationClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../LakeFormationClient";
+import { UpdateResourceRequest, UpdateResourceResponse } from "../models/index";
+import {
+  deserializeAws_json1_1UpdateResourceCommand,
+  serializeAws_json1_1UpdateResourceCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateResourceCommandInput = UpdateResourceRequest;
+export type UpdateResourceCommandOutput = UpdateResourceResponse;
+
+export class UpdateResourceCommand extends $Command<
+  UpdateResourceCommandInput,
+  UpdateResourceCommandOutput,
+  LakeFormationClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateResourceCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: LakeFormationClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<UpdateResourceCommandInput, UpdateResourceCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateResourceCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1UpdateResourceCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateResourceCommandOutput> {
+    return deserializeAws_json1_1UpdateResourceCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
