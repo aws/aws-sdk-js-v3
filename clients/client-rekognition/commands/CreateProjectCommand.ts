4209a887c1f4017466b931c989be92e6ff8c8e9f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:54:52 -0800

    feat: add client-rekognition (#656)

---
+++ clients/client-rekognition/commands/CreateProjectCommand.ts
@@ -0,0 +1,81 @@
+import {
+  RekognitionClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../RekognitionClient";
+import { CreateProjectRequest, CreateProjectResponse } from "../models/index";
+import {
+  deserializeAws_json1_1CreateProjectCommand,
+  serializeAws_json1_1CreateProjectCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateProjectCommandInput = CreateProjectRequest;
+export type CreateProjectCommandOutput = CreateProjectResponse;
+
+export class CreateProjectCommand extends $Command<
+  CreateProjectCommandInput,
+  CreateProjectCommandOutput,
+  RekognitionClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateProjectCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: RekognitionClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateProjectCommandInput, CreateProjectCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateProjectCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateProjectCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateProjectCommandOutput> {
+    return deserializeAws_json1_1CreateProjectCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-rekognition/commands/GetFaceSearchCommand.ts
@@ -0,0 +1,81 @@
+import {
+  RekognitionClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../RekognitionClient";
+import { GetFaceSearchRequest, GetFaceSearchResponse } from "../models/index";
+import {
+  deserializeAws_json1_1GetFaceSearchCommand,
+  serializeAws_json1_1GetFaceSearchCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetFaceSearchCommandInput = GetFaceSearchRequest;
+export type GetFaceSearchCommandOutput = GetFaceSearchResponse;
+
+export class GetFaceSearchCommand extends $Command<
+  GetFaceSearchCommandInput,
+  GetFaceSearchCommandOutput,
+  RekognitionClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetFaceSearchCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: RekognitionClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetFaceSearchCommandInput, GetFaceSearchCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetFaceSearchCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1GetFaceSearchCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetFaceSearchCommandOutput> {
+    return deserializeAws_json1_1GetFaceSearchCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
