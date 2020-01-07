df9a3eb6ea94b176f794d433bfcc63143b86d2f5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:23 -0800

    feat: add client-lightsail (#656)

---
+++ clients/client-lightsail/commands/StartRelationalDatabaseCommand.ts
@@ -0,0 +1,90 @@
+import {
+  LightsailClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../LightsailClient";
+import {
+  StartRelationalDatabaseRequest,
+  StartRelationalDatabaseResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1StartRelationalDatabaseCommand,
+  serializeAws_json1_1StartRelationalDatabaseCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type StartRelationalDatabaseCommandInput = StartRelationalDatabaseRequest;
+export type StartRelationalDatabaseCommandOutput = StartRelationalDatabaseResult;
+
+export class StartRelationalDatabaseCommand extends $Command<
+  StartRelationalDatabaseCommandInput,
+  StartRelationalDatabaseCommandOutput,
+  LightsailClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: StartRelationalDatabaseCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: LightsailClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    StartRelationalDatabaseCommandInput,
+    StartRelationalDatabaseCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: StartRelationalDatabaseCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1StartRelationalDatabaseCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<StartRelationalDatabaseCommandOutput> {
+    return deserializeAws_json1_1StartRelationalDatabaseCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-lightsail/commands/UpdateRelationalDatabaseCommand.ts
@@ -0,0 +1,90 @@
+import {
+  LightsailClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../LightsailClient";
+import {
+  UpdateRelationalDatabaseRequest,
+  UpdateRelationalDatabaseResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1UpdateRelationalDatabaseCommand,
+  serializeAws_json1_1UpdateRelationalDatabaseCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateRelationalDatabaseCommandInput = UpdateRelationalDatabaseRequest;
+export type UpdateRelationalDatabaseCommandOutput = UpdateRelationalDatabaseResult;
+
+export class UpdateRelationalDatabaseCommand extends $Command<
+  UpdateRelationalDatabaseCommandInput,
+  UpdateRelationalDatabaseCommandOutput,
+  LightsailClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateRelationalDatabaseCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: LightsailClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateRelationalDatabaseCommandInput,
+    UpdateRelationalDatabaseCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateRelationalDatabaseCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1UpdateRelationalDatabaseCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateRelationalDatabaseCommandOutput> {
+    return deserializeAws_json1_1UpdateRelationalDatabaseCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
