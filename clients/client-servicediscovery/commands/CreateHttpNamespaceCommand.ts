c0869d9c7b3e12b18e9e6579619caef463a0cb2d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:55:46 -0800

    feat: add client-servicediscovery (#656)

---
+++ clients/client-servicediscovery/commands/CreateHttpNamespaceCommand.ts
@@ -0,0 +1,87 @@
+import {
+  ServiceDiscoveryClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ServiceDiscoveryClient";
+import {
+  CreateHttpNamespaceRequest,
+  CreateHttpNamespaceResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1CreateHttpNamespaceCommand,
+  serializeAws_json1_1CreateHttpNamespaceCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateHttpNamespaceCommandInput = CreateHttpNamespaceRequest;
+export type CreateHttpNamespaceCommandOutput = CreateHttpNamespaceResponse;
+
+export class CreateHttpNamespaceCommand extends $Command<
+  CreateHttpNamespaceCommandInput,
+  CreateHttpNamespaceCommandOutput,
+  ServiceDiscoveryClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateHttpNamespaceCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ServiceDiscoveryClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    CreateHttpNamespaceCommandInput,
+    CreateHttpNamespaceCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateHttpNamespaceCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateHttpNamespaceCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateHttpNamespaceCommandOutput> {
+    return deserializeAws_json1_1CreateHttpNamespaceCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-servicediscovery/commands/ListOperationsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceDiscoveryClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ServiceDiscoveryClient";
+import { ListOperationsRequest, ListOperationsResponse } from "../models/index";
+import {
+  deserializeAws_json1_1ListOperationsCommand,
+  serializeAws_json1_1ListOperationsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListOperationsCommandInput = ListOperationsRequest;
+export type ListOperationsCommandOutput = ListOperationsResponse;
+
+export class ListOperationsCommand extends $Command<
+  ListOperationsCommandInput,
+  ListOperationsCommandOutput,
+  ServiceDiscoveryClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListOperationsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ServiceDiscoveryClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListOperationsCommandInput, ListOperationsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListOperationsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListOperationsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListOperationsCommandOutput> {
+    return deserializeAws_json1_1ListOperationsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
