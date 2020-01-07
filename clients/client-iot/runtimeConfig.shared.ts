5f50f4db07437618c2b7cc84fc1bf0c3001dc1c5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-07 10:39:38 -0800

    chore: remove clients blocking yarn test:all (#667)

--- clients/client-iot/runtimeConfig.shared.ts
+++
@@ -1,7 +0,0 @@
-import { defaultRegionInfoProvider } from "./endpoints";
-
-export const ClientSharedValues = {
-  apiVersion: "2015-05-28",
-  signingName: "execute-api",
-  regionInfoProvider: defaultRegionInfoProvider
-};
