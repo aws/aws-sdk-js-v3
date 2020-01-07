a2588f7f43d6bd44f043457f1532a96c0e33dbb4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:38 -0800

    feat: add client-macie (#656)

---
+++ clients/client-macie/commands/AssociateMemberAccountCommand.ts
@@ -0,0 +1,85 @@
+import {
+  MacieClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MacieClient";
+import { AssociateMemberAccountRequest } from "../models/index";
+import {
+  deserializeAws_json1_1AssociateMemberAccountCommand,
+  serializeAws_json1_1AssociateMemberAccountCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type AssociateMemberAccountCommandInput = AssociateMemberAccountRequest;
+export type AssociateMemberAccountCommandOutput = __MetadataBearer;
+
+export class AssociateMemberAccountCommand extends $Command<
+  AssociateMemberAccountCommandInput,
+  AssociateMemberAccountCommandOutput,
+  MacieClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: AssociateMemberAccountCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MacieClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    AssociateMemberAccountCommandInput,
+    AssociateMemberAccountCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AssociateMemberAccountCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AssociateMemberAccountCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AssociateMemberAccountCommandOutput> {
+    return deserializeAws_json1_1AssociateMemberAccountCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
