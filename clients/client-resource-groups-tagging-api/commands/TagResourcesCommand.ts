00be4e6aeb29768aa7e99120d6614843c384a3db
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:55:00 -0800

    feat: add client-resource-groups-tagging-api (#656)

---
+++ clients/client-resource-groups-tagging-api/commands/TagResourcesCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ResourceGroupsTaggingAPIClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ResourceGroupsTaggingAPIClient";
+import { TagResourcesInput, TagResourcesOutput } from "../models/index";
+import {
+  deserializeAws_json1_1TagResourcesCommand,
+  serializeAws_json1_1TagResourcesCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type TagResourcesCommandInput = TagResourcesInput;
+export type TagResourcesCommandOutput = TagResourcesOutput;
+
+export class TagResourcesCommand extends $Command<
+  TagResourcesCommandInput,
+  TagResourcesCommandOutput,
+  ResourceGroupsTaggingAPIClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: TagResourcesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ResourceGroupsTaggingAPIClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<TagResourcesCommandInput, TagResourcesCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: TagResourcesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1TagResourcesCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<TagResourcesCommandOutput> {
+    return deserializeAws_json1_1TagResourcesCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
