d4ea1867d5b1f84a509fdc0e0db761b56e945e53
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:49:19 -0800

    feat: add client-cognito-identity (#656)

---
+++ clients/client-cognito-identity/commands/SetIdentityPoolRolesCommand.ts
@@ -0,0 +1,85 @@
+import {
+  CognitoIdentityClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CognitoIdentityClient";
+import { SetIdentityPoolRolesInput } from "../models/index";
+import {
+  deserializeAws_json1_1SetIdentityPoolRolesCommand,
+  serializeAws_json1_1SetIdentityPoolRolesCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type SetIdentityPoolRolesCommandInput = SetIdentityPoolRolesInput;
+export type SetIdentityPoolRolesCommandOutput = __MetadataBearer;
+
+export class SetIdentityPoolRolesCommand extends $Command<
+  SetIdentityPoolRolesCommandInput,
+  SetIdentityPoolRolesCommandOutput,
+  CognitoIdentityClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: SetIdentityPoolRolesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CognitoIdentityClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    SetIdentityPoolRolesCommandInput,
+    SetIdentityPoolRolesCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: SetIdentityPoolRolesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1SetIdentityPoolRolesCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<SetIdentityPoolRolesCommandOutput> {
+    return deserializeAws_json1_1SetIdentityPoolRolesCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
