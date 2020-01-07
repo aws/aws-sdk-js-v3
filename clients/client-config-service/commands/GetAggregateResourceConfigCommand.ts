c8e9ae9c16e0409518b9ab92f6230a0ea0ccf15b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:49:44 -0800

    feat: add client-config-service (#656)

---
+++ clients/client-config-service/commands/GetAggregateResourceConfigCommand.ts
@@ -0,0 +1,93 @@
+import {
+  ConfigServiceClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ConfigServiceClient";
+import {
+  GetAggregateResourceConfigRequest,
+  GetAggregateResourceConfigResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1GetAggregateResourceConfigCommand,
+  serializeAws_json1_1GetAggregateResourceConfigCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetAggregateResourceConfigCommandInput = GetAggregateResourceConfigRequest;
+export type GetAggregateResourceConfigCommandOutput = GetAggregateResourceConfigResponse;
+
+export class GetAggregateResourceConfigCommand extends $Command<
+  GetAggregateResourceConfigCommandInput,
+  GetAggregateResourceConfigCommandOutput,
+  ConfigServiceClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetAggregateResourceConfigCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ConfigServiceClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    GetAggregateResourceConfigCommandInput,
+    GetAggregateResourceConfigCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetAggregateResourceConfigCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1GetAggregateResourceConfigCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetAggregateResourceConfigCommandOutput> {
+    return deserializeAws_json1_1GetAggregateResourceConfigCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-config-service/commands/PutConformancePackCommand.ts
@@ -0,0 +1,84 @@
+import {
+  ConfigServiceClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ConfigServiceClient";
+import {
+  PutConformancePackRequest,
+  PutConformancePackResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1PutConformancePackCommand,
+  serializeAws_json1_1PutConformancePackCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type PutConformancePackCommandInput = PutConformancePackRequest;
+export type PutConformancePackCommandOutput = PutConformancePackResponse;
+
+export class PutConformancePackCommand extends $Command<
+  PutConformancePackCommandInput,
+  PutConformancePackCommandOutput,
+  ConfigServiceClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: PutConformancePackCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ConfigServiceClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<PutConformancePackCommandInput, PutConformancePackCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: PutConformancePackCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1PutConformancePackCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<PutConformancePackCommandOutput> {
+    return deserializeAws_json1_1PutConformancePackCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
