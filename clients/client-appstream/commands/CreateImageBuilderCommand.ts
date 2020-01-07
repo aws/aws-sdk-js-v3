c4ffdcf4e472d3d6f4529770cdce4f76f4607037
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:05 -0800

    feat: add client-appstream (#656)

---
+++ clients/client-appstream/commands/CreateImageBuilderCommand.ts
@@ -0,0 +1,84 @@
+import {
+  AppStreamClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../AppStreamClient";
+import {
+  CreateImageBuilderRequest,
+  CreateImageBuilderResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1CreateImageBuilderCommand,
+  serializeAws_json1_1CreateImageBuilderCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateImageBuilderCommandInput = CreateImageBuilderRequest;
+export type CreateImageBuilderCommandOutput = CreateImageBuilderResult;
+
+export class CreateImageBuilderCommand extends $Command<
+  CreateImageBuilderCommandInput,
+  CreateImageBuilderCommandOutput,
+  AppStreamClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateImageBuilderCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: AppStreamClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateImageBuilderCommandInput, CreateImageBuilderCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateImageBuilderCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateImageBuilderCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateImageBuilderCommandOutput> {
+    return deserializeAws_json1_1CreateImageBuilderCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
