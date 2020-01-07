ac518abbd992a19a634a94759b16e75db51318af
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:08 -0800

    feat: add client-kms (#656)

---
+++ clients/client-kms/commands/UpdateKeyDescriptionCommand.ts
@@ -0,0 +1,85 @@
+import {
+  KMSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../KMSClient";
+import { UpdateKeyDescriptionRequest } from "../models/index";
+import {
+  deserializeAws_json1_1UpdateKeyDescriptionCommand,
+  serializeAws_json1_1UpdateKeyDescriptionCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type UpdateKeyDescriptionCommandInput = UpdateKeyDescriptionRequest;
+export type UpdateKeyDescriptionCommandOutput = __MetadataBearer;
+
+export class UpdateKeyDescriptionCommand extends $Command<
+  UpdateKeyDescriptionCommandInput,
+  UpdateKeyDescriptionCommandOutput,
+  KMSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateKeyDescriptionCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: KMSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateKeyDescriptionCommandInput,
+    UpdateKeyDescriptionCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateKeyDescriptionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1UpdateKeyDescriptionCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateKeyDescriptionCommandOutput> {
+    return deserializeAws_json1_1UpdateKeyDescriptionCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
