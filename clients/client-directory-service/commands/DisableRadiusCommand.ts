cba073f1cb6f78767913e565b25892561ae87fb2
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:46 -0800

    feat: add client-directory-service (#656)

---
+++ clients/client-directory-service/commands/DisableRadiusCommand.ts
@@ -0,0 +1,81 @@
+import {
+  DirectoryServiceClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DirectoryServiceClient";
+import { DisableRadiusRequest, DisableRadiusResult } from "../models/index";
+import {
+  deserializeAws_json1_1DisableRadiusCommand,
+  serializeAws_json1_1DisableRadiusCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DisableRadiusCommandInput = DisableRadiusRequest;
+export type DisableRadiusCommandOutput = DisableRadiusResult;
+
+export class DisableRadiusCommand extends $Command<
+  DisableRadiusCommandInput,
+  DisableRadiusCommandOutput,
+  DirectoryServiceClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DisableRadiusCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DirectoryServiceClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DisableRadiusCommandInput, DisableRadiusCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DisableRadiusCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DisableRadiusCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DisableRadiusCommandOutput> {
+    return deserializeAws_json1_1DisableRadiusCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
