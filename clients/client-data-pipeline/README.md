97320bcc8825c02712eff0ce8ecf3bb469253f8b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:06 -0800

    feat: add client-data-pipeline (#656)

---
+++ clients/client-data-pipeline/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-data-pipeline
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-data-pipeline/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-data-pipeline)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-data-pipeline.svg)](https://www.npmjs.com/package/@aws-sdk/client-data-pipeline)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-data-pipeline/commands/AddTagsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  DataPipelineClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DataPipelineClient";
+import { AddTagsInput, AddTagsOutput } from "../models/index";
+import {
+  deserializeAws_json1_1AddTagsCommand,
+  serializeAws_json1_1AddTagsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type AddTagsCommandInput = AddTagsInput;
+export type AddTagsCommandOutput = AddTagsOutput;
+
+export class AddTagsCommand extends $Command<
+  AddTagsCommandInput,
+  AddTagsCommandOutput,
+  DataPipelineClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: AddTagsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DataPipelineClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<AddTagsCommandInput, AddTagsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AddTagsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AddTagsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AddTagsCommandOutput> {
+    return deserializeAws_json1_1AddTagsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
