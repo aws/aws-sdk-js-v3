9b80f1ceb22576ef0b8229d104747f226a13e553
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:39 -0800

    feat: add client-direct-connect (#656)

---
+++ clients/client-direct-connect/DirectConnectClient.ts
@@ -0,0 +1,393 @@
+import {
+  AcceptDirectConnectGatewayAssociationProposalRequest,
+  AcceptDirectConnectGatewayAssociationProposalResult,
+  AllocateConnectionOnInterconnectRequest,
+  AllocateHostedConnectionRequest,
+  AllocatePrivateVirtualInterfaceRequest,
+  AllocatePublicVirtualInterfaceRequest,
+  AllocateTransitVirtualInterfaceRequest,
+  AllocateTransitVirtualInterfaceResult,
+  AssociateConnectionWithLagRequest,
+  AssociateHostedConnectionRequest,
+  AssociateVirtualInterfaceRequest,
+  ConfirmConnectionRequest,
+  ConfirmConnectionResponse,
+  ConfirmPrivateVirtualInterfaceRequest,
+  ConfirmPrivateVirtualInterfaceResponse,
+  ConfirmPublicVirtualInterfaceRequest,
+  ConfirmPublicVirtualInterfaceResponse,
+  ConfirmTransitVirtualInterfaceRequest,
+  ConfirmTransitVirtualInterfaceResponse,
+  Connection,
+  Connections,
+  CreateBGPPeerRequest,
+  CreateBGPPeerResponse,
+  CreateConnectionRequest,
+  CreateDirectConnectGatewayAssociationProposalRequest,
+  CreateDirectConnectGatewayAssociationProposalResult,
+  CreateDirectConnectGatewayAssociationRequest,
+  CreateDirectConnectGatewayAssociationResult,
+  CreateDirectConnectGatewayRequest,
+  CreateDirectConnectGatewayResult,
+  CreateInterconnectRequest,
+  CreateLagRequest,
+  CreatePrivateVirtualInterfaceRequest,
+  CreatePublicVirtualInterfaceRequest,
+  CreateTransitVirtualInterfaceRequest,
+  CreateTransitVirtualInterfaceResult,
+  DeleteBGPPeerRequest,
+  DeleteBGPPeerResponse,
+  DeleteConnectionRequest,
+  DeleteDirectConnectGatewayAssociationProposalRequest,
+  DeleteDirectConnectGatewayAssociationProposalResult,
+  DeleteDirectConnectGatewayAssociationRequest,
+  DeleteDirectConnectGatewayAssociationResult,
+  DeleteDirectConnectGatewayRequest,
+  DeleteDirectConnectGatewayResult,
+  DeleteInterconnectRequest,
+  DeleteInterconnectResponse,
+  DeleteLagRequest,
+  DeleteVirtualInterfaceRequest,
+  DeleteVirtualInterfaceResponse,
+  DescribeConnectionLoaRequest,
+  DescribeConnectionLoaResponse,
+  DescribeConnectionsOnInterconnectRequest,
+  DescribeConnectionsRequest,
+  DescribeDirectConnectGatewayAssociationProposalsRequest,
+  DescribeDirectConnectGatewayAssociationProposalsResult,
+  DescribeDirectConnectGatewayAssociationsRequest,
+  DescribeDirectConnectGatewayAssociationsResult,
+  DescribeDirectConnectGatewayAttachmentsRequest,
+  DescribeDirectConnectGatewayAttachmentsResult,
+  DescribeDirectConnectGatewaysRequest,
+  DescribeDirectConnectGatewaysResult,
+  DescribeHostedConnectionsRequest,
+  DescribeInterconnectLoaRequest,
+  DescribeInterconnectLoaResponse,
+  DescribeInterconnectsRequest,
+  DescribeLagsRequest,
+  DescribeLoaRequest,
+  DescribeTagsRequest,
+  DescribeTagsResponse,
+  DescribeVirtualInterfacesRequest,
+  DisassociateConnectionFromLagRequest,
+  Interconnect,
+  Interconnects,
+  Lag,
+  Lags,
+  Loa,
+  Locations,
+  TagResourceRequest,
+  TagResourceResponse,
+  UntagResourceRequest,
+  UntagResourceResponse,
+  UpdateDirectConnectGatewayAssociationRequest,
+  UpdateDirectConnectGatewayAssociationResult,
+  UpdateLagRequest,
+  UpdateVirtualInterfaceAttributesRequest,
+  VirtualGateways,
+  VirtualInterface,
+  VirtualInterfaces
+} from "./models/index";
+import { ClientDefaultValues as __ClientDefaultValues } from "./runtimeConfig";
+import {
+  EndpointsInputConfig,
+  EndpointsResolvedConfig,
+  RegionInputConfig,
+  RegionResolvedConfig,
+  resolveEndpointsConfig,
+  resolveRegionConfig
+} from "@aws-sdk/config-resolver";
+import { getContentLengthPlugin } from "@aws-sdk/middleware-content-length";
+import {
+  HostHeaderInputConfig,
+  HostHeaderResolvedConfig,
+  getHostHeaderPlugin,
+  resolveHostHeaderConfig
+} from "@aws-sdk/middleware-host-header";
+import {
+  RetryInputConfig,
+  RetryResolvedConfig,
+  getRetryPlugin,
+  resolveRetryConfig
+} from "@aws-sdk/middleware-retry";
+import {
+  AwsAuthInputConfig,
+  AwsAuthResolvedConfig,
+  getAwsAuthPlugin,
+  resolveAwsAuthConfig
+} from "@aws-sdk/middleware-signing";
+import {
+  UserAgentInputConfig,
+  UserAgentResolvedConfig,
+  getUserAgentPlugin,
+  resolveUserAgentConfig
+} from "@aws-sdk/middleware-user-agent";
+import { HttpHandler as __HttpHandler } from "@aws-sdk/protocol-http";
+import {
+  Client as __Client,
+  SmithyConfiguration as __SmithyConfiguration,
+  SmithyResolvedConfiguration as __SmithyResolvedConfiguration
+} from "@aws-sdk/smithy-client";
+import {
+  RegionInfoProvider,
+  Credentials as __Credentials,
+  Decoder as __Decoder,
+  Encoder as __Encoder,
+  HashConstructor as __HashConstructor,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  Provider as __Provider,
+  StreamCollector as __StreamCollector,
+  UrlParser as __UrlParser
+} from "@aws-sdk/types";
+
+export type ServiceInputTypes =
+  | {}
+  | AcceptDirectConnectGatewayAssociationProposalRequest
+  | AllocateConnectionOnInterconnectRequest
+  | AllocateHostedConnectionRequest
+  | AllocatePrivateVirtualInterfaceRequest
+  | AllocatePublicVirtualInterfaceRequest
+  | AllocateTransitVirtualInterfaceRequest
+  | AssociateConnectionWithLagRequest
+  | AssociateHostedConnectionRequest
+  | AssociateVirtualInterfaceRequest
+  | ConfirmConnectionRequest
+  | ConfirmPrivateVirtualInterfaceRequest
+  | ConfirmPublicVirtualInterfaceRequest
+  | ConfirmTransitVirtualInterfaceRequest
+  | CreateBGPPeerRequest
+  | CreateConnectionRequest
+  | CreateDirectConnectGatewayAssociationProposalRequest
+  | CreateDirectConnectGatewayAssociationRequest
+  | CreateDirectConnectGatewayRequest
+  | CreateInterconnectRequest
+  | CreateLagRequest
+  | CreatePrivateVirtualInterfaceRequest
+  | CreatePublicVirtualInterfaceRequest
+  | CreateTransitVirtualInterfaceRequest
+  | DeleteBGPPeerRequest
+  | DeleteConnectionRequest
+  | DeleteDirectConnectGatewayAssociationProposalRequest
+  | DeleteDirectConnectGatewayAssociationRequest
+  | DeleteDirectConnectGatewayRequest
+  | DeleteInterconnectRequest
+  | DeleteLagRequest
+  | DeleteVirtualInterfaceRequest
+  | DescribeConnectionLoaRequest
+  | DescribeConnectionsOnInterconnectRequest
+  | DescribeConnectionsRequest
+  | DescribeDirectConnectGatewayAssociationProposalsRequest
+  | DescribeDirectConnectGatewayAssociationsRequest
+  | DescribeDirectConnectGatewayAttachmentsRequest
+  | DescribeDirectConnectGatewaysRequest
+  | DescribeHostedConnectionsRequest
+  | DescribeInterconnectLoaRequest
+  | DescribeInterconnectsRequest
+  | DescribeLagsRequest
+  | DescribeLoaRequest
+  | DescribeTagsRequest
+  | DescribeVirtualInterfacesRequest
+  | DisassociateConnectionFromLagRequest
+  | TagResourceRequest
+  | UntagResourceRequest
+  | UpdateDirectConnectGatewayAssociationRequest
+  | UpdateLagRequest
+  | UpdateVirtualInterfaceAttributesRequest;
+
+export type ServiceOutputTypes =
+  | AcceptDirectConnectGatewayAssociationProposalResult
+  | AllocateTransitVirtualInterfaceResult
+  | ConfirmConnectionResponse
+  | ConfirmPrivateVirtualInterfaceResponse
+  | ConfirmPublicVirtualInterfaceResponse
+  | ConfirmTransitVirtualInterfaceResponse
+  | Connection
+  | Connection
+  | Connection
+  | Connection
+  | Connection
+  | Connection
+  | Connection
+  | Connections
+  | Connections
+  | Connections
+  | CreateBGPPeerResponse
+  | CreateDirectConnectGatewayAssociationProposalResult
+  | CreateDirectConnectGatewayAssociationResult
+  | CreateDirectConnectGatewayResult
+  | CreateTransitVirtualInterfaceResult
+  | DeleteBGPPeerResponse
+  | DeleteDirectConnectGatewayAssociationProposalResult
+  | DeleteDirectConnectGatewayAssociationResult
+  | DeleteDirectConnectGatewayResult
+  | DeleteInterconnectResponse
+  | DeleteVirtualInterfaceResponse
+  | DescribeConnectionLoaResponse
+  | DescribeDirectConnectGatewayAssociationProposalsResult
+  | DescribeDirectConnectGatewayAssociationsResult
+  | DescribeDirectConnectGatewayAttachmentsResult
+  | DescribeDirectConnectGatewaysResult
+  | DescribeInterconnectLoaResponse
+  | DescribeTagsResponse
+  | Interconnect
+  | Interconnects
+  | Lag
+  | Lag
+  | Lag
+  | Lags
+  | Loa
+  | Locations
+  | TagResourceResponse
+  | UntagResourceResponse
+  | UpdateDirectConnectGatewayAssociationResult
+  | VirtualGateways
+  | VirtualInterface
+  | VirtualInterface
+  | VirtualInterface
+  | VirtualInterface
+  | VirtualInterface
+  | VirtualInterface
+  | VirtualInterfaces;
+
+export interface ClientDefaults
+  extends Partial<__SmithyResolvedConfiguration<__HttpHandlerOptions>> {
+  /**
+   * The HTTP handler to use. Fetch in browser and Https in Nodejs.
+   */
+  requestHandler?: __HttpHandler;
+
+  /**
+   * A constructor for a class implementing the @aws-sdk/types.Hash interface
+   * that computes the SHA-256 HMAC or checksum of a string or binary buffer.
+   */
+  sha256?: __HashConstructor;
+
+  /**
+   * The function that will be used to convert strings into HTTP endpoints.
+   */
+  urlParser?: __UrlParser;
+
+  /**
+   * A function that can calculate the length of a request body.
+   */
+  bodyLengthChecker?: (body: any) => number | undefined;
+
+  /**
+   * A function that converts a stream into an array of bytes.
+   */
+  streamCollector?: __StreamCollector;
+
+  /**
+   * The function that will be used to convert a base64-encoded string to a byte array
+   */
+  base64Decoder?: __Decoder;
+
+  /**
+   * The function that will be used to convert binary data to a base64-encoded string
+   */
+  base64Encoder?: __Encoder;
+
+  /**
+   * The function that will be used to convert a UTF8-encoded string to a byte array
+   */
+  utf8Decoder?: __Decoder;
+
+  /**
+   * The function that will be used to convert binary data to a UTF-8 encoded string
+   */
+  utf8Encoder?: __Encoder;
+
+  /**
+   * The string that will be used to populate default value in 'User-Agent' header
+   */
+  defaultUserAgent?: string;
+
+  /**
+   * The runtime environment
+   */
+  runtime?: string;
+
+  /**
+   * The service name with which to sign requests.
+   */
+  signingName?: string;
+
+  /**
+   * Default credentials provider; Not available in browser runtime
+   */
+  credentialDefaultProvider?: (input: any) => __Provider<__Credentials>;
+
+  /**
+   * Provider function that return promise of a region string
+   */
+  regionDefaultProvider?: (input: any) => __Provider<string>;
+
+  /**
+   * Fetch related hostname, signing name or signing region with given region.
+   */
+  regionInfoProvider?: RegionInfoProvider;
+}
+
+export type DirectConnectClientConfig = Partial<
+  __SmithyConfiguration<__HttpHandlerOptions>
+> &
+  ClientDefaults &
+  RegionInputConfig &
+  EndpointsInputConfig &
+  AwsAuthInputConfig &
+  RetryInputConfig &
+  UserAgentInputConfig &
+  HostHeaderInputConfig;
+
+export type DirectConnectClientResolvedConfig = __SmithyResolvedConfiguration<
+  __HttpHandlerOptions
+> &
+  Required<ClientDefaults> &
+  RegionResolvedConfig &
+  EndpointsResolvedConfig &
+  AwsAuthResolvedConfig &
+  RetryResolvedConfig &
+  UserAgentResolvedConfig &
+  HostHeaderResolvedConfig;
+
+/**
+ *
+ *          <p>AWS Direct Connect links your internal network to an AWS Direct Connect location over a standard Ethernet fiber-optic cable.
+ *       One end of the cable is connected to your router, the other to an AWS Direct Connect router. With this connection
+ *       in place, you can create virtual interfaces directly to the AWS cloud (for example, to Amazon EC2
+ *       and Amazon S3) and to Amazon VPC, bypassing Internet service providers in your network path. A
+ *       connection provides access to all AWS Regions except the China (Beijing) and (China) Ningxia Regions.
+ *       AWS resources in the China Regions can only be accessed through locations associated with those Regions.</p>
+ *
+ */
+export class DirectConnectClient extends __Client<
+  __HttpHandlerOptions,
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  DirectConnectClientResolvedConfig
+> {
+  readonly config: DirectConnectClientResolvedConfig;
+
+  constructor(configuration: DirectConnectClientConfig) {
+    let _config_0 = {
+      ...__ClientDefaultValues,
+      ...configuration
+    };
+    let _config_1 = resolveRegionConfig(_config_0);
+    let _config_2 = resolveEndpointsConfig(_config_1);
+    let _config_3 = resolveAwsAuthConfig(_config_2);
+    let _config_4 = resolveRetryConfig(_config_3);
+    let _config_5 = resolveUserAgentConfig(_config_4);
+    let _config_6 = resolveHostHeaderConfig(_config_5);
+    super(_config_6);
+    this.config = _config_6;
+    this.middlewareStack.use(getAwsAuthPlugin(this.config));
+    this.middlewareStack.use(getRetryPlugin(this.config));
+    this.middlewareStack.use(getUserAgentPlugin(this.config));
+    this.middlewareStack.use(getContentLengthPlugin(this.config));
+    this.middlewareStack.use(getHostHeaderPlugin(this.config));
+  }
+
+  destroy(): void {}
+}
---
+++ clients/client-direct-connect/commands/DeleteDirectConnectGatewayAssociationProposalCommand.ts
@@ -0,0 +1,95 @@
+import {
+  DirectConnectClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DirectConnectClient";
+import {
+  DeleteDirectConnectGatewayAssociationProposalRequest,
+  DeleteDirectConnectGatewayAssociationProposalResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1DeleteDirectConnectGatewayAssociationProposalCommand,
+  serializeAws_json1_1DeleteDirectConnectGatewayAssociationProposalCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DeleteDirectConnectGatewayAssociationProposalCommandInput = DeleteDirectConnectGatewayAssociationProposalRequest;
+export type DeleteDirectConnectGatewayAssociationProposalCommandOutput = DeleteDirectConnectGatewayAssociationProposalResult;
+
+export class DeleteDirectConnectGatewayAssociationProposalCommand extends $Command<
+  DeleteDirectConnectGatewayAssociationProposalCommandInput,
+  DeleteDirectConnectGatewayAssociationProposalCommandOutput,
+  DirectConnectClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(
+    readonly input: DeleteDirectConnectGatewayAssociationProposalCommandInput
+  ) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DirectConnectClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DeleteDirectConnectGatewayAssociationProposalCommandInput,
+    DeleteDirectConnectGatewayAssociationProposalCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteDirectConnectGatewayAssociationProposalCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteDirectConnectGatewayAssociationProposalCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteDirectConnectGatewayAssociationProposalCommandOutput> {
+    return deserializeAws_json1_1DeleteDirectConnectGatewayAssociationProposalCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
