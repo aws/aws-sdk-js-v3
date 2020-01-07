75a8d21f00d8d3c4a8d6595fdac39c11e274e96c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:49:33 -0800

    feat: add client-comprehend (#656)

---
+++ clients/client-comprehend/commands/CreateEntityRecognizerCommand.ts
@@ -0,0 +1,87 @@
+import {
+  ComprehendClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ComprehendClient";
+import {
+  CreateEntityRecognizerRequest,
+  CreateEntityRecognizerResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1CreateEntityRecognizerCommand,
+  serializeAws_json1_1CreateEntityRecognizerCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateEntityRecognizerCommandInput = CreateEntityRecognizerRequest;
+export type CreateEntityRecognizerCommandOutput = CreateEntityRecognizerResponse;
+
+export class CreateEntityRecognizerCommand extends $Command<
+  CreateEntityRecognizerCommandInput,
+  CreateEntityRecognizerCommandOutput,
+  ComprehendClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateEntityRecognizerCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ComprehendClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    CreateEntityRecognizerCommandInput,
+    CreateEntityRecognizerCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateEntityRecognizerCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateEntityRecognizerCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateEntityRecognizerCommandOutput> {
+    return deserializeAws_json1_1CreateEntityRecognizerCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-comprehend/commands/ListEndpointsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ComprehendClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ComprehendClient";
+import { ListEndpointsRequest, ListEndpointsResponse } from "../models/index";
+import {
+  deserializeAws_json1_1ListEndpointsCommand,
+  serializeAws_json1_1ListEndpointsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListEndpointsCommandInput = ListEndpointsRequest;
+export type ListEndpointsCommandOutput = ListEndpointsResponse;
+
+export class ListEndpointsCommand extends $Command<
+  ListEndpointsCommandInput,
+  ListEndpointsCommandOutput,
+  ComprehendClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListEndpointsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ComprehendClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListEndpointsCommandInput, ListEndpointsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListEndpointsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListEndpointsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListEndpointsCommandOutput> {
+    return deserializeAws_json1_1ListEndpointsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
