7224af0b670b693b5e4975eeec687910cf3a9e7d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:56 -0800

    feat: add client-migration-hub (#656)

---
+++ clients/client-migration-hub/commands/DescribeMigrationTaskCommand.ts
@@ -0,0 +1,87 @@
+import {
+  MigrationHubClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../MigrationHubClient";
+import {
+  DescribeMigrationTaskRequest,
+  DescribeMigrationTaskResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1DescribeMigrationTaskCommand,
+  serializeAws_json1_1DescribeMigrationTaskCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeMigrationTaskCommandInput = DescribeMigrationTaskRequest;
+export type DescribeMigrationTaskCommandOutput = DescribeMigrationTaskResult;
+
+export class DescribeMigrationTaskCommand extends $Command<
+  DescribeMigrationTaskCommandInput,
+  DescribeMigrationTaskCommandOutput,
+  MigrationHubClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeMigrationTaskCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: MigrationHubClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeMigrationTaskCommandInput,
+    DescribeMigrationTaskCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeMigrationTaskCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeMigrationTaskCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeMigrationTaskCommandOutput> {
+    return deserializeAws_json1_1DescribeMigrationTaskCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
