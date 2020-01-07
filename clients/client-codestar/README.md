851adbbbf34f71c61a21ba2e49b13a3f8767eca7
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:49:13 -0800

    feat: add client-codestar (#656)

---
+++ clients/client-codestar/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-codestar
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-codestar/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-codestar)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-codestar.svg)](https://www.npmjs.com/package/@aws-sdk/client-codestar)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-codestar/commands/CreateUserProfileCommand.ts
@@ -0,0 +1,84 @@
+import {
+  CodeStarClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CodeStarClient";
+import {
+  CreateUserProfileRequest,
+  CreateUserProfileResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1CreateUserProfileCommand,
+  serializeAws_json1_1CreateUserProfileCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateUserProfileCommandInput = CreateUserProfileRequest;
+export type CreateUserProfileCommandOutput = CreateUserProfileResult;
+
+export class CreateUserProfileCommand extends $Command<
+  CreateUserProfileCommandInput,
+  CreateUserProfileCommandOutput,
+  CodeStarClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateUserProfileCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CodeStarClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateUserProfileCommandInput, CreateUserProfileCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateUserProfileCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateUserProfileCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateUserProfileCommandOutput> {
+    return deserializeAws_json1_1CreateUserProfileCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
