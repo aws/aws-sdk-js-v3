7ec0a7251566fd3bb0908143823fff0e25a84d3d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:39:56 -0800

    chore: remove legacy packages (#655)

--- packages/is-iterable/src/index.ts
+++
@@ -1,7 +0,0 @@
-export function isIterable(arg: any): arg is Iterable<any> {
-  return (
-    Boolean(arg) &&
-    typeof Symbol !== "undefined" &&
-    typeof arg[Symbol.iterator] === "function"
-  );
-}
