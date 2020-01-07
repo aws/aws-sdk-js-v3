937ccc21dc2b7c51310c06ffce07009098b391c8
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 17:13:40 -0800

    feat: add client-compute-optimizer (#658)

---
+++ clients/client-compute-optimizer/commands/GetEnrollmentStatusCommand.ts
@@ -0,0 +1,87 @@
+import {
+  ComputeOptimizerClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ComputeOptimizerClient";
+import {
+  GetEnrollmentStatusRequest,
+  GetEnrollmentStatusResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_0GetEnrollmentStatusCommand,
+  serializeAws_json1_0GetEnrollmentStatusCommand
+} from "../protocols/Aws_json1_0";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetEnrollmentStatusCommandInput = GetEnrollmentStatusRequest;
+export type GetEnrollmentStatusCommandOutput = GetEnrollmentStatusResponse;
+
+export class GetEnrollmentStatusCommand extends $Command<
+  GetEnrollmentStatusCommandInput,
+  GetEnrollmentStatusCommandOutput,
+  ComputeOptimizerClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetEnrollmentStatusCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ComputeOptimizerClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    GetEnrollmentStatusCommandInput,
+    GetEnrollmentStatusCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetEnrollmentStatusCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_0GetEnrollmentStatusCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetEnrollmentStatusCommandOutput> {
+    return deserializeAws_json1_0GetEnrollmentStatusCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
