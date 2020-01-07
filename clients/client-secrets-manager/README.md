04a1bc68f49e2ba80a27f332e174d420d01cd012
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:55:27 -0800

    feat: add client-secrets-manager (#656)

---
+++ clients/client-secrets-manager/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-secrets-manager
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-secrets-manager/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-secrets-manager)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-secrets-manager.svg)](https://www.npmjs.com/package/@aws-sdk/client-secrets-manager)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-secrets-manager/commands/ListSecretsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  SecretsManagerClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SecretsManagerClient";
+import { ListSecretsRequest, ListSecretsResponse } from "../models/index";
+import {
+  deserializeAws_json1_1ListSecretsCommand,
+  serializeAws_json1_1ListSecretsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListSecretsCommandInput = ListSecretsRequest;
+export type ListSecretsCommandOutput = ListSecretsResponse;
+
+export class ListSecretsCommand extends $Command<
+  ListSecretsCommandInput,
+  ListSecretsCommandOutput,
+  SecretsManagerClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListSecretsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SecretsManagerClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListSecretsCommandInput, ListSecretsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListSecretsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListSecretsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListSecretsCommandOutput> {
+    return deserializeAws_json1_1ListSecretsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
