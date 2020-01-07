93ce0b36312322de1f4418d5308c58bbf55f9283
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:03 -0800

    feat: add client-kinesis-analytics-v2 (#656)

---
+++ clients/client-kinesis-analytics-v2/commands/AddApplicationCloudWatchLoggingOptionCommand.ts
@@ -0,0 +1,95 @@
+import {
+  KinesisAnalyticsV2ClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../KinesisAnalyticsV2Client";
+import {
+  AddApplicationCloudWatchLoggingOptionRequest,
+  AddApplicationCloudWatchLoggingOptionResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1AddApplicationCloudWatchLoggingOptionCommand,
+  serializeAws_json1_1AddApplicationCloudWatchLoggingOptionCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type AddApplicationCloudWatchLoggingOptionCommandInput = AddApplicationCloudWatchLoggingOptionRequest;
+export type AddApplicationCloudWatchLoggingOptionCommandOutput = AddApplicationCloudWatchLoggingOptionResponse;
+
+export class AddApplicationCloudWatchLoggingOptionCommand extends $Command<
+  AddApplicationCloudWatchLoggingOptionCommandInput,
+  AddApplicationCloudWatchLoggingOptionCommandOutput,
+  KinesisAnalyticsV2ClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(
+    readonly input: AddApplicationCloudWatchLoggingOptionCommandInput
+  ) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: KinesisAnalyticsV2ClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    AddApplicationCloudWatchLoggingOptionCommandInput,
+    AddApplicationCloudWatchLoggingOptionCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AddApplicationCloudWatchLoggingOptionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AddApplicationCloudWatchLoggingOptionCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AddApplicationCloudWatchLoggingOptionCommandOutput> {
+    return deserializeAws_json1_1AddApplicationCloudWatchLoggingOptionCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
