ae565fc15571c58e725a3227dadbae84eee79480
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:42 -0800

    feat: add client-cloudwatch-logs (#656)

---
+++ clients/client-cloudwatch-logs/commands/AssociateKmsKeyCommand.ts
@@ -0,0 +1,82 @@
+import {
+  CloudWatchLogsClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudWatchLogsClient";
+import { AssociateKmsKeyRequest } from "../models/index";
+import {
+  deserializeAws_json1_1AssociateKmsKeyCommand,
+  serializeAws_json1_1AssociateKmsKeyCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type AssociateKmsKeyCommandInput = AssociateKmsKeyRequest;
+export type AssociateKmsKeyCommandOutput = __MetadataBearer;
+
+export class AssociateKmsKeyCommand extends $Command<
+  AssociateKmsKeyCommandInput,
+  AssociateKmsKeyCommandOutput,
+  CloudWatchLogsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: AssociateKmsKeyCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudWatchLogsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<AssociateKmsKeyCommandInput, AssociateKmsKeyCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AssociateKmsKeyCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AssociateKmsKeyCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AssociateKmsKeyCommandOutput> {
+    return deserializeAws_json1_1AssociateKmsKeyCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
