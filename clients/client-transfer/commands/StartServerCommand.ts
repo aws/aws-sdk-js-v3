49cf9cce22fcb89d023c3e5df4f791741fdd3fa5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:56:27 -0800

    feat: add client-transfer (#656)

---
+++ clients/client-transfer/commands/StartServerCommand.ts
@@ -0,0 +1,82 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  TransferClientResolvedConfig
+} from "../TransferClient";
+import { StartServerRequest } from "../models/index";
+import {
+  deserializeAws_json1_1StartServerCommand,
+  serializeAws_json1_1StartServerCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type StartServerCommandInput = StartServerRequest;
+export type StartServerCommandOutput = __MetadataBearer;
+
+export class StartServerCommand extends $Command<
+  StartServerCommandInput,
+  StartServerCommandOutput,
+  TransferClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: StartServerCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: TransferClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<StartServerCommandInput, StartServerCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: StartServerCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1StartServerCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<StartServerCommandOutput> {
+    return deserializeAws_json1_1StartServerCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
