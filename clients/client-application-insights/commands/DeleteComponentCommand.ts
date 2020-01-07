e9a13dc1407037eb893e64f753df5dd74abf9e8b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:48:01 -0800

    feat: add client-application-insights (#656)

---
+++ clients/client-application-insights/commands/DeleteComponentCommand.ts
@@ -0,0 +1,84 @@
+import {
+  ApplicationInsightsClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ApplicationInsightsClient";
+import {
+  DeleteComponentRequest,
+  DeleteComponentResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1DeleteComponentCommand,
+  serializeAws_json1_1DeleteComponentCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DeleteComponentCommandInput = DeleteComponentRequest;
+export type DeleteComponentCommandOutput = DeleteComponentResponse;
+
+export class DeleteComponentCommand extends $Command<
+  DeleteComponentCommandInput,
+  DeleteComponentCommandOutput,
+  ApplicationInsightsClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteComponentCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ApplicationInsightsClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DeleteComponentCommandInput, DeleteComponentCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteComponentCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DeleteComponentCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteComponentCommandOutput> {
+    return deserializeAws_json1_1DeleteComponentCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
