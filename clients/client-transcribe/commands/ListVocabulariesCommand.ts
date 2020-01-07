b812c59c0ab54391ac65d8d61b36014a40ac9d81
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:56:24 -0800

    feat: add client-transcribe (#656)

---
+++ clients/client-transcribe/commands/ListVocabulariesCommand.ts
@@ -0,0 +1,84 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  TranscribeClientResolvedConfig
+} from "../TranscribeClient";
+import {
+  ListVocabulariesRequest,
+  ListVocabulariesResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1ListVocabulariesCommand,
+  serializeAws_json1_1ListVocabulariesCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListVocabulariesCommandInput = ListVocabulariesRequest;
+export type ListVocabulariesCommandOutput = ListVocabulariesResponse;
+
+export class ListVocabulariesCommand extends $Command<
+  ListVocabulariesCommandInput,
+  ListVocabulariesCommandOutput,
+  TranscribeClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListVocabulariesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: TranscribeClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListVocabulariesCommandInput, ListVocabulariesCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListVocabulariesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListVocabulariesCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListVocabulariesCommandOutput> {
+    return deserializeAws_json1_1ListVocabulariesCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
