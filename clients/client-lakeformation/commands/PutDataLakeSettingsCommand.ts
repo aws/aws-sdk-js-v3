693019b18e479a8f1954f2c36539c3d0dd454259
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:53:14 -0800

    feat: add client-lakeformation (#656)

---
+++ clients/client-lakeformation/commands/PutDataLakeSettingsCommand.ts
@@ -0,0 +1,87 @@
+import {
+  LakeFormationClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../LakeFormationClient";
+import {
+  PutDataLakeSettingsRequest,
+  PutDataLakeSettingsResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1PutDataLakeSettingsCommand,
+  serializeAws_json1_1PutDataLakeSettingsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type PutDataLakeSettingsCommandInput = PutDataLakeSettingsRequest;
+export type PutDataLakeSettingsCommandOutput = PutDataLakeSettingsResponse;
+
+export class PutDataLakeSettingsCommand extends $Command<
+  PutDataLakeSettingsCommandInput,
+  PutDataLakeSettingsCommandOutput,
+  LakeFormationClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: PutDataLakeSettingsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: LakeFormationClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    PutDataLakeSettingsCommandInput,
+    PutDataLakeSettingsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: PutDataLakeSettingsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1PutDataLakeSettingsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<PutDataLakeSettingsCommandOutput> {
+    return deserializeAws_json1_1PutDataLakeSettingsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
