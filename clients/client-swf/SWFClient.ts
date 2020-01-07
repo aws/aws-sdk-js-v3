b36d4963c5151e2108e7954c4fc912ddeff9472d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 17:14:00 -0800

    feat: add client-swf (#658)

---
+++ clients/client-swf/SWFClient.ts
@@ -0,0 +1,320 @@
+import {
+  ActivityTask,
+  ActivityTaskStatus,
+  ActivityTypeDetail,
+  ActivityTypeInfos,
+  CountClosedWorkflowExecutionsInput,
+  CountOpenWorkflowExecutionsInput,
+  CountPendingActivityTasksInput,
+  CountPendingDecisionTasksInput,
+  DecisionTask,
+  DeprecateActivityTypeInput,
+  DeprecateDomainInput,
+  DeprecateWorkflowTypeInput,
+  DescribeActivityTypeInput,
+  DescribeDomainInput,
+  DescribeWorkflowExecutionInput,
+  DescribeWorkflowTypeInput,
+  DomainDetail,
+  DomainInfos,
+  GetWorkflowExecutionHistoryInput,
+  History,
+  ListActivityTypesInput,
+  ListClosedWorkflowExecutionsInput,
+  ListDomainsInput,
+  ListOpenWorkflowExecutionsInput,
+  ListTagsForResourceInput,
+  ListTagsForResourceOutput,
+  ListWorkflowTypesInput,
+  PendingTaskCount,
+  PollForActivityTaskInput,
+  PollForDecisionTaskInput,
+  RecordActivityTaskHeartbeatInput,
+  RegisterActivityTypeInput,
+  RegisterDomainInput,
+  RegisterWorkflowTypeInput,
+  RequestCancelWorkflowExecutionInput,
+  RespondActivityTaskCanceledInput,
+  RespondActivityTaskCompletedInput,
+  RespondActivityTaskFailedInput,
+  RespondDecisionTaskCompletedInput,
+  Run,
+  SignalWorkflowExecutionInput,
+  StartWorkflowExecutionInput,
+  TagResourceInput,
+  TerminateWorkflowExecutionInput,
+  UndeprecateActivityTypeInput,
+  UndeprecateDomainInput,
+  UndeprecateWorkflowTypeInput,
+  UntagResourceInput,
+  WorkflowExecutionCount,
+  WorkflowExecutionDetail,
+  WorkflowExecutionInfos,
+  WorkflowTypeDetail,
+  WorkflowTypeInfos
+} from "./models/index";
+import { ClientDefaultValues as __ClientDefaultValues } from "./runtimeConfig";
+import {
+  EndpointsInputConfig,
+  EndpointsResolvedConfig,
+  RegionInputConfig,
+  RegionResolvedConfig,
+  resolveEndpointsConfig,
+  resolveRegionConfig
+} from "@aws-sdk/config-resolver";
+import { getContentLengthPlugin } from "@aws-sdk/middleware-content-length";
+import {
+  HostHeaderInputConfig,
+  HostHeaderResolvedConfig,
+  getHostHeaderPlugin,
+  resolveHostHeaderConfig
+} from "@aws-sdk/middleware-host-header";
+import {
+  RetryInputConfig,
+  RetryResolvedConfig,
+  getRetryPlugin,
+  resolveRetryConfig
+} from "@aws-sdk/middleware-retry";
+import {
+  AwsAuthInputConfig,
+  AwsAuthResolvedConfig,
+  getAwsAuthPlugin,
+  resolveAwsAuthConfig
+} from "@aws-sdk/middleware-signing";
+import {
+  UserAgentInputConfig,
+  UserAgentResolvedConfig,
+  getUserAgentPlugin,
+  resolveUserAgentConfig
+} from "@aws-sdk/middleware-user-agent";
+import { HttpHandler as __HttpHandler } from "@aws-sdk/protocol-http";
+import {
+  Client as __Client,
+  SmithyConfiguration as __SmithyConfiguration,
+  SmithyResolvedConfiguration as __SmithyResolvedConfiguration
+} from "@aws-sdk/smithy-client";
+import {
+  RegionInfoProvider,
+  Credentials as __Credentials,
+  Decoder as __Decoder,
+  Encoder as __Encoder,
+  HashConstructor as __HashConstructor,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer,
+  Provider as __Provider,
+  StreamCollector as __StreamCollector,
+  UrlParser as __UrlParser
+} from "@aws-sdk/types";
+
+export type ServiceInputTypes =
+  | CountClosedWorkflowExecutionsInput
+  | CountOpenWorkflowExecutionsInput
+  | CountPendingActivityTasksInput
+  | CountPendingDecisionTasksInput
+  | DeprecateActivityTypeInput
+  | DeprecateDomainInput
+  | DeprecateWorkflowTypeInput
+  | DescribeActivityTypeInput
+  | DescribeDomainInput
+  | DescribeWorkflowExecutionInput
+  | DescribeWorkflowTypeInput
+  | GetWorkflowExecutionHistoryInput
+  | ListActivityTypesInput
+  | ListClosedWorkflowExecutionsInput
+  | ListDomainsInput
+  | ListOpenWorkflowExecutionsInput
+  | ListTagsForResourceInput
+  | ListWorkflowTypesInput
+  | PollForActivityTaskInput
+  | PollForDecisionTaskInput
+  | RecordActivityTaskHeartbeatInput
+  | RegisterActivityTypeInput
+  | RegisterDomainInput
+  | RegisterWorkflowTypeInput
+  | RequestCancelWorkflowExecutionInput
+  | RespondActivityTaskCanceledInput
+  | RespondActivityTaskCompletedInput
+  | RespondActivityTaskFailedInput
+  | RespondDecisionTaskCompletedInput
+  | SignalWorkflowExecutionInput
+  | StartWorkflowExecutionInput
+  | TagResourceInput
+  | TerminateWorkflowExecutionInput
+  | UndeprecateActivityTypeInput
+  | UndeprecateDomainInput
+  | UndeprecateWorkflowTypeInput
+  | UntagResourceInput;
+
+export type ServiceOutputTypes =
+  | __MetadataBearer
+  | ActivityTask
+  | ActivityTaskStatus
+  | ActivityTypeDetail
+  | ActivityTypeInfos
+  | DecisionTask
+  | DomainDetail
+  | DomainInfos
+  | History
+  | ListTagsForResourceOutput
+  | PendingTaskCount
+  | PendingTaskCount
+  | Run
+  | WorkflowExecutionCount
+  | WorkflowExecutionCount
+  | WorkflowExecutionDetail
+  | WorkflowExecutionInfos
+  | WorkflowExecutionInfos
+  | WorkflowTypeDetail
+  | WorkflowTypeInfos;
+
+export interface ClientDefaults
+  extends Partial<__SmithyResolvedConfiguration<__HttpHandlerOptions>> {
+  /**
+   * The HTTP handler to use. Fetch in browser and Https in Nodejs.
+   */
+  requestHandler?: __HttpHandler;
+
+  /**
+   * A constructor for a class implementing the @aws-sdk/types.Hash interface
+   * that computes the SHA-256 HMAC or checksum of a string or binary buffer.
+   */
+  sha256?: __HashConstructor;
+
+  /**
+   * The function that will be used to convert strings into HTTP endpoints.
+   */
+  urlParser?: __UrlParser;
+
+  /**
+   * A function that can calculate the length of a request body.
+   */
+  bodyLengthChecker?: (body: any) => number | undefined;
+
+  /**
+   * A function that converts a stream into an array of bytes.
+   */
+  streamCollector?: __StreamCollector;
+
+  /**
+   * The function that will be used to convert a base64-encoded string to a byte array
+   */
+  base64Decoder?: __Decoder;
+
+  /**
+   * The function that will be used to convert binary data to a base64-encoded string
+   */
+  base64Encoder?: __Encoder;
+
+  /**
+   * The function that will be used to convert a UTF8-encoded string to a byte array
+   */
+  utf8Decoder?: __Decoder;
+
+  /**
+   * The function that will be used to convert binary data to a UTF-8 encoded string
+   */
+  utf8Encoder?: __Encoder;
+
+  /**
+   * The string that will be used to populate default value in 'User-Agent' header
+   */
+  defaultUserAgent?: string;
+
+  /**
+   * The runtime environment
+   */
+  runtime?: string;
+
+  /**
+   * The service name with which to sign requests.
+   */
+  signingName?: string;
+
+  /**
+   * Default credentials provider; Not available in browser runtime
+   */
+  credentialDefaultProvider?: (input: any) => __Provider<__Credentials>;
+
+  /**
+   * Provider function that return promise of a region string
+   */
+  regionDefaultProvider?: (input: any) => __Provider<string>;
+
+  /**
+   * Fetch related hostname, signing name or signing region with given region.
+   */
+  regionInfoProvider?: RegionInfoProvider;
+}
+
+export type SWFClientConfig = Partial<
+  __SmithyConfiguration<__HttpHandlerOptions>
+> &
+  ClientDefaults &
+  RegionInputConfig &
+  EndpointsInputConfig &
+  AwsAuthInputConfig &
+  RetryInputConfig &
+  UserAgentInputConfig &
+  HostHeaderInputConfig;
+
+export type SWFClientResolvedConfig = __SmithyResolvedConfiguration<
+  __HttpHandlerOptions
+> &
+  Required<ClientDefaults> &
+  RegionResolvedConfig &
+  EndpointsResolvedConfig &
+  AwsAuthResolvedConfig &
+  RetryResolvedConfig &
+  UserAgentResolvedConfig &
+  HostHeaderResolvedConfig;
+
+/**
+ *
+ *          <fullname>Amazon Simple Workflow Service</fullname>
+ *
+ *          <p>The Amazon Simple Workflow Service (Amazon SWF) makes it easy to build applications that use Amazon's cloud to
+ *       coordinate work across distributed components. In Amazon SWF, a <i>task</i>
+ *       represents a logical unit of work that is performed by a component of your workflow.
+ *       Coordinating tasks in a workflow involves managing intertask dependencies, scheduling, and
+ *       concurrency in accordance with the logical flow of the application.</p>
+ *
+ *          <p>Amazon SWF gives you full control over implementing tasks and coordinating them without
+ *       worrying about underlying complexities such as tracking their progress and maintaining their
+ *       state.</p>
+ *
+ *          <p>This documentation serves as reference only. For a broader overview of the Amazon SWF
+ *       programming model, see the <i>
+ *                <a href="https://docs.aws.amazon.com/amazonswf/latest/developerguide/">Amazon SWF Developer Guide</a>
+ *             </i>.</p>
+ *
+ */
+export class SWFClient extends __Client<
+  __HttpHandlerOptions,
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  SWFClientResolvedConfig
+> {
+  readonly config: SWFClientResolvedConfig;
+
+  constructor(configuration: SWFClientConfig) {
+    let _config_0 = {
+      ...__ClientDefaultValues,
+      ...configuration
+    };
+    let _config_1 = resolveRegionConfig(_config_0);
+    let _config_2 = resolveEndpointsConfig(_config_1);
+    let _config_3 = resolveAwsAuthConfig(_config_2);
+    let _config_4 = resolveRetryConfig(_config_3);
+    let _config_5 = resolveUserAgentConfig(_config_4);
+    let _config_6 = resolveHostHeaderConfig(_config_5);
+    super(_config_6);
+    this.config = _config_6;
+    this.middlewareStack.use(getAwsAuthPlugin(this.config));
+    this.middlewareStack.use(getRetryPlugin(this.config));
+    this.middlewareStack.use(getUserAgentPlugin(this.config));
+    this.middlewareStack.use(getContentLengthPlugin(this.config));
+    this.middlewareStack.use(getHostHeaderPlugin(this.config));
+  }
+
+  destroy(): void {}
+}
---
+++ clients/client-swf/commands/CountPendingDecisionTasksCommand.ts
@@ -0,0 +1,90 @@
+import {
+  SWFClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SWFClient";
+import {
+  CountPendingDecisionTasksInput,
+  PendingTaskCount
+} from "../models/index";
+import {
+  deserializeAws_json1_0CountPendingDecisionTasksCommand,
+  serializeAws_json1_0CountPendingDecisionTasksCommand
+} from "../protocols/Aws_json1_0";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CountPendingDecisionTasksCommandInput = CountPendingDecisionTasksInput;
+export type CountPendingDecisionTasksCommandOutput = PendingTaskCount;
+
+export class CountPendingDecisionTasksCommand extends $Command<
+  CountPendingDecisionTasksCommandInput,
+  CountPendingDecisionTasksCommandOutput,
+  SWFClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CountPendingDecisionTasksCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SWFClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    CountPendingDecisionTasksCommandInput,
+    CountPendingDecisionTasksCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CountPendingDecisionTasksCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_0CountPendingDecisionTasksCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CountPendingDecisionTasksCommandOutput> {
+    return deserializeAws_json1_0CountPendingDecisionTasksCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
