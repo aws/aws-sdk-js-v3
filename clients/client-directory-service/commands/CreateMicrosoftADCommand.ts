263a969330a0dbe93ad8cf0e6f6bf620b3f497ac
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:50:46 -0800

    feat: add client-directory-service (#656)

---
+++ clients/client-directory-service/commands/CreateMicrosoftADCommand.ts
@@ -0,0 +1,84 @@
+import {
+  DirectoryServiceClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DirectoryServiceClient";
+import {
+  CreateMicrosoftADRequest,
+  CreateMicrosoftADResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1CreateMicrosoftADCommand,
+  serializeAws_json1_1CreateMicrosoftADCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateMicrosoftADCommandInput = CreateMicrosoftADRequest;
+export type CreateMicrosoftADCommandOutput = CreateMicrosoftADResult;
+
+export class CreateMicrosoftADCommand extends $Command<
+  CreateMicrosoftADCommandInput,
+  CreateMicrosoftADCommandOutput,
+  DirectoryServiceClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateMicrosoftADCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DirectoryServiceClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateMicrosoftADCommandInput, CreateMicrosoftADCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateMicrosoftADCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateMicrosoftADCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateMicrosoftADCommandOutput> {
+    return deserializeAws_json1_1CreateMicrosoftADCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-directory-service/commands/UpdateNumberOfDomainControllersCommand.ts
@@ -0,0 +1,93 @@
+import {
+  DirectoryServiceClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DirectoryServiceClient";
+import {
+  UpdateNumberOfDomainControllersRequest,
+  UpdateNumberOfDomainControllersResult
+} from "../models/index";
+import {
+  deserializeAws_json1_1UpdateNumberOfDomainControllersCommand,
+  serializeAws_json1_1UpdateNumberOfDomainControllersCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateNumberOfDomainControllersCommandInput = UpdateNumberOfDomainControllersRequest;
+export type UpdateNumberOfDomainControllersCommandOutput = UpdateNumberOfDomainControllersResult;
+
+export class UpdateNumberOfDomainControllersCommand extends $Command<
+  UpdateNumberOfDomainControllersCommandInput,
+  UpdateNumberOfDomainControllersCommandOutput,
+  DirectoryServiceClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateNumberOfDomainControllersCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DirectoryServiceClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateNumberOfDomainControllersCommandInput,
+    UpdateNumberOfDomainControllersCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateNumberOfDomainControllersCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1UpdateNumberOfDomainControllersCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateNumberOfDomainControllersCommandOutput> {
+    return deserializeAws_json1_1UpdateNumberOfDomainControllersCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
