bb911af2484d3c43c8bdaeee08d7b655e3481086
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:51:19 -0800

    feat: add client-eventbridge (#656)

---
+++ clients/client-eventbridge/commands/ListEventBusesCommand.ts
@@ -0,0 +1,81 @@
+import {
+  EventBridgeClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../EventBridgeClient";
+import { ListEventBusesRequest, ListEventBusesResponse } from "../models/index";
+import {
+  deserializeAws_json1_1ListEventBusesCommand,
+  serializeAws_json1_1ListEventBusesCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListEventBusesCommandInput = ListEventBusesRequest;
+export type ListEventBusesCommandOutput = ListEventBusesResponse;
+
+export class ListEventBusesCommand extends $Command<
+  ListEventBusesCommandInput,
+  ListEventBusesCommandOutput,
+  EventBridgeClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListEventBusesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: EventBridgeClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListEventBusesCommandInput, ListEventBusesCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListEventBusesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListEventBusesCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListEventBusesCommandOutput> {
+    return deserializeAws_json1_1ListEventBusesCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-eventbridge/commands/ListPartnerEventSourceAccountsCommand.ts
@@ -0,0 +1,93 @@
+import {
+  EventBridgeClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../EventBridgeClient";
+import {
+  ListPartnerEventSourceAccountsRequest,
+  ListPartnerEventSourceAccountsResponse
+} from "../models/index";
+import {
+  deserializeAws_json1_1ListPartnerEventSourceAccountsCommand,
+  serializeAws_json1_1ListPartnerEventSourceAccountsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListPartnerEventSourceAccountsCommandInput = ListPartnerEventSourceAccountsRequest;
+export type ListPartnerEventSourceAccountsCommandOutput = ListPartnerEventSourceAccountsResponse;
+
+export class ListPartnerEventSourceAccountsCommand extends $Command<
+  ListPartnerEventSourceAccountsCommandInput,
+  ListPartnerEventSourceAccountsCommandOutput,
+  EventBridgeClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListPartnerEventSourceAccountsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: EventBridgeClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListPartnerEventSourceAccountsCommandInput,
+    ListPartnerEventSourceAccountsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListPartnerEventSourceAccountsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ListPartnerEventSourceAccountsCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListPartnerEventSourceAccountsCommandOutput> {
+    return deserializeAws_json1_1ListPartnerEventSourceAccountsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
