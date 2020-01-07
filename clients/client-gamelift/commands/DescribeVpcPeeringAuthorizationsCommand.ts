cab9fddbb2f7beb4aa00ab58d226a6f0322d2004
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:52:04 -0800

    feat: add client-gamelift (#656)

---
+++ clients/client-gamelift/commands/DescribeVpcPeeringAuthorizationsCommand.ts
@@ -0,0 +1,93 @@
+import {
+  GameLiftClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../GameLiftClient";
+import {
+  DescribeVpcPeeringAuthorizationsInput,
+  DescribeVpcPeeringAuthorizationsOutput
+} from "../models/index";
+import {
+  deserializeAws_json1_1DescribeVpcPeeringAuthorizationsCommand,
+  serializeAws_json1_1DescribeVpcPeeringAuthorizationsCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeVpcPeeringAuthorizationsCommandInput = DescribeVpcPeeringAuthorizationsInput;
+export type DescribeVpcPeeringAuthorizationsCommandOutput = DescribeVpcPeeringAuthorizationsOutput;
+
+export class DescribeVpcPeeringAuthorizationsCommand extends $Command<
+  DescribeVpcPeeringAuthorizationsCommandInput,
+  DescribeVpcPeeringAuthorizationsCommandOutput,
+  GameLiftClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeVpcPeeringAuthorizationsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: GameLiftClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeVpcPeeringAuthorizationsCommandInput,
+    DescribeVpcPeeringAuthorizationsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeVpcPeeringAuthorizationsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1DescribeVpcPeeringAuthorizationsCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeVpcPeeringAuthorizationsCommandOutput> {
+    return deserializeAws_json1_1DescribeVpcPeeringAuthorizationsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
