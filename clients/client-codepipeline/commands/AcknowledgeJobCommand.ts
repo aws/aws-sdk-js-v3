9ef6a65ee7f0aeacdb21b9475ef7959e017aeb74
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-06 16:49:06 -0800

    feat: add client-codepipeline (#656)

---
+++ clients/client-codepipeline/commands/AcknowledgeJobCommand.ts
@@ -0,0 +1,81 @@
+import {
+  CodePipelineClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CodePipelineClient";
+import { AcknowledgeJobInput, AcknowledgeJobOutput } from "../models/index";
+import {
+  deserializeAws_json1_1AcknowledgeJobCommand,
+  serializeAws_json1_1AcknowledgeJobCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type AcknowledgeJobCommandInput = AcknowledgeJobInput;
+export type AcknowledgeJobCommandOutput = AcknowledgeJobOutput;
+
+export class AcknowledgeJobCommand extends $Command<
+  AcknowledgeJobCommandInput,
+  AcknowledgeJobCommandOutput,
+  CodePipelineClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: AcknowledgeJobCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CodePipelineClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<AcknowledgeJobCommandInput, AcknowledgeJobCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: AcknowledgeJobCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1AcknowledgeJobCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<AcknowledgeJobCommandOutput> {
+    return deserializeAws_json1_1AcknowledgeJobCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
