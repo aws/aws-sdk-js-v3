69883089dcca81a096a5a6e47b8be4b45876e03b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-09 18:07:19 -0800

    fix: prettify CODE_GEN_OUTPUT_DIR to improve performance (#708)

--- scripts/generate-clients/code-prettify.js
+++ scripts/generate-clients/code-prettify.js
@@ -1,21 +1,11 @@
-const path = require("path");
-const { readdirSync } = require("fs");
 const { spawnProcess } = require("./spawn-process");
 const { CODE_GEN_OUTPUT_DIR } = require("./code-gen-dir");
 
 const prettifyCode = async () => {
-  for (const modelName of readdirSync(CODE_GEN_OUTPUT_DIR)) {
-    if (modelName === "source") continue;
-    const artifactPath = path.join(
-      CODE_GEN_OUTPUT_DIR,
-      modelName,
-      "typescript-codegen"
-    );
-    await spawnProcess("./node_modules/.bin/prettier", [
-      "--write",
-      `${artifactPath}/**/*.{ts,md,json}`
-    ]);
-  }
+  await spawnProcess("./node_modules/.bin/prettier", [
+    "--write",
+    `${CODE_GEN_OUTPUT_DIR}/**/*.{ts,js,md,json}`
+  ]);
 };
 
 module.exports = {
