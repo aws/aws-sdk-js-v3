021bf27fb1f838438de97b04d3159407343770c4
Allan Zheng <zheallan@amazon.com>
2020-01-14 13:08:19 -0800

    chore: run karma test separately; nohoist karma

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,13 @@ module.exports = {
   projects: ["<rootDir>/packages/*/jest.config.js"],
   testPathIgnorePatterns: [
     "<rootDir>/packages/add-glacier-checksum-headers-browser",
+    "<rootDir>/packages/stream-collector-browser",
     "<rootDir>/clients/client-.*"
   ],
   coveragePathIgnorePatterns: [
     "/node_modules/",
     "<rootDir>/packages/add-glacier-checksum-headers-browser",
-    "<rootDir>/packages/crypto-sjcl-*",
-    "<rootDir>/packages/xml-body-parser/vendor/",
+    "<rootDir>/packages/stream-collector-browser",
     "<rootDir>/clients/client-.*",
     "/__fixtures__/"
   ]
