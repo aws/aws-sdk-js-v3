fa1cbc18e93e50b16e03fffa13154e9a957241be
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-14 15:16:47 -0800

    feat: add client-s3-control (#729)

---
+++ clients/client-s3-control/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2018-08-20",
+  signingName: "s3",
+  regionInfoProvider: defaultRegionInfoProvider
+};
