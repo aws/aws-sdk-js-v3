ecc399e380963869a770c0ce00652db92150fd1d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:38:19 +0000

    chore: add client-cloudsearch

---
+++ clients/client-cloudsearch/commands/DescribeServiceAccessPoliciesCommand.ts
@@ -0,0 +1,93 @@
+import {
+  CloudSearchClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudSearchClient";
+import {
+  DescribeServiceAccessPoliciesRequest,
+  DescribeServiceAccessPoliciesResponse
+} from "../models/index";
+import {
+  deserializeAws_queryDescribeServiceAccessPoliciesCommand,
+  serializeAws_queryDescribeServiceAccessPoliciesCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeServiceAccessPoliciesCommandInput = DescribeServiceAccessPoliciesRequest;
+export type DescribeServiceAccessPoliciesCommandOutput = DescribeServiceAccessPoliciesResponse;
+
+export class DescribeServiceAccessPoliciesCommand extends $Command<
+  DescribeServiceAccessPoliciesCommandInput,
+  DescribeServiceAccessPoliciesCommandOutput,
+  CloudSearchClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeServiceAccessPoliciesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudSearchClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeServiceAccessPoliciesCommandInput,
+    DescribeServiceAccessPoliciesCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeServiceAccessPoliciesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryDescribeServiceAccessPoliciesCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeServiceAccessPoliciesCommandOutput> {
+    return deserializeAws_queryDescribeServiceAccessPoliciesCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
