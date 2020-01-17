f3e5cf37b27ba7faff583f4136c526214d16e5c3
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:40:33 +0000

    chore: add client-sns

---
+++ clients/client-sns/commands/ListPlatformApplicationsCommand.ts
@@ -0,0 +1,87 @@
+import {
+  SNSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SNSClient";
+import {
+  ListPlatformApplicationsInput,
+  ListPlatformApplicationsResponse
+} from "../models/index";
+import {
+  deserializeAws_queryListPlatformApplicationsCommand,
+  serializeAws_queryListPlatformApplicationsCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListPlatformApplicationsCommandInput = ListPlatformApplicationsInput;
+export type ListPlatformApplicationsCommandOutput = ListPlatformApplicationsResponse;
+
+export class ListPlatformApplicationsCommand extends $Command<
+  ListPlatformApplicationsCommandInput,
+  ListPlatformApplicationsCommandOutput,
+  SNSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListPlatformApplicationsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SNSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListPlatformApplicationsCommandInput,
+    ListPlatformApplicationsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListPlatformApplicationsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryListPlatformApplicationsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListPlatformApplicationsCommandOutput> {
+    return deserializeAws_queryListPlatformApplicationsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
