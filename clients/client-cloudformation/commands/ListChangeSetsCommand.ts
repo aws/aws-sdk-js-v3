0db76c6cd6dcad6a22b9bdae99c659f6b65f0078
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:38:07 +0000

    chore: add client-cloudformation

---
+++ clients/client-cloudformation/commands/ListChangeSetsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  CloudFormationClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudFormationClient";
+import { ListChangeSetsInput, ListChangeSetsOutput } from "../models/index";
+import {
+  deserializeAws_queryListChangeSetsCommand,
+  serializeAws_queryListChangeSetsCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListChangeSetsCommandInput = ListChangeSetsInput;
+export type ListChangeSetsCommandOutput = ListChangeSetsOutput;
+
+export class ListChangeSetsCommand extends $Command<
+  ListChangeSetsCommandInput,
+  ListChangeSetsCommandOutput,
+  CloudFormationClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListChangeSetsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudFormationClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListChangeSetsCommandInput, ListChangeSetsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListChangeSetsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryListChangeSetsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListChangeSetsCommandOutput> {
+    return deserializeAws_queryListChangeSetsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-cloudformation/commands/ListExportsCommand.ts
@@ -0,0 +1,81 @@
+import {
+  CloudFormationClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudFormationClient";
+import { ListExportsInput, ListExportsOutput } from "../models/index";
+import {
+  deserializeAws_queryListExportsCommand,
+  serializeAws_queryListExportsCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListExportsCommandInput = ListExportsInput;
+export type ListExportsCommandOutput = ListExportsOutput;
+
+export class ListExportsCommand extends $Command<
+  ListExportsCommandInput,
+  ListExportsCommandOutput,
+  CloudFormationClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListExportsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudFormationClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListExportsCommandInput, ListExportsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListExportsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryListExportsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListExportsCommandOutput> {
+    return deserializeAws_queryListExportsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
