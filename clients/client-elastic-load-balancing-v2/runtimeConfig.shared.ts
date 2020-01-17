73162dcd888f585b945d63afb8474ca787afbad4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:39:05 +0000

    chore: add client-elastic-load-balancing-v2

---
+++ clients/client-elastic-load-balancing-v2/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2015-12-01",
+  signingName: "elasticloadbalancing",
+  regionInfoProvider: defaultRegionInfoProvider
+};
