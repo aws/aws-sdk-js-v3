6858e0c490279cfac28d5d14593491cfb54883d5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:37:52 +0000

    chore: add client-auto-scaling

---
+++ clients/client-auto-scaling/commands/CreateLaunchConfigurationCommand.ts
@@ -0,0 +1,88 @@
+import {
+  AutoScalingClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../AutoScalingClient";
+import { CreateLaunchConfigurationType } from "../models/index";
+import {
+  deserializeAws_queryCreateLaunchConfigurationCommand,
+  serializeAws_queryCreateLaunchConfigurationCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type CreateLaunchConfigurationCommandInput = CreateLaunchConfigurationType;
+export type CreateLaunchConfigurationCommandOutput = __MetadataBearer;
+
+export class CreateLaunchConfigurationCommand extends $Command<
+  CreateLaunchConfigurationCommandInput,
+  CreateLaunchConfigurationCommandOutput,
+  AutoScalingClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateLaunchConfigurationCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: AutoScalingClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    CreateLaunchConfigurationCommandInput,
+    CreateLaunchConfigurationCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateLaunchConfigurationCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryCreateLaunchConfigurationCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateLaunchConfigurationCommandOutput> {
+    return deserializeAws_queryCreateLaunchConfigurationCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-auto-scaling/commands/DescribeAutoScalingGroupsCommand.ts
@@ -0,0 +1,90 @@
+import {
+  AutoScalingClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../AutoScalingClient";
+import {
+  AutoScalingGroupNamesType,
+  AutoScalingGroupsType
+} from "../models/index";
+import {
+  deserializeAws_queryDescribeAutoScalingGroupsCommand,
+  serializeAws_queryDescribeAutoScalingGroupsCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeAutoScalingGroupsCommandInput = AutoScalingGroupNamesType;
+export type DescribeAutoScalingGroupsCommandOutput = AutoScalingGroupsType;
+
+export class DescribeAutoScalingGroupsCommand extends $Command<
+  DescribeAutoScalingGroupsCommandInput,
+  DescribeAutoScalingGroupsCommandOutput,
+  AutoScalingClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeAutoScalingGroupsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: AutoScalingClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeAutoScalingGroupsCommandInput,
+    DescribeAutoScalingGroupsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeAutoScalingGroupsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryDescribeAutoScalingGroupsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeAutoScalingGroupsCommandOutput> {
+    return deserializeAws_queryDescribeAutoScalingGroupsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
