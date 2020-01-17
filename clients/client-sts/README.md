6f7b832591d8f4ad1279ea439c77497be8322cda
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:40:56 +0000

    chore: add client-sts

---
+++ clients/client-sts/README.md
@@ -0,0 +1,6 @@
+@aws-sdk/client-sts
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/client-sts/preview.svg)](https://www.npmjs.com/package/@aws-sdk/client-sts)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/client-sts.svg)](https://www.npmjs.com/package/@aws-sdk/client-sts)
+
+For SDK usage, please step to [SDK reademe](https://github.com/aws/aws-sdk-js-v3).
---
+++ clients/client-sts/commands/GetFederationTokenCommand.ts
@@ -0,0 +1,84 @@
+import {
+  STSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../STSClient";
+import {
+  GetFederationTokenRequest,
+  GetFederationTokenResponse
+} from "../models/index";
+import {
+  deserializeAws_queryGetFederationTokenCommand,
+  serializeAws_queryGetFederationTokenCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type GetFederationTokenCommandInput = GetFederationTokenRequest;
+export type GetFederationTokenCommandOutput = GetFederationTokenResponse;
+
+export class GetFederationTokenCommand extends $Command<
+  GetFederationTokenCommandInput,
+  GetFederationTokenCommandOutput,
+  STSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: GetFederationTokenCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: STSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<GetFederationTokenCommandInput, GetFederationTokenCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: GetFederationTokenCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryGetFederationTokenCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<GetFederationTokenCommandOutput> {
+    return deserializeAws_queryGetFederationTokenCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
