024a472ca10965fd0e0f763443758fda349ec4df
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:38:40 +0000

    chore: add client-docdb

---
+++ clients/client-docdb/commands/DescribeDBClusterParametersCommand.ts
@@ -0,0 +1,90 @@
+import {
+  DocDBClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DocDBClient";
+import {
+  DBClusterParameterGroupDetails,
+  DescribeDBClusterParametersMessage
+} from "../models/index";
+import {
+  deserializeAws_queryDescribeDBClusterParametersCommand,
+  serializeAws_queryDescribeDBClusterParametersCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeDBClusterParametersCommandInput = DescribeDBClusterParametersMessage;
+export type DescribeDBClusterParametersCommandOutput = DBClusterParameterGroupDetails;
+
+export class DescribeDBClusterParametersCommand extends $Command<
+  DescribeDBClusterParametersCommandInput,
+  DescribeDBClusterParametersCommandOutput,
+  DocDBClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeDBClusterParametersCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DocDBClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeDBClusterParametersCommandInput,
+    DescribeDBClusterParametersCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeDBClusterParametersCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryDescribeDBClusterParametersCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeDBClusterParametersCommandOutput> {
+    return deserializeAws_queryDescribeDBClusterParametersCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-docdb/commands/DescribeDBEngineVersionsCommand.ts
@@ -0,0 +1,87 @@
+import {
+  DocDBClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DocDBClient";
+import {
+  DBEngineVersionMessage,
+  DescribeDBEngineVersionsMessage
+} from "../models/index";
+import {
+  deserializeAws_queryDescribeDBEngineVersionsCommand,
+  serializeAws_queryDescribeDBEngineVersionsCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeDBEngineVersionsCommandInput = DescribeDBEngineVersionsMessage;
+export type DescribeDBEngineVersionsCommandOutput = DBEngineVersionMessage;
+
+export class DescribeDBEngineVersionsCommand extends $Command<
+  DescribeDBEngineVersionsCommandInput,
+  DescribeDBEngineVersionsCommandOutput,
+  DocDBClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeDBEngineVersionsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DocDBClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeDBEngineVersionsCommandInput,
+    DescribeDBEngineVersionsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeDBEngineVersionsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryDescribeDBEngineVersionsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeDBEngineVersionsCommandOutput> {
+    return deserializeAws_queryDescribeDBEngineVersionsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
