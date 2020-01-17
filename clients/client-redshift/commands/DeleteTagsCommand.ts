cac90b0dda309ea357b45c39cca774fda2e50340
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:40:15 +0000

    chore: add client-redshift

---
+++ clients/client-redshift/commands/DeleteTagsCommand.ts
@@ -0,0 +1,82 @@
+import {
+  RedshiftClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../RedshiftClient";
+import { DeleteTagsMessage } from "../models/index";
+import {
+  deserializeAws_queryDeleteTagsCommand,
+  serializeAws_queryDeleteTagsCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  MetadataBearer as __MetadataBearer
+} from "@aws-sdk/types";
+
+export type DeleteTagsCommandInput = DeleteTagsMessage;
+export type DeleteTagsCommandOutput = __MetadataBearer;
+
+export class DeleteTagsCommand extends $Command<
+  DeleteTagsCommandInput,
+  DeleteTagsCommandOutput,
+  RedshiftClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DeleteTagsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: RedshiftClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DeleteTagsCommandInput, DeleteTagsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DeleteTagsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryDeleteTagsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DeleteTagsCommandOutput> {
+    return deserializeAws_queryDeleteTagsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-redshift/commands/DescribeResizeCommand.ts
@@ -0,0 +1,81 @@
+import {
+  RedshiftClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../RedshiftClient";
+import { DescribeResizeMessage, ResizeProgressMessage } from "../models/index";
+import {
+  deserializeAws_queryDescribeResizeCommand,
+  serializeAws_queryDescribeResizeCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeResizeCommandInput = DescribeResizeMessage;
+export type DescribeResizeCommandOutput = ResizeProgressMessage;
+
+export class DescribeResizeCommand extends $Command<
+  DescribeResizeCommandInput,
+  DescribeResizeCommandOutput,
+  RedshiftClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeResizeCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: RedshiftClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<DescribeResizeCommandInput, DescribeResizeCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeResizeCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryDescribeResizeCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeResizeCommandOutput> {
+    return deserializeAws_queryDescribeResizeCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
