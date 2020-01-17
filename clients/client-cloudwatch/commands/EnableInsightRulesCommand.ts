0cc217512c3e6812d656b3b1d548fbcddbcb9ed1
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:38:30 +0000

    chore: add client-cloudwatch

---
+++ clients/client-cloudwatch/commands/EnableInsightRulesCommand.ts
@@ -0,0 +1,84 @@
+import {
+  CloudWatchClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudWatchClient";
+import {
+  EnableInsightRulesInput,
+  EnableInsightRulesOutput
+} from "../models/index";
+import {
+  deserializeAws_queryEnableInsightRulesCommand,
+  serializeAws_queryEnableInsightRulesCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type EnableInsightRulesCommandInput = EnableInsightRulesInput;
+export type EnableInsightRulesCommandOutput = EnableInsightRulesOutput;
+
+export class EnableInsightRulesCommand extends $Command<
+  EnableInsightRulesCommandInput,
+  EnableInsightRulesCommandOutput,
+  CloudWatchClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: EnableInsightRulesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudWatchClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<EnableInsightRulesCommandInput, EnableInsightRulesCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: EnableInsightRulesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryEnableInsightRulesCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<EnableInsightRulesCommandOutput> {
+    return deserializeAws_queryEnableInsightRulesCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
