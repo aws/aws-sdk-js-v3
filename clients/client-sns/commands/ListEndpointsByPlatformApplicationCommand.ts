a478806602b6bacdf63a0891c2d58072bb737fc3
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:40:33 +0000

    chore: add client-sns

---
+++ clients/client-sns/commands/ListEndpointsByPlatformApplicationCommand.ts
@@ -0,0 +1,93 @@
+import {
+  SNSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SNSClient";
+import {
+  ListEndpointsByPlatformApplicationInput,
+  ListEndpointsByPlatformApplicationResponse
+} from "../models/index";
+import {
+  deserializeAws_queryListEndpointsByPlatformApplicationCommand,
+  serializeAws_queryListEndpointsByPlatformApplicationCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListEndpointsByPlatformApplicationCommandInput = ListEndpointsByPlatformApplicationInput;
+export type ListEndpointsByPlatformApplicationCommandOutput = ListEndpointsByPlatformApplicationResponse;
+
+export class ListEndpointsByPlatformApplicationCommand extends $Command<
+  ListEndpointsByPlatformApplicationCommandInput,
+  ListEndpointsByPlatformApplicationCommandOutput,
+  SNSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListEndpointsByPlatformApplicationCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SNSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListEndpointsByPlatformApplicationCommandInput,
+    ListEndpointsByPlatformApplicationCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListEndpointsByPlatformApplicationCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryListEndpointsByPlatformApplicationCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListEndpointsByPlatformApplicationCommandOutput> {
+    return deserializeAws_queryListEndpointsByPlatformApplicationCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-sns/runtimeConfig.shared.ts
@@ -0,0 +1,7 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2010-03-31",
+  signingName: "sns",
+  regionInfoProvider: defaultRegionInfoProvider
+};
