0e1bbea2c9549bb6d2dd87372064af4e4002b2d1
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:38:40 +0000

    chore: add client-docdb

---
+++ clients/client-docdb/commands/CopyDBClusterSnapshotCommand.ts
@@ -0,0 +1,87 @@
+import {
+  DocDBClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DocDBClient";
+import {
+  CopyDBClusterSnapshotMessage,
+  DBClusterSnapshot
+} from "../models/index";
+import {
+  deserializeAws_queryCopyDBClusterSnapshotCommand,
+  serializeAws_queryCopyDBClusterSnapshotCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CopyDBClusterSnapshotCommandInput = CopyDBClusterSnapshotMessage;
+export type CopyDBClusterSnapshotCommandOutput = DBClusterSnapshot;
+
+export class CopyDBClusterSnapshotCommand extends $Command<
+  CopyDBClusterSnapshotCommandInput,
+  CopyDBClusterSnapshotCommandOutput,
+  DocDBClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CopyDBClusterSnapshotCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DocDBClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    CopyDBClusterSnapshotCommandInput,
+    CopyDBClusterSnapshotCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CopyDBClusterSnapshotCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryCopyDBClusterSnapshotCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CopyDBClusterSnapshotCommandOutput> {
+    return deserializeAws_queryCopyDBClusterSnapshotCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-docdb/commands/ModifyDBClusterCommand.ts
@@ -0,0 +1,81 @@
+import {
+  DocDBClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../DocDBClient";
+import { DBCluster, ModifyDBClusterMessage } from "../models/index";
+import {
+  deserializeAws_queryModifyDBClusterCommand,
+  serializeAws_queryModifyDBClusterCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ModifyDBClusterCommandInput = ModifyDBClusterMessage;
+export type ModifyDBClusterCommandOutput = DBCluster;
+
+export class ModifyDBClusterCommand extends $Command<
+  ModifyDBClusterCommandInput,
+  ModifyDBClusterCommandOutput,
+  DocDBClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ModifyDBClusterCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DocDBClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ModifyDBClusterCommandInput, ModifyDBClusterCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ModifyDBClusterCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryModifyDBClusterCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ModifyDBClusterCommandOutput> {
+    return deserializeAws_queryModifyDBClusterCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
