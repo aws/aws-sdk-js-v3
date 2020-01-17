7ccfd86fdfeeb6413c6cbaec23c1bed06a290352
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:38:19 +0000

    chore: add client-cloudsearch

---
+++ clients/client-cloudsearch/commands/UpdateScalingParametersCommand.ts
@@ -0,0 +1,87 @@
+import {
+  CloudSearchClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudSearchClient";
+import {
+  UpdateScalingParametersRequest,
+  UpdateScalingParametersResponse
+} from "../models/index";
+import {
+  deserializeAws_queryUpdateScalingParametersCommand,
+  serializeAws_queryUpdateScalingParametersCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateScalingParametersCommandInput = UpdateScalingParametersRequest;
+export type UpdateScalingParametersCommandOutput = UpdateScalingParametersResponse;
+
+export class UpdateScalingParametersCommand extends $Command<
+  UpdateScalingParametersCommandInput,
+  UpdateScalingParametersCommandOutput,
+  CloudSearchClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateScalingParametersCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudSearchClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    UpdateScalingParametersCommandInput,
+    UpdateScalingParametersCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateScalingParametersCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryUpdateScalingParametersCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateScalingParametersCommandOutput> {
+    return deserializeAws_queryUpdateScalingParametersCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
