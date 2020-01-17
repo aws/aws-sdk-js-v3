bf9895b877b92116c80d1a7d3ab72314f2884c03
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:40:42 +0000

    chore: add client-sqs

---
+++ clients/client-sqs/commands/ReceiveMessageCommand.ts
@@ -0,0 +1,81 @@
+import {
+  SQSClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../SQSClient";
+import { ReceiveMessageRequest, ReceiveMessageResult } from "../models/index";
+import {
+  deserializeAws_queryReceiveMessageCommand,
+  serializeAws_queryReceiveMessageCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ReceiveMessageCommandInput = ReceiveMessageRequest;
+export type ReceiveMessageCommandOutput = ReceiveMessageResult;
+
+export class ReceiveMessageCommand extends $Command<
+  ReceiveMessageCommandInput,
+  ReceiveMessageCommandOutput,
+  SQSClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ReceiveMessageCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: SQSClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ReceiveMessageCommandInput, ReceiveMessageCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ReceiveMessageCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryReceiveMessageCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ReceiveMessageCommandOutput> {
+    return deserializeAws_queryReceiveMessageCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
