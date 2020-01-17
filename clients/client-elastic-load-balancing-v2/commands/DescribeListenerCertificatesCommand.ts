67ab9429ab3a2e786609d5ac6b15073649a5f953
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-17 00:39:05 +0000

    chore: add client-elastic-load-balancing-v2

---
+++ clients/client-elastic-load-balancing-v2/commands/DescribeListenerCertificatesCommand.ts
@@ -0,0 +1,93 @@
+import {
+  ElasticLoadBalancingv2ClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../ElasticLoadBalancingv2Client";
+import {
+  DescribeListenerCertificatesInput,
+  DescribeListenerCertificatesOutput
+} from "../models/index";
+import {
+  deserializeAws_queryDescribeListenerCertificatesCommand,
+  serializeAws_queryDescribeListenerCertificatesCommand
+} from "../protocols/Aws_query";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type DescribeListenerCertificatesCommandInput = DescribeListenerCertificatesInput;
+export type DescribeListenerCertificatesCommandOutput = DescribeListenerCertificatesOutput;
+
+export class DescribeListenerCertificatesCommand extends $Command<
+  DescribeListenerCertificatesCommandInput,
+  DescribeListenerCertificatesCommandOutput,
+  ElasticLoadBalancingv2ClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: DescribeListenerCertificatesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: ElasticLoadBalancingv2ClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    DescribeListenerCertificatesCommandInput,
+    DescribeListenerCertificatesCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: DescribeListenerCertificatesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_queryDescribeListenerCertificatesCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<DescribeListenerCertificatesCommandOutput> {
+    return deserializeAws_queryDescribeListenerCertificatesCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
