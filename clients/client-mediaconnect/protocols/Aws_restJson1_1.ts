d988982916d164a73ffcd07837833e33fea61bea
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-24 01:13:40 +0000

    chore: codegen for fix collectBody default return

--- clients/client-mediaconnect/protocols/Aws_restJson1_1.ts
+++ clients/client-mediaconnect/protocols/Aws_restJson1_1.ts
@@ -3025,7 +3025,9 @@ const collectBody = (
   streamBody: any,
   context: __SerdeContext
 ): Promise<Uint8Array> => {
-  return context.streamCollector(streamBody) || new Uint8Array();
+  return (
+    context.streamCollector(streamBody) || Promise.resolve(new Uint8Array())
+  );
 };
 
 // Encode Uint8Array data into string with utf-8.
