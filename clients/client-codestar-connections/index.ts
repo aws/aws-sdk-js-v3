2445b374e988d6a984f1031bd06f1e68e96e03a4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-24 18:54:50 +0000

    feat: add client-codestar-connections

---
+++ clients/client-codestar-connections/index.ts
@@ -0,0 +1,6 @@
+export * from "./CodeStarconnectionsClient";
+export * from "./CodeStarconnections";
+export * from "./commands/CreateConnectionCommand";
+export * from "./commands/DeleteConnectionCommand";
+export * from "./commands/GetConnectionCommand";
+export * from "./commands/ListConnectionsCommand";
