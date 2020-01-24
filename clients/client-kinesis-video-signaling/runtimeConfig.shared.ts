aefe7033ef5429cb9d6addd455996968641d3037
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-24 18:55:39 +0000

    feat: add client-kinesis-video-signaling

---
+++ clients/client-kinesis-video-signaling/runtimeConfig.shared.ts
@@ -0,0 +1,8 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2019-12-04",
+  disableHostPrefix: false,
+  signingName: "kinesisvideo",
+  regionInfoProvider: defaultRegionInfoProvider
+};
