b6710c54a602b45bf51efad52f0aa232419c4980
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-24 18:54:50 +0000

    feat: add client-codestar-connections

---
+++ clients/client-codestar-connections/runtimeConfig.shared.ts
@@ -0,0 +1,8 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2019-12-01",
+  disableHostPrefix: false,
+  signingName: "codestar-connections",
+  regionInfoProvider: defaultRegionInfoProvider
+};
