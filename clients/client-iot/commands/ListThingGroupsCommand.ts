94c3076d809ede0ca742ba60f439bca546245775
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-23 17:00:53 -0800

    feat: add client-iot and client-savingsplans (#783)

---
+++ clients/client-iot/commands/ListThingGroupsCommand.ts
@@ -0,0 +1,84 @@
+import {
+  IoTClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../IoTClient";
+import {
+  ListThingGroupsRequest,
+  ListThingGroupsResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1ListThingGroupsCommand,
+  serializeAws_restJson1_1ListThingGroupsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListThingGroupsCommandInput = ListThingGroupsRequest;
+export type ListThingGroupsCommandOutput = ListThingGroupsResponse;
+
+export class ListThingGroupsCommand extends $Command<
+  ListThingGroupsCommandInput,
+  ListThingGroupsCommandOutput,
+  IoTClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListThingGroupsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: IoTClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ListThingGroupsCommandInput, ListThingGroupsCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListThingGroupsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1ListThingGroupsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListThingGroupsCommandOutput> {
+    return deserializeAws_restJson1_1ListThingGroupsCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-iot/commands/ListThingPrincipalsCommand.ts
@@ -0,0 +1,90 @@
+import {
+  IoTClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../IoTClient";
+import {
+  ListThingPrincipalsRequest,
+  ListThingPrincipalsResponse
+} from "../models/index";
+import {
+  deserializeAws_restJson1_1ListThingPrincipalsCommand,
+  serializeAws_restJson1_1ListThingPrincipalsCommand
+} from "../protocols/Aws_restJson1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListThingPrincipalsCommandInput = ListThingPrincipalsRequest;
+export type ListThingPrincipalsCommandOutput = ListThingPrincipalsResponse;
+
+export class ListThingPrincipalsCommand extends $Command<
+  ListThingPrincipalsCommandInput,
+  ListThingPrincipalsCommandOutput,
+  IoTClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListThingPrincipalsCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: IoTClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListThingPrincipalsCommandInput,
+    ListThingPrincipalsCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListThingPrincipalsCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restJson1_1ListThingPrincipalsCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListThingPrincipalsCommandOutput> {
+    return deserializeAws_restJson1_1ListThingPrincipalsCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
