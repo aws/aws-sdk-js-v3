a89275006d284269260169d58ab5ab964341e9e4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-28 18:47:15 -0800

    fix: date appended in cloudfront operation names (#806)

---
+++ clients/client-cloudfront/commands/ListFieldLevelEncryptionProfilesCommand.ts
@@ -0,0 +1,93 @@
+import {
+  CloudFrontClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudFrontClient";
+import {
+  ListFieldLevelEncryptionProfilesRequest,
+  ListFieldLevelEncryptionProfilesResult
+} from "../models/index";
+import {
+  deserializeAws_restXmlListFieldLevelEncryptionProfilesCommand,
+  serializeAws_restXmlListFieldLevelEncryptionProfilesCommand
+} from "../protocols/Aws_restXml";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ListFieldLevelEncryptionProfilesCommandInput = ListFieldLevelEncryptionProfilesRequest;
+export type ListFieldLevelEncryptionProfilesCommandOutput = ListFieldLevelEncryptionProfilesResult;
+
+export class ListFieldLevelEncryptionProfilesCommand extends $Command<
+  ListFieldLevelEncryptionProfilesCommandInput,
+  ListFieldLevelEncryptionProfilesCommandOutput,
+  CloudFrontClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ListFieldLevelEncryptionProfilesCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudFrontClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<
+    ListFieldLevelEncryptionProfilesCommandInput,
+    ListFieldLevelEncryptionProfilesCommandOutput
+  > {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ListFieldLevelEncryptionProfilesCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restXmlListFieldLevelEncryptionProfilesCommand(
+      input,
+      context
+    );
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ListFieldLevelEncryptionProfilesCommandOutput> {
+    return deserializeAws_restXmlListFieldLevelEncryptionProfilesCommand(
+      output,
+      context
+    );
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
