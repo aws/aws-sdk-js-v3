cd889cadba1aa01522312598a7217c592f7a6b00
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-29 01:02:33 +0000

    feat: add client-workmail

---
+++ clients/client-workmail/commands/CreateAliasCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  WorkMailClientResolvedConfig
+} from "../WorkMailClient";
+import { CreateAliasRequest, CreateAliasResponse } from "../models/index";
+import {
+  deserializeAws_json1_1CreateAliasCommand,
+  serializeAws_json1_1CreateAliasCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type CreateAliasCommandInput = CreateAliasRequest;
+export type CreateAliasCommandOutput = CreateAliasResponse;
+
+export class CreateAliasCommand extends $Command<
+  CreateAliasCommandInput,
+  CreateAliasCommandOutput,
+  WorkMailClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: CreateAliasCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: WorkMailClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<CreateAliasCommandInput, CreateAliasCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: CreateAliasCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1CreateAliasCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<CreateAliasCommandOutput> {
+    return deserializeAws_json1_1CreateAliasCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
---
+++ clients/client-workmail/commands/ResetPasswordCommand.ts
@@ -0,0 +1,81 @@
+import {
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  WorkMailClientResolvedConfig
+} from "../WorkMailClient";
+import { ResetPasswordRequest, ResetPasswordResponse } from "../models/index";
+import {
+  deserializeAws_json1_1ResetPasswordCommand,
+  serializeAws_json1_1ResetPasswordCommand
+} from "../protocols/Aws_json1_1";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type ResetPasswordCommandInput = ResetPasswordRequest;
+export type ResetPasswordCommandOutput = ResetPasswordResponse;
+
+export class ResetPasswordCommand extends $Command<
+  ResetPasswordCommandInput,
+  ResetPasswordCommandOutput,
+  WorkMailClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: ResetPasswordCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: WorkMailClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<ResetPasswordCommandInput, ResetPasswordCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: ResetPasswordCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_json1_1ResetPasswordCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<ResetPasswordCommandOutput> {
+    return deserializeAws_json1_1ResetPasswordCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
