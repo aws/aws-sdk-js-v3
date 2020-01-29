a2b19a225491372ca5f47939ba5fd2e2c8a1740d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-28 18:47:15 -0800

    fix: date appended in cloudfront operation names (#806)

---
+++ clients/client-cloudfront/commands/UpdateDistributionCommand.ts
@@ -0,0 +1,84 @@
+import {
+  CloudFrontClientResolvedConfig,
+  ServiceInputTypes,
+  ServiceOutputTypes
+} from "../CloudFrontClient";
+import {
+  UpdateDistributionRequest,
+  UpdateDistributionResult
+} from "../models/index";
+import {
+  deserializeAws_restXmlUpdateDistributionCommand,
+  serializeAws_restXmlUpdateDistributionCommand
+} from "../protocols/Aws_restXml";
+import { getSerdePlugin } from "@aws-sdk/middleware-serde";
+import {
+  HttpRequest as __HttpRequest,
+  HttpResponse as __HttpResponse
+} from "@aws-sdk/protocol-http";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import {
+  FinalizeHandlerArguments,
+  Handler,
+  HandlerExecutionContext,
+  MiddlewareStack,
+  SerdeContext,
+  HttpHandlerOptions as __HttpHandlerOptions
+} from "@aws-sdk/types";
+
+export type UpdateDistributionCommandInput = UpdateDistributionRequest;
+export type UpdateDistributionCommandOutput = UpdateDistributionResult;
+
+export class UpdateDistributionCommand extends $Command<
+  UpdateDistributionCommandInput,
+  UpdateDistributionCommandOutput,
+  CloudFrontClientResolvedConfig
+> {
+  // Start section: command_properties
+  // End section: command_properties
+
+  constructor(readonly input: UpdateDistributionCommandInput) {
+    // Start section: command_constructor
+    super();
+    // End section: command_constructor
+  }
+
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: CloudFrontClientResolvedConfig,
+    options?: __HttpHandlerOptions
+  ): Handler<UpdateDistributionCommandInput, UpdateDistributionCommandOutput> {
+    this.middlewareStack.use(
+      getSerdePlugin(configuration, this.serialize, this.deserialize)
+    );
+
+    const stack = clientStack.concat(this.middlewareStack);
+
+    const handlerExecutionContext: HandlerExecutionContext = {
+      logger: {} as any
+    };
+    const { requestHandler } = configuration;
+    return stack.resolve(
+      (request: FinalizeHandlerArguments<any>) =>
+        requestHandler.handle(request.request as __HttpRequest, options || {}),
+      handlerExecutionContext
+    );
+  }
+
+  private serialize(
+    input: UpdateDistributionCommandInput,
+    context: SerdeContext
+  ): Promise<__HttpRequest> {
+    return serializeAws_restXmlUpdateDistributionCommand(input, context);
+  }
+
+  private deserialize(
+    output: __HttpResponse,
+    context: SerdeContext
+  ): Promise<UpdateDistributionCommandOutput> {
+    return deserializeAws_restXmlUpdateDistributionCommand(output, context);
+  }
+
+  // Start section: command_body_extra
+  // End section: command_body_extra
+}
