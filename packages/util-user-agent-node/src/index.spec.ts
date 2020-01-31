80295bbca4d82cc9db50ff173fbb675ce9fed0ef
Chase Coalwell <chasecoa@amazon.com>
2020-01-31 15:40:22 -0800

    feat: x-amz-user-agent for browser (#826)

--- packages/util-user-agent-node/src/index.spec.ts
+++ packages/util-user-agent-node/src/index.spec.ts
@@ -1,4 +1,4 @@
-import { defaultUserAgent, appendToUserAgent } from ".";
+import { defaultUserAgent } from ".";
 import { HttpRequest } from "@aws-sdk/protocol-http";
 import * as process from "process";
 
@@ -19,18 +19,5 @@ describe("defaultUserAgent", () => {
       `aws-sdk-nodejs-v3-client-s3-node/0.1.0 ${process.platform}/${process.version} exec-env/ecs`
     );
     if (originEnv) process.env.AWS_EXECUTION_ENV = originEnv;
-  });
-
-  it("append to user agent", () => {
-    const defaultValue = defaultUserAgent("client-s3-node", "0.1.0");
-    const request = new HttpRequest({
-      headers: { "User-Agent": defaultValue },
-      method: "GET",
-      protocol: "json",
-      hostname: "foo.amazonaws.com",
-      path: "/"
-    });
-    appendToUserAgent(request, "http/2.0");
-    expect(request.headers["User-Agent"]).toBe(`${defaultValue} http/2.0`);
   });
 });
