464013e08877bb1dd3337786d6b431bf956d431a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-05 17:20:03 -0800

    feat: add client-accessanalyzer (#867)

---
+++ clients/client-accessanalyzer/runtimeConfig.shared.ts
@@ -0,0 +1,8 @@
+import { defaultRegionInfoProvider } from "./endpoints";
+
+export const ClientSharedValues = {
+  apiVersion: "2019-11-01",
+  disableHostPrefix: false,
+  signingName: "replace-for-service-name",
+  regionInfoProvider: defaultRegionInfoProvider
+};
