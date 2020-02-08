0357eca88481eb983aee6a4b95d8c252774eb0f1
Chase Coalwell <chasecoa@amazon.com>
2020-02-07 16:03:54 -0800

    fix: removes unused signature-v4 target packages (#889)

--- packages/signature-v4-browser/jest.config.js
+++
@@ -1,7 +0,0 @@
-const base = require("../../jest.config.base.js");
-
-module.exports = {
-  ...base,
-  //only test build dist, avoid testing the package twice
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"]
-};
