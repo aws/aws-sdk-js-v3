5abc521490264d5206ce4c6c41ce7313e644d3c5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-10 15:12:40 -0800

    chore: codegen for adding react-native target (#903)

---
+++ clients/client-sfn/runtimeConfig.rn.ts
@@ -0,0 +1,12 @@
+import { parseUrl } from "@aws-sdk/url-parser-node";
+import { name, version } from "./package.json";
+import { ClientDefaults } from "./SFNClient";
+import { ClientDefaultValues as BrowserDefaults } from "./runtimeConfig.browser";
+
+export const ClientDefaultValues: Required<ClientDefaults> = {
+  ...BrowserDefaults,
+  urlParser: parseUrl,
+  defaultUserAgent: `aws-sdk-js-v3-react-native-${name}/${version}`,
+  runtime: "browser",
+  signingName: "states"
+};
