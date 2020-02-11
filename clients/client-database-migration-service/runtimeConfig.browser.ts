a99d29f9713871d412d1a5549e7478a857420323
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-10 17:40:36 -0800

    chore: codegen for remove duplicate signingName (#907)

--- clients/client-database-migration-service/runtimeConfig.browser.ts
+++ clients/client-database-migration-service/runtimeConfig.browser.ts
@@ -24,7 +24,6 @@ export const ClientDefaultValues: Required<ClientDefaults> = {
   utf8Encoder: toUtf8,
   defaultUserAgent: defaultUserAgent(name, version),
   runtime: "browser",
-  signingName: "dms",
   credentialDefaultProvider: invalidFunction("Credential is missing") as any,
   regionDefaultProvider: invalidFunction("Region is missing") as any
 };
