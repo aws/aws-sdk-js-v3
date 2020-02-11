a83348214c9ebdcd7bbe16a741fca0e74c8b1adc
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-10 18:38:56 -0800

    chore: codegen for rn config template fix (#912)

--- clients/client-codeguruprofiler/runtimeConfig.rn.ts
+++ clients/client-codeguruprofiler/runtimeConfig.rn.ts
@@ -1,3 +1,5 @@
+import { FetchHttpHandler } from "@aws-sdk/fetch-http-handler";
+import { streamCollector } from "@aws-sdk/stream-collector-rn";
 import { parseUrl } from "@aws-sdk/url-parser-node";
 import { name, version } from "./package.json";
 import { ClientDefaults } from "./CodeGuruProfilerClient";
@@ -5,7 +7,9 @@ import { ClientDefaultValues as BrowserDefaults } from "./runtimeConfig.browser"
 
 export const ClientDefaultValues: Required<ClientDefaults> = {
   ...BrowserDefaults,
+  requestHandler: new FetchHttpHandler({ bufferBody: true }),
+  streamCollector,
   urlParser: parseUrl,
   defaultUserAgent: `aws-sdk-js-v3-react-native-${name}/${version}`,
-  runtime: "browser"
+  runtime: "react-native"
 };
