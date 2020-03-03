bf12908eb9218deae759579ca8dff96aa0c489aa
Chase Coalwell <chasecoa@amazon.com>
2020-03-03 10:04:59 -0800

    feat: remove url-parser-universal (#971)

--- packages/url-parser-universal/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { UrlParser } from "@aws-sdk/types";
-import { parseUrl as browserUrlParser } from "@aws-sdk/url-parser-browser";
-import { parseUrl as nodeUrlParser } from "@aws-sdk/url-parser-node";
-
-declare const URL: any;
-
-export const parseUrl: UrlParser =
-  typeof URL !== "undefined" ? browserUrlParser : nodeUrlParser;
