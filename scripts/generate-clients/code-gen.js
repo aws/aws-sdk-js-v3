fb0dfdaa939cc6eaf961c0378602cf61cfc693a8
AllanFly120 <zheallan@amazon.com>
2020-03-04 16:52:20 -0800

    chore: add parameter to gradle task and update code-gen script (#976)
    * chore: smithy gradle can generated from designated folder
    
    * chore: generate-clients scripts supports globs

--- scripts/generate-clients/code-gen.js
+++ scripts/generate-clients/code-gen.js
@@ -2,36 +2,56 @@ const path = require("path");
 const { copyFileSync, emptyDirSync } = require("fs-extra");
 const { readdirSync, lstatSync } = require("fs");
 const { spawnProcess } = require("./spawn-process");
-const { CODE_GEN_ROOT, CODE_GEN_INPUT_DIR } = require("./code-gen-dir");
+const { CODE_GEN_ROOT, TEMP_CODE_GEN_INPUT_DIR } = require("./code-gen-dir");
+const Glob = require("glob");
 
 async function generateClients(models) {
-  console.info("models directory: ", models);
-  if (models === CODE_GEN_INPUT_DIR) {
-    // This script will clean the CODE_GEN_INPUT_DIR in execution.
-    // throw to avoid input model being removed
-    throw new Error(
-      `models directory cannot be the same as ${CODE_GEN_INPUT_DIR}`
-    );
-  } else {
-    console.log(`clearing code gen input folder...`);
-    emptyDirSync(CODE_GEN_INPUT_DIR);
-    console.log(`copying models from ${models} to ${CODE_GEN_INPUT_DIR}...`);
+  let designatedModels = false;
+  if (typeof models === "string") {
+    //`models` is a folder path
+    designatedModels = true;
+    emptyDirSync(TEMP_CODE_GEN_INPUT_DIR);
+    console.log(`preparing models from ${models}...`);
     for (const modelFileName of readdirSync(models)) {
       const modelPath = path.join(models, modelFileName);
       if (!lstatSync(modelPath).isFile()) continue;
       console.log(`copying model ${modelFileName}...`);
-      copyFileSync(modelPath, path.join(CODE_GEN_INPUT_DIR, modelFileName), {
-        overwrite: true
+      copyFileSync(
+        modelPath,
+        path.join(TEMP_CODE_GEN_INPUT_DIR, modelFileName),
+        {
+          overwrite: true
+        }
+      );
+    }
+  } else if (Array.isArray(models)) {
+    //`models` is a list of globs
+    designatedModels = true;
+    emptyDirSync(TEMP_CODE_GEN_INPUT_DIR);
+    models.forEach(pattern => {
+      const files = Glob.sync(pattern, {
+        realpath: true,
+        absolute: true
       });
-    }
+      files.forEach(file => {
+        if (!lstatSync(file).isFile()) return;
+        const name = path.basename(file);
+        console.log(`copying model ${name}...`);
+        copyFileSync(file, path.join(TEMP_CODE_GEN_INPUT_DIR, name), {
+          overwrite: true
+        });
+      });
+    });
+  } else {
+    console.log("no model supplied, generating all AWS clients");
   }
-  await spawnProcess(
-    "./gradlew",
-    [":sdk-codegen:clean", ":sdk-codegen:build"],
-    {
-      cwd: CODE_GEN_ROOT
-    }
-  );
+  const options = [":sdk-codegen:clean", ":sdk-codegen:build"];
+  if (designatedModels) {
+    options.push(`-PmodelsDirProp=${TEMP_CODE_GEN_INPUT_DIR}`);
+  }
+  await spawnProcess("./gradlew", options, {
+    cwd: CODE_GEN_ROOT
+  });
 }
 
 module.exports = {
--- scripts/generate-clients/index.js
+++ scripts/generate-clients/index.js
@@ -1,31 +1,38 @@
 const yargs = require("yargs");
 const path = require("path");
-const { emptyDirSync } = require("fs-extra");
+const { emptyDirSync, rmdirSync } = require("fs-extra");
 const { generateClients } = require("./code-gen");
 const { copyToClients } = require("./copy-to-clients");
-const { CODE_GEN_INPUT_DIR, CODE_GEN_OUTPUT_DIR } = require("./code-gen-dir");
+const {
+  CODE_GEN_OUTPUT_DIR,
+  TEMP_CODE_GEN_INPUT_DIR
+} = require("./code-gen-dir");
 const { prettifyCode } = require("./code-prettify");
 
 const CLIENTS_DIR = path.normalize(path.join(__dirname, "..", "..", "clients"));
 
-const { models, output: clientsDir } = yargs
+const { models, globs, output: clientsDir } = yargs
   .alias("m", "models")
   .string("m")
-  .describe("m", "the directory of models")
-  .required("m")
+  .describe("m", "The directory of models.")
+  .alias("g", "globs")
+  .array("g")
+  .describe("g", "A list of smithy model globs")
+  .conflicts("models", "globs") //either models(path) or globs is accepted
   .alias("o", "output")
   .string("o")
-  .describe("o", "the output directory for built clients")
+  .describe("o", "The output directory for built clients")
   .default("o", CLIENTS_DIR)
   .help().argv;
 
 (async () => {
   try {
-    await generateClients(models);
+    await generateClients(models || globs);
     await prettifyCode();
     await copyToClients(clientsDir);
-    emptyDirSync(CODE_GEN_INPUT_DIR);
     emptyDirSync(CODE_GEN_OUTPUT_DIR);
+    emptyDirSync(TEMP_CODE_GEN_INPUT_DIR);
+    rmdirSync(TEMP_CODE_GEN_INPUT_DIR);
   } catch (e) {
     console.log(e);
     process.exit(1);
