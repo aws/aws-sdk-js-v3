5d58fdc24ab6d1a98183ccedb38599940a6ed410
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-03-06 21:29:11 +0000

    fix: no LocationConstraint for us-east-1 if not explicitly passed

--- packages/middleware-location-constraint/src/index.spec.ts
+++ packages/middleware-location-constraint/src/index.spec.ts
@@ -2,61 +2,63 @@ import { locationConstraintMiddleware } from "./";
 
 describe("locationConstrainMiddleware", () => {
   const next = jest.fn();
+  const basicInput = {
+    foo: "bar"
+  };
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it("should remove any CreateBucketConfiguration from requests directed at us-east-1", async () => {
+  describe("for region us-east-1", () => {
     const handler = locationConstraintMiddleware({
       region: () => Promise.resolve("us-east-1")
     })(next, {} as any);
-    const input = {
-      CreateBucketConfiguration: { LocationConstraint: "us-east-1" },
-      foo: "bar"
-    };
-    await handler({ input });
 
-    expect(next.mock.calls.length).toBe(1);
-    expect(next.mock.calls[0][0]).toEqual({
-      input: {
-        ...input,
-        CreateBucketConfiguration: undefined
-      }
+    it("should not update LocationConstraint if it's already set", async () => {
+      const input = {
+        ...basicInput,
+        CreateBucketConfiguration: { LocationConstraint: "us-east-1" }
+      };
+      await handler({ input });
+      expect(next.mock.calls.length).toBe(1);
+      expect(next.mock.calls[0][0]).toEqual({ input });
+    });
+
+    it("should not add LocationConstraint if it's not set", async () => {
+      const input = basicInput;
+      await handler({ input });
+      expect(next.mock.calls.length).toBe(1);
+      expect(next.mock.calls[0][0]).toEqual({ input });
     });
   });
 
-  it("should apply a CreateBucketConfiguration with a LocationConstraint of the target region for requests directed outside of us-east-1", async () => {
+  describe("for region not us-east-1", () => {
+    const region = "us-east-2";
     const handler = locationConstraintMiddleware({
-      region: () => Promise.resolve("us-east-2")
+      region: () => Promise.resolve(region)
     })(next, {} as any);
-    const input = {
-      foo: "bar"
-    };
 
-    await handler({ input });
-
-    expect(next.mock.calls.length).toBe(1);
-    expect(next.mock.calls[0][0]).toEqual({
-      input: {
-        ...input,
-        CreateBucketConfiguration: { LocationConstraint: "us-east-2" }
-      }
+    it("should not update LocationConstraint if it's already set", async () => {
+      const input = {
+        ...basicInput,
+        CreateBucketConfiguration: { LocationConstraint: "us-east-1" }
+      };
+      await handler({ input });
+      expect(next.mock.calls.length).toBe(1);
+      expect(next.mock.calls[0][0]).toEqual({ input });
     });
-  });
 
-  it("should do nothing if a LocationConstraint had already been set on a request directed outside of us-east-1", async () => {
-    const handler = locationConstraintMiddleware({
-      region: () => Promise.resolve("us-east-2")
-    })(next, {} as any);
-    const input = {
-      CreateBucketConfiguration: { LocationConstraint: "us-east-1" },
-      foo: "bar"
-    };
-
-    await handler({ input });
-
-    expect(next.mock.calls.length).toBe(1);
-    expect(next.mock.calls[0][0]).toEqual({ input });
+    it("should add region as LocationConstraint if it's not set", async () => {
+      const input = basicInput;
+      await handler({ input });
+      expect(next.mock.calls.length).toBe(1);
+      expect(next.mock.calls[0][0]).toEqual({
+        input: {
+          ...input,
+          CreateBucketConfiguration: { LocationConstraint: region }
+        }
+      });
+    });
   });
 });
