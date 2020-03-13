ab1d09d773aaa5fbb306a7ecd070a9a2070f3162
AllanFly120 <zheallan@amazon.com>
2020-03-13 16:48:36 -0700

    fix: codegen for using pure JS hasher in RN (#998)
    The @aws-crypto/sha256-browser package doesn't work properly in ReactNative. So we use replace it will pure JS hasher(@aws-crypto/sha256-js) instead.
    Codegen for awslabs/smithy-typescript#144

--- clients/client-application-insights/runtimeConfig.rn.ts
+++ clients/client-application-insights/runtimeConfig.rn.ts
@@ -1,4 +1,5 @@
 import { FetchHttpHandler } from "@aws-sdk/fetch-http-handler";
+import { Sha256 } from "@aws-crypto/sha256-js";
 import { streamCollector } from "@aws-sdk/stream-collector-rn";
 import { parseUrl } from "@aws-sdk/url-parser-node";
 import { name, version } from "./package.json";
@@ -8,6 +9,7 @@ import { ClientDefaultValues as BrowserDefaults } from "./runtimeConfig.browser"
 export const ClientDefaultValues: Required<ClientDefaults> = {
   ...BrowserDefaults,
   requestHandler: new FetchHttpHandler({ bufferBody: true }),
+  sha256: Sha256,
   streamCollector,
   urlParser: parseUrl,
   defaultUserAgent: `aws-sdk-js-v3-react-native-${name}/${version}`,
