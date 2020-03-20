91018927f284860ecd692ccd500779267266b6bf
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-03-20 10:27:17 -0700

    chore: update ReactNative extension from rn to native (#1025)

---
+++ clients/client-rekognition/runtimeConfig.native.ts
@@ -0,0 +1,17 @@
+import { FetchHttpHandler } from "@aws-sdk/fetch-http-handler";
+import { Sha256 } from "@aws-crypto/sha256-js";
+import { streamCollector } from "@aws-sdk/stream-collector-native";
+import { parseUrl } from "@aws-sdk/url-parser-node";
+import { name, version } from "./package.json";
+import { ClientDefaults } from "./RekognitionClient";
+import { ClientDefaultValues as BrowserDefaults } from "./runtimeConfig.browser";
+
+export const ClientDefaultValues: Required<ClientDefaults> = {
+  ...BrowserDefaults,
+  requestHandler: new FetchHttpHandler({ bufferBody: true }),
+  sha256: Sha256,
+  streamCollector,
+  urlParser: parseUrl,
+  defaultUserAgent: `aws-sdk-js-v3-react-native-${name}/${version}`,
+  runtime: "react-native"
+};
---
+++ clients/client-s3/runtimeConfig.native.ts
@@ -0,0 +1,24 @@
+import { invalidFunction } from "@aws-sdk/invalid-dependency";
+import { FetchHttpHandler } from "@aws-sdk/fetch-http-handler";
+import { Sha256 } from "@aws-crypto/sha256-js";
+import { streamCollector } from "@aws-sdk/stream-collector-native";
+import { parseUrl } from "@aws-sdk/url-parser-node";
+import { name, version } from "./package.json";
+import { ClientDefaults } from "./S3Client";
+import { ClientDefaultValues as BrowserDefaults } from "./runtimeConfig.browser";
+
+export const ClientDefaultValues: Required<ClientDefaults> = {
+  ...BrowserDefaults,
+  requestHandler: new FetchHttpHandler({ bufferBody: true }),
+  sha256: Sha256,
+  streamCollector,
+  urlParser: parseUrl,
+  defaultUserAgent: `aws-sdk-js-v3-react-native-${name}/${version}`,
+  runtime: "react-native",
+  eventStreamSerdeProvider: () => ({
+    serialize: invalidFunction("event stream is not supported in ReactNative."),
+    deserialize: invalidFunction(
+      "event stream is not supported in ReactNative."
+    )
+  })
+};
