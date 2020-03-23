dc6161b8508c094fcdf4ad7de14b6d27bcb090a8
stickevi <stickevi@amazon.com>
2020-03-23 16:15:59 -0700

    Use 0.4.3 smithy gradle plugin

--- codegen/sdk-codegen/build.gradle.kts
+++ codegen/sdk-codegen/build.gradle.kts
@@ -17,7 +17,7 @@ import software.amazon.smithy.model.node.Node
 import software.amazon.smithy.gradle.tasks.SmithyBuild
 
 plugins {
-    id("software.amazon.smithy") version "0.4.1"
+    id("software.amazon.smithy") version "0.4.3"
 }
 
 dependencies {
