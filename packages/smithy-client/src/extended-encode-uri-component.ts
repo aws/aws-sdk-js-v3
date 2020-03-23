4c3e3a4ec45e13f91b8b802e9f7ea588452c2024
stickevi <stickevi@amazon.com>
2020-03-23 16:13:45 -0700

    Use + for spaces in encoding

--- packages/smithy-client/src/extended-encode-uri-component.ts
+++ packages/smithy-client/src/extended-encode-uri-component.ts
@@ -3,7 +3,7 @@
  * to fully adhere to RFC 3986.
  */
 export function extendedEncodeURIComponent(str: string): string {
-  return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
+  return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
     return "%" + c.charCodeAt(0).toString(16);
-  });
+  }).replace(/%20/g, "+");
 }
