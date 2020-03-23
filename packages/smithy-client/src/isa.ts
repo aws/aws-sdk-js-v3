6581ba5f4a59e22aee741ea16e7ceecf8a7afa09
stickevi <stickevi@amazon.com>
2020-03-23 16:12:02 -0700

    Update isa to work for errors

--- packages/smithy-client/src/isa.ts
+++ packages/smithy-client/src/isa.ts
@@ -3,6 +3,10 @@
  */
 export function isa<T>(o: any, ...ids: string[]): o is T {
   return (
-    typeof o === "object" && "__type" in o && ids.indexOf(o["__type"]) > -1
+    typeof o === "object" && (
+      // Checks for name after __type, as name is used instead for errors.
+      "__type" in o && ids.indexOf(o["__type"]) > -1
+      || "name" in o && ids.indexOf(o["name"]) > -1
+    )
   );
 }
