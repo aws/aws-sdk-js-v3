272d1e061612be76afab0d2235163a47be4d1859
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-04-01 18:41:55 -0700

    fix: test getCallerIdentity instead of getSession for STS (#1067)

--- features/sts/step_definitions/sts.js
+++ features/sts/step_definitions/sts.js
@@ -21,15 +21,6 @@ module.exports = function() {
     }
   );
 
-  this.Then(
-    /^the result should contain an access key ID and secret access key$/,
-    function(callback) {
-      this.assert.compare(this.data.Credentials.AccessKeyId.length, ">", 0);
-      this.assert.compare(this.data.Credentials.SecretAccessKey.length, ">", 0);
-      callback();
-    }
-  );
-
   this.Given(/^I try to assume role with web identity$/, function(callback) {
     var params = {
       RoleArn: "arn:aws:iam::123456789:role/WebIdentity",
