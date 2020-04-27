1303244b1c4ceb2025ec24a69493c82145bd5374
Russell Anthony <russell@kingsmen.cc>
2020-04-27 16:19:19 -0700

    Build Uint8Array from ReadableStream without experimental Response constructor

--- packages/stream-collector-browser/src/index.ts
+++ packages/stream-collector-browser/src/index.ts
@@ -1,9 +1,20 @@
 import { StreamCollector } from "@aws-sdk/types";
 
-export const streamCollector: StreamCollector = (
-  stream: ReadableStream
+export const streamCollector: StreamCollector = async (
+  stream: ReadableStream<Uint8Array>
 ): Promise<Uint8Array> => {
-  return new Response(stream)
-    .arrayBuffer()
-    .then(arrayBuffer => new Uint8Array(arrayBuffer));
+  let res = new Uint8Array(0);
+  const reader = stream.getReader();
+  let isDone = false;
+  while(!isDone) {
+    const { done, value } = await reader.read();
+    if(value) {
+      const prior = res;
+      res = new Uint8Array(prior.length + value.length);
+      res.set(prior);
+      res.set(value, prior.length);
+    }
+    isDone = done;
+  }
+  return res;
 };
