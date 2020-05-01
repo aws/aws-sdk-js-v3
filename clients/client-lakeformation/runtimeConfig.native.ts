efc8570af4019ce4f07a94afde82661ad64bf3d4
AllanFly120 <zheallan@amazon.com>
2020-05-01 14:11:46 -0700

    fix: use JS url parser in ReactNative (#1129)

--- clients/client-lakeformation/runtimeConfig.native.ts
+++ clients/client-lakeformation/runtimeConfig.native.ts
@@ -2,7 +2,7 @@ import { name, version } from "./package.json";
 import { Sha256 } from "@aws-crypto/sha256-js";
 import { FetchHttpHandler } from "@aws-sdk/fetch-http-handler";
 import { streamCollector } from "@aws-sdk/stream-collector-native";
-import { parseUrl } from "@aws-sdk/url-parser-browser";
+import { parseUrl } from "@aws-sdk/url-parser-node";
 import { ClientDefaults } from "./LakeFormationClient";
 import { ClientDefaultValues as BrowserDefaults } from "./runtimeConfig.browser";
 
