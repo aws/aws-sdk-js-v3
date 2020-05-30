226d89b2a848be47126ae9c1eb6a6c8147742886
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-05-30 01:06:45 +0000

    chore: added tests for isTransientError

--- packages/service-error-classification/src/index.spec.ts
+++ packages/service-error-classification/src/index.spec.ts
@@ -1,15 +1,21 @@
-import { CLOCK_SKEW_ERROR_CODES, THROTTLING_ERROR_CODES } from "./constants";
-import { isClockSkewError, isThrottlingError } from "./index";
+import {
+  CLOCK_SKEW_ERROR_CODES,
+  THROTTLING_ERROR_CODES,
+  TRANSIENT_ERROR_CODES,
+  TRANSIENT_ERROR_STATUS_CODES
+} from "./constants";
+import { isClockSkewError, isThrottlingError, isTransientError } from "./index";
 import { SdkError } from "@aws-sdk/types";
 
 const checkForErrorType = (
   isErrorTypeFunc: (error: SdkError) => boolean,
-  errorName: string,
+  options: { name?: string; httpStatusCode?: number },
   errorTypeResult: boolean
 ) => {
+  const { name, httpStatusCode } = options;
   const error = Object.assign(new Error(), {
-    name: errorName,
-    $metadata: {}
+    name,
+    $metadata: { httpStatusCode }
   });
   expect(isErrorTypeFunc(error)).toBe(errorTypeResult);
 };
@@ -17,7 +23,7 @@ const checkForErrorType = (
 describe("isClockSkewError", () => {
   CLOCK_SKEW_ERROR_CODES.forEach(name => {
     it(`should declare error with the name "${name}" to be a ClockSkew error`, () => {
-      checkForErrorType(isClockSkewError, name, true);
+      checkForErrorType(isClockSkewError, { name }, true);
     });
   });
 
@@ -25,7 +31,7 @@ describe("isClockSkewError", () => {
     const name = Math.random().toString(36).substring(2);
     if (!CLOCK_SKEW_ERROR_CODES.includes(name)) {
       it(`should not declare error with the name "${name}" to be a ClockSkew error`, () => {
-        checkForErrorType(isClockSkewError, name, false);
+        checkForErrorType(isClockSkewError, { name }, false);
       });
       break;
     }
@@ -35,7 +41,7 @@ describe("isClockSkewError", () => {
 describe("isThrottlingError", () => {
   THROTTLING_ERROR_CODES.forEach(name => {
     it(`should declare error with the name "${name}" to be a Throttling error`, () => {
-      checkForErrorType(isThrottlingError, name, true);
+      checkForErrorType(isThrottlingError, { name }, true);
     });
   });
 
@@ -43,7 +49,41 @@ describe("isThrottlingError", () => {
     const name = Math.random().toString(36).substring(2);
     if (!THROTTLING_ERROR_CODES.includes(name)) {
       it(`should not declare error with the name "${name}" to be a Throttling error`, () => {
-        checkForErrorType(isThrottlingError, name, false);
+        checkForErrorType(isThrottlingError, { name }, false);
+      });
+      break;
+    }
+  }
+});
+
+describe("isTransientError", () => {
+  TRANSIENT_ERROR_CODES.forEach(name => {
+    it(`should declare error with the name "${name}" to be a Throttling error`, () => {
+      checkForErrorType(isTransientError, { name }, true);
+    });
+  });
+
+  TRANSIENT_ERROR_STATUS_CODES.forEach(httpStatusCode => {
+    it(`should declare error with the HTTP Status Code "${httpStatusCode}" to be a Throttling error`, () => {
+      checkForErrorType(isTransientError, { httpStatusCode }, true);
+    });
+  });
+
+  while (true) {
+    const name = Math.random().toString(36).substring(2);
+    if (!TRANSIENT_ERROR_CODES.includes(name)) {
+      it(`should not declare error with the name "${name}" to be a Throttling error`, () => {
+        checkForErrorType(isTransientError, { name }, false);
+      });
+      break;
+    }
+  }
+
+  while (true) {
+    const httpStatusCode = Math.ceil(Math.random() * 10 ** 3);
+    if (!TRANSIENT_ERROR_STATUS_CODES.includes(httpStatusCode)) {
+      it(`should declare error with the HTTP Status Code "${httpStatusCode}" to be a Throttling error`, () => {
+        checkForErrorType(isTransientError, { httpStatusCode }, false);
       });
       break;
     }
