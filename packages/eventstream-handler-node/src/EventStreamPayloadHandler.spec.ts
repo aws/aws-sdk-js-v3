b4ef84b52b64a1ed931d78e4959108ae36ba707c
AllanFly120 <zheallan@amazon.com>
2020-06-01 12:31:12 -0700

    test(eventstream-handler-node): fix test broken in Node14 (#1221)
    * test(eventstream-handler-node): fix test broken in Node14
    
    * test(eventstream-handler-node): fix test in Node14

--- packages/eventstream-handler-node/src/EventStreamPayloadHandler.spec.ts
+++ packages/eventstream-handler-node/src/EventStreamPayloadHandler.spec.ts
@@ -39,7 +39,8 @@ describe("EventStreamPayloadHandler", () => {
     ).rejects.toThrow("Eventstream payload must be a Readable stream.");
   });
 
-  it("should close the request payload if downstream middleware throws", async () => {
+  it("should close the request payload if downstream middleware throws", async done => {
+    expect.assertions(2);
     (mockNextHandler as any).mockImplementationOnce(() =>
       Promise.reject(new Error())
     );
@@ -61,9 +62,11 @@ describe("EventStreamPayloadHandler", () => {
     expect(error instanceof Error).toBe(true);
     //Expect stream is closed
     //Ref: should use writableEnded when bumped to Node 13+
-    expect(() => {
-      mockRequest.body.write("");
-    }).toThrowError("write after end");
+    (mockRequest.body as PassThrough).on("error", err => {
+      expect(err.message).toEqual("write after end");
+      done();
+    });
+    mockRequest.body.write("This should be allowed to write.");
   });
 
   it("should call event signer with request signature from signing middleware", async () => {
