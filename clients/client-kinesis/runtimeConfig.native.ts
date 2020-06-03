7e7d3c81aa9eb012cb538dfdc382a45c59caa240
AllanFly120 <zheallan@amazon.com>
2020-06-03 11:35:53 -0700

    feat: codegen enabling eventstream serde in RN (#1236)

--- clients/client-kinesis/runtimeConfig.native.ts
+++ clients/client-kinesis/runtimeConfig.native.ts
@@ -1,6 +1,5 @@
 import { name, version } from "./package.json";
 import { Sha256 } from "@aws-crypto/sha256-js";
-import { invalidFunction } from "@aws-sdk/invalid-dependency";
 import { parseUrl } from "@aws-sdk/url-parser-node";
 import { ClientDefaults } from "./KinesisClient";
 import { ClientDefaultValues as BrowserDefaults } from "./runtimeConfig.browser";
@@ -9,12 +8,6 @@ export const ClientDefaultValues: Required<ClientDefaults> = {
   ...BrowserDefaults,
   runtime: "react-native",
   defaultUserAgent: `aws-sdk-js-v3-react-native-${name}/${version}`,
-  eventStreamSerdeProvider: () => ({
-    serialize: invalidFunction("event stream is not supported in ReactNative."),
-    deserialize: invalidFunction(
-      "event stream is not supported in ReactNative."
-    )
-  }),
   sha256: Sha256,
   urlParser: parseUrl
 };
