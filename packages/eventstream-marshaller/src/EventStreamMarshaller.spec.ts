8f4bb4a76855075bd4f6028f2e882fd87fddb647
Allan Zheng <zheallan@amazon.com>
2020-06-03 17:51:14 -0700

    feat: deprecate util-utf8-universal

--- packages/eventstream-marshaller/src/EventStreamMarshaller.spec.ts
+++ packages/eventstream-marshaller/src/EventStreamMarshaller.spec.ts
@@ -1,6 +1,6 @@
 import { EventStreamMarshaller } from "./EventStreamMarshaller";
 import { vectors } from "./TestVectors.fixture";
-import { fromUtf8, toUtf8 } from "@aws-sdk/util-utf8-universal";
+import { fromUtf8, toUtf8 } from "@aws-sdk/util-utf8-node";
 
 describe("eventstream parsing", () => {
   const marshaller = new EventStreamMarshaller(toUtf8, fromUtf8);
