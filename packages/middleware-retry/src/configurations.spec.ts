a283d539593ddb581aca25f3855dc6418615da10
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-06-09 01:36:51 +0000

    test: complete tests for configurations.ts

--- packages/middleware-retry/src/configurations.spec.ts
+++ packages/middleware-retry/src/configurations.spec.ts
@@ -1,4 +1,5 @@
 import { resolveRetryConfig } from "./configurations";
+import { StandardRetryStrategy } from "./defaultStrategy";
 
 describe("resolveRetryConfig", () => {
   describe("maxAttempts", () => {
@@ -16,10 +17,31 @@ describe("resolveRetryConfig", () => {
   });
 
   describe("retryStrategy", () => {
-    it("uses passed retryStrategy if present", () => {});
+    it("uses passed retryStrategy if present", () => {
+      const mockRetryStrategy = {
+        maxAttempts: 2,
+        retry: jest.fn()
+      };
+      const { retryStrategy } = resolveRetryConfig({
+        retryStrategy: mockRetryStrategy
+      });
+      expect(retryStrategy).toEqual(mockRetryStrategy);
+    });
+
     describe("creates StandardRetryStrategy if retryStrategy not present", () => {
-      it("uses maxAttempts if present", () => {});
-      it("uses default 3 if maxAttempts is not present", () => {});
+      describe("uses maxAttempts if present", () => {
+        [1, 2, 3].forEach(maxAttempts => {
+          const { retryStrategy } = resolveRetryConfig({ maxAttempts });
+          expect(retryStrategy).toBeInstanceOf(StandardRetryStrategy);
+          expect(retryStrategy.maxAttempts).toBe(maxAttempts);
+        });
+      });
+
+      it("uses default 3 if maxAttempts is not present", () => {
+        const { retryStrategy } = resolveRetryConfig({});
+        expect(retryStrategy).toBeInstanceOf(StandardRetryStrategy);
+        expect(retryStrategy.maxAttempts).toBe(3);
+      });
     });
   });
 });
