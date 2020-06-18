83c419ef2c1d96fdb13aad05f504ef8fd6e36382
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-06-18 09:57:26 -0700

    fix: overwrite clients in protocol clients (#1283)

--- scripts/generate-clients/copy-to-clients.js
+++ scripts/generate-clients/copy-to-clients.js
@@ -24,12 +24,13 @@ const getOverwritablePredicate = packageName => pathName => {
   return (
     pathName
       .toLowerCase()
-      .indexOf(
+      .startsWith(
         packageName
           .toLowerCase()
           .replace("@aws-sdk/client-", "")
+          .replace("@aws-sdk/aws-", "")
           .replace(/-/g, "")
-      ) >= 0 || overwritablePathnames.indexOf(pathName) >= 0
+      ) || overwritablePathnames.indexOf(pathName) >= 0
   );
 };
 
