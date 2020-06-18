033fdf53dbf01f903fcd81afc578dc17c95425a7
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-06-18 03:07:41 +0000

    chore: update env variable name and value

--- packages/retry-config-provider/src/fromEnv.spec.ts
+++ packages/retry-config-provider/src/fromEnv.spec.ts
@@ -1,9 +1,9 @@
-import { fromEnv, ENV_REGION } from "./fromEnv";
+import { fromEnv, ENV_MAX_ATTEMPTS } from "./fromEnv";
 import { ProviderError } from "@aws-sdk/property-provider";
 
 describe("fromEnv", () => {
-  const envRegion = process.env[ENV_REGION];
-  const mockEnvRegion = "mockEnvRegion";
+  const envRegion = process.env[ENV_MAX_ATTEMPTS];
+  const mockEnvMaxAttempts = "mockEnvMaxAttempts";
 
   const getProviderError = (envVarName: string) =>
     new ProviderError(
@@ -11,21 +11,21 @@ describe("fromEnv", () => {
     );
 
   beforeEach(() => {
-    delete process.env[ENV_REGION];
+    delete process.env[ENV_MAX_ATTEMPTS];
   });
 
   afterAll(() => {
-    process.env[ENV_REGION] = envRegion;
+    process.env[ENV_MAX_ATTEMPTS] = envRegion;
   });
 
-  it(`returns value in '${ENV_REGION}' env var when set`, () => {
-    process.env[ENV_REGION] = mockEnvRegion;
-    return expect(fromEnv()()).resolves.toBe(mockEnvRegion);
+  it(`returns value in '${ENV_MAX_ATTEMPTS}' env var when set`, () => {
+    process.env[ENV_MAX_ATTEMPTS] = mockEnvMaxAttempts;
+    return expect(fromEnv()()).resolves.toBe(mockEnvMaxAttempts);
   });
 
-  it(`throws when '${ENV_REGION}' env var is not set`, () => {
+  it(`throws when '${ENV_MAX_ATTEMPTS}' env var is not set`, () => {
     return expect(fromEnv()()).rejects.toMatchObject(
-      getProviderError(ENV_REGION)
+      getProviderError(ENV_MAX_ATTEMPTS)
     );
   });
 });
--- packages/retry-config-provider/src/fromEnv.ts
+++ packages/retry-config-provider/src/fromEnv.ts
@@ -1,15 +1,15 @@
 import { ProviderError } from "@aws-sdk/property-provider";
 import { Provider } from "@aws-sdk/types";
 
-export const ENV_REGION = "AWS_REGION";
+export const ENV_MAX_ATTEMPTS = "AWS_MAX_ATTEMPTS";
 
 export const fromEnv = (): Provider<string> => async () => {
-  const envRegion = process.env[ENV_REGION];
+  const envRegion = process.env[ENV_MAX_ATTEMPTS];
   if (envRegion) {
     return envRegion;
   }
 
   throw new ProviderError(
-    `No value defined for the ${ENV_REGION} environment variable`
+    `No value defined for the ${ENV_MAX_ATTEMPTS} environment variable`
   );
 };
