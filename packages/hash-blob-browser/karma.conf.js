bddeb3d172f1e26ff9e429140333d6ea624168c2
AllanFly120 <zheallan@amazon.com>
2020-06-24 17:47:06 -0700

    chore: move karma related devdeps to repo root (#1292)
    * chore: move karma related devdeps to repo root
    
    * chore: update lock file
    
    * test(middleware-sdk-transcribe-streaming): fix unit tests

--- packages/hash-blob-browser/karma.conf.js
+++ packages/hash-blob-browser/karma.conf.js
@@ -1,53 +1,13 @@
-// Karma configuration
-
 process.env.CHROME_BIN = require("puppeteer").executablePath();
-
 module.exports = function (config) {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: "",
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ["jasmine", "karma-typescript"],
-
-    // list of files / patterns to load in the browser
     files: ["src/**/*.ts"],
-
-    // list of files / patterns to exclude
-    exclude: [],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    exclude: ["**/*.d.ts"],
     preprocessors: {
-      "src/**/*.ts": "karma-typescript"
+      "**/*.ts": "karma-typescript"
     },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["progress", "coverage", "karma-typescript"],
-
-    karmaTypescriptConfig: {
-      tsconfig: "./tsconfig.json",
-      bundlerOptions: {
-        addNodeGlobals: false
-      }
-    },
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
+    reporters: ["progress", "karma-typescript"],
     browsers: ["ChromeHeadlessNoSandbox"],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
@@ -55,13 +15,12 @@ module.exports = function (config) {
         flags: ["--no-sandbox"]
       }
     },
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
+    karmaTypescriptConfig: {
+      tsconfig: "./tsconfig.json",
+      bundlerOptions: {
+        addNodeGlobals: false
+      }
+    },
+    singleRun: true
   });
 };
