d735a50c70d1c5752ec14efb8e15c6c4363272a9
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-07-01 18:17:04 +0000

    fix: revert to 0 default retries with comment

--- packages/credential-provider-imds/src/remoteProvider/RemoteProviderInit.ts
+++ packages/credential-provider-imds/src/remoteProvider/RemoteProviderInit.ts
@@ -1,5 +1,8 @@
 export const DEFAULT_TIMEOUT = 1000;
-export const DEFAULT_MAX_RETRIES = 3;
+
+// The default in AWS SDK for Python and CLI (botocore) is no retry or one attempt
+// https://github.com/boto/botocore/blob/646c61a7065933e75bab545b785e6098bc94c081/botocore/utils.py#L273
+export const DEFAULT_MAX_RETRIES = 0;
 
 export interface RemoteProviderConfig {
   /**
