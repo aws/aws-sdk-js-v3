f276aca81b05502496b20f96da71f026ddbb6eb9
Allan Zheng <zheallan@amazon.com>
2020-07-02 09:33:07 -0700

    test: add RN test cases

--- tests/react-native/End2End/App.tsx
+++ tests/react-native/End2End/App.tsx
@@ -8,16 +8,36 @@
  * @format
  */
 
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   StatusBar,
-  Button
+  Button,
+  Text
 } from "react-native";
+import { S3 } from "@aws-sdk/client-s3";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 
 const App = () => {
+  const [responseContent, setResponseContent] = useState("");
+  const [uploadId, setUploadId] = useState("");
+  const [uploadParts, setUploadParts] = useState<
+    Array<{ ETag: string; PartNumber: number }>
+  >([]);
+  const s3 = new S3({
+    credentials: fromCognitoIdentityPool({
+      identityPoolId: process.env.AWS_SMOKE_TEST_IDENTITY_POOL_ID,
+      client: new CognitoIdentityClient({
+        region: "us-west-2"
+      })
+    }),
+    region: "us-west-2"
+  });
+  const Key = `smoke-test-rn`;
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -26,9 +46,115 @@ const App = () => {
           contentInsetAdjustmentBehavior="automatic"
           contentContainerStyle={styles.scrollViewContainer}
         >
-          <React.Fragment>
-            <Button title="S3.PutObject" onPress={() => {}} />
-          </React.Fragment>
+          <Fragment>
+            <Button
+              title="S3.PutObject"
+              testID="s3PutObject"
+              onPress={async () => {
+                const res = await s3.putObject({
+                  Bucket: process.env.AWS_SMOKE_TEST_BUCKET,
+                  Key,
+                  Body: "this a test payload from RN end-to-end test."
+                });
+                setResponseContent(JSON.stringify(res));
+              }}
+            />
+            <Button
+              title="S3.getObject"
+              testID="s3GetObject"
+              onPress={async () => {
+                const res = await s3.getObject({
+                  Bucket: process.env.AWS_SMOKE_TEST_BUCKET,
+                  Key
+                });
+                if (!(res.Body instanceof Blob))
+                  throw new Error(
+                    `Expected Blob payload but got ${res.Body.constructor}`
+                  );
+                const reader = new FileReader();
+                reader.addEventListener("loadend", data => {
+                  setResponseContent(reader.result as string);
+                });
+                reader.readAsText(res.Body);
+              }}
+            />
+            <Button
+              title="S3.listObjects"
+              testID="s3ListObjects"
+              onPress={async () => {
+                const res = await s3.listObjects({
+                  Bucket: process.env.AWS_SMOKE_TEST_BUCKET
+                });
+                setResponseContent(JSON.stringify(res));
+              }}
+            />
+            <Button
+              title="S3.createMultipartUpload"
+              testID="s3createMultipartUpload"
+              onPress={async () => {
+                const res = await s3.createMultipartUpload({
+                  Bucket: process.env.AWS_SMOKE_TEST_BUCKET,
+                  Key: `${Key}-multipart`
+                });
+                setUploadId(res.UploadId);
+                setResponseContent(JSON.stringify(res));
+              }}
+            />
+            <Button
+              title="S3.uploadPart"
+              testID="s3uploadPart"
+              onPress={async () => {
+                console.log("uploadId ", uploadId);
+                const res = await s3.uploadPart({
+                  Bucket: process.env.AWS_SMOKE_TEST_BUCKET,
+                  Key: `${Key}-multipart`,
+                  PartNumber: 1,
+                  UploadId: uploadId,
+                  Body: new Blob(new Array(1024).fill("x"))
+                });
+                uploadParts.push({ PartNumber: 1, ETag: res.ETag });
+                setUploadParts(uploadParts);
+                setResponseContent(JSON.stringify(res));
+              }}
+            />
+            <Button
+              title="S3.listParts"
+              testID="s3ListParts"
+              onPress={async () => {
+                const res = await s3.listParts({
+                  Bucket: process.env.AWS_SMOKE_TEST_BUCKET,
+                  Key: `${Key}-multipart`,
+                  UploadId: uploadId
+                });
+                setResponseContent(JSON.stringify(res));
+              }}
+            />
+            <Button
+              title="S3.completeMultipartUpload"
+              testID="s3CompleteMultipartUpload"
+              onPress={async () => {
+                const res = await s3.completeMultipartUpload({
+                  Bucket: process.env.AWS_SMOKE_TEST_BUCKET,
+                  Key: `${Key}-multipart`,
+                  UploadId: uploadId,
+                  MultipartUpload: { Parts: uploadParts }
+                });
+                setResponseContent(JSON.stringify(res));
+              }}
+            />
+            <Button
+              title="S3.headObject"
+              testID="s3headObject"
+              onPress={async () => {
+                const res = await s3.headObject({
+                  Bucket: process.env.AWS_SMOKE_TEST_BUCKET,
+                  Key: `${Key}-multipart`
+                });
+                setResponseContent(JSON.stringify(res));
+              }}
+            />
+            <Text testID="responseContent">{responseContent}</Text>
+          </Fragment>
         </ScrollView>
       </SafeAreaView>
     </>
--- tests/react-native/End2End/babel.config.js
+++ tests/react-native/End2End/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"]
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "transform-inline-environment-variables",
+      {
+        include: [
+          "AWS_SMOKE_TEST_REGION",
+          "AWS_SMOKE_TEST_BUCKET",
+          "AWS_SMOKE_TEST_IDENTITY_POOL_ID"
+        ]
+      }
+    ]
+  ]
 };
