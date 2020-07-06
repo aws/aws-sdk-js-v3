e75fe712f72ab276037d51e09a47df8923e7244f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-07-06 15:43:26 -0700

    chore(credential-provider-imds): providerConfigFromInit arrow function (#1315)

--- packages/credential-provider-imds/src/remoteProvider/RemoteProviderInit.ts
+++ packages/credential-provider-imds/src/remoteProvider/RemoteProviderInit.ts
@@ -1,4 +1,7 @@
 export const DEFAULT_TIMEOUT = 1000;
+
+// The default in AWS SDK for Python and CLI (botocore) is no retry or one attempt
+// https://github.com/boto/botocore/blob/646c61a7065933e75bab545b785e6098bc94c081/botocore/utils.py#L273
 export const DEFAULT_MAX_RETRIES = 0;
 
 export interface RemoteProviderConfig {
@@ -15,10 +18,7 @@ export interface RemoteProviderConfig {
 
 export type RemoteProviderInit = Partial<RemoteProviderConfig>;
 
-export function providerConfigFromInit(
-  init: RemoteProviderInit
-): RemoteProviderConfig {
-  const { timeout = DEFAULT_TIMEOUT, maxRetries = DEFAULT_MAX_RETRIES } = init;
-
-  return { maxRetries, timeout };
-}
+export const providerConfigFromInit = ({
+  maxRetries = DEFAULT_MAX_RETRIES,
+  timeout = DEFAULT_TIMEOUT
+}: RemoteProviderInit): RemoteProviderConfig => ({ maxRetries, timeout });
