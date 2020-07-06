e02805d1c8d5a42b8d0edcd95c5239f04de33ec1
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-07-06 09:58:14 -0700

    fix: run jest tests from all folders in cjs (#1324)

--- jest.config.base.js
+++ jest.config.base.js
@@ -2,6 +2,6 @@ module.exports = {
   // remove testMatch once we move to ts-jest
   testMatch: [
     "**/__tests__/**/*.js?(x)",
-    "**/dist/cjs/?(*.)+(spec|test).js?(x)"
+    "**/dist/cjs/**/?(*.)+(spec|test).js?(x)"
   ]
 };
