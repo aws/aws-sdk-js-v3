6e86721126c581a82b962d6e4a3ab5c0221dcbe1
Allan Zheng <zheallan@amazon.com>
2020-07-07 14:24:59 -0700

    fix: address PR feedbacks

--- packages/middleware-sdk-transcribe-streaming/src/signer.spec.ts
+++ packages/middleware-sdk-transcribe-streaming/src/signer.spec.ts
@@ -2,28 +2,22 @@ import { HttpRequest } from "@aws-sdk/protocol-http";
 import { SignatureV4 } from "./signer";
 describe("transcribe streaming", () => {
   describe("WebSocket request signer", () => {
-    const toSign = new HttpRequest({
-      headers: {
-        "x-amz-foo": "foo",
-        bar: "bar",
-        "amz-sdk-invocation-id": "123",
-        "amz-sdk-request": "attempt=1",
-        host: "aws.amazon.com"
-      },
-      body: "hello world",
-      query: {
-        prop1: "A",
-        prop2: "B"
-      }
-    });
-    beforeEach(() => {
-      jest.mock("@aws-sdk/protocol-http");
-      ((HttpRequest as any) as jest.Mock).mockReturnValue({
-        isInstance: () => true
-      });
-    });
     it("should invoke base SigV4 signer correctly", async () => {
       expect.assertions(5);
+      const toSign = new HttpRequest({
+        headers: {
+          "x-amz-foo": "foo",
+          bar: "bar",
+          "amz-sdk-invocation-id": "123",
+          "amz-sdk-request": "attempt=1",
+          host: "aws.amazon.com"
+        },
+        body: "hello world",
+        query: {
+          prop1: "A",
+          prop2: "B"
+        }
+      });
       const mockBaseSigner = {
         presign: jest
           .fn()
