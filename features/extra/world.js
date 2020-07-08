065aa85e19d1ac7981ef06698453136721e6fc9c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-07-08 00:44:11 +0000

    chore: remove unused configuration from integ tests

--- features/extra/world.js
+++ features/extra/world.js
@@ -1,14 +1,4 @@
 const world = require("./helpers");
-const fs = require("fs");
-const path = require("path");
-
-try {
-  world.config = JSON.parse(fs.readFileSync(path.resolve("./configuration")));
-} catch (e) {
-} finally {
-  process.env["CONFIGURED_REGION"] = "us-west-2";
-}
-
 const WorldConstructor = function WorldConstructor() {
   return world;
 };
