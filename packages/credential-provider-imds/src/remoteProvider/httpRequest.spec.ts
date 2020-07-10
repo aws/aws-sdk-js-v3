160840b81fbb4703c28a1775dae8ceac0f2e5b04
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-07-10 01:36:03 +0000

    test: use an open port for httpRequest

--- packages/credential-provider-imds/src/remoteProvider/httpRequest.spec.ts
+++ packages/credential-provider-imds/src/remoteProvider/httpRequest.spec.ts
@@ -1,18 +1,37 @@
 import * as nock from "nock";
+import { createServer } from "http";
 import { httpRequest } from "./httpRequest";
 
 describe("httpRequest", () => {
+  let port: number;
   const host = "localhost";
   const path = "/";
+
+  const getOpenPort = async (candidatePort: number = 4321): Promise<number> => {
+    try {
+      return new Promise<number>((resolve, reject) => {
+        const server = createServer();
+        server.on("error", () => reject());
+        server.listen(candidatePort);
+        server.close(() => resolve(candidatePort));
+      });
+    } catch (e) {
+      return await getOpenPort(candidatePort + 1);
+    }
+  };
+
+  beforeAll(async () => {
+    port = await getOpenPort();
+  });
 
   describe("returns response", () => {
     it("defaults to method GET", async () => {
       const expectedResponse = "expectedResponse";
-      const scope = nock("http://" + host)
+      const scope = nock(`http://${host}:${port}`)
         .get(path)
         .reply(200, expectedResponse);
 
-      const response = await httpRequest({ host, path });
+      const response = await httpRequest({ host, path, port });
       expect(response.toString()).toStrictEqual(expectedResponse);
 
       scope.done();
@@ -21,11 +40,11 @@ describe("httpRequest", () => {
     it("uses method passed in options", async () => {
       const method = "POST";
       const expectedResponse = "expectedResponse";
-      const scope = nock("http://" + host)
+      const scope = nock(`http://${host}:${port}`)
         .post(path)
         .reply(200, expectedResponse);
 
-      const response = await httpRequest({ host, path, method });
+      const response = await httpRequest({ host, path, port, method });
       expect(response.toString()).toStrictEqual(expectedResponse);
 
       scope.done();
