764c7c16cd8525df1a76fe07dfb66ad80ca0c5d2
AllanFly120 <zheallan@amazon.com>
2020-07-27 15:33:20 -0700

    test: fix import for elbv2 integ test
    follow up to #1379

--- features/elbv2/step_definitions/elbv2.js
+++ features/elbv2/step_definitions/elbv2.js
@@ -1,7 +1,7 @@
-const { ElasticLoadBalancingv2 } = require("../../../clients/client-elastic-load-balancing-v2");
+const { ElasticLoadBalancingV2 } = require("../../../clients/client-elastic-load-balancing-v2");
 const { Before } = require("cucumber");
 
 Before({ tags: "@elbv2" }, function (scenario, callback) {
-  this.service = new ElasticLoadBalancingv2({});
+  this.service = new ElasticLoadBalancingV2({});
   callback();
 });
