7ba25c4ddd1a87926b3055d32b1cc6e0ba25eeee
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-07-28 05:21:05 +0000

    test: test endpoint in resolveEndpointsConfig if defined

--- packages/config-resolver/src/EndpointsConfig.spec.ts
+++ packages/config-resolver/src/EndpointsConfig.spec.ts
@@ -1,3 +1,5 @@
+import { Endpoint } from "@aws-sdk/types";
+
 import { resolveEndpointsConfig } from "./EndpointsConfig";
 
 describe("EndpointsConfig", () => {
@@ -13,13 +15,39 @@ describe("EndpointsConfig", () => {
 
   describe("tls", () => {
     [true, false].forEach((tls) => {
-      it(`assigns input.tls when it's ${tls}`, () => {
+      it(`returns input.tls when it's ${tls}`, () => {
         expect(resolveEndpointsConfig({ ...input, tls }).tls).toStrictEqual(tls);
       });
     });
 
-    it("assigns true is input.tls is undefined", () => {
+    it("returns true is input.tls is undefined", () => {
       expect(resolveEndpointsConfig({ ...input }).tls).toStrictEqual(true);
+    });
+  });
+
+  describe("endpoint", () => {
+    describe("if defined in input.endpoint", () => {
+      const mockEndpoint: Endpoint = { protocol: "protocol", hostname: "hostname", path: "path" };
+
+      it("returns output of urlParser if endpoint if of type string", () => {
+        const endpoint = "endpoint";
+        urlParser.mockReturnValueOnce(mockEndpoint);
+        expect(resolveEndpointsConfig({ ...input, endpoint }).endpoint()).resolves.toBe(mockEndpoint);
+        expect(urlParser).toHaveBeenCalledTimes(1);
+        expect(urlParser).toHaveBeenCalledWith(endpoint);
+      });
+
+      it("returns promisified endpoint if it's of type object", () => {
+        const endpoint = mockEndpoint;
+        expect(resolveEndpointsConfig({ ...input, endpoint }).endpoint()).resolves.toBe(endpoint);
+        expect(urlParser).not.toHaveBeenCalled();
+      });
+
+      it("returns endpoint if it's already Provider<Endpoint>", () => {
+        const endpoint = () => Promise.resolve(mockEndpoint);
+        expect(resolveEndpointsConfig({ ...input, endpoint }).endpoint()).resolves.toBe(mockEndpoint);
+        expect(urlParser).not.toHaveBeenCalled();
+      });
     });
   });
 });
