ffe353b8c66b5b8d9777aa1c26b72042b1af5285
Alex Forsyth <alex-git@amazon.com>
2020-08-17 12:22:26 -0400

    feat: add pagination global types

---
+++ packages/types/src/pagination.ts
@@ -0,0 +1,14 @@
+/**
+ * Expected type definition of a paginator.
+ */
+export type Paginator<T> = AsyncGenerator<T, T, unknown>;
+
+/**
+ * Expected paginator configuration passed to an operation. Services will extend
+ * this interface definition and may type client further.
+ */
+export interface PaginationConfiguration {
+  client: unknown;
+  pageSize?: number;
+  startingToken?: string;
+}
