214f414ac80c86d292c72e9ca4e9fc15e79d79bf
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-09-01 23:46:51 +0000

    test: add test for getLoggerPlugin

---
+++ packages/middleware-logger/src/loggerMiddleware.spec.ts
@@ -0,0 +1,18 @@
+import { MiddlewareStack } from "@aws-sdk/types";
+
+import { getLoggerPlugin, loggerMiddlewareOptions } from "./loggerMiddleware";
+
+describe("getLoggerPlugin", () => {
+  const mockClientStack = {
+    add: jest.fn(),
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("adds loggerMiddleware", () => {
+    getLoggerPlugin({}).applyToStack((mockClientStack as unknown) as MiddlewareStack<any, any>);
+    expect(mockClientStack.add).toHaveBeenCalledTimes(1);
+    expect(mockClientStack.add.mock.calls[0][1]).toEqual(loggerMiddlewareOptions);
+  });
+});
