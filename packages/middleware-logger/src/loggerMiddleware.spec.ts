d9f1f305da34dce8e733b13b324010b87eec402d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-09-02 00:18:50 +0000

    test: loggerMiddleware without context.logger

--- packages/middleware-logger/src/loggerMiddleware.spec.ts
+++ packages/middleware-logger/src/loggerMiddleware.spec.ts
@@ -1,6 +1,6 @@
-import { MiddlewareStack } from "@aws-sdk/types";
+import { FinalizeHandlerArguments, MiddlewareStack } from "@aws-sdk/types";
 
-import { getLoggerPlugin, loggerMiddlewareOptions } from "./loggerMiddleware";
+import { getLoggerPlugin, loggerMiddleware, loggerMiddlewareOptions } from "./loggerMiddleware";
 
 describe("getLoggerPlugin", () => {
   const mockClientStack = {
@@ -14,5 +14,33 @@ describe("getLoggerPlugin", () => {
     getLoggerPlugin({}).applyToStack((mockClientStack as unknown) as MiddlewareStack<any, any>);
     expect(mockClientStack.add).toHaveBeenCalledTimes(1);
     expect(mockClientStack.add.mock.calls[0][1]).toEqual(loggerMiddlewareOptions);
+  });
+});
+
+describe("loggerMiddleware", () => {
+  const next = jest.fn();
+  const args = {
+    request: {
+      method: "GET",
+      headers: {},
+    },
+  };
+  const mockResponse = {
+    statusCode: 200,
+    headers: {},
+  };
+
+  beforeEach(() => {
+    next.mockResolvedValueOnce(mockResponse);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns without logging if context.logger is not defined", async () => {
+    const response = await loggerMiddleware()(next, {})(args as FinalizeHandlerArguments<any>);
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(response).toStrictEqual(mockResponse);
   });
 });
