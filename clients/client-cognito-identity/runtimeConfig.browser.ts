f053f0c7747bd0f9a7244f5a8271f4a3b96910bd
Rafail Varonos <rafail@varonos.be>
2020-09-09 01:07:38 +0200

    fix build error with tsConfig esModuleInterop

--- clients/client-cognito-identity/runtimeConfig.browser.ts
+++ clients/client-cognito-identity/runtimeConfig.browser.ts
@@ -1,4 +1,4 @@
-import * as packageInfo from './package.json'; 
+import packageInfo from "./package.json"; 
 import { Sha256 } from "@aws-crypto/sha256-browser";
 import { FetchHttpHandler, streamCollector } from "@aws-sdk/fetch-http-handler";
 import { invalidFunction } from "@aws-sdk/invalid-dependency";
