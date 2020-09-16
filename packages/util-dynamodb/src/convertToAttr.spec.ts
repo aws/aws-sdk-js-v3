f51b84bdf5d0b3772fd6a9e3726bdbf44b119824
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-09-16 20:12:10 +0000

    chore: split tests based on types

--- packages/util-dynamodb/src/convertToAttr.spec.ts
+++ packages/util-dynamodb/src/convertToAttr.spec.ts
@@ -1,50 +1,56 @@
 import { convertToAttr } from "./convertToAttr";
 
 describe("convertToAttr", () => {
-  [true, false].forEach((bool) => {
-    it(`returns for boolean: ${bool}`, () => {
-      expect(convertToAttr(bool)).toEqual({ BOOL: bool });
+  describe("boolean", () => {
+    [true, false].forEach((bool) => {
+      it(`returns for boolean: ${bool}`, () => {
+        expect(convertToAttr(bool)).toEqual({ BOOL: bool });
+      });
     });
   });
 
-  [1, Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER].forEach((num) => {
-    it(`returns for number (integer): ${num}`, () => {
-      expect(convertToAttr(num)).toEqual({ N: num.toString() });
+  describe("number", () => {
+    [1, Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER].forEach((num) => {
+      it(`returns for number (integer): ${num}`, () => {
+        expect(convertToAttr(num)).toEqual({ N: num.toString() });
+      });
+    });
+
+    [3.14].forEach((num) => {
+      it(`returns for number (floating point): ${num}`, () => {
+        expect(convertToAttr(num)).toEqual({ N: num.toString() });
+      });
+    });
+
+    [Number.NaN, Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY].forEach((num) => {
+      it(`throws for number (special numeric value): ${num}`, () => {
+        expect(() => {
+          convertToAttr(num);
+        }).toThrowError(`Special numeric value ${num} is not allowed`);
+      });
+    });
+
+    [Number.MAX_SAFE_INTEGER + 1].forEach((num) => {
+      it(`throws for number greater than Number.MAX_SAFE_INTEGER: ${num}`, () => {
+        expect(() => {
+          convertToAttr(num);
+        }).toThrowError(`Number ${num} is greater than Number.MAX_SAFE_INTEGER. Use BigInt.`);
+      });
+    });
+
+    [Number.MIN_SAFE_INTEGER - 1].forEach((num) => {
+      it(`throws for number lesser than Number.MIN_SAFE_INTEGER: ${num}`, () => {
+        expect(() => {
+          convertToAttr(num);
+        }).toThrowError(`Number ${num} is lesser than Number.MIN_SAFE_INTEGER. Use BigInt.`);
+      });
     });
   });
 
-  [3.14].forEach((num) => {
-    it(`returns for number (floating point): ${num}`, () => {
-      expect(convertToAttr(num)).toEqual({ N: num.toString() });
+  describe("string", () => {
+    it("returns for string", () => {
+      const str = "str";
+      expect(convertToAttr(str)).toEqual({ S: str });
     });
-  });
-
-  [Number.NaN, Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY].forEach((num) => {
-    it(`throws for number (special numeric value): ${num}`, () => {
-      expect(() => {
-        convertToAttr(num);
-      }).toThrowError(`Special numeric value ${num} is not allowed`);
-    });
-  });
-
-  [Number.MAX_SAFE_INTEGER + 1].forEach((num) => {
-    it(`throws for number greater than Number.MAX_SAFE_INTEGER: ${num}`, () => {
-      expect(() => {
-        convertToAttr(num);
-      }).toThrowError(`Number ${num} is greater than Number.MAX_SAFE_INTEGER. Use BigInt.`);
-    });
-  });
-
-  [Number.MIN_SAFE_INTEGER - 1].forEach((num) => {
-    it(`throws for number lesser than Number.MIN_SAFE_INTEGER: ${num}`, () => {
-      expect(() => {
-        convertToAttr(num);
-      }).toThrowError(`Number ${num} is lesser than Number.MIN_SAFE_INTEGER. Use BigInt.`);
-    });
-  });
-
-  it("returns for string", () => {
-    const str = "str";
-    expect(convertToAttr(str)).toEqual({ S: str });
   });
 });
