16675143b5bc9bb91947fd7797c5238bbca5816d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-09-16 18:07:17 +0000

    feat: add boolean to convertToAttr

--- packages/util-dynamodb/src/convertToAttr.spec.ts
+++ packages/util-dynamodb/src/convertToAttr.spec.ts
@@ -1,6 +1,12 @@
 import { convertToAttr } from "./convertToAttr";
 
 describe("convertToAttr", () => {
+  [true, false].forEach((bool) => {
+    it(`test boolean "${bool}"`, () => {
+      expect(convertToAttr(bool)).toEqual({ BOOL: bool });
+    });
+  });
+
   it("returns for number", () => {
     const num = 5;
     expect(convertToAttr(num)).toEqual({ N: num.toString() });
--- packages/util-dynamodb/src/convertToAttr.ts
+++ packages/util-dynamodb/src/convertToAttr.ts
@@ -3,7 +3,9 @@ import { AttributeValue } from "@aws-sdk/client-dynamodb";
 import { NativeAttributeValue } from "./models";
 
 export const convertToAttr = (inputVal: NativeAttributeValue): AttributeValue => {
-  if (typeof inputVal === "number") {
+  if (typeof inputVal === "boolean") {
+    return { BOOL: inputVal };
+  } else if (typeof inputVal === "number") {
     return { N: inputVal.toString() };
   } else {
     // @ts-ignore
