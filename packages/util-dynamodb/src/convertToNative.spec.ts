f3599409cc83ee00b7d89a904cd0f1ec0caa3230
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-09-21 16:47:35 +0000

    feat: basic convertToNative with support for string and number

---
+++ packages/util-dynamodb/src/convertToNative.spec.ts
@@ -0,0 +1,40 @@
+import { convertToNative } from "./convertToNative";
+
+describe("convertToNative", () => {
+  const input = {
+    B: undefined,
+    BOOL: undefined,
+    BS: undefined,
+    L: undefined,
+    M: undefined,
+    N: undefined,
+    NS: undefined,
+    NULL: undefined,
+    S: undefined,
+    SS: undefined,
+  };
+
+  describe("number", () => {
+    ["1", Number.MAX_SAFE_INTEGER.toString(), Number.MIN_SAFE_INTEGER.toString()].forEach((num) => {
+      it(`returns for number (integer): ${num}`, () => {
+        expect(convertToNative({ ...input, N: num })).toEqual(Number(num));
+      });
+    });
+
+    ["1.01", Math.PI.toString(), Math.E.toString(), Number.MIN_VALUE.toString(), Number.EPSILON.toString()].forEach(
+      (num) => {
+        it(`returns for number (floating point): ${num}`, () => {
+          expect(convertToNative({ ...input, N: num })).toEqual(Number(num));
+        });
+      }
+    );
+  });
+
+  describe("string", () => {
+    ["", "string", "'single-quote'", '"double-quote"'].forEach((str) => {
+      it(`returns for string: ${str}`, () => {
+        expect(convertToNative({ ...input, S: str })).toEqual(str);
+      });
+    });
+  });
+});
---
+++ packages/util-dynamodb/src/convertToNative.ts
@@ -0,0 +1,22 @@
+import { AttributeValue } from "@aws-sdk/client-dynamodb";
+
+import { NativeAttributeValue } from "./models";
+
+/**
+ * Convert a DynamoDB AttributeValue object to its equivalent JavaScript type.
+ *
+ */
+export const convertToNative = (data: AttributeValue): NativeAttributeValue => {
+  for (const type in data) {
+    // @ts-expect-error Element implicitly has an 'any' type
+    if (data[type] !== undefined) {
+      if (type === "N") {
+        return Number(data[type]);
+      } else if (type === "S") {
+        return data[type] as string;
+      }
+      throw new Error(`Unsupported type passed: ${type}`);
+    }
+  }
+  throw new Error(`No value defined in convertToNative: ${data}`);
+};
