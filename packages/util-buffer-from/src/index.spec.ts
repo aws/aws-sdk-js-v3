aae33f4678803973fc7ff759bab1277b3b560899
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-10-13 00:45:11 +0000

    test: simplify index.spec.ts

--- packages/util-buffer-from/src/index.spec.ts
+++ packages/util-buffer-from/src/index.spec.ts
@@ -1,38 +1,25 @@
-jest.mock("buffer", () => {
-  const Buffer = jest.fn().mockReturnValue(new Uint8Array(0));
-  (Buffer as any).from = jest.fn().mockReturnValue(new Uint8Array(0));
-
-  return { Buffer };
-});
 import { Buffer } from "buffer";
 
 import { fromArrayBuffer, fromString } from "./";
+
+jest.mock("buffer");
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe("fromArrayBuffer", () => {
   it("throws if argument is not an ArrayBuffer", () => {
     expect(() => fromArrayBuffer(255 as any)).toThrow();
   });
 
-  describe("Buffer.from", () => {
-    beforeEach(() => {
-      (Buffer.from as any).mockClear();
-    });
-
-    it("returns if argument is an ArrayBuffer", () => {
-      const underlyingBuffer = new ArrayBuffer(0);
-      const offsetArg = 12;
-      const lengthArg = 13;
-      fromArrayBuffer(underlyingBuffer, offsetArg, lengthArg);
-
-      const { calls } = (Buffer.from as any).mock;
-      expect(calls.length).toBe(1);
-      expect(calls[0].length).toBe(3);
-
-      const [buffer, offset, length] = calls[0];
-      expect(buffer).toBe(underlyingBuffer);
-      expect(offset).toBe(offsetArg);
-      expect(length).toBe(lengthArg);
-    });
+  it("returns if argument is an ArrayBuffer", () => {
+    const underlyingBuffer = new ArrayBuffer(0);
+    const offsetArg = 12;
+    const lengthArg = 13;
+    fromArrayBuffer(underlyingBuffer, offsetArg, lengthArg);
+    expect(Buffer.from).toHaveBeenCalledTimes(1);
+    expect(Buffer.from).toHaveBeenCalledWith(underlyingBuffer, offsetArg, lengthArg);
   });
 });
 
@@ -41,23 +28,12 @@ describe("fromString", () => {
     expect(() => fromString(255 as any)).toThrow();
   });
 
-  describe("Buffer.from", () => {
-    beforeEach(() => {
-      (Buffer.from as any).mockClear();
-    });
+  it("returns if argument is an ArrayBuffer", () => {
+    const inputArg = "a string";
+    const encodingArg = "utf16le";
+    fromString(inputArg, encodingArg);
 
-    it("returns if argument is an ArrayBuffer", () => {
-      const inputArg = "a string";
-      const encodingArg = "utf16le";
-      fromString(inputArg, encodingArg);
-
-      const { calls } = (Buffer.from as any).mock;
-      expect(calls.length).toBe(1);
-      expect(calls[0].length).toBe(2);
-
-      const [input, encoding] = calls[0];
-      expect(input).toBe(inputArg);
-      expect(encoding).toBe(encodingArg);
-    });
+    expect(Buffer.from).toHaveBeenCalledTimes(1);
+    expect(Buffer.from).toHaveBeenCalledWith(inputArg, encodingArg);
   });
 });
