d0777d4d0fb438b02eddf3188a86c7e8865c04f4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-10-13 09:27:17 -0700

    chore(util-utf8-*): convert to arrow functions (#1566)

--- packages/util-utf8-node/src/index.ts
+++ packages/util-utf8-node/src/index.ts
@@ -1,11 +1,9 @@
 import { fromArrayBuffer, fromString } from "@aws-sdk/util-buffer-from";
 
-export function fromUtf8(input: string): Uint8Array {
+export const fromUtf8 = (input: string): Uint8Array => {
   const buf = fromString(input, "utf8");
-
   return new Uint8Array(buf.buffer, buf.byteOffset, buf.byteLength / Uint8Array.BYTES_PER_ELEMENT);
-}
+};
 
-export function toUtf8(input: Uint8Array): string {
-  return fromArrayBuffer(input.buffer, input.byteOffset, input.byteLength).toString("utf8");
-}
+export const toUtf8 = (input: Uint8Array): string =>
+  fromArrayBuffer(input.buffer, input.byteOffset, input.byteLength).toString("utf8");
