ae8c1415d116c7795d750214ce3f28c0d8a0f834
Alex Forsyth <alex-git@amazon.com>
2020-10-15 16:20:23 -0400

    feat: adding common workspace configuations for lib

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const base = require("./jest.config.base.js");
 module.exports = {
   ...base,
   projects: [
+    "<rootDir>/lib/*/jest.config.js",
     "<rootDir>/protocol_tests/*/jest.config.js",
     "<rootDir>/packages/*/jest.config.js",
     "<rootDir>/clients/*/jest.config.js",
