bf91b743a89fed926af01108469ae904c516e191
Alex Forsyth <alex-git@amazon.com>
2020-10-23 15:38:39 -0400

    feat: showing example code using polly presigner to get url

---
+++ packages/polly-request-presigner/example-code/usage.ts
@@ -0,0 +1,29 @@
+import { Polly, PollyClient } from "@aws-sdk/client-polly";
+
+import { getSynthesizeSpeechUrl } from "../src/index";
+
+const synthesizeSpeechParams = {
+  Text: "Hello world, the poly presigner is really cool!",
+  OutputFormat: "mp3",
+  VoiceId: "Kimberly",
+};
+
+// do Synthesize with full poly
+(async () => {
+  let url = await getSynthesizeSpeechUrl({
+    client: new Polly({
+      region: "us-east-1",
+    }),
+    params: synthesizeSpeechParams,
+  });
+  console.log(url);
+})();
+
+// do Synthesize with poly client
+(async () => {
+  let url = await getSynthesizeSpeechUrl({
+    client: new PollyClient({}),
+    params: synthesizeSpeechParams,
+  });
+  console.log(url);
+})();
