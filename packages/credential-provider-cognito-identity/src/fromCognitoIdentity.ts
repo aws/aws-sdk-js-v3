17c8518e24bcd4746525c0d312a0cc41e255235c
Alex Forsyth <alex-git@amazon.com>
2020-10-28 12:49:26 -0400

    fix: propogate identityId from cognito identity

--- packages/credential-provider-cognito-identity/src/fromCognitoIdentity.ts
+++ packages/credential-provider-cognito-identity/src/fromCognitoIdentity.ts
@@ -5,6 +5,13 @@ import { CredentialProvider, Credentials } from "@aws-sdk/types";
 import { CognitoProviderParameters } from "./CognitoProviderParameters";
 import { resolveLogins } from "./resolveLogins";
 
+export interface CognitoIdentityCredentials extends Credentials {
+  /**
+   * The Cognito ID returned by the last call to AWS.CognitoIdentity.getOpenIdToken().
+   */
+  identityId: string;
+}
+
 /**
  * Retrieves temporary AWS credentials using Amazon Cognito's
  * `GetCredentialsForIdentity` operation.
@@ -12,7 +19,7 @@ import { resolveLogins } from "./resolveLogins";
  * Results from this function call are not cached internally.
  */
 export function fromCognitoIdentity(parameters: FromCognitoIdentityParameters): CredentialProvider {
-  return async (): Promise<Credentials> => {
+  return async (): Promise<CognitoIdentityCredentials> => {
     const {
       Credentials: {
         AccessKeyId = throwOnMissingAccessKeyId(),
@@ -29,6 +36,7 @@ export function fromCognitoIdentity(parameters: FromCognitoIdentityParameters): 
     );
 
     return {
+      identityId: parameters.identityId,
       accessKeyId: AccessKeyId,
       secretAccessKey: SecretKey,
       sessionToken: SessionToken,
