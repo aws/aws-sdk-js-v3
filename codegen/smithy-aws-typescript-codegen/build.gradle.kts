2e6a9e62066e5fb99c535a832ecbdbc36d7c9c3d
Chase Coalwell <chasecoa@amazon.com>
2020-11-13 15:04:49 -0800

    fix(codegen): add aws-iam-traits dependency

--- codegen/smithy-aws-typescript-codegen/build.gradle.kts
+++ codegen/smithy-aws-typescript-codegen/build.gradle.kts
@@ -34,6 +34,7 @@ tasks.withType<Test> {
 
 dependencies {
     api("software.amazon.smithy:smithy-aws-traits:1.0.5")
+    api("software.amazon.smithy:smithy-aws-iam-traits:1.0.5")
     api("software.amazon.smithy:smithy-typescript-codegen:0.2.0")
     testCompile("org.junit.jupiter:junit-jupiter-api:5.4.0")
     testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.0")
