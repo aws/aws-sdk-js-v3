cc8d14086be24d0f17c3dc50f51baa82be1d231f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-11-19 02:32:20 +0000

    fix(abort-controller): make AbortSignal WHATWG Spec compliant

--- packages/abort-controller/src/AbortSignal.ts
+++ packages/abort-controller/src/AbortSignal.ts
@@ -1,8 +1,8 @@
 import { AbortHandler, AbortSignal as IAbortSignal } from "@aws-sdk/types";
 
 export class AbortSignal implements IAbortSignal {
-  public onabort?: AbortHandler;
-  private _aborted!: boolean;
+  public onabort: AbortHandler | null = null;
+  private _aborted = false;
 
   constructor() {
     Object.defineProperty(this, "_aborted", {
@@ -24,8 +24,8 @@ export class AbortSignal implements IAbortSignal {
   abort(): void {
     this._aborted = true;
     if (this.onabort) {
-      this.onabort();
-      this.onabort = undefined;
+      this.onabort(this);
+      this.onabort = null;
     }
   }
 }
