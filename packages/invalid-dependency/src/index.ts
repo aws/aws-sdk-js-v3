dad74c034482299e0fa99b6f61fb30d72bd2f572
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-11-25 16:47:06 +0000

    fix(invalid-dependency): invalidAsyncFunction to reject with error

--- packages/invalid-dependency/src/index.ts
+++ packages/invalid-dependency/src/index.ts
@@ -2,4 +2,4 @@ export const invalidFunction = (message: string) => () => {
   throw new Error(message);
 };
 
-export const invalidAsyncFunction = (message: string) => () => Promise.resolve(new Error(message));
+export const invalidAsyncFunction = (message: string) => () => Promise.reject(new Error(message));
