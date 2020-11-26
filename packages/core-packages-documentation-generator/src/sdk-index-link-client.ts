13dc46e6043af204cc0c4e7f1d8c7e7e61fc5c50
Allan Zheng <zheallan@amazon.com>
2020-11-25 18:07:44 -0800

    docs(core-packages-documentation-generator): update landing page navigation

--- packages/core-packages-documentation-generator/src/sdk-index-link-client.ts
+++ packages/core-packages-documentation-generator/src/sdk-index-link-client.ts
@@ -2,6 +2,7 @@ import { isAbsolute, join, relative, resolve, sep } from "path";
 import { BindOption } from "typedoc";
 import { Component, RendererComponent } from "typedoc/dist/lib/output/components";
 import { PageEvent } from "typedoc/dist/lib/output/events";
+import { NavigationItem } from "typedoc/dist/lib/output/models/NavigationItem";
 
 const PROJECT_ROOT = join(__dirname, "..", "..", "..", "..");
 @Component({ name: "SdkIndexLinkClientPlugin" })
@@ -25,26 +26,69 @@ export class SdkIndexLinkClientPlugin extends RendererComponent {
   onPageBegin(page: PageEvent) {
     const out = isAbsolute(this.out) ? this.out : resolve(PROJECT_ROOT, this.out);
     const clientDocs = isAbsolute(this.clientDocs) ? this.clientDocs : resolve(PROJECT_ROOT, this.clientDocs);
-
     // Get relative path from core packages doc to clients' doc.
     const clientDocsPattern = relative(out, clientDocs);
+
     if (page.model === page.project) {
-      // Entry point index.html and global.html page.
-      page.project.children
-        .filter((child) => child.sources[0].fileName.startsWith(`clients${sep}`))
-        .forEach((child) => {
-          // "clients/client-s3" => "client-s3"
-          const clientName = child.sources[0].fileName.split(sep)[1];
-          const clientDocDir = clientDocsPattern.replace(/{{CLIENT}}/g, clientName);
-          child.url = join(clientDocDir, "index.html");
-          // @ts-ignore attach temporary flag.
-          child._skipRendering = true;
-        });
+      page.navigation = this.groupNavigation(page.navigation);
+
+      page.navigation.children.filter(this.isClient).forEach((child) => {
+        // "clients/client-s3" => "client-s3"
+        const clientName = child.reflection.sources[0].fileName.split(sep)[1];
+        const clientDocDir = clientDocsPattern.replace(/{{CLIENT}}/g, clientName);
+        child.url = join(clientDocDir, "index.html");
+        // @ts-ignore attach temporary flag.
+        child.reflection._skipRendering = true;
+      });
     }
 
     // Skip rendering empty landing page for each client.
     if (page.model._skipRendering) {
       page.preventDefault();
     }
+  }
+
+  /**
+   * Group navigation in Client, Packages and Libraries sections. It will update the
+   * supplied navigation object;
+   */
+  private groupNavigation(navigation: NavigationItem): NavigationItem {
+    if (this.isGrouped(navigation)) return navigation;
+
+    const modules = navigation.children.filter((child) => child?.reflection?.sources[0].fileName);
+    const clients: NavigationItem[] = [];
+    const packages: NavigationItem[] = [];
+    const libs: NavigationItem[] = [];
+    const isLib = (item: NavigationItem) => item?.reflection?.sources[0].fileName.startsWith(`lib${sep}`);
+    modules.forEach((item) => {
+      if (this.isClient(item)) {
+        clients.push(item);
+      } else if (isLib(item)) {
+        libs.push(item);
+      } else {
+        packages.push(item);
+      }
+    });
+
+    navigation.children = [
+      new NavigationItem("Clients"),
+      ...clients,
+      new NavigationItem("Libraries"),
+      ...libs,
+      new NavigationItem("Packages"),
+      ...packages,
+    ];
+    return navigation;
+  }
+
+  private isGrouped(navigation: NavigationItem): boolean {
+    const childrenNames = navigation.children.map((child) => child.title);
+    return (
+      childrenNames.includes("Clients") && childrenNames.includes("Packages") && childrenNames.includes("Libraries")
+    );
+  }
+
+  private isClient(item: NavigationItem): boolean {
+    return item?.reflection?.sources[0].fileName.startsWith(`clients${sep}`);
   }
 }
