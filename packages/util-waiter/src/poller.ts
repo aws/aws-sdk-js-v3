a62010786217b98df1937f6d997f68572da3f5c3
Alex Forsyth <alex-git@amazon.com>
2020-12-01 16:04:32 -0500

    feat: generic forever polling

---
+++ packages/util-waiter/src/poller.ts
@@ -0,0 +1,38 @@
+import { sleep } from "./utils/sleep";
+import { WaiterOptions, WaiterResult, WaiterState } from "./waiter";
+
+function exponentialBackoff(floor: number, ciel: number, attempt: number): number {
+  return Math.min(ciel, floor * 2 ** (attempt - 1));
+}
+
+/**
+ * Function that runs indefinite polling as part of waiters.
+ * @param params options passed to the waiter.
+ * @param client AWS SDK Client
+ * @param input client input
+ * @param stateChecker function that checks the acceptor states on each poll.
+ */
+export async function runPolling<T, S>(
+  params: WaiterOptions,
+  client: T,
+  input: S,
+  acceptorChecks: (client: T, input: S) => Promise<WaiterResult>
+): Promise<WaiterResult> {
+  let currentAttempt = 1;
+  let currentDelay = params.minDelay;
+
+  while (true) {
+    await sleep(currentDelay);
+    if (params.abortController && params.abortController.signal) {
+      return { state: WaiterState.RETRY };
+    }
+
+    const { state } = await acceptorChecks(client, input);
+    if (state == WaiterState.SUCCESS || state == WaiterState.FAILURE) {
+      return { state };
+    }
+
+    currentAttempt += 1;
+    currentDelay = exponentialBackoff(params.minDelay, params.maxDelay, currentAttempt);
+  }
+}
