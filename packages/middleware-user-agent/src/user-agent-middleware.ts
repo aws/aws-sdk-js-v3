de12942af430107d49181d72ebcdf002a8c90180
Allan Zheng <zheallan@amazon.com>
2020-12-07 10:40:27 -0800

    feat(util-user-agent-node): update node user agent provider to the latest

--- packages/middleware-user-agent/src/user-agent-middleware.ts
+++ packages/middleware-user-agent/src/user-agent-middleware.ts
@@ -4,20 +4,38 @@ import {
   BuildHandlerArguments,
   BuildHandlerOptions,
   BuildHandlerOutput,
+  HandlerExecutionContext,
   MetadataBearer,
   Pluggable,
+  UserAgent,
 } from "@aws-sdk/types";
 
 import { UserAgentResolvedConfig } from "./configurations";
 
+const USER_AGENT = "user-agent";
+const X_AMZ_USER_AGENT = "x-amz-user-agent";
+
+/**
+ * Build user agent header sections from:
+ * 1. runtime-specific default user agent provider;
+ * 2. custom user agent from `customUserAgent` client config;
+ * 3. handler execution context set by internal SDK components;
+ * The built user agent will be set to `x-amz-user-agent` header for ALL the
+ * runtimes.
+ * Please note that any override to the `user-agent` or `x-amz-user-agent` header
+ * in the HTTP request is discouraged. Please use `customUserAgent` client
+ * config or middleware setting the `userAgent` context to generate desired user
+ * agent.
+ */
 export function userAgentMiddleware(options: UserAgentResolvedConfig) {
-  return <Output extends MetadataBearer>(next: BuildHandler<any, any>): BuildHandler<any, any> => (
-    args: BuildHandlerArguments<any>
-  ): Promise<BuildHandlerOutput<Output>> => {
+  return <Output extends MetadataBearer>(
+    next: BuildHandler<any, any>,
+    { userAgent }: HandlerExecutionContext
+  ): BuildHandler<any, any> => (args: BuildHandlerArguments<any>): Promise<BuildHandlerOutput<Output>> => {
     const { request } = args;
     if (!HttpRequest.isInstance(request)) return next(args);
     const { headers } = request;
-    const userAgentHeader = options.runtime === "node" ? "user-agent" : "x-amz-user-agent";
+    const userAgentHeader = options.runtime === "node" ? USER_AGENT : X_AMZ_USER_AGENT;
     if (!headers[userAgentHeader]) {
       headers[userAgentHeader] = `${options.defaultUserAgent}`;
     } else {
@@ -32,6 +50,11 @@ export function userAgentMiddleware(options: UserAgentResolvedConfig) {
     });
   };
 }
+
+/**
+ * @private
+ */
+export const buildUserAgent;
 
 export const getUserAgentMiddlewareOptions: BuildHandlerOptions = {
   name: "getUserAgentMiddleware",
