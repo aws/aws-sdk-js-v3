ab29da147e547de92230658b064370f0d193794e
Allan Zheng <zheallan@amazon.com>
2020-12-08 17:07:00 -0800

    feat(util-user-agent-browser): detect browser UA in new format

--- packages/util-user-agent-browser/src/index.spec.ts
+++ packages/util-user-agent-browser/src/index.spec.ts
@@ -1,17 +1,13 @@
 import { defaultUserAgent } from ".";
 
-it("should response basic browser default user agent", () => {
-  let originUserAgent = "";
-  if (typeof navigator === "undefined") {
-    navigator = {} as any;
-    originUserAgent =
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36";
-    (navigator.userAgent as any) = originUserAgent;
-  } else {
-    originUserAgent = navigator.userAgent;
-  }
-  const userAgent = defaultUserAgent("client-s3", "0.1.0");
-  expect(userAgent).toEqual(expect.stringContaining("aws-sdk-js/0.1.0"));
-  expect(userAgent).toEqual(expect.stringContaining(originUserAgent));
-  expect(userAgent).toEqual(expect.stringContaining("sdk-client/client-s3"));
+it("should response basic browser default user agent", async () => {
+  const ua =
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36";
+  jest.spyOn(window.navigator, "userAgent", "get").mockReturnValue(ua);
+  const userAgent = await defaultUserAgent("s3", "0.1.0")();
+  expect(userAgent).toContainEqual(["aws-sdk-js", "0.1.0"]);
+  expect(userAgent).toContainEqual(["api/s3", "0.1.0"]);
+  expect(userAgent).toContainEqual(["os/macOS", "10.15.7"]);
+  expect(userAgent).toContainEqual(["md/browser", "Chrome_86.0.4240.111"]);
+  jest.clearAllMocks();
 });
--- packages/util-user-agent-browser/src/index.ts
+++ packages/util-user-agent-browser/src/index.ts
@@ -1,13 +1,26 @@
-export const defaultUserAgent = (packageName: string, packageVersion: string): string =>
-  [
+import { Provider, UserAgent } from "@aws-sdk/types";
+import { parse } from "bowser";
+
+/**
+ * Default provider to the user agent in browsers. It's a best effort to infer
+ * the device information. It uses bowser library to detect the browser and virsion
+ */
+export const defaultUserAgent = (packageName: string, packageVersion: string): Provider<UserAgent> => {
+  // TODO: remove this post GA and version changed to 3.x.x
+  const version = packageVersion.replace(/^1\./, "3.");
+  const parsedUA = window?.navigator?.userAgent ? parse(window.navigator.userAgent) : undefined;
+  return async () => [
     // sdk-metadata
-    // TODO: remove this post GA and version changed to 3.x.x
-    `aws-sdk-js/${packageVersion.replace(/^1\./, "3.")}`,
+    ["aws-sdk-js", version],
+    // api-metadata
+    // TODO: use api name instead of package name
+    [`api/${packageName}`, version],
     // os-metadata
-    window?.navigator?.product,
-    window?.navigator?.userAgent,
+    [`os/${parsedUA?.os?.name || "other"}`, parsedUA?.os?.version],
     // language-metadata
-    `sdk-client/${packageName}`,
-  ]
-    .filter((section) => section && section.length > 0)
-    .join(" ");
+    // ECMAScript edition doesn't matter in JS. This is a best guess.
+    ["lang/js", "ES5"],
+    // browser vendor and version. Use `brows` to save 2 bytes on wire.
+    ["md/browser", `${parsedUA?.browser?.name ?? "unknown"}_${parsedUA?.browser?.version ?? "unknown"}`],
+  ];
+};
