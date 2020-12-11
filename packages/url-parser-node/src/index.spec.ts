7e7feb162aa0648b28b0e7a31457a7aedcb1465f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-12-11 12:51:39 -0800

    chore(url-parser-node): use WHATWG URL instead of url.parse (#1766)

--- packages/url-parser-node/src/index.spec.ts
+++ packages/url-parser-node/src/index.spec.ts
@@ -3,43 +3,61 @@ import { Endpoint } from "@aws-sdk/types";
 import { parseUrl } from "./";
 
 describe("parseUrl", () => {
-  const testCases = new Map<string, Endpoint>([
-    [
-      "https://www.example.com/path/to%20the/file.ext?snap=cr%C3%A4ckle&snap=p%C3%B4p&fizz=buzz&quux",
-      {
-        protocol: "https:",
-        hostname: "www.example.com",
-        path: "/path/to%20the/file.ext",
-        query: {
-          snap: ["cräckle", "pôp"],
-          fizz: "buzz",
-          quux: null,
+  describe("should correctly parse string", () => {
+    const successCases: [string, Endpoint][] = [
+      [
+        "https://www.example.com/path/to%20the/file.ext?snap=cr%C3%A4ckle&snap=p%C3%B4p&fizz=buzz&quux",
+        {
+          protocol: "https:",
+          hostname: "www.example.com",
+          path: "/path/to%20the/file.ext",
+          query: {
+            snap: ["cräckle", "pôp"],
+            fizz: "buzz",
+            quux: null,
+          },
         },
-      },
-    ],
-    [
-      "http://example.com:54321",
-      {
-        protocol: "http:",
-        hostname: "example.com",
-        port: 54321,
-        path: "/",
-      },
-    ],
-    [
-      "https://example.com?foo=bar",
-      {
-        protocol: "https:",
-        hostname: "example.com",
-        path: "/",
-        query: { foo: "bar" },
-      },
-    ],
-  ]);
+      ],
+      [
+        "http://example.com:54321",
+        {
+          protocol: "http:",
+          hostname: "example.com",
+          port: 54321,
+          path: "/",
+        },
+      ],
+      [
+        "https://example.com?foo=bar",
+        {
+          protocol: "https:",
+          hostname: "example.com",
+          path: "/",
+          query: { foo: "bar" },
+        },
+      ],
+    ];
 
-  for (const [url, parsed] of testCases) {
-    it(`should correctly parse ${url}`, () => {
-      expect(parseUrl(url)).toEqual(parsed);
+    successCases.forEach(([url, parsed]) => {
+      it(url, () => {
+        expect(parseUrl(url)).toEqual(parsed);
+      });
     });
-  }
+  });
+
+  describe("should throw error", () => {
+    const failureCases: [string, Error][] = [
+      ["example.com", new TypeError("Invalid URL: example.com")],
+      ["endpoint", new TypeError("Invalid URL: endpoint")],
+    ];
+
+    failureCases.forEach(([url, error]) => {
+      it(url, () => {
+        expect(() => {
+          const output = parseUrl(url);
+          console.log(output);
+        }).toThrow(error);
+      });
+    });
+  });
 });
--- packages/url-parser-node/src/index.ts
+++ packages/url-parser-node/src/index.ts
@@ -1,9 +1,8 @@
 import { parseQueryString } from "@aws-sdk/querystring-parser";
 import { Endpoint, QueryParameterBag, UrlParser } from "@aws-sdk/types";
-import { parse } from "url";
 
 export const parseUrl: UrlParser = (url: string): Endpoint => {
-  const { hostname = "localhost", pathname = "/", port, protocol = "https:", search } = parse(url);
+  const { hostname = "localhost", pathname = "/", port, protocol = "https:", search } = new URL(url);
 
   let query: QueryParameterBag | undefined;
   if (search) {
