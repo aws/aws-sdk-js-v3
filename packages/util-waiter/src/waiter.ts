0e282da4e53f2f3cb1825703fd93ab36a973a502
Alex Forsyth <alex-git@amazon.com>
2020-12-23 15:12:14 -0500

    fix: minor update to min/max delay description wording

--- packages/util-waiter/src/waiter.ts
+++ packages/util-waiter/src/waiter.ts
@@ -18,15 +18,15 @@ export interface WaiterConfiguration<Client> {
 
   /**
    * The minimum amount of time to delay between retries in seconds. This value defaults
-   * to 2 if not specified. If specified, this value MUST be greater than or equal to 1
-   * and less than or equal to maxDelay.
+   * to service default if not specified. If specified, this value MUST be greater than
+   * or equal to 1 and less than or equal to maxDelay.
    */
   minDelay?: number;
 
   /**
    * The maximum amount of time to delay between retries in seconds. The maximum amount
-   * of time in seconds to delay between each retry. This value defaults to 120 if not
-   * specified (2 minutes). If specified, this value MUST be greater than or equal to 1.
+   * of time in seconds to delay between each retry. This value defaults to service default
+   * if not specified. If specified, this value MUST be greater than or equal to 1.
    */
   maxDelay?: number;
 }
