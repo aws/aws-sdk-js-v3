6d8385a5cdd050133fd1b543293769e21d10520b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-01-12 02:19:57 +0000

    fix: remove limit of 64 characters on valid hostname

--- packages/protocol-http/src/isValidHostname.ts
+++ packages/protocol-http/src/isValidHostname.ts
@@ -1,4 +1,4 @@
 export function isValidHostname(hostname: string): boolean {
-  const hostPattern = /^[a-z0-9][a-z0-9\.\-]{1,61}[a-z0-9]$/;
+  const hostPattern = /^[a-z0-9][a-z0-9\.\-]*[a-z0-9]$/;
   return hostPattern.test(hostname);
 }
