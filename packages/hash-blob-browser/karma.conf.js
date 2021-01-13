6253b5710a653126804b6498166a8abb91555cc2
AllanFly120 <zheallan@amazon.com>
2021-01-13 01:49:13 +0000

    test(karma): fix karma dependency karma test

--- packages/hash-blob-browser/karma.conf.js
+++ packages/hash-blob-browser/karma.conf.js
@@ -16,7 +16,6 @@ module.exports = function (config) {
       },
     },
     karmaTypescriptConfig: {
-      tsconfig: "./tsconfig.json",
       bundlerOptions: {
         addNodeGlobals: false,
       },
