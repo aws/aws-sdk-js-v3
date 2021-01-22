b5dcbf7bdd0534407f96f786f7f3214ddd3cfc68
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-01-22 01:07:34 +0000

    feat: improve prettier performance by running Promise.all()

--- scripts/generate-clients/code-prettify.js
+++ scripts/generate-clients/code-prettify.js
@@ -1,12 +1,18 @@
 // @ts-check
 const { spawnProcess } = require("../utils/spawn-process");
+const { readdirSync } = require("fs");
 const path = require("path");
 
 const prettifyCode = async (dir) => {
-  await spawnProcess(path.join(__dirname, "..", "..", "node_modules", ".bin", "prettier"), [
-    "--write",
-    `${dir}/**/*.{ts,js,md,json}`,
-  ]);
+  const spawnedProcessArr = [];
+  for (const modelName of readdirSync(dir)) {
+    if (modelName === "source") continue;
+    const artifactPath = path.join(dir, modelName, "typescript-codegen");
+    spawnedProcessArr.push(
+      spawnProcess("./node_modules/.bin/prettier", ["--write", `${artifactPath}/**/*.{ts,md,json}`])
+    );
+  }
+  await Promise.all(spawnedProcessArr);
 };
 
 module.exports = {
