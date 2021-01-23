0e90f4ff6f6935c2a4a9c2e5fc74cf27a17383a4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-01-23 17:47:15 +0000

    test(util-dynamodb): accept interface as in input for marshall

--- packages/util-dynamodb/src/marshall.spec.ts
+++ packages/util-dynamodb/src/marshall.spec.ts
@@ -4,31 +4,76 @@ import { marshall } from "./marshall";
 jest.mock("./convertToAttr");
 
 describe("marshall", () => {
-  const input = { a: "A", b: "B" };
-
-  beforeEach(() => {
-    (convertToAttr as jest.Mock).mockReturnValue({ M: input });
-  });
-
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it("calls convertToAttr", () => {
-    expect(marshall(input)).toEqual(input);
-    expect(convertToAttr).toHaveBeenCalledTimes(1);
-    expect(convertToAttr).toHaveBeenCalledWith(input, undefined);
+  describe("with object as an input", () => {
+    const input = { a: "A", b: "B" };
+    const output = { a: { S: "A" }, b: { S: "B" } };
+
+    beforeEach(() => {
+      (convertToAttr as jest.Mock).mockReturnValue({ M: output });
+    });
+
+    it("calls convertToAttr with object input", () => {
+      expect(marshall(input)).toEqual(output);
+      expect(convertToAttr).toHaveBeenCalledTimes(1);
+      expect(convertToAttr).toHaveBeenCalledWith(input, undefined);
+    });
   });
 
   ["convertEmptyValues", "removeUndefinedValues"].forEach((option) => {
     describe(`options.${option}`, () => {
+      const input = { a: "A", b: "B" };
+      const output = { a: { S: "A" }, b: { S: "B" } };
+
+      beforeEach(() => {
+        (convertToAttr as jest.Mock).mockReturnValue({ M: output });
+      });
+
       [false, true].forEach((value) => {
         it(`passes ${value} to convertToAttr`, () => {
-          expect(marshall(input, { [option]: value })).toEqual(input);
+          expect(marshall(input, { [option]: value })).toEqual(output);
           expect(convertToAttr).toHaveBeenCalledTimes(1);
           expect(convertToAttr).toHaveBeenCalledWith(input, { [option]: value });
         });
       });
+    });
+  });
+
+  describe("with type as an input", () => {
+    type TestInputType = { a: string; b: string };
+    const input: TestInputType = { a: "A", b: "B" };
+    const output = { a: { S: "A" }, b: { S: "B" } };
+
+    beforeEach(() => {
+      (convertToAttr as jest.Mock).mockReturnValue({ M: output });
+    });
+
+    it("calls convertToAttr with object input", () => {
+      expect(marshall(input)).toEqual(output);
+      expect(convertToAttr).toHaveBeenCalledTimes(1);
+      expect(convertToAttr).toHaveBeenCalledWith(input, undefined);
+    });
+  });
+
+  describe("with Interface as an input", () => {
+    interface TestInputInterface {
+      a: string;
+      b: string;
+    }
+    const input: TestInputInterface = { a: "A", b: "B" };
+    const output = { a: { S: "A" }, b: { S: "B" } };
+
+    beforeEach(() => {
+      (convertToAttr as jest.Mock).mockReturnValue({ M: output });
+    });
+
+    it("calls convertToAttr with object input", () => {
+      expect(marshall(input)).toEqual(output);
+      expect(convertToAttr).toHaveBeenCalledTimes(1);
+      expect(convertToAttr).toHaveBeenCalledWith(input, undefined);
     });
   });
 });
