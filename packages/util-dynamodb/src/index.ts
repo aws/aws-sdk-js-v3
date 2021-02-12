32dcd0972201d846541416a6a63f11a78477807f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-02-12 17:47:48 +0000

    chore: export DocumentClient

--- packages/util-dynamodb/src/index.ts
+++ packages/util-dynamodb/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./convertToAttr";
 export * from "./convertToNative";
+export * from "./DocumentClient";
 export * from "./marshall";
 export * from "./models";
 export * from "./unmarshall";
