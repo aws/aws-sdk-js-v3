1019be73795409f5c706d777ebd17f03125f8bd5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-02-17 20:02:58 +0000

    chore: code to add getOmitRetryHeadersPlugin

--- clients/client-appintegrations/AppIntegrationsClient.ts
+++ clients/client-appintegrations/AppIntegrationsClient.ts
@@ -45,7 +45,13 @@ import {
   resolveHostHeaderConfig,
 } from "@aws-sdk/middleware-host-header";
 import { getLoggerPlugin } from "@aws-sdk/middleware-logger";
-import { RetryInputConfig, RetryResolvedConfig, getRetryPlugin, resolveRetryConfig } from "@aws-sdk/middleware-retry";
+import {
+  RetryInputConfig,
+  RetryResolvedConfig,
+  getOmitRetryHeadersPlugin,
+  getRetryPlugin,
+  resolveRetryConfig,
+} from "@aws-sdk/middleware-retry";
 import {
   AwsAuthInputConfig,
   AwsAuthResolvedConfig,
@@ -248,6 +254,7 @@ export class AppIntegrationsClient extends __Client<
     this.middlewareStack.use(getHostHeaderPlugin(this.config));
     this.middlewareStack.use(getLoggerPlugin(this.config));
     this.middlewareStack.use(getUserAgentPlugin(this.config));
+    this.middlewareStack.use(getOmitRetryHeadersPlugin(this.config));
   }
 
   destroy(): void {
