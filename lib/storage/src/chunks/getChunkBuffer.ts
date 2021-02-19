05b6f8a9ced4fe5e941580391bdfdeafcc0189a1
Alex Forsyth <alex-git@amazon.com>
2021-02-18 19:47:32 -0500

    fix: remove unneeded add

--- lib/storage/src/chunks/getChunkBuffer.ts
+++ lib/storage/src/chunks/getChunkBuffer.ts
@@ -3,7 +3,7 @@ import { RawDataPart } from "../Upload";
 export async function* getChunkBuffer(data: Buffer, partSize: number): AsyncGenerator<RawDataPart, void, undefined> {
   let partNumber = 1;
   let startByte = 0;
-  let endByte = startByte + partSize;
+  let endByte = partSize;
 
   while (endByte < data.length) {
     yield {
