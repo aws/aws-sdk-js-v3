16e94c118ba2b432c861d7bd09365d252c77561c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-02-21 17:06:10 +0000

    feat(lib-dynamodb): add PutCommand which accepts JS Native types

---
+++ lib/lib-dynamodb/README.md
@@ -0,0 +1,4 @@
+# @aws-sdk/lib-dynamodb
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/lib-dynamodb/latest.svg)](https://www.npmjs.com/package/@aws-sdk/lib-dynamodb)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/lib-dynamodb.svg)](https://www.npmjs.com/package/@aws-sdk/lib-dynamodb)
---
+++ lib/lib-dynamodb/src/commands/PutCommand.ts
@@ -0,0 +1,94 @@
+import {
+  DynamoDBClientResolvedConfig,
+  PutItemCommand,
+  PutItemCommandInput,
+  PutItemCommandOutput,
+  ServiceInputTypes,
+  ServiceOutputTypes,
+} from "@aws-sdk/client-dynamodb";
+import { Command as $Command } from "@aws-sdk/smithy-client";
+import { Handler, HttpHandlerOptions, MiddlewareStack } from "@aws-sdk/types";
+import { marshall, NativeAttributeValue, unmarshall } from "@aws-sdk/util-dynamodb";
+
+import { TranslateConfiguration } from "./types";
+
+export type PutCommandInput = Omit<PutItemCommandInput, "Item"> & {
+  Item: { [key: string]: NativeAttributeValue } | undefined;
+};
+
+export type PutCommandOutput = Omit<PutItemCommandOutput, "Attributes"> & {
+  Attributes: { [key: string]: NativeAttributeValue };
+};
+
+/**
+ * Creates a new item, or replaces an old item with a new item by
+ * delegating to `PutItemCommand`.
+ *
+ * Supply the same parameters as `PutItemCommand` with
+ * `AttributeValue`s substituted by native JavaScript types.
+ *
+ * @see DynamoDB.putItem
+ * @example Create a new item in a table
+ *  const params = {
+ *    TableName : 'Table',
+ *    Item: {
+ *       HashKey: 'haskey',
+ *       NumAttribute: 1,
+ *       BoolAttribute: true,
+ *       ListAttribute: [1, 'two', false],
+ *       MapAttribute: { foo: 'bar'},
+ *       NullAttribute: null
+ *    }
+ *  };
+ *
+ *  const client = new DynamoDBClient({});
+ *
+ *  try {
+ *   const data = await client.send(new PutCommand(params));
+ *   console.log(data);
+ *  } catch(err) {
+ *   console.log(err);
+ *  }
+ *
+ */
+export class PutCommand extends $Command<PutCommandInput, PutCommandOutput, DynamoDBClientResolvedConfig> {
+  private command: PutItemCommand;
+
+  constructor(readonly input: PutCommandInput, private readonly translateConfiguration?: TranslateConfiguration) {
+    super();
+    this.command = new PutItemCommand({
+      ...input,
+      Item: marshall(input.Item, translateConfiguration?.marshallOptions),
+    });
+  }
+
+  /**
+   * @internal
+   */
+  resolveMiddleware(
+    clientStack: MiddlewareStack<ServiceInputTypes, ServiceOutputTypes>,
+    configuration: DynamoDBClientResolvedConfig,
+    options?: HttpHandlerOptions
+  ): Handler<PutCommandInput, PutCommandOutput> {
+    const handler = this.command.resolveMiddleware(clientStack, configuration, options);
+
+    return () =>
+      new Promise((resolve, reject) => {
+        handler(this.command)
+          .then((data) => {
+            resolve({
+              ...data,
+              output: {
+                ...data.output,
+                ...(data.output.Attributes && {
+                  Attributes: unmarshall(data.output.Attributes, this.translateConfiguration?.unmarshallOptions),
+                }),
+              } as PutCommandOutput,
+            });
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+  }
+}
