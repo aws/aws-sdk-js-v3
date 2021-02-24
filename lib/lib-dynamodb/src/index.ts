d0192a1f237baf1d12dfe9f158c3238f23ea913e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-02-24 00:48:26 +0000

    chore: export Batch*Command

--- lib/lib-dynamodb/src/index.ts
+++ lib/lib-dynamodb/src/index.ts
@@ -1,3 +1,5 @@
+export * from "./commands/BatchGetCommand";
+export * from "./commands/BatchWriteCommand";
 export * from "./commands/DeleteCommand";
 export * from "./commands/GetCommand";
 export * from "./commands/PutCommand";
