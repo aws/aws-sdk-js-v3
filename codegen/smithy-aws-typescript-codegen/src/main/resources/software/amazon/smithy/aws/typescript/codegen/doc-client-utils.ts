62a1d2ec1d125c1a1392372d544f4ded70047330
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-03-04 00:38:43 +0000

    fix(lib-dynamodb): util shouldn't override input

--- codegen/smithy-aws-typescript-codegen/src/main/resources/software/amazon/smithy/aws/typescript/codegen/doc-client-utils.ts
+++ codegen/smithy-aws-typescript-codegen/src/main/resources/software/amazon/smithy/aws/typescript/codegen/doc-client-utils.ts
@@ -9,70 +9,56 @@ export type AllNodes = {
   children?: KeyNode[] | AllNodes;
 };
 
-const processKeyInObj = (obj: any, keyNode: KeyNode, processFunc: Function) => {
-  const { key, children } = keyNode;
-  if (Array.isArray(obj)) {
-    // Process key for each element of the array.
-    obj = obj.map((item: any) => processKeyInObj(item, keyNode, processFunc));
-  } else if (obj[key]) {
+const processObj = (obj: any, processFunc: Function, children?: KeyNode[] | AllNodes): any => {
+  if (obj) {
     if (!children || (Array.isArray(children) && children.length === 0)) {
-      // Leaf of KeyNode, process the key.
-      if (Array.isArray(obj[key])) {
-        obj[key] = obj[key].map(processFunc);
-      } else {
-        obj[key] = processFunc(obj[key]);
-      }
+      // Leaf of KeyNode, process the object.
+      return processFunc(obj);
     } else {
       // Not leaf node, process the children.
       if (Array.isArray(children)) {
         // Specific keys of children need to be processed.
-        for (const keyNodeChild of children) {
-          if (Array.isArray(obj[key])) {
-            // Each element of array needs to be processed.
-            obj[key] = obj[key].map((item: any) => processKeyInObj(item, keyNodeChild, processFunc));
-          } else {
-            obj[key] = processKeyInObj(obj[key], keyNodeChild, processFunc);
-          }
-        }
+        return processKeysInObj(obj, processFunc, children);
       } else {
         // All children require processing.
-        obj[key] = processAllKeysInObj(obj[key], children, processFunc);
+        return processAllKeysInObj(obj, processFunc, children.children);
       }
     }
   }
-  return obj;
+  return undefined;
 };
 
-const processKeysInObj = (obj: any, keyNodes: KeyNode[], processFunc: Function) => {
-  for (const keyNode of keyNodes) {
-    obj = processKeyInObj(obj, keyNode, processFunc);
+const processKeyInObj = (obj: any, processFunc: Function, children?: KeyNode[] | AllNodes): any => {
+  if (Array.isArray(obj)) {
+    return obj.map((item: any) => processObj(item, processFunc, children));
   }
-  return obj;
+  return processObj(obj, processFunc, children);
 };
 
-const processAllKeysInObj = (obj: any, allNodes: AllNodes, processFunc: Function) => {
-  const { children } = allNodes;
-  for (const key in obj) {
-    if (!children) {
-      // Leaf, process key without children.
-      obj = processKeyInObj(obj, { key }, processFunc);
-    } else if (Array.isArray(children)) {
-      for (const keyNode of children) {
-        obj[key] = processKeyInObj(obj[key], keyNode, processFunc);
-      }
-    } else {
-      obj[key] = processAllKeysInObj(obj[key], children, processFunc);
-    }
-  }
-  return obj;
-};
+const processKeysInObj = (obj: any, processFunc: Function, keyNodes: KeyNode[]) =>
+  keyNodes.reduce(
+    (acc, { key, children }) => ({
+      ...acc,
+      [key]: processKeyInObj(acc[key], processFunc, children),
+    }),
+    obj
+  );
+
+const processAllKeysInObj = (obj: any, processFunc: Function, children?: KeyNode[] | AllNodes): any =>
+  Object.entries(obj).reduce(
+    (acc, [key, value]) => ({
+      ...acc,
+      [key]: processKeyInObj(value, processFunc, children),
+    }),
+    {}
+  );
 
 export const marshallInput = (obj: any, keyNodes: KeyNode[], options?: marshallOptions) => {
   const marshallFunc = (toMarshall: any) => marshall(toMarshall, options);
-  return processKeysInObj(obj, keyNodes, marshallFunc);
+  return processKeysInObj(obj, marshallFunc, keyNodes);
 };
 
 export const unmarshallOutput = (obj: any, keyNodes: KeyNode[], options?: unmarshallOptions) => {
   const unmarshallFunc = (toMarshall: any) => unmarshall(toMarshall, options);
-  return processKeysInObj(obj, keyNodes, unmarshallFunc);
+  return processKeysInObj(obj, unmarshallFunc, keyNodes);
 };
