99bc743d73006f833acc7fdd5c990b42e4b1defc
Nicolas Perraut <n.perraut@gmail.com>
2021-03-06 17:33:35 +0100

    fix(middleware-retry): make react-native-get-random-values optional
    Related to #1536

--- packages/middleware-retry/src/index.ts
+++ packages/middleware-retry/src/index.ts
@@ -1,3 +1,11 @@
+//reference: https://github.com/uuidjs/uuid#getrandomvalues-not-supported
+try {
+  //@ts-ignore
+  require("react-native-get-random-values");
+} catch {
+  // do nothing
+}
+
 export * from "./retryMiddleware";
 export * from "./omitRetryHeadersMiddleware";
 export * from "./defaultStrategy";
