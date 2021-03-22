952cfda344aba8c1d6e5f24664c8209d0698a1fd
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-03-22 14:43:12 +0000

    fix(client-transcribe-streaming): use ts-jest

--- clients/client-transcribe-streaming/jest.config.js
+++ clients/client-transcribe-streaming/jest.config.js
@@ -1,13 +1,4 @@
-const base = require("../../jest.config.base.js");
-
 module.exports = {
-  ...base,
-  // Only test cjs dist, avoid testing the package twice
-  testPathIgnorePatterns: ["/node_modules/", "/es/", ".*.integ.spec.js"],
-  coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "/commands/",
-    "/protocols/", // protocols tested in protocol protocol_tests folder
-    "endpoints", // endpoint tested in tests/functional/endpoints
-  ],
+  preset: "ts-jest",
+  testMatch: ["**/*.spec.ts", "!**/*.browser.spec.ts", "!**/*.integ.spec.ts"],
 };
