98c64899100de1eee48ec145298dc3717de5504a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-03-25 00:09:31 +0000

    feat(credential-provider-web-identity): add fromTokenFile
    refs: https://github.com/aws/aws-sdk-js-v3/pull/2147#issuecomment-806203135

---
+++ packages/credential-provider-web-identity/README.md
@@ -0,0 +1,101 @@
+# @aws-sdk/credential-provider-web-identity
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/credential-provider-web-identity/latest.svg)](https://www.npmjs.com/package/@aws-sdk/credential-provider-web-identity)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/credential-provider-web-identity.svg)](https://www.npmjs.com/package/@aws-sdk/credential-provider-web-identity)
+
+## AWS Credential Provider for Node.js - AssumeRole
+
+This module includes functions which get credentials by calling STS assumeRole\* APIs.
+
+## fromTokenFile
+
+The function `fromTokenFile` returns `CredentialProvider` that reads credentials as follows:
+
+- Reads file location of where the OIDC token is stored from either environment or config file parameters.
+- Reads IAM role wanting to be assumed from either environment or config file paramters.
+- Reads optional role session name to be used to distinguish sessions from either environment or config file paramters.
+  If session name is not defined, it comes up with a role session name.
+- Reads OIDC token from file on disk.
+- Calls sts:AssumeRoleWithWebIdentity via `roleAssumerWithWebIdentity` option to get credentials.
+- Uses credentials of source_profile to assume the role specified if specified.
+
+| **Environment Variable**    | **Config Variable**     | **Required** | **Description**                                   |
+| --------------------------- | ----------------------- | ------------ | ------------------------------------------------- |
+| AWS_WEB_IDENTITY_TOKEN_FILE | web_identity_token_file | true         | File location of where the `OIDC` token is stored |
+| AWS_IAM_ROLE_ARN            | role_arn                | true         | The IAM role wanting to be assumed                |
+| AWS_IAM_ROLE_SESSION_NAME   | role_session_name       | false        | The IAM session name used to distinguish sessions |
+
+### Supported configuration
+
+The following options are supported:
+
+- `profile` - The configuration profile to use. If not specified, the provider
+  will use the value in the `AWS_PROFILE` environment variable or `default` by
+  default.
+- `filepath` - The path to the shared credentials file. If not specified, the
+  provider will use the value in the `AWS_SHARED_CREDENTIALS_FILE` environment
+  variable or `~/.aws/credentials` by default.
+- `configFilepath` - The path to the shared config file. If not specified, the
+  provider will use the value in the `AWS_CONFIG_FILE` environment variable or
+  `~/.aws/config` by default.
+- `roleAssumer` - A function that assumes a role and returns a promise
+  fulfilled with credentials for the assumed role. You may call `sts:assumeRole`
+  API within this function.
+- `roleAssumerWithWebIdentity` - A function that assumes a role with web identity
+  and returns a promise fulfilled with credentials for the assumed role. You may call
+  `sts:assumeRoleWithWebIdentity` API within this function.
+
+### Examples
+
+A basic example of using fromTokenFile:
+
+```js
+import { STSClient, AssumeRoleWithWebIdentityCommand } from "@aws-sdk/client-sts";
+import { fromTokenFile } from "@aws-sdk/credential-provider-web-identity";
+
+const stsClient = new STSClient({});
+
+const roleAssumerWithWebIdentity = async (params) => {
+  const { Credentials } = await stsClient.send(
+    new AssumeRoleWithWebIdentityCommand(params)
+  );
+  if (!Credentials || !Credentials.AccessKeyId || !Credentials.SecretAccessKey) {
+    throw new Error(`Invalid response from STS.assumeRole call with role ${params.RoleArn}`);
+  }
+  return {
+    accessKeyId: Credentials.AccessKeyId,
+    secretAccessKey: Credentials.SecretAccessKey,
+    sessionToken: Credentials.SessionToken,
+    expiration: Credentials.Expiration,
+  };
+};
+
+const client = new FooClient({
+  credentials: fromTokenFile({
+    roleAssumerWithWebIdentity
+  });
+});
+```
+
+#### Values in environment variables
+
+The values can be defined in environment variables as follows:
+
+```console
+$ node
+> Object.fromEntries(Object.entries(process.env).filter(([key, value]) => key.startsWith("AWS_")));
+{
+  AWS_WEB_IDENTITY_TOKEN_FILE: '/temp/token',
+  AWS_ROLE_ARN: 'arn:aws:iam::123456789012:role/example-role-arn'
+}
+```
+
+#### Values in configuration files
+
+The values can be defined in configuration files as follows:
+
+```
+[sample-profile]
+web_identity_token_file = /temp/token
+role_session_name = arn:aws:iam::123456789012:role/example-role-arn
+```
---
+++ packages/credential-provider-web-identity/src/fromTokenFile.spec.ts
@@ -0,0 +1,314 @@
+import { AssumeRoleCommandInput, AssumeRoleWithWebIdentityCommandInput } from "@aws-sdk/client-sts";
+import { getMasterProfileName, parseKnownFiles } from "@aws-sdk/credential-provider-ini";
+import { ProviderError } from "@aws-sdk/property-provider";
+import { Credentials } from "@aws-sdk/types";
+import { readFileSync } from "fs";
+
+import { fromTokenFile, FromTokenFileInit } from "./fromTokenFile";
+
+const ENV_TOKEN_FILE = "AWS_WEB_IDENTITY_TOKEN_FILE";
+const ENV_ROLE_ARN = "AWS_ROLE_ARN";
+const ENV_ROLE_SESSION_NAME = "AWS_ROLE_SESSION_NAME";
+
+jest.mock("fs");
+jest.mock("@aws-sdk/credential-provider-ini");
+
+const MOCK_CREDS = {
+  accessKeyId: "accessKeyId",
+  secretAccessKey: "secretAccessKey",
+  sessionToken: "sessionToken",
+};
+
+const mockTokenFile = "mockTokenFile";
+const mockTokenValue = "exampletoken";
+const mockRoleArn = "mockRoleArn";
+const mockRoleSessionName = "mockRoleSessionName";
+
+describe(fromTokenFile.name, () => {
+  beforeEach(() => {
+    (readFileSync as jest.Mock).mockReturnValue(mockTokenValue);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  const testRoleAssumerWithWebIdentityNotDefined = async (errorPrefix: string) => {
+    try {
+      // @ts-ignore An argument for 'init' was not provided.
+      await fromTokenFile({})();
+      fail(`Expected error to be thrown`);
+    } catch (error) {
+      expect(error).toEqual(new ProviderError(`${errorPrefix}, but no role assumption callback was provided.`, false));
+    }
+  };
+
+  const testReadFileSyncError = async (init: FromTokenFileInit) => {
+    const readFileSyncError = new Error("readFileSyncError");
+    (readFileSync as jest.Mock).mockImplementation(() => {
+      throw readFileSyncError;
+    });
+    try {
+      await fromTokenFile(init)();
+      fail(`Exepcted error to be thrown`);
+    } catch (error) {
+      expect(error).toEqual(readFileSyncError);
+    }
+    expect(readFileSync).toHaveBeenCalledTimes(1);
+  };
+
+  const testRoleAssumerWithWebIdentitySuccess = async (init: FromTokenFileInit) => {
+    const creds = await fromTokenFile(init)();
+    expect(creds).toEqual(MOCK_CREDS);
+    expect(readFileSync).toHaveBeenCalledTimes(1);
+    expect(readFileSync).toHaveBeenCalledWith(mockTokenFile, { encoding: "ascii" });
+  };
+
+  const testRandomValueForRoleSessionName = async () => {
+    const mockDateNow = Date.now();
+    const spyDateNow = jest.spyOn(Date, "now").mockReturnValueOnce(mockDateNow);
+
+    const creds = await fromTokenFile({
+      roleAssumerWithWebIdentity: async (params: AssumeRoleWithWebIdentityCommandInput) => {
+        expect(params.RoleSessionName).toEqual(`aws-sdk-js-session-${mockDateNow}`);
+        return MOCK_CREDS;
+      },
+    })();
+    expect(creds).toEqual(MOCK_CREDS);
+    expect(spyDateNow).toHaveBeenCalledTimes(1);
+  };
+
+  describe("reads config from env", () => {
+    const original_ENV_TOKEN_FILE = process.env[ENV_TOKEN_FILE];
+    const original_ENV_ROLE_ARN = process.env[ENV_ROLE_ARN];
+    const original_ENV_ROLE_SESSION_NAME = process.env[ENV_ROLE_SESSION_NAME];
+
+    beforeEach(() => {
+      process.env[ENV_TOKEN_FILE] = mockTokenFile;
+      process.env[ENV_ROLE_ARN] = mockRoleArn;
+      process.env[ENV_ROLE_SESSION_NAME] = mockRoleSessionName;
+    });
+
+    afterAll(() => {
+      process.env[ENV_TOKEN_FILE] = original_ENV_TOKEN_FILE;
+      process.env[ENV_ROLE_ARN] = original_ENV_ROLE_ARN;
+      process.env[ENV_ROLE_SESSION_NAME] = original_ENV_ROLE_SESSION_NAME;
+    });
+
+    it("throws if roleAssumerWithWebIdentity is not defined", async () => {
+      return testRoleAssumerWithWebIdentityNotDefined(
+        `Role Arn '${process.env[ENV_ROLE_ARN]}' needs to be assumed with web identity`
+      );
+    });
+
+    it("throws if ENV_TOKEN_FILE read from disk failed", async () => {
+      return testReadFileSyncError({
+        roleAssumerWithWebIdentity: async (params: AssumeRoleWithWebIdentityCommandInput) => {
+          return MOCK_CREDS;
+        },
+      });
+    });
+
+    it("passes values to roleAssumerWithWebIdentity", async () => {
+      return testRoleAssumerWithWebIdentitySuccess({
+        roleAssumerWithWebIdentity: async (params: AssumeRoleWithWebIdentityCommandInput) => {
+          expect(params.WebIdentityToken).toEqual(mockTokenValue);
+          expect(params.RoleArn).toEqual(mockRoleArn);
+          expect(params.RoleSessionName).toEqual(mockRoleSessionName);
+          return MOCK_CREDS;
+        },
+      });
+    });
+
+    it("generates a random value for RoleSessionName if not available", async () => {
+      delete process.env[ENV_ROLE_SESSION_NAME];
+      return testRandomValueForRoleSessionName();
+    });
+  });
+
+  describe("reads config from shared ini", () => {
+    const original_ENV_TOKEN_FILE = process.env[ENV_TOKEN_FILE];
+    const masterProfileName = "masterProfileName";
+
+    beforeAll(() => {
+      delete process.env[ENV_TOKEN_FILE];
+    });
+
+    afterAll(() => {
+      process.env[ENV_TOKEN_FILE] = original_ENV_TOKEN_FILE;
+    });
+
+    beforeEach(() => {
+      (getMasterProfileName as jest.Mock).mockReturnValue(masterProfileName);
+    });
+
+    describe("without source_profile", () => {
+      beforeEach(() => {
+        (parseKnownFiles as jest.Mock).mockResolvedValue({
+          [masterProfileName]: {
+            web_identity_token_file: mockTokenFile,
+            role_arn: mockRoleArn,
+            role_session_name: mockRoleSessionName,
+          },
+        });
+      });
+
+      it("throws if roleAssumerWithWebIdentity is not defined", async () => {
+        return testRoleAssumerWithWebIdentityNotDefined(
+          `Profile '${masterProfileName}' requires a role to be assumed with web identity`
+        );
+      });
+
+      it("throws if web_identity_token_file read from disk failed", async () => {
+        return testReadFileSyncError({
+          roleAssumerWithWebIdentity: async (params: AssumeRoleWithWebIdentityCommandInput) => {
+            return MOCK_CREDS;
+          },
+        });
+      });
+
+      it("passes values to roleAssumerWithWebIdentity", async () => {
+        return testRoleAssumerWithWebIdentitySuccess({
+          roleAssumerWithWebIdentity: async (params: AssumeRoleWithWebIdentityCommandInput) => {
+            expect(params.WebIdentityToken).toEqual(mockTokenValue);
+            expect(params.RoleArn).toEqual(mockRoleArn);
+            expect(params.RoleSessionName).toEqual(mockRoleSessionName);
+            return MOCK_CREDS;
+          },
+        });
+      });
+
+      it("generates a random value for RoleSessionName if not available", async () => {
+        (parseKnownFiles as jest.Mock).mockResolvedValue({
+          [masterProfileName]: {
+            web_identity_token_file: mockTokenFile,
+            role_arn: mockRoleArn,
+          },
+        });
+        return testRandomValueForRoleSessionName();
+      });
+    });
+
+    describe("with source_profile", () => {
+      const MOCK_SOURCE_CREDS = {
+        accessKeyId: "accessKeyIdSource",
+        secretAccessKey: "secretAccessKeySource",
+        sessionToken: "sessionTokenSource",
+      };
+
+      const sourceProfileName = "sourceProfileName";
+      const mockMasterRoleArn = "mockMasterRoleArn";
+      const mockMasterRoleSessionName = "mockMasterRoleSessionName";
+
+      beforeEach(() => {
+        (parseKnownFiles as jest.Mock).mockResolvedValue({
+          [sourceProfileName]: {
+            web_identity_token_file: mockTokenFile,
+            role_arn: mockRoleArn,
+            role_session_name: mockRoleSessionName,
+          },
+          [masterProfileName]: {
+            source_profile: sourceProfileName,
+            role_arn: mockMasterRoleArn,
+            role_session_name: mockMasterRoleSessionName,
+          },
+        });
+      });
+
+      it("throws if roleAssumer is not defined", async () => {
+        try {
+          // @ts-ignore An argument for 'init' was not provided.
+          await fromTokenFile({})();
+          fail(`Expected error to be thrown`);
+        } catch (error) {
+          expect(error).toEqual(
+            new ProviderError(
+              `Profile '${masterProfileName}' requires a role to be assumed, but no role assumption callback was provided.`,
+              false
+            )
+          );
+        }
+      });
+
+      it("throws if roleAssumerWithWebIdentity is not defined", async () => {
+        try {
+          // @ts-ignore An argument for 'init' was not provided.
+          await fromTokenFile({
+            roleAssumer: async (sourceCreds: Credentials, params: AssumeRoleCommandInput) => {
+              return MOCK_CREDS;
+            },
+          })();
+          fail(`Expected error to be thrown`);
+        } catch (error) {
+          expect(error).toEqual(
+            new ProviderError(
+              `Profile '${sourceProfileName}' requires a role to be assumed with web identity, but no role assumption callback was provided.`,
+              false
+            )
+          );
+        }
+      });
+
+      it("throws if web_identity_token_file read from disk failed", async () => {
+        return testReadFileSyncError({
+          roleAssumer: async (sourceCreds: Credentials, params: AssumeRoleCommandInput) => {
+            return MOCK_CREDS;
+          },
+          roleAssumerWithWebIdentity: async (params: AssumeRoleWithWebIdentityCommandInput) => {
+            return MOCK_SOURCE_CREDS;
+          },
+        });
+      });
+
+      it("passes values to roleAssumerWithWebIdentity", async () => {
+        return testRoleAssumerWithWebIdentitySuccess({
+          roleAssumer: async (sourceCreds: Credentials, params: AssumeRoleCommandInput) => {
+            expect(sourceCreds).toEqual(MOCK_SOURCE_CREDS);
+            expect(params.RoleArn).toEqual(mockMasterRoleArn);
+            expect(params.RoleSessionName).toEqual(mockMasterRoleSessionName);
+            return MOCK_CREDS;
+          },
+          roleAssumerWithWebIdentity: async (params: AssumeRoleWithWebIdentityCommandInput) => {
+            expect(params.WebIdentityToken).toEqual(mockTokenValue);
+            expect(params.RoleArn).toEqual(mockRoleArn);
+            expect(params.RoleSessionName).toEqual(mockRoleSessionName);
+            return MOCK_SOURCE_CREDS;
+          },
+        });
+      });
+
+      it("generates a random value for RoleSessionName if not available", async () => {
+        (parseKnownFiles as jest.Mock).mockResolvedValue({
+          [sourceProfileName]: {
+            web_identity_token_file: mockTokenFile,
+            role_arn: mockRoleArn,
+          },
+          [masterProfileName]: {
+            source_profile: sourceProfileName,
+            role_arn: mockMasterRoleArn,
+          },
+        });
+        const mockDateNowFirst = Date.now();
+        const mockDateNowSecond = mockDateNowFirst + 1;
+        const spyDateNow = jest
+          .spyOn(Date, "now")
+          .mockReturnValueOnce(mockDateNowFirst)
+          .mockReturnValueOnce(mockDateNowSecond);
+
+        const creds = await fromTokenFile({
+          roleAssumer: async (sourceCreds: Credentials, params: AssumeRoleCommandInput) => {
+            expect(params.RoleSessionName).toEqual(`aws-sdk-js-session-${mockDateNowFirst}`);
+            return MOCK_CREDS;
+          },
+          roleAssumerWithWebIdentity: async (params: AssumeRoleWithWebIdentityCommandInput) => {
+            expect(params.RoleSessionName).toEqual(`aws-sdk-js-session-${mockDateNowSecond}`);
+            return MOCK_SOURCE_CREDS;
+          },
+        })();
+        expect(creds).toEqual(MOCK_CREDS);
+        expect(spyDateNow).toHaveBeenCalledTimes(2);
+      });
+    });
+  });
+});
