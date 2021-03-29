28af36a9f7d98a18e29988837adf4c145a76c032
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-03-29 08:25:08 -0700

    chore: remove calls to react-native-get-random-values from code

--- packages/middleware-retry/src/index.ts
+++ packages/middleware-retry/src/index.ts
@@ -1,11 +1,3 @@
-//reference: https://github.com/uuidjs/uuid#getrandomvalues-not-supported
-try {
-  //@ts-ignore
-  require("react-native-get-random-values");
-} catch {
-  // do nothing
-}
-
 export * from "./retryMiddleware";
 export * from "./omitRetryHeadersMiddleware";
 export * from "./defaultStrategy";
--- packages/middleware-sdk-transcribe-streaming/src/index.native.ts
+++
@@ -1,9 +0,0 @@
-//reference: https://github.com/uuidjs/uuid#getrandomvalues-not-supported
-import "react-native-get-random-values";
-export * from "./websocket-handler";
-export * from "./eventstream-handler";
-export * from "./signer";
-export * from "./configuration";
-export * from "./middleware-endpoint";
-export * from "./middleware-session-id";
-export * from "./plugin";
