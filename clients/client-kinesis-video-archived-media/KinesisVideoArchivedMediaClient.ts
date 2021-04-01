f4b4e2afe108ca28c0ff493c08ce9d0e4a11822d
AllanZhengYP <zheallan@amazon.com>
2021-04-01 11:14:12 -0700

    docs: add documentation for destroy method (#2204)

--- clients/client-kinesis-video-archived-media/KinesisVideoArchivedMediaClient.ts
+++ clients/client-kinesis-video-archived-media/KinesisVideoArchivedMediaClient.ts
@@ -223,6 +223,11 @@ export class KinesisVideoArchivedMediaClient extends __Client<
     this.middlewareStack.use(getUserAgentPlugin(this.config));
   }
 
+  /**
+   * Destroy underlying resources, like sockets. It's usually not necessary to do this.
+   * However in Node.js, it's best to explicitly shut down the client's agent when it is no longer needed.
+   * Otherwise, sockets might stay open for quite a long time before the server terminates them.
+   */
   destroy(): void {
     super.destroy();
   }
