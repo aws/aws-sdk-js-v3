96ff1641f678bb0ead81a4b4ea30f75236dfc7d1
AllanZhengYP <zheallan@amazon.com>
2021-04-28 16:18:36 -0700

    fix(xml-builder): xml entity encoding for more characters (#2309)
    XML entity encoding for carriage return, line feed, next line, line separator characters,
    quote and single quote

--- packages/xml-builder/src/escape-element.ts
+++ packages/xml-builder/src/escape-element.ts
@@ -2,5 +2,14 @@
  * Escapes characters that can not be in an XML element.
  */
 export function escapeElement(value: string): string {
-  return value.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  return value
+    .replace(/&/g, "&amp;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/\r/g, "&#x0D;")
+    .replace(/\n/g, "&#x0A;")
+    .replace(/\u0085/g, "&#x85;")
+    .replace(/\u2028/, "&#x2028;");
 }
