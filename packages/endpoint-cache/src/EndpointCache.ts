bff5fa8a72daf7f6c21c1eb255938e1591a6d6a8
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-05-05 23:46:10 +0000

    chore(endpoint-cache): add basic operations

--- packages/endpoint-cache/src/EndpointCache.ts
+++ packages/endpoint-cache/src/EndpointCache.ts
@@ -1,9 +1,29 @@
 import { LRUCache } from "mnemonist";
 
 export class EndpointCache {
-  private readonly cache: LRUCache<string, string>;
+  // ToDo: remove `| undefined` once LRUCache.remove(key) is available.
+  // Refs: https://github.com/Yomguithereal/mnemonist/issues/143
+  private readonly cache: LRUCache<string, string | undefined>;
 
   constructor(capacity: number) {
     this.cache = new LRUCache(capacity);
+  }
+
+  get(key: string) {
+    return this.cache.get(key);
+  }
+
+  set(key: string, value: string) {
+    this.cache.set(key, value);
+  }
+
+  remove(key: string) {
+    // Replace with remove call once support is added upstream
+    // Refs: https://github.com/Yomguithereal/mnemonist/issues/143
+    this.cache.set(key, undefined);
+  }
+
+  clear() {
+    this.cache.clear();
   }
 }
