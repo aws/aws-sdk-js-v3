b958ffbb881d1064651a111bb69ce046dd2f9bcd
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-05-06 00:46:29 +0000

    feat(endpoint-cache): return un-expired endpoint on get

--- packages/endpoint-cache/src/EndpointCache.ts
+++ packages/endpoint-cache/src/EndpointCache.ts
@@ -13,8 +13,26 @@ export class EndpointCache {
     this.cache = new LRUCache(capacity);
   }
 
+  private getEndpoint(endpointsWithExpiry: EndpointWithExpiry[]) {
+    const now = Date.now();
+    const endpoints = endpointsWithExpiry
+      .filter((endpoint) => now > endpoint.Expires)
+      .map((endpoint) => endpoint.Address);
+    return endpoints[Math.floor(Math.random() * endpoints.length)];
+  }
+
   get(key: string) {
-    return this.cache.get(key);
+    const endpointsWithExpiry = this.cache.get(key);
+
+    if (!endpointsWithExpiry) {
+      return;
+    }
+
+    const endpoint = this.getEndpoint(endpointsWithExpiry);
+    if (endpoint === undefined) {
+      this.remove(key);
+    }
+    return endpoint;
   }
 
   set(key: string, endpoints: Endpoint[]) {
