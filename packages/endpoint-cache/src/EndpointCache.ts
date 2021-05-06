c8fc1f0c8342b3db577a2bc2f6f26cc9e1d81dac
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-05-06 00:23:09 +0000

    feat(endpoint-cache): write setter which converts CachePeriodInMinutes to Expires

--- packages/endpoint-cache/src/EndpointCache.ts
+++ packages/endpoint-cache/src/EndpointCache.ts
@@ -1,9 +1,13 @@
 import { LRUCache } from "mnemonist";
 
+import { Endpoint } from "./Endpoint";
+
+interface EndpointWithExpiry extends Pick<Endpoint, "Address"> {
+  Expires: number;
+}
+
 export class EndpointCache {
-  // ToDo: remove `| undefined` once LRUCache.remove(key) is available.
-  // Refs: https://github.com/Yomguithereal/mnemonist/issues/143
-  private readonly cache: LRUCache<string, string | undefined>;
+  private readonly cache: LRUCache<string, EndpointWithExpiry[]>;
 
   constructor(capacity: number) {
     this.cache = new LRUCache(capacity);
@@ -13,14 +17,21 @@ export class EndpointCache {
     return this.cache.get(key);
   }
 
-  set(key: string, value: string) {
-    this.cache.set(key, value);
+  set(key: string, endpoints: Endpoint[]) {
+    const now = Date.now();
+    this.cache.set(
+      key,
+      endpoints.map(({ Address = "", CachePeriodInMinutes = 1 }) => ({
+        Address,
+        Expires: now + CachePeriodInMinutes * 60 * 100,
+      }))
+    );
   }
 
   remove(key: string) {
     // Replace with remove call once support is added upstream
     // Refs: https://github.com/Yomguithereal/mnemonist/issues/143
-    this.cache.set(key, undefined);
+    this.cache.set(key, []);
   }
 
   clear() {
