482e52c9bdcaa10291166795cbf234bed48d83ac
Paul Draper <paulddraper@gmail.com>
2021-05-08 09:42:22 -0600

    fix(clients): properly decode SQS messages
    Fixes #2362.

--- clients/client-sqs/protocols/Aws_query.ts
+++ clients/client-sqs/protocols/Aws_query.ts
@@ -102,6 +102,7 @@ import {
   SerdeContext as __SerdeContext,
 } from "@aws-sdk/types";
 import { parse as xmlParse } from "fast-xml-parser";
+import { decode as xmlDecode } from "he";
 
 export const serializeAws_queryAddPermissionCommand = async (
   input: AddPermissionCommandInput,
@@ -3130,14 +3131,6 @@ const buildHttpRpcRequest = async (
   return new __HttpRequest(contents);
 };
 
-const decodeEscapedXML = (str: string) =>
-  str
-    .replace(/&amp;/g, "&")
-    .replace(/&apos;/g, "'")
-    .replace(/&quot;/g, '"')
-    .replace(/&gt;/g, ">")
-    .replace(/&lt;/g, "<");
-
 const parseBody = (streamBody: any, context: __SerdeContext): any =>
   collectBodyString(streamBody, context).then((encoded) => {
     if (encoded.length) {
@@ -3146,7 +3139,7 @@ const parseBody = (streamBody: any, context: __SerdeContext): any =>
         ignoreAttributes: false,
         parseNodeValue: false,
         trimValues: false,
-        tagValueProcessor: (val, tagName) => (val.trim() === "" ? "" : decodeEscapedXML(val)),
+        tagValueProcessor: (val, tagName) => (val.trim() === "" ? "" : xmlDecode(val)),
       });
       const textNodeName = "#text";
       const key = Object.keys(parsedObj)[0];
