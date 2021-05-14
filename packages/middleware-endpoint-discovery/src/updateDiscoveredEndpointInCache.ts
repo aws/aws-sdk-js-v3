a955f8018bb09c3765ec05cd02066456821c52f3
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-05-14 00:48:55 +0000

    fix: use observer pattern in updateDiscoveredeEndpointInCache

--- packages/middleware-endpoint-discovery/src/updateDiscoveredEndpointInCache.ts
+++ packages/middleware-endpoint-discovery/src/updateDiscoveredEndpointInCache.ts
@@ -7,59 +7,82 @@ export interface UpdateDiscoveredEndpointInCacheOptions extends EndpointDiscover
   endpointDiscoveryCommandCtor: new (comandConfig: any) => any;
 }
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const requestQueue: { [key: string]: { resolve: Function; reject: Function }[] } = {};
 
 export const updateDiscoveredEndpointInCache = async (
   config: EndpointDiscoveryResolvedConfig & PreviouslyResolved,
   options: UpdateDiscoveredEndpointInCacheOptions
-) => {
-  const { endpointCache } = config;
-  const { cacheKey, commandName, identifiers } = options;
+) =>
+  new Promise<void>((resolve, reject) => {
+    const { endpointCache } = config;
+    const { cacheKey, commandName, identifiers } = options;
 
-  let endpoints = endpointCache.get(cacheKey);
+    const endpoints = endpointCache.get(cacheKey);
 
-  // Wait for other endpoint operations to complete before making new calls.
-  while (endpoints && endpoints.length === 1 && endpoints[0].Address === "") {
-    await sleep(1000);
-    endpoints = endpointCache.get(cacheKey);
-  }
+    if (endpoints && endpoints.length === 1 && endpoints[0].Address === "") {
+      // Endpoint operation already in-flight.
+      // Add request to request queue only if discovered endpoint is required.
+      if (options.isDiscoveredEndpointRequired) {
+        if (!requestQueue[cacheKey]) requestQueue[cacheKey] = [];
+        requestQueue[cacheKey].push({ resolve, reject });
+      } else {
+        resolve();
+      }
+    } else if (endpoints && endpoints.length > 0) {
+      // Endpoint record is present in cache.
+      resolve();
+    } else {
+      // put in a placeholder for endpoints already requested, prevent
+      // too much in-flight calls.
+      const placeholderEndpoints = [{ Address: "", CachePeriodInMinutes: 1 }];
+      endpointCache.set(cacheKey, placeholderEndpoints);
 
-  if (endpoints && endpoints.length > 0) {
-    // Endpoint record is present in cache.
-    return;
-  } else {
-    // put in a placeholder for endpoints already requested, prevent
-    // too much in-flight calls.
-    const placeholderEndpoints = [{ Address: "", CachePeriodInMinutes: 1 }];
-    endpointCache.set(cacheKey, placeholderEndpoints);
-
-    try {
       const command = new options.endpointDiscoveryCommandCtor({
         Operation: commandName.substr(0, commandName.length - 7), // strip "Command"
         Identifiers: identifiers,
       });
       const handler = command.resolveMiddleware(options.clientStack, config, options.options);
-      const result = await handler(command);
-      endpointCache.set(cacheKey, result.output.Endpoints);
-    } catch (error) {
-      if (error.name === "InvalidEndpointException" || error.$metadata?.httpStatusCode === 421) {
-        // Endpoint is invalid, delete the cache entry.
-        endpointCache.delete(cacheKey);
-      }
-      if (options.isDiscoveredEndpointRequired) {
-        // Endpoint Discovery is required, rethrow error.
-        throw Object.assign(
-          new Error(
-            `The operation to discover endpoint failed.` +
-              ` Please retry, or provide a custom endpoint and disable endpoint discovery to proceed.`
-          ),
-          { reason: error }
-        );
-      } else {
-        // Endpoint Discovery is optional. No error needs to be thrown.
-        // Set placeHolder endpoint to disable refresh for one minute.
-        endpointCache.set(cacheKey, placeholderEndpoints);
-      }
+      handler(command)
+        .then((result: any) => {
+          endpointCache.set(cacheKey, result.output.Endpoints);
+          if (requestQueue[cacheKey]) {
+            requestQueue[cacheKey].forEach(({ resolve }) => {
+              resolve();
+            });
+            delete requestQueue[cacheKey];
+          }
+          resolve();
+        })
+        .catch((error: any) => {
+          if (error.name === "InvalidEndpointException" || error.$metadata?.httpStatusCode === 421) {
+            // Endpoint is invalid, delete the cache entry.
+            endpointCache.delete(cacheKey);
+          }
+
+          const errorToThrow = Object.assign(
+            new Error(
+              `The operation to discover endpoint failed.` +
+                ` Please retry, or provide a custom endpoint and disable endpoint discovery to proceed.`
+            ),
+            { reason: error }
+          );
+
+          //fail all the pending requests in batch
+          if (requestQueue[cacheKey]) {
+            requestQueue[cacheKey].forEach(({ reject }) => {
+              reject(errorToThrow);
+            });
+            delete requestQueue[cacheKey];
+          }
+
+          if (options.isDiscoveredEndpointRequired) {
+            reject(errorToThrow);
+          } else {
+            // Endpoint Discovery is optional. No error needs to be thrown.
+            // Set placeHolder endpoint to disable refresh for one minute.
+            endpointCache.set(cacheKey, placeholderEndpoints);
+            resolve();
+          }
+        });
     }
-  }
-};
+  });
