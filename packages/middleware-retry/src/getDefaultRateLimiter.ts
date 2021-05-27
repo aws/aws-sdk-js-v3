3a4e0e533ce3e7d4f36f7d7792549b563846b7f3
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-05-27 14:14:47 +0000

    feat: add getSendToken implementation

--- packages/middleware-retry/src/getDefaultRateLimiter.ts
+++ packages/middleware-retry/src/getDefaultRateLimiter.ts
@@ -5,8 +5,8 @@ const currentTimeInSeconds = () => Date.now() / 1000;
 export const getDefaultRateLimiter = (): RateLimiter => {
   const fillRate = undefined;
   const maxCapacity = undefined;
-  const currentCapacity = 0;
-  const lastTimestamp = undefined;
+  let currentCapacity = 0;
+  let lastTimestamp = undefined;
   const enabled = false;
   const measuredTxRate = 0;
   const last_tx_rate_bucket = Math.floor(currentTimeInSeconds());
@@ -14,9 +14,33 @@ export const getDefaultRateLimiter = (): RateLimiter => {
   const lastMaxRate = 0;
   const lastThrottleTime = currentTimeInSeconds();
 
-  const getSendToken = async () => {
-    // Code to throttle if enabled === true.
+  const tokenBucketRefill = () => {
+    const timestamp = currentTimeInSeconds();
+    if (!lastTimestamp) {
+      lastTimestamp = timestamp;
+      return;
+    }
+
+    const fillAmount = (timestamp - lastTimestamp) * fillRate;
+    currentCapacity = Math.min(maxCapacity, currentCapacity + fillAmount);
+    lastTimestamp = timestamp;
   };
+
+  const tokenBucketAcquire = async (amount: number) => {
+    // Client side throttling is not enabled until we see a throttling error.
+    if (!enabled) {
+      return;
+    }
+
+    tokenBucketRefill();
+    if (amount > currentCapacity) {
+      const delay = (amount - currentCapacity) / fillRate;
+      await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+    currentCapacity = currentCapacity - amount;
+  };
+
+  const getSendToken = async () => tokenBucketAcquire(1);
 
   const updateClientSendingRate = (response: any) => {
     // Code to updaye client sending rate.
