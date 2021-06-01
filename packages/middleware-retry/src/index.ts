09debdf77d60b75657f403c1e11dfb36cbbd5638
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-06-01 03:58:41 +0000

    chore: export AdaptiveRetryStrategy

--- packages/middleware-retry/src/index.ts
+++ packages/middleware-retry/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./retryMiddleware";
 export * from "./omitRetryHeadersMiddleware";
 export * from "./StandardRetryStrategy";
+export * from "./AdaptiveRetryStrategy";
 export * from "./configurations";
 export * from "./delayDecider";
 export * from "./retryDecider";
