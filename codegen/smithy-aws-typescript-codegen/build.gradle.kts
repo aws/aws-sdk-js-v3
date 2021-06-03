37d8d0a6b0be688f2761af6a592d14f2886612be
Allan Zheng <zheallan@amazon.com>
2021-06-03 13:47:51 -0700

    chore: add smithy-aws-cloudformation-traits dependency

--- codegen/smithy-aws-typescript-codegen/build.gradle.kts
+++ codegen/smithy-aws-typescript-codegen/build.gradle.kts
@@ -18,6 +18,7 @@ extra["displayName"] = "Smithy :: AWS :: Typescript :: Codegen"
 extra["moduleName"] = "software.amazon.smithy.aws.typescript.codegen"
 
 dependencies {
+    api("software.amazon.smithy:smithy-aws-cloudformation-traits:[1.7.0, 1.8.0[")
     api("software.amazon.smithy:smithy-aws-traits:[1.7.0, 1.8.0[")
     api("software.amazon.smithy:smithy-waiters:[1.7.0, 1.8.0[")
     api("software.amazon.smithy:smithy-aws-iam-traits:[1.7.0, 1.8.0[")
