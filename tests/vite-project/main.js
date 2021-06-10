3e50f77843d852bef6061d7bcdf3f6897e9cf449
Walker <walker@praiseweb.com.br>
2021-06-10 20:15:05 -0300

    test(vite-project): scaffhold a vite project with @aws-sdk cognito import

---
+++ tests/vite-project/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+import '@aws-sdk/credential-provider-cognito-identity'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
