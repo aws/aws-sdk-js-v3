909853c366478a2da9fb9d85762a581188e7838d
Walker <walker@praiseweb.com.br>
2021-06-18 09:35:15 -0300

    fix(client-cognito-identity): build package also to cjs because it has browser runtime

--- clients/client-cognito-identity/buildPackage.js
+++ clients/client-cognito-identity/buildPackage.js
@@ -2,4 +2,6 @@ const { readFileSync, writeFileSync } = require("fs");
 
 const encoding = "utf8";
 const file = readFileSync("./package.json", { encoding });
-writeFileSync("./dist/es/package.js", `export default ${file}`, { encoding });
+const code = `export default ${file}`;
+writeFileSync("./dist/es/package.js", code, { encoding });
+writeFileSync("./dist/cjs/package.js", code, { encoding });
