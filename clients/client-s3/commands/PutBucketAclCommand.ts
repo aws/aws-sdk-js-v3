e8897940b42b04abcbec26b5bdb1f940884c8f07
Allan Zheng <zheallan@amazon.com>
2021-06-21 17:47:13 -0700

    feat(client-s3): add md5 header for operations with httpChecksumRequired trait

--- clients/client-s3/commands/PutBucketAclCommand.ts
+++ clients/client-s3/commands/PutBucketAclCommand.ts
@@ -4,6 +4,7 @@ import {
   deserializeAws_restXmlPutBucketAclCommand,
   serializeAws_restXmlPutBucketAclCommand,
 } from "../protocols/Aws_restXml";
+import { getApplyMd5BodyChecksumPlugin } from "@aws-sdk/middleware-apply-body-checksum";
 import { getBucketEndpointPlugin } from "@aws-sdk/middleware-bucket-endpoint";
 import { getSerdePlugin } from "@aws-sdk/middleware-serde";
 import { HttpRequest as __HttpRequest, HttpResponse as __HttpResponse } from "@aws-sdk/protocol-http";
@@ -252,6 +253,7 @@ export class PutBucketAclCommand extends $Command<
   ): Handler<PutBucketAclCommandInput, PutBucketAclCommandOutput> {
     this.middlewareStack.use(getSerdePlugin(configuration, this.serialize, this.deserialize));
     this.middlewareStack.use(getBucketEndpointPlugin(configuration));
+    this.middlewareStack.use(getApplyMd5BodyChecksumPlugin(configuration));
 
     const stack = clientStack.concat(this.middlewareStack);
 
