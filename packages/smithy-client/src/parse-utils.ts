54d567c05830bbfe04c6dbc96c2e7adc8984056e
Jordon Phillips <JordonPhillips@users.noreply.github.com>
2021-06-25 15:48:44 +0200

    chore: prefer arrow functions
    Co-authored-by: Trivikram Kamat <16024985+trivikr@users.noreply.github.com>

--- packages/smithy-client/src/parse-utils.ts
+++ packages/smithy-client/src/parse-utils.ts
@@ -1,10 +1,10 @@
 /**
- * Give an input string, stritctly parses a boolean value.
+ * Give an input string, strictly parses a boolean value.
  *
  * @param value The boolean string to parse.
  * @returns true for "true", false for "false", otherwise an error is thrown.
  */
-export function parseBoolean(value: "true" | "false"): boolean {
+export const parseBoolean = (value: "true" | "false"): boolean => {
   switch (value) {
     case "true":
       return true;
