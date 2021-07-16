330615dd6a6ecd615ef2340b4fd6c9a300ae200e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-07-16 15:46:29 +0000

    fix: ignore TS warning as process.emitWarning may not be defined

--- packages/postinstall-node-version-check/src/emitWarningIfUnsupportedVersion.ts
+++ packages/postinstall-node-version-check/src/emitWarningIfUnsupportedVersion.ts
@@ -1,4 +1,7 @@
 export const emitWarningIfUnsupportedVersion = (version: string) => {
+  // process.emitWarning will not be defined if user is on node < 6.
+  // We don't support node < 6, but do not want postinstall script to fail.
+  // @ts-expect-error This condition will always return true since this function is always defined
   if (process.emitWarning && version && parseInt(version.substring(1, version.indexOf("."))) < 12) {
     process.emitWarning(
       `The AWS SDK for JavaScript (v3) will\n` +
