b44a22a85f3bc31687e5f1b38216689dcf5f7e04
AllanZhengYP <zheallan@amazon.com>
2021-07-19 21:19:20 +0000

    test: add test to validate ts and tslib versions

---
+++ tests/typescript/validate-versions.ts
@@ -0,0 +1,30 @@
+/**
+ * Validate the tslib and tsc version is consistent and corresponds to each other.
+ */
+import { execSync } from "child_process";
+import { readFileSync } from "fs";
+
+import * as expectedVersions from "./versions.json";
+
+type PackageInfo = {
+  name: string;
+  version: string;
+  private: boolean;
+  location: string;
+};
+
+console.log("Validate the tslib and tsc version is consistent and corresponds to each other.");
+const packagesInfo: PackageInfo[] = JSON.parse(execSync("./node_modules/.bin/lerna list -l --json").toString());
+for (const { name, location } of packagesInfo) {
+  const manifest = readFileSync(`${location}/package.json`, "utf8");
+  const { dependencies, devDependencies } = JSON.parse(manifest);
+  for (const [expectedName, expectedVersion] of Object.entries(expectedVersions)) {
+    if (expectedName === name) {
+      const actualVersion = dependencies[expectedName] || devDependencies[expectedName];
+      if (actualVersion !== expectedVersion) {
+        throw new Error(`Expected version ${expectedVersion} for ${name}, but got ${actualVersion}`);
+      }
+    }
+  }
+}
+console.log("succeeded!");
