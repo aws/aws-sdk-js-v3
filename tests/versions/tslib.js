5cbc92b70dee198751e7cedfc6be91c0c53512e7
AllanZhengYP <zheallan@amazon.com>
2021-07-20 20:44:35 +0000

    fix: use js not ts-node

--- tests/versions/tslib.js
+++ tests/versions/tslib.js
@@ -1,21 +1,14 @@
 /**
  * Validate the tslib and tsc version is consistent and corresponds to each other.
  */
-import { execSync } from "child_process";
-import { readFileSync } from "fs";
-import JSON from "json5";
-import { join } from "path";
-
-type PackageInfo = {
-  name: string;
-  version: string;
-  private: boolean;
-  location: string;
-};
+const { execSync } = require("child_process");
+const { readFileSync } = require("fs");
+const JSON = require("json5");
+const { join } = require("path");
 
 console.log("Validating the tslib and tsc version is consistent within workspace.");
-const expectedVersions: { [name: string]: string } = JSON.parse(readFileSync(join(__dirname, "versions.json"), "utf8"));
-const packagesInfo: PackageInfo[] = JSON.parse(execSync("./node_modules/.bin/lerna list -l --json").toString());
+const expectedVersions = JSON.parse(readFileSync(join(__dirname, "versions.json"), "utf8"));
+const packagesInfo = JSON.parse(execSync("./node_modules/.bin/lerna list -l --json").toString());
 for (const { name, location } of packagesInfo) {
   const manifest = readFileSync(`${location}/package.json`, "utf8");
   const { dependencies, devDependencies } = JSON.parse(manifest);
