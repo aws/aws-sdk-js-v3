68a45e0ff9c7a06973b95446297cb36caf2f2940
Sam Chung <samc@seek.com.au>
2021-07-21 21:02:08 +1000

    feat: add paginators for lib-dynamodb

---
+++ lib/lib-dynamodb/src/pagination/Interfaces.ts
@@ -0,0 +1,8 @@
+import { PaginationConfiguration } from "@aws-sdk/types";
+
+import { DynamoDBDocument } from "../DynamoDBDocument";
+import { DynamoDBDocumentClient } from "../DynamoDBDocumentClient";
+
+export interface DynamoDBDocumentPaginationConfiguration extends PaginationConfiguration {
+  client: DynamoDBDocument | DynamoDBDocumentClient;
+}
