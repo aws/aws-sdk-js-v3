3c1ef7c95d415a06ee436e623559d8c2d5375a9c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-26 07:53:11 -0700

    chore(postinstall-node-version-check): remove unused package

--- packages/postinstall-node-version-check/src/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { emitWarningIfUnsupportedVersion } from "./emitWarningIfUnsupportedVersion";
-jest.mock("./emitWarningIfUnsupportedVersion");
-
-it("index", () => {
-  require("./index");
-  expect(emitWarningIfUnsupportedVersion).toHaveBeenCalledTimes(1);
-  expect(emitWarningIfUnsupportedVersion).toHaveBeenCalledWith(process.version);
-});
