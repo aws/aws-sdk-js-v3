0f895c305dd3e26322e14521f1ad5f562b2cdb45
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-08-10 23:43:32 +0000

    feat: add configuration to read endpoint

--- packages/credential-provider-imds/src/getInstanceMetadataHost.spec.ts
+++ packages/credential-provider-imds/src/getInstanceMetadataHost.spec.ts
@@ -1,7 +1,39 @@
+import { loadConfig, LoadedConfigSelectors } from "@aws-sdk/node-config-provider";
+
 import { getInstanceMetadataHost } from "./getInstanceMetadataHost";
+import { ENDPOINT_CONFIG_OPTIONS } from "./remoteProvider/EndpointConfigOptions";
+
+jest.mock("@aws-sdk/node-config-provider");
 
 describe(getInstanceMetadataHost.name, () => {
-  it("returns 169.254.169.254", () => {
-    expect(getInstanceMetadataHost()).resolves.toBe("169.254.169.254");
+  afterEach(() => {
+    expect(loadConfig).toBeCalledTimes(1);
+    expect(loadConfig).toBeCalledWith(ENDPOINT_CONFIG_OPTIONS);
+    jest.clearAllMocks();
+  });
+
+  describe("when loadConfig returns endpoint", () => {
+    it("throws ERR_INVALID_URL when endpoint is invalid", () => {
+      const endpoint = "invalid_endpoint";
+      (loadConfig as jest.Mock).mockReturnValueOnce(() => Promise.resolve(endpoint));
+      expect(getInstanceMetadataHost()).rejects.toThrow();
+    });
+
+    it("returns host when endpoint is valid", () => {
+      const host = "127.0.0.1";
+      const endpoint = `http://${host}`;
+      (loadConfig as jest.Mock).mockReturnValueOnce(() => Promise.resolve(endpoint));
+      expect(getInstanceMetadataHost()).resolves.toBe(host);
+    });
+  });
+
+  describe("when loadConfig returns undefined", () => {
+    beforeEach(() => {
+      (loadConfig as jest.Mock).mockReturnValueOnce(() => Promise.resolve(undefined));
+    });
+
+    it("returns 169.254.169.254", () => {
+      expect(getInstanceMetadataHost()).resolves.toBe("169.254.169.254");
+    });
   });
 });
--- packages/credential-provider-imds/src/getInstanceMetadataHost.ts
+++ packages/credential-provider-imds/src/getInstanceMetadataHost.ts
@@ -1,1 +1,12 @@
-export const getInstanceMetadataHost = () => Promise.resolve("169.254.169.254");
+import { loadConfig } from "@aws-sdk/node-config-provider";
+
+import { ENDPOINT_CONFIG_OPTIONS } from "./remoteProvider/EndpointConfigOptions";
+
+export const getInstanceMetadataHost = async () => {
+  const endpoint = await loadConfig(ENDPOINT_CONFIG_OPTIONS)();
+  if (!endpoint) {
+    return "169.254.169.254";
+  }
+  const { host } = new URL(endpoint);
+  return host;
+};
