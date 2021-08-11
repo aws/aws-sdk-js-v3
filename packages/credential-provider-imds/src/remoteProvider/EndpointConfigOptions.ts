1b5cf479166b5f7c0e4192011eccb6cba691814b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-08-11 02:04:12 +0000

    docs: add docs for getInstanceMetadataHost

--- packages/credential-provider-imds/src/remoteProvider/EndpointConfigOptions.ts
+++ packages/credential-provider-imds/src/remoteProvider/EndpointConfigOptions.ts
@@ -1,7 +1,7 @@
 import { LoadedConfigSelectors } from "@aws-sdk/node-config-provider";
 
-const ENV_ENDPOINT_NAME = "AWS_EC2_METADATA_SERVICE_ENDPOINT";
-const CONFIG_ENDPOINT_NAME = "ec2_metadata_service_endpoint";
+export const ENV_ENDPOINT_NAME = "AWS_EC2_METADATA_SERVICE_ENDPOINT";
+export const CONFIG_ENDPOINT_NAME = "ec2_metadata_service_endpoint";
 
 export const ENDPOINT_CONFIG_OPTIONS: LoadedConfigSelectors<string | undefined> = {
   environmentVariableSelector: (env) => env[ENV_ENDPOINT_NAME],
--- packages/credential-provider-imds/src/remoteProvider/EndpointModeConfigOptions.ts
+++ packages/credential-provider-imds/src/remoteProvider/EndpointModeConfigOptions.ts
@@ -2,8 +2,8 @@ import { LoadedConfigSelectors } from "@aws-sdk/node-config-provider";
 
 import { EndpointMode } from "./EndpointMode";
 
-const ENV_ENDPOINT_MODE_NAME = "AWS_EC2_METADATA_SERVICE_ENDPOINT_MODE";
-const CONFIG_ENDPOINT_MODE_NAME = "ec2_metadata_service_endpoint_mode";
+export const ENV_ENDPOINT_MODE_NAME = "AWS_EC2_METADATA_SERVICE_ENDPOINT_MODE";
+export const CONFIG_ENDPOINT_MODE_NAME = "ec2_metadata_service_endpoint_mode";
 
 export const ENDPOINT_MODE_CONFIG_OPTIONS: LoadedConfigSelectors<string | undefined> = {
   environmentVariableSelector: (env) => env[ENV_ENDPOINT_MODE_NAME],
