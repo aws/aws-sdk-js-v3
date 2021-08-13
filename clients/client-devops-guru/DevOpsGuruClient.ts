885cbc45a8c8e021e9591163ba1fecbaadddeb7b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-08-13 11:05:08 -0700

    chore(clients): re-introduce CORS retry headers for supported services (#2658)

--- clients/client-devops-guru/DevOpsGuruClient.ts
+++ clients/client-devops-guru/DevOpsGuruClient.ts
@@ -75,13 +75,7 @@ import {
   resolveHostHeaderConfig,
 } from "@aws-sdk/middleware-host-header";
 import { getLoggerPlugin } from "@aws-sdk/middleware-logger";
-import {
-  RetryInputConfig,
-  RetryResolvedConfig,
-  getOmitRetryHeadersPlugin,
-  getRetryPlugin,
-  resolveRetryConfig,
-} from "@aws-sdk/middleware-retry";
+import { RetryInputConfig, RetryResolvedConfig, getRetryPlugin, resolveRetryConfig } from "@aws-sdk/middleware-retry";
 import {
   AwsAuthInputConfig,
   AwsAuthResolvedConfig,
@@ -346,7 +340,6 @@ export class DevOpsGuruClient extends __Client<
     this.middlewareStack.use(getLoggerPlugin(this.config));
     this.middlewareStack.use(getAwsAuthPlugin(this.config));
     this.middlewareStack.use(getUserAgentPlugin(this.config));
-    this.middlewareStack.use(getOmitRetryHeadersPlugin(this.config));
   }
 
   /**
