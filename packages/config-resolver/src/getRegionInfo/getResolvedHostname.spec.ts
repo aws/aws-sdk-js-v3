2a7aba3e31e768a5902523a71cfa8d41df4a63fe
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-08-23 08:27:54 -0700

    test(config-resolver): getResolvedHostname.spec.ts

---
+++ packages/config-resolver/src/getRegionInfo/getResolvedHostname.spec.ts
@@ -0,0 +1,57 @@
+import { getResolvedHostName } from "./getResolvedHostname";
+import { getResolvedPartition } from "./getResolvedPartition";
+import { PartitionHash, RegionHash } from "./types";
+
+jest.mock("./getResolvedPartition");
+
+describe(getResolvedHostName.name, () => {
+  const mockRegion = "mockRegion";
+  const mockPartition = "mockPartition";
+  const mockHostname = "{region}.mockHostname.com";
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns hostname if available in regionHash", () => {
+    const mockRegionHash: RegionHash = {
+      [mockRegion]: {
+        hostname: mockHostname,
+      },
+    };
+    const mockPartitionHash: PartitionHash = {};
+    expect(getResolvedHostName(mockRegion, { regionHash: mockRegionHash, partitionHash: mockPartitionHash })).toBe(
+      mockHostname
+    );
+    expect(getResolvedPartition).not.toHaveBeenCalled();
+  });
+
+  describe("when hostname is not available in regionHash", () => {
+    const mockRegionHash: RegionHash = {};
+
+    beforeEach(() => {
+      (getResolvedPartition as jest.Mock).mockReturnValue(mockPartition);
+    });
+
+    it("returns hostname if available in partitionHash", () => {
+      const mockPartitionHash: PartitionHash = {
+        [mockPartition]: {
+          regions: [mockRegion, `${mockRegion}2`, `${mockRegion}3`],
+          hostname: mockHostname,
+        },
+      };
+      expect(getResolvedHostName(mockRegion, { regionHash: mockRegionHash, partitionHash: mockPartitionHash })).toBe(
+        mockHostname.replace("{region}", mockRegion)
+      );
+      expect(getResolvedPartition).toHaveBeenCalledTimes(1);
+      expect(getResolvedPartition).toHaveBeenCalledWith(mockRegion, { partitionHash: mockPartitionHash });
+    });
+
+    it("throws if partitionHash is empty", () => {
+      const mockPartitionHash: PartitionHash = {};
+      expect(() =>
+        getResolvedHostName(mockRegion, { regionHash: mockRegionHash, partitionHash: mockPartitionHash })
+      ).toThrow();
+    });
+  });
+});
