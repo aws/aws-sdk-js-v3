6df7f166a55b0ca0f2e8f3bf92a2ec869a024821
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-08-23 08:07:24 -0700

    test(config-resolver): getResolvedPartition.spec.ts

--- packages/config-resolver/src/getRegionInfo/getResolvedPartition.ts
+++ packages/config-resolver/src/getRegionInfo/getResolvedPartition.ts
@@ -1,4 +1,4 @@
 import { RegionInfoOptions } from "./types";
 
 export const getResolvedPartition = (region: string, { partitionHash }: Omit<RegionInfoOptions, "regionHash">) =>
-  Object.keys(partitionHash).find((key) => partitionHash[key].regions.includes(region)) ?? "aws";
+  Object.keys(partitionHash || {}).find((key) => partitionHash[key].regions.includes(region)) ?? "aws";
