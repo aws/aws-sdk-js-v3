2fe6af84dcbeddd34e75cc7c7e86be4fa3b7ad6c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-08-25 00:22:15 +0000

    test: resolveEndpointsConfig.spec.ts

--- packages/config-resolver/src/endpointsConfig/resolveEndpointsConfig.spec.ts
+++ packages/config-resolver/src/endpointsConfig/resolveEndpointsConfig.spec.ts
@@ -1,144 +1,63 @@
-import { Endpoint } from "@aws-sdk/types";
-
+import { getEndpointFromRegion } from "./getEndpointFromRegion";
+import { normalizeEndpoint } from "./normalizeEndpoint";
 import { resolveEndpointsConfig } from "./resolveEndpointsConfig";
 
-describe("EndpointsConfig", () => {
-  const region = jest.fn();
-  const urlParser = jest.fn();
-  const regionInfoProvider = jest.fn();
+jest.mock("./getEndpointFromRegion");
+jest.mock("./normalizeEndpoint");
 
-  const input = { region, urlParser, regionInfoProvider };
+describe(resolveEndpointsConfig.name, () => {
+  const mockEndpoint = {
+    protocol: "http:",
+    hostname: "localhost",
+    path: "/",
+  };
+
+  const mockInput = { endpoint: mockEndpoint } as any;
+
+  beforeEach(() => {
+    (getEndpointFromRegion as jest.Mock).mockResolvedValueOnce(mockEndpoint);
+    (normalizeEndpoint as jest.Mock).mockReturnValueOnce(() => Promise.resolve(mockEndpoint));
+  });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   describe("tls", () => {
-    [true, false].forEach((tls) => {
-      it(`returns input.tls when it's ${tls}`, () => {
-        expect(resolveEndpointsConfig({ ...input, tls }).tls).toStrictEqual(tls);
-      });
+    it.each([true, false])("returns %s when it's %s", (tls) => {
+      expect(resolveEndpointsConfig({ ...mockInput, tls }).tls).toStrictEqual(tls);
     });
 
     it("returns true is input.tls is undefined", () => {
-      expect(resolveEndpointsConfig({ ...input }).tls).toStrictEqual(true);
+      expect(resolveEndpointsConfig({ ...mockInput }).tls).toStrictEqual(true);
+    });
+  });
+
+  describe("isCustomEndpoint", () => {
+    it("returns true when endpoint is defined", () => {
+      expect(resolveEndpointsConfig(mockInput).isCustomEndpoint).toStrictEqual(true);
+    });
+
+    it("returns false when endpoint is not defined", () => {
+      expect(resolveEndpointsConfig({} as any).isCustomEndpoint).toStrictEqual(false);
     });
   });
 
   describe("endpoint", () => {
-    describe("if defined in input.endpoint", () => {
-      const mockEndpoint: Endpoint = { protocol: "protocol", hostname: "hostname", path: "path" };
-
-      it("returns output of urlParser if endpoint is of type string", async () => {
-        const endpoint = "endpoint";
-        urlParser.mockReturnValueOnce(mockEndpoint);
-        const { endpoint: endpointProvider, isCustomEndpoint } = resolveEndpointsConfig({ ...input, endpoint });
-        expect(isCustomEndpoint).toBe(true);
-        const endpointOutput = await endpointProvider();
-        expect(endpointOutput).toStrictEqual(mockEndpoint);
-        expect(urlParser).toHaveBeenCalledTimes(1);
-        expect(urlParser).toHaveBeenCalledWith(endpoint);
-      });
-
-      it("returns promisified endpoint if it's of type object", async () => {
-        const endpoint = mockEndpoint;
-        const { endpoint: endpointProvider, isCustomEndpoint } = resolveEndpointsConfig({ ...input, endpoint });
-        expect(isCustomEndpoint).toBe(true);
-        const endpointOutput = await endpointProvider();
-        expect(endpointOutput).toStrictEqual(endpoint);
-        expect(urlParser).not.toHaveBeenCalled();
-      });
-
-      it("returns endpoint if it's already Provider<Endpoint>", async () => {
-        const endpoint = () => Promise.resolve(mockEndpoint);
-        const { endpoint: endpointProvider, isCustomEndpoint } = resolveEndpointsConfig({ ...input, endpoint });
-        expect(isCustomEndpoint).toBe(true);
-        const endpointOutput = await endpointProvider();
-        expect(endpointOutput).toStrictEqual(mockEndpoint);
-        expect(urlParser).not.toHaveBeenCalled();
-      });
+    it("returns from normalizeEndpoint when endpoint is defined", async () => {
+      const endpoint = await resolveEndpointsConfig(mockInput).endpoint();
+      expect(endpoint).toStrictEqual(mockEndpoint);
+      expect(normalizeEndpoint).toHaveBeenCalledTimes(1);
+      expect(normalizeEndpoint).toHaveBeenCalledWith(mockInput);
+      expect(getEndpointFromRegion).not.toHaveBeenCalled();
     });
 
-    describe("if not defined in input.endpoint", () => {
-      const mockRegion = "mockRegion";
-      const mockHostname = "mockHostname";
-      const mockEndpoint: Endpoint = { protocol: "protocol", hostname: "hostname", path: "path" };
-
-      it("isCustomEndpoint should be false", () => {
-        const { isCustomEndpoint } = resolveEndpointsConfig({ ...input });
-        expect(isCustomEndpoint).toBe(false);
-      });
-
-      describe("returns endpoint", () => {
-        beforeEach(() => {
-          region.mockResolvedValueOnce(mockRegion);
-          regionInfoProvider.mockResolvedValueOnce({ hostname: mockHostname });
-          urlParser.mockReturnValueOnce(mockEndpoint);
-        });
-
-        it("passes http in urlParser if tls is false", async () => {
-          const endpoint = await resolveEndpointsConfig({ ...input, tls: false }).endpoint();
-          expect(endpoint).toStrictEqual(mockEndpoint);
-          expect(urlParser).toHaveBeenCalledTimes(1);
-          expect(urlParser).toHaveBeenCalledWith(`http://${mockHostname}`);
-        });
-
-        [true, undefined].forEach((tls) => {
-          it(`passes https in urlParser if tls is ${tls}`, async () => {
-            const endpoint = await resolveEndpointsConfig({ ...input, tls }).endpoint();
-            expect(endpoint).toStrictEqual(mockEndpoint);
-            expect(urlParser).toHaveBeenCalledTimes(1);
-            expect(urlParser).toHaveBeenCalledWith(`https://${mockHostname}`);
-          });
-        });
-      });
-
-      describe("throws error", () => {
-        const error = new Error("error");
-
-        describe("if region", () => {
-          it("throws error", () => {
-            region.mockRejectedValueOnce(error);
-            return expect(resolveEndpointsConfig(input).endpoint()).rejects.toStrictEqual(error);
-          });
-
-          it("is invalid", () => {
-            [
-              "",
-              "has_underscore",
-              "-starts-with-dash",
-              "ends-with-dash-",
-              "-starts-and-ends-with-dash-",
-              "-",
-              "c0nt@in$-$ymb01$",
-              "0123456789012345678901234567890123456789012345678901234567890123", // 64 characters
-            ].forEach((invalidRegion) => {
-              region.mockResolvedValueOnce(invalidRegion);
-              return expect(resolveEndpointsConfig(input).endpoint()).rejects.toStrictEqual(
-                new Error("Invalid region in client config")
-              );
-            });
-          });
-        });
-
-        describe("if regionInfoProvider", () => {
-          beforeEach(() => {
-            region.mockResolvedValueOnce(mockRegion);
-          });
-
-          it("throws error", () => {
-            regionInfoProvider.mockRejectedValueOnce(error);
-            return expect(resolveEndpointsConfig(input).endpoint()).rejects.toStrictEqual(error);
-          });
-
-          it("returns undefined", () => {
-            regionInfoProvider.mockResolvedValueOnce(undefined);
-            return expect(resolveEndpointsConfig(input).endpoint()).rejects.toStrictEqual(
-              new Error("Cannot resolve hostname from client config")
-            );
-          });
-        });
-      });
+    it("returns from getEndpointFromRegion when endpoint is not defined", async () => {
+      const endpoint = await resolveEndpointsConfig({} as any).endpoint();
+      expect(endpoint).toStrictEqual(mockEndpoint);
+      expect(normalizeEndpoint).not.toHaveBeenCalled();
+      expect(getEndpointFromRegion).toHaveBeenCalledTimes(1);
+      expect(getEndpointFromRegion).toHaveBeenCalledWith({});
     });
   });
 });
