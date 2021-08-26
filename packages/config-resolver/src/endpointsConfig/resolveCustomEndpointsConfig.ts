55866b1ee299646037cb83723fd3b969a8fec4cd
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-08-26 10:00:43 -0700

    chore: call endpointsConfig utilities

--- packages/config-resolver/src/endpointsConfig/resolveCustomEndpointsConfig.ts
+++ packages/config-resolver/src/endpointsConfig/resolveCustomEndpointsConfig.ts
@@ -1,32 +1,20 @@
 import { Endpoint, Provider, UrlParser } from "@aws-sdk/types";
 
-export interface CustomEndpointsInputConfig {
+import { EndpointsInputConfig, EndpointsResolvedConfig } from "./resolveEndpointsConfig";
+import { normalizeEndpoint } from "./utils/normalizeEndpoint";
+
+export interface CustomEndpointsInputConfig extends EndpointsInputConfig {
   /**
    * The fully qualified endpoint of the webservice.
    */
   endpoint: string | Endpoint | Provider<Endpoint>;
-
-  /**
-   * Whether TLS is enabled for requests.
-   */
-  tls?: boolean;
 }
 
 interface PreviouslyResolved {
   urlParser: UrlParser;
 }
 
-export interface CustomEndpointsResolvedConfig extends Required<CustomEndpointsInputConfig> {
-  /**
-   * Resolved value for input {@link CustomEndpointsInputConfig.endpoint}
-   */
-  endpoint: Provider<Endpoint>;
-  /**
-   * Whether the endpoint is specified by caller.
-   * @internal
-   */
-  isCustomEndpoint: true;
-}
+export interface CustomEndpointsResolvedConfig extends EndpointsResolvedConfig {}
 
 export const resolveCustomEndpointsConfig = <T>(
   input: T & CustomEndpointsInputConfig & PreviouslyResolved
@@ -36,15 +24,3 @@ export const resolveCustomEndpointsConfig = <T>(
   endpoint: normalizeEndpoint(input),
   isCustomEndpoint: true,
 });
-
-const normalizeEndpoint = (input: CustomEndpointsInputConfig & PreviouslyResolved): Provider<Endpoint> => {
-  const { endpoint, urlParser } = input;
-  if (typeof endpoint === "string") {
-    const promisified = Promise.resolve(urlParser(endpoint));
-    return () => promisified;
-  } else if (typeof endpoint === "object") {
-    const promisified = Promise.resolve(endpoint);
-    return () => promisified;
-  }
-  return endpoint;
-};
