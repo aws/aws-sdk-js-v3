8a271be6ac94080010aa4c3567b6770a2eee5547
AllanZhengYP <zheallan@amazon.com>
2021-08-30 12:38:55 -0700

    fix(middleware-sdk-rds): double encoding the presigned url (#2711)
    * fix(middleware-sdk-rds): double encoding the presigned url
    
    * fix(middleware-sdk-rds): support StartDBInstanceAutomatedBackupsReplication api
    
    According to codegen, StartDBInstanceAutomatedBackupsReplication API should be
    supported with cross-region copy.
    
    * fix(client-docdb): remove cross-region copy for non-applicable apis

--- packages/middleware-sdk-rds/src/index.ts
+++ packages/middleware-sdk-rds/src/index.ts
@@ -14,7 +14,6 @@ import {
   Provider,
 } from "@aws-sdk/types";
 import { formatUrl } from "@aws-sdk/util-format-url";
-import { escapeUri } from "@aws-sdk/util-uri-escape";
 
 const regARN = /arn:[\w+=/,.@-]+:[\w+=/,.@-]+:([\w+=/,.@-]*)?:[0-9]+:[\w+=/,.@-]+(:[\w+=/,.@-]+)?(:[\w+=/,.@-]+)?/;
 
@@ -23,6 +22,7 @@ const sourceIds: string[] = [
   "SourceDBInstanceIdentifier",
   "ReplicationSourceIdentifier",
   "SourceDBClusterSnapshotIdentifier",
+  "SourceDBInstanceArn",
 ];
 
 const sourceIdToCommandKeyMap: { [key: string]: string } = {
@@ -30,6 +30,7 @@ const sourceIdToCommandKeyMap: { [key: string]: string } = {
   SourceDBInstanceIdentifier: "CreateDBInstanceReadReplica",
   ReplicationSourceIdentifier: "CreateDBCluster",
   SourceDBClusterSnapshotIdentifier: "CopyDBClusterSnapshot",
+  SourceDBInstanceArn: "StartDBInstanceAutomatedBackupsReplication",
 };
 
 const version = "2014-10-31";
@@ -94,7 +95,7 @@ export function crossRegionPresignedUrlMiddleware(options: PreviouslyResolved): 
           ...args,
           input: {
             ...args.input,
-            PreSignedUrl: escapeUri(formatUrl(presignedRequest)),
+            PreSignedUrl: formatUrl(presignedRequest),
           },
         };
       }
