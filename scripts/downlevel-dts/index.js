58e0b22912c3414b7abe8606f0d6725eeb9efd2b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-09-27 20:10:06 +0000

    feat(scripts): run downlevel-dts in parallel
    yarn run v1.22.11
    $ node ./scripts/downlevel-dts
    Done in 60.48s.
    yarn downlevel-dts  1980.35s user 87.63s system 3411% cpu 1:00.61 total

--- scripts/downlevel-dts/index.js
+++ scripts/downlevel-dts/index.js
@@ -1,10 +1,14 @@
 // @ts-check
 const yargs = require("yargs");
 
-const { existsSync, readdirSync, readFileSync, statSync, writeFileSync } = require("fs");
+const { readFileSync, readdirSync, statSync } = require("fs");
+const { access, readFile, writeFile } = require("fs/promises");
 const { join } = require("path");
-const { execSync } = require("child_process");
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
 const stripComments = require("strip-comments");
+const { cpus } = require("os");
+const parallelLimit = require("async/parallelLimit");
 
 // ToDo: Write downlevel-dts as a yargs command, and import yargs in scripts instead.
 yargs
@@ -32,60 +36,72 @@ const getAllFiles = (dirPath, arrayOfFiles = []) => {
   return arrayOfFiles;
 };
 
-packages
+const downlevelWorkspace = async (workspacesDir, workspaceName) => {
+  const workspaceDir = join(workspacesDir, workspaceName);
+
+  const packageJsonPath = join(workspaceDir, "package.json");
+  const packageJson = JSON.parse((await readFile(packageJsonPath)).toString());
+  if (!packageJson.scripts["downlevel-dts"]) {
+    console.error(`The "downlevel-dts" script is not defined for "${workspaceDir}"`);
+    return;
+  }
+  const downlevelArgs = packageJson.scripts["downlevel-dts"].split(" ");
+  const downlevelDirname = downlevelArgs[2].replace(`${downlevelArgs[1]}/`, "");
+
+  const tsTypesConfigPath = join(workspaceDir, "tsconfig.types.json");
+  const declarationDirname = JSON.parse((await readFile(tsTypesConfigPath)).toString()).compilerOptions.declarationDir;
+
+  if (!declarationDirname) {
+    throw new Error(`The declarationDir is not defined in "${tsTypesConfigPath}".`);
+  }
+
+  const declarationDir = join(workspaceDir, declarationDirname);
+  try {
+    await access(declarationDir);
+  } catch (error) {
+    throw new Error(
+      `The types for "${workspaceName}" do not exist.\n` +
+        `Please build types for workspace "${workspaceDir}" before running downlevel-dts script.`
+    );
+  }
+
+  const downlevelDir = join(declarationDir, downlevelDirname);
+  // Create downlevel-dts folder if it doesn't exist
+  try {
+    await access(downlevelDir);
+  } catch (error) {
+    await exec(["yarn", "downlevel-dts"].join(" "), { cwd: workspaceDir });
+  }
+
+  // Strip comments from downlevel-dts files
+  try {
+    await access(downlevelDir);
+    getAllFiles(downlevelDir).forEach(async (downlevelTypesFilepath) => {
+      try {
+        const content = await readFile(downlevelTypesFilepath, "utf8");
+        await writeFile(downlevelTypesFilepath, stripComments(content));
+      } catch (error) {
+        console.error(`Error while stripping comments from "${downlevelTypesFilepath.replace(process.cwd(), "")}"`);
+        console.error(error);
+      }
+    });
+  } catch (error) {}
+};
+
+const workspaces = packages
   .map((dir) => dir.replace("/*", ""))
-  .forEach((workspacesDir) => {
-    // Process each workspace in workspace directory
+  .flatMap((workspacesDir) =>
     readdirSync(join(process.cwd(), workspacesDir), { withFileTypes: true })
       .filter((dirent) => dirent.isDirectory())
-      .map((dirent) => dirent.name)
-      .forEach((workspaceName) => {
-        const workspaceDir = join(workspacesDir, workspaceName);
+      .map((dirent) => ({ workspacesDir, workspaceName: dirent.name }))
+  );
 
-        const packageJsonPath = join(workspaceDir, "package.json");
-        const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
-        if (!packageJson.scripts["downlevel-dts"]) {
-          console.error(`The "downlevel-dts" script is not defined for "${workspaceDir}"`);
-          return;
-        }
-        const downlevelArgs = packageJson.scripts["downlevel-dts"].split(" ");
-        const downlevelDirname = downlevelArgs[2].replace(`${downlevelArgs[1]}/`, "");
+const tasks = workspaces.map(({ workspacesDir, workspaceName }) => async () => {
+  await downlevelWorkspace(workspacesDir, workspaceName);
+});
 
-        const tsTypesConfigPath = join(workspaceDir, "tsconfig.types.json");
-        const declarationDirname = JSON.parse(readFileSync(tsTypesConfigPath).toString()).compilerOptions
-          .declarationDir;
-
-        if (!declarationDirname) {
-          throw new Error(`The declarationDir is not defined in "${tsTypesConfigPath}".`);
-        }
-
-        const declarationDir = join(workspaceDir, declarationDirname);
-        if (!existsSync(declarationDir)) {
-          throw new Error(
-            `The types for "${workspaceName}" do not exist.\n` +
-              `Please build types for workspace "${workspaceDir}" before running downlevel-dts script.`
-          );
-        }
-
-        const downlevelDir = join(declarationDir, downlevelDirname);
-        // Create downlevel-dts folder if it doesn't exist
-        if (!existsSync(downlevelDir)) {
-          execSync(["yarn", "downlevel-dts"].join(" "), { cwd: workspaceDir });
-        }
-
-        // Strip comments from downlevel-dts files
-        if (existsSync(downlevelDir)) {
-          getAllFiles(downlevelDir).forEach((downlevelTypesFilepath) => {
-            try {
-              const content = readFileSync(downlevelTypesFilepath, "utf8");
-              writeFileSync(downlevelTypesFilepath, stripComments(content));
-            } catch (error) {
-              console.error(
-                `Error while stripping comments from "${downlevelTypesFilepath.replace(process.cwd(), "")}"`
-              );
-              console.error(error);
-            }
-          });
-        }
-      });
-  });
+parallelLimit(tasks, cpus().length, function (err) {
+  if (err) {
+    throw err;
+  }
+});
