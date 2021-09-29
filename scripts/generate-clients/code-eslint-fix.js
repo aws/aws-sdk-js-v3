481feba13e876203cd065f62ed44d3c591e68793
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-09-29 05:41:02 +0000

    chore: update comment about esprint failure

--- scripts/generate-clients/code-eslint-fix.js
+++ scripts/generate-clients/code-eslint-fix.js
@@ -10,8 +10,9 @@ const eslintFixCode = async () => {
       "--quiet",
     ]);
   } catch (error) {
-    // esprint throws error as there's no way to disable 'prefer-const'
-    // which fails in clients. We'll still get fixed code though.
+    // esprint throws error as the clients source code does not follow 'prefer-const' rule.
+    // And esprint does not have a way to override rules written in .eslintrc
+    // We will still get linted code though.
   }
 };
 
