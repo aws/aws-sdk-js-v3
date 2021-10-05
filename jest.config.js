615a271dadfbe6d7deca1678abebbf4a6c29125c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-10-05 15:53:03 -0700

    fix: set isolatedModules in ts-jest
    Refs: https://github.com/kulshekhar/ts-jest/issues/1967#issuecomment-697494014
    Refs: https://github.com/kulshekhar/ts-jest/issues/2015

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,9 @@ module.exports = {
   ],
   testPathIgnorePatterns: ["/node_modules/", "<rootDir>/clients/client-.*"],
   coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/clients/client-.*", "/__fixtures__/"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
 };
