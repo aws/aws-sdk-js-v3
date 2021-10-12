6dd0af82243c175e1b5fc400f2a1893a095895ee
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-10-12 10:22:30 -0700

    fix(scripts): do not destructure function parameter (#2891)

--- scripts/downlevel-dts/getAllFiles.mjs
+++ scripts/downlevel-dts/getAllFiles.mjs
@@ -1,15 +1,18 @@
+// @ts-check
 import { readdir, stat } from "fs/promises";
+import { join } from "path";
 
 export const getAllFiles = async (dirPath, arrayOfFiles = []) => {
   const files = await readdir(dirPath);
 
   for (const file of files) {
-    const { isDirectory } = await stat(dirPath + "/" + file);
-    if (isDirectory()) {
-      const filesInDirectory = await getAllFiles(dirPath + "/" + file, arrayOfFiles);
-      arrayOfFiles.push(filesInDirectory);
+    const filePath = join(dirPath, file);
+    const stats = await stat(filePath);
+    if (stats.isDirectory()) {
+      const filesInDirectory = await getAllFiles(filePath, arrayOfFiles);
+      arrayOfFiles.concat(filesInDirectory);
     } else {
-      arrayOfFiles.push(join(dirPath, "/", file));
+      arrayOfFiles.push(filePath);
     }
   }
 
