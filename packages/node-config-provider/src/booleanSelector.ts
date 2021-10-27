0b9756fbae2b29f1d2a376d157dc282c32757fd3
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-10-27 21:28:32 +0000

    feat(node-config-provider): add utility booleanSelector

---
+++ packages/node-config-provider/src/booleanSelector.ts
@@ -0,0 +1,11 @@
+export enum SelectorType {
+  ENV = "env",
+  SHARED_CONFIG = "shared config entry",
+}
+
+export const booleanSelector = (obj: Object, key: string, type: SelectorType) => {
+  if (!Object.prototype.hasOwnProperty.call(obj, key)) return undefined;
+  if (obj[key] === "true") return true;
+  if (obj[key] === "false") return false;
+  throw new Error(`Cannot load ${type} "${key}". Expected "true" or "false", got ${obj[key]}.`);
+};
