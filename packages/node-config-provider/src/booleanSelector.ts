3be502b18de10d96d9a7fbedbdabe433c2a891d8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-10-27 22:33:17 +0000

    chore: add docs and @internal for booleanSelector

--- packages/node-config-provider/src/booleanSelector.ts
+++ packages/node-config-provider/src/booleanSelector.ts
@@ -3,6 +3,14 @@ export enum SelectorType {
   CONFIG = "shared config entry",
 }
 
+/**
+ * Returns boolean value true/false for string value "true"/"false",
+ * if the string is defined in obj[key]
+ * Returns undefined, if obj[key] is not defined.
+ * Throws error for all other cases.
+ *
+ * @internal
+ */
 export const booleanSelector = (obj: { [key: string]: string }, key: string, type: SelectorType) => {
   if (!(key in obj)) return undefined;
   if (obj[key] === "true") return true;
