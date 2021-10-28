504d08142df92221f1bc052a6e20979c46ccd514
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-10-28 01:16:08 +0000

    chore: use booleanSelector

--- packages/middleware-bucket-endpoint/src/NodeUseArnRegionConfigOptions.spec.ts
+++ packages/middleware-bucket-endpoint/src/NodeUseArnRegionConfigOptions.spec.ts
@@ -1,69 +1,50 @@
+import { booleanSelector, SelectorType } from "@aws-sdk/node-config-provider";
+
 import {
-  NODE_USE_ARN_REGION_CONFIG_OPTIONS as loadOptions,
+  NODE_USE_ARN_REGION_CONFIG_OPTIONS,
   NODE_USE_ARN_REGION_ENV_NAME,
   NODE_USE_ARN_REGION_INI_NAME,
 } from "./NodeUseArnRegionConfigOptions";
 
-describe("Node useArnRegion config loader", () => {
-  describe("environment variable selector", () => {
-    const env: { [NODE_USE_ARN_REGION_ENV_NAME]: any } = {} as any;
+jest.mock("@aws-sdk/node-config-provider");
 
-    beforeEach(() => {
-      delete env[NODE_USE_ARN_REGION_ENV_NAME];
-    });
-
-    it(`should return undefined if the environment variable doesn't have ${NODE_USE_ARN_REGION_ENV_NAME}`, () => {
-      expect(loadOptions.environmentVariableSelector(env)).toBeUndefined();
-    });
-
-    [
-      { envValue: "true", parsed: true },
-      { envValue: "false", parsed: false },
-    ].forEach(({ envValue, parsed }) => {
-      it(`should return boolean if the environment variable ${NODE_USE_ARN_REGION_ENV_NAME} is ${envValue}`, () => {
-        env[NODE_USE_ARN_REGION_ENV_NAME] = envValue;
-        expect(loadOptions.environmentVariableSelector(env)).toBe(parsed);
-      });
-    });
-
-    ["0", "1", "yes", "no", undefined, null, void 0, ""].forEach((envValue) => {
-      it(`should throw if the environment variable ${NODE_USE_ARN_REGION_ENV_NAME} is ${envValue}`, () => {
-        env[NODE_USE_ARN_REGION_ENV_NAME] = envValue as any;
-        expect(() => loadOptions.environmentVariableSelector(env)).toThrow(
-          `Cannot load env ${NODE_USE_ARN_REGION_ENV_NAME}. Expected "true" or "false", got ${envValue}.`
-        );
-      });
-    });
+describe("NODE_USE_ARN_REGION_CONFIG_OPTIONS", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  describe("shared INI files selector", () => {
+  const test = (func: Function, obj: { [key: string]: string }, key: string, type: SelectorType) => {
+    it.each([true, false, undefined])("returns %s", (output) => {
+      (booleanSelector as jest.Mock).mockReturnValueOnce(output);
+      expect(func(obj)).toEqual(output);
+      expect(booleanSelector).toBeCalledWith(obj, key, type);
+    });
+
+    it("throws error", () => {
+      const mockError = new Error("error");
+      (booleanSelector as jest.Mock).mockImplementationOnce(() => {
+        throw mockError;
+      });
+      expect(() => {
+        func(obj);
+      }).toThrow(mockError);
+    });
+  };
+
+  describe("calls booleanSelector for environmentVariableSelector", () => {
+    const env: { [NODE_USE_ARN_REGION_ENV_NAME]: any } = {} as any;
+    const { environmentVariableSelector } = NODE_USE_ARN_REGION_CONFIG_OPTIONS;
+    test(environmentVariableSelector, env, NODE_USE_ARN_REGION_ENV_NAME, SelectorType.ENV);
+  });
+
+  describe("calls booleanSelector for configFileSelector", () => {
     const profileContent: { [NODE_USE_ARN_REGION_INI_NAME]: any } = {} as any;
+    const { configFileSelector } = NODE_USE_ARN_REGION_CONFIG_OPTIONS;
+    test(configFileSelector, profileContent, NODE_USE_ARN_REGION_INI_NAME, SelectorType.CONFIG);
+  });
 
-    beforeEach(() => {
-      delete profileContent[NODE_USE_ARN_REGION_INI_NAME];
-    });
-
-    it(`should return undefined if shared config profile doesn't have ${NODE_USE_ARN_REGION_INI_NAME}`, () => {
-      expect(loadOptions.configFileSelector(profileContent)).toBeUndefined();
-    });
-
-    [
-      { value: "true", parsed: true },
-      { value: "false", parsed: false },
-    ].forEach(({ value, parsed }) => {
-      it(`should return boolean if the shared config profile ${NODE_USE_ARN_REGION_INI_NAME} is ${value}`, () => {
-        profileContent[NODE_USE_ARN_REGION_INI_NAME] = value;
-        expect(loadOptions.configFileSelector(profileContent)).toBe(parsed);
-      });
-    });
-
-    ["0", "1", "yes", "no", undefined, null, void 0, ""].forEach((value) => {
-      it(`should throw if the shared config profile ${NODE_USE_ARN_REGION_INI_NAME} is ${value}`, () => {
-        profileContent[NODE_USE_ARN_REGION_INI_NAME] = value as any;
-        expect(() => loadOptions.configFileSelector(profileContent)).toThrow(
-          `Cannot load shared config entry ${NODE_USE_ARN_REGION_INI_NAME}. Expected "true" or "false", got ${value}.`
-        );
-      });
-    });
+  it("returns false for default", () => {
+    const { default: defaultValue } = NODE_USE_ARN_REGION_CONFIG_OPTIONS;
+    expect(defaultValue).toEqual(false);
   });
 });
