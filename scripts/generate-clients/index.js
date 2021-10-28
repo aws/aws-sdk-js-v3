6aac9d6c62c40fbe7e1ea3db4d366f1683a8d8ed
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-10-28 16:10:35 +0000

    chore: call operations on CODE_GEN_GENERIC_CLIENT_OUTPUT_DIR

--- scripts/generate-clients/index.js
+++ scripts/generate-clients/index.js
@@ -2,10 +2,11 @@
 const yargs = require("yargs");
 const path = require("path");
 const { emptyDirSync, rmdirSync } = require("fs-extra");
-const { generateClients, generateProtocolTests } = require("./code-gen");
+const { generateClients, generateGenericClient, generateProtocolTests } = require("./code-gen");
 const { copyToClients, copyServerTests } = require("./copy-to-clients");
 const {
   CODE_GEN_SDK_OUTPUT_DIR,
+  CODE_GEN_GENERIC_CLIENT_OUTPUT_DIR,
   CODE_GEN_PROTOCOL_TESTS_OUTPUT_DIR,
   TEMP_CODE_GEN_INPUT_DIR,
 } = require("./code-gen-dir");
@@ -58,17 +59,29 @@ const {
     }
 
     await generateClients(models || globs);
-    if (!noPrivateClients) await generateProtocolTests();
+    if (!noPrivateClients) {
+      await generateGenericClient();
+      await generateProtocolTests();
+    }
 
     await eslintFixCode();
     await prettifyCode(CODE_GEN_SDK_OUTPUT_DIR);
-    if (!noPrivateClients) await prettifyCode(CODE_GEN_PROTOCOL_TESTS_OUTPUT_DIR);
+    if (!noPrivateClients) {
+      await prettifyCode(CODE_GEN_GENERIC_CLIENT_OUTPUT_DIR);
+      await prettifyCode(CODE_GEN_PROTOCOL_TESTS_OUTPUT_DIR);
+    }
 
     await copyToClients(CODE_GEN_SDK_OUTPUT_DIR, clientsDir);
-    if (!noPrivateClients) await copyToClients(CODE_GEN_PROTOCOL_TESTS_OUTPUT_DIR, PROTOCOL_TESTS_CLIENTS_DIR);
+    if (!noPrivateClients) {
+      await copyToClients(CODE_GEN_GENERIC_CLIENT_OUTPUT_DIR, PROTOCOL_TESTS_CLIENTS_DIR);
+      await copyToClients(CODE_GEN_PROTOCOL_TESTS_OUTPUT_DIR, PROTOCOL_TESTS_CLIENTS_DIR);
+    }
 
     emptyDirSync(CODE_GEN_SDK_OUTPUT_DIR);
-    if (!noPrivateClients) emptyDirSync(CODE_GEN_PROTOCOL_TESTS_OUTPUT_DIR);
+    if (!noPrivateClients) {
+      emptyDirSync(CODE_GEN_GENERIC_CLIENT_OUTPUT_DIR);
+      emptyDirSync(CODE_GEN_PROTOCOL_TESTS_OUTPUT_DIR);
+    }
     emptyDirSync(TEMP_CODE_GEN_INPUT_DIR);
 
     rmdirSync(TEMP_CODE_GEN_INPUT_DIR);
