4c8ed6e488fbc914d8ee7a1ed795f52a41bc4383
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-10-29 00:53:22 +0000

    fix(middleware-*): move useDualstackEndpoint to previously resolved

--- packages/middleware-bucket-endpoint/src/configurations.ts
+++ packages/middleware-bucket-endpoint/src/configurations.ts
@@ -15,13 +15,6 @@ export interface BucketEndpointInputConfig {
    */
   useAccelerateEndpoint?: boolean;
   /**
-   * Enables IPv6/IPv4 dualstack endpoint. When a DNS lookup is performed on an endpoint of this type, it returns an “A”
-   * record with an IPv4 address and an “AAAA” record with an IPv6 address. In most cases the network stack in the
-   * client environment will automatically prefer the AAAA record and make a connection using the IPv6 address. Note,
-   * however, that currently on Windows, the IPv4 address will be preferred.
-   */
-  useDualstackEndpoint: Provider<boolean>;
-  /**
    * Whether to override the request region with the region inferred from requested resource's ARN. Defaults to false
    */
   useArnRegion?: boolean | Provider<boolean>;
@@ -36,6 +29,7 @@ interface PreviouslyResolved {
   isCustomEndpoint: boolean;
   region: Provider<string>;
   regionInfoProvider: RegionInfoProvider;
+  useDualstackEndpoint: Provider<boolean>;
 }
 
 export interface BucketEndpointResolvedConfig {
