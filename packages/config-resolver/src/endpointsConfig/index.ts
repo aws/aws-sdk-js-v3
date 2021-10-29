17838e1cf647195554120b04423463ff171e77df
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-10-29 18:21:44 +0000

    chore(config-resolver): move endoint config options to endpointsConfig folder

--- packages/config-resolver/src/endpointsConfig/index.ts
+++ packages/config-resolver/src/endpointsConfig/index.ts
@@ -1,2 +1,4 @@
+export * from "./NodeUseDualstackEndpointConfigOptions";
+export * from "./NodeUseFipsEndpointConfigOptions";
 export * from "./resolveCustomEndpointsConfig";
 export * from "./resolveEndpointsConfig";
--- packages/config-resolver/src/regionConfig/index.ts
+++ packages/config-resolver/src/regionConfig/index.ts
@@ -1,4 +1,2 @@
-export * from "./NodeUseDualstackEndpointConfigOptions";
-export * from "./NodeUseFipsEndpointConfigOptions";
 export * from "./config";
 export * from "./resolveRegionConfig";
