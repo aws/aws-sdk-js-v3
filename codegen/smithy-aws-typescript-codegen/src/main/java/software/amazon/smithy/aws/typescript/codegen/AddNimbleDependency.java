b6efbd9e6d13cf7abe41bb15c334593d741ce9ca
AllanZhengYP <zheallan@amazon.com>
2021-11-01 23:12:01 +0000

    chore(client-nimble): add customization changing StudioComponentConfiguration to union

---
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AddNimbleDependency.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.smithy.aws.typescript.codegen;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import software.amazon.smithy.aws.traits.ServiceTrait;
+import software.amazon.smithy.build.PluginContext;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.shapes.ServiceShape;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.shapes.ShapeType;
+import software.amazon.smithy.model.transform.ModelTransformer;
+import software.amazon.smithy.typescript.codegen.TypeScriptSettings;
+import software.amazon.smithy.typescript.codegen.integration.TypeScriptIntegration;
+import software.amazon.smithy.utils.SmithyInternalApi;
+
+/**
+ * Add Nimble customization.
+ */
+@SmithyInternalApi
+public class AddNimbleDependency implements TypeScriptIntegration {
+
+    @Override
+    public Model preprocessModel(PluginContext context, TypeScriptSettings settings) {
+        Model model = context.getModel();
+        ServiceShape service = settings.getService(model);
+        String serviceId = service.getTrait(ServiceTrait.class).map(ServiceTrait::getSdkId).orElse("");
+        if (!serviceId.equals("nimble")) {
+            return context.getModel();
+        }
+        Map<ShapeId, ShapeType> overWriteTypeMap = model.getStructureShapes().stream()
+                .map(structure -> structure.getId())
+                .filter(shapeId -> shapeId.getName().equals("StudioComponentConfiguration"))
+                .collect(Collectors.toMap(shapeId -> shapeId, shapeId -> ShapeType.UNION));
+        return ModelTransformer.create().changeShapeType(model, overWriteTypeMap);
+    }
+}
