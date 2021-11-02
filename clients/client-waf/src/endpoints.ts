55846925ca93976459bd9a001cf458b1c2e35396
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-11-02 18:38:42 +0000

    chore(clients): update endpoints as of 2021/11/02

--- clients/client-waf/src/endpoints.ts
+++ clients/client-waf/src/endpoints.ts
@@ -50,6 +50,10 @@ const regionHash: RegionHash = {
     ],
     signingRegion: "us-east-1",
   },
+  "aws-global-fips": {
+    hostname: "waf-fips.amazonaws.com",
+    signingRegion: "us-east-1",
+  },
 };
 
 const partitionHash: PartitionHash = {
