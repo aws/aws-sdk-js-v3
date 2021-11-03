20ee75c95d1bc674fe732ecc4b44b38d66cd1a0f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-11-03 18:24:27 +0000

    test(functional): fips+dualstack endpoints

---
+++ tests/functional/endpoints/fips-dualstack/index.spec.ts
@@ -0,0 +1,22 @@
+import { join } from "path";
+
+import testCases from "./test_cases_supported.json";
+
+const getClientPackageName = (sdkId: string) =>
+  `client-${sdkId
+    .split(" ")
+    .map((word) => word.toLowerCase())
+    .join("-")}`;
+
+describe("endpoints.fips-dualstack", () => {
+  for (const { sdkId, region, hostname } of testCases) {
+    const clientPackageName = getClientPackageName(sdkId);
+    it(`testing "${clientPackageName}" with region: ${region}`, async () => {
+      const { defaultRegionInfoProvider } = await import(
+        join("..", "..", "..", "..", "clients", clientPackageName, "src", "endpoints")
+      );
+      const regionInfo = await defaultRegionInfoProvider(region, { isFipsEndpoint: true, isDualstackEndpoint: true });
+      expect(regionInfo.hostname).toEqual(hostname);
+    });
+  }
+});
