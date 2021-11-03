cc06244d2c227cf91823cf4fd3a1c1ef4a370976
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-11-03 14:15:12 +0000

    chore(functional): wip for dualstack tests

---
+++ tests/functional/endpoints/dualstack/index.spec.ts
@@ -0,0 +1,22 @@
+import { join } from "path";
+
+import testCases from "./test_cases.json";
+
+const getClientPackageName = (sdkId: string) =>
+  `client-${sdkId
+    .split(" ")
+    .map((word) => word.toLowerCase())
+    .join("-")}`;
+
+describe("endpoints.dualstack", () => {
+  for (const { sdkId, region, hostname } of testCases) {
+    const clientPackageName = getClientPackageName(sdkId);
+    it(`testing "${clientPackageName}" with region: ${region}`, async () => {
+      const { defaultRegionInfoProvider } = await import(
+        join("..", "..", "..", "..", "clients", clientPackageName, "src", "endpoints")
+      );
+      const regionInfo = await defaultRegionInfoProvider(region, { isFipsEndpoint: true });
+      expect(regionInfo.hostname).toEqual(hostname);
+    });
+  }
+});
