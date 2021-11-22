f82846781de99217687b1e35fc575ff5d2429b76
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-11-22 15:30:25 +0000

    chore(protocol_tests): bump smithy to 1.14.x

--- private/aws-protocoltests-restjson/test/functional/restjson1.spec.ts
+++ private/aws-protocoltests-restjson/test/functional/restjson1.spec.ts
@@ -7715,7 +7715,7 @@ it("RestJsonHttpWithEmptyStructurePayload:Request", async () => {
 /**
  * Serializes a payload targeting a structure
  */
-it.skip("RestJsonTestPayloadStructure:Request", async () => {
+it("RestJsonTestPayloadStructure:Request", async () => {
   const client = new RestJsonProtocolClient({
     ...clientParams,
     requestHandler: new RequestSerializationTestHandler(),
@@ -7757,7 +7757,11 @@ it.skip("RestJsonTestPayloadStructure:Request", async () => {
 /**
  * Serializes an request with header members but no payload
  */
+<<<<<<< HEAD
 it.skip("RestJsonHttpWithHeadersButNoPayload:Request", async () => {
+=======
+it("RestJsonHttpWithHeadersButNoPayload:Request", async () => {
+>>>>>>> e2c63c12e4 (chore(protocol_tests): bump smithy to 1.14.x)
   const client = new RestJsonProtocolClient({
     ...clientParams,
     requestHandler: new RequestSerializationTestHandler(),
