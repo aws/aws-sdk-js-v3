6ac70d5d1158eb86bf08d3523351fc758e02f50b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-01-03 22:56:40 +0000

    chore(jest): remove clients from jest projects

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
     "<rootDir>/lib/*/jest.config.js",
     "<rootDir>/private/*/jest.config.js",
     "<rootDir>/packages/*/jest.config.js",
-    "<rootDir>/clients/*/jest.config.js",
   ],
   testPathIgnorePatterns: ["/node_modules/", "<rootDir>/clients/client-.*"],
   coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/clients/client-.*", "/__fixtures__/"],
