b3a24df7b8c99c8dd749397f69c09cd263e515a9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-01-03 14:14:29 +0000

    chore(clients): remove unsupported vesion deprecation warning

--- clients/client-kafkaconnect/src/runtimeConfig.ts
+++ clients/client-kafkaconnect/src/runtimeConfig.ts
@@ -19,13 +19,11 @@ import { defaultUserAgent } from "@aws-sdk/util-user-agent-node";
 import { fromUtf8, toUtf8 } from "@aws-sdk/util-utf8-node";
 import { KafkaConnectClientConfig } from "./KafkaConnectClient";
 import { getRuntimeConfig as getSharedRuntimeConfig } from "./runtimeConfig.shared";
-import { emitWarningIfUnsupportedVersion } from "@aws-sdk/smithy-client";
 
 /**
  * @internal
  */
 export const getRuntimeConfig = (config: KafkaConnectClientConfig) => {
-  emitWarningIfUnsupportedVersion(process.version);
   const clientSharedValues = getSharedRuntimeConfig(config);
   return {
     ...clientSharedValues,
