1b112d535047aa877a01fefb3c37368340e84afe
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-01-06 04:14:13 +0000

    chore(fetch-http-handler): defined tsconfig.test.json

--- packages/fetch-http-handler/jest.config.js
+++ packages/fetch-http-handler/jest.config.js
@@ -3,5 +3,4 @@ const base = require("../../jest.config.base.js");
 module.exports = {
   ...base,
   testEnvironment: "jsdom",
-  testPathIgnorePatterns: ["/node_modules/", "(.*).browser.spec.js"],
 };
