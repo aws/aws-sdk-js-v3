5859e782921f5010700e60eac63a317829b79e9a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-01-07 15:04:21 +0000

    chore(tsconfig): use tsconfig.test.json in ts-jest options

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: "ts-jest",
   testMatch: ["**/*.spec.ts", "!**/*.browser.spec.ts", "!**/*.integ.spec.ts"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+    },
+  },
 };
