3fbbab207d3a68c10b86238c1aa0daf2e063ffae
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-01-07 09:10:42 -0800

    chore(jest): remove unused configuration (#3160)

--- jest.config.integ.js
+++ jest.config.integ.js
@@ -1,8 +1,3 @@
-const base = require("./jest.config.base.js");
-
 module.exports = {
-  ...base,
   projects: ["<rootDir>/clients/*/jest.integ.config.js"],
-  testPathIgnorePatterns: ["/node_modules/"],
-  coveragePathIgnorePatterns: ["/node_modules/", "/__fixtures__/"],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
-const base = require("./jest.config.base.js");
-
 module.exports = {
-  ...base,
   projects: [
     "<rootDir>/lib/*/jest.config.js",
     "<rootDir>/private/*/jest.config.js",
     "<rootDir>/packages/*/jest.config.js",
   ],
-  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/clients/client-.*"],
-  coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/clients/client-.*", "/__fixtures__/"],
 };
