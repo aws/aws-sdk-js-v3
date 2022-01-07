2675ed39892d8f0055e152054a1b9d84fb9fbf33
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-01-07 11:11:22 -0800

    chore(jest): use custom tsconfig for running tests (#3161)

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,4 +1,15 @@
+const { compilerOptions } = require("@tsconfig/recommended/tsconfig.json");
+
 module.exports = {
   preset: "ts-jest",
   testMatch: ["**/*.spec.ts", "!**/*.browser.spec.ts", "!**/*.integ.spec.ts"],
+  globals: {
+    "ts-jest": {
+      tsconfig: {
+        ...compilerOptions,
+        noImplicitAny: false,
+        strictNullChecks: false,
+      },
+    },
+  },
 };
