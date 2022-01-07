09c0eb899adb3a437c49795c9c55fd570596f9f1
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-01-07 16:55:28 +0000

    chore(jest): define tsconfig in testing

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,9 +1,11 @@
+const { compilerOptions } = require("@tsconfig/recommended/tsconfig.json");
+
 module.exports = {
   preset: "ts-jest",
   testMatch: ["**/*.spec.ts", "!**/*.browser.spec.ts", "!**/*.integ.spec.ts"],
   globals: {
     "ts-jest": {
-      tsconfig: "tsconfig.test.json",
+      tsconfig: { ...compilerOptions, noImplicitAny: false },
     },
   },
 };
