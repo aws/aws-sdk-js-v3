e8f8b02effb5f1a23feea09757e6019e1b8e4b61
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-01-20 22:30:56 +0000

    chore(scripts): add utility updateVersions

--- scripts/update-versions/default.ts
+++ scripts/update-versions/default.ts
@@ -3,18 +3,8 @@
 // Updates versions for internal packages `@aws-sdk/*` to `*`
 // in dependencies/devDependencies/peerDependencies
 
-import { readFileSync, writeFileSync } from "fs";
-import { join } from "path";
-
 import { getPackageNameToDefaultHash } from "./getPackageNameToDefaultHash";
-import { getUpdatedPackageJson } from "./getUpdatedPackageJson";
-import { getWorkspacePaths } from "./getWorkspacePaths";
+import { updateVersions } from "./updateVersions";
 
 const packageNameToDefaultHash = getPackageNameToDefaultHash();
-
-getWorkspacePaths().forEach((workspacePath) => {
-  const packageJsonPath = join(workspacePath, "package.json");
-  const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
-  const updatedPackageJson = getUpdatedPackageJson(packageJson, packageNameToDefaultHash);
-  writeFileSync(packageJsonPath, JSON.stringify(updatedPackageJson, null, 2).concat(`\n`));
-});
+updateVersions(packageNameToDefaultHash);
