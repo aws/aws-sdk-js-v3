8392ebd829a2faa8352d860ed8bf083e323454b7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-07 06:20:22 +0000

    chore: convert loadSharedConfigFiles to async

--- packages/shared-ini-file-loader/src/loadSharedConfigFiles.ts
+++ packages/shared-ini-file-loader/src/loadSharedConfigFiles.ts
@@ -4,7 +4,7 @@ import { getHomeDir } from "./getHomeDir";
 import { normalizeConfigFile } from "./normalizeConfigFile";
 import { parseIni } from "./parseIni";
 import { slurpFile } from "./slurpFile";
-import { ParsedIniData, SharedConfigFiles } from "./types";
+import { SharedConfigFiles } from "./types";
 
 export const ENV_CREDENTIALS_PATH = "AWS_SHARED_CREDENTIALS_FILE";
 export const ENV_CONFIG_PATH = "AWS_CONFIG_FILE";
@@ -27,20 +27,19 @@ export interface SharedConfigInit {
 
 const swallowError = () => ({});
 
-export const loadSharedConfigFiles = (init: SharedConfigInit = {}): Promise<SharedConfigFiles> => {
+export const loadSharedConfigFiles = async (init: SharedConfigInit = {}): Promise<SharedConfigFiles> => {
   const {
     filepath = process.env[ENV_CREDENTIALS_PATH] || join(getHomeDir(), ".aws", "credentials"),
     configFilepath = process.env[ENV_CONFIG_PATH] || join(getHomeDir(), ".aws", "config"),
   } = init;
 
-  return Promise.all([
+  const parsedFiles = await Promise.all([
     slurpFile(configFilepath).then(parseIni).then(normalizeConfigFile).catch(swallowError),
     slurpFile(filepath).then(parseIni).catch(swallowError),
-  ]).then((parsedFiles: Array<ParsedIniData>) => {
-    const [configFile, credentialsFile] = parsedFiles;
-    return {
-      configFile,
-      credentialsFile,
-    };
-  });
+  ]);
+
+  return {
+    configFile: parsedFiles[0],
+    credentialsFile: parsedFiles[1],
+  };
 };
