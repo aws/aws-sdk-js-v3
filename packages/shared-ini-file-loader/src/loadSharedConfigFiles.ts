cc2d2ffc3e0bca940e4010b21ebb1dc132beaa6b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-07 06:18:50 +0000

    chore: call slurpFile from loadSharedConfigFiles

--- packages/shared-ini-file-loader/src/loadSharedConfigFiles.ts
+++ packages/shared-ini-file-loader/src/loadSharedConfigFiles.ts
@@ -1,10 +1,9 @@
-// ToDo: Change to "fs/promises" when supporting nodejs>=14
-import { promises as fsPromises } from "fs";
 import { join } from "path";
 
 import { getHomeDir } from "./getHomeDir";
 import { normalizeConfigFile } from "./normalizeConfigFile";
 import { parseIni } from "./parseIni";
+import { slurpFile } from "./slurpFile";
 import { ParsedIniData, SharedConfigFiles } from "./types";
 
 export const ENV_CREDENTIALS_PATH = "AWS_SHARED_CREDENTIALS_FILE";
@@ -28,42 +27,20 @@ export interface SharedConfigInit {
 
 const swallowError = () => ({});
 
-type SharedConfigStatus = {
-  contents: SharedConfigFiles;
-  isReading: boolean;
-  requestQueue: { resolve: Function }[];
-};
-const SharedConfigHash: { [key: string]: SharedConfigStatus } = {};
+export const loadSharedConfigFiles = (init: SharedConfigInit = {}): Promise<SharedConfigFiles> => {
+  const {
+    filepath = process.env[ENV_CREDENTIALS_PATH] || join(getHomeDir(), ".aws", "credentials"),
+    configFilepath = process.env[ENV_CONFIG_PATH] || join(getHomeDir(), ".aws", "config"),
+  } = init;
 
-export const loadSharedConfigFiles = (init: SharedConfigInit = {}): Promise<SharedConfigFiles> =>
-  new Promise((resolve) => {
-    const {
-      filepath = process.env[ENV_CREDENTIALS_PATH] || join(getHomeDir(), ".aws", "credentials"),
-      configFilepath = process.env[ENV_CONFIG_PATH] || join(getHomeDir(), ".aws", "config"),
-    } = init;
-
-    const key = JSON.stringify({ filepath, configFilepath });
-
-    if (!SharedConfigHash[key]) {
-      // Config not read yet. Set to isReading and read shared config.
-      SharedConfigHash[key] = { isReading: true, contents: {} as SharedConfigFiles, requestQueue: [{ resolve }] };
-
-      Promise.all([
-        fsPromises.readFile(configFilepath, "utf-8").then(parseIni).then(normalizeConfigFile).catch(swallowError),
-        fsPromises.readFile(filepath, "utf-8").then(parseIni).catch(swallowError),
-      ]).then((parsedFiles: Array<ParsedIniData>) => {
-        const [configFile, credentialsFile] = parsedFiles;
-        const sharedConfig = { configFile, credentialsFile };
-        SharedConfigHash[key].isReading = false;
-        SharedConfigHash[key].contents = sharedConfig;
-        while (SharedConfigHash[key].requestQueue.length) {
-          const { resolve } = SharedConfigHash[key].requestQueue.pop()!;
-          resolve(sharedConfig);
-        }
-      });
-    } else if (SharedConfigHash[key].isReading) {
-      SharedConfigHash[key].requestQueue.push({ resolve });
-    } else {
-      resolve(SharedConfigHash[key].contents);
-    }
+  return Promise.all([
+    slurpFile(configFilepath).then(parseIni).then(normalizeConfigFile).catch(swallowError),
+    slurpFile(filepath).then(parseIni).catch(swallowError),
+  ]).then((parsedFiles: Array<ParsedIniData>) => {
+    const [configFile, credentialsFile] = parsedFiles;
+    return {
+      configFile,
+      credentialsFile,
+    };
   });
+};
