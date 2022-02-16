7a907b68cc00cec5cd0d0eca9af39a0942b18001
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-16 05:36:47 +0000

    fix(scripts): generate clients in batches

---
+++ scripts/generate-clients/get-model-filepaths.js
@@ -0,0 +1,27 @@
+const { readdirSync, lstatSync } = require("fs");
+const Glob = require("glob");
+const { join } = require("path");
+
+const getModelFilepaths = (models) => {
+  if (typeof models === "string") {
+    //`models` is a folder path
+    console.log(`preparing models from ${models}...`);
+    return readdirSync(models)
+      .map((modelFileName) => join(models, modelFileName))
+      .filter((modelFilepath) => lstatSync(modelFilepath).isFile());
+  } else if (Array.isArray(models)) {
+    //`models` is a list of globs
+    console.log(`preparing models from ${models}...`);
+    return models
+      .map((pattern) =>
+        Glob.sync(pattern, {
+          realpath: true,
+          absolute: true,
+        })
+      )
+      .flat()
+      .filter((modelFilepath) => lstatSync(modelFilepath).isFile());
+  }
+};
+
+module.exports = { getModelFilepaths };
