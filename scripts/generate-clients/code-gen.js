22933fd4c581328d8b33f17fac346f039f6c0af5
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-16 05:54:05 +0000

    chore(scripts): accept batchSize from CLI

--- scripts/generate-clients/code-gen.js
+++ scripts/generate-clients/code-gen.js
@@ -3,29 +3,17 @@ const { basename, join, relative } = require("path");
 const { emptyDirSync } = require("fs-extra");
 const { copyFileSync } = require("fs");
 const { spawnProcess } = require("../utils/spawn-process");
-const {
-  CODE_GEN_ROOT,
-  CODE_GEN_SDK_ROOT,
-  DEFAULT_CODE_GEN_INPUT_DIR,
-  TEMP_CODE_GEN_INPUT_DIR,
-} = require("./code-gen-dir");
+const { CODE_GEN_ROOT, CODE_GEN_SDK_ROOT, TEMP_CODE_GEN_INPUT_DIR } = require("./code-gen-dir");
 const { getModelFilepaths } = require("./get-model-filepaths");
 
-const generateClients = async (models) => {
-  if (!models) {
-    // Models not defined. Choose default input directory
-    models = DEFAULT_CODE_GEN_INPUT_DIR;
-  }
-
+const generateClients = async (models, batchSize) => {
   const filepaths = getModelFilepaths(models);
-
   const options = [
     ":sdk-codegen:clean",
     ":sdk-codegen:build",
     `-PmodelsDirProp=${relative(CODE_GEN_SDK_ROOT, TEMP_CODE_GEN_INPUT_DIR)}`,
   ];
 
-  const batchSize = 50;
   while (filepaths.length > 0) {
     emptyDirSync(TEMP_CODE_GEN_INPUT_DIR);
     const filepathsToCopy = filepaths.splice(0, batchSize);
