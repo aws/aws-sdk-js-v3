b34e3e7b11bfcbfc46b60a86867ea55995681bc9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-24 22:59:31 +0000

    chore: call stringHasher from flexibleChecksumsMiddleware

--- packages/middleware-flexible-checksums/src/flexibleChecksumsMiddleware.ts
+++ packages/middleware-flexible-checksums/src/flexibleChecksumsMiddleware.ts
@@ -15,6 +15,7 @@ import { FlexibleChecksumsMiddlewareConfig } from "./getFlexibleChecksumsPlugin"
 import { hasHeader } from "./hasHeader";
 import { isStreaming } from "./isStreaming";
 import { selectChecksumAlgorithmFunction } from "./selectChecksumAlgorithmFunction";
+import { stringHasher } from "./stringHasher";
 import { validateChecksumFromResponse } from "./validateChecksumFromResponse";
 
 export const flexibleChecksumsMiddleware =
@@ -59,10 +60,10 @@ export const flexibleChecksumsMiddleware =
         };
         delete updatedHeaders["content-length"];
       } else if (!hasHeader(checksumLocationName, headers)) {
-        const checksum = await getChecksum(requestBody, { streamHasher, checksumAlgorithmFn, base64Encoder });
+        const rawChecksum = await stringHasher(checksumAlgorithmFn, requestBody);
         updatedHeaders = {
           ...headers,
-          [checksumLocationName]: checksum,
+          [checksumLocationName]: base64Encoder(rawChecksum),
         };
       }
     }
