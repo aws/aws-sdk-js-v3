c310ca8f5e5864906f9e0a2bb87904c647666e52
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-28 18:21:56 +0000

    fix(hash-stream-node): support copying fs.ReadStream from file descriptor

--- packages/hash-stream-node/src/fsCreateReadStream.ts
+++ packages/hash-stream-node/src/fsCreateReadStream.ts
@@ -1,9 +1,10 @@
 import { createReadStream, ReadStream } from "fs";
 
 export const fsCreateReadStream = (readStream: ReadStream) =>
-  createReadStream(readStream.path, {
-    // ToDo: Removed to fix https://github.com/aws/aws-sdk-js-v3/issues/3368
-    // fd: (readStream as any).fd,
-    start: (readStream as any).start,
-    end: (readStream as any).end,
-  });
+  typeof readStream.path !== "undefined"
+    ? createReadStream(readStream.path, {
+        start: (readStream as any).start,
+        end: (readStream as any).end,
+      })
+    : // if there is an fd option, filename is ignored by createReadStream
+      createReadStream("", { fd: (readStream as any).fd });
