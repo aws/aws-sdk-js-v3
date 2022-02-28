1baea2cf3bcb226699c2659431760ed97bc97ac3
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-28 20:05:58 +0000

    test: mock file object from MDN example

--- packages/util-body-length-browser/src/calculateBodyLength.spec.ts
+++ packages/util-body-length-browser/src/calculateBodyLength.spec.ts
@@ -19,4 +19,18 @@ describe(calculateBodyLength.name, () => {
   it("should handle TypedArray inputs", () => {
     expect(calculateBodyLength(typedArray)).toEqual(1);
   });
+
+  it("should handle File object", () => {
+    // Mock File Object https://developer.mozilla.org/en-US/docs/Web/API/File/File#example
+    const lastModifiedDate = new Date();
+    const mockFileObject = {
+      lastModified: lastModifiedDate.getTime(),
+      lastModifiedDate,
+      name: "foo.txt",
+      size: 3,
+      type: "text/plain",
+      webkitRelativePath: "",
+    };
+    expect(calculateBodyLength(mockFileObject)).toEqual(mockFileObject.size);
+  });
 });
