dd42eba9f530a550481615fce884bde9ffd1839a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-28 19:23:47 +0000

    chore: move calculateBodyLength to it's own file

--- packages/util-body-length-node/src/calculateBodyLength.spec.ts
+++ packages/util-body-length-node/src/calculateBodyLength.spec.ts
@@ -1,21 +1,12 @@
-import { createReadStream, lstatSync } from "fs";
-
 import { calculateBodyLength } from "./calculateBodyLength";
 
+const arrayBuffer = new ArrayBuffer(1);
+const typedArray = new Uint8Array(1);
+const view = new DataView(arrayBuffer);
+
 describe(calculateBodyLength.name, () => {
-  const arrayBuffer = new ArrayBuffer(1);
-  const typedArray = new Uint8Array(1);
-  const view = new DataView(arrayBuffer);
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it.each([
-    [0, null],
-    [0, undefined],
-  ])("should return %s for %s", (output, input) => {
-    expect(calculateBodyLength(input)).toEqual(output);
+  it("should handle null/undefined objects", () => {
+    expect(calculateBodyLength(null)).toEqual(0);
   });
 
   it("should handle string inputs", () => {
@@ -36,11 +27,5 @@ describe(calculateBodyLength.name, () => {
 
   it("should handle DataView inputs", () => {
     expect(calculateBodyLength(view)).toEqual(1);
-  });
-
-  it("should handle stream created using fs.createReadStream", () => {
-    const fileSize = lstatSync(__filename).size;
-    const fsReadStream = createReadStream(__filename);
-    expect(calculateBodyLength(fsReadStream)).toEqual(fileSize);
   });
 });
