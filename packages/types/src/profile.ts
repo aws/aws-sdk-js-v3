9415d69f1ffc0a8d558d12fbb64846b5cdcea748
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-11 23:00:54 +0000

    chore(types): mark profile types as internal

--- packages/types/src/profile.ts
+++ packages/types/src/profile.ts
@@ -1,11 +1,20 @@
+/**
+ * @internal
+ */
 export interface Profile {
   [key: string]: string | undefined;
 }
 
+/**
+ * @internal
+ */
 export interface ParsedIniData {
   [key: string]: Profile;
 }
 
+/**
+ * @internal
+ */
 export interface SharedConfigFiles {
   credentialsFile: ParsedIniData;
   configFile: ParsedIniData;
