914009c5472ac184aa3567a7a1d758ba5cf7dda5
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-11 22:40:35 +0000

    chore(shared-ini-file-loader): re-export profile types from types

---
+++ packages/shared-ini-file-loader/src/types.ts
@@ -0,0 +1,20 @@
+import {
+  ParsedIniData as __ParsedIniData,
+  Profile as __Profile,
+  SharedConfigFiles as __SharedConfigFiles,
+} from "@aws-sdk/types";
+
+/**
+ * @deprecated Use Profile from "@aws-sdk/types" instead
+ */
+export type Profile = __Profile;
+
+/**
+ * @deprecated Use ParsedIniData from "@aws-sdk/types" instead
+ */
+export type ParsedIniData = __ParsedIniData;
+
+/**
+ * @deprecated Use SharedConfigFiles from "@aws-sdk/types" instead
+ */
+export type SharedConfigFiles = __SharedConfigFiles;
