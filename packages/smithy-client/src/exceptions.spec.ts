af437ac36410764119ef4027802614e0340d96ed
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2022-03-15 15:54:32 +0100

    chore(smithy-client): add unit tests

--- packages/smithy-client/src/exceptions.spec.ts
+++ packages/smithy-client/src/exceptions.spec.ts
@@ -1,4 +1,4 @@
-import { decorateServiceException, ServiceException } from "./exceptions";
+import { decorateServiceException, ExceptionOptionType, ServiceException } from "./exceptions";
 
 it("ServiceException extends from Error", () => {
   expect(
@@ -9,6 +9,23 @@ it("ServiceException extends from Error", () => {
       $metadata: {},
     })
   ).toBeInstanceOf(Error);
+});
+
+it("ExceptionOptionType allows specifying message", () => {
+  class SomeException extends ServiceException {
+    constructor(opts: ExceptionOptionType<SomeException, ServiceException>) {
+      super({
+        name: "SomeException",
+        $fault: "client",
+        ...opts,
+      });
+    }
+  }
+  const exception = new SomeException({
+    message: "message",
+    $metadata: {},
+  });
+  expect(exception.message).toBe("message");
 });
 
 describe("decorateServiceException", () => {
