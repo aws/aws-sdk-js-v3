88f698fd00ca51f7a5442f022739472f48e9159d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-17 17:42:54 +0000

    test(shared-ini-file-loader): use await import in slurpFile.spec.ts

--- packages/shared-ini-file-loader/src/slurpFile.spec.ts
+++ packages/shared-ini-file-loader/src/slurpFile.spec.ts
@@ -1,40 +1,43 @@
+// ToDo: Change to "fs/promises" when supporting nodejs>=14
+import { promises } from "fs";
+
+jest.mock("fs", () => ({ promises: { readFile: jest.fn() } }));
+
 describe("slurpFile", () => {
   let slurpFile;
-  let mockReadFile;
 
   const UTF8 = "utf8";
   const getMockFileContents = (path: string, options = UTF8) => JSON.stringify({ path, options });
 
-  beforeEach(() => {
-    mockReadFile = jest.fn().mockImplementation((path, options) => Promise.resolve(getMockFileContents(path, options)));
-    jest.mock("fs", () => ({ promises: { readFile: mockReadFile } }));
+  beforeEach(async () => {
+    (promises.readFile as jest.Mock).mockImplementation((path, options) =>
+      Promise.resolve(getMockFileContents(path, options))
+    );
 
-    const slurpFileRequire = require("./slurpFile");
-    slurpFile = slurpFileRequire.slurpFile;
+    slurpFile = ((await import("./slurpFile")) as any).slurpFile;
   });
 
   afterEach(() => {
     jest.clearAllMocks();
-    jest.resetModules();
   });
 
   it("makes one readFile call for a filepath irrepsective of slurpFile calls", async () => {
     const mockPath = "/mock/path";
     const mockPathContent = getMockFileContents(mockPath);
 
-    expect(mockReadFile).not.toHaveBeenCalled();
+    expect(promises.readFile).not.toHaveBeenCalled();
     const fileContentArr = await Promise.all([slurpFile(mockPath), slurpFile(mockPath)]);
     expect(fileContentArr).toStrictEqual([mockPathContent, mockPathContent]);
 
     // There is one readFile call even through slurpFile is called in parallel twice.
-    expect(mockReadFile).toHaveBeenCalledTimes(1);
-    expect(mockReadFile).toHaveBeenCalledWith(mockPath, UTF8);
+    expect(promises.readFile).toHaveBeenCalledTimes(1);
+    expect(promises.readFile).toHaveBeenCalledWith(mockPath, UTF8);
 
     const fileContent = await slurpFile(mockPath);
     expect(fileContent).toStrictEqual(mockPathContent);
 
     // There is one readFile call even through slurpFile is called for the third time.
-    expect(mockReadFile).toHaveBeenCalledTimes(1);
+    expect(promises.readFile).toHaveBeenCalledTimes(1);
   });
 
   it("makes multiple readFile calls with based on filepaths", async () => {
@@ -44,14 +47,14 @@ describe("slurpFile", () => {
     const mockPath2 = "/mock/path/2";
     const mockPathContent2 = getMockFileContents(mockPath2);
 
-    expect(mockReadFile).not.toHaveBeenCalled();
+    expect(promises.readFile).not.toHaveBeenCalled();
     const fileContentArr = await Promise.all([slurpFile(mockPath1), slurpFile(mockPath2)]);
     expect(fileContentArr).toStrictEqual([mockPathContent1, mockPathContent2]);
 
     // There are two readFile calls as slurpFile is called in parallel with different filepaths.
-    expect(mockReadFile).toHaveBeenCalledTimes(2);
-    expect(mockReadFile).toHaveBeenNthCalledWith(1, mockPath1, UTF8);
-    expect(mockReadFile).toHaveBeenNthCalledWith(2, mockPath2, UTF8);
+    expect(promises.readFile).toHaveBeenCalledTimes(2);
+    expect(promises.readFile).toHaveBeenNthCalledWith(1, mockPath1, UTF8);
+    expect(promises.readFile).toHaveBeenNthCalledWith(2, mockPath2, UTF8);
 
     const fileContent1 = await slurpFile(mockPath1);
     expect(fileContent1).toStrictEqual(mockPathContent1);
@@ -59,6 +62,6 @@ describe("slurpFile", () => {
     expect(fileContent2).toStrictEqual(mockPathContent2);
 
     // There is one readFile call even through slurpFile is called for the third time.
-    expect(mockReadFile).toHaveBeenCalledTimes(2);
+    expect(promises.readFile).toHaveBeenCalledTimes(2);
   });
 });
