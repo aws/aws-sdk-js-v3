12f46290893c35db9fe7fdc862bfcdc05c22a1c1
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-21 19:38:26 +0000

    fix: use single function normalizeProvider

--- packages/util-middleware/src/index.ts
+++ packages/util-middleware/src/index.ts
@@ -1,1 +1,1 @@
-export * from "./normalizeBoolean";
+export * from "./normalizeProvider";
--- packages/util-middleware/src/normalizeBoolean.ts
+++
@@ -1,9 +0,0 @@
-import { Provider } from "@aws-sdk/types";
-
-export const normalizeBoolean = (value: boolean | Provider<boolean>): Provider<boolean> => {
-  if (typeof value === "boolean") {
-    const promisified = Promise.resolve(value);
-    return () => promisified;
-  }
-  return value as Provider<boolean>;
-};
