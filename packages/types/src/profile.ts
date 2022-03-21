3ee47499d084e390799dd2440b0b088d43647349
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-21 15:25:40 +0000

    fix(types): remove @internal jsdoc tag from profile types

--- packages/types/src/profile.ts
+++ packages/types/src/profile.ts
@@ -1,20 +1,11 @@
-/**
- * @internal
- */
 export interface Profile {
   [key: string]: string | undefined;
 }
 
-/**
- * @internal
- */
 export interface ParsedIniData {
   [key: string]: Profile;
 }
 
-/**
- * @internal
- */
 export interface SharedConfigFiles {
   credentialsFile: ParsedIniData;
   configFile: ParsedIniData;
