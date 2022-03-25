9bdbda80ecc204c8f008a8d8dcad4fc6ad34e56d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-25 01:21:20 +0000

    chore(util-credentials): delete util-credentials

--- packages/util-credentials/src/index.ts
+++
@@ -1,4 +0,0 @@
-// ToDo: Delete and deprecate util-credentials package after all dependents are moved
-// to use "@aws-sdk/shared-inin-file-loader" and a release is done.
-export * from "./get-master-profile-name";
-export * from "./parse-known-profiles";
