ddca6cfb2ce40b1df615ed459e2795cbe168efac
awstools <awstools@amazon.com>
2022-03-31 18:23:42 +0000

    docs(client-auditmanager): This release adds documentation updates for Audit Manager. The updates provide data deletion guidance when a customer deregisters Audit Manager or deregisters a delegated administrator.

--- clients/client-auditmanager/src/commands/DeregisterAccountCommand.ts
+++ clients/client-auditmanager/src/commands/DeregisterAccountCommand.ts
@@ -23,6 +23,45 @@ export interface DeregisterAccountCommandOutput extends DeregisterAccountRespons
 
 /**
  * <p> Deregisters an account in Audit Manager. </p>
+ *          <note>
+ *             <p>When you deregister your account from Audit Manager, your data isn’t deleted. If you want
+ *             to delete your resource data, you must perform that task separately before you
+ *             deregister your account. Either, you can do this in the Audit Manager console. Or,
+ *             you can use one of the delete API operations that are provided by Audit Manager. </p>
+ *             <p>To delete your Audit Manager resource data, see the following instructions: </p>
+ *             <ul>
+ *                <li>
+ *                   <p>
+ *                      <a href="https://docs.aws.amazon.com/audit-manager/latest/APIReference/API_DeleteAssessment.html">DeleteAssessment</a> (see also: <a href="https://docs.aws.amazon.com/audit-manager/latest/userguide/delete-assessment.html">Deleting an
+ *                      assessment</a> in the <i>Audit Manager User
+ *                   Guide</i>)</p>
+ *                </li>
+ *                <li>
+ *                   <p>
+ *                      <a href="https://docs.aws.amazon.com/audit-manager/latest/APIReference/API_DeleteAssessmentFramework.html">DeleteAssessmentFramework</a> (see also: <a href="https://docs.aws.amazon.com/audit-manager/latest/userguide/delete-custom-framework.html">Deleting a
+ *                      custom framework</a> in the <i>Audit Manager User
+ *                      Guide</i>)</p>
+ *                </li>
+ *                <li>
+ *                   <p>
+ *                      <a href="https://docs.aws.amazon.com/audit-manager/latest/APIReference/API_DeleteAssessmentFrameworkShare.html">DeleteAssessmentFrameworkShare</a> (see also: <a href="https://docs.aws.amazon.com/audit-manager/latest/userguide/deleting-shared-framework-requests.html">Deleting a share request</a> in the <i>Audit Manager User
+ *                      Guide</i>)</p>
+ *                </li>
+ *                <li>
+ *                   <p>
+ *                      <a href="https://docs.aws.amazon.com/audit-manager/latest/APIReference/API_DeleteAssessmentReport.html">DeleteAssessmentReport</a> (see also: <a href="https://docs.aws.amazon.com/audit-manager/latest/userguide/generate-assessment-report.html#delete-assessment-report-steps">Deleting an assessment report</a> in the <i>Audit Manager
+ *                      User Guide</i>)</p>
+ *                </li>
+ *                <li>
+ *                   <p>
+ *                      <a href="https://docs.aws.amazon.com/audit-manager/latest/APIReference/API_DeleteControl.html">DeleteControl</a> (see also: <a href="https://docs.aws.amazon.com/audit-manager/latest/userguide/delete-controls.html">Deleting a custom
+ *                      control</a> in the <i>Audit Manager User
+ *                   Guide</i>)</p>
+ *                </li>
+ *             </ul>
+ *             <p>At this time, Audit Manager doesn't provide an option to delete evidence. All
+ *             available delete operations are listed above.</p>
+ *          </note>
  * @example
  * Use a bare-bones client and the command you need to make an API call.
  * ```javascript
