e31244365323cc29715fe1e3bd028b2ebe371182
AllanZhengYP <zheallan@amazon.com>
2022-04-12 18:30:00 +0000

    test(version): update version test

--- tests/versions/index.spec.ts
+++ tests/versions/index.spec.ts
@@ -1,7 +1,7 @@
 /**
  * Validate the tslib and tsc version is consistent and corresponds to each other.
  */
-import { execSync } from "child_process";
+import { spawnSync } from "child_process";
 import { readFileSync } from "fs";
 import JSON from "json5";
 import { join } from "path";
@@ -17,7 +17,12 @@ test("tslib and tsc version should be consistent within workspace.", () => {
   const expectedVersions: { [name: string]: string } = JSON.parse(
     readFileSync(join(__dirname, "versions.jsonc"), "utf8")
   );
-  const packagesInfo: PackageInfo[] = JSON.parse(execSync("./node_modules/.bin/lerna list -l --json").toString());
+  const listResp = spawnSync("yarn", ["exec", '"lerna list -l --json"'], {
+    encoding: "utf8",
+    cwd: join(__dirname, "..", ".."),
+    shell: true,
+  }).stdout.toString();
+  const packagesInfo: PackageInfo[] = JSON.parse(listResp);
   for (const { name, location } of packagesInfo) {
     const manifest = readFileSync(`${location}/package.json`, "utf8");
     const { dependencies, devDependencies } = JSON.parse(manifest);
