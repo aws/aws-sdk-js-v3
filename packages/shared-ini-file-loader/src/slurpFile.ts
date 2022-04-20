7b8954d05a19014231b818dce3dee6d067ad68c5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-04-20 12:10:12 -0700

    chore(shared-ini-file-loader): use Promises in slurpFile hash (#3546)

--- packages/shared-ini-file-loader/src/slurpFile.ts
+++ packages/shared-ini-file-loader/src/slurpFile.ts
@@ -3,45 +3,11 @@ import { promises as fsPromises } from "fs";
 
 const { readFile } = fsPromises;
 
-type callbacks = { resolve: Function; reject: Function };
-type FileStatus = {
-  contents: string;
-  isReading: boolean;
-  requestQueue: callbacks[];
+const filePromisesHash: { [key: string]: Promise<string> } = {};
+
+export const slurpFile = (path: string) => {
+  if (!filePromisesHash[path]) {
+    filePromisesHash[path] = readFile(path, "utf8");
+  }
+  return filePromisesHash[path];
 };
-
-const fileStatusHash: { [key: string]: FileStatus } = {};
-
-export const slurpFile = (path: string) =>
-  new Promise<string>((resolve, reject) => {
-    if (!fileStatusHash[path]) {
-      // File not read yet, set file isReading to true and read file.
-      fileStatusHash[path] = { isReading: true, contents: "", requestQueue: [] };
-      fileStatusHash[path].requestQueue.push({ resolve, reject });
-      readFile(path, "utf8")
-        .then((data) => {
-          // File read successful
-          fileStatusHash[path].isReading = false;
-          fileStatusHash[path].contents = data;
-          const { requestQueue } = fileStatusHash[path];
-          while (requestQueue.length) {
-            const { resolve } = requestQueue.pop()!;
-            resolve(data);
-          }
-        })
-        .catch((err) => {
-          // File read failed;
-          fileStatusHash[path].isReading = false;
-          const { requestQueue } = fileStatusHash[path];
-          while (requestQueue.length) {
-            const { reject } = requestQueue.pop()!;
-            reject(err);
-          }
-        });
-    } else if (fileStatusHash[path].isReading) {
-      // File currently being read. Add callbacks to the request queue.
-      fileStatusHash[path].requestQueue.push({ resolve, reject });
-    } else {
-      resolve(fileStatusHash[path].contents);
-    }
-  });
