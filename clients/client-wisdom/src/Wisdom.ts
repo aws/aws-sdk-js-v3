6f1e5c3419678763f13a68bc71ec722a399ae2d9
awstools <awstools@amazon.com>
2022-04-21 18:17:54 +0000

    feat(client-wisdom): This release updates the GetRecommendations API to include a trigger event list for classifying and grouping recommendations.

--- clients/client-wisdom/src/Wisdom.ts
+++ clients/client-wisdom/src/Wisdom.ts
@@ -146,11 +146,9 @@ import {
 import { WisdomClient } from "./WisdomClient";
 
 /**
- * <p>All Amazon Connect Wisdom functionality is accessible using the API. For example, you can create an
- *       assistant and a knowledge base.</p>
- *
- *          <p>Some more advanced features are only accessible using the Wisdom API. For example, you
- *       can manually manage content by uploading custom files and control their lifecycle. </p>
+ * <p>Amazon Connect Wisdom delivers agents the information they need to solve customer issues as they're actively
+ *         speaking with customers. Agents can search across connected repositories from within their agent desktop
+ *         to find answers quickly. Use the Amazon Connect Wisdom APIs to create an assistant and a knowledge base, for example, or manage content by uploading custom files.</p>
  */
 export class Wisdom extends WisdomClient {
   /**
