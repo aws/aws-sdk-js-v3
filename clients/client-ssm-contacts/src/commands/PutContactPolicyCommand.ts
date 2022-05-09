eb5ccc69c7744cb9e02ee0cba98d500fe6b4a4fb
awstools <awstools@amazon.com>
2022-05-09 18:17:51 +0000

    docs(client-ssm-contacts): Fixed an error in the DescribeEngagement example for AWS Incident Manager.

--- clients/client-ssm-contacts/src/commands/PutContactPolicyCommand.ts
+++ clients/client-ssm-contacts/src/commands/PutContactPolicyCommand.ts
@@ -23,7 +23,9 @@ export interface PutContactPolicyCommandInput extends PutContactPolicyRequest {}
 export interface PutContactPolicyCommandOutput extends PutContactPolicyResult, __MetadataBearer {}
 
 /**
- * <p>Adds a resource to the specified contact or escalation plan.</p>
+ * <p>Adds a resource policy to the specified contact or escalation plan. The resource policy
+ *          is used to share the contact or escalation plan using Resource Access Manager (RAM). For more information about cross-account sharing, see <a href="https://docs.aws.amazon.com/incident-manager/latest/userguide/xa.html">Setting up
+ *             cross-account functionality</a>.</p>
  * @example
  * Use a bare-bones client and the command you need to make an API call.
  * ```javascript
