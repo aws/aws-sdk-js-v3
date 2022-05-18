0f427f3a187d49c10e82272a8905056a3857f646
awstools <awstools@amazon.com>
2022-05-18 18:16:54 +0000

    feat(client-app-mesh): This release updates the existing Create and Update APIs for meshes and virtual nodes by adding a new IP preference field. This new IP preference field can be used to control the IP versions being used with the mesh and allows for IPv6 support within App Mesh.

--- clients/client-app-mesh/src/AppMeshClient.ts
+++ clients/client-app-mesh/src/AppMeshClient.ts
@@ -377,15 +377,17 @@ type AppMeshClientResolvedConfigType = __SmithyResolvedConfiguration<__HttpHandl
 export interface AppMeshClientResolvedConfig extends AppMeshClientResolvedConfigType {}
 
 /**
- * <p>App Mesh is a service mesh based on the Envoy proxy that makes it easy to monitor and
- *          control microservices. App Mesh standardizes how your microservices communicate, giving you
- *          end-to-end visibility and helping to ensure high availability for your applications.</p>
- *          <p>App Mesh gives you consistent visibility and network traffic controls for every
- *          microservice in an application. You can use App Mesh with Amazon Web Services Fargate, Amazon ECS, Amazon EKS,
- *          Kubernetes on Amazon Web Services, and Amazon EC2.</p>
+ * <p>App Mesh is a service mesh based on the Envoy proxy that makes it easy to
+ *          monitor and control microservices. App Mesh standardizes how your microservices
+ *          communicate, giving you end-to-end visibility and helping to ensure high availability for
+ *          your applications.</p>
+ *          <p>App Mesh gives you consistent visibility and network traffic controls for
+ *          every microservice in an application. You can use App Mesh with Amazon Web Services Fargate, Amazon ECS, Amazon EKS, Kubernetes on Amazon Web Services, and
+ *             Amazon EC2.</p>
  *          <note>
- *             <p>App Mesh supports microservice applications that use service discovery naming for their
- *             components. For more information about service discovery on Amazon ECS, see <a href="https://docs.aws.amazon.com/AmazonECS/latest/developerguide/service-discovery.html">Service Discovery</a> in the <i>Amazon Elastic Container Service Developer Guide</i>. Kubernetes
+ *             <p>App Mesh supports microservice applications that use service discovery
+ *             naming for their components. For more information about service discovery on Amazon ECS, see <a href="https://docs.aws.amazon.com/AmazonECS/latest/developerguide/service-discovery.html">Service
+ *                Discovery</a> in the <i>Amazon Elastic Container Service Developer Guide</i>. Kubernetes
  *                <code>kube-dns</code> and <code>coredns</code> are supported. For more information,
  *             see <a href="https://kubernetes.io/docs/concepts/services-networking/dns-pod-service/">DNS
  *                for Services and Pods</a> in the Kubernetes documentation.</p>
--- clients/client-app-mesh/src/commands/CreateVirtualGatewayCommand.ts
+++ clients/client-app-mesh/src/commands/CreateVirtualGatewayCommand.ts
@@ -25,10 +25,9 @@ export interface CreateVirtualGatewayCommandOutput extends CreateVirtualGatewayO
 /**
  * <p>Creates a virtual gateway.</p>
  *          <p>A virtual gateway allows resources outside your mesh to communicate to resources that
- *          are inside your mesh. The virtual gateway represents an Envoy proxy running in an Amazon ECS
- *          task, in a Kubernetes service, or on an Amazon EC2 instance. Unlike a virtual node, which
- *          represents an Envoy running with an application, a virtual gateway represents Envoy
- *          deployed by itself.</p>
+ *          are inside your mesh. The virtual gateway represents an Envoy proxy running in an Amazon ECS task, in a Kubernetes service, or on an Amazon EC2 instance. Unlike a
+ *          virtual node, which represents an Envoy running with an application, a virtual gateway
+ *          represents Envoy deployed by itself.</p>
  *          <p>For more information about virtual gateways, see <a href="https://docs.aws.amazon.com/app-mesh/latest/userguide/virtual_gateways.html">Virtual gateways</a>. </p>
  * @example
  * Use a bare-bones client and the command you need to make an API call.
