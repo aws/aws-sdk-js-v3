6b084b9e2a7d1da2ac01b5edfd62f6d6b3385542
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-05-25 10:47:07 -0700

    chore(smithy-client): emit unsupported version warning for Node.js <14.x (#3636)

--- packages/smithy-client/src/emitWarningIfUnsupportedVersion.ts
+++ packages/smithy-client/src/emitWarningIfUnsupportedVersion.ts
@@ -7,14 +7,14 @@ let warningEmitted = false;
  * @param {string} version - The Node.js version string.
  */
 export const emitWarningIfUnsupportedVersion = (version: string) => {
-  if (version && !warningEmitted && parseInt(version.substring(1, version.indexOf("."))) < 12) {
+  if (version && !warningEmitted && parseInt(version.substring(1, version.indexOf("."))) < 14) {
     warningEmitted = true;
     process.emitWarning(
       `The AWS SDK for JavaScript (v3) will\n` +
-        `no longer support Node.js ${version} as of January 1, 2022.\n` +
+        `no longer support Node.js ${version} on November 1, 2022.\n\n` +
         `To continue receiving updates to AWS services, bug fixes, and security\n` +
-        `updates please upgrade to Node.js 12.x or later.\n\n` +
-        `More information can be found at: https://a.co/1l6FLnu`,
+        `updates please upgrade to Node.js 14.x or later.\n\n` +
+        `For details, please refer our blog post: https://a.co/48dbdYz`,
       `NodeDeprecationWarning`
     );
   }
