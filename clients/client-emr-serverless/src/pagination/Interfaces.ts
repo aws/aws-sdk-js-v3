67689a89e98c93af243622f8abc955e3486caa19
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-05-27 02:07:14 +0000

    chore(client-emr-serverless): delete client

--- clients/client-emr-serverless/src/pagination/Interfaces.ts
+++
@@ -1,9 +0,0 @@
-// smithy-typescript generated code
-import { PaginationConfiguration } from "@aws-sdk/types";
-
-import { EMRServerless } from "../EMRServerless";
-import { EMRServerlessClient } from "../EMRServerlessClient";
-
-export interface EMRServerlessPaginationConfiguration extends PaginationConfiguration {
-  client: EMRServerless | EMRServerlessClient;
-}
