980d924989a18e84e8f7b9885fad13518727c11a
AllanZhengYP <zheallan@amazon.com>
2022-06-03 18:54:32 +0000

    fix(middleware-recursion-detection): address feedbacks

--- packages/middleware-recursion-detection/src/index.ts
+++ packages/middleware-recursion-detection/src/index.ts
@@ -22,30 +22,30 @@ interface PreviouslyResolved {
  * Inject to trace ID to request header to detect recursion invocation in Lambda.
  * @internal
  */
-export function recursionDetectionMiddleware(options: PreviouslyResolved): BuildMiddleware<any, any> {
-  return <Output extends MetadataBearer>(next: BuildHandler<any, Output>): BuildHandler<any, Output> =>
-    async (args: BuildHandlerArguments<any>): Promise<BuildHandlerOutput<Output>> => {
-      const { request } = args;
-      if (!HttpRequest.isInstance(request) || options.runtime !== "node") {
-        return next(args);
-      }
+export const recursionDetectionMiddleware =
+  (options: PreviouslyResolved): BuildMiddleware<any, any> =>
+  <Output extends MetadataBearer>(next: BuildHandler<any, Output>): BuildHandler<any, Output> =>
+  async (args: BuildHandlerArguments<any>): Promise<BuildHandlerOutput<Output>> => {
+    const { request } = args;
+    if (
+      !HttpRequest.isInstance(request) ||
+      options.runtime !== "node" ||
+      request.headers.hasOwnProperty(TRACE_ID_HEADER_NAME)
+    ) {
+      return next(args);
+    }
 
-      const functionName = process.env[ENV_LAMBDA_FUNCTION_NAME];
-      const traceId = process.env[ENV_TRACE_ID];
-      const nonEmptyString = (str: unknown): str is string => typeof str === "string" && str.length > 0;
-      if (
-        !request.headers.hasOwnProperty(TRACE_ID_HEADER_NAME) &&
-        nonEmptyString(functionName) &&
-        nonEmptyString(traceId)
-      ) {
-        request.headers[TRACE_ID_HEADER_NAME] = traceId;
-      }
-      return next({
-        ...args,
-        request,
-      });
-    };
-}
+    const functionName = process.env[ENV_LAMBDA_FUNCTION_NAME];
+    const traceId = process.env[ENV_TRACE_ID];
+    const nonEmptyString = (str: unknown): str is string => typeof str === "string" && str.length > 0;
+    if (nonEmptyString(functionName) && nonEmptyString(traceId)) {
+      request.headers[TRACE_ID_HEADER_NAME] = traceId;
+    }
+    return next({
+      ...args,
+      request,
+    });
+  };
 
 // @internal
 export const addRecursionDetectionMiddlewareOptions: BuildHandlerOptions & AbsoluteLocation = {
