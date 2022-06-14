95361044a5f2291389bd2886fb5dffe238ae6cdb
George Fu <kuhe@users.noreply.github.com>
2022-06-14 11:45:56 -0400

    fix(shared-ini-file-loader): update ini parsing (#3682)
    * fix(shared-ini-file-loader): update ini parsing
    
    * fix(shared-ini-file-loader): use indexOf instead of split
    
    * fix(shared-ini-file-loader): require non-empty for ini parse

--- packages/shared-ini-file-loader/src/parseIni.ts
+++ packages/shared-ini-file-loader/src/parseIni.ts
@@ -7,18 +7,26 @@ export const parseIni = (iniData: string): ParsedIniData => {
   let currentSection: string | undefined;
 
   for (let line of iniData.split(/\r?\n/)) {
-    line = line.split(/(^|\s)[;#]/)[0]; // remove comments
-    const section = line.match(/^\s*\[([^\[\]]+)]\s*$/);
-    if (section) {
-      currentSection = section[1];
+    line = line.split(/(^|\s)[;#]/)[0].trim(); // remove comments and trim
+    const isSection: boolean = line[0] === "[" && line[line.length - 1] === "]";
+    if (isSection) {
+      currentSection = line.substring(1, line.length - 1);
       if (profileNameBlockList.includes(currentSection)) {
         throw new Error(`Found invalid profile name "${currentSection}"`);
       }
     } else if (currentSection) {
-      const item = line.match(/^\s*(.+?)\s*=\s*(.+?)\s*$/);
-      if (item) {
+      const indexOfEqualsSign = line.indexOf("=");
+      const start = 0;
+      const end: number = line.length - 1;
+      const isAssignment: boolean =
+        indexOfEqualsSign !== -1 && indexOfEqualsSign !== start && indexOfEqualsSign !== end;
+      if (isAssignment) {
+        const [name, value]: [string, string] = [
+          line.substring(0, indexOfEqualsSign).trim(),
+          line.substring(indexOfEqualsSign + 1).trim(),
+        ];
         map[currentSection] = map[currentSection] || {};
-        map[currentSection][item[1]] = item[2];
+        map[currentSection][name] = value;
       }
     }
   }
