0b69c9b48ad1a78a47d3f029b290c57141ed3844
AllanZhengYP <zheallan@amazon.com>
2022-06-23 15:58:15 -0700

    fix(client-redshiftserverless): remove redshift-serverless client (#3733)

--- clients/client-redshiftserverless/src/pagination/Interfaces.ts
+++
@@ -1,9 +0,0 @@
-// smithy-typescript generated code
-import { PaginationConfiguration } from "@aws-sdk/types";
-
-import { RedshiftServerless } from "../RedshiftServerless";
-import { RedshiftServerlessClient } from "../RedshiftServerlessClient";
-
-export interface RedshiftServerlessPaginationConfiguration extends PaginationConfiguration {
-  client: RedshiftServerless | RedshiftServerlessClient;
-}
