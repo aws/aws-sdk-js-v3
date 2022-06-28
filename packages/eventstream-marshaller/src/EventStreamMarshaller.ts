19fddb20a9b477c932dad564ae2c5b5a64c9014f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-06-28 16:22:29 -0400

    chore(eventstream-marshaller): deprecate EventStreamMarshaller (#3749)

--- packages/eventstream-marshaller/src/EventStreamMarshaller.ts
+++ packages/eventstream-marshaller/src/EventStreamMarshaller.ts
@@ -1,61 +1,29 @@
-import { Crc32 } from "@aws-crypto/crc32";
+import { EventStreamCodec } from "@aws-sdk/eventstream-codec";
 import { Message, MessageHeaders } from "@aws-sdk/types";
 import { Decoder, Encoder } from "@aws-sdk/types";
-
-import { HeaderMarshaller } from "./HeaderMarshaller";
-import { splitMessage } from "./splitMessage";
 
 /**
  * A marshaller that can convert binary-packed event stream messages into
  * JavaScript objects and back again into their binary format.
+ *
+ * @deprecated Use EventStreamCodec from @aws-sdk/eventstream-codec instead.
  */
 export class EventStreamMarshaller {
-  private readonly headerMarshaller: HeaderMarshaller;
+  private readonly codec: EventStreamCodec;
 
   constructor(toUtf8: Encoder, fromUtf8: Decoder) {
-    this.headerMarshaller = new HeaderMarshaller(toUtf8, fromUtf8);
+    this.codec = new EventStreamCodec(toUtf8, fromUtf8);
   }
 
-  /**
-   * Convert a structured JavaScript object with tagged headers into a binary
-   * event stream message.
-   */
-  marshall({ headers: rawHeaders, body }: Message): Uint8Array {
-    const headers = this.headerMarshaller.format(rawHeaders);
-    const length = headers.byteLength + body.byteLength + 16;
-
-    const out = new Uint8Array(length);
-    const view = new DataView(out.buffer, out.byteOffset, out.byteLength);
-    const checksum = new Crc32();
-
-    // Format message
-    view.setUint32(0, length, false);
-    view.setUint32(4, headers.byteLength, false);
-    view.setUint32(8, checksum.update(out.subarray(0, 8)).digest(), false);
-    out.set(headers, 12);
-    out.set(body, headers.byteLength + 12);
-
-    // Write trailing message checksum
-    view.setUint32(length - 4, checksum.update(out.subarray(8, length - 4)).digest(), false);
-
-    return out;
+  marshall(message: Message): Uint8Array {
+    return this.codec.encode(message);
   }
 
-  /**
-   * Convert a binary event stream message into a JavaScript object with an
-   * opaque, binary body and tagged, parsed headers.
-   */
   unmarshall(message: ArrayBufferView): Message {
-    const { headers, body } = splitMessage(message);
-
-    return { headers: this.headerMarshaller.parse(headers), body };
+    return this.codec.decode(message);
   }
 
-  /**
-   * Convert a structured JavaScript object with tagged headers into a binary
-   * event stream message header.
-   */
   formatHeaders(rawHeaders: MessageHeaders): Uint8Array {
-    return this.headerMarshaller.format(rawHeaders);
+    return this.codec.formatHeaders(rawHeaders);
   }
 }
