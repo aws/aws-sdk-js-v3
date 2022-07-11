c88ac5691529c8f455c7f3d21aa70280f243e595
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-07-11 15:10:57 +0000

    chore(types): add types for EventStreamMarshaller ser/deser functions

--- packages/types/src/eventStream.ts
+++ packages/types/src/eventStream.ts
@@ -87,9 +87,17 @@ export interface EventStreamSerdeContext {
   eventStreamMarshaller: EventStreamMarshaller;
 }
 
+export interface EventStreamMarshallerDeserFn {
+  <T>(body: any, deserializer: (input: Record<string, Message>) => Promise<T>): AsyncIterable<T>;
+}
+
+export interface EventStreamMarshallerSerFn {
+  <T>(input: AsyncIterable<T>, serializer: (event: T) => Message): any;
+}
+
 export interface EventStreamMarshaller {
-  deserialize: (body: any, deserializer: (input: Record<string, Message>) => any) => AsyncIterable<any>;
-  serialize: (input: AsyncIterable<any>, serializer: (event: any) => Message) => any;
+  deserialize: EventStreamMarshallerDeserFn;
+  serialize: EventStreamMarshallerSerFn;
 }
 
 export interface EventStreamRequestSigner {
