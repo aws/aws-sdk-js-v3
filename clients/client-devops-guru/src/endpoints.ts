145ed3aeffd2c08bed0c7f92e5cc17a253c14c40
awstools <awstools@amazon.com>
2022-07-15 18:25:44 +0000

    feat(clients): update client endpoints as of 2022-07-15

--- clients/client-devops-guru/src/endpoints.ts
+++ clients/client-devops-guru/src/endpoints.ts
@@ -2,7 +2,32 @@
 import { getRegionInfo, PartitionHash, RegionHash } from "@aws-sdk/config-resolver";
 import { RegionInfoProvider, RegionInfoProviderOptions } from "@aws-sdk/types";
 
-const regionHash: RegionHash = {};
+const regionHash: RegionHash = {
+  "us-east-1": {
+    variants: [
+      {
+        hostname: "devops-guru-fips.us-east-1.amazonaws.com",
+        tags: ["fips"],
+      },
+    ],
+  },
+  "us-east-2": {
+    variants: [
+      {
+        hostname: "devops-guru-fips.us-east-2.amazonaws.com",
+        tags: ["fips"],
+      },
+    ],
+  },
+  "us-west-2": {
+    variants: [
+      {
+        hostname: "devops-guru-fips.us-west-2.amazonaws.com",
+        tags: ["fips"],
+      },
+    ],
+  },
+};
 
 const partitionHash: PartitionHash = {
   aws: {
@@ -23,6 +48,9 @@ const partitionHash: PartitionHash = {
       "eu-west-1",
       "eu-west-2",
       "eu-west-3",
+      "fips-us-east-1",
+      "fips-us-east-2",
+      "fips-us-west-2",
       "me-south-1",
       "sa-east-1",
       "us-east-1",
