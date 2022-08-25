bfe5f0e75ca2378441143dd2d74c1ba357370501
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-08-25 01:15:32 +0000

    chore(util-endpoints): add util 'sbooleanEquals'

---
+++ packages/util-endpoints/src/lib/booleanEquals.ts
@@ -0,0 +1,5 @@
+/**
+ * Evaluates two boolean values value1 and value2 for equality and returns
+ * true if both values match.
+ */
+export const booleanEquals = (value1: boolean, value2: boolean): boolean => value1 === value2;
--- packages/util-endpoints/src/lib/index.ts
+++ packages/util-endpoints/src/lib/index.ts
@@ -1,3 +1,4 @@
+export * from "./booleanEquals";
 export * from "./getAttr";
 export * from "./isSet";
 export * from "./not";
