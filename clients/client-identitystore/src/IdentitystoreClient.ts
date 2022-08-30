724c8bc18d7829c3beaa28c9e6b97ea7e851b583
awstools <awstools@amazon.com>
2022-08-30 20:37:02 +0000

    docs(client-identitystore): Documentation updates for the Identity Store CLI Reference.

--- clients/client-identitystore/src/IdentitystoreClient.ts
+++ clients/client-identitystore/src/IdentitystoreClient.ts
@@ -225,14 +225,14 @@ type IdentitystoreClientResolvedConfigType = __SmithyResolvedConfiguration<__Htt
 export interface IdentitystoreClientResolvedConfig extends IdentitystoreClientResolvedConfigType {}
 
 /**
- * <p>The Identity Store service used by Amazon Web Services Single Sign On provides a single place to retrieve all of
- *          your identities (users and groups). For more information, see the <a href="https://docs.aws.amazon.com/singlesignon/latest/userguide/what-is.html">Amazon Web Services SSO User
+ * <p>The Identity Store service used by AWS IAM Identity Center (successor to AWS Single Sign-On) provides a single place to retrieve all of
+ *          your identities (users and groups). For more information, see the <a href="https://docs.aws.amazon.com/singlesignon/latest/userguide/what-is.html">IAM Identity Center User
  *             Guide</a>.</p>
  *
  *          <note>
- *             <p>Although Amazon Web Services Single Sign-On was renamed, the <code>sso</code> and
+ *             <p>Although AWS Single Sign-On was renamed, the <code>sso</code> and
  *             <code>identitystore</code> API namespaces will continue to retain their original name for
- *             backward compatibility purposes. For more information, see <a href="https://docs.aws.amazon.com/singlesignon/latest/userguide/what-is.html#renamed">Amazon Web Services SSO rename</a>.</p>
+ *             backward compatibility purposes. For more information, see <a href="https://docs.aws.amazon.com/singlesignon/latest/userguide/what-is.html#renamed">IAM Identity Center rename</a>.</p>
  *          </note>
  *
  *          <p>This reference guide describes the identity store operations that you can call
