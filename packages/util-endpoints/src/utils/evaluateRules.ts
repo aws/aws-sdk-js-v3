b965c38ec87ad3c6eaa0e14772613d5062ebdabb
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-09-07 20:48:41 +0000

    chore(util-endpoints): explicitly add return type for resolveEndpoint

--- packages/util-endpoints/src/utils/evaluateRules.ts
+++ packages/util-endpoints/src/utils/evaluateRules.ts
@@ -1,9 +1,11 @@
+import { EndpointV2 } from "@aws-sdk/types";
+
 import { EndpointError, EvaluateOptions, RuleSetRules } from "../types";
 import { evaluateEndpointRule } from "./evaluateEndpointRule";
 import { evaluateErrorRule } from "./evaluateErrorRule";
 import { evaluateTreeRule } from "./evaluateTreeRule";
 
-export const evaluateRules = (rules: RuleSetRules, options: EvaluateOptions) => {
+export const evaluateRules = (rules: RuleSetRules, options: EvaluateOptions): EndpointV2 => {
   for (const rule of rules) {
     if (rule.type === "endpoint") {
       const endpointOrUndefined = evaluateEndpointRule(rule, options);
