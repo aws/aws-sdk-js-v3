5bb234d64aed13b3227fdb7e1168f00298c7171e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-09-12 12:59:55 -0700

    chore(util-endpoints): return undefined when value absent in referenceRecord (#3930)

--- packages/util-endpoints/src/utils/getReferenceValue.ts
+++ packages/util-endpoints/src/utils/getReferenceValue.ts
@@ -1,12 +1,9 @@
-import { EndpointError, EvaluateOptions, ReferenceObject } from "../types";
+import { EvaluateOptions, ReferenceObject } from "../types";
 
 export const getReferenceValue = ({ ref }: ReferenceObject, options: EvaluateOptions) => {
   const referenceRecord = {
     ...options.endpointParams,
     ...options.referenceRecord,
   };
-  if (referenceRecord[ref] == undefined) {
-    throw new EndpointError(`Reference '${ref}' not defined`);
-  }
   return referenceRecord[ref];
 };
