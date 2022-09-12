5f7c1b7f1672c70dacf16b664545b1ad1778a2c4
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-09-12 18:49:01 +0000

    fix(util-endpoints): call multi-level functions from callFunction

--- packages/util-endpoints/src/utils/callFunction.ts
+++ packages/util-endpoints/src/utils/callFunction.ts
@@ -5,5 +5,5 @@ import { evaluateExpression } from "./evaluateExpression";
 export const callFunction = ({ fn, argv }: FunctionObject, options: EvaluateOptions): FunctionReturn => {
   const argvArray = argv.map((arg) => (typeof arg === "boolean" ? arg : evaluateExpression(arg, "arg", options)));
   // @ts-ignore Element implicitly has an 'any' type
-  return lib[fn](...argvArray);
+  return fn.split(".").reduce((acc, key) => acc[key], lib)(...argvArray);
 };
