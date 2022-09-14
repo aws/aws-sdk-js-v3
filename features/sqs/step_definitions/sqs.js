4d3c423d87222b3dcdd743de936025fa9037d799
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-09-14 16:42:25 +0000

    chore: remove callback from Before/After

--- features/sqs/step_definitions/sqs.js
+++ features/sqs/step_definitions/sqs.js
@@ -1,17 +1,13 @@
 const { Before, After } = require("@cucumber/cucumber");
 
-Before({ tags: "@sqs" }, function (scenario, callback) {
+Before({ tags: "@sqs" }, function () {
   const { SQS } = require("../../../clients/client-sqs");
-  this.service = new SQS({
-    region: "us-east-1",
-  });
+  this.service = new SQS({ region: "us-east-1" });
   this.createdQueues = [];
-  callback();
 });
 
-After({ tags: "@sqs" }, function (scenario, callback) {
-  this.createdQueues.forEach((url) => {
-    this.request(null, "deleteQueue", { QueueUrl: url });
-  });
-  callback();
+After({ tags: "@sqs" }, async function () {
+  for (const queueUrl of this.createdQueues) {
+    await this.service.deleteQueue({ QueueUrl: queueUrl });
+  }
 });
