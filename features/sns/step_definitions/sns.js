9195a8cac1795c2af33294d672c109aa9a3e2131
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-09-15 12:30:49 -0700

    fix(e2e): delete sns topic in After call (#3959)

--- features/sns/step_definitions/sns.js
+++ features/sns/step_definitions/sns.js
@@ -1,57 +1,38 @@
-const { Before, Given, Then } = require("@cucumber/cucumber");
+const { After, Before, Given, Then } = require("@cucumber/cucumber");
 
-Before({ tags: "@sns" }, function (scenario, callback) {
+Before({ tags: "@sns" }, function () {
   const { SNS } = require("../../../clients/client-sns");
   this.service = new SNS({});
-  callback();
 });
 
-Given("I create an SNS topic with name {string}", function (name, callback) {
-  const world = this;
-  this.request(
-    null,
-    "createTopic",
-    {
-      Name: name,
-    },
-    callback,
-    function (data) {
-      world.topicArn = data.TopicArn;
-    }
-  );
+After({ tags: "@sns" }, async function () {
+  if (this.topicArn) {
+    await this.service.deleteTopic({ TopicArn: this.topicArn });
+    this.topicArn = undefined;
+  }
 });
 
-Given("I list the SNS topics", function (callback) {
-  this.request(null, "listTopics", {}, callback);
+Given("I create an SNS topic with prefix {string}", async function (prefix) {
+  const topicName = this.uniqueName(prefix);
+  const { TopicArn } = await this.service.createTopic({ Name: topicName });
+  this.topicArn = TopicArn;
 });
 
-Then("the list should contain the topic ARN", function (callback) {
+Given("I list the SNS topics", async function () {
+  this.data = await this.service.listTopics({});
+});
+
+Then("the list should contain the topic ARN", function () {
   const arn = this.topicArn;
   this.assert.contains(this.data.Topics, function (topic) {
     return topic.TopicArn === arn;
   });
-  callback();
 });
 
-Then("I delete the SNS topic", function (callback) {
-  this.request(
-    null,
-    "deleteTopic",
-    {
-      TopicArn: this.topicArn,
-    },
-    callback
-  );
-});
-
-Given("I get SNS topic attributes with an invalid ARN", function (callback) {
-  this.request(
-    null,
-    "getTopicAttributes",
-    {
-      TopicArn: "INVALID",
-    },
-    callback,
-    false
-  );
+Given("I get SNS topic attributes with an invalid ARN", async function () {
+  try {
+    await this.service.getTopicAttributes({ TopicArn: "INVALID" });
+  } catch (error) {
+    this.error = error;
+  }
 });
