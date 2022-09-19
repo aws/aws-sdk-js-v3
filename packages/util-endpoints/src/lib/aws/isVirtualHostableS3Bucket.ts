d6cf7866e01787c1d05cda943a522bca781641ee
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-09-19 13:36:31 +0000

    chore(util-endpoints): add 3/4 checks for isVirtualHostableS3Bucket

--- packages/util-endpoints/src/lib/aws/isVirtualHostableS3Bucket.ts
+++ packages/util-endpoints/src/lib/aws/isVirtualHostableS3Bucket.ts
@@ -1,15 +1,8 @@
+import { isValidHostLabel } from "packages/util-endpoints/dist-types/lib";
+
 /**
  * Evaluates whether a string is a DNS compatible bucket name and can be used with
  * virtual hosted style addressing.
- *
- * In addition to the restrictions defined in RFC 1123 and isValidHostLabel,
- * the isVirtualHostableS3Bucket also validates that:
- * - the bucket name is between [3,63] characters
- * - does not contain upper case characters
- * - is not formatted as an IP address.
- *
- * If allowSubDomains is true, then the provided value may be zero or more dotted
- * values, which are each validated as per rules above.
  */
 export const isVirtualHostableS3Bucket = (value: string, allowSubDomains = false) => {
   if (allowSubDomains) {
@@ -20,4 +13,23 @@ export const isVirtualHostableS3Bucket = (value: string, allowSubDomains = false
     }
     return true;
   }
+
+  // Value must be a valid host label.
+  if (!isValidHostLabel(value)) {
+    return false;
+  }
+
+  // Value must be between 3 and 63 characters long.
+  if (value.length < 3 || value.length > 63) {
+    return false;
+  }
+
+  // Value must not contain upper case characters.
+  if (value !== value.toLowerCase()) {
+    return false;
+  }
+
+  // ToDo: Value must not be formatted as an IP address.
+
+  return true;
 };
