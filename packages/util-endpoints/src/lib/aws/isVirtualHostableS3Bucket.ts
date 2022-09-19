69ff42514092f53e34a082a515823a01649e20ff
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-09-19 13:25:48 +0000

    chore(util-endpoints): add skeleton for isVirtualHostableS3Bucket

---
+++ packages/util-endpoints/src/lib/aws/isVirtualHostableS3Bucket.ts
@@ -0,0 +1,14 @@
+/**
+ * Evaluates whether a string is a DNS compatible bucket name and can be used with
+ * virtual hosted style addressing.
+ *
+ * In addition to the restrictions defined in RFC 1123 and isValidHostLabel,
+ * the isVirtualHostableS3Bucket also validates that:
+ * - the bucket name is between [3,63] characters
+ * - does not contain upper case characters
+ * - is not formatted as an IP address.
+ *
+ * If allowSubDomains is true, then the provided value may be zero or more dotted
+ * values, which are each validated as per rules above.
+ */
+export const isVirtualHostableS3Bucket = (value: string, allowSubDomains = false) => {};
