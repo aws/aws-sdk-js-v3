4f39f834477b219b9a220522defae5b4dcfbdd4d
awstools <awstools@amazon.com>
2022-09-22 18:23:06 +0000

    feat(clients): update client endpoints as of 2022-09-22

--- clients/client-connectcampaigns/src/endpoints.ts
+++ clients/client-connectcampaigns/src/endpoints.ts
@@ -2,7 +2,24 @@
 import { getRegionInfo, PartitionHash, RegionHash } from "@aws-sdk/config-resolver";
 import { RegionInfoProvider, RegionInfoProviderOptions } from "@aws-sdk/types";
 
-const regionHash: RegionHash = {};
+const regionHash: RegionHash = {
+  "us-east-1": {
+    variants: [
+      {
+        hostname: "connect-campaigns-fips.us-east-1.amazonaws.com",
+        tags: ["fips"],
+      },
+    ],
+  },
+  "us-west-2": {
+    variants: [
+      {
+        hostname: "connect-campaigns-fips.us-west-2.amazonaws.com",
+        tags: ["fips"],
+      },
+    ],
+  },
+};
 
 const partitionHash: PartitionHash = {
   aws: {
@@ -23,6 +40,8 @@ const partitionHash: PartitionHash = {
       "eu-west-1",
       "eu-west-2",
       "eu-west-3",
+      "fips-us-east-1",
+      "fips-us-west-2",
       "me-central-1",
       "me-south-1",
       "sa-east-1",
