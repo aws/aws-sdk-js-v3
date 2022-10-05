ab0e7be36e7e7f8a0c04834357aaad643c7912c3
George Fu <kuhe@users.noreply.github.com>
2022-10-05 14:00:12 -0400

    fix(node-http-handler): check for error code in isTransientError (#4018)

--- packages/node-http-handler/src/constants.ts
+++ packages/node-http-handler/src/constants.ts
@@ -1,4 +1,5 @@
 /**
  * Node.js system error codes that indicate timeout.
+ * @deprecated use NODEJS_TIMEOUT_ERROR_CODES from @aws-sdk/service-error-classification/constants
  */
 export const NODEJS_TIMEOUT_ERROR_CODES = ["ECONNRESET", "EPIPE", "ETIMEDOUT"];
