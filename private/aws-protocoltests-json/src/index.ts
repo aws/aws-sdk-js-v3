3539db593c7fd0aa680b4475d21b351dcbaf18f8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-10-11 18:02:50 +0000

    chore(clients): add missing newline

--- private/aws-protocoltests-json/src/index.ts
+++ private/aws-protocoltests-json/src/index.ts
@@ -3,4 +3,5 @@ export * from "./JsonProtocol";
 export * from "./JsonProtocolClient";
 export * from "./commands";
 export * from "./models";
+
 export { JsonProtocolServiceException } from "./models/JsonProtocolServiceException";
