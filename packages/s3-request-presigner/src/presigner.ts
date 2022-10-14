893dfbffb2450e0ff26ac2cada9138162e9a631e
George Fu <georgfu@amazon.com>
2022-10-14 14:59:01 +0000

    chore(signers): use assignment instead of extension

--- packages/s3-request-presigner/src/presigner.ts
+++ packages/s3-request-presigner/src/presigner.ts
@@ -3,4 +3,4 @@ import { S3RequestPresigner as Moved } from "@aws-sdk/s3-signer";
 /**
  * @deprecated use S3RequestPresigner from @aws-sdk/s3-signer
  */
-export class S3RequestPresigner extends Moved {}
+export const S3RequestPresigner = Moved;
