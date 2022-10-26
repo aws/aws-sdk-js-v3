b8e62f939162973dd3d4dde50b59a53e4c0eeed9
Steven Yuan <yuasteve@amazon.com>
2022-10-26 14:25:19 +0000

    chore(codegen): upgrade to Java 17
    Upgrade to Java 17 for code generator and adjust CI workflows.

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -75,8 +75,8 @@ subprojects {
         apply(plugin = "java-library")
 
         java {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
 
         tasks.withType<JavaCompile> {
