95631eb7aa6f17b5051bedd31e7e4532ee9d2f8c
awstools <awstools@amazon.com>
2022-10-26 18:33:57 +0000

    feat(clients): update client endpoints as of 2022-10-26

--- clients/client-inspector2/src/commands/ListFiltersCommand.ts
+++ clients/client-inspector2/src/commands/ListFiltersCommand.ts
@@ -55,7 +55,7 @@ export class ListFiltersCommand extends $Command<
 
   public static getEndpointParameterInstructions(): EndpointParameterInstructions {
     return {
-      UseFIPS: { type: "builtInParams", name: "useFIPS" },
+      UseFIPS: { type: "builtInParams", name: "useFipsEndpoint" },
       Endpoint: { type: "builtInParams", name: "endpoint" },
       Region: { type: "builtInParams", name: "region" },
       UseDualStack: { type: "builtInParams", name: "useDualstackEndpoint" },
