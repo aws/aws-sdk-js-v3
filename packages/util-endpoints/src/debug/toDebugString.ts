db597c277e4aa4aa1886bcc08ef861719d196907
George Fu <georgfu@amazon.com>
2022-10-27 14:54:49 +0000

    chore(endpoint): add spacing to debug output

--- packages/util-endpoints/src/debug/toDebugString.ts
+++ packages/util-endpoints/src/debug/toDebugString.ts
@@ -10,16 +10,16 @@ export function toDebugString(input: FunctionObject): string;
 export function toDebugString(input: FunctionReturn): string;
 export function toDebugString(input: EndpointObject): string;
 export function toDebugString(input: any): string {
-  if (typeof input !== 'object' || input == null) {
+  if (typeof input !== "object" || input == null) {
     return input;
   }
 
-  if ('ref' in input) {
-    return `$${toDebugString(input.ref)}`
+  if ("ref" in input) {
+    return `$${toDebugString(input.ref)}`;
   }
 
-  if ('fn' in input) {
-    return `${input.fn}(${(input.argv || []).map(toDebugString)})`;
+  if ("fn" in input) {
+    return `${input.fn}(${(input.argv || []).map(toDebugString).join(", ")})`;
   }
 
   return JSON.stringify(input, null, 2);
