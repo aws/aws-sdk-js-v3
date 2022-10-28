d733a32b43fb79259a6c4567a60b3f75973a81cd
awstools <awstools@amazon.com>
2022-10-28 18:31:40 +0000

    docs(client-cloud9): Update to the documentation section of the Cloud9 API Reference guide.

--- clients/client-cloud9/src/models/models_0.ts
+++ clients/client-cloud9/src/models/models_0.ts
@@ -109,9 +109,11 @@ export interface CreateEnvironmentEC2Request {
    * <p>The identifier for the Amazon Machine Image (AMI) that's used to create the EC2 instance.
    *       To choose an AMI for the instance, you must specify a valid AMI alias or a valid Amazon EC2 Systems Manager (SSM)
    *       path.</p>
-   *          <p>The default AMI is used if the parameter isn't explicitly assigned a value in the request.
-   *       Because Amazon Linux AMI has ended standard support as of December 31, 2020, we recommend you
-   *       choose Amazon Linux 2, which includes long term support through 2023.</p>
+   *          <p>The default Amazon Linux AMI is currently used if the parameter isn't explicitly assigned
+   *       a value in the request. </p>
+   *          <p>In the future the parameter for Amazon Linux will no longer be available when you specify
+   *       an AMI for your instance. Amazon Linux 2 will then become the default AMI, which is used to
+   *       launch your instance if no parameter is explicitly defined.</p>
    *          <p>
    *             <b>AMI aliases </b>
    *          </p>
