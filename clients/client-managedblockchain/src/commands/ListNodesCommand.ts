fb917651af63202781f950ca9b84b1b981b6f490
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-10-31 17:05:23 -0700

    chore(clients): extend TypeScript config from @tsconfig/node14 (#4125)

--- clients/client-managedblockchain/src/commands/ListNodesCommand.ts
+++ clients/client-managedblockchain/src/commands/ListNodesCommand.ts
@@ -63,7 +63,7 @@ export class ListNodesCommand extends $Command<
       UseFIPS: { type: "builtInParams", name: "useFipsEndpoint" },
       Endpoint: { type: "builtInParams", name: "endpoint" },
       Region: { type: "builtInParams", name: "region" },
-      UseDualStack: { type: "builtInParams", name: "useDualstackEndpoint" },
+      UseDualStack: { type: "builtInParams", name: "useDualStack" },
     };
   }
 
