7811cd959b611e13466c7c1f0469947ef32a3695
George Fu <kuhe@users.noreply.github.com>
2022-11-10 11:59:10 -0500

    fix(types): add client config interface test with s3 example (#4156)
    * fix(types): add client config interface test with s3 example
    
    * fix(types): decouple V1orV2Endpoint type from EndpointBearer type
    
    * fix(lib-storage): type fix, allow undefined endpoint
    
    * fix(types): code reorganization in client-api-test

--- packages/middleware-serde/src/serdePlugin.ts
+++ packages/middleware-serde/src/serdePlugin.ts
@@ -5,6 +5,7 @@ import {
   MetadataBearer,
   MiddlewareStack,
   Pluggable,
+  Provider,
   RequestSerializer,
   ResponseDeserializer,
   SerializeHandlerOptions,
@@ -30,22 +31,22 @@ export const serializerMiddlewareOption: SerializeHandlerOptions = {
 
 // Type the modifies the EndpointBearer to make it compatible with Endpoints 2.0 change.
 // Must be removed after all clients has been onboard the Endpoints 2.0
-export type V1OrV2Endpoint<T extends EndpointBearer> = T & {
+export type V1OrV2Endpoint = {
+  // for v2
   urlParser?: UrlParser;
+
+  // for v1
+  endpoint?: Provider<Endpoint>;
 };
 
-export function getSerdePlugin<
-  InputType extends object,
-  SerDeContext extends EndpointBearer,
-  OutputType extends MetadataBearer
->(
-  config: V1OrV2Endpoint<SerDeContext>,
-  serializer: RequestSerializer<any, SerDeContext>,
+export function getSerdePlugin<InputType extends object, SerDeContext, OutputType extends MetadataBearer>(
+  config: V1OrV2Endpoint,
+  serializer: RequestSerializer<any, SerDeContext & EndpointBearer>,
   deserializer: ResponseDeserializer<OutputType, any, SerDeContext>
 ): Pluggable<InputType, OutputType> {
   return {
     applyToStack: (commandStack: MiddlewareStack<InputType, OutputType>) => {
-      commandStack.add(deserializerMiddleware(config, deserializer), deserializerMiddlewareOption);
+      commandStack.add(deserializerMiddleware(config as SerDeContext, deserializer), deserializerMiddlewareOption);
       commandStack.add(serializerMiddleware(config, serializer), serializerMiddlewareOption);
     },
   };
