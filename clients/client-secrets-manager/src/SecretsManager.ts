b658082fe11ca24c2fff4bf9a078d31f2f0c9888
awstools <awstools@amazon.com>
2022-11-17 19:28:07 +0000

    docs(client-secrets-manager): Documentation updates for Secrets Manager.

--- clients/client-secrets-manager/src/SecretsManager.ts
+++ clients/client-secrets-manager/src/SecretsManager.ts
@@ -859,6 +859,7 @@ export class SecretsManager extends SecretsManagerClient {
    *       version. If the <code>AWSPENDING</code> staging label is present but not attached to the same
    *       version as <code>AWSCURRENT</code>, then any later invocation of <code>RotateSecret</code>
    *       assumes that a previous rotation request is still in progress and returns an error.</p>
+   *          <p>When rotation is unsuccessful, the <code>AWSPENDING</code> staging label might be attached to an empty secret version. For more information, see <a href="https://docs.aws.amazon.com/secretsmanager/latest/userguide/troubleshoot_rotation.html">Troubleshoot rotation</a> in the <i>Secrets Manager User Guide</i>.</p>
    *          <p>Secrets Manager generates a CloudTrail log entry when you call this action. Do not include sensitive information in request parameters because it might be logged. For more information, see <a href="https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieve-ct-entries.html">Logging Secrets Manager events with CloudTrail</a>.</p>
    *          <p>
    *             <b>Required permissions: </b>
--- clients/client-secrets-manager/src/models/models_0.ts
+++ clients/client-secrets-manager/src/models/models_0.ts
@@ -561,7 +561,7 @@ export interface DeleteSecretRequest {
    *             <p>Use this parameter with caution. This parameter causes the operation to skip the normal
    *         recovery window before the permanent deletion that Secrets Manager would normally impose with the
    *           <code>RecoveryWindowInDays</code> parameter. If you delete a secret with the
-   *           <code>ForceDeleteWithouRecovery</code> parameter, then you have no opportunity to recover
+   *           <code>ForceDeleteWithoutRecovery</code> parameter, then you have no opportunity to recover
    *         the secret. You lose the secret permanently.</p>
    *          </important>
    */
@@ -607,37 +607,42 @@ export interface RotationRulesType {
    *          <p>In <code>DescribeSecret</code> and <code>ListSecrets</code>, this value is calculated from
    *       the rotation schedule after every successful rotation. In <code>RotateSecret</code>, you can
    *       set the rotation schedule in <code>RotationRules</code> with <code>AutomaticallyAfterDays</code>
-   *       or <code>ScheduleExpression</code>, but not both.</p>
+   *       or <code>ScheduleExpression</code>, but not both. To set a rotation schedule in hours, use
+   *       <code>ScheduleExpression</code>.</p>
    */
   AutomaticallyAfterDays?: number;
 
   /**
-   * <p>The length of the rotation window in hours, for example <code>3h</code> for a three hour window. Secrets Manager
-   *       rotates your secret at any time during this window. The window must not go into the next UTC
-   *       day. If you don't specify this value, the window automatically ends at the end of
-   *       the UTC day. The window begins according to the <code>ScheduleExpression</code>. For more
-   *       information, including examples, see <a href="https://docs.aws.amazon.com/secretsmanager/latest/userguide/rotate-secrets_schedule.html">Schedule expressions
-   *         in Secrets Manager rotation</a>.</p>
+   * <p>The length of the rotation window in hours, for example <code>3h</code> for a three
+   *       hour window. Secrets Manager rotates your secret at any time during this window. The window must not
+   *       extend into the next rotation window or the next UTC day. The window starts according to the <code>ScheduleExpression</code>. If you don't specify a <code>Duration</code>,
+   *       for a <code>ScheduleExpression</code> in hours, the window automatically closes after one
+   *       hour. For a <code>ScheduleExpression</code> in days, the window automatically closes at the
+   *       end of the UTC day. For
+   *       more information, including examples, see <a href="https://docs.aws.amazon.com/secretsmanager/latest/userguide/rotate-secrets_schedule.html">Schedule expressions
+   *       in Secrets Manager rotation</a> in the <i>Secrets Manager Users Guide</i>.</p>
    */
   Duration?: string;
 
   /**
    * <p>A <code>cron()</code> or <code>rate()</code> expression that defines the schedule for
-   *       rotating your secret. Secrets Manager rotation schedules use UTC time zone. </p>
-   *          <p>Secrets Manager <code>rate()</code> expressions
-   *       represent the interval in days that you want to rotate your secret, for example
-   *       <code>rate(10 days)</code>. If you use a <code>rate()</code> expression, the rotation
-   *       window opens at midnight, and Secrets Manager rotates your secret any time that day after midnight.
-   *       You can set a <code>Duration</code> to shorten the rotation window.</p>
-   *          <p>You can use a <code>cron()</code> expression to create rotation schedules that are
+   *       rotating your secret. Secrets Manager rotation schedules use UTC time zone. Secrets Manager rotates your secret any time during a rotation window.</p>
+   *          <p>Secrets Manager <code>rate()</code> expressions represent the interval in hours or days that you
+   *       want to rotate your secret, for example <code>rate(12 hours)</code> or
+   *       <code>rate(10 days)</code>. You can rotate a secret as often as every four hours. If you
+   *       use a <code>rate()</code> expression, the rotation
+   *       window starts at midnight. For a rate in hours, the default rotation window closes after one
+   *       hour. For a rate in days, the default rotation window closes at the end of the day. You can
+   *       set the <code>Duration</code> to change the rotation window. The rotation window must not
+   *       extend into the next UTC day or into the next rotation window.</p>
+   *          <p>You can use a <code>cron()</code> expression to create a rotation schedule that is
    *       more detailed than a rotation interval. For more information, including examples, see
-   *       <a href="https://docs.aws.amazon.com/secretsmanager/latest/userguide/rotate-secrets_schedule.html">Schedule expressions
-   *         in Secrets Manager rotation</a>. If you use a <code>cron()</code> expression, Secrets Manager rotates
-   *       your secret any time during that day after the window opens. For example,
-   *       <code>cron(0 8 1 * ? *)</code> represents a rotation window that occurs on the first
-   *       day of every month beginning at 8:00 AM UTC. Secrets Manager rotates the secret any time that day
-   *       after 8:00 AM. You can set a <code>Duration</code> to shorten
-   *       the rotation window.</p>
+   *       <a href="https://docs.aws.amazon.com/secretsmanager/latest/userguide/rotate-secrets_schedule.html">Schedule expressions in
+   *       Secrets Manager rotation</a> in the <i>Secrets Manager Users Guide</i>. For a cron expression
+   *       that represents a schedule in hours, the default rotation window closes after one hour. For
+   *       a cron expression that represents a schedule in days, the default rotation window closes at
+   *       the end of the day. You can set the <code>Duration</code> to change the rotation window. The
+   *       rotation window must not extend into the next UTC day or into the next rotation window.</p>
    */
   ScheduleExpression?: string;
 }
@@ -1042,7 +1047,7 @@ export interface ListSecretsRequest {
   Filters?: Filter[];
 
   /**
-   * <p>Lists secrets in the requested order. </p>
+   * <p>Secrets are listed by <code>CreatedDate</code>. </p>
    */
   SortOrder?: SortOrderType | string;
 }
