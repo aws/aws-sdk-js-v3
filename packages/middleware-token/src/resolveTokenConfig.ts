2af788e106ab8761a0edb572a43fddd998334cb8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-11-29 01:40:09 +0000

    chore: use direct function reference to nodeProvider

--- packages/middleware-token/src/resolveTokenConfig.ts
+++ packages/middleware-token/src/resolveTokenConfig.ts
@@ -4,5 +4,5 @@ import { tokenDefaultProvider } from "./tokenDefaultProvider";
 
 export const resolveTokenConfig = <T>(input: T & TokenInputConfig): T & TokenResolvedConfig => ({
   ...input,
-  token: input.token ? normalizeTokenProvider(input.token) : tokenDefaultProvider(input),
+  token: input.token ? normalizeTokenProvider(input.token) : tokenDefaultProvider(input as any),
 });
--- packages/middleware-token/src/tokenDefaultProvider.ts
+++ packages/middleware-token/src/tokenDefaultProvider.ts
@@ -1,3 +1,3 @@
 import { nodeProvider } from "@aws-sdk/token-providers";
 
-export const tokenDefaultProvider = (input: unknown) => nodeProvider(input as any);
+export const tokenDefaultProvider = nodeProvider;
