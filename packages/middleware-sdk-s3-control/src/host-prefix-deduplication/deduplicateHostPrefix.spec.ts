c5c8cd1bc7bcb744023f40a8a0addd0f1a565d86
George Fu <georgfu@amazon.com>
2022-12-14 02:40:54 +0000

    fix(client-s3-control): formatting

--- packages/middleware-sdk-s3-control/src/host-prefix-deduplication/deduplicateHostPrefix.spec.ts
+++ packages/middleware-sdk-s3-control/src/host-prefix-deduplication/deduplicateHostPrefix.spec.ts
@@ -5,14 +5,13 @@ describe(deduplicateHostPrefix.name, () => {
     expect(deduplicateHostPrefix("a.a.host.com")).toEqual("a.host.com");
     expect(deduplicateHostPrefix("1234567890.1234567890.host.com")).toEqual("1234567890.host.com");
     expect(deduplicateHostPrefix("abcdefgh.abcdefgh.host.com")).toEqual("abcdefgh.host.com");
-  }),
-    it("should do nothing if no duplication exists in the first two positions", () => {
-      expect(deduplicateHostPrefix("b.a.host.com")).toEqual("b.a.host.com");
-      expect(deduplicateHostPrefix("0123456789.1234567890.host.com")).toEqual("0123456789.1234567890.host.com");
-      expect(deduplicateHostPrefix("zabcdefg.abcdefgh.host.com")).toEqual("zabcdefg.abcdefgh.host.com");
+  });
 
-      expect(deduplicateHostPrefix("12345.abcdefgh.12345.12345.host.com")).toEqual(
-        "12345.abcdefgh.12345.12345.host.com"
-      );
-    });
+  it("should do nothing if no duplication exists in the first two positions", () => {
+    expect(deduplicateHostPrefix("b.a.host.com")).toEqual("b.a.host.com");
+    expect(deduplicateHostPrefix("0123456789.1234567890.host.com")).toEqual("0123456789.1234567890.host.com");
+    expect(deduplicateHostPrefix("zabcdefg.abcdefgh.host.com")).toEqual("zabcdefg.abcdefgh.host.com");
+
+    expect(deduplicateHostPrefix("12345.abcdefgh.12345.12345.host.com")).toEqual("12345.abcdefgh.12345.12345.host.com");
+  });
 });
