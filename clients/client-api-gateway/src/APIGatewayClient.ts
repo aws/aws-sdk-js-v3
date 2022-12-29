79788977e89b47333020871b062effb991708107
awstools <awstools@amazon.com>
2022-12-29 19:11:15 +0000

    docs(client-api-gateway): Documentation updates for Amazon API Gateway

--- clients/client-api-gateway/src/APIGatewayClient.ts
+++ clients/client-api-gateway/src/APIGatewayClient.ts
@@ -679,7 +679,7 @@ export interface APIGatewayClientResolvedConfig extends APIGatewayClientResolved
 
 /**
  * <fullname>Amazon API Gateway</fullname>
- *         <p>Amazon API Gateway helps developers deliver robust, secure, and scalable mobile and web application back ends. API Gateway allows developers to securely connect mobile and web applications to APIs that run on AWS Lambda, Amazon EC2, or other publicly addressable web services that are hosted outside of AWS.</p>
+ *          <p>Amazon API Gateway helps developers deliver robust, secure, and scalable mobile and web application back ends. API Gateway allows developers to securely connect mobile and web applications to APIs that run on AWS Lambda, Amazon EC2, or other publicly addressable web services that are hosted outside of AWS.</p>
  */
 export class APIGatewayClient extends __Client<
   __HttpHandlerOptions,
