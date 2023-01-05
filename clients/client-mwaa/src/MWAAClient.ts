5316f31ca3af43c5344d46cce4fd93727ebe735f
awstools <awstools@amazon.com>
2023-01-05 19:13:50 +0000

    docs(client-mwaa): MWAA supports Apache Airflow version 2.4.3.

--- clients/client-mwaa/src/MWAAClient.ts
+++ clients/client-mwaa/src/MWAAClient.ts
@@ -249,89 +249,89 @@ export interface MWAAClientResolvedConfig extends MWAAClientResolvedConfigType {
 
 /**
  * <fullname>Amazon Managed Workflows for Apache Airflow</fullname>
- *         <p>This section contains the Amazon Managed Workflows for Apache Airflow (MWAA) API reference documentation. For more information, see <a href="https://docs.aws.amazon.com/mwaa/latest/userguide/what-is-mwaa.html">What Is Amazon MWAA?</a>.</p>
- *         <p>
+ *          <p>This section contains the Amazon Managed Workflows for Apache Airflow (MWAA) API reference documentation. For more information, see <a href="https://docs.aws.amazon.com/mwaa/latest/userguide/what-is-mwaa.html">What Is Amazon MWAA?</a>.</p>
+ *          <p>
  *             <b>Endpoints</b>
  *          </p>
- *         <ul>
+ *          <ul>
  *             <li>
- *                 <p>
+ *                <p>
  *                   <code>api.airflow.{region}.amazonaws.com</code> - This endpoint is used for environment management.</p>
- *                 <ul>
+ *                <ul>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_CreateEnvironment.html">CreateEnvironment</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_DeleteEnvironment.html">DeleteEnvironment</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_GetEnvironment.html">GetEnvironment</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_ListEnvironments.html">ListEnvironments</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_ListTagsForResource.html">ListTagsForResource</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_TagResource.html">TagResource</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_UntagResource.html">UntagResource</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_UpdateEnvironment.html">UpdateEnvironment</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                </ul>
  *             </li>
  *             <li>
- *                 <p>
+ *                <p>
  *                   <code>env.airflow.{region}.amazonaws.com</code> - This endpoint is used to operate the Airflow environment.</p>
- *                 <ul>
+ *                <ul>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_CreateCliToken.html ">CreateCliToken</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_CreateWebLoginToken.html">CreateWebLoginToken</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                </ul>
  *             </li>
  *             <li>
- *                 <p>
+ *                <p>
  *                   <code>ops.airflow.{region}.amazonaws.com</code> - This endpoint is used to push environment metrics that track environment health.</p>
- *                 <ul>
+ *                <ul>
  *                   <li>
- *                         <p>
+ *                      <p>
  *                         <a href="https://docs.aws.amazon.com/mwaa/latest/API/API_PublishMetrics.html ">PublishMetrics</a>
  *                      </p>
- *                     </li>
+ *                   </li>
  *                </ul>
  *             </li>
  *          </ul>
- *         <p>
+ *          <p>
  *             <b>Regions</b>
  *          </p>
- *         <p>For a list of regions that Amazon MWAA supports, see <a href="https://docs.aws.amazon.com/mwaa/latest/userguide/what-is-mwaa.html#regions-mwaa">Region availability</a> in the <i>Amazon MWAA User Guide</i>.</p>
+ *          <p>For a list of regions that Amazon MWAA supports, see <a href="https://docs.aws.amazon.com/mwaa/latest/userguide/what-is-mwaa.html#regions-mwaa">Region availability</a> in the <i>Amazon MWAA User Guide</i>.</p>
  */
 export class MWAAClient extends __Client<
   __HttpHandlerOptions,
