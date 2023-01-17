fa63303c12a86a7a1cbd134c29150e1ff3d8bc53
awstools <awstools@amazon.com>
2023-01-17 19:10:38 +0000

    feat(client-network-firewall): Network Firewall now allows creation of dual stack endpoints, enabling inspection of IPv6 traffic.

--- clients/client-network-firewall/src/models/models_0.ts
+++ clients/client-network-firewall/src/models/models_0.ts
@@ -52,7 +52,7 @@ export interface ActionDefinition {
  */
 export interface Address {
   /**
-   * <p>Specify an IP address or a block of IP addresses in Classless Inter-Domain Routing (CIDR) notation. Network Firewall supports all address ranges for IPv4. </p>
+   * <p>Specify an IP address or a block of IP addresses in Classless Inter-Domain Routing (CIDR) notation. Network Firewall supports all address ranges for IPv4 and IPv6. </p>
    *          <p>Examples: </p>
    *          <ul>
    *             <li>
@@ -60,6 +60,12 @@ export interface Address {
    *             </li>
    *             <li>
    *                <p>To configure Network Firewall to inspect for IP addresses from 192.0.2.0 to 192.0.2.255, specify <code>192.0.2.0/24</code>.</p>
+   *             </li>
+   *             <li>
+   *                <p>To configure Network Firewall to inspect for the IP address 1111:0000:0000:0000:0000:0000:0000:0111, specify <code>1111:0000:0000:0000:0000:0000:0000:0111/128</code>.</p>
+   *             </li>
+   *             <li>
+   *                <p>To configure Network Firewall to inspect for IP addresses from 1111:0000:0000:0000:0000:0000:0000:0000 to 1111:0000:0000:0000:ffff:ffff:ffff:ffff, specify <code>1111:0000:0000:0000:0000:0000:0000:0000/64</code>.</p>
    *             </li>
    *          </ul>
    *          <p>For more information about CIDR notation, see the Wikipedia entry <a href="https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing">Classless
@@ -259,6 +265,11 @@ export class ThrottlingException extends __BaseException {
   }
 }
 
+export enum IPAddressType {
+  DUALSTACK = "DUALSTACK",
+  IPV4 = "IPV4",
+}
+
 /**
  * <p>The ID for a subnet that you want to associate with the firewall. This is used with
  *             <a>CreateFirewall</a> and <a>AssociateSubnets</a>. Network Firewall
@@ -270,6 +281,11 @@ export interface SubnetMapping {
    * <p>The unique identifier for the subnet. </p>
    */
   SubnetId: string | undefined;
+
+  /**
+   * <p>The subnet's IP address type. You can't change the IP address type after you create the subnet.</p>
+   */
+  IPAddressType?: IPAddressType | string;
 }
 
 export interface AssociateSubnetsRequest {
@@ -1208,7 +1224,7 @@ export interface Header {
   /**
    * <p>The source IP address or address range to inspect for, in CIDR notation.
    *           To match with any address, specify <code>ANY</code>. </p>
-   *          <p>Specify an IP address or a block of IP addresses in Classless Inter-Domain Routing (CIDR) notation. Network Firewall supports all address ranges for IPv4. </p>
+   *          <p>Specify an IP address or a block of IP addresses in Classless Inter-Domain Routing (CIDR) notation. Network Firewall supports all address ranges for IPv4 and IPv6. </p>
    *          <p>Examples: </p>
    *          <ul>
    *             <li>
@@ -1216,6 +1232,12 @@ export interface Header {
    *             </li>
    *             <li>
    *                <p>To configure Network Firewall to inspect for IP addresses from 192.0.2.0 to 192.0.2.255, specify <code>192.0.2.0/24</code>.</p>
+   *             </li>
+   *             <li>
+   *                <p>To configure Network Firewall to inspect for the IP address 1111:0000:0000:0000:0000:0000:0000:0111, specify <code>1111:0000:0000:0000:0000:0000:0000:0111/128</code>.</p>
+   *             </li>
+   *             <li>
+   *                <p>To configure Network Firewall to inspect for IP addresses from 1111:0000:0000:0000:0000:0000:0000:0000 to 1111:0000:0000:0000:ffff:ffff:ffff:ffff, specify <code>1111:0000:0000:0000:0000:0000:0000:0000/64</code>.</p>
    *             </li>
    *          </ul>
    *          <p>For more information about CIDR notation, see the Wikipedia entry <a href="https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing">Classless
@@ -1242,7 +1264,7 @@ export interface Header {
   /**
    * <p>The destination IP address or address range to inspect for, in CIDR notation.
    *           To match with any address, specify <code>ANY</code>. </p>
-   *          <p>Specify an IP address or a block of IP addresses in Classless Inter-Domain Routing (CIDR) notation. Network Firewall supports all address ranges for IPv4. </p>
+   *          <p>Specify an IP address or a block of IP addresses in Classless Inter-Domain Routing (CIDR) notation. Network Firewall supports all address ranges for IPv4 and IPv6. </p>
    *          <p>Examples: </p>
    *          <ul>
    *             <li>
@@ -1250,6 +1272,12 @@ export interface Header {
    *             </li>
    *             <li>
    *                <p>To configure Network Firewall to inspect for IP addresses from 192.0.2.0 to 192.0.2.255, specify <code>192.0.2.0/24</code>.</p>
+   *             </li>
+   *             <li>
+   *                <p>To configure Network Firewall to inspect for the IP address 1111:0000:0000:0000:0000:0000:0000:0111, specify <code>1111:0000:0000:0000:0000:0000:0000:0111/128</code>.</p>
+   *             </li>
+   *             <li>
+   *                <p>To configure Network Firewall to inspect for IP addresses from 1111:0000:0000:0000:0000:0000:0000:0000 to 1111:0000:0000:0000:ffff:ffff:ffff:ffff, specify <code>1111:0000:0000:0000:0000:0000:0000:0000/64</code>.</p>
    *             </li>
    *          </ul>
    *          <p>For more information about CIDR notation, see the Wikipedia entry <a href="https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing">Classless
