02e017dd9cc18c8f70a6c813cfdbfdde32b8d802
fossand <fossand@amazon.com>
2023-01-19 09:43:54 -0800

    fix(util-utf8): add missing dependency in middleware-flexible-checksums

--- packages/util-utf8/src/toUint8Array.spec.ts
+++ packages/util-utf8/src/toUint8Array.spec.ts
@@ -5,6 +5,11 @@ describe("toUint8Array", () => {
     const expected = new Uint8Array([240, 159, 144, 142, 240, 159, 145, 177, 226, 157, 164]);
     expect(toUint8Array("🐎👱❤")).toStrictEqual(expected);
   });
+  it(`should convert buffer to Uint8Array`, () => {
+    const expected = new Uint8Array([240, 159, 144, 142, 240, 159, 145, 177, 226, 157, 164]);
+    const buffer = Buffer.from(expected);
+    expect(toUint8Array(buffer)).toStrictEqual(expected);
+  });
   it(`should convert ArrayBuffer to Uint8Array`, () => {
     const input = new Uint32Array([240]);
     const expected = new Uint8Array([240, 0, 0, 0]);
