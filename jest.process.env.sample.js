6b87e74dff833d0bd352758e465c38e771b6a405
George Fu <kuhe@users.noreply.github.com>
2023-01-31 16:28:08 +0000

    fix(s3-presigned-post): switch to test:e2e

--- jest.process.env.sample.js
+++
@@ -1,4 +0,0 @@
-process.env.AWS_SMOKE_TEST_BUCKET = "";
-process.env.AWS_SMOKE_TEST_REGION = "";
-
-// copy this file to jest.process.env.js
--- packages/s3-presigned-post/src/createPresignedPost.integ.spec.ts
+++ packages/s3-presigned-post/src/createPresignedPost.integ.spec.ts
@@ -1,3 +1,6 @@
+// Run using AWS_SMOKE_TEST_REGION=[Region] AWS_SMOKE_TEST_BUCKET=[Bucket] yarn test:e2e
+// These params are established in /tests/e2e.
+
 import { NoSuchKey, S3 } from "@aws-sdk/client-s3";
 import { STS } from "@aws-sdk/client-sts";
 const FormData = require("form-data");
@@ -6,10 +9,6 @@ import { createReadStream, rmSync, writeFileSync } from "fs";
 import { join } from "path";
 
 import { createPresignedPost } from "./createPresignedPost";
-
-try {
-  require("../../../jest.process.env");
-} catch (ignored) {}
 
 describe(createPresignedPost.name, () => {
   it("should allow custom endpoints to be modified by endpoint resolution options", async () => {
@@ -37,15 +36,10 @@ describe(createPresignedPost.name, () => {
     let Bucket: string;
     let Key: string;
     let client: S3;
-    let accountId: string;
     let contents: string;
     let fileLocation: string;
 
     beforeAll(async () => {
-      accountId = (await new STS({}).getCallerIdentity({})).Account ?? "";
-      if (!accountId) {
-        throw new Error("No AWS Account ID found.");
-      }
       region = process.env.AWS_SMOKE_TEST_REGION ?? "";
       if (!region) {
         throw new Error("process.env.AWS_SMOKE_TEST_REGION is not set.");
@@ -54,12 +48,11 @@ describe(createPresignedPost.name, () => {
       if (!Bucket) {
         throw new Error("process.env.AWS_SMOKE_TEST_BUCKET is not set.");
       }
-      Bucket += "-" + accountId;
 
       Key = `aws-sdk-js-integration-test-s3-presigned-post-${Date.now()}.txt`;
       contents = "Hello, world!";
       fileLocation = join(__dirname, Key);
-      client = new S3({ region, endpoint: `https://s3-fips.dualstack.${region}.amazonaws.com` });
+      client = new S3({ region, endpoint: `https://s3.dualstack.${region}.amazonaws.com` });
 
       await client.createBucket({ Bucket });
       writeFileSync(fileLocation, contents, "utf-8");
@@ -80,7 +73,7 @@ describe(createPresignedPost.name, () => {
     it("should put an object using a presigned post w/ custom endpoint", async () => {
       const { url, fields } = await createPresignedPost(client, { Bucket, Key });
 
-      expect(url).toBe(`https://${Bucket}.s3-fips.dualstack.us-east-1.amazonaws.com/`);
+      expect(url).toBe(`https://${Bucket}.s3.dualstack.${region}.amazonaws.com/`);
 
       const form = new FormData();
       Object.entries(fields).forEach(([field, value]) => {
