6c5ac912cae21bed838fc361f285175223455b85
Michael Young <micyouna@amazon.com>
2023-02-13 21:03:38 +0000

    chore(remove-rename): remove references to legacy plugin options

--- packages/service-client-documentation-generator/src/index.ts
+++ packages/service-client-documentation-generator/src/index.ts
@@ -1,23 +1,14 @@
 import { Application, ParameterType } from "typedoc";
 
-// import { SdkClientRenameProjectPlugin } from "./sdk-client-rename-project";
 import { SdkClientTocPlugin } from "./sdk-client-toc-plugin";
 
 export function load(app: Application) {
-  // app.options.addDeclaration({
-  //   name: "defaultServiceSuffix",
-  //   help: "Default suffix to be used when renaming service project.",
-  //   defaultValue: "AWS SDK for JavaScript v3",
-  //   type: ParameterType.String,
-  // });
   app.options.addDeclaration({
     name: "defaultGroup",
     help: "Default group to place categories as children",
     defaultValue: "SDK",
     type: ParameterType.String,
   });
-
-  // new SdkClientRenameProjectPlugin(app.options, app.logger, app.renderer);
 
   new SdkClientTocPlugin(app.options, app.logger, app.renderer);
 }
