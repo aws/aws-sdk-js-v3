09fcd43380351bac8895d1eb9792e4b3e39713e8
Miles Ziemer <mziemer@amazon.com>
2023-02-13 15:34:36 -0500

    fix(protocol-http): export field types
    The Field, FieldPosition, and Fields types were not being
    exported from the package when they were initially created.

--- packages/protocol-http/src/index.ts
+++ packages/protocol-http/src/index.ts
@@ -1,3 +1,6 @@
+export * from "./Field";
+export * from "./FieldPosition";
+export * from "./Fields";
 export * from "./httpHandler";
 export * from "./httpRequest";
 export * from "./httpResponse";
