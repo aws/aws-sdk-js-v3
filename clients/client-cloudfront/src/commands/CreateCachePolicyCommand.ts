4835fc800bf05075eb76090c6202b3ccb5d9682f
awstools <awstools@amazon.com>
2023-02-22 19:12:28 +0000

    feat(client-cloudfront): CloudFront now supports block lists in origin request policies so that you can forward all headers, cookies, or query string from viewer requests to the origin *except* for those specified in the block list.

--- clients/client-cloudfront/src/commands/CreateCachePolicyCommand.ts
+++ clients/client-cloudfront/src/commands/CreateCachePolicyCommand.ts
@@ -45,11 +45,11 @@ export interface CreateCachePolicyCommandOutput extends CreateCachePolicyResult,
  * 					objects to stay in the CloudFront cache.</p>
  *             </li>
  *          </ul>
- *          <p>The headers, cookies, and query strings that are included in the cache key are
- * 			automatically included in requests that CloudFront sends to the origin. CloudFront sends a request
- * 			when it can't find an object in its cache that matches the request's cache key. If you
- * 			want to send values to the origin but <i>not</i> include them in the cache
- * 			key, use <code>OriginRequestPolicy</code>.</p>
+ *          <p>The headers, cookies, and query strings that are included in the cache key are also included
+ * 			in requests that CloudFront sends to the origin. CloudFront sends a request when it can't find an
+ * 			object in its cache that matches the request's cache key. If you want to send values to
+ * 			the origin but <i>not</i> include them in the cache key, use
+ * 			<code>OriginRequestPolicy</code>.</p>
  *          <p>For more information about cache policies, see <a href="https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/controlling-the-cache-key.html">Controlling the cache key</a> in the
  * 				<i>Amazon CloudFront Developer Guide</i>.</p>
  * @example
