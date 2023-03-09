5f4d40bb15368f2cd04eaca17442cbb3d49907f5
Michael Young <michael25young25@gmail.com>
2023-03-09 16:22:32 -0500

    docs(rm-reexport-tags): remove unnecessary release tags from index files

--- packages/types/src/identity/index.ts
+++ packages/types/src/identity/index.ts
@@ -1,20 +1,5 @@
-/**
- * @public
- */
 export * from "./AnonymousIdentity";
-/**
- * @public
- */
 export * from "./AwsCredentialIdentity";
-/**
- * @public
- */
 export * from "./Identity";
-/**
- * @public
- */
 export * from "./LoginIdentity";
-/**
- * @public
- */
 export * from "./TokenIdentity";
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,108 +1,27 @@
-/**
- * @public
- */
 export * from "./abort";
-/**
- * @public
- */
 export * from "./auth";
-/**
- * @public
- */
 export * from "./checksum";
-/**
- * @public
- */
 export * from "./client";
-/**
- * @public
- */
 export * from "./command";
-/**
- * @public
- */
 export * from "./credentials";
-/**
- * @public
- */
 export * from "./crypto";
-/**
- * @public
- */
 export * from "./dns";
-/**
- * @public
- */
 export * from "./endpoint";
-/**
- * @public
- */
 export * from "./eventStream";
-/**
- * @public
- */
 export * from "./http";
-/**
- * @public
- */
 export * from "./identity";
-/**
- * @public
- */
 export * from "./logger";
-/**
- * @public
- */
 export * from "./middleware";
-/**
- * @public
- */
 export * from "./pagination";
-/**
- * @public
- */
 export * from "./profile";
-/**
- * @public
- */
 export * from "./request";
-/**
- * @public
- */
 export * from "./response";
-/**
- * @public
- */
 export * from "./retry";
-/**
- * @public
- */
 export * from "./serde";
-/**
- * @public
- */
 export * from "./shapes";
-/**
- * @public
- */
 export * from "./signature";
-/**
- * @public
- */
 export * from "./stream";
-/**
- * @public
- */
 export * from "./token";
-/**
- * @public
- */
 export * from "./transfer";
-/**
- * @public
- */
 export * from "./util";
-/**
- * @public
- */
 export * from "./waiter";
