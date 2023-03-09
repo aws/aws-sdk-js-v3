2f4481bcb97e04953c1d9ecb6ba34d66797766cd
Miles Ziemer <45497130+milesziemer@users.noreply.github.com>
2023-03-09 11:14:23 -0500

    chore(protocoltests): use HttpResponse constructor (#4505)
    This commit contains changes from the codegen changes in
    https://github.com/awslabs/smithy-typescript/pull/698. It updates
    protocol tests to construct the HttpResponse object, instead of
    using a regular object.

--- private/aws-protocoltests-restjson/test/functional/restjson1.spec.ts
+++ private/aws-protocoltests-restjson/test/functional/restjson1.spec.ts
@@ -106,11 +106,11 @@ class ResponseDeserializationTestHandler implements HttpHandler {
 
   handle(request: HttpRequest, options?: HttpHandlerOptions): Promise<{ response: HttpResponse }> {
     return Promise.resolve({
-      response: {
+      response: new HttpResponse({
         statusCode: this.code,
         headers: this.headers,
         body: Readable.from([this.body]),
-      },
+      }),
     });
   }
 }
