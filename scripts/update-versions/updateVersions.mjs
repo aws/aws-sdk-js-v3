4d478109a72b4612418adffd28c1cd71d7f9f805
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-03-20 14:31:34 -0700

    fix(scripts): prettify package.json when updating version numbers (#4552)

--- scripts/update-versions/updateVersions.mjs
+++ scripts/update-versions/updateVersions.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { readFileSync, writeFileSync } from "fs";
 import { join } from "path";
+import { format } from "prettier";
 
 import { getWorkspacePaths } from "../utils/getWorkspacePaths.mjs";
 import { getUpdatedPackageJson } from "./getUpdatedPackageJson.mjs";
@@ -10,6 +11,6 @@ export const updateVersions = (depToVersionHash) => {
     const packageJsonPath = join(workspacePath, "package.json");
     const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
     const updatedPackageJson = getUpdatedPackageJson(packageJson, depToVersionHash);
-    writeFileSync(packageJsonPath, JSON.stringify(updatedPackageJson, null, 2).concat(`\n`));
+    writeFileSync(packageJsonPath, format(JSON.stringify(updatedPackageJson), { parser: "json" }));
   });
 };
