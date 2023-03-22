c4a2e5927daa073e82e4f3d4638d2d35db46cf7f
Yenfry Herrera Feliz <herrergy@amazon.com>
2023-03-22 11:20:16 -0700

    test: add unit test to set-socket-keep-alive
    Unit tests check the following:
    - that when keepAlive is provided as true it should set keepAlive to true to the socket.
    - that when keepAlive is provided as true and a custom initial delay is given they are set correctly.
    - that when keepAlive is provided as set or not provided at all the keepAlive option is not set to the socket.

---
+++ packages/node-http-handler/src/set-socket-keep-alive.spec.ts
@@ -0,0 +1,69 @@
+import { setSocketKeepAlive } from "./set-socket-keep-alive";
+import { EventEmitter } from "events";
+
+class MockSocket {
+    enableKeepAlive: boolean;
+    keepAliveInitialDelay: number;
+
+    setKeepAlive(enable, initialDelay) {
+        this.enableKeepAlive = enable;
+        this.keepAliveInitialDelay = initialDelay;
+    }
+
+    isKeepAliveEnabled() {
+        return this.enableKeepAlive;
+    }
+
+    getKeepALiveInitialDelay() {
+        return this.keepAliveInitialDelay;
+    }
+}
+
+describe("setSocketKeepAlive", () => {
+    let request: any;
+    let socket;
+
+    beforeEach(() => {
+        request = new EventEmitter();
+        socket = new MockSocket();
+    });
+
+    it("should set keepAlive to true", () => {
+        setSocketKeepAlive(request, {
+            keepAlive: true,
+        });
+        request.emit("socket", socket);
+
+        expect(socket.isKeepAliveEnabled()).toBeTruthy();
+    });
+
+    it("should set keepAlive to true with custom initialDelay", () => {
+        const initialDelay = 5 * 1000;
+        setSocketKeepAlive(request, {
+            keepAlive: true,
+            initialDelayInMSecs: initialDelay,
+        });
+        request.emit("socket", socket);
+
+        expect(
+            socket.isKeepAliveEnabled() &&
+            socket.getKeepALiveInitialDelay() === initialDelay
+        ).toBeTruthy();
+    });
+
+    it("should not set keepAlive at all when keepAlive is false", () => {
+        setSocketKeepAlive(request, {
+            keepAlive: false,
+        });
+        request.emit("socket", socket);
+
+        expect(socket.isKeepAliveEnabled()).toBeFalsy();
+    });
+
+    it("should not set keepAlive at all when socketKeepAlive is undefined", () => {
+        setSocketKeepAlive(request, undefined);
+        request.emit("socket", socket);
+
+        expect(socket.isKeepAliveEnabled()).toBeFalsy();
+    });
+});
