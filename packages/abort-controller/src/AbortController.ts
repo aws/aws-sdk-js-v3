895a65b9574afdd0d39d447cd66fe98b7d2e0385
George Fu <kuhe@users.noreply.github.com>
2023-03-29 14:06:12 -0400

    docs(abort-controller): add message about browsers

--- packages/abort-controller/src/AbortController.ts
+++ packages/abort-controller/src/AbortController.ts
@@ -2,6 +2,10 @@ import { AbortController as IAbortController } from "@aws-sdk/types";
 
 import { AbortSignal } from "./AbortSignal";
 
+/**
+ * This implementation should not be necessary in browsers.
+ * Use the global native implementation in browsers.
+ */
 export class AbortController implements IAbortController {
   public readonly signal: AbortSignal = new AbortSignal();
 
