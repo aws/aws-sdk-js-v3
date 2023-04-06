252cf13208c7103b167a233775d321ea749e7e91
George Fu <kuhe@users.noreply.github.com>
2023-04-06 17:32:14 +0000

    feat(middleware-sdk-rekognitionstreaming): create port middleware

---
+++ packages/middleware-sdk-rekognitionstreaming/README.md
@@ -0,0 +1,4 @@
+# @aws-sdk/middleware-sdk-rekognitionstreaming
+
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/middleware-sdk-rekognitionstreaming/latest.svg)](https://www.npmjs.com/package/@aws-sdk/middleware-sdk-rekognitionstreaming)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/middleware-sdk-rekognitionstreaming.svg)](https://www.npmjs.com/package/@aws-sdk/middleware-sdk-rekognitionstreaming)
---
+++ packages/middleware-sdk-rekognitionstreaming/src/plugin.ts
@@ -0,0 +1,13 @@
+import { Pluggable, RequestHandler } from "@aws-sdk/types";
+
+import { websocketPortMiddleware, websocketPortMiddlewareOptions } from "./middleware-port";
+
+interface PreviouslyResolved {
+  requestHandler: RequestHandler<any, any>;
+}
+
+export const getRekognitionStreamingPlugin = (config: PreviouslyResolved): Pluggable<any, any> => ({
+  applyToStack: (clientStack) => {
+    clientStack.addRelativeTo(websocketPortMiddleware(config), websocketPortMiddlewareOptions);
+  },
+});
