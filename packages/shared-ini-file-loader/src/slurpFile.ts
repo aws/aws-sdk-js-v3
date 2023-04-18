f2d3baf5a33dc305b9e43681030ae733fc2372e7
Harry <harrisonhemstreet@gmail.com>
2023-04-18 11:32:59 -0600

    changes with notes

--- packages/shared-ini-file-loader/src/slurpFile.ts
+++ packages/shared-ini-file-loader/src/slurpFile.ts
@@ -3,9 +3,19 @@ import { promises as fsPromises } from "fs";
 
 const { readFile } = fsPromises;
 
+import { execSync } from "child_process";
+
 const filePromisesHash: Record<string, Promise<string>> = {};
 
 export const slurpFile = (path: string) => {
+  // make the change here in this function. we need to allow for both regular file paths and file paths in a Docker container
+  // ~/.aws/
+  // /root/aws
+  exec("cd ~ && pwd") -> /root -> /usr / home / harry / aws
+  let shellUserName = execSync("whoami").toString();
+  if (execSync("whoami").toString() === "root") {
+    path = '/root/.aws'
+  }
   if (!filePromisesHash[path]) {
     filePromisesHash[path] = readFile(path, "utf8");
   }
