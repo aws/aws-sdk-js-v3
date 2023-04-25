3507d983888ce07d48828dfa1a8b33be79ee8333
George Fu <kuhe@users.noreply.github.com>
2023-04-25 23:55:01 +0000

    chore(e2e): add e2e yarn lerna usage comment

--- tests/e2e/index.js
+++ tests/e2e/index.js
@@ -6,6 +6,8 @@ const { spawnProcess } = require("../../scripts/utils/spawn-process");
 const run = async () => {
   try {
     const integTestResourcesEnv = await getIntegTestResources();
+    // For CI compatibility, the command must be "yarn lerna ..."
+    // and not "lerna" directly.
     await spawnProcess("yarn", ["lerna", "run", "test:e2e", "--concurrency", "1"], {
       cwd: join(__dirname, "..", ".."),
       env: {
