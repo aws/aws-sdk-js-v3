024a3a4c789185ae20060f163d7606baae15ed39
George Fu <kuhe@users.noreply.github.com>
2023-04-27 19:23:59 +0000

    test(middleware): add request integration tests to middleware, part 1

---
+++ packages/middleware-endpoint-discovery/src/middleware-endpoint-discovery.integ.spec.ts
@@ -0,0 +1,64 @@
+import { TimestreamQuery } from "@aws-sdk/client-timestream-query";
+import { TimestreamWrite } from "@aws-sdk/client-timestream-write";
+import { EndpointCache } from "@aws-sdk/endpoint-cache";
+
+import { requireRequestsFrom } from "../../../private/aws-util-test/src";
+import { getCacheKey } from "./getCacheKey";
+
+describe("middleware-endpoint-discovery", () => {
+  describe(TimestreamQuery.name, () => {
+    it("should use endpoint discovery cache", async () => {
+      const client = new TimestreamQuery({
+        region: "us-west-2",
+        endpointDiscoveryEnabled: true,
+      });
+
+      client.config.endpointCache = new EndpointCache(1000);
+      const cacheKey = await getCacheKey("DescribeScheduledQueryCommand", client.config, {});
+      client.config.endpointCache.set(cacheKey, [
+        {
+          Address: "https://my-endpoint.amazonaws.com",
+          CachePeriodInMinutes: 1,
+        },
+      ]);
+
+      requireRequestsFrom(client).toMatch({
+        hostname: /https\:\/\/my-endpoint.amazonaws.com$/,
+      });
+
+      await client.describeScheduledQuery({
+        ScheduledQueryArn: "arn:aws:timestream:us-west-2:1234567890:scheduled-query/1",
+      });
+
+      expect.assertions(1);
+    });
+  });
+
+  describe(TimestreamWrite.name, () => {
+    it("should use endpoint discovery cache", async () => {
+      const client = new TimestreamWrite({
+        region: "us-west-2",
+        endpointDiscoveryEnabled: true,
+      });
+
+      client.config.endpointCache = new EndpointCache(1000);
+      const cacheKey = await getCacheKey("DescribeBatchLoadTaskCommand", client.config, {});
+      client.config.endpointCache.set(cacheKey, [
+        {
+          Address: "https://my-endpoint.amazonaws.com",
+          CachePeriodInMinutes: 1,
+        },
+      ]);
+
+      requireRequestsFrom(client).toMatch({
+        hostname: /https\:\/\/my-endpoint.amazonaws.com$/,
+      });
+
+      await client.describeBatchLoadTask({
+        TaskId: "task-id",
+      });
+
+      expect.assertions(1);
+    });
+  });
+});
---
+++ packages/middleware-flexible-checksums/src/middleware-flexible-checksums.integ.spec.ts
@@ -0,0 +1,86 @@
+import { S3 } from "@aws-sdk/client-s3";
+
+import { requireRequestsFrom } from "../../../private/aws-util-test/src";
+
+describe("middleware-flexible-checksums", () => {
+  describe(S3.name, () => {
+    it("should set flexible checksums (SHA256)", async () => {
+      const client = new S3({ region: "us-west-2" });
+
+      requireRequestsFrom(client).toMatch({
+        method: "PUT",
+        hostname: "s3.us-west-2.amazonaws.com",
+        protocol: "https:",
+        path: "/b/k",
+        headers: {
+          "content-type": "application/octet-stream",
+          "x-amz-sdk-checksum-algorithm": "SHA256",
+          "content-length": "4",
+          Expect: "100-continue",
+          "x-amz-checksum-sha256": "iNQmb9TmM40TuEX88olXnSCciXgjuSF9o+Fhk28DFYk=",
+          host: "s3.us-west-2.amazonaws.com",
+          "x-amz-user-agent": /./,
+          "user-agent": /./,
+          "amz-sdk-invocation-id": /./,
+          "amz-sdk-request": /./,
+          "x-amz-date": /./,
+          "x-amz-security-token": /./,
+          "x-amz-content-sha256": "88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589",
+          authorization: /./,
+        },
+        query: {
+          "x-id": "PutObject",
+        },
+      });
+
+      await client.putObject({
+        Bucket: "b",
+        Key: "k",
+        Body: Buffer.from("abcd"),
+        ChecksumAlgorithm: "SHA256",
+      });
+
+      expect.hasAssertions();
+    });
+
+    it("should set flexible checksums (SHA1)", async () => {
+      const client = new S3({ region: "us-west-2" });
+
+      requireRequestsFrom(client).toMatch({
+        method: "PUT",
+        hostname: "s3.us-west-2.amazonaws.com",
+        protocol: "https:",
+        path: "/b/k",
+        headers: {
+          "content-type": "application/octet-stream",
+          "x-amz-sdk-checksum-algorithm": "SHA1",
+          "content-length": "4",
+          Expect: "100-continue",
+          "x-amz-checksum-sha1": "gf6L/odXbD7LIkJvjleEc4KRes8=",
+          host: "s3.us-west-2.amazonaws.com",
+          "x-amz-user-agent": /./,
+          "user-agent": /./,
+          "amz-sdk-invocation-id": /./,
+          "amz-sdk-request": /./,
+          "x-amz-date": /./,
+          "x-amz-security-token": /./,
+          authorization: /./,
+          // this is here even if algo is SHA1
+          "x-amz-content-sha256": "88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589",
+        },
+        query: {
+          "x-id": "PutObject",
+        },
+      });
+
+      await client.putObject({
+        Bucket: "b",
+        Key: "k",
+        Body: Buffer.from("abcd"),
+        ChecksumAlgorithm: "SHA1",
+      });
+
+      expect.hasAssertions();
+    });
+  });
+});
