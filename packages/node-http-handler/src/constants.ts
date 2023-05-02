824f5a71211da9324af2a430ffcf5e5b7fb9aeb7
Ran Vaknin <rvaknin@amazon.com>
2023-05-02 15:57:17 -0700

    fixed typo

--- packages/node-http-handler/src/constants.ts
+++ packages/node-http-handler/src/constants.ts
@@ -2,4 +2,4 @@
  * Node.js system error codes that indicate timeout.
  * @deprecated use NODEJS_TIMEOUT_ERROR_CODES from @aws-sdk/service-error-classification/constants
  */
-export const NODEJS_TIMEOUT_ERROR_CODES = ["ECONNRESET", "ECONNRESET", "EPIPE", "ETIMEDOUT"];
+export const NODEJS_TIMEOUT_ERROR_CODES = ["ECONNRESET", "ECONNREFUSED", "EPIPE", "ETIMEDOUT"];
