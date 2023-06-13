924a200e0cea23040cebb8eca8ff527bc88a2ecb
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-06-13 14:17:02 -0700

    test(credential-provider-imds): abort pending requests on nock while testing timeout

--- packages/credential-provider-imds/src/remoteProvider/httpRequest.spec.ts
+++ packages/credential-provider-imds/src/remoteProvider/httpRequest.spec.ts
@@ -114,7 +114,7 @@ describe("httpRequest", () => {
     );
     expect(requestSpy.mock.results[0].value.socket).toHaveProperty("destroyed", true);
 
-    await new Promise((resolve) => setTimeout(resolve, timeout * 2));
+    nock.abortPendingRequests();
     scope.done();
   });
 });
