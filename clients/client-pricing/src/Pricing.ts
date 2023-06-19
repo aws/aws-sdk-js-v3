947c8ba8dddbb3374199f0285b0a42f548abb62d
awstools <awstools@amazon.com>
2023-06-19 18:14:34 +0000

    feat(client-pricing): This release updates the PriceListArn regex pattern.

--- clients/client-pricing/src/Pricing.ts
+++ clients/client-pricing/src/Pricing.ts
@@ -113,22 +113,33 @@ export interface Pricing {
 
 /**
  * @public
- * <p>Amazon Web Services Price List API is a centralized and convenient way to
- *          programmatically query Amazon Web Services for services, products, and pricing information. The Amazon Web Services Price List
- *          uses standardized product attributes such as <code>Location</code>, <code>Storage
- *             Class</code>, and <code>Operating System</code>, and provides prices at the SKU
- *          level. You can use the Amazon Web Services Price List to build cost control and scenario planning tools, reconcile
- *          billing data, forecast future spend for budgeting purposes, and provide cost benefit
- *          analysis that compare your internal workloads with Amazon Web Services.</p>
- *          <p>Use <code>GetServices</code> without a service code to retrieve the service codes for all AWS services, then
- *          <code>GetServices</code> with a service code to retrieve the attribute names for
- *          that service. After you have the service code and attribute names, you can use <code>GetAttributeValues</code>
- *          to see what values are available for an attribute. With the service code and an attribute name and value,
- *          you can use <code>GetProducts</code> to find specific products that you're interested in, such as
+ * <p>The Amazon Web Services Price List API is a centralized and convenient way to programmatically
+ *          query Amazon Web Services for services, products, and pricing information. The Amazon Web Services Price List uses standardized product attributes such as <code>Location</code>,
+ *             <code>Storage Class</code>, and <code>Operating System</code>, and provides prices at
+ *          the SKU level. You can use the Amazon Web Services Price List to do the following:</p>
+ *          <ul>
+ *             <li>
+ *                <p>Build cost control and scenario planning tools</p>
+ *             </li>
+ *             <li>
+ *                <p>Reconcile billing data</p>
+ *             </li>
+ *             <li>
+ *                <p>Forecast future spend for budgeting purposes</p>
+ *             </li>
+ *             <li>
+ *                <p>Provide cost benefit analysis that compare your internal workloads with Amazon Web Services</p>
+ *             </li>
+ *          </ul>
+ *          <p>Use <code>GetServices</code> without a service code to retrieve the service codes for
+ *          all Amazon Web Services, then <code>GetServices</code> with a service code to
+ *          retrieve the attribute names for that service. After you have the service code and
+ *          attribute names, you can use <code>GetAttributeValues</code> to see what values are
+ *          available for an attribute. With the service code and an attribute name and value, you can
+ *          use <code>GetProducts</code> to find specific products that you're interested in, such as
  *          an <code>AmazonEC2</code> instance, with a <code>Provisioned IOPS</code>
  *             <code>volumeType</code>.</p>
- *          <p>Service Endpoint</p>
- *          <p>Amazon Web Services Price List service API provides the following two endpoints:</p>
+ *          <p>You can use the following endpoints for the Amazon Web Services Price List API:</p>
  *          <ul>
  *             <li>
  *                <p>https://api.pricing.us-east-1.amazonaws.com</p>
