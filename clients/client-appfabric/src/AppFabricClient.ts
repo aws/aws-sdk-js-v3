bfd0e0cd0ba59515cefbc340de59f6b071d5a003
awstools <awstools@amazon.com>
2023-06-27 18:25:31 +0000

    feat(client-appfabric): Initial release of AWS AppFabric for connecting SaaS applications for better productivity and security.

---
+++ clients/client-appfabric/src/AppFabricClient.ts
@@ -0,0 +1,407 @@
+// smithy-typescript generated code
+import { RegionInputConfig, RegionResolvedConfig, resolveRegionConfig } from "@aws-sdk/config-resolver";
+import { getContentLengthPlugin } from "@aws-sdk/middleware-content-length";
+import { EndpointInputConfig, EndpointResolvedConfig, resolveEndpointConfig } from "@aws-sdk/middleware-endpoint";
+import {
+  getHostHeaderPlugin,
+  HostHeaderInputConfig,
+  HostHeaderResolvedConfig,
+  resolveHostHeaderConfig,
+} from "@aws-sdk/middleware-host-header";
+import { getLoggerPlugin } from "@aws-sdk/middleware-logger";
+import { getRecursionDetectionPlugin } from "@aws-sdk/middleware-recursion-detection";
+import { getRetryPlugin, resolveRetryConfig, RetryInputConfig, RetryResolvedConfig } from "@aws-sdk/middleware-retry";
+import {
+  AwsAuthInputConfig,
+  AwsAuthResolvedConfig,
+  getAwsAuthPlugin,
+  resolveAwsAuthConfig,
+} from "@aws-sdk/middleware-signing";
+import {
+  getUserAgentPlugin,
+  resolveUserAgentConfig,
+  UserAgentInputConfig,
+  UserAgentResolvedConfig,
+} from "@aws-sdk/middleware-user-agent";
+import {
+  Client as __Client,
+  DefaultsMode as __DefaultsMode,
+  SmithyConfiguration as __SmithyConfiguration,
+  SmithyResolvedConfiguration as __SmithyResolvedConfiguration,
+} from "@aws-sdk/smithy-client";
+import {
+  BodyLengthCalculator as __BodyLengthCalculator,
+  Checksum as __Checksum,
+  ChecksumConstructor as __ChecksumConstructor,
+  Credentials as __Credentials,
+  EndpointV2 as __EndpointV2,
+  Hash as __Hash,
+  HashConstructor as __HashConstructor,
+  HttpHandlerOptions as __HttpHandlerOptions,
+  Logger as __Logger,
+  Provider,
+  UrlParser as __UrlParser,
+  UserAgent as __UserAgent,
+} from "@aws-sdk/types";
+import { HttpHandler as __HttpHandler } from "@smithy/protocol-http";
+import {
+  Decoder as __Decoder,
+  Encoder as __Encoder,
+  Provider as __Provider,
+  StreamCollector as __StreamCollector,
+} from "@smithy/types";
+
+import {
+  BatchGetUserAccessTasksCommandInput,
+  BatchGetUserAccessTasksCommandOutput,
+} from "./commands/BatchGetUserAccessTasksCommand";
+import {
+  ConnectAppAuthorizationCommandInput,
+  ConnectAppAuthorizationCommandOutput,
+} from "./commands/ConnectAppAuthorizationCommand";
+import {
+  CreateAppAuthorizationCommandInput,
+  CreateAppAuthorizationCommandOutput,
+} from "./commands/CreateAppAuthorizationCommand";
+import { CreateAppBundleCommandInput, CreateAppBundleCommandOutput } from "./commands/CreateAppBundleCommand";
+import { CreateIngestionCommandInput, CreateIngestionCommandOutput } from "./commands/CreateIngestionCommand";
+import {
+  CreateIngestionDestinationCommandInput,
+  CreateIngestionDestinationCommandOutput,
+} from "./commands/CreateIngestionDestinationCommand";
+import {
+  DeleteAppAuthorizationCommandInput,
+  DeleteAppAuthorizationCommandOutput,
+} from "./commands/DeleteAppAuthorizationCommand";
+import { DeleteAppBundleCommandInput, DeleteAppBundleCommandOutput } from "./commands/DeleteAppBundleCommand";
+import { DeleteIngestionCommandInput, DeleteIngestionCommandOutput } from "./commands/DeleteIngestionCommand";
+import {
+  DeleteIngestionDestinationCommandInput,
+  DeleteIngestionDestinationCommandOutput,
+} from "./commands/DeleteIngestionDestinationCommand";
+import {
+  GetAppAuthorizationCommandInput,
+  GetAppAuthorizationCommandOutput,
+} from "./commands/GetAppAuthorizationCommand";
+import { GetAppBundleCommandInput, GetAppBundleCommandOutput } from "./commands/GetAppBundleCommand";
+import { GetIngestionCommandInput, GetIngestionCommandOutput } from "./commands/GetIngestionCommand";
+import {
+  GetIngestionDestinationCommandInput,
+  GetIngestionDestinationCommandOutput,
+} from "./commands/GetIngestionDestinationCommand";
+import {
+  ListAppAuthorizationsCommandInput,
+  ListAppAuthorizationsCommandOutput,
+} from "./commands/ListAppAuthorizationsCommand";
+import { ListAppBundlesCommandInput, ListAppBundlesCommandOutput } from "./commands/ListAppBundlesCommand";
+import {
+  ListIngestionDestinationsCommandInput,
+  ListIngestionDestinationsCommandOutput,
+} from "./commands/ListIngestionDestinationsCommand";
+import { ListIngestionsCommandInput, ListIngestionsCommandOutput } from "./commands/ListIngestionsCommand";
+import {
+  ListTagsForResourceCommandInput,
+  ListTagsForResourceCommandOutput,
+} from "./commands/ListTagsForResourceCommand";
+import { StartIngestionCommandInput, StartIngestionCommandOutput } from "./commands/StartIngestionCommand";
+import {
+  StartUserAccessTasksCommandInput,
+  StartUserAccessTasksCommandOutput,
+} from "./commands/StartUserAccessTasksCommand";
+import { StopIngestionCommandInput, StopIngestionCommandOutput } from "./commands/StopIngestionCommand";
+import { TagResourceCommandInput, TagResourceCommandOutput } from "./commands/TagResourceCommand";
+import { UntagResourceCommandInput, UntagResourceCommandOutput } from "./commands/UntagResourceCommand";
+import {
+  UpdateAppAuthorizationCommandInput,
+  UpdateAppAuthorizationCommandOutput,
+} from "./commands/UpdateAppAuthorizationCommand";
+import {
+  UpdateIngestionDestinationCommandInput,
+  UpdateIngestionDestinationCommandOutput,
+} from "./commands/UpdateIngestionDestinationCommand";
+import {
+  ClientInputEndpointParameters,
+  ClientResolvedEndpointParameters,
+  EndpointParameters,
+  resolveClientEndpointParameters,
+} from "./endpoint/EndpointParameters";
+import { getRuntimeConfig as __getRuntimeConfig } from "./runtimeConfig";
+
+export { __Client };
+
+/**
+ * @public
+ */
+export type ServiceInputTypes =
+  | BatchGetUserAccessTasksCommandInput
+  | ConnectAppAuthorizationCommandInput
+  | CreateAppAuthorizationCommandInput
+  | CreateAppBundleCommandInput
+  | CreateIngestionCommandInput
+  | CreateIngestionDestinationCommandInput
+  | DeleteAppAuthorizationCommandInput
+  | DeleteAppBundleCommandInput
+  | DeleteIngestionCommandInput
+  | DeleteIngestionDestinationCommandInput
+  | GetAppAuthorizationCommandInput
+  | GetAppBundleCommandInput
+  | GetIngestionCommandInput
+  | GetIngestionDestinationCommandInput
+  | ListAppAuthorizationsCommandInput
+  | ListAppBundlesCommandInput
+  | ListIngestionDestinationsCommandInput
+  | ListIngestionsCommandInput
+  | ListTagsForResourceCommandInput
+  | StartIngestionCommandInput
+  | StartUserAccessTasksCommandInput
+  | StopIngestionCommandInput
+  | TagResourceCommandInput
+  | UntagResourceCommandInput
+  | UpdateAppAuthorizationCommandInput
+  | UpdateIngestionDestinationCommandInput;
+
+/**
+ * @public
+ */
+export type ServiceOutputTypes =
+  | BatchGetUserAccessTasksCommandOutput
+  | ConnectAppAuthorizationCommandOutput
+  | CreateAppAuthorizationCommandOutput
+  | CreateAppBundleCommandOutput
+  | CreateIngestionCommandOutput
+  | CreateIngestionDestinationCommandOutput
+  | DeleteAppAuthorizationCommandOutput
+  | DeleteAppBundleCommandOutput
+  | DeleteIngestionCommandOutput
+  | DeleteIngestionDestinationCommandOutput
+  | GetAppAuthorizationCommandOutput
+  | GetAppBundleCommandOutput
+  | GetIngestionCommandOutput
+  | GetIngestionDestinationCommandOutput
+  | ListAppAuthorizationsCommandOutput
+  | ListAppBundlesCommandOutput
+  | ListIngestionDestinationsCommandOutput
+  | ListIngestionsCommandOutput
+  | ListTagsForResourceCommandOutput
+  | StartIngestionCommandOutput
+  | StartUserAccessTasksCommandOutput
+  | StopIngestionCommandOutput
+  | TagResourceCommandOutput
+  | UntagResourceCommandOutput
+  | UpdateAppAuthorizationCommandOutput
+  | UpdateIngestionDestinationCommandOutput;
+
+/**
+ * @public
+ */
+export interface ClientDefaults extends Partial<__SmithyResolvedConfiguration<__HttpHandlerOptions>> {
+  /**
+   * The HTTP handler to use. Fetch in browser and Https in Nodejs.
+   */
+  requestHandler?: __HttpHandler;
+
+  /**
+   * A constructor for a class implementing the {@link @aws-sdk/types#ChecksumConstructor} interface
+   * that computes the SHA-256 HMAC or checksum of a string or binary buffer.
+   * @internal
+   */
+  sha256?: __ChecksumConstructor | __HashConstructor;
+
+  /**
+   * The function that will be used to convert strings into HTTP endpoints.
+   * @internal
+   */
+  urlParser?: __UrlParser;
+
+  /**
+   * A function that can calculate the length of a request body.
+   * @internal
+   */
+  bodyLengthChecker?: __BodyLengthCalculator;
+
+  /**
+   * A function that converts a stream into an array of bytes.
+   * @internal
+   */
+  streamCollector?: __StreamCollector;
+
+  /**
+   * The function that will be used to convert a base64-encoded string to a byte array.
+   * @internal
+   */
+  base64Decoder?: __Decoder;
+
+  /**
+   * The function that will be used to convert binary data to a base64-encoded string.
+   * @internal
+   */
+  base64Encoder?: __Encoder;
+
+  /**
+   * The function that will be used to convert a UTF8-encoded string to a byte array.
+   * @internal
+   */
+  utf8Decoder?: __Decoder;
+
+  /**
+   * The function that will be used to convert binary data to a UTF-8 encoded string.
+   * @internal
+   */
+  utf8Encoder?: __Encoder;
+
+  /**
+   * The runtime environment.
+   * @internal
+   */
+  runtime?: string;
+
+  /**
+   * Disable dynamically changing the endpoint of the client based on the hostPrefix
+   * trait of an operation.
+   */
+  disableHostPrefix?: boolean;
+
+  /**
+   * Unique service identifier.
+   * @internal
+   */
+  serviceId?: string;
+
+  /**
+   * Enables IPv6/IPv4 dualstack endpoint.
+   */
+  useDualstackEndpoint?: boolean | __Provider<boolean>;
+
+  /**
+   * Enables FIPS compatible endpoints.
+   */
+  useFipsEndpoint?: boolean | __Provider<boolean>;
+
+  /**
+   * The AWS region to which this client will send requests
+   */
+  region?: string | __Provider<string>;
+
+  /**
+   * Default credentials provider; Not available in browser runtime.
+   * @internal
+   */
+  credentialDefaultProvider?: (input: any) => __Provider<__Credentials>;
+
+  /**
+   * The provider populating default tracking information to be sent with `user-agent`, `x-amz-user-agent` header
+   * @internal
+   */
+  defaultUserAgentProvider?: Provider<__UserAgent>;
+
+  /**
+   * Value for how many times a request will be made at most in case of retry.
+   */
+  maxAttempts?: number | __Provider<number>;
+
+  /**
+   * Specifies which retry algorithm to use.
+   */
+  retryMode?: string | __Provider<string>;
+
+  /**
+   * Optional logger for logging debug/info/warn/error.
+   */
+  logger?: __Logger;
+
+  /**
+   * The {@link @aws-sdk/smithy-client#DefaultsMode} that will be used to determine how certain default configuration options are resolved in the SDK.
+   */
+  defaultsMode?: __DefaultsMode | __Provider<__DefaultsMode>;
+}
+
+/**
+ * @public
+ */
+export type AppFabricClientConfigType = Partial<__SmithyConfiguration<__HttpHandlerOptions>> &
+  ClientDefaults &
+  RegionInputConfig &
+  EndpointInputConfig<EndpointParameters> &
+  RetryInputConfig &
+  HostHeaderInputConfig &
+  AwsAuthInputConfig &
+  UserAgentInputConfig &
+  ClientInputEndpointParameters;
+/**
+ * @public
+ *
+ *  The configuration interface of AppFabricClient class constructor that set the region, credentials and other options.
+ */
+export interface AppFabricClientConfig extends AppFabricClientConfigType {}
+
+/**
+ * @public
+ */
+export type AppFabricClientResolvedConfigType = __SmithyResolvedConfiguration<__HttpHandlerOptions> &
+  Required<ClientDefaults> &
+  RegionResolvedConfig &
+  EndpointResolvedConfig<EndpointParameters> &
+  RetryResolvedConfig &
+  HostHeaderResolvedConfig &
+  AwsAuthResolvedConfig &
+  UserAgentResolvedConfig &
+  ClientResolvedEndpointParameters;
+/**
+ * @public
+ *
+ *  The resolved configuration interface of AppFabricClient class. This is resolved and normalized from the {@link AppFabricClientConfig | constructor configuration interface}.
+ */
+export interface AppFabricClientResolvedConfig extends AppFabricClientResolvedConfigType {}
+
+/**
+ * @public
+ * <p>Amazon Web Services AppFabric quickly connects software as a service (SaaS) applications across your
+ *          organization. This allows IT and security teams to easily manage and secure applications
+ *          using a standard schema, and employees can complete everyday tasks faster using generative
+ *          artificial intelligence (AI). You can use these APIs to complete AppFabric tasks, such as
+ *          setting up audit log ingestions or viewing user access. For more information about AppFabric,
+ *          including the required permissions to use the service, see the <a href="https://docs.aws.amazon.com/appfabric/latest/adminguide/">Amazon Web Services AppFabric Administration Guide</a>. For more
+ *          information about using the Command Line Interface (CLI) to manage your
+ *          AppFabric resources, see the <a href="https://docs.aws.amazon.com/cli/latest/reference/appfabric/index.html">AppFabric section of the CLI
+ *             Reference</a>.</p>
+ */
+export class AppFabricClient extends __Client<
+  __HttpHandlerOptions,
+  ServiceInputTypes,
+  ServiceOutputTypes,
+  AppFabricClientResolvedConfig
+> {
+  /**
+   * The resolved configuration of AppFabricClient class. This is resolved and normalized from the {@link AppFabricClientConfig | constructor configuration interface}.
+   */
+  readonly config: AppFabricClientResolvedConfig;
+
+  constructor(configuration: AppFabricClientConfig) {
+    const _config_0 = __getRuntimeConfig(configuration);
+    const _config_1 = resolveClientEndpointParameters(_config_0);
+    const _config_2 = resolveRegionConfig(_config_1);
+    const _config_3 = resolveEndpointConfig(_config_2);
+    const _config_4 = resolveRetryConfig(_config_3);
+    const _config_5 = resolveHostHeaderConfig(_config_4);
+    const _config_6 = resolveAwsAuthConfig(_config_5);
+    const _config_7 = resolveUserAgentConfig(_config_6);
+    super(_config_7);
+    this.config = _config_7;
+    this.middlewareStack.use(getRetryPlugin(this.config));
+    this.middlewareStack.use(getContentLengthPlugin(this.config));
+    this.middlewareStack.use(getHostHeaderPlugin(this.config));
+    this.middlewareStack.use(getLoggerPlugin(this.config));
+    this.middlewareStack.use(getRecursionDetectionPlugin(this.config));
+    this.middlewareStack.use(getAwsAuthPlugin(this.config));
+    this.middlewareStack.use(getUserAgentPlugin(this.config));
+  }
+
+  /**
+   * Destroy underlying resources, like sockets. It's usually not necessary to do this.
+   * However in Node.js, it's best to explicitly shut down the client's agent when it is no longer needed.
+   * Otherwise, sockets might stay open for quite a long time before the server terminates them.
+   */
+  destroy(): void {
+    super.destroy();
+  }
+}
---
+++ clients/client-appfabric/src/index.ts
@@ -0,0 +1,22 @@
+// smithy-typescript generated code
+/* eslint-disable */
+/**
+ * <p>Amazon Web Services AppFabric quickly connects software as a service (SaaS) applications across your
+ *          organization. This allows IT and security teams to easily manage and secure applications
+ *          using a standard schema, and employees can complete everyday tasks faster using generative
+ *          artificial intelligence (AI). You can use these APIs to complete AppFabric tasks, such as
+ *          setting up audit log ingestions or viewing user access. For more information about AppFabric,
+ *          including the required permissions to use the service, see the <a href="https://docs.aws.amazon.com/appfabric/latest/adminguide/">Amazon Web Services AppFabric Administration Guide</a>. For more
+ *          information about using the Command Line Interface (CLI) to manage your
+ *          AppFabric resources, see the <a href="https://docs.aws.amazon.com/cli/latest/reference/appfabric/index.html">AppFabric section of the CLI
+ *             Reference</a>.</p>
+ *
+ * @packageDocumentation
+ */
+export * from "./AppFabricClient";
+export * from "./AppFabric";
+export * from "./commands";
+export * from "./pagination";
+export * from "./models";
+
+export { AppFabricServiceException } from "./models/AppFabricServiceException";
