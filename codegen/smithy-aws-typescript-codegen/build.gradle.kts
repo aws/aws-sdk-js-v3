6b98455481c481e373852d33e4a689e1964a2ffd
Chase Coalwell <782571+srchase@users.noreply.github.com>
2023-07-03 17:14:31 -0600

    feat(codegen): bump codegen version to 0.17.0

--- codegen/smithy-aws-typescript-codegen/build.gradle.kts
+++ codegen/smithy-aws-typescript-codegen/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     api("software.amazon.smithy:smithy-protocol-test-traits:$smithyVersion")
     api("software.amazon.smithy:smithy-model:$smithyVersion")
     api("software.amazon.smithy:smithy-rules-engine:$smithyVersion")
-    api("software.amazon.smithy.typescript:smithy-typescript-codegen:0.16.0")
+    api("software.amazon.smithy.typescript:smithy-typescript-codegen:0.17.0")
 }
 
 tasks.register("set-aws-sdk-versions") {
