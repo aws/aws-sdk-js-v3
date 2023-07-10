e0751b49d5a07b4504283a6a34f9706789e446fc
Chase Coalwell <782571+srchase@users.noreply.github.com>
2023-07-10 14:56:51 -0600

    chore(packages): re-export types from @smithy/types (#4900)

--- packages/types/src/identity/Identity.ts
+++ packages/types/src/identity/Identity.ts
@@ -1,16 +1,1 @@
-/**
- * @public
- */
-export interface Identity {
-  /**
-   * A `Date` when the identity or credential will no longer be accepted.
-   */
-  readonly expiration?: Date;
-}
-
-/**
- * @public
- */
-export interface IdentityProvider<IdentityT extends Identity> {
-  (identityProperties?: Record<string, any>): Promise<IdentityT>;
-}
+export { Identity, IdentityProvider } from "@smithy/types";
--- packages/types/src/serde.ts
+++ packages/types/src/serde.ts
@@ -1,73 +1,15 @@
-import { Endpoint } from "./http";
-import { RequestHandler } from "./transfer";
-import { Decoder, Encoder, Provider } from "./util";
-
-/**
- * @public
- *
- * Interface for object requires an Endpoint set.
- */
-export interface EndpointBearer {
-  /* TODO(endpointsv2) post-release */
-  // Keep using Endpoint V1 interface in serde
-  // After all services have onboard EndpointV2, we need to migrate it to Endpoint V2 interface too.
-  endpoint: Provider<Endpoint>;
-}
-
-/**
- * @public
- */
-export interface StreamCollector {
-  /**
-   * A function that converts a stream into an array of bytes.
-   *
-   * @param stream - The low-level native stream from browser or Nodejs runtime
-   */
-  (stream: any): Promise<Uint8Array>;
-}
-
-/**
- * @public
- *
- * Request and Response serde util functions and settings for AWS services
- */
-export interface SerdeContext extends EndpointBearer {
-  base64Encoder: Encoder;
-  base64Decoder: Decoder;
-  utf8Encoder: Encoder;
-  utf8Decoder: Decoder;
-  streamCollector: StreamCollector;
-  requestHandler: RequestHandler<any, any>;
-  disableHostPrefix: boolean;
-}
-
-/**
- * @public
- */
-export interface RequestSerializer<Request, Context extends EndpointBearer = any> {
-  /**
-   * Converts the provided `input` into a request object
-   *
-   * @param input - The user input to serialize.
-   *
-   * @param context - Context containing runtime-specific util functions.
-   */
-  (input: any, context: Context): Promise<Request>;
-}
-
-/**
- * @public
- */
-export interface ResponseDeserializer<OutputType, ResponseType = any, Context = any> {
-  /**
-   * Converts the output of an operation into JavaScript types.
-   *
-   * @param output - The HTTP response received from the service
-   *
-   * @param context - context containing runtime-specific util functions.
-   */
-  (output: ResponseType, context: Context): Promise<OutputType>;
-}
+export {
+  EndpointBearer,
+  StreamCollector,
+  SerdeContext,
+  ResponseDeserializer,
+  RequestSerializer,
+  SdkStreamMixin,
+  SdkStream,
+  WithSdkStreamMixin,
+  SdkStreamMixinInjector,
+  SdkStreamSerdeContext,
+} from "@smithy/types";
 
 /**
  * @public
@@ -89,51 +31,4 @@ declare global {
    * @public
    */
   export interface Blob {}
-}
-
-/**
- * The interface contains mix-in utility functions to transfer the runtime-specific
- * stream implementation to specified format. Each stream can ONLY be transformed
- * once.
- */
-export interface SdkStreamMixin {
-  transformToByteArray: () => Promise<Uint8Array>;
-  transformToString: (encoding?: string) => Promise<string>;
-  transformToWebStream: () => ReadableStream;
-}
-
-/**
- * @public
- *
- * The type describing a runtime-specific stream implementation with mix-in
- * utility functions.
- */
-export type SdkStream<BaseStream> = BaseStream & SdkStreamMixin;
-
-/**
- * @public
- *
- * Indicates that the member of type T with
- * key StreamKey have been extended
- * with the SdkStreamMixin helper methods.
- */
-export type WithSdkStreamMixin<T, StreamKey extends keyof T> = {
-  [key in keyof T]: key extends StreamKey ? SdkStream<T[StreamKey]> : T[key];
-};
-
-/**
- * Interface for internal function to inject stream utility functions
- * implementation
- *
- * @internal
- */
-export interface SdkStreamMixinInjector {
-  (stream: unknown): SdkStreamMixin;
-}
-
-/**
- * @internal
- */
-export interface SdkStreamSerdeContext {
-  sdkStreamMixin: SdkStreamMixinInjector;
 }
