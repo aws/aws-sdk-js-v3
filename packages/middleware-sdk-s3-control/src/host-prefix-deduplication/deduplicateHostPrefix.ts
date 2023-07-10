2ffe660597360a266005dc9586ef6acef67c9db8
George Fu <kuhe@users.noreply.github.com>
2023-07-10 16:09:00 +0000

    fix(middleware-sdk-s3-control): ignore IP address in host prefix dedupe

--- packages/middleware-sdk-s3-control/src/host-prefix-deduplication/deduplicateHostPrefix.ts
+++ packages/middleware-sdk-s3-control/src/host-prefix-deduplication/deduplicateHostPrefix.ts
@@ -1,11 +1,16 @@
+import { isIpAddress } from "@aws-sdk/util-endpoints";
+
 /**
  * @example
  * 12345.12345.____.com should become 12345.____.com.
  */
 export const deduplicateHostPrefix = (hostname: string): string => {
-  const [prefix1, prefix2, ...rest] = hostname.split(".");
-  if (prefix1 === prefix2) {
-    return [prefix1, ...rest].join(".");
+  const [p1, p2, p3, p4, ...rest] = hostname.split(".");
+  if (isIpAddress(`${p1}.${p2}.${p3}.${parseInt(p4, 10)}`)) {
+    return hostname;
+  }
+  if (p1 === p2) {
+    return [p2, p3, p4, ...rest].join(".");
   }
   return hostname;
 };
