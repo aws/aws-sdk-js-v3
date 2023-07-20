72eb5981c9ae26beb893bc9399c94deba30cb1ad
awstools <awstools@amazon.com>
2023-07-20 18:25:53 +0000

    feat(client-s3): Improve performance of S3 clients by simplifying and optimizing endpoint resolution.

--- clients/client-s3/src/endpoint/EndpointParameters.ts
+++ clients/client-s3/src/endpoint/EndpointParameters.ts
@@ -24,6 +24,7 @@ export const resolveClientEndpointParameters = <T>(
     ...options,
     useFipsEndpoint: options.useFipsEndpoint ?? false,
     useDualstackEndpoint: options.useDualstackEndpoint ?? false,
+    forcePathStyle: options.forcePathStyle ?? false,
     useAccelerateEndpoint: options.useAccelerateEndpoint ?? false,
     useGlobalEndpoint: options.useGlobalEndpoint ?? false,
     disableMultiregionAccessPoints: options.disableMultiregionAccessPoints ?? false,
