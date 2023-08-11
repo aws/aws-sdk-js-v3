506f62f70825823762a58fd395eaf4f2afc43336
George Fu <kuhe@users.noreply.github.com>
2023-08-11 13:04:07 -0400

    chore(scripts): use main to start

--- scripts/prepare-smithy-typescript.sh
+++ scripts/prepare-smithy-typescript.sh
@@ -1,6 +1,6 @@
 # script to be run in the root of the checkout of awslabs/smithy-typescript.
 
-COMMIT=1f9fcad7015cdf639e98d4e951ceddc2a1ec11db
+COMMIT=main
 
 git fetch origin $COMMIT
 git checkout -f $COMMIT
