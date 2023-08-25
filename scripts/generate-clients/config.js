18912fc100c0b9cdc4f3e3378f27f1371b224bf7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-08-25 21:07:05 +0000

    fix(scripts): use full commit hash in client codegen

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,4 +1,4 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
-  SMITHY_TS_COMMIT: "d942a87",
+  SMITHY_TS_COMMIT: "d942a87081c0ca101f77b2336042773b26b0b9b1",
 };
