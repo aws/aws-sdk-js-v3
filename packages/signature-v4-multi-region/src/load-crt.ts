29d9ff902629fadef2866f4157c5f7fa293d4a6d
George Fu <kuhe@users.noreply.github.com>
2023-09-21 22:21:38 +0000

    chore(signature-v4-multi-region): add warning when dynamically loading CRT

--- packages/signature-v4-multi-region/src/load-crt.ts
+++ packages/signature-v4-multi-region/src/load-crt.ts
@@ -13,6 +13,20 @@ export function loadCrt(): void {
       const __require = require;
       const moduleName = "@aws-sdk/signature-v4-crt";
       __require.call(null, moduleName);
+
+      process.emitWarning(
+        `The package @aws-sdk/signature-v4-crt has been loaded dynamically.
+To avoid this warning, please explicitly import the package in your application with:
+
+import "@aws-sdk/signature-v4-crt"; // ESM
+require("@aws-sdk/signature-v4-crt"); // CJS
+
+In a future version of the AWS SDK for JavaScript (v3), this warning
+will become an error and dynamic loading will not be available.
+
+See https://github.com/aws/aws-sdk-js-v3/issues/5229.
+`
+      );
     }
   } catch (e) {
     // ignored.
