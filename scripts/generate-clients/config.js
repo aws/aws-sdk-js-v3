1ccfce17e1c96c83ba19cbe197c6973e7774495b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-10-09 06:33:58 -0700

    chore(clients): support reading ini subsections and get entire config in configSelector (#5330)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,5 +1,5 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "73d098dceb8490898154253f0f41bb876edb8cca",
+  SMITHY_TS_COMMIT: "5fb6d4c68777a3d358158eddcb18d22da3fd5c8d",
 };
