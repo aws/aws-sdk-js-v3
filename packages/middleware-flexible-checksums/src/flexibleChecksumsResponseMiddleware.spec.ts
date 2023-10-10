b347c027cccf8a7c0c5a46b8bc88bc4ce7e24f4c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-10 19:17:00 +0000

    test: skip response checksum cases

---
+++ packages/middleware-flexible-checksums/src/flexibleChecksumsResponseMiddleware.spec.ts
@@ -0,0 +1,73 @@
+import { HttpRequest } from "@smithy/protocol-http";
+import { BuildHandlerArguments } from "@smithy/types";
+
+import { PreviouslyResolved } from "./configuration";
+import { flexibleChecksumsResponseMiddleware } from "./flexibleChecksumsResponseMiddleware";
+import { FlexibleChecksumsMiddlewareConfig } from "./getFlexibleChecksumsPlugin";
+import { isStreaming } from "./isStreaming";
+import { validateChecksumFromResponse } from "./validateChecksumFromResponse";
+
+jest.mock("@smithy/protocol-http");
+jest.mock("./isStreaming");
+jest.mock("./validateChecksumFromResponse");
+
+describe(flexibleChecksumsResponseMiddleware.name, () => {
+  const mockNext = jest.fn();
+
+  const mockChecksum = "mockChecksum";
+  const mockChecksumAlgorithmFunction = jest.fn();
+  const mockChecksumLocationName = "mock-checksum-location-name";
+
+  const mockInput = {};
+  const mockConfig = {} as PreviouslyResolved;
+  const mockRequestValidationModeMember = "ChecksumEnabled";
+  const mockMiddlewareConfig = {
+    input: mockInput,
+    requestChecksumRequired: false,
+    requestValidationModeMember: mockRequestValidationModeMember,
+  };
+
+  const mockBody = { body: "mockRequestBody" };
+  const mockHeaders = { "content-length": 100, "content-encoding": "gzip" };
+  const mockRequest = { body: mockBody, headers: mockHeaders };
+  const mockArgs = { request: mockRequest } as BuildHandlerArguments<any>;
+  const mockResult = { response: { body: "mockResponsebody" } };
+
+  beforeEach(() => {
+    mockNext.mockResolvedValueOnce(mockResult);
+    const { isInstance } = HttpRequest;
+    (isInstance as unknown as jest.Mock).mockReturnValue(true);
+    (isStreaming as jest.Mock).mockReturnValue(false);
+  });
+
+  afterEach(() => {
+    expect(mockNext).toHaveBeenCalledTimes(1);
+    jest.clearAllMocks();
+  });
+
+  describe("skips", () => {
+    it("if not an instance of HttpRequest", async () => {
+      const { isInstance } = HttpRequest;
+      (isInstance as unknown as jest.Mock).mockReturnValue(false);
+      const handler = flexibleChecksumsResponseMiddleware(mockConfig, mockMiddlewareConfig)(mockNext, {});
+      await handler(mockArgs);
+      expect(validateChecksumFromResponse).not.toHaveBeenCalled();
+    });
+
+    describe("response checksum", () => {
+      it("if requestValidationModeMember is not defined", async () => {
+        const mockMwConfig = Object.assign({}, mockMiddlewareConfig) as FlexibleChecksumsMiddlewareConfig;
+        delete mockMwConfig.requestValidationModeMember;
+        const handler = flexibleChecksumsResponseMiddleware(mockConfig, mockMwConfig)(mockNext, {});
+        await handler(mockArgs);
+        expect(validateChecksumFromResponse).not.toHaveBeenCalled();
+      });
+
+      it("if requestValidationModeMember is not enabled in input", async () => {
+        const handler = flexibleChecksumsResponseMiddleware(mockConfig, mockMiddlewareConfig)(mockNext, {});
+        await handler({ ...mockArgs, input: mockInput });
+        expect(validateChecksumFromResponse).not.toHaveBeenCalled();
+      });
+    });
+  });
+});
