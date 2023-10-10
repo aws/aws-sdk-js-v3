d2cabc9adc475195cfe6c3054394b5d5080971b8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-10 22:11:23 +0000

    test: create mockOptions in validateChecksumFromResponse.spec.ts

--- packages/middleware-flexible-checksums/src/validateChecksumFromResponse.spec.ts
+++ packages/middleware-flexible-checksums/src/validateChecksumFromResponse.spec.ts
@@ -28,6 +28,12 @@ describe(validateChecksumFromResponse.name, () => {
 
   const mockChecksum = "mockChecksum";
   const mockResponseAlgorithms = [ChecksumAlgorithm.CRC32, ChecksumAlgorithm.CRC32C];
+
+  const mockOptions = {
+    config: mockConfig,
+    responseAlgorithms: mockResponseAlgorithms,
+  };
+
   const mockChecksumAlgorithmFn = jest.fn();
 
   const getMockResponseWithHeader = (headerKey: string, headerValue: string) => ({
@@ -58,26 +64,20 @@ describe(validateChecksumFromResponse.name, () => {
 
     it("if response algorithms is empty", async () => {
       const emptyAlgorithmsList = [];
-      await validateChecksumFromResponse(mockResponse, { config: mockConfig, responseAlgorithms: emptyAlgorithmsList });
+      await validateChecksumFromResponse(mockResponse, { ...mockOptions, responseAlgorithms: emptyAlgorithmsList });
       expect(getChecksumAlgorithmListForResponse).toHaveBeenCalledWith(emptyAlgorithmsList);
       expect(getChecksumLocationName).not.toHaveBeenCalled();
     });
 
     it("if updated algorithm list from response is empty", async () => {
       (getChecksumAlgorithmListForResponse as jest.Mock).mockImplementation(() => []);
-      await validateChecksumFromResponse(mockResponse, {
-        config: mockConfig,
-        responseAlgorithms: mockResponseAlgorithms,
-      });
+      await validateChecksumFromResponse(mockResponse, mockOptions);
       expect(getChecksumAlgorithmListForResponse).toHaveBeenCalledWith(mockResponseAlgorithms);
       expect(getChecksumLocationName).not.toHaveBeenCalled();
     });
 
     it("if checksum is not present in header", async () => {
-      await validateChecksumFromResponse(mockResponse, {
-        config: mockConfig,
-        responseAlgorithms: mockResponseAlgorithms,
-      });
+      await validateChecksumFromResponse(mockResponse, mockOptions);
       expect(getChecksumAlgorithmListForResponse).toHaveBeenCalledWith(mockResponseAlgorithms);
       expect(getChecksumLocationName).toHaveBeenCalledTimes(mockResponseAlgorithms.length);
     });
@@ -92,20 +92,14 @@ describe(validateChecksumFromResponse.name, () => {
 
     it("when checksum is populated for first algorithm", async () => {
       const responseWithChecksum = getMockResponseWithHeader(mockResponseAlgorithms[0], mockChecksum);
-      await validateChecksumFromResponse(responseWithChecksum, {
-        config: mockConfig,
-        responseAlgorithms: mockResponseAlgorithms,
-      });
+      await validateChecksumFromResponse(responseWithChecksum, mockOptions);
       expect(getChecksumLocationName).toHaveBeenCalledTimes(1);
       expect(getChecksumLocationName).toHaveBeenCalledWith(mockResponseAlgorithms[0]);
     });
 
     it("when checksum is populated for second algorithm", async () => {
       const responseWithChecksum = getMockResponseWithHeader(mockResponseAlgorithms[1], mockChecksum);
-      await validateChecksumFromResponse(responseWithChecksum, {
-        config: mockConfig,
-        responseAlgorithms: mockResponseAlgorithms,
-      });
+      await validateChecksumFromResponse(responseWithChecksum, mockOptions);
       expect(getChecksumLocationName).toHaveBeenCalledTimes(2);
       expect(getChecksumLocationName).toHaveBeenNthCalledWith(1, mockResponseAlgorithms[0]);
       expect(getChecksumLocationName).toHaveBeenNthCalledWith(2, mockResponseAlgorithms[1]);
@@ -116,10 +110,7 @@ describe(validateChecksumFromResponse.name, () => {
     const incorrectChecksum = "incorrectChecksum";
     const responseWithChecksum = getMockResponseWithHeader(mockResponseAlgorithms[0], incorrectChecksum);
     try {
-      await validateChecksumFromResponse(responseWithChecksum, {
-        config: mockConfig,
-        responseAlgorithms: mockResponseAlgorithms,
-      });
+      await validateChecksumFromResponse(responseWithChecksum, mockOptions);
       fail("should throw checksum mismatch error");
     } catch (error) {
       expect(error.message).toMatch(
