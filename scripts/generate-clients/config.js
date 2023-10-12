39126a49b1750fabd059a3cb2abd3b89917d6b02
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-10-11 22:35:32 -0700

    chore: read service specific endpoints from env/config (#5350)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,5 +1,5 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "5fb6d4c68777a3d358158eddcb18d22da3fd5c8d",
+  SMITHY_TS_COMMIT: "fcc0c4602ff33b3f3e00e4ced42f85ffa57e38bc",
 };
