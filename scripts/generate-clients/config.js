4231bbddd921649b3190f53a448484d5f1784679
George Fu <kuhe@users.noreply.github.com>
2023-10-13 15:19:27 +0000

    chore(codegen): update smithy commit to closed enums

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,5 +1,5 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "fcc0c4602ff33b3f3e00e4ced42f85ffa57e38bc",
+  SMITHY_TS_COMMIT: "d7f5cda69cb2bffd7491266be8a5f864efb887ba",
 };
