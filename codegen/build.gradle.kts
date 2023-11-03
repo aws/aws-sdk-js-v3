6ec9eee486b2843f68b923238d429d16cfd956c0
Hunter Mellema <hpm@amazon.com>
2023-11-03 08:36:34 -0600

    chore(codegen): bump codegen version to 0.19.0

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -28,7 +28,7 @@ allprojects {
         mavenCentral()
     }
     group = "software.amazon.smithy.typescript"
-    version = "0.18.0"
+    version = "0.19.0"
 }
 
 // The root project doesn't produce a JAR.
