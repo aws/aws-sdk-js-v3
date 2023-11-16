516e74138f8d4e6c779fe21009e1bb189e3213b8
Steven Yuan <yuasteve@amazon.com>
2023-11-15 18:32:55 -0800

    chore(codegen): update smithy-typescript commit

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,5 +1,5 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "b4ff4377ebaf5f51b89e6e98cdbb4b205f69594b",
+  SMITHY_TS_COMMIT: "dd4c267ccdc0c9d0097eaa6d3d9bfaa947038566",
 };
