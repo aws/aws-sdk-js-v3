c04cf658d7d01cb303541fa59a34bdc5b3139ad6
Steven Yuan <yuasteve@amazon.com>
2023-11-16 14:30:52 -0800

    feat(experimentalIdentityAndAuth): update `@httpBearerAuth` customizations

--- codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/auth/http/integration/AwsCustomizeHttpBearerTokenAuthPlugin.java
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/auth/http/integration/AwsCustomizeHttpBearerTokenAuthPlugin.java
@@ -5,8 +5,12 @@
 
 package software.amazon.smithy.aws.typescript.codegen.auth.http.integration;
 
+import static software.amazon.smithy.aws.typescript.codegen.AwsTraitsUtils.isAwsService;
+
 import java.util.List;
 import software.amazon.smithy.aws.typescript.codegen.AwsDependency;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.shapes.ServiceShape;
 import software.amazon.smithy.model.traits.HttpBearerAuthTrait;
 import software.amazon.smithy.typescript.codegen.LanguageTarget;
 import software.amazon.smithy.typescript.codegen.TypeScriptSettings;
@@ -41,29 +45,37 @@ public final class AwsCustomizeHttpBearerTokenAuthPlugin implements HttpAuthType
     }
 
     @Override
-    public void customizeSupportedHttpAuthSchemes(SupportedHttpAuthSchemesIndex supportedHttpAuthSchemesIndex) {
-        HttpAuthScheme authScheme = supportedHttpAuthSchemesIndex.getHttpAuthScheme(HttpBearerAuthTrait.ID).toBuilder()
-            // Current behavior of unconfigured `token` is to throw an error.
-            // This may need to be customized if a service is released with multiple auth schemes.
-            .putDefaultIdentityProvider(LanguageTarget.BROWSER, w ->
-                w.write("async () => { throw new Error(\"`token` is missing\"); }"))
-            // Use `@aws-sdk/token-providers` as the default identity provider chain for Node.js
-            .putDefaultIdentityProvider(LanguageTarget.NODE, w -> {
-                w.addDependency(AwsDependency.TOKEN_PROVIDERS);
-                w.addImport("nodeProvider", null, AwsDependency.TOKEN_PROVIDERS);
-                w.write("async (idProps) => await nodeProvider(idProps?.__config || {})(idProps)");
-            })
-            // Add __config as an identityProperty for backward compatibility of the `nodeProvider` default provider
-            .propertiesExtractor(s -> w -> w.write("""
-                (__config, context) => ({
-                    /**
-                     * @internal
-                     */
-                    identityProperties: {
-                        __config,
-                    },
-                }),"""))
-            .build();
-        supportedHttpAuthSchemesIndex.putHttpAuthScheme(authScheme.getSchemeId(), authScheme);
+    public void customizeSupportedHttpAuthSchemes(
+        SupportedHttpAuthSchemesIndex supportedHttpAuthSchemesIndex,
+        Model model,
+        TypeScriptSettings settings
+    ) {
+        ServiceShape service = settings.getService(model);
+        if (isAwsService(service)) {
+            HttpAuthScheme authScheme = supportedHttpAuthSchemesIndex.getHttpAuthScheme(HttpBearerAuthTrait.ID)
+                .toBuilder()
+                // Current behavior of unconfigured `token` is to throw an error.
+                // This may need to be customized if a service is released with multiple auth schemes.
+                .putDefaultIdentityProvider(LanguageTarget.BROWSER, w ->
+                    w.write("async () => { throw new Error(\"`token` is missing\"); }"))
+                // Use `@aws-sdk/token-providers` as the default identity provider chain for Node.js
+                .putDefaultIdentityProvider(LanguageTarget.NODE, w -> {
+                    w.addDependency(AwsDependency.TOKEN_PROVIDERS);
+                    w.addImport("nodeProvider", null, AwsDependency.TOKEN_PROVIDERS);
+                    w.write("async (idProps) => await nodeProvider(idProps?.__config || {})(idProps)");
+                })
+                // Add __config as an identityProperty for backward compatibility of the `nodeProvider` default provider
+                .propertiesExtractor(s -> w -> w.write("""
+                    (__config, context) => ({
+                        /**
+                         * @internal
+                         */
+                        identityProperties: {
+                            __config,
+                        },
+                    }),"""))
+                .build();
+            supportedHttpAuthSchemesIndex.putHttpAuthScheme(authScheme.getSchemeId(), authScheme);
+        }
     }
 }
