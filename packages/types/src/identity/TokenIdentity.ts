77f3ef67f6d624bd45ca69ca1dd998f8a5fa4145
Chase Coalwell <782571+srchase@users.noreply.github.com>
2023-11-27 15:33:23 -0700

    chore(packages): re-export TokenIdentity types (#5524)

--- packages/types/src/identity/TokenIdentity.ts
+++ packages/types/src/identity/TokenIdentity.ts
@@ -1,16 +1,1 @@
-import { Identity, IdentityProvider } from "./Identity";
-
-/**
- * @public
- */
-export interface TokenIdentity extends Identity {
-  /**
-   * The literal token string
-   */
-  readonly token: string;
-}
-
-/**
- * @public
- */
-export type TokenIdentityProvider = IdentityProvider<TokenIdentity>;
+export { TokenIdentity, TokenIdentityProvider } from "@smithy/types";
