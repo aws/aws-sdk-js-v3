002d8a15e03e1f839b38371861316c3c30b968cb
nick bouldien <nick.bouldien@gmail.com>
2023-12-06 10:45:19 -0500

    fix typo in jsdoc example
    use the imported fromContainerMetadata instead of fromInstanceMetadata

--- packages/credential-providers/src/fromContainerMetadata.ts
+++ packages/credential-providers/src/fromContainerMetadata.ts
@@ -14,7 +14,7 @@ export interface RemoteProviderInit extends _RemoteProviderInit {}
  * // const { fromContainerMetadata } = require("@aws-sdk/credential-providers"); // CommonJS import
  *
  * const foo = new FooClient({
- *   credentials: fromInstanceMetadata({
+ *   credentials: fromContainerMetadata({
  *     // Optional. The connection timeout (in milliseconds) to apply to any remote requests. If not specified, a default value
  *     // of`1000` (one second) is used.
  *     timeout: 1000,
