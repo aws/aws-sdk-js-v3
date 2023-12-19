0efc260005a140810710368eb14e8add61fe5445
George Fu <kuhe@users.noreply.github.com>
2023-12-19 20:26:02 +0000

    chore: update smithy hash

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,5 +1,5 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "9485a7319aefdc44ff8cdb06402a6e486450c78d",
+  SMITHY_TS_COMMIT: "f0ff3b7cdb93ce607c3873bab9a553566e0aec0b",
 };
