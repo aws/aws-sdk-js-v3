d84e349e9df7ef41daa6d0de799be4e4882d01e3
George Fu <kuhe@users.noreply.github.com>
2023-12-22 20:54:56 +0000

    fix(token-providers): script writeFile

--- packages/token-providers/scripts/nocheck.js
+++ packages/token-providers/scripts/nocheck.js
@@ -1,32 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 
-let root = path.join(__dirname, "..");
-try {
-  addNoCheck();
-} catch (e) {
-  // potential ENAMETOOLONG error in CI, we then use the package relative root.
-  root = "";
-  addNoCheck();
-}
+const root = path.join(__dirname, "..");
 
-function addNoCheck() {
-  const browserFile = path.join(root, "dist-types", "bundle", "client-sso-oidc-browser.d.ts");
-  const nodeFile = path.join(root, "dist-types", "bundle", "client-sso-oidc-node.d.ts");
+const browserFile = path.join(root, "dist-types", "bundle", "client-sso-oidc-browser.d.ts");
+const nodeFile = path.join(root, "dist-types", "bundle", "client-sso-oidc-node.d.ts");
 
-  const noCheck = "// @ts-nocheck\n";
+const noCheck = "// @ts-nocheck\n";
 
-  for (const file of [browserFile, nodeFile]) {
-    if (fs.existsSync(file)) {
-      const contents = fs.readFileSync(file, "utf-8");
-      if (!contents.startsWith(noCheck)) {
-        fs.writeFileSync(noCheck + contents, "utf-8");
-        console.info("Rewriting", file, "with ts-nocheck.");
-      } else {
-        console.info("No-op: ", file, "already starts with ts-nocheck.");
-      }
+for (const file of [browserFile, nodeFile]) {
+  if (fs.existsSync(file)) {
+    const contents = fs.readFileSync(file, "utf-8");
+    if (!contents.startsWith(noCheck)) {
+      fs.writeFileSync(file, noCheck + contents, "utf-8");
+      console.info("Rewriting", file, "with ts-nocheck.");
     } else {
-      throw new Error("File not found: " + file);
+      console.info("No-op: ", file, "already starts with ts-nocheck.");
     }
+  } else {
+    throw new Error("File not found: " + file);
   }
 }
