e6e582420615dc398396f1046461874fb2a3ca3d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-12-27 20:48:37 +0000

    chore: add NODE_DISABLE_REQUEST_COMPRESSION_CONFIG_OPTIONS

--- packages/middleware-compression/src/index.ts
+++ packages/middleware-compression/src/index.ts
@@ -1,3 +1,4 @@
+export * from "./NODE_DISABLE_REQUEST_COMPRESSION_CONFIG_OPTIONS";
 export * from "./compressionMiddleware";
 export * from "./configurations";
 export * from "./resolveCompressionConfig";
