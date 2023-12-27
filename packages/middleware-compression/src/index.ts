dbf0ed515ad7479f6f8f81efce36f910365b14ae
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-12-27 21:40:08 +0000

    chore: add NODE_REQUEST_MIN_COMPRESSION_SIZE_BYTES_CONFIG_OPTIONS

--- packages/middleware-compression/src/index.ts
+++ packages/middleware-compression/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./NODE_DISABLE_REQUEST_COMPRESSION_CONFIG_OPTIONS";
+export * from "./NODE_REQUEST_MIN_COMPRESSION_SIZE_BYTES_CONFIG_OPTIONS";
 export * from "./compressionMiddleware";
 export * from "./configurations";
 export * from "./resolveCompressionConfig";
