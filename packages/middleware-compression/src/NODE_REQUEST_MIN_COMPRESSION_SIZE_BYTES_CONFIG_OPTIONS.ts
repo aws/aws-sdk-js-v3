6aff70058533f567b99223b61dc5486382dbcbc8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-12-29 09:17:24 +0000

    chore: use numberSelector from @smithy/util-config-provider

--- packages/middleware-compression/src/NODE_REQUEST_MIN_COMPRESSION_SIZE_BYTES_CONFIG_OPTIONS.ts
+++ packages/middleware-compression/src/NODE_REQUEST_MIN_COMPRESSION_SIZE_BYTES_CONFIG_OPTIONS.ts
@@ -1,5 +1,5 @@
 import { LoadedConfigSelectors } from "@smithy/node-config-provider";
-import { SelectorType } from "@smithy/util-config-provider";
+import { numberSelector, SelectorType } from "@smithy/util-config-provider";
 
 /**
  * @internal
@@ -10,21 +10,6 @@ export const NODE_REQUEST_MIN_COMPRESSION_SIZE_BYTES_ENV_NAME = "AWS_REQUEST_MIN
  * @internal
  */
 export const NODE_REQUEST_MIN_COMPRESSION_SIZE_BYTES_INI_NAME = "request_min_compression_size_bytes";
-
-/**
- * ToDo: switch to using numberSelector from @smithy/util-config-provider once available.
- * https://github.com/smithy-lang/smithy-typescript/pull/1126
- */
-const numberSelector = (obj: Record<string, string | undefined>, key: string, type: SelectorType) => {
-  if (!(key in obj)) return undefined;
-
-  const numberValue = parseInt(obj[key] as string, 10);
-  if (Number.isNaN(numberValue)) {
-    throw new TypeError(`Cannot load ${type} '${key}'. Expected number, got '${obj[key]}'.`);
-  }
-
-  return numberValue;
-};
 
 /**
  * @internal
