6ff6d7a697208ab955df8011a6f51a2b7e25392c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-12-29 08:35:04 +0000

    chore: bump smithy-ts commit to include util-config-provider numberSelector

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,5 +1,5 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "2df620b6882388ce4773eb4ea5a012eb44b001f8",
+  SMITHY_TS_COMMIT: "cb044e77827c9f97b75167be61b6d39beb03ad1e",
 };
