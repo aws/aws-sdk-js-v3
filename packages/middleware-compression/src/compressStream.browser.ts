9445ad4e685d1d5916cee8344dd0cdc58bc2868f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-12-29 07:39:03 +0000

    chore: add compression.browser.ts

--- packages/middleware-compression/src/compressStream.browser.ts
+++ packages/middleware-compression/src/compressStream.browser.ts
@@ -1,2 +1,35 @@
-export const compressStream = async (body: ReadableStream): Promise<ReadableStream> =>
-  body.pipeThrough(new CompressionStream("gzip"));
+import { AsyncGzip } from "fflate";
+
+export const compressStream = async (body: ReadableStream): Promise<ReadableStream> => {
+  let endCallback: () => void;
+  const asyncGzip = new AsyncGzip();
+
+  // Replace with Compression Streams API once supported in all browsers.
+  // https://developer.mozilla.org/en-US/docs/Web/API/Compression_Streams_API
+  const compressionStream = new TransformStream({
+    start(controller) {
+      asyncGzip.ondata = (err, dat, final) => {
+        if (err) {
+          controller.error(err);
+        } else {
+          controller.enqueue(dat);
+          if (final) {
+            if (endCallback) endCallback();
+            else controller.terminate();
+          }
+        }
+      };
+    },
+    transform(chunk) {
+      asyncGzip.push(chunk);
+    },
+    flush() {
+      return new Promise((resolve) => {
+        endCallback = resolve;
+        asyncGzip.push(new Uint8Array(0), true);
+      });
+    },
+  });
+
+  return body.pipeThrough(compressionStream);
+};
