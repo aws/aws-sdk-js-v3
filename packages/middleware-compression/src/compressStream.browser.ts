05fa55e4ffaffb647938c4ce8027f7ef1f0b6ff0
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-12-29 08:31:20 +0000

    test: compressStream.browser.spec.ts

--- packages/middleware-compression/src/compressStream.browser.ts
+++ packages/middleware-compression/src/compressStream.browser.ts
@@ -8,11 +8,11 @@ export const compressStream = async (body: ReadableStream): Promise<ReadableStre
   // https://developer.mozilla.org/en-US/docs/Web/API/Compression_Streams_API
   const compressionStream = new TransformStream({
     start(controller) {
-      asyncGzip.ondata = (err, dat, final) => {
+      asyncGzip.ondata = (err, data, final) => {
         if (err) {
           controller.error(err);
         } else {
-          controller.enqueue(dat);
+          controller.enqueue(data);
           if (final) {
             if (endCallback) endCallback();
             else controller.terminate();
