7a28cab0896b8d5cc380bac95f9264668c40fd03
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-01-11 09:15:16 -0800

    chore: delete source code for middleware-compression (#5673)

--- packages/middleware-compression/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./NODE_DISABLE_REQUEST_COMPRESSION_CONFIG_OPTIONS";
-export * from "./NODE_REQUEST_MIN_COMPRESSION_SIZE_BYTES_CONFIG_OPTIONS";
-export * from "./compressionMiddleware";
-export * from "./configurations";
-export * from "./getCompressionPlugin";
-export * from "./resolveCompressionConfig";
