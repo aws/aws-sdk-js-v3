0ad2379ca7ddb82eca3420c56d91a59fd21a7966
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-01-11 16:56:30 +0000

    chore: delete source code for middleware-compression

--- packages/middleware-compression/src/compressStream.ts
+++
@@ -1,4 +0,0 @@
-import { Readable } from "stream";
-import { createGzip } from "zlib";
-
-export const compressStream = async (body: Readable): Promise<Readable> => body.pipe(createGzip());
--- packages/middleware-compression/src/constants.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Compression Algorithms supported by the SDK.
- */
-export enum CompressionAlgorithm {
-  GZIP = "gzip",
-}
-
-export const CLIENT_SUPPORTED_ALGORITHMS: CompressionAlgorithm[] = [CompressionAlgorithm.GZIP];
