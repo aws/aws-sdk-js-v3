175169260726c018a5049069ee062f6ef98275d7
George Fu <kuhe@users.noreply.github.com>
2024-01-31 12:25:35 -0500

    chore: set peer range during update:versions:default (#5748)

--- scripts/update-versions/default.mjs
+++ scripts/update-versions/default.mjs
@@ -4,6 +4,8 @@
 // in dependencies/devDependencies/peerDependencies
 
 import { getDepToDefaultVersionHash } from "./getDepToDefaultVersionHash.mjs";
+import { runUpdatePeers } from "./peers.mjs";
 import { updateVersions } from "./updateVersions.mjs";
 
 updateVersions(getDepToDefaultVersionHash());
+runUpdatePeers();
