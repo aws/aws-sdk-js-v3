1453f2dcc6c07eedc61969d56d76701750f391bc
Miles Ziemer <mziemer@amazon.com>
2024-02-28 16:00:31 -0500

    chore: update smithy-ts commit hash

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "3357fda448ea8cee3f7bfa7caeda2768a9751f8f",
+  SMITHY_TS_COMMIT: "64cd9c816b84040a20de65b7efd8bd8982873333",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
