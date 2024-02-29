6358dccb86fe9cca052ad81a6b573e53a1178a62
siddsriv <srivastava41099@gmail.com>
2024-02-29 18:06:29 +0000

    chore(ec2-metadata-service): add index.ts

---
+++ packages/ec2-metadata-service/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./MetadataService";
+export * from "./EndpointMode";
+export * from "./getEndpoint";
+export * from "./getEndpointConfigOptions";
+export * from "./getEndpointModeConfigOptions";
+export * from "./getMetadataServiceEndpoint";
