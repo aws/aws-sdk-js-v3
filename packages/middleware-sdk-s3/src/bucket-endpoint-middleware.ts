f05b54007141a36d7d364a4ca9a0864ab4a84b4f
George Fu <kuhe@users.noreply.github.com>
2024-02-29 12:06:07 -0500

    fix(middleware-sdk-s3): restore bucketEndpoint configurability (#5840)
    * fix(middleware-sdk-s3): restore bucketEndpoint configurability
    
    * test: fix tests

---
+++ packages/middleware-sdk-s3/src/bucket-endpoint-middleware.ts
@@ -0,0 +1,56 @@
+import {
+  HandlerExecutionContext,
+  MetadataBearer,
+  RelativeMiddlewareOptions,
+  SerializeHandler,
+  SerializeHandlerArguments,
+  SerializeHandlerOutput,
+  SerializeMiddleware,
+} from "@smithy/types";
+
+interface PreviouslyResolved {
+  bucketEndpoint?: boolean;
+}
+
+/**
+ * @internal
+ */
+export function bucketEndpointMiddleware(options: PreviouslyResolved): SerializeMiddleware<any, any> {
+  return <Output extends MetadataBearer>(
+      next: SerializeHandler<any, Output>,
+      context: HandlerExecutionContext
+    ): SerializeHandler<any, Output> =>
+    async (args: SerializeHandlerArguments<any>): Promise<SerializeHandlerOutput<Output>> => {
+      if (options.bucketEndpoint) {
+        const endpoint = context.endpointV2;
+        if (endpoint) {
+          const bucket: string | undefined = args.input.Bucket;
+          if (typeof bucket === "string") {
+            try {
+              const bucketEndpointUrl = new URL(bucket);
+              endpoint.url = bucketEndpointUrl;
+            } catch (e) {
+              const warning = `@aws-sdk/middleware-sdk-s3: bucketEndpoint=true was set but Bucket=${bucket} could not be parsed as URL.`;
+              if (context.logger?.constructor?.name === "NoOpLogger") {
+                console.warn(warning);
+              } else {
+                context.logger?.warn?.(warning);
+              }
+              throw e;
+            }
+          }
+        }
+      }
+      return next(args);
+    };
+}
+
+/**
+ * @internal
+ */
+export const bucketEndpointMiddlewareOptions: RelativeMiddlewareOptions = {
+  name: "bucketEndpointMiddleware",
+  override: true,
+  relation: "after",
+  toMiddleware: "endpointV2Middleware",
+};
--- packages/middleware-sdk-s3/src/middleware-sdk-s3.integ.spec.ts
+++ packages/middleware-sdk-s3/src/middleware-sdk-s3.integ.spec.ts
@@ -57,5 +57,31 @@ describe("middleware-sdk-s3", () => {
 
       expect.hasAssertions();
     });
+
+    it("allows using a bucket input value as the endpoint", async () => {
+      const client = new S3({
+        region: "us-west-2",
+        bucketEndpoint: true,
+      });
+
+      requireRequestsFrom(client).toMatch({
+        query: { "x-id": "PutObject" },
+        protocol: "https:",
+        hostname: "mybucket.com",
+        port: 8888,
+        path: "/my-bucket-path/my-key",
+        headers: {
+          host: "mybucket.com:8888",
+        },
+      });
+
+      await client.putObject({
+        Bucket: "https://mybucket.com:8888/my-bucket-path",
+        Key: "my-key",
+        Body: "abcd",
+      });
+
+      expect.hasAssertions();
+    });
   });
 });
