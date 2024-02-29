9e98b5cdddbcddc6615627af8f87d70b0ead60a3
siddsriv <srivastava41099@gmail.com>
2024-02-29 17:33:53 +0000

    feat(ec2-metadata-service): adding requestWithToken and fetchImdsJson

--- packages/ec2-metadata-service/src/MetadataService.spec.ts
+++ packages/ec2-metadata-service/src/MetadataService.spec.ts
@@ -17,4 +17,22 @@ describe("MetadataService", () => {
     const response = await metadataService.fetchMetadataToken();
     expect(response).toEqual({});
   });
+
+  it("should successfully make a request with token and return response body", async () => {
+    const path = "latest/meta-data/hostname";
+    const response = await metadataService.requestWithToken(path, {});
+    expect(response).toEqual({});
+  });
+
+  it("should successfully make a request with token and fetch IMDS text in JSON", async () => {
+    const path = "latest/meta-data/instance-type";
+    const response = await metadataService.fetchImdsJson(path);
+    expect(response).toEqual({});
+  });
+
+  it("should successfully make a request with token and return the instance identity document", async () => {
+    const path = "/latest/dynamic/instance-identity/document";
+    const response = await metadataService.requestWithToken(path);
+    expect(response).toEqual({});
+  });
 });
--- packages/ec2-metadata-service/src/MetadataService.ts
+++ packages/ec2-metadata-service/src/MetadataService.ts
@@ -46,7 +46,7 @@ export class MetadataService {
     try {
       const { response } = await handler.handle(request, {} as HttpHandlerOptions);
       if (response.statusCode === 200 && response.body) {
-        // Directly read the response body as plaintext
+        // handle response.body as stream
         return sdkStreamMixin(response.body).transformToString();
       } else {
         throw new Error(`Request failed with status code ${response.statusCode}`);
@@ -72,7 +72,7 @@ export class MetadataService {
     try {
       const { response } = await handler.handle(tokenRequest, {} as HttpHandlerOptions);
       if (response.statusCode === 200 && response.body) {
-        // Directly read the response body as plaintext
+        // Handle response.body as a stream
         return sdkStreamMixin(response.body).transformToString();
       } else {
         throw new Error(`Failed to fetch metadata token with status code ${response.statusCode}`);
@@ -80,5 +80,40 @@ export class MetadataService {
     } catch (error) {
       throw new Error(`Error fetching metadata token: ${error}`);
     }
+  }
+
+  async requestWithToken(
+    path: string,
+    options?: { method?: string; headers?: Record<string, string> }
+  ): Promise<string> {
+    const token = await this.fetchMetadataToken();
+    const endpointUrl = new URL(this.endpoint);
+    const handler = new NodeHttpHandler();
+    const request = new HttpRequest({
+      method: options?.method || "GET",
+      headers: {
+        ...options?.headers,
+        "X-aws-ec2-metadata-token": token,
+      },
+      hostname: endpointUrl.hostname,
+      path: endpointUrl.pathname + path,
+      protocol: endpointUrl.protocol,
+    });
+
+    try {
+      const { response } = await handler.handle(request, {} as HttpHandlerOptions);
+      if (response.statusCode === 200 && response.body) {
+        return sdkStreamMixin(response.body).transformToString();
+      } else {
+        throw new Error(`Request failed with status code: ${response.statusCode}`);
+      }
+    } catch (error) {
+      throw new Error(`Error making request with token to the metadata service: ${error}`);
+    }
+  }
+
+  async fetchImdsJson(path: string): Promise<unknown> {
+    const jsonText = await this.requestWithToken(path);
+    return JSON.parse('"' + jsonText + '"');
   }
 }
