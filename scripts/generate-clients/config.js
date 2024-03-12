9dac985726683f9450bec32c951a1c9d37c639ab
George Fu <kuhe@users.noreply.github.com>
2024-03-12 20:10:02 +0000

    chore(codegen): update smithy version, blob documentation hints

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "3e3022397897bdf03c6a20e1f5ddac5aa09a65ea",
+  SMITHY_TS_COMMIT: "d9297957de5dd0dcb7f5ec2fdf3c39a0771ac1d9",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
