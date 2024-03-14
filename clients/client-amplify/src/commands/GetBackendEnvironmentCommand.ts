07675a9722052f4f098938c642c10b66b3f1445f
awstools <awstools@amazon.com>
2024-03-14 18:15:00 +0000

    docs(client-amplify): Documentation updates for Amplify. Identifies the APIs available only to apps created using Amplify Gen 1.

--- clients/client-amplify/src/commands/GetBackendEnvironmentCommand.ts
+++ clients/client-amplify/src/commands/GetBackendEnvironmentCommand.ts
@@ -29,6 +29,12 @@ export interface GetBackendEnvironmentCommandOutput extends GetBackendEnvironmen
 /**
  * @public
  * <p>Returns a backend environment for an Amplify app. </p>
+ *          <p>This API is available only to Amplify Gen 1 applications where the
+ *             backend was created using Amplify Studio or the Amplify
+ *             command line interface (CLI). This API isn’t available to applications created using the
+ *             Amplify Gen 2 public preview. When you deploy an application with
+ *             Amplify Gen 2, you provision the app's backend infrastructure using Typescript
+ *             code.</p>
  * @example
  * Use a bare-bones client and the command you need to make an API call.
  * ```javascript
