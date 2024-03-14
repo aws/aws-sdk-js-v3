019ab19b7710e42dc41044cd03a240ad2d80ddf6
siddsriv <srivastava41099@gmail.com>
2024-03-14 15:09:48 +0000

    chore(client-iot-roborunner): deprecate iot roborunner

--- clients/client-iot-roborunner/src/pagination/index.ts
+++
@@ -1,6 +0,0 @@
-// smithy-typescript generated code
-export * from "./Interfaces";
-export * from "./ListDestinationsPaginator";
-export * from "./ListSitesPaginator";
-export * from "./ListWorkerFleetsPaginator";
-export * from "./ListWorkersPaginator";
