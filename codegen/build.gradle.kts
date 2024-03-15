343b06633d070883d1a13df20daa4847e738b594
Hunter Mellema <124718352+hpmellema@users.noreply.github.com>
2024-03-15 13:17:51 -0600

    chore: upgrade gradle version to 8.2.1 (#5908)

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -232,9 +232,9 @@ subprojects {
         // Configure jacoco to generate an HTML report.
         tasks.jacocoTestReport {
             reports {
-                xml.isEnabled = false
-                csv.isEnabled = false
-                html.destination = file("$buildDir/reports/jacoco")
+                xml.required.set(false)
+                csv.required.set(false)
+                html.outputLocation.set(file("$buildDir/reports/jacoco"))
             }
         }
 
--- codegen/sdk-codegen/build.gradle.kts
+++ codegen/sdk-codegen/build.gradle.kts
@@ -146,7 +146,7 @@ tasks.register("generate-smithy-build") {
 
 tasks.register("generate-default-configs-provider", JavaExec::class) {
     classpath = sourceSets["main"].runtimeClasspath
-    main = "software.amazon.smithy.aws.typescript.codegen.DefaultsModeConfigGenerator"
+    mainClass.set("software.amazon.smithy.aws.typescript.codegen.DefaultsModeConfigGenerator")
     args(listOf(project.properties["defaultsModeConfigOutput"]))
 }
 
