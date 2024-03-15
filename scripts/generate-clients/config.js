c2aba8b75a26a468e2f30b592d2e9b463d31732c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-03-15 20:11:06 +0000

    chore(codegen): update commit to move deprecation after description

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "ea953e10903ee7303d7e0759191a9bd9a2608450",
+  SMITHY_TS_COMMIT: "47555d30d155f4b6d6966c2dfe28740d18e5b5a1",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
