9fa97548ba5c5f85e7d20b93df6e029c63b598e5
siddsriv <srivastava41099@gmail.com>
2024-03-21 16:53:41 +0000

    chore(ec2-metadata-service): rename options file to include in dist-types

---
+++ packages/ec2-metadata-service/src/MetadataServiceOptions.ts
@@ -0,0 +1,30 @@
+/**
+ * @public
+ */
+export interface MetadataServiceOptions {
+  /**
+   * the endpoint of the instance metadata service.
+   */
+  endpoint?: string;
+  /**
+   * a map of options to pass to the underlying HTTP request.
+   */
+  httpOptions?: {
+    /**
+     * a timeout value in milliseconds to wait before aborting the connection. Set to 0 for no timeout.
+     */
+    timeout?: number;
+  };
+  /**
+   * Prevent IMDSv1 fallback.
+   */
+  ec2MetadataV1Disabled?: boolean;
+  /**
+   * profile name to check for IMDSv1 settings.
+   */
+  profile?: string;
+  /**
+   * when true, metadata service will not fetch token, which indicates usage of IMDSv1
+   */
+  disableFetchToken?: false;
+}
