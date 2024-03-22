becb9e4e6bc992931125e9c7adadf30fdd90a0a5
George Fu <kuhe@users.noreply.github.com>
2024-03-22 19:53:36 +0000

    feat(core): wip

--- packages/core/src/protocols/serde/AwsRestXml.ts
+++ packages/core/src/protocols/serde/AwsRestXml.ts
@@ -1,35 +1,32 @@
-// import { XmlNode } from "[@aws-sdk]/xml-builder";
-import { requestBuilder } from "@smithy/core";
-import { RuntimeModelInterpreter } from "@smithy/core";
-import { ISmithyModelShapeId, ISmithyModelStructureShape } from "@smithy/core";
-import { HttpRequest as IHttpRequest, HttpResponse as IHttpResponse, SerdeContext } from "@smithy/types";
+import type { ISmithyModelShapeId } from "@smithy/core";
+import type { HttpRequest, HttpResponse, SerdeContext } from "@smithy/types";
 
-export class AwsRestXml extends RuntimeModelInterpreter {
+import { AwsRuntimeModelInterpreter } from "./AwsRuntimeModelInterpreter";
+import { parameterNameMap } from "./parameterNameMap";
+
+/**
+ *
+ * Model runtime interpreter for AwsRestXml.
+ *
+ * @internal
+ *
+ */
+export class AwsRestXml extends AwsRuntimeModelInterpreter {
+  public parameterNameMap = parameterNameMap;
+
   public async serialize<I>(
     input: I,
     requestShapeId: ISmithyModelShapeId,
     context: SerdeContext
-  ): Promise<IHttpRequest> {
-    const requestShape: ISmithyModelStructureShape = this.getShape(requestShapeId);
-    const b = requestBuilder(input, context);
-    const headers = {} as Record<string, string>;
-    const query = {} as Record<string, string>;
-    let body: any;
-    for (const [name, { target, traits }] of Object.entries(requestShape.members)) {
-      const headerName = traits["smithy.api#httpHeader"];
-      const isPayload = traits["smithy.api#httpPayload"];
-      if (traits["smithy.api#httpHeader"]) {
-      }
-    }
-    return null as any;
+  ): Promise<HttpRequest> {
+    throw new Error("not yet implemented");
   }
 
   public async deserialize<O>(
-    httpResponse: IHttpResponse,
+    httpResponse: HttpResponse,
     responseShapeId: ISmithyModelShapeId,
     context: SerdeContext
   ): Promise<O> {
-    const responseShape: ISmithyModelStructureShape = this.getShape(responseShapeId);
-    return null as any;
+    throw new Error("not yet implemented");
   }
 }
