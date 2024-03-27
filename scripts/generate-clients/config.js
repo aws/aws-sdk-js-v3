81bf8d849b72905a8b73321ae9bea19842d5f1f9
Miles Ziemer <45497130+milesziemer@users.noreply.github.com>
2024-03-27 16:07:47 -0400

    chore(codegen): bump version to 0.20.0 (#5942)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "47555d30d155f4b6d6966c2dfe28740d18e5b5a1",
+  SMITHY_TS_COMMIT: "66f2b660e6b1dfc40699772a51778086495fd2cf",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
