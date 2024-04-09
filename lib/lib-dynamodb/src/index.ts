4de64333ad33e4e09020adc66c5255e30352a909
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-04-09 09:08:04 -0700

    chore(lib-dynamodb): re-export options and NativeAttribute* types (#5982)

--- lib/lib-dynamodb/src/index.ts
+++ lib/lib-dynamodb/src/index.ts
@@ -5,3 +5,5 @@ export * from "./DynamoDBDocumentClient";
 export * from "./commands";
 export * from "./pagination";
 export { NumberValueImpl as NumberValue } from "@aws-sdk/util-dynamodb";
+export { marshallOptions, unmarshallOptions } from "@aws-sdk/util-dynamodb";
+export { NativeAttributeValue, NativeAttributeBinary, NativeScalarAttributeValue } from "@aws-sdk/util-dynamodb";
