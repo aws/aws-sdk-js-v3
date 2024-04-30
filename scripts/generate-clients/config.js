9149dce4ff7dc9c6e4ace5161c0bf216648009be
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-04-30 15:02:44 -0700

    chore(codegen): update smithy-ts commit to update engines to node >= 16 (#6040)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "cad7ae02e13a97ea11147ca391d78db3fa275008",
+  SMITHY_TS_COMMIT: "2239905e89a876ea1b49d0b187b4ba9f37d30224",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
