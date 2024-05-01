6fba490226f6c393689ae963e21b315525388ba8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-05-01 14:57:00 +0000

    chore: update SMITHY_TS_COMMIT to include @tsconfig/node16 dependency update

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "de5c29ad1d0803e785ea2067e9cf9dfed3b5ce5c",
+  SMITHY_TS_COMMIT: "cb4d52023a1143b3dafbb115e59c48bbd0e6f8d9",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
