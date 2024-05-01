306aafc59224d7f745662fd134429ead66b42e79
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-05-01 08:08:54 -0700

    chore(clients): remove redundant service client doc generator (#6041)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "2239905e89a876ea1b49d0b187b4ba9f37d30224",
+  SMITHY_TS_COMMIT: "85bba74a4667b1ae5235606b5a34c51d304e66dc",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
