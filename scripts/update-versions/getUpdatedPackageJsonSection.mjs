cff6ff6dd4d88c5d0096681bc62ec88923749fae
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-05-03 19:21:21 +0000

    chore(scripts): use pinned version for client peerDependencies in credential providers

--- scripts/update-versions/getUpdatedPackageJsonSection.mjs
+++ scripts/update-versions/getUpdatedPackageJsonSection.mjs
@@ -1,11 +1,24 @@
 // @ts-check
-export const getUpdatedPackageJsonSection = (section, depToVersionHash, isPeer = false) =>
+export const getUpdatedPackageJsonSection = (section, depToVersionHash, { isPeer, packageName }) =>
   Object.entries(section)
     .filter(([key, value]) => key.startsWith("@aws-sdk/") && !value.startsWith("file:"))
     .reduce((acc, [key]) => {
       const newVersion = depToVersionHash[key];
       if (newVersion) {
-        acc[key] = isPeer && newVersion !== "*" ? `^${newVersion}` : newVersion;
+        // Use exact version if it's asterisk or not a peer dependency.
+        if (newVersion === "*" || !isPeer) {
+          acc[key] = newVersion;
+          return acc;
+        }
+
+        // Use exact version for client peerDependencies in credential-provider packages.
+        if (packageName.startsWith("@aws-sdk/credential-provider") && key.startsWith("@aws-sdk/client-")) {
+          acc[key] = newVersion;
+          return acc;
+        }
+
+        // Use caret version for other peerDependencies.
+        acc[key] = `^${newVersion}`;
       }
       return acc;
     }, section);
