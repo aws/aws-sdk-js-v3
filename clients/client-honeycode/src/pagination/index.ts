2d3c66054f6892ca2fdcedb21bd48b21cb2a5272
siddsriv <srivastava41099@gmail.com>
2024-05-07 03:39:42 +0000

    chore(client-honeycode): deprecate honeycode

--- clients/client-honeycode/src/pagination/index.ts
+++
@@ -1,6 +0,0 @@
-// smithy-typescript generated code
-export * from "./Interfaces";
-export * from "./ListTableColumnsPaginator";
-export * from "./ListTableRowsPaginator";
-export * from "./ListTablesPaginator";
-export * from "./QueryTableRowsPaginator";
