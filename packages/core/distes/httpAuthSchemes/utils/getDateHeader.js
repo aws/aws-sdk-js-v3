5d1fef70d61668676648f417586a090a39dbd6da
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-05-07 21:59:15 +0000

    chore(core): dist folders with tsconfig from main

---
+++ packages/core/distes/httpAuthSchemes/utils/getDateHeader.js
@@ -0,0 +1,3 @@
+import { HttpResponse } from "@smithy/protocol-http";
+export const getDateHeader = (response) =>
+  HttpResponse.isInstance(response) ? response.headers?.date ?? response.headers?.Date : undefined;
