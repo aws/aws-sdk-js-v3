625a6cf540cbe98b844e00c1bde078db490acff9
awstools <awstools@amazon.com>
2024-05-29 18:16:54 +0000

    feat(client-athena): Throwing validation errors on CreateNotebook with Name containing `/`,`:`,`\`

--- clients/client-athena/src/commands/ImportNotebookCommand.ts
+++ clients/client-athena/src/commands/ImportNotebookCommand.ts
@@ -28,10 +28,11 @@ export interface ImportNotebookCommandOutput extends ImportNotebookOutput, __Met
 
 /**
  * <p>Imports a single <code>ipynb</code> file to a Spark enabled workgroup. To import the
- *             notebook, the request must specify a value for either <code>Payload</code> or <code>NoteBookS3LocationUri</code>. If neither is specified or both are specified, an
- *             <code>InvalidRequestException</code> occurs. The maximum file size that can be imported is 10
- *             megabytes. If an <code>ipynb</code> file with the same name already exists in the
- *             workgroup, throws an error.</p>
+ *             notebook, the request must specify a value for either <code>Payload</code> or
+ *                 <code>NoteBookS3LocationUri</code>. If neither is specified or both are specified,
+ *             an <code>InvalidRequestException</code> occurs. The maximum file size that can be
+ *             imported is 10 megabytes. If an <code>ipynb</code> file with the same name already
+ *             exists in the workgroup, throws an error.</p>
  * @example
  * Use a bare-bones client and the command you need to make an API call.
  * ```javascript
