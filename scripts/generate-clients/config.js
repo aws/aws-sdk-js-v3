063522f860d6551f3f965ce087437beff894ad33
Kevin Stich <stickevi@amazon.com>
2024-06-05 22:18:43 -0700

    chore(codegen): bump smithy-typescript version to 0.21.1

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "05b2fb2f92af20e824cc71ed4490ed0c7075964e",
+  SMITHY_TS_COMMIT: "25726c12c40479b73c2919c56e2ce8c41e2a22d6",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
