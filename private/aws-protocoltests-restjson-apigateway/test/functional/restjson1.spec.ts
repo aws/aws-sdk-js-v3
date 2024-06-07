f6d069da19bcd28a75bd66deabb2ad559051a73e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-06-07 19:14:26 +0000

    test(protocoltests): add APIGateway client

---
+++ private/aws-protocoltests-restjson-apigateway/test/functional/restjson1.spec.ts
@@ -0,0 +1,180 @@
+// smithy-typescript generated code
+import { HttpHandler, HttpRequest, HttpResponse } from "@smithy/protocol-http";
+import { HeaderBag, HttpHandlerOptions } from "@smithy/types";
+import { Readable } from "stream";
+
+import { APIGatewayClient } from "../../src/APIGatewayClient";
+import { GetRestApisCommand } from "../../src/commands/GetRestApisCommand";
+
+/**
+ * Throws an expected exception that contains the serialized request.
+ */
+class EXPECTED_REQUEST_SERIALIZATION_ERROR extends Error {
+  constructor(readonly request: HttpRequest) {
+    super();
+  }
+}
+
+/**
+ * Throws an EXPECTED_REQUEST_SERIALIZATION_ERROR error before sending a
+ * request. The thrown exception contains the serialized request.
+ */
+class RequestSerializationTestHandler implements HttpHandler {
+  handle(request: HttpRequest, options?: HttpHandlerOptions): Promise<{ response: HttpResponse }> {
+    return Promise.reject(new EXPECTED_REQUEST_SERIALIZATION_ERROR(request));
+  }
+  updateHttpClientConfig(key: never, value: never): void {}
+  httpHandlerConfigs() {
+    return {};
+  }
+}
+
+/**
+ * Returns a resolved Promise of the specified response contents.
+ */
+class ResponseDeserializationTestHandler implements HttpHandler {
+  isSuccess: boolean;
+  code: number;
+  headers: HeaderBag;
+  body: String;
+
+  constructor(isSuccess: boolean, code: number, headers?: HeaderBag, body?: String) {
+    this.isSuccess = isSuccess;
+    this.code = code;
+    if (headers === undefined) {
+      this.headers = {};
+    } else {
+      this.headers = headers;
+    }
+    if (body === undefined) {
+      body = "";
+    }
+    this.body = body;
+  }
+
+  handle(request: HttpRequest, options?: HttpHandlerOptions): Promise<{ response: HttpResponse }> {
+    return Promise.resolve({
+      response: new HttpResponse({
+        statusCode: this.code,
+        headers: this.headers,
+        body: Readable.from([this.body]),
+      }),
+    });
+  }
+  updateHttpClientConfig(key: never, value: never): void {}
+  httpHandlerConfigs() {
+    return {};
+  }
+}
+
+interface comparableParts {
+  [key: string]: string;
+}
+
+/**
+ * Generates a standard map of un-equal values given input parts.
+ */
+const compareParts = (expectedParts: comparableParts, generatedParts: comparableParts) => {
+  const unequalParts: any = {};
+  Object.keys(expectedParts).forEach((key) => {
+    if (generatedParts[key] === undefined) {
+      unequalParts[key] = { exp: expectedParts[key], gen: undefined };
+    } else if (!equivalentContents(expectedParts[key], generatedParts[key])) {
+      unequalParts[key] = { exp: expectedParts[key], gen: generatedParts[key] };
+    }
+  });
+
+  Object.keys(generatedParts).forEach((key) => {
+    if (expectedParts[key] === undefined) {
+      unequalParts[key] = { exp: undefined, gen: generatedParts[key] };
+    }
+  });
+
+  if (Object.keys(unequalParts).length !== 0) {
+    return unequalParts;
+  }
+  return undefined;
+};
+
+/**
+ * Compares all types for equivalent contents, doing nested
+ * equality checks based on non-`$metadata`
+ * properties that have defined values.
+ */
+const equivalentContents = (expected: any, generated: any): boolean => {
+  const localExpected = expected;
+
+  // Short circuit on equality.
+  if (localExpected == generated) {
+    return true;
+  }
+
+  // If a test fails with an issue in the below 6 lines, it's likely
+  // due to an issue in the nestedness or existence of the property
+  // being compared.
+  delete localExpected["$metadata"];
+  delete generated["$metadata"];
+  Object.keys(localExpected).forEach((key) => localExpected[key] === undefined && delete localExpected[key]);
+  Object.keys(generated).forEach((key) => generated[key] === undefined && delete generated[key]);
+
+  const expectedProperties = Object.getOwnPropertyNames(localExpected);
+  const generatedProperties = Object.getOwnPropertyNames(generated);
+
+  // Short circuit on different property counts.
+  if (expectedProperties.length != generatedProperties.length) {
+    return false;
+  }
+
+  // Compare properties directly.
+  for (let index = 0; index < expectedProperties.length; index++) {
+    const propertyName = expectedProperties[index];
+    if (!equivalentContents(localExpected[propertyName], generated[propertyName])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const clientParams = {
+  region: "us-west-2",
+  credentials: { accessKeyId: "key", secretAccessKey: "secret" },
+};
+
+/**
+ * A wrapper function that shadows `fail` from jest-jasmine2
+ * (jasmine2 was replaced with circus in > v27 as the default test runner)
+ */
+const fail = (error?: any): never => {
+  throw new Error(error);
+};
+
+/**
+ * API Gateway requires that this Accept header is set on all requests.
+ */
+it("ApiGatewayAccept:Request", async () => {
+  const client = new APIGatewayClient({
+    ...clientParams,
+    requestHandler: new RequestSerializationTestHandler(),
+  });
+
+  const command = new GetRestApisCommand({} as any);
+  try {
+    await client.send(command);
+    fail("Expected an EXPECTED_REQUEST_SERIALIZATION_ERROR to be thrown");
+    return;
+  } catch (err) {
+    if (!(err instanceof EXPECTED_REQUEST_SERIALIZATION_ERROR)) {
+      fail(err);
+      return;
+    }
+    const r = err.request;
+    expect(r.method).toBe("GET");
+    expect(r.path).toBe("/restapis");
+
+    expect(r.headers["accept"]).toBeDefined();
+    expect(r.headers["accept"]).toBe("application/json");
+
+    expect(r.body).toBeFalsy();
+  }
+});
