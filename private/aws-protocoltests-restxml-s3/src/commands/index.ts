43c582728ab4f80bdf6d5157f7cdc362fa803ea9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-06-07 19:28:02 +0000

    test(protocoltests): add S3 client

---
+++ private/aws-protocoltests-restxml-s3/src/commands/index.ts
@@ -0,0 +1,5 @@
+// smithy-typescript generated code
+export * from "./DeleteObjectTaggingCommand";
+export * from "./GetBucketLocationCommand";
+export * from "./GetObjectCommand";
+export * from "./ListObjectsV2Command";
