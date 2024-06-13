4759f6a8c2bd3625b42e07b1b5ab47459d1fc666
George Fu <kuhe@users.noreply.github.com>
2024-06-13 17:16:53 +0000

    fix(util-dynamodb): fix signature overload resolution for marshall() fn

--- packages/util-dynamodb/src/marshall.spec.ts
+++ packages/util-dynamodb/src/marshall.spec.ts
@@ -1,3 +1,5 @@
+import { AttributeValue } from "@aws-sdk/client-dynamodb";
+
 import { convertToAttr } from "./convertToAttr";
 import { marshall } from "./marshall";
 
@@ -50,6 +52,26 @@ describe("marshall", () => {
     expect(marshall(input)).toEqual(mockOutput);
     expect(convertToAttr).toHaveBeenCalledTimes(1);
     expect(convertToAttr).toHaveBeenCalledWith(input, undefined);
+  });
+
+  it("should resolve signatures correctly", () => {
+    // eslint-disable @typescript-eslint/no-unused-vars
+    const ss: AttributeValue.SSMember = marshall(new Set(["a"]));
+    const ns: AttributeValue.NSMember = marshall(new Set([0]));
+    const bs: AttributeValue.BSMember = marshall(new Set([new Uint8Array()]));
+    const s: AttributeValue.SMember = marshall("a");
+    const n: AttributeValue.NMember = marshall(0);
+    const nil: AttributeValue.NULLMember = marshall(null);
+    const bool: AttributeValue.BOOLMember = marshall(false);
+    const array: AttributeValue[] = marshall([]);
+    const object: Record<string, AttributeValue> = marshall({
+      pk: "abc",
+      sk: "xyz",
+    });
+    const unrecognizedClassInstance1: Record<string, AttributeValue> = marshall(new Map());
+    const unrecognizedClassInstance2: Record<string, AttributeValue> = marshall(new Date());
+    const unrecognizedNonClassInstanceValue: AttributeValue.$UnknownMember = marshall(BigInt(0));
+    // eslint-enable @typescript-eslint/no-unused-vars
   });
 
   it("with class instance as an input", () => {
