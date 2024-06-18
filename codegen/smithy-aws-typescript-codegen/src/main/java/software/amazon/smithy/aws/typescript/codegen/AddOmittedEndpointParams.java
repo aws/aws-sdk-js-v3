7648a7b99abcea5ac59bc066bd4dabde6541305d
siddsriv <srivastava41099@gmail.com>
2024-06-18 17:32:22 +0000

    chore(codegen): checkstyle fixes

--- codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AddOmittedEndpointParams.java
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AddOmittedEndpointParams.java
@@ -4,20 +4,21 @@
  */
 
  package software.amazon.smithy.aws.typescript.codegen;
+
  import software.amazon.smithy.typescript.codegen.TypeScriptCodegenContext;
- import software.amazon.smithy.typescript.codegen.endpointsV2.OmitEndpointParams;
  import software.amazon.smithy.typescript.codegen.integration.TypeScriptIntegration;
+ import software.amazon.smithy.typescript.codegen.endpointsV2.OmitEndpointParams;
  import software.amazon.smithy.utils.SetUtils;
  import software.amazon.smithy.utils.SmithyInternalApi;
- 
+
  @SmithyInternalApi
  public class AddOmittedEndpointParams implements TypeScriptIntegration {
- 
+
      @Override
      public void customize(TypeScriptCodegenContext codegenContext) {
          setParamForOmission();
      }
- 
+
      private void setParamForOmission() {
          OmitEndpointParams.addOmittedParams(SetUtils.of("AccountId"));
      }
