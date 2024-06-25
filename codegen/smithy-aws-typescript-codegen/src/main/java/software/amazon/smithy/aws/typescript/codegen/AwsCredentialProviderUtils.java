1e0a60e8479f418d746ded945ee4689ebd6fc549
Isaiah Hall <ishallh@amazon.com>
2024-06-25 17:42:26 -0400

    chore: fix style for CI error

--- codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AwsCredentialProviderUtils.java
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AwsCredentialProviderUtils.java
@@ -33,7 +33,8 @@ public final class AwsCredentialProviderUtils {
      * The dependencies are skipped in first party credential providers to avoid circular dependency issue.
      */
     public static void addAwsCredentialProviderDependencies(ServiceShape service, TypeScriptWriter writer) {
-        boolean isStsClient = service.getId().equals(ShapeId.from("com.amazonaws.sts#AWSSecurityTokenServiceV20110615"));
+        boolean isStsClient = 
+                service.getId().equals(ShapeId.from("com.amazonaws.sts#AWSSecurityTokenServiceV20110615"));
         boolean isSsoOidcClient = service.getId().equals(ShapeId.from("com.amazonaws.ssooidc#AWSSSOOIDCService"));
         if (!isSsoOidcClient) {
             // SSO OIDC client is required in Sso credential provider
