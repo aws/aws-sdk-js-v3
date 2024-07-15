9caa8ed0f83e4730fd5c755ee6d2184a8788beed
George Fu <kuhe@users.noreply.github.com>
2024-07-15 19:22:01 +0000

    chore(clients): codegen sync for stream utils

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "35bf43f0cac62249483078b18bce023c45462ba1",
+  SMITHY_TS_COMMIT: "11b9cd96717714c18a5380149719d926f48c05a1",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
