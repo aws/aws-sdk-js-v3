d94cd1e778c7cca1d00e337a17d404aa5f81f5c1
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-07-29 21:59:37 +0000

    chore(codegen): bump smithy commit to update fast-xml-parser

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "1e4e6ae17577ee46995e23ad67bcad9189c950e5",
+  SMITHY_TS_COMMIT: "c86fa1906dab07e676f3a4e9e0eb983e76064e7e",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
