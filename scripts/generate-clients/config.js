658787ba000bc32c5605f279b320f725008e3180
George Fu <kuhe@users.noreply.github.com>
2024-07-30 19:07:15 +0000

    chore(codegen): update smithy hash for id&auth SRA

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "c86fa1906dab07e676f3a4e9e0eb983e76064e7e",
+  SMITHY_TS_COMMIT: "354d175a81a4c15ae2d01bf0a77e6012f5330f62",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
