71f373db6be51dbc2833da4c9222882590762036
awstools <awstools@amazon.com>
2024-08-01 18:21:51 +0000

    feat(client-ssm-quicksetup): This release adds API support for the QuickSetup feature of AWS Systems Manager

---
+++ clients/client-ssm-quicksetup/src/pagination/Interfaces.ts
@@ -0,0 +1,11 @@
+// smithy-typescript generated code
+import { PaginationConfiguration } from "@smithy/types";
+
+import { SSMQuickSetupClient } from "../SSMQuickSetupClient";
+
+/**
+ * @public
+ */
+export interface SSMQuickSetupPaginationConfiguration extends PaginationConfiguration {
+  client: SSMQuickSetupClient;
+}
