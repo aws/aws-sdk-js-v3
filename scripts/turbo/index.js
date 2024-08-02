0de2971f999982ecfe8248705567d1e5a1c19a06
Isaiah Hall <ishallh@amazon.com>
2024-08-02 17:13:01 -0400

    chore(turbo): updated Turbo version

--- scripts/turbo/index.js
+++ scripts/turbo/index.js
@@ -1,5 +1,6 @@
-const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+const fs = require("fs");
 const { spawnProcess } = require("../utils/spawn-process");
+const path = require("path");
 
 // Fetch Secrets In CI Environments
 const fetchSecret = async (secretName) => {
@@ -7,28 +8,44 @@ const fetchSecret = async (secretName) => {
   if (!account) {
     return null;
   }
-  const partialArn = `arn:aws:secretsmanager:us-west-2:${account}:secret:${secretName}`;
-  const secretsManager = new SecretsManagerClient({ region: "us-west-2" });
-  const command = new GetSecretValueCommand({ partialArn });
+  try {
+    if (!fs.existsSync(path.join(__dirname, "node_modules/"))) {
+      await installDeps();
+    }
+    const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+    const partialArn = `arn:aws:secretsmanager:us-west-2:${account}:secret:${secretName}`;
+    const secretsManager = new SecretsManagerClient({ region: "us-west-2" });
+    const command = new GetSecretValueCommand({ SecretId: partialArn });
 
-  const response = await secretsManager.send(command);
-  return response.SecretString;
+    const response = await secretsManager.send(command);
+    return response.SecretString;
+  } catch (error) {
+    console.error("Error fetching turbo remote cache secret:", error);
+    return null;
+  }
+};
+
+const installDeps = async () => {
+  return spawnProcess("npm", ["install"], { stdio: "inherit", cwd: __dirname });
 };
 
 const runTurbo = async (task, args, apiSecret, apiEndpoint) => {
-  const command = ["turbo", "run", task];
+  let command = ["turbo", "run", task];
   if (apiSecret && apiEndpoint) {
     command = command.concat([
-      `--api="${apiEndpoint}"`,
-      `--team="aws-sdk-js"`,
+      `--api=${apiEndpoint}`,
+      `--team=aws-sdk-js`,
       `--token=${apiSecret}`,
       "--concurrency=100%",
     ]);
   }
   command = command.concat(args);
-  console.info("Running:", "npx turbo run ", task);
-  await spawnProcess("npx", command, { stdio: "inherit" });
-  return;
+  const turboRoot = path.join(__dirname, "..", "..");
+  try {
+    return spawnProcess("npx", command, { stdio: "inherit", cwd: turboRoot });
+  } catch (error) {
+    console.error("Error running turbo:", error);
+  }
 };
 
 const main = async () => {
@@ -38,10 +55,10 @@ const main = async () => {
   const args = process.argv.slice(2);
 
   if (!apiSecret || !apiEndpoint) {
-    runTurbo(args[0], args.slice[1]);
+    await runTurbo(args[0], args.slice(1));
+  } else {
+    await runTurbo(args[0], args.slice(1), apiSecret, apiEndpoint);
   }
-
-  runTurbo(args[0], args.slice[1], apiSecret, apiEndpoint);
 };
 
 main();
