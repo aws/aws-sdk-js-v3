82ea670c8df1aa3fc00ca872c1105c9fbf59a678
awstools <awstools@amazon.com>
2024-08-06 18:13:44 +0000

    feat(client-cost-optimization-hub): This release adds savings percentage support to the ListRecommendationSummaries API.

--- clients/client-cost-optimization-hub/src/protocols/Aws_json1_0.ts
+++ clients/client-cost-optimization-hub/src/protocols/Aws_json1_0.ts
@@ -98,6 +98,7 @@ import {
   SavingsPlansCostCalculation,
   SavingsPlansPricing,
   StorageConfiguration,
+  SummaryMetrics,
   Tag,
   ThrottlingException,
   UpdateEnrollmentStatusRequest,
@@ -476,6 +477,8 @@ const de_ValidationExceptionRes = async (parsedOutput: any, context: __SerdeCont
 
 // se_ResourceTypeList omitted.
 
+// se_SummaryMetricsList omitted.
+
 // se_Tag omitted.
 
 // se_TagList omitted.
@@ -749,6 +752,7 @@ const de_ListRecommendationSummariesResponse = (
     estimatedTotalDedupedSavings: __limitedParseDouble,
     groupBy: __expectString,
     items: (_: any) => de_RecommendationSummariesList(_, context),
+    metrics: _json,
     nextToken: __expectString,
   }) as any;
 };
@@ -1062,6 +1066,8 @@ const de_StorageConfiguration = (output: any, context: __SerdeContext): StorageC
     type: __expectString,
   }) as any;
 };
+
+// de_SummaryMetricsResult omitted.
 
 // de_Tag omitted.
 
