1f30d80a7264fe008ab37901c16a7f18970c60df
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-08-12 16:19:46 +0000

    fix(client-s3-control): populate memberName as contextParams value

--- clients/client-s3-control/src/commands/DeleteAccessPointPolicyCommand.ts
+++ clients/client-s3-control/src/commands/DeleteAccessPointPolicyCommand.ts
@@ -86,7 +86,7 @@ export class DeleteAccessPointPolicyCommand extends $Command
   .ep({
     ...commonParams,
     RequiresAccountId: { type: "staticContextParams", value: true },
-    AccessPointName: { type: "contextParams", name: "AccessPointName" },
+    AccessPointName: { type: "contextParams", name: "Name" },
     AccountId: { type: "contextParams", name: "AccountId" },
   })
   .m(function (this: any, Command: any, cs: any, config: S3ControlClientResolvedConfig, o: any) {
