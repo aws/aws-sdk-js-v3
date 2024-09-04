f155e2987ec2e7f9f42e2799b09433ca0cbc1e3c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-09-04 09:49:25 -0700

    chore(codegen): string array and operation context params (#6435)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "8b3d752e3231820b94befa5f7118b7e134d759e9",
+  SMITHY_TS_COMMIT: "a2bb933fc4dea318cdda9db998e1606aefb77c51",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
