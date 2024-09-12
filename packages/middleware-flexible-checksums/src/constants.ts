5156e60b9b9a16cf394baa749662af96339e59b2
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-09-12 16:02:51 +0000

    chore: use discriminated unions over string enums
    Refs: https://rlee.dev/stop-misusing-typescript-string-enums#heading-drawbacks-of-string-enums

--- packages/middleware-flexible-checksums/src/constants.ts
+++ packages/middleware-flexible-checksums/src/constants.ts
@@ -1,14 +1,14 @@
 /**
  * Determines when a checksum will be calculated for request payloads.
  */
-export enum RequestChecksumCalculation {
+export const RequestChecksumCalculation = {
   /**
    * When set, a checksum will be calculated for all request payloads of operations
    * modeled with the {@link httpChecksum} trait where `requestChecksumRequired` is `true`
    * AND/OR a `requestAlgorithmMember` is modeled.
    * {@link https://smithy.io/2.0/aws/aws-core.html#aws-protocols-httpchecksum-trait httpChecksum}
    */
-  WHEN_SUPPORTED = "WHEN_SUPPORTED",
+  WHEN_SUPPORTED: "WHEN_SUPPORTED",
 
   /**
    * When set, a checksum will only be calculated for request payloads of operations
@@ -16,20 +16,22 @@ export enum RequestChecksumCalculation {
    * OR where a `requestAlgorithmMember` is modeled and the user sets it.
    * {@link https://smithy.io/2.0/aws/aws-core.html#aws-protocols-httpchecksum-trait httpChecksum}
    */
-  WHEN_REQUIRED = "WHEN_REQUIRED",
-}
+  WHEN_REQUIRED: "WHEN_REQUIRED",
+} as const;
+
+export type RequestChecksumCalculation = (typeof RequestChecksumCalculation)[keyof typeof RequestChecksumCalculation];
 
 /**
  * Determines when checksum validation will be performed on response payloads.
  */
-export enum ResponseChecksumValidation {
+export const ResponseChecksumValidation = {
   /**
    * When set, checksum validation MUST be performed on all response payloads of operations
    * modeled with the {@link httpChecksum} trait where `responseAlgorithms` is modeled,
    * except when no modeled checksum algorithms are supported by an SDK.
    * {@link https://smithy.io/2.0/aws/aws-core.html#aws-protocols-httpchecksum-trait httpChecksum}
    */
-  WHEN_SUPPORTED = "WHEN_SUPPORTED",
+  WHEN_SUPPORTED: "WHEN_SUPPORTED",
 
   /**
    * When set, checksum validation MUST NOT be performed on response payloads of operations UNLESS
@@ -37,8 +39,10 @@ export enum ResponseChecksumValidation {
    * It is currently impossible to model an operation as requiring a response checksum,
    * but this setting leaves the door open for future updates.
    */
-  WHEN_REQUIRED = "WHEN_REQUIRED",
-}
+  WHEN_REQUIRED: "WHEN_REQUIRED",
+} as const;
+
+export type ResponseChecksumValidation = (typeof ResponseChecksumValidation)[keyof typeof ResponseChecksumValidation];
 
 /**
  * Checksum Algorithms supported by the SDK.
