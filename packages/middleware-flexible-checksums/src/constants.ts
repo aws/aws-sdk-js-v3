853d833bd75b5fe32db7ccc572c74ad9e36f905b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-09-12 15:50:38 +0000

    chore(middleware-flexible-checksums): add RequestChecksumCalculation and ResponseChecksumValidation

--- packages/middleware-flexible-checksums/src/constants.ts
+++ packages/middleware-flexible-checksums/src/constants.ts
@@ -1,4 +1,46 @@
 /**
+ * Determines when a checksum will be calculated for request payloads.
+ */
+export enum RequestChecksumCalculation {
+  /**
+   * When set, a checksum will be calculated for all request payloads of operations
+   * modeled with the {@link httpChecksum} trait where `requestChecksumRequired` is `true`
+   * AND/OR a `requestAlgorithmMember` is modeled.
+   * {@link https://smithy.io/2.0/aws/aws-core.html#aws-protocols-httpchecksum-trait httpChecksum}
+   */
+  WHEN_SUPPORTED = "WHEN_SUPPORTED",
+
+  /**
+   * When set, a checksum will only be calculated for request payloads of operations
+   * modeled with the {@link httpChecksum} trait where `requestChecksumRequired` is `true`
+   * OR where a `requestAlgorithmMember` is modeled and the user sets it.
+   * {@link https://smithy.io/2.0/aws/aws-core.html#aws-protocols-httpchecksum-trait httpChecksum}
+   */
+  WHEN_REQUIRED = "WHEN_REQUIRED",
+}
+
+/**
+ * Determines when checksum validation will be performed on response payloads.
+ */
+export enum ResponseChecksumValidation {
+  /**
+   * When set, checksum validation MUST be performed on all response payloads of operations
+   * modeled with the {@link httpChecksum} trait where `responseAlgorithms` is modeled,
+   * except when no modeled checksum algorithms are supported by an SDK.
+   * {@link https://smithy.io/2.0/aws/aws-core.html#aws-protocols-httpchecksum-trait httpChecksum}
+   */
+  WHEN_SUPPORTED = "WHEN_SUPPORTED",
+
+  /**
+   * When set, checksum validation MUST NOT be performed on response payloads of operations UNLESS
+   * the SDK supports the modeled checksum algorithms AND the user has set the `requestValidationModeMember` to `ENABLED`.
+   * It is currently impossible to model an operation as requiring a response checksum,
+   * but this setting leaves the door open for future updates.
+   */
+  WHEN_REQUIRED = "WHEN_REQUIRED",
+}
+
+/**
  * Checksum Algorithms supported by the SDK.
  */
 export enum ChecksumAlgorithm {
