1d4ebff641b74e6b6bcc024db8fb3e028de786fc
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-09-17 13:30:19 -0700

    fix(middleware-flexible-checksums): use union for new config types (#6489)

--- packages/middleware-flexible-checksums/src/NODE_REQUEST_CHECKSUM_CALCULATION_CONFIG_OPTIONS.ts
+++ packages/middleware-flexible-checksums/src/NODE_REQUEST_CHECKSUM_CALCULATION_CONFIG_OPTIONS.ts
@@ -6,7 +6,7 @@ import { SelectorType, stringUnionSelector } from "./stringUnionSelector";
 export const ENV_REQUEST_CHECKSUM_CALCULATION = "AWS_REQUEST_CHECKSUM_CALCULATION";
 export const CONFIG_REQUEST_CHECKSUM_CALCULATION = "request_checksum_calculation";
 
-export const NODE_REQUEST_CHECKSUM_CALCULATION_CONFIG_OPTIONS: LoadedConfigSelectors<string> = {
+export const NODE_REQUEST_CHECKSUM_CALCULATION_CONFIG_OPTIONS: LoadedConfigSelectors<RequestChecksumCalculation> = {
   environmentVariableSelector: (env) =>
     stringUnionSelector(env, ENV_REQUEST_CHECKSUM_CALCULATION, RequestChecksumCalculation, SelectorType.ENV),
   configFileSelector: (profile) =>
--- packages/middleware-flexible-checksums/src/NODE_RESPONSE_CHECKSUM_VALIDATION_CONFIG_OPTIONS.ts
+++ packages/middleware-flexible-checksums/src/NODE_RESPONSE_CHECKSUM_VALIDATION_CONFIG_OPTIONS.ts
@@ -1,15 +1,15 @@
 import { LoadedConfigSelectors } from "@smithy/node-config-provider";
 
-import { DEFAULT_RESPONSE_CHECKSUM_VALIDATION, RequestChecksumCalculation } from "./constants";
+import { DEFAULT_RESPONSE_CHECKSUM_VALIDATION, ResponseChecksumValidation } from "./constants";
 import { SelectorType, stringUnionSelector } from "./stringUnionSelector";
 
 export const ENV_RESPONSE_CHECKSUM_VALIDATION = "AWS_RESPONSE_CHECKSUM_VALIDATION";
 export const CONFIG_RESPONSE_CHECKSUM_VALIDATION = "response_checksum_validation";
 
-export const NODE_RESPONSE_CHECKSUM_VALIDATION_CONFIG_OPTIONS: LoadedConfigSelectors<string> = {
+export const NODE_RESPONSE_CHECKSUM_VALIDATION_CONFIG_OPTIONS: LoadedConfigSelectors<ResponseChecksumValidation> = {
   environmentVariableSelector: (env) =>
-    stringUnionSelector(env, ENV_RESPONSE_CHECKSUM_VALIDATION, RequestChecksumCalculation, SelectorType.ENV),
+    stringUnionSelector(env, ENV_RESPONSE_CHECKSUM_VALIDATION, ResponseChecksumValidation, SelectorType.ENV),
   configFileSelector: (profile) =>
-    stringUnionSelector(profile, CONFIG_RESPONSE_CHECKSUM_VALIDATION, RequestChecksumCalculation, SelectorType.CONFIG),
+    stringUnionSelector(profile, CONFIG_RESPONSE_CHECKSUM_VALIDATION, ResponseChecksumValidation, SelectorType.CONFIG),
   default: DEFAULT_RESPONSE_CHECKSUM_VALIDATION,
 };
