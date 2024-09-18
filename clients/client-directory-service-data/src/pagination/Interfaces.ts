66eca9762638f79bd605b7da5569275ca0b4b503
awstools <awstools@amazon.com>
2024-09-18 18:19:00 +0000

    feat(client-directory-service-data): Added new AWS Directory Service Data API, enabling you to manage data stored in AWS Directory Service directories. This includes APIs for creating, reading, updating, and deleting directory users, groups, and group memberships.

---
+++ clients/client-directory-service-data/src/pagination/Interfaces.ts
@@ -0,0 +1,11 @@
+// smithy-typescript generated code
+import { PaginationConfiguration } from "@smithy/types";
+
+import { DirectoryServiceDataClient } from "../DirectoryServiceDataClient";
+
+/**
+ * @public
+ */
+export interface DirectoryServiceDataPaginationConfiguration extends PaginationConfiguration {
+  client: DirectoryServiceDataClient;
+}
---
+++ clients/client-directory-service-data/src/pagination/ListGroupsForMemberPaginator.ts
@@ -0,0 +1,24 @@
+// smithy-typescript generated code
+import { createPaginator } from "@smithy/core";
+import { Paginator } from "@smithy/types";
+
+import {
+  ListGroupsForMemberCommand,
+  ListGroupsForMemberCommandInput,
+  ListGroupsForMemberCommandOutput,
+} from "../commands/ListGroupsForMemberCommand";
+import { DirectoryServiceDataClient } from "../DirectoryServiceDataClient";
+import { DirectoryServiceDataPaginationConfiguration } from "./Interfaces";
+
+/**
+ * @public
+ */
+export const paginateListGroupsForMember: (
+  config: DirectoryServiceDataPaginationConfiguration,
+  input: ListGroupsForMemberCommandInput,
+  ...rest: any[]
+) => Paginator<ListGroupsForMemberCommandOutput> = createPaginator<
+  DirectoryServiceDataPaginationConfiguration,
+  ListGroupsForMemberCommandInput,
+  ListGroupsForMemberCommandOutput
+>(DirectoryServiceDataClient, ListGroupsForMemberCommand, "NextToken", "NextToken", "MaxResults");
