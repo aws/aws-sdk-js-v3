afb95351b7410a650aeea491c9e5e147da794eeb
Ran Vaknin <rvaknin@dev-dsk-rvaknin2-2a-e69e90c1.us-west-2.amazon.com>
2024-09-20 17:12:10 +0000

    fix(sdk-codegen): corrected model parsing and awsQuery error code with shapeID swap

--- codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/ProcessAwsQueryWaiters.java
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/ProcessAwsQueryWaiters.java
@@ -1,142 +1,87 @@
 package software.amazon.smithy.aws.typescript.codegen;
 
+import software.amazon.smithy.aws.traits.protocols.AwsQueryErrorTrait;
 import software.amazon.smithy.model.Model;
-import software.amazon.smithy.model.node.ArrayNode;
-import software.amazon.smithy.model.node.Node;
 import software.amazon.smithy.model.node.ObjectNode;
-import software.amazon.smithy.model.node.StringNode;
-import software.amazon.smithy.model.shapes.AbstractShapeBuilder;
-import software.amazon.smithy.model.shapes.OperationShape;
-import software.amazon.smithy.model.shapes.Shape;
-import software.amazon.smithy.model.shapes.ShapeId;
-import software.amazon.smithy.model.traits.DynamicTrait;
-import software.amazon.smithy.model.traits.Trait;
+import software.amazon.smithy.model.shapes.*;
+import software.amazon.smithy.model.traits.ErrorTrait;
 import software.amazon.smithy.model.transform.ModelTransformer;
 import software.amazon.smithy.typescript.codegen.TypeScriptSettings;
 import software.amazon.smithy.typescript.codegen.integration.TypeScriptIntegration;
 import software.amazon.smithy.utils.SmithyInternalApi;
+import software.amazon.smithy.model.knowledge.TopDownIndex;
+import software.amazon.smithy.waiters.Acceptor;
+import software.amazon.smithy.waiters.Matcher;
+import software.amazon.smithy.waiters.WaitableTrait;
+import software.amazon.smithy.waiters.Waiter;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SmithyInternalApi
 public final class ProcessAwsQueryWaiters implements TypeScriptIntegration {
 
     @Override
     public Model preprocessModel(Model model, TypeScriptSettings settings) {
-        // create mapping of exception shape IDs and awsQuery error code
-        Map<String, ShapeId> errorCodeToShapeId = new HashMap<>();
-        for (Shape shape : model.toSet()) {
-            if (shape.hasTrait("smithy.api#error") && shape.hasTrait("aws.protocols#awsQueryError")) {
-                Optional<Trait> awsQueryTraitOpt = shape.findTrait("aws.protocols#awsQueryError");
-                if (awsQueryTraitOpt.isPresent()) {
-                    Trait awsQueryTrait = awsQueryTraitOpt.get();
-                    ObjectNode traitValue = awsQueryTrait.toNode().expectObjectNode();
-                    Optional<Node> memberNodeOpt = traitValue.getMember("code");
-                    if (memberNodeOpt.isPresent()){
-                        Optional<StringNode> codeNodeOpt = memberNodeOpt.get().asStringNode();
-                        if (codeNodeOpt.isPresent()){
-                            String code = codeNodeOpt.get().getValue();
-                            errorCodeToShapeId.put(code, shape.getId());
-                        }
-                    }
+        Map<String, String> errorCodeToShapeId = new HashMap<>();
+
+        ServiceShape serviceShape = settings.getService(model);
+        TopDownIndex topDownIndex = TopDownIndex.of(model);
+        for (OperationShape operationShape : topDownIndex.getContainedOperations(serviceShape)) {
+            for (ShapeId errorShapeId : operationShape.getErrors()) {
+                Shape errorShape = model.expectShape(errorShapeId);
+                if (errorShape.hasTrait(ErrorTrait.class) && errorShape.hasTrait(AwsQueryErrorTrait.class)) {
+                    AwsQueryErrorTrait awsQueryTrait = errorShape.expectTrait(AwsQueryErrorTrait.class);
+                    errorCodeToShapeId.put(awsQueryTrait.getCode(), errorShape.getId().getName());
                 }
             }
         }
-
 
         List<Shape> modifiedShapes = new ArrayList<>();
 
-        for (Shape shape : model.toSet()) {
-            Optional<Trait> waitableTraitOpt = shape.findTrait("smithy.waiters#waitable");
-            if (waitableTraitOpt.isPresent()) {
-                Trait waitableTrait = waitableTraitOpt.get();
-                ObjectNode traitValue = waitableTrait.toNode().expectObjectNode();
-                ObjectNode.Builder traitValueBuilder = traitValue.toBuilder();
-                boolean modified = false;
+        for (OperationShape operationShape : topDownIndex.getContainedOperations(serviceShape)) {
+            OperationShape.Builder operationBuilder = operationShape.toBuilder();
+            if (operationShape.hasTrait(WaitableTrait.class)) {
+                operationBuilder.removeTrait(WaitableTrait.ID);
+                WaitableTrait waiterTrait = operationShape.expectTrait(WaitableTrait.class);
+                WaitableTrait.Builder waitableTraitBuilder = (WaitableTrait.Builder) waiterTrait.toBuilder();
+                for (Map.Entry<String, Waiter> entry : waiterTrait.getWaiters().entrySet()){
+                    String name = entry.getKey();
+                    Waiter waiter = entry.getValue();
+                    Waiter.Builder waiterBuilder = (Waiter.Builder) waiter.toBuilder();
+                    waiterBuilder.clearAcceptors();
+                    for (Acceptor acceptor : waiter.getAcceptors()){
+                        ObjectNode acceptorNode = acceptor.toNode().expectObjectNode();
+                        Matcher matcher = acceptor.getMatcher();
+                        if (matcher instanceof Matcher.ErrorTypeMember){
+                            ObjectNode matcherNode = matcher.toNode().expectObjectNode();
 
-                for (Map.Entry<StringNode, Node> waiterEntry : traitValue.getMembers().entrySet()) {
-                    StringNode waiterNameNode = waiterEntry.getKey();
-                    Node waiterNode = waiterEntry.getValue();
+                            String errorCode = matcherNode.expectStringMember("errorType").getValue();
+                            if (errorCodeToShapeId.containsKey(errorCode)){
+                                matcherNode = matcherNode.toBuilder()
+                                        .withMember("errorType", errorCodeToShapeId.get(errorCode))
+                                        .build();
 
-                    if (waiterNode.isObjectNode()) {
-                        ObjectNode waiterObject = waiterNode.expectObjectNode();
-                        ObjectNode.Builder waiterObjectBuilder = waiterObject.toBuilder();
-                        boolean waiterModified = false;
-
-                        Optional<ArrayNode> acceptorsArrayOpt = waiterObject.getArrayMember("acceptors");
-                        if (acceptorsArrayOpt.isPresent()) {
-                            ArrayNode acceptorsArray = acceptorsArrayOpt.get();
-                            ArrayNode.Builder acceptorsArrayBuilder = ArrayNode.builder();
-                            boolean acceptorsModified = false;
-
-                            for (Node acceptorNode : acceptorsArray.getElements()) {
-                                if (acceptorNode.isObjectNode()) {
-                                    ObjectNode acceptorObject = acceptorNode.expectObjectNode();
-                                    Optional<ObjectNode> matcherObjectOpt = acceptorObject.getObjectMember("matcher");
-                                    if (matcherObjectOpt.isPresent()) {
-                                        ObjectNode matcherObject = matcherObjectOpt.get();
-                                        Optional<StringNode> errorTypeNodeOpt = matcherObject.getStringMember("errorType");
-                                        if (errorTypeNodeOpt.isPresent()) {
-                                            String errorType = errorTypeNodeOpt.get().getValue();
-                                            ShapeId errorShapeId = errorCodeToShapeId.get(errorType);
-                                            if (errorShapeId != null) {
-                                                // Replace the errorType value with the shape ID
-                                                ObjectNode modifiedMatcherObject = matcherObject.withMember("errorType", Node.from(errorShapeId.toString()));
-                                                ObjectNode modifiedAcceptorObject = acceptorObject.withMember("matcher", modifiedMatcherObject);
-                                                acceptorsArrayBuilder.withValue(modifiedAcceptorObject);
-                                                acceptorsModified = true;
-                                                continue;
-                                            }
-                                        }
-                                    }
-                                    // If not modified, add the original acceptor
-                                    acceptorsArrayBuilder.withValue(acceptorObject);
-                                } else {
-                                    acceptorsArrayBuilder.withValue(acceptorNode);
-                                }
-                            }
-                            if (acceptorsModified) {
-                                waiterObjectBuilder.withMember("acceptors", acceptorsArrayBuilder.build());
-                                waiterModified = true;
+                                acceptorNode = acceptorNode.toBuilder()
+                                        .withMember("matcher", matcherNode)
+                                        .build();
                             }
                         }
-                        if (waiterModified) {
-                            traitValueBuilder.withMember(waiterNameNode, waiterObjectBuilder.build());
-                            modified = true;
-                        }
+                        waiterBuilder.addAcceptor(Acceptor.fromNode(acceptorNode));
                     }
+                    waitableTraitBuilder.put(name,waiterBuilder.build());
                 }
-
-                if (modified) {
-                    Trait modifiedWaitableTrait = new DynamicTrait(waitableTrait.toShapeId(), traitValueBuilder.build());
-                    AbstractShapeBuilder<?, ?> shapeBuilder = Shape.shapeToBuilder(shape);
-
-                    shapeBuilder.removeTrait(waitableTrait.toShapeId());
-                    shapeBuilder.addTrait(modifiedWaitableTrait);
-                    Shape modifiedShape = shapeBuilder.build();
-
-                    modifiedShapes.add(modifiedShape);
-                }
+                operationBuilder.addTrait(waitableTraitBuilder.build());
             }
+            modifiedShapes.add(operationBuilder.build());
         }
 
-        // replace the modified shapes in the model
         if (!modifiedShapes.isEmpty()) {
             ModelTransformer transformer = ModelTransformer.create();
             model = transformer.replaceShapes(model, modifiedShapes);
-        }
-
-        // print the transformation
-        for (Shape updatedShape : model.toSet()) {
-            Optional<Trait> waitableTraitOpt = updatedShape.findTrait("smithy.waiters#waitable");
-            if (waitableTraitOpt.isPresent()) {
-                Trait waitableTrait = waitableTraitOpt.get();
-                ObjectNode traitValue = waitableTrait.toNode().expectObjectNode();
-
-                System.out.println("Waitable trait for shape: " + updatedShape.getId());
-                String json = Node.prettyPrintJson(traitValue);
-                System.out.println(json);
-            }
         }
 
         return model;
