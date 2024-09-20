70db983487462270e050502c7d1fc20d6bbc1695
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-09-20 12:37:44 -0700

    chore(middleware-flexible-checksums): re-order checksum implementations based on performance (#6500)

--- packages/middleware-flexible-checksums/src/types.ts
+++ packages/middleware-flexible-checksums/src/types.ts
@@ -12,11 +12,10 @@ export const CLIENT_SUPPORTED_ALGORITHMS = [
 
 /**
  * Priority order for validating checksum algorithm. A faster algorithm has higher priority.
- * ToDo: update the priority order based on profiling of JavaScript implementations.
  */
 export const PRIORITY_ORDER_ALGORITHMS = [
+  ChecksumAlgorithm.SHA256,
+  ChecksumAlgorithm.SHA1,
   ChecksumAlgorithm.CRC32,
   ChecksumAlgorithm.CRC32C,
-  ChecksumAlgorithm.SHA1,
-  ChecksumAlgorithm.SHA256,
 ];
