b9e9e19db75dffc241aaac15e19a57ffa5fd09cf
siddsriv <srivastava41099@gmail.com>
2024-09-27 17:37:45 +0000

    chore(codegen): update typescript integration for codegen change in prev commit

--- codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AddUserAgentDependency.java
+++ codegen/smithy-aws-typescript-codegen/src/main/java/software/amazon/smithy/aws/typescript/codegen/AddUserAgentDependency.java
@@ -1,7 +1,7 @@
 /*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 package software.amazon.smithy.aws.typescript.codegen;
 
