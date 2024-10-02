49e686d11a00430db02c7b8c4d3288409aa30260
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-10-02 21:59:17 +0000

    fix(scripts): explicitly replace '-' to '' in getPackageTags

--- tests/e2e-legacy/getPackageTags.mjs
+++ tests/e2e-legacy/getPackageTags.mjs
@@ -1,5 +1,5 @@
 export const getPackageTags = (packages) =>
   packages
     .map((name) => name.replace("@aws-sdk/client-", ""))
-    .map((name) => name.replace("-"))
+    .map((name) => name.replace("-", ""))
     .map((name) => `@${name}`);
