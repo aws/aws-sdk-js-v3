06dacde53054e8367bcdf8d972de8f8ac56fdc65
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-10-02 21:50:16 +0000

    chore(scripts): use string commands for getting scope

--- tests/e2e-legacy/preview.mjs
+++ tests/e2e-legacy/preview.mjs
@@ -5,17 +5,17 @@ import { getDirName } from "./getDirName.mjs";
 import { getAllTags } from "./getAllTags.mjs";
 import { getPackageTags } from "./getPackageTags.mjs";
 import { runTestForTags } from "./runTestForTags.mjs";
-import conventionalCommitsParser from "conventional-commits-parser";
 
 const __dirname = getDirName();
 
 const execOptions = { ...process, cwd: __dirname, encoding: "utf-8" };
-const gitCommitMessage = execSync(`git show -s --format=%s`, execOptions);
-const { scope } = conventionalCommitsParser.sync(gitCommitMessage);
+const commitMessage = execSync(`git show -s --format=%s`, execOptions);
+const prefix = commitMessage.split(":")[0];
+const scope = prefix.substring(prefix.indexOf("(") + 1, prefix.indexOf(")"));
 console.info(`Updated scope: ${scope}`);
 
 if (!scope) {
-  console.info(`Couldn't find scope in commit message '${gitCommitMessage}'`);
+  console.info(`Couldn't find scope in commit message '${commitMessage}'`);
   process.exit(1);
 }
 
