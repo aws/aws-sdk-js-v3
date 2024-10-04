f114b7a99de2063d2f86ba838830dc512c739edc
George Fu <kuhe@users.noreply.github.com>
2024-10-04 15:28:34 +0000

    chore(clients): codegen sync for unset union payloads, header list parsing

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "80515513f9b4ff3e6140d335a8edfb2126b8c352",
+  SMITHY_TS_COMMIT: "e8b99b1bd437ce3caf83893737858bcba02ed4a8",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
