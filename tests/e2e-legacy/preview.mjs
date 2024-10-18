6b338cd53dfd21f708ee400f9ce79bbc48f3fb0d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-10-18 20:50:33 +0000

    test(scripts): skip if client is not found in commit messages

--- tests/e2e-legacy/preview.mjs
+++ tests/e2e-legacy/preview.mjs
@@ -11,22 +11,24 @@ const __dirname = getDirName();
 const execOptions = { ...process, cwd: __dirname, encoding: "utf-8" };
 const commitsSinceOriginHead = execSync(`git log --oneline origin/main..HEAD --format=%s`, execOptions).split("\n");
 
-const updatedClients = new Set();
+const updatedClientsSet = new Set();
 for (const commitMessage of commitsSinceOriginHead) {
   const prefix = commitMessage.split(":")[0];
   const scope = prefix.substring(prefix.indexOf("(") + 1, prefix.indexOf(")"));
   if (scope && scope.startsWith("client-")) {
-    updatedClients.add(`@aws-sdk/${scope}`);
+    updatedClientsSet.add(`@aws-sdk/${scope}`);
   }
 }
+
+const updatedClients = [...updatedClientsSet];
 console.info(`Updated packages: ${updatedClients}`);
 
-if (updatedClients.size === 0) {
+if (updatedClients.length === 0) {
   console.info(`Couldn't find clients in commit messages:\n '${commitsSinceOriginHead.join("\n")}'`);
-  process.exit(1);
+  process.exit(0);
 }
 
 const allTags = getAllTags();
-const changedPackageTags = getPackageTags([...updatedClients]);
+const changedPackageTags = getPackageTags(updatedClients);
 const tagsToTest = changedPackageTags.filter((tag) => allTags.includes(tag));
 runTestForTags(tagsToTest);
