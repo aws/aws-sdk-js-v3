be9357a740ddb66864dce3f352bb95c5bbdaa65c
George Fu <kuhe@users.noreply.github.com>
2024-10-29 19:46:09 +0000

    test(client-eventbridge): fix crt import

--- clients/client-eventbridge/test/EventBridge.spec.ts
+++ clients/client-eventbridge/test/EventBridge.spec.ts
@@ -1,5 +1,14 @@
 import "@aws-sdk/signature-v4-crt";
 
+import { CrtSignerV4 } from "@aws-sdk/signature-v4-crt";
+import { signatureV4CrtContainer } from "@aws-sdk/signature-v4-multi-region";
+import { crtAvailability } from "@aws-sdk/util-user-agent-node";
+
+// Vite build seems to be unable to recognize this injection
+// code from the signature-v4-crt index module.
+signatureV4CrtContainer.CrtSignerV4 = CrtSignerV4;
+crtAvailability.isCrtAvailable = true;
+
 import { FinalizeRequestMiddleware } from "@aws-sdk/types";
 import { describe, expect, test as it } from "vitest";
 
