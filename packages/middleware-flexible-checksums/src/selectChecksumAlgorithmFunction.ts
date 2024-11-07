2cd531b9f48fabcb49169ee55cd1a1edbaa1e4fd
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-11-07 23:29:17 +0000

    feat(middleware-flexible-checksums): use Node.js native CRC32 checksum API

--- packages/middleware-flexible-checksums/src/selectChecksumAlgorithmFunction.ts
+++ packages/middleware-flexible-checksums/src/selectChecksumAlgorithmFunction.ts
@@ -1,9 +1,9 @@
-import { AwsCrc32 } from "@aws-crypto/crc32";
 import { AwsCrc32c } from "@aws-crypto/crc32c";
 import { ChecksumConstructor, HashConstructor } from "@smithy/types";
 
 import { PreviouslyResolved } from "./configuration";
 import { ChecksumAlgorithm } from "./constants";
+import { getCrc32ChecksumAlgorithmFunction } from "./getCrc32ChecksumAlgorithmFunction";
 
 /**
  * Returns the function that will compute the checksum for the given {@link ChecksumAlgorithm}.
@@ -14,7 +14,7 @@ export const selectChecksumAlgorithmFunction = (
 ): ChecksumConstructor | HashConstructor =>
   ({
     [ChecksumAlgorithm.MD5]: config.md5,
-    [ChecksumAlgorithm.CRC32]: AwsCrc32,
+    [ChecksumAlgorithm.CRC32]: getCrc32ChecksumAlgorithmFunction(),
     [ChecksumAlgorithm.CRC32C]: AwsCrc32c,
     [ChecksumAlgorithm.SHA1]: config.sha1,
     [ChecksumAlgorithm.SHA256]: config.sha256,
