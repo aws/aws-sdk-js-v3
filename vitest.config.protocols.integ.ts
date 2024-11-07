ea3482f4ab989788055038b686735a493288b4ed
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-11-07 08:11:42 -0800

    test(aws-restjson-*): use vitest (#6628)

--- vitest.config.protocols.integ.ts
+++ vitest.config.protocols.integ.ts
@@ -2,12 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: [
-      "**/*/node_modules/**/*.spec.ts",
-      "**/*.{integ,e2e,browser}.spec.ts",
-      "private/aws-restjson-server/**/*.spec.ts",
-      "private/aws-restjson-validation-server/**/*.spec.ts",
-    ],
+    exclude: ["**/*/node_modules/**/*.spec.ts", "**/*.{integ,e2e,browser}.spec.ts"],
     include: ["private/**/*.spec.ts"],
     environment: "node",
     globals: true,
