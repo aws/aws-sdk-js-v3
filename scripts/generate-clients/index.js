4b50eedf7ac64599ef045b2844cc32f190530eea
siddsriv <srivastava41099@gmail.com>
2024-11-13 20:45:56 +0000

    chore(scripts): set env empty for CI

--- scripts/generate-clients/index.js
+++ scripts/generate-clients/index.js
@@ -142,7 +142,7 @@ const {
     }
 
     require("./customizations/workspaces-thin-client")();
-    await spawnProcess("yarn", [], { cwd: REPO_ROOT, stdio: "inherit" });
+    await spawnProcess("yarn", [], { cwd: REPO_ROOT, stdio: "inherit", env: { ...process.env, CI: "" } });
     require("../runtime-dependency-version-check/runtime-dep-version-check");
   } catch (e) {
     console.log(e);
