2968e4fb511aea111b254bb9a5b923c55d94328a
George Fu <kuhe@users.noreply.github.com>
2024-12-09 21:43:12 +0000

    chore(codegen): update for String dispatch fix

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "7f0bf78613de2d09d1648f5deb698e1a6456f251",
+  SMITHY_TS_COMMIT: "ce56edfde025985f769cc34b18e3ee3a99a7942a",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
