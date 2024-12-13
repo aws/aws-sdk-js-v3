6df2e2287f139e8c84706d13ae94a53a616a5c51
siddsriv <srivastava41099@gmail.com>
2024-12-13 16:31:44 +0000

    chore(util-user-agent-node): add deprecated name

--- packages/util-user-agent-node/src/nodeAppIdConfigOptions.ts
+++ packages/util-user-agent-node/src/nodeAppIdConfigOptions.ts
@@ -14,8 +14,13 @@ export const UA_APP_ID_INI_NAME = "sdk_ua_app_id";
 /**
  * @internal
  */
+export const UA_APP_ID_INI_NAME_DEPRECATED = "sdk-ua-app-id";
+
+/**
+ * @internal
+ */
 export const NODE_APP_ID_CONFIG_OPTIONS: LoadedConfigSelectors<string | undefined> = {
   environmentVariableSelector: (env) => env[UA_APP_ID_ENV_NAME],
-  configFileSelector: (profile) => profile[UA_APP_ID_INI_NAME],
+  configFileSelector: (profile) => profile[UA_APP_ID_INI_NAME] ?? profile[UA_APP_ID_INI_NAME_DEPRECATED],
   default: DEFAULT_UA_APP_ID,
 };
