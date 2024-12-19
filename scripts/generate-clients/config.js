8af7a02f5bead379744a6cafde38925fb1789062
George Fu <kuhe@users.noreply.github.com>
2024-12-19 13:28:20 -0500

    chore(codegen): update for operationContextParams fix (#6751)
    * chore(codegen): update for operationContextParams fix
    
    * chore: codegen sync

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "7fa6e5ecec01e783340ccaa8786f1f5786fa4fbc",
+  SMITHY_TS_COMMIT: "35aad304fb4c667ebd1a04be8265993c122e3acd",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
