53504cc1cb6a79409b3f4007c6e1846f759883c4
awstools <awstools@amazon.com>
2024-12-28 04:23:14 +0000

    feat(client-ecr): Restoring custom endpoint functionality for ECR

--- clients/client-ecr/src/endpoint/endpointResolver.ts
+++ clients/client-ecr/src/endpoint/endpointResolver.ts
@@ -8,7 +8,7 @@ import { ruleSet } from "./ruleset";
 
 const cache = new EndpointCache({
   size: 50,
-  params: ["Region", "UseDualStack", "UseFIPS"],
+  params: ["Endpoint", "Region", "UseDualStack", "UseFIPS"],
 });
 
 export const defaultEndpointResolver = (
