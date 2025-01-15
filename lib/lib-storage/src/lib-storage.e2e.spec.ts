4afe35de0a559d2d2a640c831b5b76b230869ee6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-01-15 06:26:25 +0000

    test(lib-storage): add checks for requestChecksumCalculation

--- lib/lib-storage/src/lib-storage.e2e.spec.ts
+++ lib/lib-storage/src/lib-storage.e2e.spec.ts
@@ -24,134 +24,141 @@ describe("@aws-sdk/lib-storage", () => {
     Key = ``;
     data = randomBytes(20_240_000);
     dataString = data.toString();
-
-    client = new S3({
-      region,
-    });
   });
 
-  describe("Upload", () => {
-    beforeAll(() => {
-      Key = `multi-part-file-${Date.now()}`;
-    });
-    afterAll(async () => {
-      await client.deleteObject({ Bucket, Key });
-    });
+  describe.each([undefined, "WHEN_REQUIRED", "WHEN_SUPPORTED"])(
+    "requestChecksumCalculation: %s",
+    (requestChecksumCalculation) => {
+      beforeAll(() => {
+        // @ts-expect-error: Types of property 'requestChecksumCalculation' are incompatible
+        client = new S3({
+          region,
+          requestChecksumCalculation,
+        });
+        Key = `multi-part-file-${Date.now()}`;
+      });
 
-    it("should upload in parts for input type bytes", async () => {
-      const s3Upload = new Upload({
-        client,
-        params: {
+      afterAll(async () => {
+        await client.deleteObject({ Bucket, Key });
+      });
+
+      it("should upload in parts for input type bytes", async () => {
+        const s3Upload = new Upload({
+          client,
+          params: {
+            Bucket,
+            Key,
+            Body: data,
+          },
+        });
+        await s3Upload.done();
+
+        const object = await client.getObject({
           Bucket,
           Key,
-          Body: data,
-        },
-      });
-      await s3Upload.done();
+        });
 
-      const object = await client.getObject({
-        Bucket,
-        Key,
+        expect(await object.Body?.transformToString()).toEqual(dataString);
       });
 
-      expect(await object.Body?.transformToString()).toEqual(dataString);
-    });
+      it("should upload in parts for input type string", async () => {
+        const s3Upload = new Upload({
+          client,
+          params: {
+            Bucket,
+            Key,
+            Body: dataString,
+          },
+        });
+        await s3Upload.done();
 
-    it("should upload in parts for input type string", async () => {
-      const s3Upload = new Upload({
-        client,
-        params: {
+        const object = await client.getObject({
           Bucket,
           Key,
-          Body: dataString,
-        },
-      });
-      await s3Upload.done();
+        });
 
-      const object = await client.getObject({
-        Bucket,
-        Key,
+        expect(await object.Body?.transformToString()).toEqual(dataString);
       });
 
-      expect(await object.Body?.transformToString()).toEqual(dataString);
-    });
+      it("should upload in parts for input type Readable", async () => {
+        const s3Upload = new Upload({
+          client,
+          params: {
+            Bucket,
+            Key,
+            Body: Readable.from(data),
+          },
+        });
+        await s3Upload.done();
 
-    it("should upload in parts for input type Readable", async () => {
-      const s3Upload = new Upload({
-        client,
-        params: {
+        const object = await client.getObject({
           Bucket,
           Key,
-          Body: Readable.from(data),
-        },
-      });
-      await s3Upload.done();
+        });
 
-      const object = await client.getObject({
-        Bucket,
-        Key,
+        expect(await object.Body?.transformToString()).toEqual(dataString);
       });
 
-      expect(await object.Body?.transformToString()).toEqual(dataString);
-    });
-
-    it("should call AbortMultipartUpload if unable to complete a multipart upload.", async () => {
-      class MockFailureS3 extends S3 {
-        public counter = 0;
-        async send(command: any, ...rest: any[]) {
-          if (command?.constructor?.name === "UploadPartCommand" && this.counter++ % 3 === 0) {
-            throw new Error("simulated upload part error");
+      it("should call AbortMultipartUpload if unable to complete a multipart upload.", async () => {
+        class MockFailureS3 extends S3 {
+          public counter = 0;
+          async send(command: any, ...rest: any[]) {
+            if (command?.constructor?.name === "UploadPartCommand" && this.counter++ % 3 === 0) {
+              throw new Error("simulated upload part error");
+            }
+            return super.send(command, ...rest);
           }
-          return super.send(command, ...rest);
         }
-      }
 
-      const client = new MockFailureS3({
-        region,
+        const client = new MockFailureS3({
+          region,
+        });
+
+        const requestLog = [] as string[];
+
+        client.middlewareStack.add(
+          (next, context) => async (args) => {
+            const result = await next(args);
+            requestLog.push(
+              [context.clientName, context.commandName, result.output.$metadata.httpStatusCode].join(" ")
+            );
+            return result;
+          },
+          {
+            name: "E2eRequestLog",
+            step: "build",
+            override: true,
+          }
+        );
+
+        const s3Upload = new Upload({
+          client,
+          params: {
+            Bucket,
+            Key,
+            Body: data,
+          },
+        });
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        await s3Upload.done().catch((ignored) => {});
+
+        const uploadStatus = await client
+          .listParts({
+            Bucket,
+            Key,
+            UploadId: s3Upload.uploadId,
+          })
+          .then((listParts) => listParts.$metadata.httpStatusCode)
+          .catch((err) => err.toString());
+
+        expect(uploadStatus).toMatch(/NoSuchUpload:(.*?)aborted or completed\./);
+        expect(requestLog).toEqual([
+          "S3Client CreateMultipartUploadCommand 200",
+          "S3Client UploadPartCommand 200",
+          "S3Client UploadPartCommand 200",
+          "S3Client AbortMultipartUploadCommand 204",
+        ]);
       });
-
-      const requestLog = [] as string[];
-
-      client.middlewareStack.add(
-        (next, context) => async (args) => {
-          const result = await next(args);
-          requestLog.push([context.clientName, context.commandName, result.output.$metadata.httpStatusCode].join(" "));
-          return result;
-        },
-        {
-          name: "E2eRequestLog",
-          step: "build",
-          override: true,
-        }
-      );
-
-      const s3Upload = new Upload({
-        client,
-        params: {
-          Bucket,
-          Key,
-          Body: data,
-        },
-      });
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      await s3Upload.done().catch((ignored) => {});
-
-      const uploadStatus = await client
-        .listParts({
-          Bucket,
-          Key,
-          UploadId: s3Upload.uploadId,
-        })
-        .then((listParts) => listParts.$metadata.httpStatusCode)
-        .catch((err) => err.toString());
-
-      expect(uploadStatus).toMatch(/NoSuchUpload:(.*?)aborted or completed\./);
-      expect(requestLog).toEqual([
-        "S3Client CreateMultipartUploadCommand 200",
-        "S3Client UploadPartCommand 200",
-        "S3Client UploadPartCommand 200",
-        "S3Client AbortMultipartUploadCommand 204",
-      ]);
-    });
-  });
+    }
+  );
 }, 45_000);
