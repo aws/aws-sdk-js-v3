0e8963ea2fe4d520cdb9987a0decd8cb792be7c7
George Fu <kuhe@users.noreply.github.com>
2025-01-22 20:46:44 -0500

    chore(codegen): bump smithy-typescript-codegen to 0.26.0 (#6843)
    Co-authored-by: Trivikram Kamat <16024985+trivikr@users.noreply.github.com>

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -31,7 +31,7 @@ allprojects {
         mavenCentral()
     }
     group = "software.amazon.smithy.typescript"
-    version = "0.25.0"
+    version = "0.26.0"
 }
 
 // The root project doesn't produce a JAR.
--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "de3a850552111a7018df855f808258074a90ab62",
+  SMITHY_TS_COMMIT: "fbe3c04b5627a8aea693b5bfc1598adbac0213d5",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
