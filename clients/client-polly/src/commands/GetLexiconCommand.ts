217e1cb29a3f5908ad68416b3348e97e67608ec6
awstools <awstools@amazon.com>
2025-02-12 19:15:27 +0000

    feat(client-polly): Added support for the new voice - Jasmine (en-SG). Jasmine is available as a Neural voice only.

--- clients/client-polly/src/commands/GetLexiconCommand.ts
+++ clients/client-polly/src/commands/GetLexiconCommand.ts
@@ -48,7 +48,7 @@ export interface GetLexiconCommandOutput extends GetLexiconOutput, __MetadataBea
  * //   },
  * //   LexiconAttributes: { // LexiconAttributes
  * //     Alphabet: "STRING_VALUE",
- * //     LanguageCode: "arb" || "cmn-CN" || "cy-GB" || "da-DK" || "de-DE" || "en-AU" || "en-GB" || "en-GB-WLS" || "en-IN" || "en-US" || "es-ES" || "es-MX" || "es-US" || "fr-CA" || "fr-FR" || "is-IS" || "it-IT" || "ja-JP" || "hi-IN" || "ko-KR" || "nb-NO" || "nl-NL" || "pl-PL" || "pt-BR" || "pt-PT" || "ro-RO" || "ru-RU" || "sv-SE" || "tr-TR" || "en-NZ" || "en-ZA" || "ca-ES" || "de-AT" || "yue-CN" || "ar-AE" || "fi-FI" || "en-IE" || "nl-BE" || "fr-BE" || "cs-CZ" || "de-CH",
+ * //     LanguageCode: "arb" || "cmn-CN" || "cy-GB" || "da-DK" || "de-DE" || "en-AU" || "en-GB" || "en-GB-WLS" || "en-IN" || "en-US" || "es-ES" || "es-MX" || "es-US" || "fr-CA" || "fr-FR" || "is-IS" || "it-IT" || "ja-JP" || "hi-IN" || "ko-KR" || "nb-NO" || "nl-NL" || "pl-PL" || "pt-BR" || "pt-PT" || "ro-RO" || "ru-RU" || "sv-SE" || "tr-TR" || "en-NZ" || "en-ZA" || "ca-ES" || "de-AT" || "yue-CN" || "ar-AE" || "fi-FI" || "en-IE" || "nl-BE" || "fr-BE" || "cs-CZ" || "de-CH" || "en-SG",
  * //     LastModified: new Date("TIMESTAMP"),
  * //     LexiconArn: "STRING_VALUE",
  * //     LexemesCount: Number("int"),
--- clients/client-polly/src/commands/ListLexiconsCommand.ts
+++ clients/client-polly/src/commands/ListLexiconsCommand.ts
@@ -46,7 +46,7 @@ export interface ListLexiconsCommandOutput extends ListLexiconsOutput, __Metadat
  * //       Name: "STRING_VALUE",
  * //       Attributes: { // LexiconAttributes
  * //         Alphabet: "STRING_VALUE",
- * //         LanguageCode: "arb" || "cmn-CN" || "cy-GB" || "da-DK" || "de-DE" || "en-AU" || "en-GB" || "en-GB-WLS" || "en-IN" || "en-US" || "es-ES" || "es-MX" || "es-US" || "fr-CA" || "fr-FR" || "is-IS" || "it-IT" || "ja-JP" || "hi-IN" || "ko-KR" || "nb-NO" || "nl-NL" || "pl-PL" || "pt-BR" || "pt-PT" || "ro-RO" || "ru-RU" || "sv-SE" || "tr-TR" || "en-NZ" || "en-ZA" || "ca-ES" || "de-AT" || "yue-CN" || "ar-AE" || "fi-FI" || "en-IE" || "nl-BE" || "fr-BE" || "cs-CZ" || "de-CH",
+ * //         LanguageCode: "arb" || "cmn-CN" || "cy-GB" || "da-DK" || "de-DE" || "en-AU" || "en-GB" || "en-GB-WLS" || "en-IN" || "en-US" || "es-ES" || "es-MX" || "es-US" || "fr-CA" || "fr-FR" || "is-IS" || "it-IT" || "ja-JP" || "hi-IN" || "ko-KR" || "nb-NO" || "nl-NL" || "pl-PL" || "pt-BR" || "pt-PT" || "ro-RO" || "ru-RU" || "sv-SE" || "tr-TR" || "en-NZ" || "en-ZA" || "ca-ES" || "de-AT" || "yue-CN" || "ar-AE" || "fi-FI" || "en-IE" || "nl-BE" || "fr-BE" || "cs-CZ" || "de-CH" || "en-SG",
  * //         LastModified: new Date("TIMESTAMP"),
  * //         LexiconArn: "STRING_VALUE",
  * //         LexemesCount: Number("int"),
