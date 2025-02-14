aa019359accbba11b9ae291cb6a76ed3768cddb7
George Fu <kuhe@users.noreply.github.com>
2025-02-14 12:01:10 -0500

    chore(codegen): sync for stream buffering

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "f0562fe63a332e520739bf7f9d0972f26cedac41",
+  SMITHY_TS_COMMIT: "cc1095b8bb73554bf2d64ef96608f9bb2405f442",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
