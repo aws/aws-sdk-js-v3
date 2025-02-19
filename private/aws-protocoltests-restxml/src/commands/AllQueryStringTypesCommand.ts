8771dc8ef9be4428cf35cb652789d5be0ce4faad
George Fu <kuhe@users.noreply.github.com>
2025-02-19 13:47:28 -0500

    test(client-secrets-manager): schema testing

--- private/aws-protocoltests-restxml/src/commands/AllQueryStringTypesCommand.ts
+++ private/aws-protocoltests-restxml/src/commands/AllQueryStringTypesCommand.ts
@@ -1,11 +1,10 @@
 // smithy-typescript generated code
-import { getSerdePlugin } from "@smithy/middleware-serde";
 import { Command as $Command } from "@smithy/smithy-client";
 import { MetadataBearer as __MetadataBearer } from "@smithy/types";
 
 import { AllQueryStringTypesInput } from "../models/models_0";
-import { de_AllQueryStringTypesCommand, se_AllQueryStringTypesCommand } from "../protocols/Aws_restXml";
 import { RestXmlProtocolClientResolvedConfig, ServiceInputTypes, ServiceOutputTypes } from "../RestXmlProtocolClient";
+import { AllQueryStringTypes } from "../schemas/aws.protocoltests.restxml";
 
 /**
  * @public
@@ -102,13 +101,12 @@ export class AllQueryStringTypesCommand extends $Command
     ServiceOutputTypes
   >()
   .m(function (this: any, Command: any, cs: any, config: RestXmlProtocolClientResolvedConfig, o: any) {
-    return [getSerdePlugin(config, this.serialize, this.deserialize)];
+    return [];
   })
   .s("RestXml", "AllQueryStringTypes", {})
   .n("RestXmlProtocolClient", "AllQueryStringTypesCommand")
   .f(void 0, void 0)
-  .ser(se_AllQueryStringTypesCommand)
-  .de(de_AllQueryStringTypesCommand)
+  .sc(AllQueryStringTypes)
   .build() {
   /** @internal type navigation helper, not in runtime. */
   protected declare static __types: {
--- private/aws-protocoltests-restxml/src/commands/HttpPayloadTraitsWithMediaTypeCommand.ts
+++ private/aws-protocoltests-restxml/src/commands/HttpPayloadTraitsWithMediaTypeCommand.ts
@@ -1,15 +1,11 @@
 // smithy-typescript generated code
-import { getSerdePlugin } from "@smithy/middleware-serde";
 import { Command as $Command } from "@smithy/smithy-client";
 import { BlobPayloadInputTypes, MetadataBearer as __MetadataBearer } from "@smithy/types";
 import { Uint8ArrayBlobAdapter } from "@smithy/util-stream";
 
 import { HttpPayloadTraitsWithMediaTypeInputOutput } from "../models/models_0";
-import {
-  de_HttpPayloadTraitsWithMediaTypeCommand,
-  se_HttpPayloadTraitsWithMediaTypeCommand,
-} from "../protocols/Aws_restXml";
 import { RestXmlProtocolClientResolvedConfig, ServiceInputTypes, ServiceOutputTypes } from "../RestXmlProtocolClient";
+import { HttpPayloadTraitsWithMediaType } from "../schemas/aws.protocoltests.restxml";
 
 /**
  * @public
@@ -90,13 +86,12 @@ export class HttpPayloadTraitsWithMediaTypeCommand extends $Command
     ServiceOutputTypes
   >()
   .m(function (this: any, Command: any, cs: any, config: RestXmlProtocolClientResolvedConfig, o: any) {
-    return [getSerdePlugin(config, this.serialize, this.deserialize)];
+    return [];
   })
   .s("RestXml", "HttpPayloadTraitsWithMediaType", {})
   .n("RestXmlProtocolClient", "HttpPayloadTraitsWithMediaTypeCommand")
   .f(void 0, void 0)
-  .ser(se_HttpPayloadTraitsWithMediaTypeCommand)
-  .de(de_HttpPayloadTraitsWithMediaTypeCommand)
+  .sc(HttpPayloadTraitsWithMediaType)
   .build() {
   /** @internal type navigation helper, not in runtime. */
   protected declare static __types: {
