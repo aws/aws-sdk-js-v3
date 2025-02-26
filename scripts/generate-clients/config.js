cb6241022ee54fd14cdc4b59870bc05d69df801c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-02-26 18:51:28 +0000

    fix(codegen): variable allocation in StringStore

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "2c1235539947ee31c3324133a1eefc5db77d487b",
+  SMITHY_TS_COMMIT: "9b26765f52f8e8d0d153d74249aa5785adb121af",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
