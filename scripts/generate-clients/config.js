7d22b4bf941fbe0ec47aefeb2222f7fb8083f876
Sreeja Milkuri <smilkuri@amazon.com>
2025-03-03 22:45:25 -0500

    chore(codegen): upgrade to smithy 1.54.0 (#6913)
    * upgrade to smithy 1.54.0
    
    * update smithy-ts commit
    
    * Revert "update smithy-ts commit"
    
    This reverts commit 12864e931f695dc60de53c056489368aab046dc2.
    
    * update smithy-ts commit

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "5aeb781faf8535a352161fbfaaf5f8013fd6a35d",
+  SMITHY_TS_COMMIT: "ee3ec22a19cd4d7e5cb1721e0848d6afa98ab6fb",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
