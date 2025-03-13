a27f1b41e3774f5ebba769badc1623ae2f086d81
awstools <awstools@amazon.com>
2025-03-13 18:12:13 +0000

    feat(client-cloudwatch-logs): Updated CreateLogAnomalyDetector to accept only kms key arn

--- clients/client-cloudwatch-logs/src/commands/GetLogEventsCommand.ts
+++ clients/client-cloudwatch-logs/src/commands/GetLogEventsCommand.ts
@@ -30,15 +30,29 @@ export interface GetLogEventsCommandOutput extends GetLogEventsResponse, __Metad
 /**
  * <p>Lists log events from the specified log stream. You can list all of the log events or
  *       filter using a time range.</p>
- *          <p>By default, this operation returns as many log events as can fit in a response size of 1MB (up to 10,000 log events).
- *       You can get additional log events by specifying one of the tokens in a subsequent call.
- *       This operation can return empty results while there are more log events available through the token.</p>
+ *          <p>
+ *             <code>GetLogEvents</code> is a paginated operation. Each page returned can contain up to 1 MB of log events or up to 10,000 log events. A returned page might only be partially full, or even empty.
+ *       For example, if the result of a query would return 15,000 log events, the first page isn't guaranteed to have 10,000 log events even if they all fit into 1 MB.</p>
+ *          <p>Partially full or empty pages
+ *       don't necessarily mean that pagination is finished. As long as the <code>nextBackwardToken</code> or <code>nextForwardToken</code> returned is NOT equal to the <code>nextToken</code>
+ *       that you passed into the API call, there might be more
+ *       log events available. The token that you use depends on the direction you want to move in along the log stream. The returned tokens are never null.</p>
+ *          <note>
+ *             <p>If you set <code>startFromHead</code> to <code>true</code> and you don’t include <code>endTime</code> in your request, you can end up in a situation where the pagination doesn't terminate.
+ *       This can happen when the new log events are being added to the target log streams
+ *       faster than they are being read. This situation is a good use case for the CloudWatch Logs <a href="https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/CloudWatchLogs_LiveTail.html">Live Tail</a> feature.</p>
+ *          </note>
  *          <p>If you are using CloudWatch cross-account observability, you can use this operation in a monitoring account and
  *       view data from the linked source accounts. For more information, see
  *       <a href="https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Unified-Cross-Account.html">CloudWatch cross-account observability</a>.</p>
  *          <p>You can specify the log group to search by using either <code>logGroupIdentifier</code> or <code>logGroupName</code>.
  *       You must include one of these two parameters, but you can't include both.
  *     </p>
+ *          <note>
+ *             <p>If you are using <a href="https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/CloudWatch-Logs-Transformation.html">log transformation</a>, the <code>GetLogEvents</code> operation returns only the original versions of log events, before they
+ *       were transformed. To view the transformed versions, you must use a <a href="https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/AnalyzingLogData.html">CloudWatch Logs query.</a>
+ *             </p>
+ *          </note>
  * @example
  * Use a bare-bones client and the command you need to make an API call.
  * ```javascript
