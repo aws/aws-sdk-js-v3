4b25098e29c7b74527ea6f232e3223e10386e440
George Fu <kuhe@users.noreply.github.com>
2025-03-21 10:43:43 -0400

    chore(clients): update smithy version for httpAuthScheme resolver

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "9ffe82d60dd45787a4279eb9723290edb39d0e10",
+  SMITHY_TS_COMMIT: "6e6e5722397bd95b20ff09154db54a7c67e947d6",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
