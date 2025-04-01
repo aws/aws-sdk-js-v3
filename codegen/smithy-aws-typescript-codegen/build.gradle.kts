13d545e102f2ed1df0427273c735cc89ea017aa7
smilkuri <smilkuri@amazon.com>
2025-04-01 20:57:04 +0000

    Bump codegen version to 0.28.0

--- codegen/smithy-aws-typescript-codegen/build.gradle.kts
+++ codegen/smithy-aws-typescript-codegen/build.gradle.kts
@@ -36,7 +36,7 @@ buildscript {
 
 dependencies {
     // Smithy TypeScript
-    api("software.amazon.smithy.typescript:smithy-typescript-codegen:0.27.0")
+    api("software.amazon.smithy.typescript:smithy-typescript-codegen:0.28.0")
 
     // Smithy generic dependencies
     api("software.amazon.smithy:smithy-model:$smithyVersion")
