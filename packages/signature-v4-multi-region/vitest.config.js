b5b0646116218d9c1c477a4f2813927bbb4b8a86
siddsriv <srivastava41099@gmail.com>
2025-04-01 18:10:05 +0000

    chore(signature-v4-multi-region): resolve deps

--- packages/signature-v4-multi-region/vitest.config.js
+++ packages/signature-v4-multi-region/vitest.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vitest/config";
-import baseConfig from "../../vitest.config.base.js";
 
 export default defineConfig({
-  ...baseConfig,
   test: {
-    ...baseConfig.test,
+    environment: "node",
     exclude: ["**/node_modules/**", "**/*.e2e.spec.ts", "**/browser.spec.ts"],
   },
 });
