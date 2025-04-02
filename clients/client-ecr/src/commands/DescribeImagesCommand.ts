873da427e825cf2cfd53d957b329d12e86da852c
awstools <awstools@amazon.com>
2025-04-02 18:21:13 +0000

    docs(client-ecr): Fix for customer issues related to AWS account ID and size limitation for token.

--- clients/client-ecr/src/commands/DescribeImagesCommand.ts
+++ clients/client-ecr/src/commands/DescribeImagesCommand.ts
@@ -30,10 +30,10 @@ export interface DescribeImagesCommandOutput extends DescribeImagesResponse, __M
 /**
  * <p>Returns metadata about the images in a repository.</p>
  *          <note>
- *             <p>Beginning with Docker version 1.9, the Docker client compresses image layers
- *                 before pushing them to a V2 Docker registry. The output of the <code>docker
- *                     images</code> command shows the uncompressed image size, so it may return a
- *                 larger image size than the image sizes returned by <a>DescribeImages</a>.</p>
+ *             <p>Starting with Docker version 1.9, the Docker client compresses image layers before
+ *                 pushing them to a V2 Docker registry. The output of the <code>docker images</code>
+ *                 command shows the uncompressed image size. Therefore, Docker might return a larger
+ *                 image than the image shown in the Amazon Web Services Management Console.</p>
  *          </note>
  * @example
  * Use a bare-bones client and the command you need to make an API call.
