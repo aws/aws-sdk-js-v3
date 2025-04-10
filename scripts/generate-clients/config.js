a7d9c1f9252b68a9f470743260b6ac50e013271e
George Fu <kuhe@users.noreply.github.com>
2025-04-10 12:52:20 -0400

    chore(codegen): bring in fix for operationContextParams (#7004)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "76264f4925e5cf6eb659be4dbb786ec5de817d89",
+  SMITHY_TS_COMMIT: "a821f249cd7e1c3e5926e97140494b7ef7fdda22",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
