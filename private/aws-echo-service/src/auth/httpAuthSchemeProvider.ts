4698c5d40e06ea7c15c66a4b1a13b15b0e0b4645
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-04-23 21:41:10 +0000

    chore(private): add config authSchemePreference

--- private/aws-echo-service/src/auth/httpAuthSchemeProvider.ts
+++ private/aws-echo-service/src/auth/httpAuthSchemeProvider.ts
@@ -7,8 +7,9 @@ import {
   HttpAuthSchemeParameters,
   HttpAuthSchemeParametersProvider,
   HttpAuthSchemeProvider,
+  Provider,
 } from "@smithy/types";
-import { getSmithyContext } from "@smithy/util-middleware";
+import { getSmithyContext, normalizeProvider } from "@smithy/util-middleware";
 
 /**
  * @internal
@@ -69,6 +70,11 @@ export const defaultEchoServiceHttpAuthSchemeProvider: EchoServiceHttpAuthScheme
  */
 export interface HttpAuthSchemeInputConfig {
   /**
+   * A comma-separated list of case-sensitive auth scheme names. An auth scheme name is a fully qualified auth scheme ID with the namespace prefix trimmed. For example, the auth scheme with ID aws.auth#sigv4 is named sigv4. @public
+   */
+  authSchemePreference?: string[] | Provider<string[]>;
+
+  /**
    * Configuration of HttpAuthSchemes for a client which provides default identity providers and signers per auth scheme.
    * @internal
    */
@@ -85,6 +91,11 @@ export interface HttpAuthSchemeInputConfig {
  * @internal
  */
 export interface HttpAuthSchemeResolvedConfig {
+  /**
+   * A comma-separated list of case-sensitive auth scheme names. An auth scheme name is a fully qualified auth scheme ID with the namespace prefix trimmed. For example, the auth scheme with ID aws.auth#sigv4 is named sigv4. @public
+   */
+  readonly authSchemePreference: Provider<string[]>;
+
   /**
    * Configuration of HttpAuthSchemes for a client which provides default identity providers and signers per auth scheme.
    * @internal
@@ -104,5 +115,7 @@ export interface HttpAuthSchemeResolvedConfig {
 export const resolveHttpAuthSchemeConfig = <T>(
   config: T & HttpAuthSchemeInputConfig
 ): T & HttpAuthSchemeResolvedConfig => {
-  return Object.assign(config, {}) as T & HttpAuthSchemeResolvedConfig;
+  return Object.assign(config, {
+    authSchemePreference: normalizeProvider(config.authSchemePreference ?? []),
+  }) as T & HttpAuthSchemeResolvedConfig;
 };
