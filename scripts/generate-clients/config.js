18b3ec711c35217560121673ecd7adb8bf5d4cb8
smilkuri <smilkuri@amazon.com>
2025-05-02 16:34:40 +0000

    chore(codegen): update commit hash

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "57e0a48e1d5b66d0bbc6dc42eae5e1200347d415",
+  SMITHY_TS_COMMIT: "b01c20d144cf1b1fffb9fe8eb35c47e864c1d04a",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
