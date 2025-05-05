f9612248e399fa00c54cab1ab3345a6494fd251d
Sreeja Milkuri <smilkuri@amazon.com>
2025-05-05 09:35:34 -0700

    chore(codegen): Upgrade to Smithy 1.57.1 (#7049)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "b01c20d144cf1b1fffb9fe8eb35c47e864c1d04a",
+  SMITHY_TS_COMMIT: "fb9d29d3c47ee3dd3218f7074959faf05366b93c",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
