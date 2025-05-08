801cece8b01e86605b521e99f419f5017f5635fe
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-05-08 08:55:34 -0700

    chore(deps): pass signing name to env credential provider (#7064)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "9fdaf206204eae04bef579e5cacb1de26edb6df9",
+  SMITHY_TS_COMMIT: "c97174319299907bae200e58697b0b602af54edb",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
