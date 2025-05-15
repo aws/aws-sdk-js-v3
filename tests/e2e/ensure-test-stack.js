ab952cb61343ce66bd848ff9848f3173419d2880
Sreeja Milkuri <smilkuri@amazon.com>
2025-05-15 12:19:30 -0400

    fix(tests): remove the catch statement (#7081)

--- tests/e2e/ensure-test-stack.js
+++ tests/e2e/ensure-test-stack.js
@@ -45,14 +45,12 @@ exports.ensureTestStack = async (client, stackName, templateBody) => {
     // TODO: Feature Request: return the last describe response from the waiter result, so we can inspect failure reasons.
     await waitUntilChangeSetCreateComplete({ client, maxWaitTime: 300 }, { ChangeSetName: Id });
   } catch (e) {
-    const { Status, StatusReason = "" } = await client
-      .send(
-        new DescribeChangeSetCommand({
-          StackName: stackName,
-          ChangeSetName: Id,
-        })
-      )
-      .catch((e) => console.warn("Failed to describe changeset", e));
+    const { Status, StatusReason = "" } = await client.send(
+      new DescribeChangeSetCommand({
+        StackName: stackName,
+        ChangeSetName: Id,
+      })
+    );
     if (Status === "FAILED" && StatusReason.includes("The submitted information didn't contain changes")) {
       await client
         .send(
