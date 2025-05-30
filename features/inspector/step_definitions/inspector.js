ec701a0738e36d27e6c763ccc829c880f3307bd3
George Fu <kuhe@users.noreply.github.com>
2025-05-29 20:29:15 -0400

    test(inspector): remove unused test

--- features/inspector/step_definitions/inspector.js
+++
@@ -1,7 +0,0 @@
-const { Before } = require("@cucumber/cucumber");
-
-Before({ tags: "@inspector" }, function (scenario, callback) {
-  const { Inspector } = require("../../../clients/client-inspector");
-  this.service = new Inspector({ region: "us-west-2" });
-  callback();
-});
