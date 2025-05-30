2c415d34f22d96147b41c90df541aec248b3237f
George Fu <kuhe@users.noreply.github.com>
2025-05-29 21:41:25 -0400

    feat(core/protocols): runtime protocol classes

---
+++ packages/core/src/submodules/protocols/json/jsonReplacer.spec.ts
@@ -0,0 +1,42 @@
+import { NumericValue } from "@smithy/core/serde";
+import { describe, expect, test as it } from "vitest";
+
+import { JsonReplacer } from "./jsonReplacer";
+
+describe(JsonReplacer.name, () => {
+  it("serializes bigint to JSON number", async () => {
+    const jsonReplacer = new JsonReplacer();
+    const data = {
+      bigint: 1000000000000000000000000000054321n,
+    };
+    const serialized = jsonReplacer.replaceInJson(JSON.stringify(data, jsonReplacer.createReplacer(), 2));
+
+    expect(serialized).toEqual(`{
+  "bigint": 1000000000000000000000000000054321
+}`);
+  });
+
+  it("serializes NumericValue to JSON number", async () => {
+    const jsonReplacer = new JsonReplacer();
+    const data = {
+      numericValue: new NumericValue("0.1000000000000000000000000000054321", "bigDecimal"),
+    };
+    const serialized = jsonReplacer.replaceInJson(JSON.stringify(data, jsonReplacer.createReplacer(), 2));
+
+    expect(serialized).toEqual(`{
+  "numericValue": 0.1000000000000000000000000000054321
+}`);
+  });
+
+  it("has lifecycle validation", async () => {
+    const jsonReplacer = new JsonReplacer();
+    expect(() => jsonReplacer.replaceInJson("")).toThrow();
+    jsonReplacer.createReplacer();
+    expect(() => jsonReplacer.createReplacer()).toThrow();
+    jsonReplacer.replaceInJson("");
+    expect(() => jsonReplacer.replaceInJson("")).toThrow();
+    expect(() => jsonReplacer.replaceInJson("")).toThrow();
+    expect(() => jsonReplacer.createReplacer()).toThrow();
+    expect(() => jsonReplacer.createReplacer()).toThrow();
+  });
+});
---
+++ packages/core/src/submodules/protocols/xml/XmlShapeSerializer.spec.ts
@@ -0,0 +1,45 @@
+import { SCHEMA } from "@smithy/core/schema";
+import { NumericValue } from "@smithy/core/serde";
+import { describe, expect, test as it } from "vitest";
+
+import { widget } from "../test-schema.spec";
+import { simpleFormatXml } from "./simpleFormatXml";
+import { XmlShapeSerializer } from "./XmlShapeSerializer";
+
+describe(XmlShapeSerializer.name, () => {
+  it("serializes data to Query", async () => {
+    const serializer = new XmlShapeSerializer({
+      xmlNamespace: "namespace",
+      serviceNamespace: "namespace",
+      timestampFormat: { default: SCHEMA.TIMESTAMP_DATE_TIME, useTrait: true },
+    });
+    serializer.setSerdeContext({
+      base64Encoder: (input: Uint8Array) => {
+        return Buffer.from(input).toString("base64");
+      },
+    } as any);
+
+    const data = {
+      timestamp: new Date(0),
+      bigint: 10000000000000000000000054321n,
+      bigdecimal: new NumericValue("0.10000000000000000000000054321", "bigDecimal"),
+      blob: new Uint8Array([0, 0, 0, 1]),
+    };
+    serializer.write(widget, data);
+    const serialization = serializer.flush();
+    expect(simpleFormatXml(serialization as string)).toEqual(`<Struct xmlns="namespace">
+  <blob>
+    AAAAAQ==
+  </blob>
+  <timestamp>
+    0
+  </timestamp>
+  <bigint>
+    10000000000000000000000054321
+  </bigint>
+  <bigdecimal>
+    0.10000000000000000000000054321
+  </bigdecimal>
+</Struct>`);
+  });
+});
