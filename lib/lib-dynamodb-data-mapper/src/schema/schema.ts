d6bbdad644aaf0e52e6d10aee512735c973aada4
Andrei Popovici <popovici.andrey@gmail.com>
2025-06-02 23:37:04 +0300

    fix: update versionion to fix husky precommit error

--- lib/lib-dynamodb-data-mapper/src/schema/schema.ts
+++ lib/lib-dynamodb-data-mapper/src/schema/schema.ts
@@ -1,26 +1,26 @@
-import {isSchemaType, SchemaType} from "./schemaType";
+import { isSchemaType, SchemaType } from "./schemaType";
 
 /**
- * A key => value mapping outlining how to convert an arbitrary JavaScript
+ * A key-to-value mapping outlining how to convert an arbitrary JavaScript
  * object into a strongly typed DynamoDB AttributeMap and back again.
  */
 export interface Schema {
-    [key: string]: SchemaType;
+  [key: string]: SchemaType;
 }
 
 /**
  * Evaluates whether the provided argument is a Schema object
  */
 export function isSchema(arg: any): arg is Schema {
-    if (!Boolean(arg) || typeof arg !== 'object') {
-        return false;
-    }
+  if (!Boolean(arg) || typeof arg !== "object") {
+    return false;
+  }
 
-    for (const key of Object.keys(arg)) {
-        if (!isSchemaType(arg[key])) {
-            return false;
-        }
+  for (const key of Object.keys(arg)) {
+    if (!isSchemaType(arg[key])) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
