d7e8fb4d8aac9d8e8ee80a2f976bf0ab240bbfe8
George Fu <kuhe@users.noreply.github.com>
2025-06-06 12:56:44 -0400

    chore(codegen): upgrade to smithy 1.58.0 (#7119)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "5ab4f99fa91fa97d2ff13e74cd2a2a88c364a503",
+  SMITHY_TS_COMMIT: "cc243072b2c38daaa84fcbddd07dcfa7227e284e",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
