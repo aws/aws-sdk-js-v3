cb55d8225983fcaf3bce7f1f506a5bb4eedb6006
Andrei Popovici <popovici.andrey@gmail.com>
2025-06-13 10:46:38 +0300

    fix: remove unnecesary exports from main index file

--- lib/lib-dynamodb-data-mapper/src/index.ts
+++ lib/lib-dynamodb-data-mapper/src/index.ts
@@ -1,3 +1,2 @@
 export * from "./DataMapper";
 export type { ModelConstructor } from "./schema";
-export type { PutCommandInput, GetCommandInput, GetCommandOutput, PutCommandOutput } from './commands/'
