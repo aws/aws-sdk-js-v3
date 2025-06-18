9c2f63bce00f2f40ac5fe1a6b40ff1f09eed9f42
Joel Scheuner <joel.scheuner.dev@gmail.com>
2025-06-18 20:48:17 +0200

    Fix type error and switch to undefined/auto detection

--- clients/client-s3/src/endpoint/EndpointParameters.ts
+++ clients/client-s3/src/endpoint/EndpointParameters.ts
@@ -24,16 +24,16 @@ export type ClientResolvedEndpointParameters = ClientInputEndpointParameters & {
 export const resolveClientEndpointParameters = <T>(
   options: T & ClientInputEndpointParameters
 ): T & ClientResolvedEndpointParameters => {
-  let forcePathStyleDefault = false;
   // Custom endpoints are incompatible with virtual-host-style, unless
   // the endpoint includes "s3." to allow for virtual-host-style testing.
-  if (options.endpoint && !options.useAccelerateEndpoint && !options.endpoint.includes("s3.")) {
-    forcePathStyleDefault = true;
+  let forcePathStyleOverride = undefined;
+  if (options.endpoint && !options.useAccelerateEndpoint) {
+    forcePathStyleOverride = options.forcePathStyle ?? true;
   }
   return Object.assign(options, {
     useFipsEndpoint: options.useFipsEndpoint ?? false,
     useDualstackEndpoint: options.useDualstackEndpoint ?? false,
-    forcePathStyle: options.forcePathStyle ?? forcePathStyleDefault,
+    forcePathStyle: options.forcePathStyle ?? forcePathStyleOverride ?? false,
     useAccelerateEndpoint: options.useAccelerateEndpoint ?? false,
     useGlobalEndpoint: options.useGlobalEndpoint ?? false,
     disableMultiregionAccessPoints: options.disableMultiregionAccessPoints ?? false,
