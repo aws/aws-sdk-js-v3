c0f0f4b9bd11d13cfd09b6fc54221a42c1a19bb6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-07-01 15:28:59 +0000

    chore(client-bedrock-runtime): use WebSocketHandler in browser runtime

--- clients/client-bedrock-runtime/src/runtimeConfig.browser.ts
+++ clients/client-bedrock-runtime/src/runtimeConfig.browser.ts
@@ -3,11 +3,16 @@
 import packageInfo from "../package.json"; // eslint-disable-line
 
 import { Sha256 } from "@aws-crypto/sha256-browser";
+import {
+  WebSocketFetchHandlerOptions,
+  WebSocketFetchHandler as WebSocketRequestHandler,
+  eventStreamPayloadHandlerProvider,
+} from "@aws-sdk/middleware-websocket";
 import { createDefaultUserAgentProvider } from "@aws-sdk/util-user-agent-browser";
 import { DEFAULT_USE_DUALSTACK_ENDPOINT, DEFAULT_USE_FIPS_ENDPOINT } from "@smithy/config-resolver";
 import { eventStreamSerdeProvider } from "@smithy/eventstream-serde-browser";
-import { FetchHttpHandler as RequestHandler, streamCollector } from "@smithy/fetch-http-handler";
-import { invalidFunction, invalidProvider } from "@smithy/invalid-dependency";
+import { FetchHttpHandler as HttpRequestHandler, streamCollector } from "@smithy/fetch-http-handler";
+import { invalidProvider } from "@smithy/invalid-dependency";
 import { calculateBodyLength } from "@smithy/util-body-length-browser";
 import { DEFAULT_MAX_ATTEMPTS, DEFAULT_RETRY_MODE } from "@smithy/util-retry";
 import { BedrockRuntimeClientConfig } from "./BedrockRuntimeClient";
@@ -33,13 +38,17 @@ export const getRuntimeConfig = (config: BedrockRuntimeClientConfig) => {
     defaultUserAgentProvider:
       config?.defaultUserAgentProvider ??
       createDefaultUserAgentProvider({ serviceId: clientSharedValues.serviceId, clientVersion: packageInfo.version }),
-    eventStreamPayloadHandlerProvider:
-      config?.eventStreamPayloadHandlerProvider ??
-      (() => ({ handle: invalidFunction("event stream request is not supported in browser.") })),
+    eventStreamPayloadHandlerProvider: config?.eventStreamPayloadHandlerProvider ?? eventStreamPayloadHandlerProvider,
     eventStreamSerdeProvider: config?.eventStreamSerdeProvider ?? eventStreamSerdeProvider,
     maxAttempts: config?.maxAttempts ?? DEFAULT_MAX_ATTEMPTS,
     region: config?.region ?? invalidProvider("Region is missing"),
-    requestHandler: RequestHandler.create(config?.requestHandler ?? defaultConfigProvider),
+    requestHandler: WebSocketRequestHandler.create(
+      (config?.requestHandler as
+        | WebSocketRequestHandler
+        | WebSocketFetchHandlerOptions
+        | (() => Promise<WebSocketFetchHandlerOptions>)) ?? defaultConfigProvider,
+      HttpRequestHandler.create(defaultConfigProvider)
+    ),
     retryMode: config?.retryMode ?? (async () => (await defaultConfigProvider()).retryMode || DEFAULT_RETRY_MODE),
     sha256: config?.sha256 ?? Sha256,
     streamCollector: config?.streamCollector ?? streamCollector,
