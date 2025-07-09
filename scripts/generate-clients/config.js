d5e95e385877226f5728829757d3dca60fe26974
George Fu <kuhe@users.noreply.github.com>
2025-07-09 11:15:04 -0400

    chore(codegen): sync for isCustomEndpoint changes

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "bae1dc72101ae1868431d9004f7c5844c74fdf5f",
+  SMITHY_TS_COMMIT: "91f91d9bb0d5f7068a0594a13b36ac3806f05cba",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
