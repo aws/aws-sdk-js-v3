30a298fbd85ebde45de1602e3a5c4085d492b113
George Fu <kuhe@users.noreply.github.com>
2025-07-15 13:21:57 -0400

    fix(clients): upgrade @smithy/middleware-endpoint to fix file/env endpoint resolution

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "437f58f271a67f0745005f16faf53696df1b5af8",
+  SMITHY_TS_COMMIT: "59a861f106887b323eb853cebb3c804deb7c2989",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
