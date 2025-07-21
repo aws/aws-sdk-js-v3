b5afe2e0d32ba304992c09ed675b244e19c08a09
George Fu <kuhe@users.noreply.github.com>
2025-07-21 15:29:56 -0400

    feat(lib-storage): concurrent requests

--- lib/lib-storage/src/s3-transfer-manager/join-streams.ts
+++ lib/lib-storage/src/s3-transfer-manager/join-streams.ts
@@ -1,17 +1,15 @@
 import { StreamingBlobPayloadOutputTypes } from "@smithy/types";
-import { isBlob, isReadableStream, sdkStreamMixin } from "@smithy/util-stream";
+import { isReadableStream, sdkStreamMixin } from "@smithy/util-stream";
 import { Readable } from "stream";
 
 import { JoinStreamIterationEvents } from "./types";
 
 // TODO: check all types. needs to join nodejs and browser together
-export function joinStreams(
-  streams: StreamingBlobPayloadOutputTypes[],
+export async function joinStreams(
+  streams: Promise<StreamingBlobPayloadOutputTypes>[],
   eventListeners?: JoinStreamIterationEvents
-): StreamingBlobPayloadOutputTypes {
-  if (streams.length === 1) {
-    return streams[0];
-  } else if (isReadableStream(streams[0])) {
+): Promise<StreamingBlobPayloadOutputTypes> {
+  if (isReadableStream(streams[0])) {
     const newReadableStream = new ReadableStream({
       async start(controller) {
         for await (const chunk of iterateStreams(streams, eventListeners)) {
@@ -21,43 +19,36 @@ export function joinStreams(
       },
     });
     return sdkStreamMixin(newReadableStream);
-  } else if (isBlob(streams[0])) {
-    throw new Error("Blob not supported yet");
   } else {
     return sdkStreamMixin(Readable.from(iterateStreams(streams, eventListeners)));
   }
 }
 
 export async function* iterateStreams(
-  streams: StreamingBlobPayloadOutputTypes[],
+  streams: Promise<StreamingBlobPayloadOutputTypes>[],
   eventListeners?: JoinStreamIterationEvents
 ): AsyncIterable<StreamingBlobPayloadOutputTypes, void, void> {
   let bytesTransferred = 0;
   let index = 0;
-  for (const stream of streams) {
+  for (const streamPromise of streams) {
+    const stream = await streamPromise;
     if (isReadableStream(stream)) {
-      // const reader = stream.getReader();
-      // while (true) {
-      //   const { done, value } = await reader.read();
-      //   if (done) {
-      //     break;
-      //   }
-      //   yield value;
-      //   bytesTransferred += value.byteLength;
-      // }
-      // reader.releaseLock();
-
-      const failure = new Error(`ReadableStreams not supported yet ${(stream as any)?.constructor?.name}`);
-      eventListeners?.onFailure?.(failure, index);
-      throw failure;
-    } else if (isBlob(stream)) {
-      throw new Error("Blob not supported yet");
+      const reader = stream.getReader();
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          break;
+        }
+        yield value;
+        bytesTransferred += value.byteLength;
+        eventListeners?.onBytes?.(bytesTransferred, index);
+      }
+      reader.releaseLock();
     } else if (stream instanceof Readable) {
       for await (const chunk of stream) {
         yield chunk;
         const chunkSize = Buffer.isBuffer(chunk) ? chunk.length : Buffer.byteLength(chunk);
         bytesTransferred += chunkSize;
-
         eventListeners?.onBytes?.(bytesTransferred, index);
       }
     } else {
