829512fa3924ea5396e0b3e4b03e3e7131f02509
George Fu <kuhe@users.noreply.github.com>
2025-07-24 12:46:17 -0400

    test: back out s3 restxml tests

--- private/aws-protocoltests-restxml-schema-s3/src/commands/index.ts
+++
@@ -1,5 +0,0 @@
-// smithy-typescript generated code
-export * from "./DeleteObjectTaggingCommand";
-export * from "./GetBucketLocationCommand";
-export * from "./GetObjectCommand";
-export * from "./ListObjectsV2Command";
--- private/aws-protocoltests-restxml-schema-s3/src/pagination/index.ts
+++
@@ -1,3 +0,0 @@
-// smithy-typescript generated code
-export * from "./Interfaces";
-export * from "./ListObjectsV2Paginator";
