d0adff498c562f955bb0ec107938845f0d966d50
George Fu <kuhe@users.noreply.github.com>
2025-07-28 12:33:16 -0400

    test: formatting comments

--- packages/core/src/submodules/account-id-endpoint/account-id-endpoint.integ.spec.ts
+++ packages/core/src/submodules/account-id-endpoint/account-id-endpoint.integ.spec.ts
@@ -1,31 +1,31 @@
 import { requireRequestsFrom } from "@aws-sdk/aws-util-test/src";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { AwsCredentialIdentity } from "@smithy/types";
+import type { AccountIdEndpointMode } from "@aws-sdk/core/account-id-endpoint";
+import type { AwsCredentialIdentity } from "@smithy/types";
 import { afterEach, beforeEach, describe, expect, test as it } from "vitest";
 
 describe("account id endpoint", () => {
-  const credentialsWithAccountId: AwsCredentialIdentity = {
-    accessKeyId: "INTEG_TEST",
-    secretAccessKey: "INTEG_TEST",
-    accountId: "123456789012",
-  };
-  const credentials = {
-    accessKeyId: "INTEG_TEST",
-    secretAccessKey: "INTEG_TEST",
-  };
+  const region = "us-west-2";
 
   beforeEach(async () => {
     delete process.env.AWS_ACCOUNT_ID_ENDPOINT_MODE;
   });
+
   afterEach(async () => {
     delete process.env.AWS_ACCOUNT_ID_ENDPOINT_MODE;
   });
 
   describe("when credentials have account id", () => {
+    const credentials: AwsCredentialIdentity = {
+      accessKeyId: "INTEG_TEST",
+      secretAccessKey: "INTEG_TEST",
+      accountId: "123456789012",
+    };
+
     it("should default to resolving endpoint with account id when it is available in the client credentials", async () => {
       const ddb = new DynamoDB({
-        region: "us-west-2",
-        credentials: credentialsWithAccountId,
+        region,
+        credentials,
       });
       requireRequestsFrom(ddb).toMatch({
         hostname: /123456789012.ddb.us-west-2.amazonaws.com/,
@@ -34,83 +34,58 @@ describe("account id endpoint", () => {
     });
 
     describe("config values", () => {
-      it("disabled", async () => {
-        const ddb = new DynamoDB({
-          region: "us-west-2",
-          credentials: credentialsWithAccountId,
-          accountIdEndpointMode: "disabled",
-        });
-        requireRequestsFrom(ddb).toMatch({
-          hostname: /dynamodb.us-west-2.amazonaws.com/,
-        });
-        await ddb.listTables();
-      });
-      it("preferred", async () => {
-        const ddb = new DynamoDB({
-          region: "us-west-2",
-          credentials: credentialsWithAccountId,
-          accountIdEndpointMode: "preferred",
-        });
-        requireRequestsFrom(ddb).toMatch({
-          hostname: /123456789012.ddb.us-west-2.amazonaws.com/,
-        });
-        await ddb.listTables();
-      });
-      it("required", async () => {
-        const ddb = new DynamoDB({
-          region: "us-west-2",
-          credentials: credentialsWithAccountId,
-          accountIdEndpointMode: "required",
-        });
-        requireRequestsFrom(ddb).toMatch({
-          hostname: /123456789012.ddb.us-west-2.amazonaws.com/,
-        });
-        await ddb.listTables();
-      });
+      it.each([
+        ["dynamodb", "disabled"],
+        ["123456789012.ddb", "preferred"],
+        ["123456789012.ddb", "required"],
+      ])(
+        "should match prefix '%s' for when accountId endpoint mode is '%s' in config",
+        async (expectedHostnamePrefix, mode) => {
+          const ddb = new DynamoDB({
+            region,
+            credentials,
+            accountIdEndpointMode: mode as AccountIdEndpointMode,
+          });
+          requireRequestsFrom(ddb).toMatch({
+            hostname: `${expectedHostnamePrefix}.${region}.amazonaws.com`,
+          });
+          await ddb.listTables();
+        }
+      );
     });
 
     describe("ENV values", () => {
-      it("disabled", async () => {
-        process.env.AWS_ACCOUNT_ID_ENDPOINT_MODE = "disabled";
-        const ddb = new DynamoDB({
-          region: "us-west-2",
-          credentials: credentialsWithAccountId,
-        });
-        requireRequestsFrom(ddb).toMatch({
-          hostname: /dynamodb.us-west-2.amazonaws.com/,
-        });
-        await ddb.listTables();
-      });
-      it("preferred", async () => {
-        process.env.AWS_ACCOUNT_ID_ENDPOINT_MODE = "preferred";
-        const ddb = new DynamoDB({
-          region: "us-west-2",
-          credentials: credentialsWithAccountId,
-        });
-        requireRequestsFrom(ddb).toMatch({
-          hostname: /123456789012.ddb.us-west-2.amazonaws.com/,
-        });
-        await ddb.listTables();
-      });
-      it("required", async () => {
-        process.env.AWS_ACCOUNT_ID_ENDPOINT_MODE = "required";
-        const ddb = new DynamoDB({
-          region: "us-west-2",
-          credentials: credentialsWithAccountId,
-        });
-        requireRequestsFrom(ddb).toMatch({
-          hostname: /123456789012.ddb.us-west-2.amazonaws.com/,
-        });
-        await ddb.listTables();
-      });
+      it.each([
+        ["dynamodb", "disabled"],
+        ["123456789012.ddb", "preferred"],
+        ["123456789012.ddb", "required"],
+      ])(
+        "should match prefix '%s' for when accountId endpoint mode is '%s' in ENV",
+        async (expectedHostnamePrefix, mode) => {
+          process.env.AWS_ACCOUNT_ID_ENDPOINT_MODE = mode;
+          const ddb = new DynamoDB({
+            region,
+            credentials,
+          });
+          requireRequestsFrom(ddb).toMatch({
+            hostname: `${expectedHostnamePrefix}.${region}.amazonaws.com`,
+          });
+          await ddb.listTables();
+        }
+      );
     });
   });
 
   describe("when credentials do not have account id", () => {
+    const credentials = {
+      accessKeyId: "INTEG_TEST",
+      secretAccessKey: "INTEG_TEST",
+    };
+
     it("it follows that the hostname will not have it either", async () => {
       const ddb = new DynamoDB({
-        region: "us-west-2",
-        credentials: credentials,
+        region,
+        credentials,
       });
       requireRequestsFrom(ddb).toMatch({
         hostname: /dynamodb.us-west-2.amazonaws.com/,
@@ -121,8 +96,8 @@ describe("account id endpoint", () => {
     it("will fail if required by ENV", async () => {
       process.env.AWS_ACCOUNT_ID_ENDPOINT_MODE = "required";
       const ddb = new DynamoDB({
-        region: "us-west-2",
-        credentials: credentials,
+        region,
+        credentials,
       });
       const error = await ddb.listTables().catch((e) => e);
       expect(error.name).toEqual("EndpointError");
@@ -130,8 +105,8 @@ describe("account id endpoint", () => {
 
     it("will fail if required by config", async () => {
       const ddb = new DynamoDB({
-        region: "us-west-2",
-        credentials: credentials,
+        region,
+        credentials,
         accountIdEndpointMode: "required",
       });
       const error = await ddb.listTables().catch((e) => e);
