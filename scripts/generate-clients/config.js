750ced560d637b33521d8b6e255f9fa532c7dfdf
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-07-29 19:33:11 -0700

    chore(codegen): upgrade smithy to 1.61.0 (#7233)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "ace4136fef6095aafb3aba9e02102f761a03a82e",
+  SMITHY_TS_COMMIT: "8e2d4cc27314f51d3d8d0ecce13a83e000190ea1",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
