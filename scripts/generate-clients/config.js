9e2e900db41c8433a5f9b510ceb357e695c4962d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-07-30 09:17:01 -0700

    chore(codegen): bump codegen version to 0.34.0 (#7235)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "8e2d4cc27314f51d3d8d0ecce13a83e000190ea1",
+  SMITHY_TS_COMMIT: "55751d0f920307e8f08071365c4e64c3e2e78009",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
