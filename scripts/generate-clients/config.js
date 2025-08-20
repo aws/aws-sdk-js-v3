16da6568a6f21f52948d03528df1a5eeb4d13864
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-08-20 14:12:55 -0700

    chore(private): rename vite.config.js to vitest.config.mts (#7269)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "0edd12e9b1e94706067e77f5a469b7eafccdab80",
+  SMITHY_TS_COMMIT: "73e7b4a437a0cad59f5bd6e18d3092e620c26b67",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
