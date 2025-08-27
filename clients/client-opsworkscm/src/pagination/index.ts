ad71c7d11d2a2a2dbc3a2d985275a92e51d60fe5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-08-27 08:33:04 -0700

    chore(client-opsworkscm): remove OpsWorksCM (#7283)

--- clients/client-opsworkscm/src/pagination/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./DescribeBackupsPaginator";
-
-export * from "./DescribeEventsPaginator";
-
-export * from "./DescribeServersPaginator";
-// smithy-typescript generated code
-export * from "./Interfaces";
-export * from "./ListTagsForResourcePaginator";
