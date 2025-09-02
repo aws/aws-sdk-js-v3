a6ab72b1a6bd71db241a4f6fffae254de109b13b
awstools <awstools@amazon.com>
2025-09-02 18:12:13 +0000

    feat(client-neptune): Removed the deprecated marker from publiclyAccessible parameter from DbInstance, CreateDbInstance and ModifyDbInstance and added relevant usage information for the parameter.

--- clients/client-neptune/src/models/models_0.ts
+++ clients/client-neptune/src/models/models_0.ts
@@ -2837,9 +2837,14 @@ export interface CreateDBInstanceMessage {
   CharacterSetName?: string | undefined;
 
   /**
-   * <p>This flag should no longer be used.</p>
-   *
-   * @deprecated
+   * <p>Indicates whether the DB instance is publicly accessible.</p>
+   *          <p>When the DB instance is publicly accessible and you connect from outside of the DB instance's virtual private
+   *       cloud (VPC), its Domain Name System (DNS) endpoint resolves to the public IP address. When you connect from within
+   *       the same VPC as the DB instance, the endpoint resolves to the private IP address. Access to the DB instance is
+   *       ultimately controlled by the security group it uses. That public access isn't permitted if the security group assigned
+   *       to the DB cluster doesn't permit it.</p>
+   *          <p>When the DB instance isn't publicly accessible, it is an internal DB instance with a DNS name that resolves to a
+   *       private IP address.</p>
    * @public
    */
   PubliclyAccessible?: boolean | undefined;
@@ -3544,9 +3549,14 @@ export interface DBInstance {
   SecondaryAvailabilityZone?: string | undefined;
 
   /**
-   * <p>This flag should no longer be used.</p>
-   *
-   * @deprecated
+   * <p>Indicates whether the DB instance is publicly accessible.</p>
+   *          <p>When the DB instance is publicly accessible and you connect from outside of the DB instance's virtual private
+   *       cloud (VPC), its Domain Name System (DNS) endpoint resolves to the public IP address. When you connect from within
+   *       the same VPC as the DB instance, the endpoint resolves to the private IP address. Access to the DB instance is
+   *       ultimately controlled by the security group it uses. That public access isn't permitted if the security group assigned
+   *       to the DB cluster doesn't permit it.</p>
+   *          <p>When the DB instance isn't publicly accessible, it is an internal DB instance with a DNS name that resolves to a
+   *       private IP address.</p>
    * @public
    */
   PubliclyAccessible?: boolean | undefined;
@@ -8150,9 +8160,14 @@ export interface ModifyDBInstanceMessage {
   DBPortNumber?: number | undefined;
 
   /**
-   * <p>This flag should no longer be used.</p>
-   *
-   * @deprecated
+   * <p>Indicates whether the DB instance is publicly accessible.</p>
+   *          <p>When the DB instance is publicly accessible and you connect from outside of the DB instance's virtual private
+   *       cloud (VPC), its Domain Name System (DNS) endpoint resolves to the public IP address. When you connect from within
+   *       the same VPC as the DB instance, the endpoint resolves to the private IP address. Access to the DB instance is
+   *       ultimately controlled by the security group it uses. That public access isn't permitted if the security group assigned
+   *       to the DB cluster doesn't permit it.</p>
+   *          <p>When the DB instance isn't publicly accessible, it is an internal DB instance with a DNS name that resolves to a
+   *       private IP address.</p>
    * @public
    */
   PubliclyAccessible?: boolean | undefined;
