cd08e4e2badb432ae35d736078fd3aaec2158b22
smilkuri <smilkuri@amazon.com>
2025-09-08 21:29:20 +0000

    test(client-s3): add integration test for selectObjectContent

---
+++ clients/client-s3/test/integ/s3-selectObjectContent.integ.spec.ts
@@ -0,0 +1,51 @@
+import { S3 } from "@aws-sdk/client-s3";
+import { describe, expect, test as it } from "vitest";
+
+import { requireRequestsFrom } from "../../../../private/aws-util-test/src";
+
+describe("selectObjectContent", () => {
+  const credentials = {
+    accessKeyId: "INTEG",
+    secretAccessKey: "INTEG",
+  };
+
+  it("should make correct request", async () => {
+    const client = new S3({ region: "us-west-2", credentials });
+
+    requireRequestsFrom(client).toMatch({
+      hostname: /aws-sdk-js-integ-test-bucket\.s3\.us-west-2\.amazonaws\.com/,
+      method: "POST",
+      path: "/test-key",
+      headers: {
+        authorization: /AWS4-HMAC-SHA256 Credential/,
+        "content-type": "application/xml",
+        "x-amz-content-sha256": /./,
+      },
+      body(xmlBody) {
+        expect(xmlBody).toContain(
+          "<Expression>SELECT user_name FROM S3Object WHERE cast(age as int) &gt; 20</Expression>"
+        );
+        expect(xmlBody).toContain("<ExpressionType>SQL</ExpressionType>");
+      },
+    });
+
+    await client.selectObjectContent({
+      Bucket: "aws-sdk-js-integ-test-bucket",
+      Key: "test-key",
+      ExpressionType: "SQL",
+      Expression: "SELECT user_name FROM S3Object WHERE cast(age as int) > 20",
+      InputSerialization: {
+        CSV: {
+          FileHeaderInfo: "USE",
+          RecordDelimiter: "\n",
+          FieldDelimiter: ",",
+        },
+      },
+      OutputSerialization: {
+        CSV: {},
+      },
+    });
+
+    expect.hasAssertions();
+  });
+});
