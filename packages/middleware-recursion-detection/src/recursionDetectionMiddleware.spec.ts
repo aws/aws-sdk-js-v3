ac324bb6935f81aea72f3c84a1376a266c11b306
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-09-09 15:41:45 -0700

    chore(middleware-recursion-detection): read TraceID from Lambda Invoke Store (#7293)

--- packages/middleware-recursion-detection/src/recursionDetectionMiddleware.spec.ts
+++ packages/middleware-recursion-detection/src/recursionDetectionMiddleware.spec.ts
@@ -1,3 +1,4 @@
+import { InvokeStore } from "@aws/lambda-invoke-store";
 import { HttpRequest } from "@smithy/protocol-http";
 import { afterAll, beforeEach, describe, expect, test as it, vi } from "vitest";
 
@@ -6,10 +7,12 @@ import { recursionDetectionMiddleware } from "./recursionDetectionMiddleware";
 describe(recursionDetectionMiddleware.name, () => {
   const mockNextHandler = vi.fn();
   const originEnv = process.env;
+
   const TRACE_ID_HEADER_NAME = "X-Amzn-Trace-Id";
 
   beforeEach(() => {
     vi.clearAllMocks();
+    vi.spyOn(InvokeStore, "getXRayTraceId").mockImplementation(() => undefined);
     process.env = {};
   });
 
@@ -17,20 +20,58 @@ describe(recursionDetectionMiddleware.name, () => {
     process.env = originEnv;
   });
 
-  it(`sets ${TRACE_ID_HEADER_NAME} header when function name and trace id environmental variables are set`, async () => {
-    process.env = {
-      AWS_LAMBDA_FUNCTION_NAME: "some-function",
-      _X_AMZN_TRACE_ID: "some-trace-id",
-    };
-    const handler = recursionDetectionMiddleware()(mockNextHandler, {} as any);
-    await handler({
-      input: {},
-      request: new HttpRequest({}),
+  describe(`sets ${TRACE_ID_HEADER_NAME} header when function name and`, () => {
+    const mockTraceIdEnv = "trace-id-from-env";
+    const mockTraceIdInvokeStore = "trace-id-from-invoke-store";
+
+    it("trace id environmental variables is set", async () => {
+      process.env = {
+        AWS_LAMBDA_FUNCTION_NAME: "some-function",
+        _X_AMZN_TRACE_ID: mockTraceIdEnv,
+      };
+      const handler = recursionDetectionMiddleware()(mockNextHandler, {} as any);
+      await handler({
+        input: {},
+        request: new HttpRequest({}),
+      });
+      const { calls } = (mockNextHandler as any).mock;
+      expect(calls.length).toBe(1);
+      const { request } = mockNextHandler.mock.calls[0][0];
+      expect(request.headers[TRACE_ID_HEADER_NAME]).toBe(mockTraceIdEnv);
     });
-    const { calls } = (mockNextHandler as any).mock;
-    expect(calls.length).toBe(1);
-    const { request } = mockNextHandler.mock.calls[0][0];
-    expect(request.headers[TRACE_ID_HEADER_NAME]).toBe("some-trace-id");
+
+    it("trace id value is set in InvokeStore", async () => {
+      vi.spyOn(InvokeStore, "getXRayTraceId").mockImplementation(() => mockTraceIdInvokeStore);
+      process.env = {
+        AWS_LAMBDA_FUNCTION_NAME: "some-function",
+      };
+      const handler = recursionDetectionMiddleware()(mockNextHandler, {} as any);
+      await handler({
+        input: {},
+        request: new HttpRequest({}),
+      });
+      const { calls } = (mockNextHandler as any).mock;
+      expect(calls.length).toBe(1);
+      const { request } = mockNextHandler.mock.calls[0][0];
+      expect(request.headers[TRACE_ID_HEADER_NAME]).toBe(mockTraceIdInvokeStore);
+    });
+
+    it("favors trace id value from InvokeStore over that from env variable", async () => {
+      vi.spyOn(InvokeStore, "getXRayTraceId").mockImplementation(() => mockTraceIdInvokeStore);
+      process.env = {
+        AWS_LAMBDA_FUNCTION_NAME: "some-function",
+        _X_AMZN_TRACE_ID: mockTraceIdEnv,
+      };
+      const handler = recursionDetectionMiddleware()(mockNextHandler, {} as any);
+      await handler({
+        input: {},
+        request: new HttpRequest({}),
+      });
+      const { calls } = (mockNextHandler as any).mock;
+      expect(calls.length).toBe(1);
+      const { request } = mockNextHandler.mock.calls[0][0];
+      expect(request.headers[TRACE_ID_HEADER_NAME]).toBe(mockTraceIdInvokeStore);
+    });
   });
 
   it(`should NOT set ${TRACE_ID_HEADER_NAME} header when function name environmental variable is NOT set`, async () => {
