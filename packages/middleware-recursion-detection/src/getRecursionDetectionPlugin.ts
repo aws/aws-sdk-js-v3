9a59e2c245c9e58a3d6d8cdb780e3a5b6d6a5292
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-09-09 18:12:27 +0000

    fix: add options in getRecursionDetectionPlugin

--- packages/middleware-recursion-detection/src/getRecursionDetectionPlugin.ts
+++ packages/middleware-recursion-detection/src/getRecursionDetectionPlugin.ts
@@ -3,12 +3,10 @@ import { Pluggable } from "@smithy/types";
 import { recursionDetectionMiddlewareOptions } from "./configuration";
 import { recursionDetectionMiddleware } from "./recursionDetectionMiddleware";
 
-// @internal
 /**
  * @internal
  */
-
-export const getRecursionDetectionPlugin = (): Pluggable<any, any> => ({
+export const getRecursionDetectionPlugin = (options: any): Pluggable<any, any> => ({
   applyToStack: (clientStack) => {
     clientStack.add(recursionDetectionMiddleware(), recursionDetectionMiddlewareOptions);
   },
