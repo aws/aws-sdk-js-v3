658cc4c35ea4dd216d9238fe5ed1296bb390dde8
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-09-10 00:22:24 +0000

    chore(codegen): bump smithy-ts commit

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "e586a101cc21c9fbd1b87a523c168ca4ac9b481b",
+  SMITHY_TS_COMMIT: "3015d27bb80562baef9d1ce3171cbaad067e387a",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
