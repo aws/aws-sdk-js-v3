8bb775f77f58fa7867ba8bdaf96eab191f98b7ff
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-09-11 02:47:08 +0000

    fix(codegen): use Effort enum in spotbugs

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -14,6 +14,7 @@
  */
 
 import org.jreleaser.model.Active
+import com.github.spotbugs.snom.Effort
 
 plugins {
     `java-library`
@@ -240,7 +241,7 @@ subprojects {
 
         // Configure the bug filter for spotbugs.
         spotbugs {
-            setEffort("max")
+            effort.set(Effort.MAX)
             val excludeFile = rootProject.file("gradleConfig/spotbugs/filter.xml")
             if (excludeFile.exists()) {
                 excludeFilter.set(excludeFile)
