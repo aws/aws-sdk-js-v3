f449c5b86ada847b37878eab3bc0e5d2614d0332
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-09-29 16:48:11 -0700

    chore(codegen): bump smithy-gradle-plugin to 1.3.0 (#7387)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "7be586d88edd5c9933d7323210110c79543f1a8c",
+  SMITHY_TS_COMMIT: "3ae16ff12819fc54a3e95475de0515f92bd34d09",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
