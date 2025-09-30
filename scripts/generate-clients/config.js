0a418cc265b71347d0582d7ec2b937d51f5407ce
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-09-30 12:53:22 -0700

    chore: bump '@smithy/*' versions (#7391)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,7 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "cc2105a5dd5dfe01ace384034e863770080e6b08",
+  SMITHY_TS_COMMIT: "b20705ddfc05e7eba3fc3669888f576622474e9c",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
