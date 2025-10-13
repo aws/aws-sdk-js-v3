14d66482635fac74ad17e1b237ed5edc8951a487
George Fu <kuhe@users.noreply.github.com>
2025-10-13 19:46:19 +0000

    chore(codegen): sync for node-http timeout fixes, deprecated documentation

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -1,7 +1,9 @@
 // Update this commit when taking up new changes from smithy-typescript.
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
-  SMITHY_TS_COMMIT: "c06e9c6ad51fd6a96dd9e493b0b418efa665d80a",
+  // Comparison link (update with previous hash):
+  // https://github.com/smithy-lang/smithy-typescript/compare/c06e9c6ad51fd6a96dd9e493b0b418efa665d80a...d1c41160522320bb5139c31cb92e79a8cdab840e
+  SMITHY_TS_COMMIT: "d1c41160522320bb5139c31cb92e79a8cdab840e",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
