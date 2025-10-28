45f629d4be1701cc892f73155654cffc4d1a78e6
George Fu <kuhe@users.noreply.github.com>
2025-10-28 13:01:41 -0400

    chore(clients): remove QLDB, RoboMaker, LookoutMetrics, LookoutVision, IoTFleetHub, and AppTest (#7461)

--- clients/client-qldb/src/pagination/index.ts
+++
@@ -1,6 +0,0 @@
-// smithy-typescript generated code
-export * from "./Interfaces";
-export * from "./ListJournalKinesisStreamsForLedgerPaginator";
-export * from "./ListJournalS3ExportsForLedgerPaginator";
-export * from "./ListJournalS3ExportsPaginator";
-export * from "./ListLedgersPaginator";
