93e388692cc71e7b037ac3c3609c58fd772a45de
George Fu <kuhe@users.noreply.github.com>
2025-10-30 15:36:37 +0000

    chore(codegen): sync for waiter and proxy fixes

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/8af2d330260513d3e51c98f1f137793b5330ceb4...56a65e69af8a8b7a814a92af716009cabe4b0b42
-  SMITHY_TS_COMMIT: "56a65e69af8a8b7a814a92af716009cabe4b0b42",
+  // https://github.com/smithy-lang/smithy-typescript/compare/56a65e69af8a8b7a814a92af716009cabe4b0b42...472a5ea035237e10c560ed362c0e0105805ad26f
+  SMITHY_TS_COMMIT: "472a5ea035237e10c560ed362c0e0105805ad26f",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
