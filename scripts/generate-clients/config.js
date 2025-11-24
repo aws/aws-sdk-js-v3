32a1a4e29e299934e3457975afaab40c0b43df6e
George Fu <kuhe@users.noreply.github.com>
2025-11-24 15:31:38 +0000

    fix(clients): export enum objects for string shapes

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/7be577c4d67a62cb304470718cc11222c991bcbf...775cd5b119247e544cfa1c51feb89b9e18805725
-  SMITHY_TS_COMMIT: "775cd5b119247e544cfa1c51feb89b9e18805725",
+  // https://github.com/smithy-lang/smithy-typescript/compare/775cd5b119247e544cfa1c51feb89b9e18805725...f8bed213e93a537ea1e26cc0a1dba8d45501bf3d
+  SMITHY_TS_COMMIT: "f8bed213e93a537ea1e26cc0a1dba8d45501bf3d",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
