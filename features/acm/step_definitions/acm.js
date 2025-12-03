e15aef3fa530d761366660757485f4a711da323a
smilkuri <smilkuri@amazon.com>
2025-12-03 16:46:49 +0000

    chore: convert bdd tests to e2e tests

--- features/acm/step_definitions/acm.js
+++
@@ -1,7 +0,0 @@
-const { Before } = require("@cucumber/cucumber");
-
-Before({ tags: "@acm" }, function (scenario, callback) {
-  const { ACM } = require("../../../clients/client-acm");
-  this.service = new ACM({});
-  callback();
-});
