3dadb26ae72b0d339e77d31f492e4789f6163ada
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-12-03 21:41:09 +0000

    chore(core): emit warning for Node.js 18.x end-of-support

--- packages/core/src/submodules/client/emitWarningIfUnsupportedVersion.spec.ts
+++ packages/core/src/submodules/client/emitWarningIfUnsupportedVersion.spec.ts
@@ -39,12 +39,12 @@ describe("emitWarningIfUnsupportedVersion", () => {
       expect(process.emitWarning).toHaveBeenCalledTimes(1);
       expect(process.emitWarning).toHaveBeenCalledWith(
         `NodeDeprecationWarning: The AWS SDK for JavaScript (v3) will
-no longer support Node.js 16.x on January 6, 2025.
+no longer support Node.js ${unsupportedVersion} in January 2026.
 
 To continue receiving updates to AWS services, bug fixes, and security
 updates please upgrade to a supported Node.js LTS version.
 
-More information can be found at: https://a.co/74kJMmI`
+More information can be found at: https://a.co/c895JFp`
       );
 
       // Verify that the warning emits only once.
