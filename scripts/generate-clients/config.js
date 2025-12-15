54775f0133beaad61de6d5b91dd0614448fa93fc
George Fu <kuhe@users.noreply.github.com>
2025-12-15 19:40:22 +0000

    chore: codegen sync

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/bbdb09131e307af317f3a6a6ee09e895749ea749...27f444b8aa045e37abfa811577fa3ed7d921516a
-  SMITHY_TS_COMMIT: "27f444b8aa045e37abfa811577fa3ed7d921516a",
+  // https://github.com/smithy-lang/smithy-typescript/compare/27f444b8aa045e37abfa811577fa3ed7d921516a...a0318e29da3a4ea61aeba98c8a28a4a9065919a6
+  SMITHY_TS_COMMIT: "a0318e29da3a4ea61aeba98c8a28a4a9065919a6",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
