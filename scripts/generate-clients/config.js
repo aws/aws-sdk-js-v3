b654d97229090036ef35b3b8fdfd76f303b81011
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-12-16 14:04:29 -0800

    chore(clients): bump '@smithy/*' versions (#7582)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/8b90f360df2a5c80c105db732b3c2f6972994854...76d7994ef3f8373bb866a60e7ef87adca147a2f0
-  SMITHY_TS_COMMIT: "76d7994ef3f8373bb866a60e7ef87adca147a2f0",
+  // https://github.com/smithy-lang/smithy-typescript/compare/76d7994ef3f8373bb866a60e7ef87adca147a2f0...0bfadd0475f6db25a9933b9e4f60de60f133f1a3
+  SMITHY_TS_COMMIT: "0bfadd0475f6db25a9933b9e4f60de60f133f1a3",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
