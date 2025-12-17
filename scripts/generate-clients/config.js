f6ec0ebb423b5e3b37922fe46a028a744ae7aa14
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2025-12-17 22:29:02 +0000

    chore(codegen): update smithy-ts commit

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/de08d556d19c8500d88249f80b81f71131cc3138...46c6eef6b80338f8acb560ba826519ade474ec5e
-  SMITHY_TS_COMMIT: "46c6eef6b80338f8acb560ba826519ade474ec5e",
+  // https://github.com/smithy-lang/smithy-typescript/compare/46c6eef6b80338f8acb560ba826519ade474ec5e...8d725cc6cb65c18918291f2fed5654e833903d7d
+  SMITHY_TS_COMMIT: "8d725cc6cb65c18918291f2fed5654e833903d7d",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
