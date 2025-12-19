1f7e98120b5897b6ce44fb2dcc47e9dbf7f5a98e
George Fu <kuhe@users.noreply.github.com>
2025-12-19 16:07:03 +0000

    chore(codegen): differentiate union and struct schemas

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/8d725cc6cb65c18918291f2fed5654e833903d7d...521d67c38f8928c514d1fcea3fc08e40c2300513
-  SMITHY_TS_COMMIT: "521d67c38f8928c514d1fcea3fc08e40c2300513",
+  // https://github.com/smithy-lang/smithy-typescript/compare/521d67c38f8928c514d1fcea3fc08e40c2300513...ce2697be6df3479d5dd239abb1a19a38093aaeeb
+  SMITHY_TS_COMMIT: "ce2697be6df3479d5dd239abb1a19a38093aaeeb",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
