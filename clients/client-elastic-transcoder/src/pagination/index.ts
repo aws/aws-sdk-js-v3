4381b2dcf13e5f7f49f1f4318b77e59a032f0ab4
Sreeja Milkuri <smilkuri@amazon.com>
2025-12-23 13:38:02 -0500

    chore(client-elastic-transcoder): remove elastic transcoder due to service shutdown (#7602)

--- clients/client-elastic-transcoder/src/pagination/index.ts
+++
@@ -1,6 +0,0 @@
-// smithy-typescript generated code
-export * from "./Interfaces";
-export * from "./ListJobsByPipelinePaginator";
-export * from "./ListJobsByStatusPaginator";
-export * from "./ListPipelinesPaginator";
-export * from "./ListPresetsPaginator";
