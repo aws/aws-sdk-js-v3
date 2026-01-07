9a090faf93be689bb2d5e46ec15e5bda8ba16f4e
George Fu <kuhe@users.noreply.github.com>
2026-01-07 12:05:58 -0500

    chore: upgrade rimraf (#7626)
    * chore(codegen): update rimraf
    
    * chore: update rimraf

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/eb9d0c639845ff9e8e94aa32ec97a3de315bc55a...29ccd13ee98634d1c6449f3b50ab377d1a515297
-  SMITHY_TS_COMMIT: "29ccd13ee98634d1c6449f3b50ab377d1a515297",
+  // https://github.com/smithy-lang/smithy-typescript/compare/29ccd13ee98634d1c6449f3b50ab377d1a515297...7e4bbf64112652f6bb5171ce2c14eebfc6aab964
+  SMITHY_TS_COMMIT: "7e4bbf64112652f6bb5171ce2c14eebfc6aab964",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
