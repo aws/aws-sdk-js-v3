71ae6ec70a7336a5e53cdc3cdbc7a2c883c7a455
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-01-13 04:03:37 +0000

    chore(codegen): bump '@smithy/*' versions

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/837c4d05cca85a32971da061d6606832691dbd24...17f8a9c0791d6e7d8bc33c85bda43a7325306b97
-  SMITHY_TS_COMMIT: "17f8a9c0791d6e7d8bc33c85bda43a7325306b97",
+  // https://github.com/smithy-lang/smithy-typescript/compare/17f8a9c0791d6e7d8bc33c85bda43a7325306b97...948b6b60c44226ce7c3055833bdb34c31e5b6116
+  SMITHY_TS_COMMIT: "948b6b60c44226ce7c3055833bdb34c31e5b6116",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
