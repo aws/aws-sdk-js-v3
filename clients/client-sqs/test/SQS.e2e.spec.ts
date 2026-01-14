6a886c01d965f8b4c000d26136efa8d6bb3e0d21
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-01-14 04:18:08 +0000

    test(client-sqs): create queue in test and delete in afterAll

--- clients/client-sqs/test/SQS.e2e.spec.ts
+++ clients/client-sqs/test/SQS.e2e.spec.ts
@@ -1,42 +1,39 @@
 import { SQS } from "@aws-sdk/client-sqs";
 import { AwsJson1_0Protocol, AwsQueryProtocol } from "@aws-sdk/core";
 import { HttpRequest, HttpResponse } from "@smithy/protocol-http";
-import { beforeAll, afterAll, describe, expect, test as it } from "vitest";
+import { afterAll, describe, expect, test as it } from "vitest";
 
 describe(SQS.name, () => {
-  const region = "us-west-2";
-  const client = new SQS({ region });
-  const QueueName = `aws-sdk-js-${crypto.randomUUID()}`;
-  let QueueUrl: string;
-
-  beforeAll(async () => {
-    const response = await client.createQueue({ QueueName });
-    QueueUrl = response.QueueUrl!;
-  });
-
-  it.each([
+  describe.each([
     { output: "text/xml", input: "application/x-www-form-urlencoded", protocol: AwsQueryProtocol },
     { output: "application/x-amz-json-1.0", input: "application/x-amz-json-1.0", protocol: AwsJson1_0Protocol },
   ])(
     "gets response with content-type=$output when request is content-type=$input with protocol $protocol.name",
-    async ({ input, output, protocol }) => {
-      const client = new SQS({ region, protocol });
-      client.middlewareStack.add(
-        (next) => async (args: any) => {
-          expect((args.request as HttpRequest).headers["content-type"]).toBe(input);
-          const response = await next(args);
-          expect((response.response as HttpResponse).headers["content-type"]).toBe(output);
-          return response;
-        },
-        { step: "build" }
-      );
-      const response = await client.getQueueUrl({ QueueName });
-      expect(response.QueueUrl).toEqual(QueueUrl);
-      expect.assertions(3);
+    ({ input, output, protocol }) => {
+      const client = new SQS({ region: "us-west-2", protocol });
+      let QueueUrl: string;
+
+      it("run createQueue", async () => {
+        client.middlewareStack.add(
+          (next) => async (args: any) => {
+            expect((args.request as HttpRequest).headers["content-type"]).toBe(input);
+            const response = await next(args);
+            expect((response.response as HttpResponse).headers["content-type"]).toBe(output);
+            return response;
+          },
+          { step: "build" }
+        );
+        const response = await client.createQueue({ QueueName: `aws-sdk-js-${crypto.randomUUID()}` });
+        QueueUrl = response.QueueUrl!;
+        expect(typeof QueueUrl).toBe("string");
+        expect.assertions(3);
+      });
+
+      afterAll(async () => {
+        await client.deleteQueue({ QueueUrl }).catch(() => {
+          // Ignore errors during cleanup (e.g. if queue creation fails)
+        });
+      });
     }
   );
-
-  afterAll(async () => {
-    await client.deleteQueue({ QueueUrl });
-  });
 });
