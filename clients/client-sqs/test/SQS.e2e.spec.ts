2dc81b45eaa03e6669c3c540c98142ad8de48ca8
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-01-14 03:53:28 +0000

    test(client-sqs): test for QueueUrl with create/delete

--- clients/client-sqs/test/SQS.e2e.spec.ts
+++ clients/client-sqs/test/SQS.e2e.spec.ts
@@ -1,16 +1,26 @@
 import { SQS } from "@aws-sdk/client-sqs";
 import { AwsJson1_0Protocol, AwsQueryProtocol } from "@aws-sdk/core";
 import { HttpRequest, HttpResponse } from "@smithy/protocol-http";
-import { describe, expect, test as it } from "vitest";
+import { beforeAll, afterAll, describe, expect, test as it } from "vitest";
 
 describe(SQS.name, () => {
+  const region = "us-west-2";
+  const client = new SQS({ region });
+  const QueueName = `aws-sdk-js-${crypto.randomUUID()}`;
+  let QueueUrl: string;
+
+  beforeAll(async () => {
+    const response = await client.createQueue({ QueueName });
+    QueueUrl = response.QueueUrl!;
+  });
+
   it.each([
     { output: "text/xml", input: "application/x-www-form-urlencoded", protocol: AwsQueryProtocol },
     { output: "application/x-amz-json-1.0", input: "application/x-amz-json-1.0", protocol: AwsJson1_0Protocol },
   ])(
     "gets response with content-type=$output when request is content-type=$input with protocol $protocol.name",
     async ({ input, output, protocol }) => {
-      const client = new SQS({ region: "us-west-2", protocol });
+      const client = new SQS({ region, protocol });
       client.middlewareStack.add(
         (next) => async (args: any) => {
           expect((args.request as HttpRequest).headers["content-type"]).toBe(input);
@@ -20,9 +30,13 @@ describe(SQS.name, () => {
         },
         { step: "build" }
       );
-      const response = await client.listQueues({ MaxResults: 1 });
-      expect(response.$metadata.httpStatusCode).toEqual(200);
+      const response = await client.getQueueUrl({ QueueName });
+      expect(response.QueueUrl).toEqual(QueueUrl);
       expect.assertions(3);
     }
   );
+
+  afterAll(async () => {
+    await client.deleteQueue({ QueueUrl });
+  });
 });
