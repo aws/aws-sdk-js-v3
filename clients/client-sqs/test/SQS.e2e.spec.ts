d0edd414f77d731fe4dbb6fb4cb3ff31a0acf0ee
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-01-14 02:28:02 +0000

    test(client-sqs): check for content-type in input and output

--- clients/client-sqs/test/SQS.e2e.spec.ts
+++ clients/client-sqs/test/SQS.e2e.spec.ts
@@ -1,30 +1,27 @@
 import { SQS } from "@aws-sdk/client-sqs";
 import { AwsJson1_0Protocol, AwsQueryProtocol } from "@aws-sdk/core";
-import { getHttpDebugLogPlugin } from "@aws-sdk/middleware-http-debug-log";
+import { HttpRequest, HttpResponse } from "@smithy/protocol-http";
 import { describe, expect, test as it } from "vitest";
 
-describe(
-  SQS.name,
-  () => {
-    const sqs = {
-      query: new SQS({
-        region: "us-west-2",
-        protocol: AwsQueryProtocol,
-      }),
-      json: new SQS({
-        region: "us-west-2",
-        protocol: AwsJson1_0Protocol,
-      }),
-    };
-
-    for (const client of Object.values(sqs)) {
-      client.middlewareStack.use(getHttpDebugLogPlugin("line headers body formatted"));
-
-      it(`can make requests with ${client.config.protocol.constructor.name}`, async () => {
-        const queues = await client.listQueues();
-        expect(queues.QueueUrls ?? []).toBeInstanceOf(Array);
-      });
+describe(SQS.name, () => {
+  it.each([
+    { output: "text/xml", input: "application/x-www-form-urlencoded", protocol: AwsQueryProtocol },
+    { output: "application/x-amz-json-1.0", input: "application/x-amz-json-1.0", protocol: AwsJson1_0Protocol },
+  ])(
+    "gets response with content-type=$output when request is content-type=$input with protocol $protocol.name",
+    async ({ input, output, protocol }) => {
+      const client = new SQS({ region: "us-west-2", protocol });
+      client.middlewareStack.add(
+        (next) => async (args: any) => {
+          expect((args.request as HttpRequest).headers["content-type"]).toBe(input);
+          const response = await next(args);
+          expect((response.response as HttpResponse).headers["content-type"]).toBe(output);
+          return response;
+        },
+        { step: "build" }
+      );
+      await client.listQueues({ MaxResults: 1 });
+      expect.assertions(2);
     }
-  },
-  60_000
-);
+  );
+});
