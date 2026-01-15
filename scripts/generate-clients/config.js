421bf87a0b9f81df4a15f6e1e8cb1c940ef5bf3c
George Fu <kuhe@users.noreply.github.com>
2026-01-15 16:46:07 +0000

    chore(codegen): update for handling __type in cbor structs

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/93408430419857fd23bb6d41894a6bb871f05d26...0e8cc4926dee4e081bfc3fe9945813f7118da73b
-  SMITHY_TS_COMMIT: "0e8cc4926dee4e081bfc3fe9945813f7118da73b",
+  // https://github.com/smithy-lang/smithy-typescript/compare/0e8cc4926dee4e081bfc3fe9945813f7118da73b...26e1588614a429ae676790b974ce2642c52eb736
+  SMITHY_TS_COMMIT: "26e1588614a429ae676790b974ce2642c52eb736",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
