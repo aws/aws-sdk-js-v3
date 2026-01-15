eea2ef451723516ef43924418fff7993b04759ca
George Fu <kuhe@users.noreply.github.com>
2026-01-15 16:18:29 -0500

    chore(codegen): tag maven release 0.42.0 (#7664)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/0e8cc4926dee4e081bfc3fe9945813f7118da73b...26e1588614a429ae676790b974ce2642c52eb736
-  SMITHY_TS_COMMIT: "26e1588614a429ae676790b974ce2642c52eb736",
+  // https://github.com/smithy-lang/smithy-typescript/compare/26e1588614a429ae676790b974ce2642c52eb736...06a3fadf96628600436373b8bbba6ce2ebdec8af
+  SMITHY_TS_COMMIT: "06a3fadf96628600436373b8bbba6ce2ebdec8af",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
