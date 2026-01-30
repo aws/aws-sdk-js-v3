db453d371ac8f270884204eaea847adb8b3e6cfc
George Fu <kuhe@users.noreply.github.com>
2026-01-30 16:02:05 -0500

    chore(codegen): smithy-typescript-aws-codegen release 0.43.1 (#7694)
    * chore(codegen): smithy-typescript-aws-codegen release 0.43.1
    
    * chore(codegen): update commit hash

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -37,7 +37,7 @@ allprojects {
         mavenCentral()
     }
     group = "software.amazon.smithy.typescript"
-    version = "0.43.0"
+    version = "0.43.1"
 }
 
 // The root project doesn't produce a JAR.
