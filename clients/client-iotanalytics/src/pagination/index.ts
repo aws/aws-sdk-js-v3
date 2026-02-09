a8bb64b784cf98d7ef7f1d6bf3c1593a95b02579
smilkuri <smilkuri@amazon.com>
2026-02-09 20:36:02 +0000

    chore(client-iotanalytics): remove iot analytics due to service shutdown

--- clients/client-iotanalytics/src/pagination/index.ts
+++
@@ -1,7 +0,0 @@
-// smithy-typescript generated code
-export * from "./Interfaces";
-export * from "./ListChannelsPaginator";
-export * from "./ListDatasetContentsPaginator";
-export * from "./ListDatasetsPaginator";
-export * from "./ListDatastoresPaginator";
-export * from "./ListPipelinesPaginator";
