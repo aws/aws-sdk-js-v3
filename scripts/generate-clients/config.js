b4d1363a685e1bf9086f76ab327744ffc7ad9e64
George Fu <kuhe@users.noreply.github.com>
2026-02-18 00:01:46 +0000

    chore(codegen): fix undefined http binding top level members

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/1165095e1c178ed8afd8d1e6d78ede6dd272a1ac...57b3119cdf42b7d9d6f6f879fdd243c8dd7c6eba
-  SMITHY_TS_COMMIT: "57b3119cdf42b7d9d6f6f879fdd243c8dd7c6eba",
+  // https://github.com/smithy-lang/smithy-typescript/compare/57b3119cdf42b7d9d6f6f879fdd243c8dd7c6eba...0076b3f7bb55ad1656178f8cfad890b006c92c1a
+  SMITHY_TS_COMMIT: "0076b3f7bb55ad1656178f8cfad890b006c92c1a",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
