994e4b03e2b6d6827a9b242350e4f07109f4ce29
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-02-18 23:55:42 +0000

    chore(codegen): bump smithy-ts commit

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/57b3119cdf42b7d9d6f6f879fdd243c8dd7c6eba...0076b3f7bb55ad1656178f8cfad890b006c92c1a
-  SMITHY_TS_COMMIT: "0076b3f7bb55ad1656178f8cfad890b006c92c1a",
+  // https://github.com/smithy-lang/smithy-typescript/compare/0076b3f7bb55ad1656178f8cfad890b006c92c1a...d714b0b39bbf3850b72fdfdbba696e5ef6c35ede
+  SMITHY_TS_COMMIT: "d714b0b39bbf3850b72fdfdbba696e5ef6c35ede",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
