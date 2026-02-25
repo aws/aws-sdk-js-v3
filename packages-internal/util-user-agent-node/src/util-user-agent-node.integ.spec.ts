d18e5392a50b217a2b5f7b6d9fe4649ff34e42ef
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-02-25 08:27:56 -0800

    test(util-user-agent-node): add integration test (#7784)

---
+++ packages-internal/util-user-agent-node/src/util-user-agent-node.integ.spec.ts
@@ -0,0 +1,21 @@
+import { requireRequestsFrom } from "@aws-sdk/aws-util-test/src";
+import { S3 } from "@aws-sdk/client-s3";
+import { version as sdkVersion } from "@aws-sdk/client-s3/package.json";
+import { platform, release } from "os";
+import { versions } from "process";
+import { describe, test as it } from "vitest";
+const { version: tscVersion } = await import("typescript");
+
+describe("util-user-agent-node", () => {
+  it("populates user agent string", async () => {
+    const client = new S3({ region: "us-west-2" });
+    requireRequestsFrom(client).toMatch({
+      headers: {
+        "user-agent": `aws-sdk-js/${sdkVersion} ua/2.1 os/${platform()}#${release()} lang/js md/nodejs#${
+          versions.node
+        } api/s3#${sdkVersion} m/E,g`,
+      },
+    });
+    await client.listBuckets();
+  });
+});
