03dad66b98363f09d1f0984b670be110caea8f62
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-03-04 15:51:45 -0800

    chore: set downlevel types to be used in typescript@'<4.5' (#7817)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/1f4cc0608b13d8a96b274f93d6e29322ff21d2ac...9660941247202ad47cdaf60f0fb3c9ec315c000a
-  SMITHY_TS_COMMIT: "9660941247202ad47cdaf60f0fb3c9ec315c000a",
+  // https://github.com/smithy-lang/smithy-typescript/compare/9660941247202ad47cdaf60f0fb3c9ec315c000a...a4d95e6967ac79514ad6a7659c0b400e3c3d94d1
+  SMITHY_TS_COMMIT: "a4d95e6967ac79514ad6a7659c0b400e3c3d94d1",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
