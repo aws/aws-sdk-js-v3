8ddd54420b62212cf3d797050e924efba1fada28
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-03-05 10:23:15 -0800

    chore: bump '@smithy/*' dependencies (#7822)
    Co-authored-by: George Fu <kuhe@users.noreply.github.com>

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/a4d95e6967ac79514ad6a7659c0b400e3c3d94d1...215d2542830dd323d1c38dd0c75c52f72b94105e
-  SMITHY_TS_COMMIT: "215d2542830dd323d1c38dd0c75c52f72b94105e",
+  // https://github.com/smithy-lang/smithy-typescript/compare/215d2542830dd323d1c38dd0c75c52f72b94105e...5eab7ea1c099488605ff3e442e26a7e5c1902d7f
+  SMITHY_TS_COMMIT: "5eab7ea1c099488605ff3e442e26a7e5c1902d7f",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
