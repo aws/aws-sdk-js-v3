51ab65d33391c724aeb83e982517ce0ae3e02bcf
George Fu <kuhe@users.noreply.github.com>
2026-03-31 15:20:31 +0000

    fix(codegen): sync for adaptive retry throttling detection fix

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/e4f9b83a545b23ca0135012990331efd551c76e7...40055f6f093357c26caca53cf6e378e0b5a0380b
-  SMITHY_TS_COMMIT: "40055f6f093357c26caca53cf6e378e0b5a0380b",
+  // https://github.com/smithy-lang/smithy-typescript/compare/40055f6f093357c26caca53cf6e378e0b5a0380b...dfec1b6f4a49745b2f4873e8cc03ab3f150ba96b
+  SMITHY_TS_COMMIT: "dfec1b6f4a49745b2f4873e8cc03ab3f150ba96b",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
