f63952d718727ede32821aac5ef648234d1c746c
George Fu <kuhe@users.noreply.github.com>
2026-04-06 16:42:37 -0400

    chore(scripts): add codegen flag for PR creation

--- scripts/generate-clients/build-smithy-typescript.js
+++ scripts/generate-clients/build-smithy-typescript.js
@@ -2,7 +2,23 @@
 const { access, rm } = require("node:fs/promises");
 const { spawnProcess } = require("../utils/spawn-process");
 
+/**
+ * @param {string} repo
+ * @param {string} commit
+ */
 const buildSmithyTypeScript = async (repo, commit) => {
+  await runWithSpecificSmithyTypeScriptVersion(repo, commit, async () => {
+    // Build smithy-typescript and publish to maven local
+    await spawnProcess("./gradlew", ["clean", "publishToMavenLocal"], { cwd: repo });
+  });
+};
+
+/**
+ * @param {string} repo
+ * @param {string} commit
+ * @param {Function} task
+ */
+const runWithSpecificSmithyTypeScriptVersion = async (repo, commit, task) => {
   let deleteSmithyTsRepo = false;
 
   // Check out smithy-typescript at repo, if it does not exist.
@@ -31,8 +47,7 @@ const buildSmithyTypeScript = async (repo, commit) => {
   }
   await spawnProcess("git", ["checkout", "-b", tempBranchName, commit], { cwd: repo });
 
-  // Build smithy-typescript and publish to maven local
-  await spawnProcess("./gradlew", ["clean", "publishToMavenLocal"], { cwd: repo });
+  await task();
 
   if (deleteSmithyTsRepo) {
     await rm(repo, { recursive: true, force: true });
@@ -43,4 +58,4 @@ const buildSmithyTypeScript = async (repo, commit) => {
   }
 };
 
-module.exports = { buildSmithyTypeScript };
+module.exports = { buildSmithyTypeScript, runWithSpecificSmithyTypeScriptVersion };
