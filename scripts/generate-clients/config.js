037593a791dde5728e7daffce24db8803ddb2b35
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-04-08 15:34:16 -0700

    chore(codegen): bump codegen version to 0.48.0 (#7924)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/dfec1b6f4a49745b2f4873e8cc03ab3f150ba96b...8776710ac30f9ed4fcb322d2dec0ade3f0f7de5e
-  SMITHY_TS_COMMIT: "8776710ac30f9ed4fcb322d2dec0ade3f0f7de5e",
+  // https://github.com/smithy-lang/smithy-typescript/compare/8776710ac30f9ed4fcb322d2dec0ade3f0f7de5e...754a468623a453d9bf9ad8a056192cc8a5627303
+  SMITHY_TS_COMMIT: "754a468623a453d9bf9ad8a056192cc8a5627303",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
