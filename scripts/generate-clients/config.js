b742fb8b7a0e9aa9700a73d10a48e14f6b4a65de
George Fu <kuhe@users.noreply.github.com>
2026-04-13 16:17:16 -0400

    chore(codegen): sync for retry attempt count api (#7927)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/8776710ac30f9ed4fcb322d2dec0ade3f0f7de5e...754a468623a453d9bf9ad8a056192cc8a5627303
-  SMITHY_TS_COMMIT: "754a468623a453d9bf9ad8a056192cc8a5627303",
+  // https://github.com/smithy-lang/smithy-typescript/compare/754a468623a453d9bf9ad8a056192cc8a5627303...8591ae75092354fd90549b459c7166cb76388eb4
+  SMITHY_TS_COMMIT: "8591ae75092354fd90549b459c7166cb76388eb4",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
