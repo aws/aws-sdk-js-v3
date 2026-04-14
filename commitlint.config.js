a296c4066b1b6c8c853addc918601ccd29ea3034
George Fu <kuhe@users.noreply.github.com>
2026-04-14 14:27:27 -0400

    chore: derestrict commit message linting (#7929)

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [2, "always", ["feat", "fix", "docs", "test", "chore"]],
+    "subject-case": [0],
+    "header-max-length": [0],
+    "body-max-line-length": [0],
   },
 };
