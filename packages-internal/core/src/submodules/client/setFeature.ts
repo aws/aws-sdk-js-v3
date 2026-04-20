d2117003151c3e6cb4032b8de4e8041740b2175d
George Fu <kuhe@users.noreply.github.com>
2026-04-20 15:58:28 -0400

    chore(core/client): retry behavior control flag

--- packages-internal/core/src/submodules/client/setFeature.ts
+++ packages-internal/core/src/submodules/client/setFeature.ts
@@ -1,8 +1,16 @@
 import type { AwsHandlerExecutionContext, AwsSdkFeatures } from "@aws-sdk/types";
+import { Retry } from "@smithy/util-retry";
 
 /**
- * @internal
+ * This is temporary code which reads AWS_NEW_RETRIES_2026
+ * and conditionally activates the v2026 retry behavior flag.
+ * todo: this will be removed at a later date.
+ */
+Retry.v2026 ||= typeof process === "object" && process.env?.AWS_NEW_RETRIES_2026 === "true";
+
+/**
  * Indicates to the request context that a given feature is active.
+ * @internal
  *
  * @param context - handler execution context.
  * @param feature - readable name of feature.
