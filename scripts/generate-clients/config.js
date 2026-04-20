273ad5be3adc5288e480655de1c5887a38540fe4
George Fu <kuhe@users.noreply.github.com>
2026-04-20 16:25:23 -0400

    chore(codegen): sync for http2 session concurrency fixes (#7942)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/8591ae75092354fd90549b459c7166cb76388eb4...af3363fd5e03e63e6b737ff9a361454de79d2e3c
-  SMITHY_TS_COMMIT: "af3363fd5e03e63e6b737ff9a361454de79d2e3c",
+  // https://github.com/smithy-lang/smithy-typescript/compare/af3363fd5e03e63e6b737ff9a361454de79d2e3c...c077b47b045f90382003cab83b3bec14cbc1ec29
+  SMITHY_TS_COMMIT: "c077b47b045f90382003cab83b3bec14cbc1ec29",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
