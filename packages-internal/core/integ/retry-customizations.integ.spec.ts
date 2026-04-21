add37970bc551a7c97f6f0984f55f66e0808bf26
George Fu <kuhe@users.noreply.github.com>
2026-04-21 15:01:04 -0400

    fix(client-sts): override IDPCommunicationError to be retryable

--- packages-internal/core/integ/retry-customizations.integ.spec.ts
+++ packages-internal/core/integ/retry-customizations.integ.spec.ts
@@ -1,6 +1,10 @@
+import { requireRequestsFrom } from "@aws-sdk/aws-util-test/src";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { DynamoDBStreams } from "@aws-sdk/client-dynamodb-streams";
+import { STS } from "@aws-sdk/client-sts";
+import { HttpResponse } from "@smithy/protocol-http";
 import { AdaptiveRetryStrategy, Retry, StandardRetryStrategy } from "@smithy/util-retry";
+import { Readable } from "node:stream";
 import { beforeAll, describe, expect, test as it } from "vitest";
 
 describe("retry customization", () => {
@@ -55,4 +59,96 @@ describe("retry customization", () => {
       });
     });
   }
+
+  describe("STS", () => {
+    it("IDPCommunicationError is retryable", async () => {
+      const sts = new STS({
+        region: "us-west-2",
+        credentials: {
+          accessKeyId: "INTEG",
+          secretAccessKey: "INTEG",
+        },
+      });
+
+      requireRequestsFrom(sts)
+        .toMatch({
+          hostname: /./,
+        })
+        .respondWith(
+          new HttpResponse({
+            statusCode: 400,
+            headers: {
+              "content-type": "text/xml",
+            },
+            body: Readable.from(
+              Buffer.from(
+                `
+<ErrorResponse>
+    <Error>
+        <Type>Sender</Type>
+        <Code>IDPCommunicationError</Code>
+        <Message>Oh No 1</Message>
+    </Error>
+    <RequestId>c6104cbe-af31-11e0-8154-cbc7ccf896c7</RequestId>
+</ErrorResponse>
+            `.trim()
+              )
+            ),
+          }),
+          new HttpResponse({
+            statusCode: 400,
+            headers: {
+              "content-type": "text/xml",
+            },
+            body: Readable.from(
+              Buffer.from(
+                `
+<ErrorResponse>
+    <Error>
+        <Type>Sender</Type>
+        <Code>IDPCommunicationError</Code>
+        <Message>Oh No 2</Message>
+    </Error>
+    <RequestId>c6104cbe-af31-11e0-8154-cbc7ccf896c7</RequestId>
+</ErrorResponse>
+            `.trim()
+              )
+            ),
+          }),
+          new HttpResponse({
+            statusCode: 200,
+            headers: {
+              "content-type": "text/xml",
+            },
+            body: Readable.from(
+              Buffer.from(
+                `
+<AssumeRoleResponse xmlns="https://sts.amazonaws.com/doc/2011-06-15/">
+  <AssumeRoleResult>
+  <SourceIdentity>Alice</SourceIdentity>
+    <AssumedRoleUser>
+      <Arn>arn:aws:sts::123456789012:assumed-role/demo/TestAR</Arn>
+      <AssumedRoleId>ARO123EXAMPLE123:TestAR</AssumedRoleId>
+    </AssumedRoleUser>
+    <Credentials>
+      <AccessKeyId>akid</AccessKeyId>
+      <SecretAccessKey>sak</SecretAccessKey>
+      <SessionToken>token</SessionToken>
+      <Expiration>2050-11-09T13:34:41Z</Expiration>
+    </Credentials>
+  </AssumeRoleResult>
+  <ResponseMetadata>
+    <RequestId>c6104cbe-af31-11e0-8154-cbc7ccf896c7</RequestId>
+  </ResponseMetadata>
+</AssumeRoleResponse>
+            `.trim()
+              )
+            ),
+          })
+        );
+
+      const id = await sts.getCallerIdentity({});
+      expect(id.$metadata.attempts).toEqual(3);
+    });
+  });
 });
