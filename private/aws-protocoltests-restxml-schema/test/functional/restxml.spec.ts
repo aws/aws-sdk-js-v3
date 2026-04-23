e7fcdad68f647bdf5bdf933501484a98c9b26b6a
George Fu <kuhe@users.noreply.github.com>
2026-04-23 10:52:02 -0400

    chore(xml-builder): up fast-xml-parser to 5.7.1

--- private/aws-protocoltests-restxml-schema/test/functional/restxml.spec.ts
+++ private/aws-protocoltests-restxml-schema/test/functional/restxml.spec.ts
@@ -8581,18 +8581,21 @@ const compareEquivalentXmlBodies = (
 ): Object => {
   const parseConfig = {
     attributeNamePrefix: "",
+    processEntities: {
+      enabled: true,
+      maxTotalExpansions: Infinity,
+    },
     htmlEntities: true,
     ignoreAttributes: false,
     ignoreDeclaration: true,
     parseTagValue: false,
     trimValues: false,
     tagValueProcessor: (_: any, val: any) => (val.trim() === "" && val.includes("\n") ? "" : undefined),
+    maxNestedTags: Infinity,
   };
 
   const parseXmlBody = (body: string) => {
     const parser = new XMLParser(parseConfig);
-    parser.addEntity("#xD", "\r");
-    parser.addEntity("#10", "\n");
     const parsedObj = parser.parse(body);
     const textNodeName = "#text";
     const key = Object.keys(parsedObj)[0];
