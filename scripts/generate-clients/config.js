3af9e9b5a8e426665904f920bc5f0ca78016a692
George Fu <kuhe@users.noreply.github.com>
2026-04-28 14:43:12 +0000

    chore(codegen): sync for adaptive retry fixes

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/4c4d986d91629c8a9184b2b6c8655180a836db67...2b5dd18207c11dc05fa180288510ee3c327ee905
-  SMITHY_TS_COMMIT: "2b5dd18207c11dc05fa180288510ee3c327ee905",
+  // https://github.com/smithy-lang/smithy-typescript/compare/2b5dd18207c11dc05fa180288510ee3c327ee905...74516bb871b0f65b4eca3af8b280ed217e4f217f
+  SMITHY_TS_COMMIT: "74516bb871b0f65b4eca3af8b280ed217e4f217f",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
