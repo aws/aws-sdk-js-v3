8007131cef5247144be98909984fd3058904ea8a
George Fu <kuhe@users.noreply.github.com>
2026-04-30 19:58:20 +0000

    chore(codegen): smithy-aws-typescript-codegen 0.49.1

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/74516bb871b0f65b4eca3af8b280ed217e4f217f...31358fa7bc02721b54a02fb0ece52064abe29143
-  SMITHY_TS_COMMIT: "31358fa7bc02721b54a02fb0ece52064abe29143",
+  // https://github.com/smithy-lang/smithy-typescript/compare/31358fa7bc02721b54a02fb0ece52064abe29143...ffa66773c40741dfe9aef8dd43dbe683e9253f55
+  SMITHY_TS_COMMIT: "ffa66773c40741dfe9aef8dd43dbe683e9253f55",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
