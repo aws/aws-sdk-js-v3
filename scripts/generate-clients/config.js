becac064e8e41a86f277fac923be1a57ad97cc7d
George Fu <kuhe@users.noreply.github.com>
2026-05-27 19:57:15 +0000

    chore(codegen): sync for undici-http-handler fixes

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/15506ee5b2adffb423789063363476ade1d5bda6...4342743b608d14cd7335078be0aade82b6b2dbf8
-  SMITHY_TS_COMMIT: "4342743b608d14cd7335078be0aade82b6b2dbf8",
+  // https://github.com/smithy-lang/smithy-typescript/compare/4342743b608d14cd7335078be0aade82b6b2dbf8...63b585dbfa4f5b8aa85168ba425c994829629fa1
+  SMITHY_TS_COMMIT: "63b585dbfa4f5b8aa85168ba425c994829629fa1",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
