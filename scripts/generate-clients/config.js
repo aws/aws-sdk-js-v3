6c243a15d8e0b059098d547a7b48311d9e43e352
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-07-07 14:39:37 -0700

    feat(undici-http-handler): bump version to 3.0.0 (#8168)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/9e76e52a04da3929c18e5930a2246a8de2099a62...29ad40ae33b8e2e242b33fcd6e6dfeda7fcec2c6
-  SMITHY_TS_COMMIT: "29ad40ae33b8e2e242b33fcd6e6dfeda7fcec2c6",
+  // https://github.com/smithy-lang/smithy-typescript/compare/29ad40ae33b8e2e242b33fcd6e6dfeda7fcec2c6...3c14866b08c9527b6262000f4194b84ce35a3e80
+  SMITHY_TS_COMMIT: "3c14866b08c9527b6262000f4194b84ce35a3e80",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
