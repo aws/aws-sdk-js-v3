a2e2650398637a623a7f99eaddd2267f26c3f2e0
awstools <awstools@amazon.com>
2026-07-08 18:15:14 +0000

    feat(client-iot-wireless): Default session downlink transmission parameters have been added to the existing Multicast Group APIs. Explicit transmission parameters are no longer required when starting a multicast session during the FUOTA procedure.

--- clients/client-iot-wireless/src/commands/SendDataToWirelessDeviceCommand.ts
+++ clients/client-iot-wireless/src/commands/SendDataToWirelessDeviceCommand.ts
@@ -2,8 +2,7 @@
 import type { MetadataBearer as __MetadataBearer } from "@smithy/types";
 
 import { _ep0, _mw0, command } from "../commandBuilder";
-import type { SendDataToWirelessDeviceRequest } from "../models/models_0";
-import type { SendDataToWirelessDeviceResponse } from "../models/models_1";
+import type { SendDataToWirelessDeviceRequest, SendDataToWirelessDeviceResponse } from "../models/models_1";
 import { SendDataToWirelessDevice$ } from "../schemas/schemas_0";
 
 /**
