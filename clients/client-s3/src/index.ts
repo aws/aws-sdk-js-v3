91723d24d4c3335b182eedc8733014472ef0f086
awstools <awstools@amazon.com>
2026-07-16 18:57:08 +0000

    docs(client-s3): Documentation update for removing the 30 day minimum restriction for transition to Standard-IA or OneZone-IA storage classes

--- clients/client-s3/src/index.ts
+++ clients/client-s3/src/index.ts
@@ -1,7 +1,23 @@
 // smithy-typescript generated code
 /* eslint-disable */
 /**
- * <p></p>
+ * <note>
+ *             <p>For information about using the Amazon S3 API—including authentication, signing requests, code examples, and error handling—see the <a href="https://docs.aws.amazon.com/AmazonS3/latest/developerguide/Welcome.html">Amazon S3 Developer Guide</a>.</p>
+ *          </note>
+ *          <p>Welcome to the <i>Amazon S3 API Reference</i>. This guide explains the Amazon Simple Storage Service (Amazon S3)
+ *       application programming interface (API).</p>
+ *          <p>Welcome to the <i>Amazon S3 API Reference</i>. This guide explains the Amazon Simple Storage Service (Amazon S3)
+ *       application programming interface (API).</p>
+ *          <p>You can use any toolkit that supports HTTP to use the REST API. You can even use a browser
+ *       to fetch objects, as long as they are anonymously readable.</p>
+ *          <p>The REST API uses the standard HTTP headers and status codes, so that standard browsers and toolkits work as expected. In some areas, we have added functionality to HTTP (for example, we added headers to support access control). In these cases, we have done our best to add the new functionality in a way that matched the style of standard HTTP usage.</p>
+ *          <p>The current version of the Amazon S3 API is <code>2006-03-01</code>.</p>
+ *          <p>Amazon S3 supports the REST API.</p>
+ *          <note>
+ *             <p>Support for SOAP over HTTP is deprecated, but it is still available over HTTPS.
+ *         However, new Amazon S3 features will not be supported for SOAP. We recommend that
+ *         you use either this REST API or the Amazon Web Services SDKs.</p>
+ *          </note>
  *
  * @packageDocumentation
  */
