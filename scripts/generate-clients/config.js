785d467fbde2fe2f3720bbb944caefcb198039ed
sid <48153483+siddsriv@users.noreply.github.com>
2026-08-11 12:22:54 -0700

    chore(codegen): update smithy-ts commit to bring in TS6 change (#8262)

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/aac2242b3af7ea8c4ddc00b948a1085cb61f7074...4097414f695596cae768d37ed1a2439d26bf519c
-  SMITHY_TS_COMMIT: "4097414f695596cae768d37ed1a2439d26bf519c",
+  // https://github.com/smithy-lang/smithy-typescript/compare/4097414f695596cae768d37ed1a2439d26bf519c...ec9b4b2b302629bd2fb17b2df6cd2268f041e569
+  SMITHY_TS_COMMIT: "ec9b4b2b302629bd2fb17b2df6cd2268f041e569",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
