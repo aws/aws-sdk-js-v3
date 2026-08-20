754527140fb77814c517c7647f36b5f08940a2ec
smilkuri <smilkuri@amazon.com>
2026-08-20 16:11:48 +0000

    chore(scripts): update smithy-ts commit hash

--- scripts/generate-clients/config.js
+++ scripts/generate-clients/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   // Use full commit hash as we explicitly fetch it.
   // Comparison link (update with previous hash):
-  // https://github.com/smithy-lang/smithy-typescript/compare/4097414f695596cae768d37ed1a2439d26bf519c...ec9b4b2b302629bd2fb17b2df6cd2268f041e569
-  SMITHY_TS_COMMIT: "ec9b4b2b302629bd2fb17b2df6cd2268f041e569",
+  // https://github.com/smithy-lang/smithy-typescript/compare/ec9b4b2b302629bd2fb17b2df6cd2268f041e569...e9151f3e043a19032caad09b05937a4962c5bd13
+  SMITHY_TS_COMMIT: "e9151f3e043a19032caad09b05937a4962c5bd13",
 };
 
 if (module.exports.SMITHY_TS_COMMIT.length < 40) {
