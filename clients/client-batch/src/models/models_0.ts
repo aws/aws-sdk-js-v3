26c213cf4c6158597405aae173787c868ccd6925
awstools <awstools@amazon.com>
2026-08-24 18:19:23 +0000

    docs(client-batch): Doc Update, Add note that UpdatePolicy applies only to EC2 managed compute environments

--- clients/client-batch/src/models/models_0.ts
+++ clients/client-batch/src/models/models_0.ts
@@ -2312,6 +2312,11 @@ export interface DescribeComputeEnvironmentsRequest {
  * <p>Specifies the infrastructure update policy for the Amazon EC2 compute environment. For more information
  *    about infrastructure updates, see <a href="https://docs.aws.amazon.com/batch/latest/userguide/updating-compute-environments.html">Updating compute environments</a> in the
  *     <i>Batch User Guide</i>.</p>
+ *          <important>
+ *             <p>The <code>updatePolicy</code> property applies only to Amazon EC2 managed compute environments.
+ *     Batch doesn't support <code>updatePolicy</code> for Fargate compute environments. If you
+ *     specify it for a Fargate compute environment, Batch ignores it.</p>
+ *          </important>
  * @public
  */
 export interface UpdatePolicy {
