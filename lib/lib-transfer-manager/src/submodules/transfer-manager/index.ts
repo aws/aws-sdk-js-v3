644631aeadb59758797450d8bb1d74bcdaa78ad6
smilkuri <smilkuri@amazon.com>
2026-09-15 03:22:16 +0000

    fix(lib-transfer-manager): bind browser deps via dependency injection

--- lib/lib-transfer-manager/src/submodules/transfer-manager/index.ts
+++ lib/lib-transfer-manager/src/submodules/transfer-manager/index.ts
@@ -1,2 +1,21 @@
-export { S3TransferManager } from "./S3TransferManager";
+import { destroyStreams, joinStreams } from "./join-streams";
+import { bindS3TransferManager } from "./S3TransferManager";
+import { createEmptyReadable, defaultWorkerCount, WorkerHttpHandler } from "./worker-http-handler";
+
+/**
+ * Binds the Node.js implementations of the environment-specific runtime
+ * dependencies (worker-thread HTTP handler and Node.js stream join/destroy
+ * helpers) into the shared transfer manager implementation via
+ * {@link bindS3TransferManager}.
+ *
+ * @alpha
+ */
+export class S3TransferManager extends bindS3TransferManager({
+  joinStreams,
+  destroyStreams,
+  WorkerHttpHandler,
+  createEmptyReadable,
+  defaultWorkerCount,
+}) {}
+
 export type { DownloadToFileRequest, DownloadToFileResponse, IS3TransferManager } from "./types";
